52678835aea507ee324ea9bd88c5b21a0d88a87d
Jeremy Denquin <jdenquin@gmail.com>
2022-02-15 15:59:49 +0100

    Add Memberships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :memberships
+  has_many :organizations, through: :memberships
+
   validates_presence_of :email, :password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_091354) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_15_145334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "organization_id", null: false
+    t.bigint "user_id", null: false
+    t.string "role", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_memberships_on_organization_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
 
   create_table "organizations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
