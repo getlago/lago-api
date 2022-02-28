c53adc380b77baf97a6bcf7c095d26ebb7c13b46
Jeremy Denquin <jdenquin@gmail.com>
2022-02-28 21:24:06 +0100

    misc: Add local env configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << 'api.lago.dev'
+
+  Dotenv.load
 end
