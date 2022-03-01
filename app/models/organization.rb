2c654e9cd6c05f6357bf9c59fbf52402ad247aa5
Jeremy Denquin <jdenquin@gmail.com>
2022-03-01 15:22:26 +0100

    feat: Add Api Key field on Organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,5 +4,18 @@ class Organization < ApplicationRecord
   has_many :memberships
   has_many :users, through: :memberships
 
+  before_create :generate_api_key
+
   validates_presence_of :name
+
+  private
+
+  def generate_api_key
+    api_key = SecureRandom.uuid
+    orga = Organization.find_by(api_key: api_key)
+
+    return generate_api_key if orga.present?
+
+    self.api_key = SecureRandom.uuid
+  end
 end
