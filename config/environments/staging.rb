4ff600a75cc3e057ad3b680408f5f0fe0030a983
Jeremy Denquin <jdenquin@gmail.com>
2022-03-02 15:59:50 +0100

    misc: Add Staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,23 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.active_storage.service = :local
+  config.log_level = :info
+  config.log_tags = [ :request_id ]
+  config.action_mailer.perform_caching = false
+  config.i18n.fallbacks = true
+  config.active_support.report_deprecations = false
+  config.log_formatter = ::Logger::Formatter.new
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  config.active_record.dump_schema_after_migration = false
+end
