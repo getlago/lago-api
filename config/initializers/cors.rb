9a7b6020c8ffbe6f4bd3737c1758452c0848e0e2
Vincent Pochet <vincent.pochet@gmail.com>
2022-03-08 11:54:25 +0100

    misc: Configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # NOTE: use env related constant instead
+    origins 'app.lago.dev'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
