56f18747a9702f93fbb15010e2a561f4f270f00d
Vincent Pochet <vincent.pochet@gmail.com>
2022-03-09 16:54:14 +0100

    Update schema to fix relation with organization

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,6 +7,6 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :organization, Types::OrganizationType
+    field :organizations, [Types::OrganizationType]
   end
 end
--- db/migrate/20220215145334_create_memberships.rb
+++ db/migrate/20220215145334_create_memberships.rb
@@ -4,8 +4,8 @@
 class CreateMemberships < ActiveRecord::Migration[7.0]
   def change
     create_table :memberships do |t|
-      t.references :organization, index: true, null: false
-      t.references :user, index: true, null: false
+      t.references :organization, index: true, null: false, foreign_key: true, type: :uuid
+      t.references :user, index: true, null: false, foreign_key: true, type: :uuid
       t.string :role, null: false
 
       t.timestamps
