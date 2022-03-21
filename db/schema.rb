b6ba0ff69abeda26d9e39b9586036d473c356edd
Vincent Pochet <vincent.pochet@gmail.com>
2022-03-21 17:13:52 +0100

    Fix db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_03_21_135224) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2022_03_21_143758) do
->>>>>>> 49d089f (Add specs for ProductsService)
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
