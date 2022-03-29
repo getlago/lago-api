97e0d59030d7e8b116e6e75169a496b35dd85260
Vincent Pochet <vincent.pochet@gmail.com>
2022-03-29 16:54:38 +0200

    feat: Update GraphQL resolver to allow fetch of plans by their ids

--- app/graphql/resolvers/plans_resolver.rb
+++ app/graphql/resolvers/plans_resolver.rb
@@ -7,18 +7,23 @@ module Resolvers
 
     description 'Query plans of an organization'
 
+    argument :ids, [String], required: false, description: 'List of plan ID to fetch'
     argument :page, Integer, required: false
     argument :limit, Integer, required: false
 
     type Types::Plans::Object.collection_type, null: false
 
-    def resolve(page: nil, limit: nil)
+    def resolve(ids: nil, page: nil, limit: nil)
       validate_organization!
 
-      current_organization
+      plans = current_organization
         .plans
         .page(page)
         .per(limit)
+
+      plans = plans.where(id: ids) if ids.present?
+
+      plans
     end
   end
 end
