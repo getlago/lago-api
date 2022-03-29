17ea71c703b06b87b8a6c237a41caea0bbe2c977
Vincent Pochet <vincent.pochet@gmail.com>
2022-03-29 17:39:22 +0200

    feat: Update GraphQL resolver to allow fetch of billable metrics by their ids

--- app/graphql/resolvers/billable_metrics_resolver.rb
+++ app/graphql/resolvers/billable_metrics_resolver.rb
@@ -7,18 +7,23 @@ module Resolvers
 
     description 'Query billable metrics of an organization'
 
+    argument :ids, Integer, required: false
     argument :page, Integer, required: false
     argument :limit, Integer, required: false
 
     type Types::BillableMetrics::Object.collection_type, null: false
 
-    def resolve(page: nil, limit: nil)
+    def resolve(ids: nil, page: nil, limit: nil)
       validate_organization!
 
-      current_organization
+      metrics = current_organization
         .billable_metrics
         .page(page)
         .per(limit)
+
+      metrics = metrics.where(ids: ids) if ids.present?
+
+      metrics
     end
   end
 end
