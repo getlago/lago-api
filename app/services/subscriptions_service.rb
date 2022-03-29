2ad93589e559c1ac4cbaaf6b150974d01efda77d
Jeremy Denquin <jdenquin@gmail.com>
2022-03-29 11:55:58 +0200

    add service specs

--- app/services/subscriptions_service.rb
+++ app/services/subscriptions_service.rb
@@ -2,18 +2,19 @@
 
 class SubscriptionsService < BaseService
   def create(organization:, params:)
-    unless current_customer(params[:customer_id])
+    unless current_customer(organization.id, params[:customer_id])
       return result.fail!('missing_argument', 'customer does not exists')
     end
 
-    unless current_plan(params[:plan_code])
+    unless current_plan(organization.id, params[:plan_code])
       return result.fail!('missing_argument', 'plan does not exists')
     end
 
+    # TODO: Handle customers with existing plans
     subscription = current_customer.subscriptions.find_or_initialize_by(
       plan_id: current_plan.id
     )
-    subscription.save!
+    subscription.mark_as_active!
 
     result.subscription = subscription
     result
@@ -25,7 +26,7 @@ class SubscriptionsService < BaseService
 
   def current_customer(organization_id = nil, customer_id = nil)
     @current_customer ||= Customer.find_by(
-      id: customer_id,
+      customer_id: customer_id,
       organization_id: organization_id
     )
   end
