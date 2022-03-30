30545a4799434024d031e0bc1795c70f16d1878f
Jeremy Denquin <jdenquin@gmail.com>
2022-03-30 15:54:43 +0200

    fix: increase redis timeout

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'], pool_timeout: 5 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'], pool_timeout: 5 }
 end
