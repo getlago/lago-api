242dc5961084965f71477e1464189ea8e93f3aba
Vincent Pochet <vincent.pochet@gmail.com>
2022-04-01 14:46:51 +0200

    feat: T-951 Create customer with subscription if customer does not exists

--- app/services/subscriptions_service.rb
+++ app/services/subscriptions_service.rb
@@ -2,8 +2,8 @@
 
 class SubscriptionsService < BaseService
   def create(organization:, params:)
-    unless current_customer(organization.id, params[:customer_id])
-      return result.fail!('missing_argument', 'customer does not exists')
+    unless current_customer(organization.id, params[:customer_id]&.strip)
+      return result.fail!('missing_argument', 'unable to find customer')
     end
 
     unless current_plan(organization.id, params[:plan_code])
@@ -12,7 +12,7 @@ class SubscriptionsService < BaseService
 
     # TODO: Handle customers with existing plans
     subscription = current_customer.subscriptions.find_or_initialize_by(
-      plan_id: current_plan.id
+      plan_id: current_plan.id,
     )
     subscription.mark_as_active!
 
@@ -25,16 +25,19 @@ class SubscriptionsService < BaseService
   private
 
   def current_customer(organization_id = nil, customer_id = nil)
-    @current_customer ||= Customer.find_by(
+    return @current_customer if @current_customer
+    return unless customer_id
+
+    @current_customer = Customer.find_or_create_by!(
       customer_id: customer_id,
-      organization_id: organization_id
+      organization_id: organization_id,
     )
   end
 
   def current_plan(organization_id = nil, code = nil)
     @current_plan ||= Plan.find_by(
       code: code,
-      organization_id: organization_id
+      organization_id: organization_id,
     )
   end
 end
