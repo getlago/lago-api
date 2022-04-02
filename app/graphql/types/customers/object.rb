93787b16b673951f58a7a6fc089253f362cd99d5
Vincent Pochet <vincent.pochet@gmail.com>
2022-04-02 20:45:58 +0200

    feat: T-952 Expose customers and subscription in GraphQL API (#49)
    * feat: T-952 Expose customers in GraphQL API
    
    * Expose subscriptions with customer
    
    * fix resolver spec name
    
    Co-authored-by: Jeremy Denquin <jdenquin@gmail.com>

---
+++ app/graphql/types/customers/object.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Types
+  module Customers
+    class Object < Types::BaseObject
+      graphql_name 'Customer'
+
+      field :id, ID, null: false
+      field :subscriptions, [Types::Subscriptions::Object]
+
+      field :customer_id, String, null: false
+      field :name, String
+
+      field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+      field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    end
+  end
+end
+p
---
+++ app/graphql/types/subscriptions/status_type_enum.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  module Subscriptions
+    class StatusTypeEnum < Types::BaseEnum
+      graphql_name 'StatusTypeEnum'
+
+      Subscription::STATUSES.each do |type|
+        value type
+      end
+    end
+  end
+end
