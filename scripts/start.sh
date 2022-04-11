b9fc7655b470821059a26c86c4e74e5f1abda84c
Jeremy Denquin <jdenquin@gmail.com>
2022-04-11 17:55:16 +0200

    misc: remove server pid if existing for production

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $RAILS_ENV == "staging" ]
+if [ "$RAILS_ENV" == "staging" ]
 then
   bundle exec rake db:create
 fi
 
+rm -f ./tmp/pids/server.pid
 bundle exec rake db:migrate
 bundle exec rails s -b 0.0.0.0
