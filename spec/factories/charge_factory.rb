f758345f55b28f54c4af777660d9d78d659a4f33
Jeremy Denquin <jdenquin@gmail.com>
2022-04-11 15:39:23 +0200

    feat: T-984-api-create-some-seeds

--- spec/factories/charge_factory.rb
+++ spec/factories/charge_factory.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :charge do
+    billable_metric
+    plan
+
+    amount_cents { Faker::Number.between(from: 100, to: 500) }
+    amount_currency { 'EUR' }
     
+    pro_rata { false }
+
+    factory :one_time_charge do
+      frequency { :one_time }
+    end
+
+    factory :recurring_charge do
+      frequency { :recurring }
+    end
   end
 end
