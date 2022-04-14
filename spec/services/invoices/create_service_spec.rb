d38276856791404a5da3a5675f72918872c4c220
Vincent Pochet <vincent.pochet@gmail.com>
2022-04-14 18:01:49 +0200

    Drop frequency everywhere

--- spec/services/invoices/create_service_spec.rb
+++ spec/services/invoices/create_service_spec.rb
@@ -23,11 +23,11 @@ RSpec.describe Invoices::CreateService, type: :service do
       create(:one_time_charge, plan: subscription.plan, charge_model: 'standard')
     end
 
-    context 'when billed monthly on beginning of period' do
+    context 'when billed monthly' do
       let(:timestamp) { Time.zone.now.beginning_of_month }
 
       let(:plan) do
-        create(:plan, interval: 'monthly', frequency: 'beginning_of_period')
+        create(:plan, interval: 'monthly')
       end
 
       it 'creates an invoice' do
@@ -53,27 +53,6 @@ RSpec.describe Invoices::CreateService, type: :service do
       end
     end
 
-    context 'when billed monthly on subscription anniversary' do
-      let(:timestamp) { subscription.anniversary_date.beginning_of_day + 2.years }
-      let(:plan) do
-        create(:plan, interval: 'monthly', frequency: 'subscription_date')
-      end
-
-      it 'creates an invoice' do
-        result = invoice_service.create
-
-        aggregate_failures do
-          expect(result).to be_success
-
-          expect(result.invoice.to_date).to eq(timestamp - 1.day)
-          expect(result.invoice.from_date).to eq(timestamp - 1.month)
-          expect(result.invoice.subscription).to eq(subscription)
-          expect(result.invoice.fees.subscription_kind.count).to eq(1)
-          expect(result.invoice.fees.charge_kind.count).to eq(1)
-        end
-      end
-    end
-
     context 'when billed monthly on first month' do
       let(:timestamp) { Time.zone.now.beginning_of_month }
       let(:started_at) { timestamp - 3.days }
@@ -88,7 +67,7 @@ RSpec.describe Invoices::CreateService, type: :service do
       end
 
       let(:plan) do
-        create(:plan, interval: 'monthly', frequency: 'beginning_of_period')
+        create(:plan, interval: 'monthly')
       end
 
       it 'creates an invoice' do
@@ -106,33 +85,11 @@ RSpec.describe Invoices::CreateService, type: :service do
       end
     end
 
-    context 'when billed yearly on beginning of period' do
+    context 'when billed yearly' do
       let(:timestamp) { Time.zone.now.beginning_of_month }
 
       let(:plan) do
-        create(:plan, interval: 'yearly', frequency: 'beginning_of_period')
-      end
-
-      it 'creates an invoice' do
-        result = invoice_service.create
-
-        aggregate_failures do
-          expect(result).to be_success
-
-          expect(result.invoice.to_date).to eq(timestamp - 1.day)
-          expect(result.invoice.from_date).to eq(timestamp - 1.year)
-          expect(result.invoice.subscription).to eq(subscription)
-          expect(result.invoice.fees.subscription_kind.count).to eq(1)
-          expect(result.invoice.fees.charge_kind.count).to eq(1)
-        end
-      end
-    end
-
-    context 'when billed yearly on subscription anniversary' do
-      let(:timestamp) { subscription.anniversary_date.beginning_of_day + 2.years }
-
-      let(:plan) do
-        create(:plan, interval: 'yearly', frequency: 'subscription_date')
+        create(:plan, interval: 'yearly')
       end
 
       it 'creates an invoice' do
@@ -152,7 +109,7 @@ RSpec.describe Invoices::CreateService, type: :service do
 
     context 'when billed yearly on first year' do
       let(:plan) do
-        create(:plan, interval: 'yearly', frequency: 'beginning_of_period')
+        create(:plan, interval: 'yearly')
       end
 
       let(:timestamp) { Time.zone.now.beginning_of_month }
@@ -183,7 +140,7 @@ RSpec.describe Invoices::CreateService, type: :service do
 
     context 'when plan is pay in advance' do
       let(:plan) do
-        create(:plan, interval: 'yearly', frequency: 'beginning_of_period', pay_in_advance: true)
+        create(:plan, interval: 'yearly', pay_in_advance: true)
       end
 
       let(:timestamp) { Time.zone.now.beginning_of_month }
@@ -208,7 +165,7 @@ RSpec.describe Invoices::CreateService, type: :service do
 
     context 'when subscription is terminated and plan is pay in arrear' do
       let(:plan) do
-        create(:plan, interval: 'monthly', frequency: 'beginning_of_period', pay_in_advance: false)
+        create(:plan, interval: 'monthly', pay_in_advance: false)
       end
 
       let(:timestamp) { Time.zone.now.beginning_of_month }
