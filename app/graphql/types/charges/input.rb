d1ab9a1e05ae2ac9599318cec24b4f5dcb29e196
Jeremy Denquin <jdenquin@gmail.com>
2022-04-20 10:08:37 +0200

    fix: add ID on charge update

--- app/graphql/types/charges/input.rb
+++ app/graphql/types/charges/input.rb
@@ -5,6 +5,7 @@ module Types
     class Input < Types::BaseInputObject
       graphql_name 'ChargeInput'
 
+      argument :id, ID, required: false
       argument :billable_metric_id, ID, required: true
       argument :amount_cents, Integer, required: true
       argument :amount_currency, Types::CurrencyEnum, required: true
