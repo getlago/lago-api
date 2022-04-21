6923366218386e89e04eb36205f5ed18f429b414
Vincent Pochet <vincent.pochet@gmail.com>
2022-04-21 11:43:11 +0200

    feat: #87 Implement aggregation logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_153049) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_093051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_153049) do
     t.integer "aggregation_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "field_name"
     t.index ["organization_id", "code"], name: "index_billable_metrics_on_organization_id_and_code", unique: true
     t.index ["organization_id"], name: "index_billable_metrics_on_organization_id"
   end
