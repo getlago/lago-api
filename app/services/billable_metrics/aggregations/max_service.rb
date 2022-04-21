9bc045c372d304edbee668c72ee3765667c48881
Vincent Pochet <vincent.pochet@gmail.com>
2022-04-21 14:47:30 +0200

    Small refact on max aggregation

--- app/services/billable_metrics/aggregations/max_service.rb
+++ app/services/billable_metrics/aggregations/max_service.rb
@@ -4,7 +4,7 @@ module BillableMetrics
   module Aggregations
     class MaxService < BillableMetrics::Aggregations::BaseService
       def aggregate(from_date:, to_date:)
-        result.aggregation = events_scope(from_date: from_date, to_date: to_date)
+        max = events_scope(from_date: from_date, to_date: to_date)
           .maximum(
             ActiveRecord::Base.sanitize_sql_for_conditions(
               [
@@ -12,8 +12,9 @@ module BillableMetrics
                 billable_metric.field_name,
               ],
             ),
-          ) || 0
+          )
 
+        result.aggregation = max || 0
         result
       rescue ActiveRecord::StatementInvalid => e
         result.fail!('aggregation_failure', e.message)
