3fbc6c00528b863b928f3bca2a2b347a470adf9a
Jeremy Denquin <jdenquin@gmail.com>
2022-04-27 16:20:51 +0200

    feat: #108 Add organization update mutation

--- app/graphql/types/organization_type.rb
+++ app/graphql/types/organization_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :api_key, String, null: false
+    field :webhook_url, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
---
+++ spec/graphql/mutations/organizations/update_spec.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Mutations::Organizations::Update, type: :graphql do
+  let(:membership) { create(:membership) }
+  let(:mutation) do
+    <<~GQL
+      mutation($input: UpdateOrganizationInput!) {
+        updateOrganization(input: $input) {
+          webhookUrl
+        }
+      }
+    GQL
+  end
+
+  it 'updates an organization' do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: membership.organization,
+      query: mutation,
+      variables: {
+        input: {
+          webhookUrl: 'http://foo.bar',
+        },
+      },
+    )
+
+    result_data = result['data']['updateOrganization']
+
+    expect(result_data['webhookUrl']).to eq('http://foo.bar')
+  end
+
+  context 'with invalid webhook url' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        current_organization: membership.organization,
+        query: mutation,
+        variables: {
+          input: {
+            webhookUrl: 'bad_url',
+          },
+        },
+      )
+
+      expect_graphql_error(result: result, message: :unprocessable_entity)
+    end
+  end
+
+  context 'without current user' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_organization: membership.organization,
+        query: mutation,
+        variables: {
+          input: {
+            webhookUrl: 'http://foo.bar',
+          },
+        },
+      )
+
+      expect_unauthorized_error(result)
+    end
+  end
+
+  context 'without current organization' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        query: mutation,
+        variables: {
+          input: {
+            webhookUrl: 'http://foo.bar',
+          },
+        },
+      )
+
+      expect_forbidden_error(result)
+    end
+  end
+end
