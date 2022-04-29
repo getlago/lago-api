a3fcd7d8e11ce05af0d17d73a9abdb1cad3d4144
Vincent Pochet <vincent.pochet@gmail.com>
2022-04-29 11:24:02 +0200

    feat: Expose customer's metadata in API

--- app/serializers/v1/customer_serializer.rb
+++ app/serializers/v1/customer_serializer.rb
@@ -8,6 +8,18 @@ module V1
         customer_id: model.customer_id,
         name: model.name,
         created_at: model.created_at.iso8601,
+        country: model.country,
+        address_line1: model.address_line1,
+        address_line2: model.address_line2,
+        state: model.state,
+        zipcode: model.zipcode,
+        email: model.email,
+        city: model.city,
+        url: model.url,
+        phone: model.phone,
+        logo_url: model.logo_url,
+        legal_name: model.legal_name,
+        legal_number: model.legal_number,
       }
     end
   end
