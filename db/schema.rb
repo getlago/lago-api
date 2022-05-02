4c44b24b307475c9a15e8c650b78c7ddaedcf2ec
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-02 11:25:20 +0200

    Do not drop charges and plans vat_rate yet

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_125635) do
     t.bigint "amount_cents", null: false
     t.string "amount_currency", null: false
     t.integer "charge_model", default: 0, null: false
+    t.float "vat_rate"
     t.index ["billable_metric_id"], name: "index_charges_on_billable_metric_id"
     t.index ["plan_id"], name: "index_charges_on_plan_id"
   end
@@ -145,6 +146,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_125635) do
     t.string "amount_currency", null: false
     t.float "trial_period"
     t.boolean "pay_in_advance", default: false, null: false
+    t.float "vat_rate"
     t.index ["code", "organization_id"], name: "index_plans_on_code_and_organization_id", unique: true
     t.index ["organization_id"], name: "index_plans_on_organization_id"
   end
