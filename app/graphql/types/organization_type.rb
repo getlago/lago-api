98f5e17e8728bf6d5c643e792722d0ecd3468537
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-05 10:21:45 +0200

    feat: #137 Organization#vatRate cannot be null (#141)

--- app/graphql/types/organization_type.rb
+++ app/graphql/types/organization_type.rb
@@ -5,7 +5,7 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :api_key, String, null: false
-    field :vat_rate, Float
+    field :vat_rate, Float, null: false
     field :webhook_url, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
