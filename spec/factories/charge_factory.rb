048d4e3904a49cbc7b4589101db0377af007ba1c
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-09 14:42:13 +0200

    Charge factory

--- spec/factories/charge_factory.rb
+++ spec/factories/charge_factory.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
 
     amount_currency { 'EUR' }
 
-    charge_model { 'standard' }
-
     factory :standard_charge do
       charge_model { 'standard' }
       amount_cents { Faker::Number.between(from: 100, to: 500) }
