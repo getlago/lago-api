491eaa94d44c0c15b39353879aa3d9b13532fa57
Jeremy Denquin <jdenquin@gmail.com>
2022-05-12 16:11:57 +0200

    hardfix: base64 decode for rsa key

--- config/initializers/rsa_keys.rb
+++ config/initializers/rsa_keys.rb
@@ -3,7 +3,7 @@
 private_key_string = if Rails.env.development? || Rails.env.test?
   File.read(Rails.root.join('.rsa_private.pem'))
 else
-  Base64.decode(ENV['RSA_PRIVATE_KEY'])
+  Base64.decode64(ENV['RSA_PRIVATE_KEY'])
 end
 
 RsaPrivateKey = OpenSSL::PKey::RSA.new(private_key_string)
