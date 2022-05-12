d3f43c500522207e5da344c6c764eb8eb2a2ca7a
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-12 10:08:37 +0200

    Add specs for customer update

--- spec/factories/customer_factory.rb
+++ spec/factories/customer_factory.rb
@@ -5,5 +5,17 @@ FactoryBot.define do
     organization
     name { Faker::TvShows::SiliconValley.character }
     customer_id { SecureRandom.uuid }
+    country { Faker::Address.country_code }
+    address_line1 { Faker::Address.street_address }
+    address_line2 { Faker::Address.secondary_address }
+    state { Faker::Address.state }
+    zipcode { Faker::Address.zip_code }
+    email { Faker::Internet.email }
+    city { Faker::Address.city }
+    url { Faker::Internet.url }
+    phone { Faker::PhoneNumber.phone_number }
+    logo_url { Faker::Internet.url }
+    legal_name { Faker::Company.name }
+    legal_number { Faker::Company.duns_number }
   end
 end
