5a034ce438797f592b52536ad1f18aa44e3b131b
Jeremy Denquin <jdenquin@gmail.com>
2022-05-12 15:19:45 +0200

    encode public key for endpoint

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class WebhooksController < Api::BaseController
       def public_key
-        render(plain: RsaPublicKey.to_s)
+        render(plain: Base64.encode64(RsaPublicKey.to_s))
       end
     end
   end
