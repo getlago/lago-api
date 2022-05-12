c8117e8c8f3edb6a58e848c7d43067fcc0183f3e
Jérémy Denquin <jdenquin@users.noreply.github.com>
2022-05-12 15:27:56 +0200

    fix: #163 add private key environment variable (#168)
    * fix: #163 add private key environment variable
    
    * fix base64 encode
    
    * fix public key
    
    * encode public key for endpoint
    
    * fix specs

--- config/initializers/rsa_keys.rb
+++ config/initializers/rsa_keys.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.test?
-  private_key_string = File.read(Rails.root.join('.rsa_private.pem'))
-  RsaPrivateKey = OpenSSL::PKey::RSA.new(private_key_string)
-  RsaPublicKey = RsaPrivateKey.public_key
+private_key_string = if Rails.env.development? || Rails.env.test?
+  File.read(Rails.root.join('.rsa_private.pem'))
 else
-  RsaPrivateKey = ENV['RSA_PRIVATE_KEY']
-  RsaPublicKey = ENV['RSA_PUBLIC_KEY']
+  Base64.decode(ENV['RSA_PRIVATE_KEY'])
 end
+
+RsaPrivateKey = OpenSSL::PKey::RSA.new(private_key_string)
+RsaPublicKey = RsaPrivateKey.public_key
