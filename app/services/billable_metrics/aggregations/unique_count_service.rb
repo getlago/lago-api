f582a8bd80eeb2589ae64f94d69cf92690d3f2f4
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-12 14:19:01 +0200

    Filter events with missing aggregation attribute

--- app/services/billable_metrics/aggregations/unique_count_service.rb
+++ app/services/billable_metrics/aggregations/unique_count_service.rb
@@ -5,15 +5,20 @@ module BillableMetrics
     class UniqueCountService < BillableMetrics::Aggregations::BaseService
       def aggregate(from_date:, to_date:)
         result.aggregation = events_scope(from_date: from_date, to_date: to_date)
-          .count(
-            ActiveRecord::Base.sanitize_sql_for_conditions(
-              [
-                'DISTINCT (events.properties->>?)',
-                billable_metric.field_name,
-              ],
-            ),
-          )
+          .where("#{sanitized_field_name} IS NOT NULL")
+          .count("DISTINCT (#{sanitized_field_name})")
         result
+      end
+
+      private
+
+      def sanitized_field_name
+        ActiveRecord::Base.sanitize_sql_for_conditions(
+          [
+            'events.properties->>?',
+            billable_metric.field_name,
+          ],
+        )
       end
     end
   end
