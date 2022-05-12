e93e15c6e480f08faa0e654f0d3f61ebb8a7b1a1
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-12 14:06:26 +0200

    Improve error handling in charge service

--- app/services/fees/charge_service.rb
+++ app/services/fees/charge_service.rb
@@ -11,11 +11,14 @@ module Fees
     def create
       return result if already_billed?
 
+      amount_result = compute_amount
+      return result.fail!(amount_result.error_code, amount_result.error) unless amount_result.success?
+
       new_fee = Fee.new(
         invoice: invoice,
         subscription: subscription,
         charge: charge,
-        amount_cents: compute_amount,
+        amount_cents: amount_result.amount_cents,
         amount_currency: charge.amount_currency,
         vat_rate: customer.applicable_vat_rate,
       )
@@ -38,18 +41,9 @@ module Fees
 
     def compute_amount
       aggregated_events = aggregator.aggregate(from_date: from_date, to_date: invoice.to_date)
-      unless aggregated_events.success?
-        result.fail!('aggregation_failure')
-        raise result.throw_error
-      end
+      return aggregated_events unless aggregated_events.success?
 
-      amount_result = charge_model.apply(value: aggregated_events.aggregation)
-      unless amount_result.success?
-        result.fail!('charge_model_failure')
-        raise result.throw_error
-      end
-
-      amount_result.amount_cents
+      charge_model.apply(value: aggregated_events.aggregation)
     end
 
     def already_billed?
