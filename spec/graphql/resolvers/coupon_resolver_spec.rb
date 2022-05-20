9889e69dc736818891e1742b73bb13b3917ee906
Vincent Pochet <vincent.pochet@gmail.com>
2022-05-20 14:28:58 +0200

    feat: #186 Add GraphQL queries to fetch coupons (#193)

---
+++ spec/graphql/resolvers/coupon_resolver_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Resolvers::CouponResolver, type: :graphql do
+  let(:query) do
+    <<~GQL
+      query($couponId: ID!) {
+        coupon(id: $couponId) {
+          id, name, customerCount
+        }
+      }
+    GQL
+  end
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:coupon) { create(:coupon, organization: organization) }
+
+  it 'returns a single coupon' do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: organization,
+      query: query,
+      variables: {
+        couponId: coupon.id,
+      },
+    )
+
+    coupon_response = result['data']['coupon']
+
+    aggregate_failures do
+      expect(coupon_response['id']).to eq(coupon.id)
+    end
+  end
+
+  context 'without current organization' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        query: query,
+        variables: {
+          couponId: coupon.id,
+        },
+      )
+
+      expect_graphql_error(
+        result: result,
+        message: 'Missing organization id',
+      )
+    end
+  end
+
+  context 'when plan is not found' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        current_organization: organization,
+        query: query,
+        variables: {
+          couponId: 'foo',
+        },
+      )
+
+      expect_graphql_error(
+        result: result,
+        message: 'Resource not found',
+      )
+    end
+  end
+end
---
+++ spec/graphql/resolvers/coupons_resolver_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Resolvers::CouponsResolver, type: :graphql do
+  let(:query) do
+    <<~GQL
+      query {
+        coupons(limit: 5) {
+          collection { id canBeDeleted }
+          metadata { currentPage, totalCount }
+        }
+      }
+    GQL
+  end
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:coupon) { create(:coupon, organization: organization) }
+
+  before { coupon }
+
+  it 'returns a list of coupons' do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: organization,
+      query: query,
+    )
+
+    coupons_response = result['data']['coupons']
+
+    aggregate_failures do
+      expect(coupons_response['collection'].count).to eq(organization.coupons.count)
+      expect(coupons_response['collection'].first['id']).to eq(coupon.id)
+      expect(coupons_response['collection'].first['canBeDeleted']).to be_truthy
+
+      expect(coupons_response['metadata']['currentPage']).to eq(1)
+      expect(coupons_response['metadata']['totalCount']).to eq(1)
+    end
+  end
+
+  context 'without current organization' do
+    it 'returns an error' do
+      result = execute_graphql(current_user: membership.user, query: query)
+
+      expect_graphql_error(
+        result: result,
+        message: 'Missing organization id',
+      )
+    end
+  end
+
+  context 'when not member of the organization' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        current_organization: create(:organization),
+        query: query,
+      )
+
+      expect_graphql_error(
+        result: result,
+        message: 'Not in organization',
+      )
+    end
+  end
+end
