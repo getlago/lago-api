8f65da8d8d3445a65df1dfa37c1028bda11a7963
Jeremy Denquin <jdenquin@gmail.com>
2022-05-24 17:35:53 +0200

    add support of docker host for development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
     if Rails.env.development?
-      origins 'app.lago.dev'
+      origins 'app.lago.dev', 'api'
     elsif ENV['LAGO_FRONT_URL']
       origins URI(ENV['LAGO_FRONT_URL']).host
     end
