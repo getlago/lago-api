8a44bbafff4517686d2de90e0b3f6d2fe14b13cf
Jeremy Denquin <jdenquin@gmail.com>
2022-06-20 17:06:47 +0200

    add files

--- db/migrate/20220620150551_add_slug_to_customers.rb
+++ db/migrate/20220620150551_add_slug_to_customers.rb
@@ -3,9 +3,12 @@
 class AddSlugToCustomers < ActiveRecord::Migration[7.0]
   def change
     add_column :customers, :slug, :string
+<<<<<<< HEAD
     add_column :customers, :sequential_id, :bigint
 
     LagoApi::Application.load_tasks
     Rake::Task['customers:generate_slug'].invoke
+=======
+>>>>>>> 52d78a6... add files
   end
 end
