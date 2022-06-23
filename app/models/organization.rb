2150db3691c389e6a8a4dedf403ef33e8ad1d2cb
Jeremy Denquin <jdenquin@gmail.com>
2022-06-23 15:25:30 +0200

    fix empty logo condition

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -28,7 +28,9 @@ class Organization < ApplicationRecord
   validates :logo, image: { authorized_content_type: %w[image/png image/jpg], max_size: 800.kilobytes }, if: :logo?
 
   def logo_url
-    Rails.application.routes.url_helpers.rails_blob_url(logo, host: ENV['LAGO_API_URL']) if logo.present?
+    return if logo.blank?
+
+    Rails.application.routes.url_helpers.rails_blob_url(logo, host: ENV['LAGO_API_URL'])
   end
 
   private
