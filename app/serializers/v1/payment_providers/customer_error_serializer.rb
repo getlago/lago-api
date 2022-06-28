01d4c9c50fc7877c28f99446798956008c20fe43
Vincent Pochet <vincent.pochet@gmail.com>
2022-06-28 10:10:45 +0200

    Fix error with customer

--- app/serializers/v1/payment_providers/customer_error_serializer.rb
+++ app/serializers/v1/payment_providers/customer_error_serializer.rb
@@ -5,8 +5,8 @@ module V1
     class CustomerErrorSerializer < ModelSerializer
       def serialize
         {
-          lago_id: model.id,
-          lago_customer_id: model.customer_id,
+          lago_customer_id: model.id,
+          customer_id: model.customer_id,
           payment_provider: model.payment_provider,
           provider_error: options[:provider_error],
         }
