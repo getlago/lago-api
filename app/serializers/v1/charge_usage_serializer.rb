6d45c87e7ae29fa6f6ebe6db2e304cedde919a85
Vincent Pochet <vincent.pochet@gmail.com>
2022-07-05 18:24:48 +0200

    Lago_id

--- app/serializers/v1/charge_usage_serializer.rb
+++ app/serializers/v1/charge_usage_serializer.rb
@@ -8,11 +8,11 @@ module V1
         amount_cents: model.amount_cents,
         amount_currency: model.amount_currency,
         charge: {
-          id: model.charge.id,
+          lago_id: model.charge.id,
           charge_model: model.charge.charge_model,
         },
         billable_metric: {
-          id: model.billable_metric.id,
+          lago_id: model.billable_metric.id,
           name: model.billable_metric.name,
           code: model.billable_metric.code,
           aggregation_type: model.billable_metric.aggregation_type,
