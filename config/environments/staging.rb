dc2c290818062db7580509eb52e2a05438d50818
Vincent Pochet <vincent.pochet@gmail.com>
2022-07-07 10:32:22 +0200

    Fix cache store config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,19 +37,17 @@ Rails.application.configure do
   config.hosts << /[a-z0-9-]+\.staging\.getlago\.com/
 
   if ENV['LAGO_MEMCACHE_SERVERS'].present?
-    config.cache_store(:mem_cache_store, ENV['LAGO_MEMCACHE_SERVERS'].split(','))
+    config.cache_store = :mem_cache_store, ENV['LAGO_MEMCACHE_SERVERS'].split(',')
 
   elsif ENV['LAGO_REDIS_CACHE_URL'].present?
-    config.cache_store(
-      :redis_cache_store, {
-        url: ENV['LAGO_REDIS_CACHE_URL'],
-        error_handler: ->(method:, returning:, exception:) {
-          Rails.logger.error(exception.message)
-          Rails.logger.error(exception.backtrace.join("\n"))
+    config.cache_store = :redis_cache_store, {
+      url: ENV['LAGO_REDIS_CACHE_URL'],
+      error_handler: ->(method:, returning:, exception:) {
+        Rails.logger.error(exception.message)
+        Rails.logger.error(exception.backtrace.join("\n"))
 
-          Sentry.capture_exception(exception)
-        },
-      }
-    )
+        Sentry.capture_exception(exception)
+      },
+    }
   end
 end
