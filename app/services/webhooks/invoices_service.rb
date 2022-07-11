1f0b5ef1384a0d79ce44dbd706c9e8dff4299cd2
Jeremy Denquin <jdenquin@gmail.com>
2022-07-11 15:02:09 +0200

    typo!

--- app/services/webhooks/invoices_service.rb
+++ app/services/webhooks/invoices_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Webhooks
-  class InvoiceService < Webhooks::BaseService
+  class InvoicesService < Webhooks::BaseService
     private
 
     def current_organization
