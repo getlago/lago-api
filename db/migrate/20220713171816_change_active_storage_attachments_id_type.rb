6f728a7fda6688ad128613150feec37d1774b6dd
Jeremy Denquin <jdenquin@gmail.com>
2022-07-13 19:30:02 +0200

    hotfix: active storage record id not null

--- db/migrate/20220713171816_change_active_storage_attachments_id_type.rb
+++ db/migrate/20220713171816_change_active_storage_attachments_id_type.rb
@@ -3,6 +3,6 @@
 class ChangeActiveStorageAttachmentsIdType < ActiveRecord::Migration[7.0]
   def change
     remove_column :active_storage_attachments, :record_id
-    add_column :active_storage_attachments, :record_id, :uuid, null: false
+    add_column :active_storage_attachments, :record_id, :uuid
   end
 end
