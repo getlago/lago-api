c36ce47a7ddf7c53eecfb9211c38c2bbe0ab6fe8
Jeremy Denquin <jdenquin@gmail.com>
2022-07-13 19:20:50 +0200

    hotfix: active storage record id type

---
+++ db/migrate/20220713171816_change_active_storage_attachments_id_type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ChangeActiveStorageAttachmentsIdType < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :active_storage_attachments, :record_id
+    add_column :active_storage_attachments, :record_id, :uuid, null: false
+  end
+end
