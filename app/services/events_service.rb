e8441cc3a81ea6fde3d284ddc96092e1dcf0d854
Lovro Colic <lovro.colic.lc@gmail.com>
2022-07-26 14:23:14 +0200

    refactor event service and add specs

--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -3,10 +3,10 @@
 class EventsService < BaseService
   def validate_params(params:)
     mandatory_augments = %i[transaction_id code]
-    mandatory_subscription_augments = %i[subscription_id customer_id]
+    mandatory_subscription_arguments = %i[subscription_id customer_id]
 
     missing_arguments = mandatory_augments.select { |arg| params[arg].blank? }
-    missing_subscription_arguments = mandatory_subscription_augments.select { |arg| params[arg].blank? }
+    missing_subscription_arguments = mandatory_subscription_arguments.select { |arg| params[arg].blank? }
     return result if missing_arguments.blank? && missing_subscription_arguments.count <= 1
 
     result.fail!('missing_mandatory_param', nil, missing_arguments + missing_subscription_arguments)
@@ -22,23 +22,10 @@ class EventsService < BaseService
   end
 
   def create(organization:, params:, timestamp:, metadata:)
-    unless current_customer(organization.id, params[:customer_id], params[:subscription_id])
-      return invalid_customer_error(organization, params)
-    end
+    validate_create(organization, params)
+    return result unless result.success?
 
-    if attached_subscriptions.count > 1
-      return blank_subscription_error(organization, params) if params[:subscription_id].blank?
-      unless attached_subscriptions.include?(params[:subscription_id])
-        return invalid_subscription_error(organization, params)
-      end
-    elsif params[:subscription_id]
-      unless attached_subscriptions.include?(params[:subscription_id])
-        return invalid_subscription_error(organization, params)
-      end
-    else
-      return blank_subscription_error(organization, params) if attached_subscriptions.blank?
-    end
-
+    subscription_id = params[:subscription_id] || attached_subscriptions.first
     event = organization.events.find_by(transaction_id: params[:transaction_id], subscription_id: subscription_id)
 
     if event
@@ -46,13 +33,11 @@ class EventsService < BaseService
       return result
     end
 
-    return invalid_code_error(organization, params) unless valid_code?(params[:code], organization)
-
     event = organization.events.new
     event.code = params[:code]
     event.transaction_id = params[:transaction_id]
     event.customer = current_customer
-    event.subscription_id = params[:subscription_id] || attached_subscriptions.first
+    event.subscription_id = subscription_id
     event.properties = params[:properties] || {}
     event.metadata = metadata || {}
 
@@ -72,14 +57,8 @@ class EventsService < BaseService
   end
 
   def batch_create(organization:, params:, timestamp:, metadata:)
-    return blank_subscription_error(organization, params) if params[:subscription_ids].blank?
-    unless current_customer(organization.id, params[:customer_id], params[:subscription_ids]&.first)
-      return invalid_customer_error(organization, params)
-    end
-
-    invalid_subscriptions = params[:subscription_ids].select { |arg| !attached_subscriptions.include?(arg) }
-    return invalid_subscription_error(organization, params) if invalid_subscriptions.present?
-    return invalid_code_error(organization, params) unless valid_code?(params[:code], organization)
+    validate_batch_create(organization, params)
+    return result unless result.success?
 
     events = []
     ActiveRecord::Base.transaction do
@@ -122,13 +101,45 @@ class EventsService < BaseService
 
   private
 
-  def current_customer(organization_id = nil, customer_id = nil, subscription_id = nil)
+  def validate_create(organization, params)
+    unless current_customer(organization, params[:customer_id], params[:subscription_id])
+      return invalid_customer_error(organization, params)
+    end
+
+    if attached_subscriptions.count > 1
+      return blank_subscription_error(organization, params) if params[:subscription_id].blank?
+      unless attached_subscriptions.include?(params[:subscription_id])
+        return invalid_subscription_error(organization, params)
+      end
+    elsif params[:subscription_id]
+      unless attached_subscriptions.include?(params[:subscription_id])
+        return invalid_subscription_error(organization, params)
+      end
+    else
+      return blank_subscription_error(organization, params) if attached_subscriptions.blank?
+    end
+
+    return invalid_code_error(organization, params) unless valid_code?(params[:code], organization)
+  end
+
+  def validate_batch_create(organization, params)
+    return blank_subscription_error(organization, params) if params[:subscription_ids].blank?
+    unless current_customer(organization, params[:customer_id], params[:subscription_ids]&.first)
+      return invalid_customer_error(organization, params)
+    end
+
+    invalid_subscriptions = params[:subscription_ids].select { |arg| !attached_subscriptions.include?(arg) }
+    return invalid_subscription_error(organization, params) if invalid_subscriptions.present?
+    return invalid_code_error(organization, params) unless valid_code?(params[:code], organization)
+  end
+
+  def current_customer(organization = nil, customer_id = nil, subscription_id = nil)
     return @current_customer if defined? @current_customer
 
     @current_customer = if subscription_id
-                          Subscription.find_by(id: subscription_id).customer
+                          organization.subscriptions.find_by(id: subscription_id)&.customer
                         else
-                          Customer.find_by(customer_id: customer_id, organization_id: organization_id)
+                          Customer.find_by(customer_id: customer_id, organization_id: organization.id)
                         end
   end
 
@@ -151,34 +162,26 @@ class EventsService < BaseService
   end
 
   def attached_subscriptions
-    @attached_subscriptions ||= current_customer.active_subscriptions.pluck(:id)
+    @attached_subscriptions ||= current_customer&.active_subscriptions&.pluck(:id)
   end
 
   def blank_subscription_error(organization, params)
-    result.fail!('missing_argument', 'subscription does not exist')
+    result.fail!('missing_argument', 'subscription does not exist or is not given')
     send_webhook_notice(organization, params)
-
-    return result
   end
 
   def invalid_subscription_error(organization, params)
     result.fail!('invalid_argument', 'subscription_id is invalid')
     send_webhook_notice(organization, params)
-
-    return result
   end
 
   def invalid_code_error(organization, params)
     result.fail!('missing_argument', 'code does not exist')
     send_webhook_notice(organization, params)
-
-    return result
   end
 
   def invalid_customer_error(organization, params)
-    result.fail!('missing_argument', 'customer does not exist')
+    result.fail!('missing_argument', 'customer cannot be found')
     send_webhook_notice(organization, params)
-
-    return result
   end
 end
--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :event do
     organization
     customer
+    subscription
 
     transaction_id { SecureRandom.uuid }
     code { Faker::Name.name.underscore }
