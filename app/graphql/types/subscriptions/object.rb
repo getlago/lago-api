a24153b1fe01c5399ea1572919ce793629d16c53
Lovro Colic <lovro.colic.lc@gmail.com>
2022-07-28 13:06:02 +0200

    add next_pending_start_date in subscription graphQL resolver

--- app/graphql/types/subscriptions/object.rb
+++ app/graphql/types/subscriptions/object.rb
@@ -12,6 +12,7 @@ module Types
       field :status, Types::Subscriptions::StatusTypeEnum
       field :name, String, null: true
       field :next_name, String, null: true
+      field :next_pending_start_date, GraphQL::Types::ISO8601Date
 
       field :anniversary_date, GraphQL::Types::ISO8601Date
       field :canceled_at, GraphQL::Types::ISO8601DateTime
