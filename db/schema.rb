546829f2fa9fc8fc31f7a0bb24133399a8cc7318
Vincent Pochet <vincent.pochet@gmail.com>
2022-07-28 17:18:28 +0200

    feat: Update Subscription data model to support anniversary date

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_101144) do
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.uuid "record_id", null: false
     t.uuid "blob_id", null: false
     t.datetime "created_at", null: false
+    t.uuid "record_id"
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
