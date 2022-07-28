b9525181acb5483250c6ed40904441d37e21af83
Jeremy Denquin <jdenquin@gmail.com>
2022-07-28 14:30:13 +0200

    feat: graphql customer active wallet boolean

--- app/graphql/types/wallet_transactions/object.rb
+++ app/graphql/types/wallet_transactions/object.rb
@@ -9,13 +9,19 @@ module Types
       field :wallet, Types::Wallets::Object
 
       field :transaction_type, Types::WalletTransactions::TransactionTypeEnum, null: false
-      field :status, Types::Wallets::StatusEnum, null: false
+      field :status, Types::WalletTransactions::StatusEnum, null: false
       field :amount, String, null: false
       field :credit_amount, String, null: false
 
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
       field :settled_at, GraphQL::Types::ISO8601DateTime, null: true
+
+      field :has_active_wallet, Boolean, null: false, description: 'Define if a customer has an active wallet'
+
+      def has_active_wallet
+        object.wallets.active.any?
+      end
     end
   end
 end
