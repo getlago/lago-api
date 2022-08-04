15e34c7259e1b5906596c702097bf19402a059c0
Romain Sempé <romain@getlago.com>
2022-08-04 11:01:50 +0200

    fix(multiple_plans): Add index on customer_id for invoices

--- db/migrate/20220727161448_add_customer_to_invoices.rb
+++ db/migrate/20220727161448_add_customer_to_invoices.rb
@@ -1,6 +1,6 @@
 class AddCustomerToInvoices < ActiveRecord::Migration[7.0]
   def change
-    add_reference :invoices, :customer, type: :uuid, foreign_key: true
+    add_reference :invoices, :customer, type: :uuid, foreign_key: true, index: true
 
     LagoApi::Application.load_tasks
     Rake::Task['invoices:fill_customer'].invoke
--- db/schema.rb
+++ db/schema.rb
@@ -325,9 +325,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_101144) do
     t.datetime "updated_at", null: false
     t.uuid "previous_subscription_id"
     t.date "subscription_date"
-    t.integer "billing_time", default: 0, null: false
     t.string "name"
     t.string "unique_id", null: false
+    t.integer "billing_time", default: 0, null: false
     t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
     t.index ["plan_id"], name: "index_subscriptions_on_plan_id"
   end
