ed3ff701cd28f4b16636a777d80d6ad358dc28ef
Jeremy Denquin <jdenquin@gmail.com>
2022-08-09 11:53:47 +0200

    add validates for wallet transactions

--- app/services/wallet_transactions/create_service.rb
+++ app/services/wallet_transactions/create_service.rb
@@ -2,31 +2,25 @@
 
 module WalletTransactions
   class CreateService < BaseService
-    def initialize(wallet_id:, paid_credits:, granted_credits:)
-      super(nil)
+    def create(**args)
+      return result unless valid?(**args)
 
-      @current_wallet = Wallet.find_by(id: wallet_id)
-      @paid_credits = paid_credits
-      @granted_credits = granted_credits
-    end
-
-    def create
-      return result.fail!(code: 'not_found') unless current_wallet
-
-      handle_paid_credits(current_wallet, paid_credits) if paid_credits
-      handle_granted_credits(current_wallet, granted_credts) if granted_credits
+      handle_paid_credits(args[:wallet_id], args[:paid_credits]) if args[:paid_credits]
+      handle_granted_credits(args[:wallet_id], args[:granted_credits]) if args[:granted_credits]
     end
 
     private
 
-    attr_reader :current_wallet, :paid_credits, :granted_credits
-
-    def handle_paid_credits
+    def handle_paid_credits(wallet_id, paid_credits)
       # TODO
     end
 
-    def handle_granted_credits
+    def handle_granted_credits(wallet_id, granted_credits)
       # TODO
+    end
+
+    def valid?(**args)
+      WalletTransactions::ValidateService.new(result, **args).valid?
     end
   end
 end
