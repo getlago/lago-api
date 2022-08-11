ef4eacfa898679cb73bb99025411e864bad26ad9
Vincent Pochet <vincent.pochet@gmail.com>
2022-08-11 18:01:17 +0200

    Add migration task for stripe webhooks

---
+++ db/migrate/20220811155332_refresh_stripe_webhooks.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class RefreshStripeWebhooks < ActiveRecord::Migration[7.0]
+  def change
+    LagoApi::Application.load_tasks
+    Rake::Task['stripe:refresh_registered_webhooks'].invoke
+  end
+end
