c994f2cfdd5295b2db59849f163e2a9c2eb45884
Romain Sempé <romain@getlago.com>
2022-08-12 16:36:51 +0200

    feat(free-units): Calculate running total on sum aggregation

--- app/services/billable_metrics/aggregations/sum_service.rb
+++ app/services/billable_metrics/aggregations/sum_service.rb
@@ -4,10 +4,11 @@ module BillableMetrics
   module Aggregations
     class SumService < BillableMetrics::Aggregations::BaseService
       def aggregate(from_date:, to_date:)
-        result.aggregation = events_scope(from_date: from_date, to_date: to_date)
+        events = events_scope(from_date: from_date, to_date: to_date)
           .where("#{sanitized_field_name} IS NOT NULL")
-          .sum("(#{sanitized_field_name})::numeric")
 
+        result.aggregation = events.sum("(#{sanitized_field_name})::numeric")
+        result.options = { running_total: running_total(events) }
         result
       rescue ActiveRecord::StatementInvalid => e
         result.fail!(code: 'aggregation_failure', message: e.message)
@@ -22,6 +23,15 @@ module BillableMetrics
             billable_metric.field_name,
           ],
         )
+      end
+
+      # NOTES: Return cumulative sum of field_name
+      def running_total(events)
+        total = 0.0
+
+        events.order(created_at: :asc)
+          .pluck(Arel.sql("(#{sanitized_field_name})::numeric"))
+          .map { |x| total += x }
       end
     end
   end
