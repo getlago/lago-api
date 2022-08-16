6b19422efaf0a18dcf3b89ac72a8e919b40bd266
Romain Sempé <romain@getlago.com>
2022-08-16 14:17:38 +0200

    feat(free-units): Migrate existing charges

---
+++ db/migrate/20220816120137_update_properties_on_percentage_charges.rb
@@ -0,0 +1,6 @@
+class UpdatePropertiesOnPercentageCharges < ActiveRecord::Migration[7.0]
+  def change
+    LagoApi::Application.load_tasks
+    Rake::Task['charges:update_properties_for_free_units'].invoke
+  end
+end
