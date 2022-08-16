42d2d3c13601ea4e94089362755dfd9badc73585
Romain Sempé <romain@getlago.com>
2022-08-16 14:42:42 +0200

    feat(free-units): Add validations on free units

--- app/services/charges/validators/percentage_service.rb
+++ app/services/charges/validators/percentage_service.rb
@@ -7,6 +7,8 @@ module Charges
         errors = []
         errors << :invalid_rate unless valid_rate?
         errors << :invalid_fixed_amount unless valid_fixed_amount?
+        errors << :invalid_free_units_per_events unless valid_free_units_per_events?
+        errors << :invalid_free_units_per_total_aggregation unless valid_free_units_per_total_aggregation?
 
         return result.fail!(code: :invalid_properties, message: errors) if errors.present?
 
@@ -31,6 +33,26 @@ module Charges
         return true if fixed_amount.nil?
 
         ::Validators::DecimalAmountService.new(fixed_amount).valid_amount?
+      end
+
+      def free_units_per_events
+        properties['free_units_per_events']
+      end
+
+      def valid_free_units_per_events?
+        return true if free_units_per_events.nil?
+
+        free_units_per_events.is_a?(Integer) && free_units_per_events.positive?
+      end
+
+      def free_units_per_total_aggregation
+        properties['free_units_per_total_aggregation']
+      end
+
+      def valid_free_units_per_total_aggregation?
+        return true if free_units_per_total_aggregation.nil?
+
+        ::Validators::DecimalAmountService.new(free_units_per_total_aggregation).valid_amount?
       end
     end
   end
--- spec/services/charges/validators/percentage_service_spec.rb
+++ spec/services/charges/validators/percentage_service_spec.rb
@@ -71,15 +71,60 @@ RSpec.describe Charges::Validators::PercentageService, type: :service do
       it { expect(percentage_service.validate.error).to include(:invalid_rate) }
     end
 
-    context 'when fixed amount cannot be converted to numeric format' do
+    context 'when free_units_per_events is not an integer' do
+      let(:percentage_properties) do
+        {
+          rate: '0.25',
+          free_units_per_events: 'foo',
+        }
+      end
+
+      it { expect(percentage_service.validate.error).to include(:invalid_free_units_per_events) }
+    end
+
+    context 'when free_units_per_events is negative amount' do
+      let(:percentage_properties) do
+        {
+          rate: '0.25',
+          free_units_per_events: -1,
+        }
+      end
+
+      it { expect(percentage_service.validate.error).to include(:invalid_free_units_per_events) }
+    end
+
+    context 'when fixed amount and free_units_per_total_aggregation cannot be converted to numeric' do
       let(:percentage_properties) do
         {
           rate: '0.25',
           fixed_amount: 'bla',
+          free_units_per_total_aggregation: 'bla',
         }
       end
 
-      it { expect(percentage_service.validate.error).to include(:invalid_fixed_amount) }
+      it 'returns invalid amounts error' do
+        expect(percentage_service.validate.error).to include(
+          :invalid_fixed_amount,
+          :invalid_free_units_per_total_aggregation,
+        )
+      end
+    end
+
+    context 'when given fixed amount and free_units_per_total_aggregation are not string' do
+      let(:percentage_properties) do
+        {
+          rate: '0.25',
+          fixed_amount: 2,
+          free_units_per_total_aggregation: 1,
+        }
+      end
+
+      it 'returns invalid amounts error' do
+        expect(percentage_service.validate.error).to include(
+          :invalid_fixed_amount,
+          :invalid_free_units_per_total_aggregation,
+        )
+      end
     end
 
     context 'when given fixed amount is not string' do
@@ -93,18 +138,26 @@ RSpec.describe Charges::Validators::PercentageService, type: :service do
       it { expect(percentage_service.validate.error).to include(:invalid_fixed_amount) }
     end
 
-    context 'with negative fixed amount' do
+    context 'with negative fixed amount, free_units_per_events and free_units_per_total_aggregation' do
       let(:percentage_properties) do
         {
           rate: '0.25',
           fixed_amount: '-2',
+          free_units_per_events: '-1',
+          free_units_per_total_aggregation: '-1',
         }
       end
 
-      it { expect(percentage_service.validate.error).to include(:invalid_fixed_amount) }
+      it 'returns invalid amounts error' do
+        expect(percentage_service.validate.error).to include(
+          :invalid_fixed_amount,
+          :invalid_free_units_per_events,
+          :invalid_free_units_per_total_aggregation,
+        )
+      end
     end
 
-    context 'without fixed amount' do
+    context 'without fixed_amount, free_units_per_events and free_units_per_total_aggregation' do
       let(:percentage_properties) do
         {
           rate: '0.25'
