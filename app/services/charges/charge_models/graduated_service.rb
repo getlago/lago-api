0c5d9c6b61d751bd79e854811463cc8f4cdc4340
Romain Sempé <romain@getlago.com>
2022-08-18 11:22:25 +0200

    feat(free-units): Calculate percentage based on fixed amount and free units

--- app/services/charges/charge_models/graduated_service.rb
+++ app/services/charges/charge_models/graduated_service.rb
@@ -9,7 +9,7 @@ module Charges
         charge.properties.map(&:with_indifferent_access)
       end
 
-      def compute_amount(value)
+      def compute_amount
         ranges.reduce(0) do |result_amount, range|
           flat_amount = BigDecimal(range[:flat_amount])
           per_unit_amount = BigDecimal(range[:per_unit_amount])
@@ -17,10 +17,10 @@ module Charges
           # NOTE: Add flat amount to the total
           result_amount += flat_amount unless value.zero?
 
-          units = compute_range_units(range[:from_value], range[:to_value], value)
+          units = compute_range_units(range[:from_value], range[:to_value])
           result_amount += units * per_unit_amount
 
-          # NOTE: value is between the bounds of the current range,
+          # NOTE: aggregation_result.aggregation is between the bounds of the current range,
           #       we must stop the loop
           break result_amount if range[:to_value].nil? || range[:to_value] >= value
 
@@ -29,7 +29,7 @@ module Charges
       end
 
       # NOTE: compute how many units to bill in the range
-      def compute_range_units(from_value, to_value, value)
+      def compute_range_units(from_value, to_value)
         # NOTE: value is higher than the to_value of the range
         if to_value && value >= to_value
           return to_value - (from_value.zero? ? 1 : from_value) + 1
@@ -40,6 +40,10 @@ module Charges
 
         # NOTE: value is in the range
         value - from_value + 1
+      end
+
+      def value
+        aggregation_result.aggregation
       end
     end
   end
