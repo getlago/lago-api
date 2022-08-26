d7955d556dc6bd7d22ebbf94718bf7c87799e6cf
LovroColic <lovro.colic.lc@gmail.com>
2022-08-26 11:38:55 +0200

    feat: apply prepaid credits (#402)
    * update spending prepaid credits logic
    
    * apply prepaid credit fixes on invoice
    
    * fix invoice spec
    
    * fix BigDecimal issues
    
    * refactor applying prepaid credits on invoice
    
    * add few style fixes
    
    * feat: add api endpoints for prepaid credit feature (#403)
    
    * add api endpoints for prepaid credit feature
    
    * fix: use internal customer id for graphql and external in api for wallets (#409)
    
    * use internal customer id for graphql and external in api
    
    * rename customer method

--- app/services/wallets/balance/decrease_service.rb
+++ app/services/wallets/balance/decrease_service.rb
@@ -2,7 +2,7 @@
 
 module Wallets
   module Balance
-    class IncreaseService < BaseService
+    class DecreaseService < BaseService
       def initialize(wallet:, credits_amount:)
         super(nil)
 
@@ -14,9 +14,12 @@ module Wallets
         amount = wallet.rate_amount * credits_amount
 
         wallet.update!(
-          balance: wallet.balance + amount,
-          credits_balance: wallet.credits_balance + credits_amount,
-          last_balance_sync_at: Time.current,
+          balance: wallet.balance - amount,
+          credits_balance: wallet.credits_balance - credits_amount,
+          last_balance_sync_at: Time.zone.now,
+          consumed_credits: wallet.consumed_credits + credits_amount,
+          consumed_amount: wallet.consumed_amount + amount,
+          last_consumed_credit_at: Time.current
         )
 
         result.wallet = wallet
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_135203) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_051923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -353,6 +353,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_135203) do
     t.datetime "settled_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "invoice_id"
+    t.index ["invoice_id"], name: "index_wallet_transactions_on_invoice_id"
     t.index ["wallet_id"], name: "index_wallet_transactions_on_wallet_id"
   end
 
@@ -406,6 +408,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_135203) do
   add_foreign_key "plans", "organizations"
   add_foreign_key "subscriptions", "customers"
   add_foreign_key "subscriptions", "plans"
+  add_foreign_key "wallet_transactions", "invoices"
   add_foreign_key "wallet_transactions", "wallets"
   add_foreign_key "wallets", "customers"
 end
