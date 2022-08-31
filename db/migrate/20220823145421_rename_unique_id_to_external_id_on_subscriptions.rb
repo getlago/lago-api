074d5d9c3d2e8305cfe30da039ce491785def632
Romain Sempé <romain@getlago.com>
2022-08-31 10:50:52 +0200

    misc: Add index for external_id on subscriptions

--- db/migrate/20220823145421_rename_unique_id_to_external_id_on_subscriptions.rb
+++ db/migrate/20220823145421_rename_unique_id_to_external_id_on_subscriptions.rb
@@ -1,5 +1,6 @@
 class RenameUniqueIdToExternalIdOnSubscriptions < ActiveRecord::Migration[7.0]
   def change
     rename_column :subscriptions, :unique_id, :external_id
+    add_index :subscriptions, :external_id
   end
 end
