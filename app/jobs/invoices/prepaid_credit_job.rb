648a63b17a88686487b762e6c8b3b02a237fc1a6
LovroColic <lovro.colic.lc@gmail.com>
2022-08-31 10:30:54 +0200

    feat: update wallet when status is changed to succeeded from API (#417)
    * update wallet when status is changed to succeeded from API
    
    * namespace prepaid credit job with invoices
    
    * move job logic into separate service

---
+++ app/jobs/invoices/prepaid_credit_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Invoices
+  class PrepaidCreditJob < ApplicationJob
+    queue_as 'wallets'
+
+    def perform(invoice)
+      Wallets::ApplyPaidCreditsService.new.call(invoice)
+    end
+  end
+end
