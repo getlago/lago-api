1fa4d933293ff1311f6aab1114a96c8dd8c423ef
Lovro Colic <lovro.colic.lc@gmail.com>
2022-09-14 09:10:25 +0200

    fix pronto issues

--- db/migrate/20220907084504_add_override_plan_to_plans.rb
+++ db/migrate/20220907084504_add_override_plan_to_plans.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddOverridePlanToPlans < ActiveRecord::Migration[7.0]
   def change
     add_reference :plans, :overridden_plan, type: :uuid, null: true, index: true, foreign_key: { to_table: :plans }
