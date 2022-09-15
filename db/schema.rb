affed6cbfebfa94cd046a997922fc9ade5519e3b
Vincent Pochet <vincent.pochet@gmail.com>
2022-09-15 11:36:56 +0200

    feat(customers): Add currency field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_130714) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_092730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -157,6 +157,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_130714) do
     t.string "payment_provider"
     t.string "slug"
     t.bigint "sequential_id"
+    t.string "currency"
     t.index ["external_id"], name: "index_customers_on_external_id"
     t.index ["organization_id"], name: "index_customers_on_organization_id"
   end
