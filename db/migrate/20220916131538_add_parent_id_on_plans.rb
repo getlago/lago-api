bc7aa2a9ae9309a4e1ce86e527369f92415d2183
Lovro Colic <lovro.colic.lc@gmail.com>
2022-09-16 15:49:30 +0200

    fix linter issue

--- db/migrate/20220916131538_add_parent_id_on_plans.rb
+++ db/migrate/20220916131538_add_parent_id_on_plans.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddParentIdOnPlans < ActiveRecord::Migration[7.0]
   def change
     add_reference :plans, :parent, type: :uuid, null: true, index: true, foreign_key: { to_table: :plans }
