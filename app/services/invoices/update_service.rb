ea53e0eea193f41a83cbe3b0cacd1975a4763eeb
Vincent Pochet <vincent.pochet@gmail.com>
2022-09-20 10:51:09 +0200

    Refact invoices/update_service to match code standard

--- app/services/invoices/update_service.rb
+++ app/services/invoices/update_service.rb
@@ -1,52 +1,54 @@
 # frozen_string_literal: true
 
-class Invoices::UpdateService < BaseService
-  def update_from_api(invoice_id:, params:)
-    invoice = Invoice.find_by(id: invoice_id)
+module Invoices
+  class UpdateService < BaseService
+    def update_from_api(invoice_id:, params:)
+      invoice = Invoice.find_by(id: invoice_id)
 
-    return result.not_found_failure!(resource: 'invoice') if invoice.blank?
+      return result.not_found_failure!(resource: 'invoice') if invoice.blank?
 
-    unless valid_status?(params[:status])
-      return result.single_validation_failure!(
-        field: :status,
-        error_code: 'value_is_invalid',
+      unless valid_status?(params[:status])
+        return result.single_validation_failure!(
+          field: :status,
+          error_code: 'value_is_invalid',
+        )
+      end
+
+      invoice.status = params[:status] if params.key?(:status)
+      invoice.save!
+
+      handle_prepaid_credits(invoice, params[:status])
+
+      result.invoice = invoice
+      track_payment_status_changed(invoice)
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    end
+
+    private
+
+    def valid_status?(status)
+      Invoice::STATUS.include?(status&.to_sym)
+    end
+
+    def track_payment_status_changed(invoice)
+      SegmentTrackJob.perform_later(
+        membership_id: CurrentContext.membership,
+        event: 'payment_status_changed',
+        properties: {
+          organization_id: invoice.organization.id,
+          invoice_id: invoice.id,
+          payment_status: invoice.status,
+        },
       )
     end
 
-    invoice.status = params[:status] if params.key?(:status)
-    invoice.save!
+    def handle_prepaid_credits(invoice, status)
+      return unless invoice.invoice_type == 'credit'
+      return unless status == 'succeeded'
 
-    handle_prepaid_credits(invoice, params[:status])
-
-    result.invoice = invoice
-    track_payment_status_changed(invoice)
-    result
-  rescue ActiveRecord::RecordInvalid => e
-    result.record_validation_failure!(record: e.record)
-  end
-
-  private
-
-  def valid_status?(status)
-    Invoice::STATUS.include?(status&.to_sym)
-  end
-
-  def track_payment_status_changed(invoice)
-    SegmentTrackJob.perform_later(
-      membership_id: CurrentContext.membership,
-      event: 'payment_status_changed',
-      properties: {
-        organization_id: invoice.organization.id,
-        invoice_id: invoice.id,
-        payment_status: invoice.status,
-      },
-    )
-  end
-
-  def handle_prepaid_credits(invoice, status)
-    return unless invoice.invoice_type == 'credit'
-    return unless status == 'succeeded'
-
-    Invoices::PrepaidCreditJob.perform_later(invoice)
+      Invoices::PrepaidCreditJob.perform_later(invoice)
+    end
   end
 end
