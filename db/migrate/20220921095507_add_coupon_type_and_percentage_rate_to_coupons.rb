34c00e523d5c21136e19c127549096dde015d305
Lovro Colic <lovro.colic.lc@gmail.com>
2022-09-22 10:41:08 +0200

    fix more linter warnings

--- db/migrate/20220921095507_add_coupon_type_and_percentage_rate_to_coupons.rb
+++ db/migrate/20220921095507_add_coupon_type_and_percentage_rate_to_coupons.rb
@@ -5,14 +5,16 @@ class AddCouponTypeAndPercentageRateToCoupons < ActiveRecord::Migration[7.0]
     change_table :coupons, bulk: true do |t|
       t.integer :coupon_type, null: false, default: 0
       t.decimal :percentage_rate, precision: 10, scale: 5
-      t.change :amount_cents, :bigint, null: true
-      t.change :amount_currency, :string, null: true
     end
+
+    change_column_null :coupons, :amount_cents, true
+    change_column_null :coupons, :amount_currency, true
 
     change_table :applied_coupons, bulk: true do |t|
       t.decimal :percentage_rate, precision: 10, scale: 5
-      t.change :amount_cents, :integer, null: true
-      t.change :amount_currency, :string, null: true
     end
+
+    change_column_null :applied_coupons, :amount_cents, true
+    change_column_null :applied_coupons, :amount_currency, true
   end
 end
