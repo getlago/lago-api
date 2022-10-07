4ec025d4f85162c3823b688e349fdc6e03c14beb
Lovro Colic <lovro.colic.lc@gmail.com>
2022-10-07 15:06:14 +0200

    update comments in subscription resolver

--- app/graphql/resolvers/customers/subscriptions_resolver.rb
+++ app/graphql/resolvers/customers/subscriptions_resolver.rb
@@ -11,7 +11,7 @@ module Resolvers
 
       type Types::Subscriptions::Object, null: false
 
-      # FE needs possibility to fetch subscriptions by status. However is status is pending, only
+      # FE needs possibility to fetch subscriptions by status. However if status is pending, only
       # starting_in_the_future subscriptions should be returned since FE handles downgraded (pending)
       # subscriptions a bit different (it checks if next_plan exists and it uses some of next plan's properties
       # that are needed in the UI)
@@ -20,12 +20,17 @@ module Resolvers
         subscriptions = object.subscriptions
 
         return subscriptions.order(created_at: :desc) if statuses.blank?
+
+        # if requested statuses do not include pending ones we should just perform filtering by given statuses
         return subscriptions.where(status: statuses).order(created_at: :desc) unless statuses&.include?('pending')
 
         statuses -= ['pending']
 
+        # if requested status is only pending, we should return only subscriptions that are starting later
         return subscriptions.starting_in_the_future.order(created_at: :desc) if statuses.blank?
 
+        # if requested statuses are array of pending and some other statuses, we should return pending subscriptions
+        # that are starting later or other subscriptions filtered by statuses without pending one
         subscriptions.where(status: statuses).or(subscriptions.starting_in_the_future).order(created_at: :desc)
       end
     end
