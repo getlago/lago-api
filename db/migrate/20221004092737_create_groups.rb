a56fc744806110a97231ef30bb8d247892b43b23
Romain Sempé <romain@getlago.com>
2022-10-10 11:16:42 +0200

    feat(groups): Create table groups

---
+++ db/migrate/20221004092737_create_groups.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups, id: :uuid do |t|
+      t.references :billable_metric, type: :uuid, index: true, foreign_key: { on_delete: :cascade }, null: false
+      t.references :parent_group, type: :uuid, index: true, foreign_key: { to_table: 'groups' }
+      t.string :key, null: false
+      t.string :value, null: false
+      t.integer :status, null: false, default: 0
+      t.timestamps
+    end
+  end
+end
