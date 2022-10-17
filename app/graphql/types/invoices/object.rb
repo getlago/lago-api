d58d567b68215c0dd57f719397c408cd92c8b408
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-10-17 15:24:37 +0200

    fix(invoices): Subscription field should be an array of subscriptions

--- app/graphql/types/invoices/object.rb
+++ app/graphql/types/invoices/object.rb
@@ -23,7 +23,7 @@ module Types
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-      field :subscription, Types::Subscriptions::Object
+      field :subscriptions, [Types::Subscriptions::Object]
       field :plan, Types::Plans::Object
     end
   end
