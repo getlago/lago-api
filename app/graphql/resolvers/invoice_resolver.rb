32943e3b034987201430b49a6292658d9412a053
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-10-18 15:58:33 +0200

    feat(invoice): add single invoice resolver

---
+++ app/graphql/resolvers/invoice_resolver.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class InvoiceResolver < Resolvers::BaseResolver
+    include AuthenticableApiUser
+    include RequiredOrganization
+
+    description 'Query a single Invoice of an organization'
+
+    argument :id, ID, required: true, description: 'Uniq ID of the invoice'
+
+    type Types::Invoices::Object, null: true
+
+    def resolve(id:)
+      validate_organization!
+
+      current_organization.invoices.find(id)
+    rescue ActiveRecord::RecordNotFound
+      not_found_error(resource: 'invoice')
+    end
+  end
+end
