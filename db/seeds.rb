9307fdb0af1ca74a2d1d9a45f587958d54f94736
Vincent Pochet <vincent.pochet@gmail.com>
2022-10-20 16:38:23 +0200

    feat(credit_notes): Add credit note seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -203,3 +203,28 @@ Subscription.all.find_each do |subscription|
     timestamp: Time.zone.now - 2.months,
   ).create
 end
+
+Invoice.all.find_each do |invoice|
+  fee = invoice.fees.sample
+  amount = fee.amount_cents / 2
+  next if amount.zero?
+
+  credit_note = CreditNote.create!(
+    customer: invoice.customer,
+    invoice: invoice,
+    credit_amount_cents: amount,
+    credit_amount_currency: fee.amount_currency,
+    credit_status: :available,
+    balance_amount_cents: amount,
+    balance_amount_currency: fee.amount_currency,
+    reason: :other,
+    total_amount_cents: amount,
+    total_amount_currency: fee.amount_currency,
+  )
+
+  credit_note.items.create!(
+    fee: fee,
+    credit_amount_cents: amount,
+    credit_amount_currency: fee.amount_currency,
+  )
+end
