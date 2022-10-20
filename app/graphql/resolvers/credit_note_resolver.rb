97a9f206cabbf206e8fa8455b823c3a3dcdd49a4
Vincent Pochet <vincent@getlago.com>
2022-10-20 15:25:30 +0200

    feat(credit_notes): Add a single credit note GraphQL resolver (#552)

---
+++ app/graphql/resolvers/credit_note_resolver.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class CreditNoteResolver < Resolvers::BaseResolver
+    include AuthenticableApiUser
+    include RequiredOrganization
+
+    description 'Query a single credit note'
+
+    argument :id, ID, required: true, description: 'Uniq ID of the credit note'
+
+    type Types::CreditNotes::Object, null: true
+
+    def resolve(id: nil)
+      validate_organization!
+
+      current_organization.credit_notes.find(id)
+    rescue ActiveRecord::RecordNotFound
+      not_found_error(resource: 'credit_note')
+    end
+  end
+end
---
+++ spec/graphql/resolvers/credit_note_resolver_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Resolvers::CreditNoteResolver, type: :graphql do
+  let(:query) do
+    <<-GQL
+      query($creditNoteId: ID!) {
+        creditNote(id: $creditNoteId) {
+          id
+          number
+          creditStatus
+          reason
+          totalAmountCents
+          totalAmountCurrency
+          creditAmountCents
+          creditAmountCurrency
+          balanceAmountCents
+          balanceAmountCurrency
+          totalAmountCents
+          totalAmountCurrency
+          createdAt
+          updatedAt
+          fileUrl
+          invoice { id number }
+          items {
+            id
+            creditAmountCents
+            creditAmountCurrency
+            createdAt
+            fee { id amountCents itemType itemCode itemName }
+          }
+        }
+      }
+    GQL
+  end
+
+  let(:membership) { create(:membership) }
+
+  let(:customer) { create(:customer, organization: membership.organization) }
+  let(:credit_note) { create(:credit_note, customer: customer) }
+
+  it 'returns a single credit note' do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: customer.organization,
+      query: query,
+      variables: {
+        creditNoteId: credit_note.id,
+      },
+    )
+
+    credit_note_response = result['data']['creditNote']
+
+    aggregate_failures do
+      expect(credit_note_response['id']).to eq(credit_note.id)
+    end
+  end
+end
