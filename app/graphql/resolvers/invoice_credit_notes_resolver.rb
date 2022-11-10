27da4493307d4844547fb0e5836fdfce177cd83a
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-11-10 16:46:46 +0100

    feat: add invoice_credit_notes_resolver

---
+++ app/graphql/resolvers/invoice_credit_notes_resolver.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class InvoiceCreditNotesResolver < Resolvers::BaseResolver
+    include AuthenticableApiUser
+    include RequiredOrganization
+
+    description "Query invoice's credit note"
+
+    argument :invoice_id, ID, required: true, description: 'Uniq ID of the invoice'
+    argument :page, Integer, required: false
+    argument :limit, Integer, required: false
+
+    type Types::CreditNotes::Object.collection_type, null: true
+
+    def resolve(invoice_id: nil, page: nil, limit: nil)
+      validate_organization!
+
+      Invoice.find(invoice_id)
+        .credit_notes
+        .order(created_at: :desc)
+        .page(page)
+        .per(limit)
+    rescue ActiveRecord::RecordNotFound
+      not_found_error(resource: 'invoice')
+    end
+  end
+end
