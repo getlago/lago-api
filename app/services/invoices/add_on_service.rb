966f5145e2ca03d35babb68fde9d17c0b9bc9277
Vincent Pochet <vincent.pochet@gmail.com>
2022-11-15 16:53:54 +0100

    New invoice should not be legacy

--- app/services/invoices/add_on_service.rb
+++ app/services/invoices/add_on_service.rb
@@ -16,9 +16,6 @@ module Invoices
           issuing_date: issuing_date,
           invoice_type: :add_on,
           payment_status: :pending,
-
-          # NOTE: Apply credits before VAT, will be changed with credit note feature
-          legacy: true,
           vat_rate: customer.applicable_vat_rate,
         )
 
