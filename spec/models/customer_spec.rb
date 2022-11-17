1c60ff92d8af4fe916c40739237af0f2717a571c
Vincent Pochet <vincent.pochet@gmail.com>
2022-11-17 12:13:07 +0100

    Fix review

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -36,9 +36,6 @@ RSpec.describe Customer, type: :model do
 
       customer.timezone = 'foo'
       expect(customer).not_to be_valid
-
-      customer.timezone = nil
-      expect(customer).to be_valid
     end
   end
 
