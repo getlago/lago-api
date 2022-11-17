46479d1ef6eb7bafbaf6b9f96230564e7b771ad7
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-11-17 17:29:04 +0000

    feat(graph) Add some graphQL fields for UI purpose

--- app/graphql/types/credit_notes/object.rb
+++ app/graphql/types/credit_notes/object.rb
@@ -40,6 +40,7 @@ module Types
 
       field :file_url, String, null: true
 
+      field :customer, Types::Customers::Object, null: false
       field :invoice, Types::Invoices::Object
       field :items, [Types::CreditNoteItems::Object], null: false
 
