cba3318c33e7dfe932f9685ba5b5c8c84b499504
LovroColic <lovro.colic.lc@gmail.com>
2022-11-17 11:45:45 +0100

    feat (coupons): allow applying multiple coupons to the customer (#604)
    * allow applying multiple coupons to the customer
    
    * fix linter issues
    
    * add amount_cents_remaining in graphql response

--- app/graphql/types/applied_coupons/object.rb
+++ app/graphql/types/applied_coupons/object.rb
@@ -14,9 +14,18 @@ module Types
       field :percentage_rate, Float, null: true
       field :frequency, Types::Coupons::FrequencyEnum, null: false
       field :frequency_duration, Integer, null: true
+      field :frequency_duration_remaining, Integer, null: true
+      field :amount_cents_remaining, Integer, null: true
 
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :terminated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+      def amount_cents_remaining
+        return nil if object.recurring?
+        return nil if object.coupon.percentage?
+
+        object.amount_cents - object.credits.sum(:amount_cents)
+      end
     end
   end
 end
