f295b65f3dcc2385685786af1a903a5e909606e5
LovroColic <lovro.colic.lc@gmail.com>
2022-11-18 10:25:29 +0100

    feat (coupons): add API endpoint for fetching all applied coupons (#606)
    * add API endpoint for fetching all applied coupons
    
    * fix linter offence

--- app/serializers/v1/applied_coupon_serializer.rb
+++ app/serializers/v1/applied_coupon_serializer.rb
@@ -9,15 +9,27 @@ module V1
         coupon_code: model.coupon.code,
         lago_customer_id: model.customer.id,
         external_customer_id: model.customer.external_id,
+        status: model.status,
         amount_cents: model.amount_cents,
+        amount_cents_remaining: amount_cents_remaining,
         amount_currency: model.amount_currency,
         percentage_rate: model.percentage_rate,
         frequency: model.frequency,
         frequency_duration: model.frequency_duration,
+        frequency_duration_remaining: model.frequency_duration_remaining,
         expiration_date: model.coupon.expiration_date&.iso8601,
         created_at: model.created_at.iso8601,
         terminated_at: model.terminated_at&.iso8601,
       }
+    end
+
+    private
+
+    def amount_cents_remaining
+      return nil if model.recurring?
+      return nil if model.coupon.percentage?
+
+      model.amount_cents - model.credits.sum(:amount_cents)
     end
   end
 end
