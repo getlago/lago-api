9e0090105caae9690c714461eaceba6487db2c60
Romain Sempé <romain@getlago.com>
2022-12-01 20:22:37 +0100

    feat(grace_period): Refresh draft invoice service

---
+++ app/services/invoices/refresh_draft_service.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Invoices
+  class RefreshDraftService < BaseService
+    def self.call(...)
+      new(...).call
+    end
+
+    def initialize(invoice:)
+      @invoice = invoice
+      super
+    end
+
+    def call
+      return result unless invoice.draft?
+
+      ActiveRecord::Base.transaction do
+        invoice.fees.destroy_all
+        invoice.credit_notes.destroy_all
+        invoice.credits.destroy_all
+        invoice.wallet_transactions.destroy_all
+
+        invoice.update!(
+          issuing_date: issuing_date,
+          vat_rate: invoice.customer.applicable_vat_rate,
+        )
+
+        Invoices::CalculateFeesService.call(
+          invoice: invoice,
+          timestamp: invoice.created_at.to_i,
+        )
+      end
+    end
+
+    private
+
+    attr_accessor :invoice
+
+    def issuing_date
+      @issuing_date ||= Time.current.in_time_zone(invoice.customer.applicable_timezone).to_date
+    end
+  end
+end
