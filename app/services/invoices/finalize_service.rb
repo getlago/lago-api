69a0d445e4c4164680796cde6ad9760e285988ad
Romain Sempé <romain@getlago.com>
2022-12-01 20:47:26 +0100

    feat(grace_period): Finalize invoice service

---
+++ app/services/invoices/finalize_service.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Invoices
+  class FinalizeService < BaseService
+    def self.call(...)
+      new(...).call
+    end
+
+    def initialize(invoice:)
+      @invoice = invoice
+      super
+    end
+
+    def call
+      ActiveRecord::Base.transaction do
+        result = Invoices::RefreshDraftService.call(invoice: invoice)
+        invoice.finalized!
+        SendWebhookJob.perform_later(:invoice, invoice) if invoice.organization.webhook_url?
+        Invoices::Payments::CreateService.new(invoice).call
+        track_invoice_created(invoice)
+
+        result
+      end
+    end
+
+    private
+
+    attr_accessor :invoice
+
+    def track_invoice_created(invoice)
+      SegmentTrackJob.perform_later(
+        membership_id: CurrentContext.membership,
+        event: 'invoice_created',
+        properties: {
+          organization_id: invoice.organization.id,
+          invoice_id: invoice.id,
+          invoice_type: invoice.invoice_type,
+        },
+      )
+    end
+  end
+end
