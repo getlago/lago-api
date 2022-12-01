fc99670b7ccf1faad0d581cd1475c538b1ef2b4d
Jeremy Denquin <jdenquin@gmail.com>
2022-12-01 11:57:50 +0100

    fix(redis): Password is defaulted to empty string by docker compose

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,7 +6,7 @@ redis_config = {
   pool_timeout: 5,
 }
 
-redis_config = redis_config.merge({ password: ENV['REDIS_PASSWORD'] }) if ENV['REDIS_PASSWORD'].present?
+redis_config = redis_config.merge({ password: ENV['REDIS_PASSWORD'] }) unless ENV['REDIS_PASSWORD'].empty?
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
