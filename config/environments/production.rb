484bf4f8a5229a2fc3e0df4ac26c06824188f40a
Jeremy Denquin <jdenquin@gmail.com>
2022-12-01 12:04:25 +0100

    fix tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
       },
     }
 
-    unless ENV['LAGO_REDIS_CACHE_PASSWORD'].empty?
+    if ENV['LAGO_REDIS_CACHE_PASSWORD'].present? && !ENV['LAGO_REDIS_CACHE_PASSWORD'].empty?
       cache_store_config = cache_store_config.merge({ password: ENV['LAGO_REDIS_CACHE_PASSWORD'] })
     end
 
