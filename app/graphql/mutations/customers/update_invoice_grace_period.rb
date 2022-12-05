183c3fe8a957230876ad88e180f10879ecbd404c
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-12-05 17:27:39 +0100

    feat: add UpdateInvoiceGracePeriod mutation

---
+++ app/graphql/mutations/customers/update_invoice_grace_period.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Customers
+    class UpdateInvoiceGracePeriod < BaseMutation
+      include AuthenticableApiUser
+
+      graphql_name 'UpdateCustomerInvoiceGracePeriod'
+      description 'Assign the invoice grace period to Customers'
+
+      argument :id, ID, required: true
+      argument :invoice_grace_period, Integer, required: false
+
+      type Types::Customers::SingleObject
+
+      def resolve(id:, invoice_grace_period:)
+        result = ::Customers::UpdateService.new(context[:current_user]).update(
+          id: id,
+          invoice_grace_period: invoice_grace_period,
+        )
+
+        result.success? ? result.customer : result_error(result)
+      end
+    end
+  end
+end
