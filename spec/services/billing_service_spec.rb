de2e050e9a2b13d2a1cae896c539034bbf3a50c4
Vincent Pochet <vincent.pochet@gmail.com>
2022-12-19 14:32:10 +0100

    Fix condition for timestamp query on invoice subscription

--- spec/services/billing_service_spec.rb
+++ spec/services/billing_service_spec.rb
@@ -367,6 +367,25 @@ RSpec.describe BillingService, type: :service do
           expect { billing_service.call }.not_to have_enqueued_job
         end
       end
+
+      context 'when timestamp is an integer' do
+        let(:invoice_subscription) do
+          create(
+            :invoice_subscription,
+            subscription: subscription,
+            recurring: true,
+            properties: {
+              timestamp: (subscription_at - 1.hour).to_i,
+            },
+          )
+        end
+
+        it 'does not enqueue a job' do
+          travel_to(subscription_at) do
+            expect { billing_service.call }.not_to have_enqueued_job
+          end
+        end
+      end
     end
   end
 end
