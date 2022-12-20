a73a52179ee744d4a50822af4fe7c1b11e924657
Romain Sempé <romain@getlago.com>
2022-12-20 11:04:51 +0100

    feat(grace_period): Refresh draft invoice via GraphQL

---
+++ app/graphql/mutations/invoices/refresh.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Invoices
+    class Refresh < BaseMutation
+      include AuthenticableApiUser
+      include RequiredOrganization
+
+      graphql_name 'RefreshInvoice'
+      description 'Refresh a draft invoice'
+
+      argument :id, ID, required: true
+
+      type Types::Invoices::Object
+
+      def resolve(**args)
+        validate_organization!
+        result = ::Invoices::RefreshDraftService.call(
+          invoice: current_organization.invoices.find_by(id: args[:id]),
+        )
+        result.success? ? result.invoice : result_error(result)
+      end
+    end
+  end
+end
---
+++ spec/graphql/mutations/invoices/refresh_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Mutations::Invoices::Refresh, type: :graphql do
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:customer) { create(:customer, organization: organization) }
+  let(:invoice) { create(:invoice, customer: customer) }
+
+  let(:mutation) do
+    <<~GQL
+      mutation($input: RefreshInvoiceInput!) {
+        refreshInvoice(input: $input) {
+          id
+          issuingDate
+        }
+      }
+    GQL
+  end
+
+  it 'refreshes the given invoice' do
+    freeze_time do
+      result = execute_graphql(
+        current_user: membership.user,
+        current_organization: organization,
+        query: mutation,
+        variables: {
+          input: { id: invoice.id },
+        },
+      )
+
+      result_data = result['data']['refreshInvoice']
+
+      aggregate_failures do
+        expect(result_data['id']).to be_present
+        expect(result_data['issuingDate']).to eq(Time.current.to_date.to_s)
+      end
+    end
+  end
+
+  context 'without current user' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_organization: membership.organization,
+        query: mutation,
+        variables: {
+          input: { id: invoice.id },
+        },
+      )
+
+      expect_unauthorized_error(result)
+    end
+  end
+
+  context 'without current organization' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        query: mutation,
+        variables: {
+          input: { id: invoice.id },
+        },
+      )
+
+      expect_forbidden_error(result)
+    end
+  end
+end
