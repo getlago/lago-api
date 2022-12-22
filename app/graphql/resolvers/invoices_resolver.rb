636470931bf8510d31a6e26b7aaf4ec280847336
Lovro Colic <lovro.colic.lc@gmail.com>
2022-12-22 10:21:34 +0100

    add invoice resolver status filter

--- app/graphql/resolvers/invoices_resolver.rb
+++ app/graphql/resolvers/invoices_resolver.rb
@@ -10,11 +10,12 @@ module Resolvers
     argument :ids, [ID], required: false, description: 'List of invoice IDs to fetch'
     argument :page, Integer, required: false
     argument :limit, Integer, required: false
+    argument :status, Types::Invoices::StatusTypeEnum, required: false
     argument :payment_status, [Types::Invoices::PaymentStatusTypeEnum], required: false
 
     type Types::Invoices::Object.collection_type, null: false
 
-    def resolve(ids: nil, page: nil, limit: nil, payment_status: nil)
+    def resolve(ids: nil, page: nil, limit: nil, payment_status: nil, status: nil)
       validate_organization!
 
       invoices = current_organization
@@ -22,7 +23,7 @@ module Resolvers
         .page(page)
         .per(limit)
 
-      # TODO: Filter later by status (draft / finalized)
+      invoices = invoices.where(status: status) if status.present?
       invoices = invoices.where(payment_status: payment_status) if payment_status.present?
       invoices = invoices.where(id: ids) if ids.present?
 
