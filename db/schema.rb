3110fd40cc340b0aa6813e112989379770ff37ae
Romain Sempé <romain@getlago.com>
2022-12-22 17:46:42 +0100

    fix: Finalize all existing invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_111209) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_164226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
