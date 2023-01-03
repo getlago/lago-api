d2abf084b56ca39277598c57802971c61cfa38b6
LovroColic <lovro.colic.lc@gmail.com>
2023-01-03 13:09:49 +0100

    fix (webhooks): change webhook public key endpoint reponse type (#689)
    * change webhook public key endpoint reponse type
    
    * return deprecated public_key logic

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -3,8 +3,20 @@
 module Api
   module V1
     class WebhooksController < Api::BaseController
+      # Deprecated method
       def public_key
         render(plain: Base64.encode64(RsaPublicKey.to_s))
+      end
+
+      def json_public_key
+        render(
+          json: {
+            webhook: {
+              public_key: Base64.encode64(RsaPublicKey.to_s),
+            },
+          },
+          status: :ok,
+        )
       end
     end
   end
--- spec/requests/api/v1/webhooks_spec.rb
+++ spec/requests/api/v1/webhooks_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Api::V1::WebhooksController, type: :request do
       expect(response.body).to eq(Base64.encode64(RsaPublicKey.to_s))
     end
   end
+
+  describe 'json_public_key' do
+    it 'returns the public key in JSON response used to verify webhook signatures' do
+      get_with_token(organization, '/api/v1/webhooks/json_public_key')
+
+      expect(response).to have_http_status(:success)
+      expect(json[:webhook][:public_key]).to eq(Base64.encode64(RsaPublicKey.to_s))
+    end
+  end
 end
