9792a1ba2d743efb4a14178935d81e61b4512e59
Romain Sempé <romain@getlago.com>
2023-01-05 10:44:58 +0100

    feat(grace_period): Change credit note items fee id to be nullable

--- db/schema.rb
+++ db/schema.rb
@@ -134,7 +134,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_152449) do
 
   create_table "credit_note_items", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "credit_note_id", null: false
-    t.uuid "fee_id", null: false
+    t.uuid "fee_id"
     t.bigint "amount_cents", default: 0, null: false
     t.string "amount_currency", null: false
     t.datetime "created_at", null: false
