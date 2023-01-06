3c309fd55eea6d1715894760804f20aa3fa06524
Lovro Colic <lovro.colic.lc@gmail.com>
2023-01-06 15:35:20 +0100

    add support for search plans

--- app/graphql/resolvers/plans_resolver.rb
+++ app/graphql/resolvers/plans_resolver.rb
@@ -10,20 +10,24 @@ module Resolvers
     argument :ids, [String], required: false, description: 'List of plan ID to fetch'
     argument :page, Integer, required: false
     argument :limit, Integer, required: false
+    argument :search_term, String, required: false
 
     type Types::Plans::Object.collection_type, null: false
 
-    def resolve(ids: nil, page: nil, limit: nil)
+    def resolve(ids: nil, page: nil, limit: nil, search_term: nil)
       validate_organization!
 
-      plans = current_organization
-        .plans
-        .page(page)
-        .per(limit)
+      query = ::Queries::PlanQuery.new(organization: current_organization)
+      result = query.call(
+        search_term:,
+        page:,
+        limit:,
+        filters: {
+          ids:,
+        },
+      )
 
-      plans = plans.where(id: ids) if ids.present?
-
-      plans
+      result.plans
     end
   end
 end
