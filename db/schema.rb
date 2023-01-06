248cad55c1e395d02428806fc90b12ecbf9dbd0c
Vincent Pochet <vincent@getlago.com>
2023-01-06 16:29:21 +0100

    fix: Turn integer amount fields into bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_150636) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_06_152449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_150636) do
   create_table "applied_add_ons", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "add_on_id", null: false
     t.uuid "customer_id", null: false
-    t.integer "amount_cents", null: false
+    t.bigint "amount_cents", null: false
     t.string "amount_currency", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -72,7 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_150636) do
     t.uuid "coupon_id", null: false
     t.uuid "customer_id", null: false
     t.integer "status", default: 0, null: false
-    t.integer "amount_cents"
+    t.bigint "amount_cents"
     t.string "amount_currency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
