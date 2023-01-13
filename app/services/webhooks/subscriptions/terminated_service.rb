ff9ce36c61804396562e6c606276237e89ee0f5d
Jérémy Denquin <jdenquin@users.noreply.github.com>
2023-01-13 15:48:41 +0100

    feat(subscriptions): Add terminated webhook (#737)
    * feat(subscriptions): Add terminated webhook
    
    * add sub wh
    
    * Update app/services/webhooks/subscriptions/terminated_service.rb
    
    fix object type
    
    Co-authored-by: Vincent Pochet <vincent@getlago.com>
    
    * add current organization
    
    Co-authored-by: Vincent Pochet <vincent@getlago.com>

---
+++ app/services/webhooks/subscriptions/terminated_service.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Webhooks
+  module Subscriptions
+    class TerminatedService < Webhooks::BaseService
+      def current_organization
+        @current_organization ||= object.organization
+      end
+
+      def object_serializer
+        ::V1::SubscriptionSerializer.new(
+          object,
+          root_name: 'subscription',
+        )
+      end
+
+      def webhook_type
+        'subscription.terminated'
+      end
+
+      def object_type
+        'subscription'
+      end
+    end
+  end
+end
