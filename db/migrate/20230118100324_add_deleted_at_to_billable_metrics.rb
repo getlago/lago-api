f654504debd09092632020bd4ced61349a88a2e8
Romain Sempé <romain@getlago.com>
2023-01-18 11:06:35 +0100

    feat(delete-bm): Add deleted_at fields to impacted tables

---
+++ db/migrate/20230118100324_add_deleted_at_to_billable_metrics.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AddDeletedAtToBillableMetrics < ActiveRecord::Migration[7.0]
+  def change
+    add_column :billable_metrics, :deleted_at, :datetime
+    add_column :charges, :deleted_at, :datetime
+    add_column :groups, :deleted_at, :datetime
+    add_column :group_properties, :deleted_at, :datetime
+    add_column :events, :deleted_at, :datetime
+    add_column :persisted_events, :deleted_at, :datetime
+
+    add_index :billable_metrics, :deleted_at
+    add_index :charges, :deleted_at
+    add_index :groups, :deleted_at
+    add_index :group_properties, :deleted_at
+    add_index :events, :deleted_at
+    add_index :persisted_events, :deleted_at
+  end
+end
