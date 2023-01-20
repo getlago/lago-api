d6f5103e336d50f8c62f06038122ccd2bf468c24
Romain Sempé <romain@getlago.com>
2023-01-20 10:22:00 +0100

    feat(delete_bm): Add activeSubscriptionsCount and draftInvoicesCount on API

--- app/serializers/v1/billable_metric_serializer.rb
+++ app/serializers/v1/billable_metric_serializer.rb
@@ -12,7 +12,24 @@ module V1
         created_at: model.created_at.iso8601,
         field_name: model.field_name,
         group: model.active_groups_as_tree,
+        active_subscriptions_count:,
+        draft_invoices_count:,
       }
+    end
+
+    private
+
+    def active_subscriptions_count
+      model.plans.joins(:subscriptions).merge(Subscription.active).count
+    end
+
+    def draft_invoices_count
+      model.plans
+        .joins(subscriptions: [:invoices])
+        .merge(Invoice.draft)
+        .select(:invoice_id)
+        .distinct
+        .count
     end
   end
 end
