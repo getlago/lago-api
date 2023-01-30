7287ab12f761d6529d54c1cc164a12f4f7d88802
Romain Sempé <romain@getlago.com>
2023-01-30 16:01:24 +0100

    feat(delete_plan): Scenario spec for deleting a plan

--- spec/scenarios/invoices_spec.rb
+++ spec/scenarios/invoices_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe 'Invoices Scenarios', :invoices_scenarios, type: :request do
+describe 'Invoices Scenarios', :scenarios, type: :request do
   let(:organization) { create(:organization, webhook_url: nil) }
 
   context 'when subscription is terminated with a grace period' do
@@ -359,43 +359,5 @@ describe 'Invoices Scenarios', :invoices_scenarios, type: :request do
           .and change { invoice.reload.status }.from('draft').to('finalized')
       end
     end
-  end
-
-  # This performs any enqueued-jobs, and continues doing so until the queue is empty.
-  # Lots of the jobs enqueue other jobs as part of their work, and this ensures that
-  # everything that's supposed to happen, happens.
-  def perform_all_enqueued_jobs
-    until enqueued_jobs.empty?
-      perform_enqueued_jobs
-      Sidekiq::Worker.drain_all
-    end
-  end
-
-  def create_subscription(params)
-    post_with_token(organization, '/api/v1/subscriptions', { subscription: params })
-    perform_all_enqueued_jobs
-  end
-
-  def perform_billing
-    Clock::SubscriptionsBillerJob.perform_later
-    perform_all_enqueued_jobs
-  end
-
-  def refresh_invoice(invoice)
-    put_with_token(organization, "/api/v1/invoices/#{invoice.id}/refresh", {})
-  end
-
-  def finalize_invoice(invoice)
-    put_with_token(organization, "/api/v1/invoices/#{invoice.id}/finalize", {})
-  end
-
-  def terminate_subscription(subscription)
-    delete_with_token(organization, "/api/v1/subscriptions/#{subscription.external_id}")
-    perform_all_enqueued_jobs
-  end
-
-  def create_or_update_customer(params)
-    post_with_token(organization, '/api/v1/customers', { customer: params })
-    perform_all_enqueued_jobs
   end
 end
