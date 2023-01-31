e69578463370403c7c77d66cf38f804b3104084f
Vincent Pochet <vincent@getlago.com>
2023-01-31 16:20:43 +0100

    feat(delete_add_on): Merge destroy methods for GraphQL and API

--- spec/services/add_ons/destroy_service_spec.rb
+++ spec/services/add_ons/destroy_service_spec.rb
@@ -3,47 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe AddOns::DestroyService, type: :service do
-  subject(:destroy_service) { described_class.new(membership.user) }
+  subject(:destroy_service) { described_class.new(add_on:) }
 
   let(:membership) { create(:membership) }
   let(:organization) { membership.organization }
 
-  let(:add_on) { create(:add_on, organization: organization) }
+  let(:add_on) { create(:add_on, organization:) }
 
   describe 'destroy' do
     before { add_on }
 
     it 'destroys the add-on' do
-      expect { destroy_service.destroy(add_on.id) }
+      expect { destroy_service.call }
         .to change(AddOn, :count).by(-1)
     end
 
     context 'when add-on is not found' do
+      let(:add_on) { nil }
+
       it 'returns an error' do
-        result = destroy_service.destroy(nil)
+        result = destroy_service.call
 
-        expect(result).not_to be_success
-        expect(result.error.error_code).to eq('add_on_not_found')
-      end
-    end
-  end
-
-  describe 'destroy_from_api' do
-    let(:add_on) { create(:add_on, organization: organization) }
-
-    it 'destroys the add_on' do
-      code = add_on.code
-
-      expect { destroy_service.destroy_from_api(organization: organization, code: code) }
-        .to change(AddOn, :count).by(-1)
-    end
-
-    context 'when add-on is not found' do
-      it 'returns an error' do
-        result = destroy_service.destroy_from_api(organization: organization, code: 'invalid12345')
-
-        expect(result).not_to be_success
-        expect(result.error.error_code).to eq('add_on_not_found')
+        aggregate_failures do
+          expect(result).not_to be_success
+          expect(result.error.error_code).to eq('add_on_not_found')
+        end
       end
     end
   end
