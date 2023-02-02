cbaeb2daaa63c54c407ec2701388fd0a916f1a89
Vincent Pochet <vincent@getlago.com>
2023-02-02 15:30:50 +0100

    Deduplicate customers

--- db/migrate/20230202110407_add_index_to_payment_provider_customers.rb
+++ db/migrate/20230202110407_add_index_to_payment_provider_customers.rb
@@ -2,6 +2,24 @@
 
 class AddIndexToPaymentProviderCustomers < ActiveRecord::Migration[7.0]
   def change
+    reversible do |dir|
+      dir.up do
+        # Remove duplicated customers before adding new index
+        payment_customers = PaymentProviderCustomers::BaseCustomer.group(:customer_id, :type)
+          .having('COUNT(id) > 1')
+          .select('COUNT(id) AS customer_count, customer_id, type')
+
+        payment_customers.each do |payment_customer|
+          customers = PaymentProviderCustomers::BaseCustomer.where(
+            customer_id: payment_customer.customer_id,
+            type: payment_customer.type,
+          ).order(updated_at: :desc)
+
+          customers[1..].each(&:destroy)
+        end
+      end
+    end
+
     add_index :payment_provider_customers, %i[customer_id type], unique: true
     remove_index :payment_provider_customers, :customer_id
   end
