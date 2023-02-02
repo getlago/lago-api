3ab872fa8f66b69976de95f26fc52c5686c4dc36
Romain Sempé <romain@getlago.com>
2023-02-02 17:32:46 +0100

    misc: Refactor AddOns::UpdateService

--- spec/services/add_ons/update_service_spec.rb
+++ spec/services/add_ons/update_service_spec.rb
@@ -3,14 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe AddOns::UpdateService, type: :service do
-  subject(:update_service) { described_class.new(membership.user) }
+  subject(:add_ons_service) { described_class.new(add_on:, params: update_args) }
 
   let(:membership) { create(:membership) }
   let(:organization) { membership.organization }
+  let(:add_on) { create(:add_on, organization:) }
 
-  let(:add_on) { create(:add_on, organization: organization) }
-
-  describe 'update' do
+  describe 'call' do
     before { add_on }
 
     let(:update_args) do
@@ -25,8 +24,7 @@ RSpec.describe AddOns::UpdateService, type: :service do
     end
 
     it 'updates the add-on' do
-      result = update_service.update(**update_args)
-
+      result = add_ons_service.call
       expect(result).to be_success
 
       aggregate_failures do
@@ -49,76 +47,13 @@ RSpec.describe AddOns::UpdateService, type: :service do
       end
 
       it 'returns an error' do
-        result = update_service.update(**update_args)
+        result = add_ons_service.call
 
         aggregate_failures do
           expect(result).not_to be_success
           expect(result.error).to be_a(BaseService::ValidationFailure)
           expect(result.error.messages[:name]).to eq(['value_is_mandatory'])
         end
-      end
-    end
-  end
-
-  describe 'update_from_api' do
-    let(:add_on) { create(:add_on, organization: organization) }
-    let(:name) { 'New Add On' }
-    let(:update_args) do
-      {
-        name: name,
-        code: 'code',
-        description: 'desc',
-        amount_cents: 100,
-        amount_currency: 'EUR',
-      }
-    end
-
-    it 'updates the add-on' do
-      result = subject.update_from_api(
-        organization: organization,
-        code: add_on.code,
-        params: update_args,
-      )
-
-      aggregate_failures do
-        expect(result).to be_success
-
-        add_on_result = result.add_on
-        expect(add_on_result.id).to eq(add_on.id)
-        expect(add_on_result.name).to eq(update_args[:name])
-        expect(add_on_result.code).to eq(update_args[:code])
-        expect(add_on_result.description).to eq(update_args[:description])
-      end
-    end
-
-    context 'with validation errors' do
-      let(:name) { nil }
-
-      it 'returns an error' do
-        result = subject.update_from_api(
-          organization: organization,
-          code: add_on.code,
-          params: update_args,
-        )
-
-        aggregate_failures do
-          expect(result).not_to be_success
-          expect(result.error).to be_a(BaseService::ValidationFailure)
-          expect(result.error.messages[:name]).to eq(['value_is_mandatory'])
-        end
-      end
-    end
-
-    context 'when add-on is not found' do
-      it 'returns an error' do
-        result = subject.update_from_api(
-          organization: organization,
-          code: 'fake_code12345',
-          params: update_args,
-        )
-
-        expect(result).not_to be_success
-        expect(result.error.error_code).to eq('add_on_not_found')
       end
     end
   end
