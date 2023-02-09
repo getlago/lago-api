c775e52e5a810df3200739caed095a8f9d4a0847
Lovro Colic <lovro.colic.lc@gmail.com>
2023-02-09 09:58:34 +0100

    add ID as in billingConfiguratio response

--- app/graphql/types/organizations/billing_configuration.rb
+++ app/graphql/types/organizations/billing_configuration.rb
@@ -5,6 +5,7 @@ module Types
     class BillingConfiguration < Types::BaseObject
       graphql_name 'OrganizationBillingConfiguration'
 
+      field :id, ID, null: false
       field :vat_rate, Float, null: false
       field :invoice_footer, String
       field :invoice_grace_period, Integer, null: false
