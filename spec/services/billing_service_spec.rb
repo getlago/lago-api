2e1b5680835c706df6f001fa26831a7ff5c280dd
Jérémy Denquin <jdenquin@users.noreply.github.com>
2023-02-13 10:23:25 +0100

    fix(specs): Fix a flaky test on subscriptions date (#820)
    * fix(specs): Fix a flaky test on subscriptions date
    
    * remove comment

--- spec/services/billing_service_spec.rb
+++ spec/services/billing_service_spec.rb
@@ -152,8 +152,6 @@ RSpec.describe BillingService, type: :service do
       let(:interval) { :weekly }
       let(:billing_time) { :anniversary }
 
-      let(:subscription_at) { DateTime.now.prev_occurring(DateTime.now.strftime('%A').downcase.to_sym) }
-
       let(:current_date) do
         DateTime.parse('20 Jun 2022').prev_occurring(subscription_at.strftime('%A').downcase.to_sym)
       end
