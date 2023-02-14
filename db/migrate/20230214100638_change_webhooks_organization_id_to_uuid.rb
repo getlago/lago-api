c1e28b7ff8fa69ce397d2997ee089371e7ffe282
Jeremy Denquin <jeremy@getlago.com>
2023-02-14 11:56:20 +0100

    add null false

--- db/migrate/20230214100638_change_webhooks_organization_id_to_uuid.rb
+++ db/migrate/20230214100638_change_webhooks_organization_id_to_uuid.rb
@@ -4,6 +4,6 @@ class ChangeWebhooksOrganizationIdToUuid < ActiveRecord::Migration[7.0]
   def change
     remove_column :webhooks, :organization_id, :bigint
 
-    add_reference :webhooks, :organization, type: :uuid, index: true
+    add_reference :webhooks, :organization, type: :uuid, index: true, null: false
   end
 end
