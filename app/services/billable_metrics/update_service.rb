ab04aeb1042daef90267959c8d8eba668e4a3613
Vincent Pochet <vincent@getlago.com>
2023-02-16 16:44:00 +0100

    misc: Merge API and GraphQL BillableMetric update services (#839)

--- app/services/billable_metrics/update_service.rb
+++ app/services/billable_metrics/update_service.rb
@@ -2,61 +2,43 @@
 
 module BillableMetrics
   class UpdateService < BaseService
-    def update(**args)
-      metric = result.user.billable_metrics.find_by(id: args[:id])
-      return result.not_found_failure!(resource: 'billable_metric') unless metric
+    def initialize(billable_metric:, params:)
+      @billable_metric = billable_metric
+      @params = params
 
-      metric.name = args[:name]
-      metric.description = args[:description] if args[:description]
+      super
+    end
+
+    def call
+      return result.not_found_failure!(resource: 'billable_metric') unless billable_metric
+
+      billable_metric.name = params[:name] if params.key?(:name)
+      billable_metric.description = params[:description] if params.key?(:description)
 
       # NOTE: Only name and description are editable if billable metric
       #       is attached to subscriptions
-      unless metric.attached_to_subscriptions?
-        metric.code = args[:code]
-        metric.aggregation_type = args[:aggregation_type]&.to_sym
-        metric.field_name = args[:field_name]
-
-        if args.key?(:group)
-          group_result = update_groups(metric, args[:group])
-          return group_result if group_result.error
-        end
-      end
-
-      metric.save!
-
-      result.billable_metric = metric
-      result
-    rescue ActiveRecord::RecordInvalid => e
-      result.record_validation_failure!(record: e.record)
-    end
-
-    def update_from_api(organization:, code:, params:)
-      metric = organization.billable_metrics.find_by(code: code)
-      return result.not_found_failure!(resource: 'billable_metric') unless metric
-
-      metric.name = params[:name] if params.key?(:name)
-      metric.description = params[:description] if params.key?(:description)
-
-      unless metric.attached_to_subscriptions?
-        metric.code = params[:code] if params.key?(:code)
-        metric.aggregation_type = params[:aggregation_type] if params.key?(:aggregation_type)
-        metric.field_name = params[:field_name] if params.key?(:field_name)
+      unless billable_metric.attached_to_subscriptions?
+        billable_metric.code = params[:code] if params.key?(:code)
+        billable_metric.aggregation_type = params[:aggregation_type]&.to_sym if params.key?(:aggregation_type)
+        billable_metric.field_name = params[:field_name] if params.key?(:field_name)
 
         if params.key?(:group)
-          group_result = update_groups(metric, params[:group])
+          group_result = update_groups(billable_metric, params[:group])
           return group_result if group_result.error
         end
       end
 
-      metric.save!
+      billable_metric.save!
 
-      result.billable_metric = metric
+      result.billable_metric = billable_metric
       result
     rescue ActiveRecord::RecordInvalid => e
       result.record_validation_failure!(record: e.record)
     end
 
     private
+
+    attr_reader :billable_metric, :params
 
     def update_groups(metric, group_params)
       ActiveRecord::Base.transaction do
