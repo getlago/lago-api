30ecf0fc06d7d16fc8d5db3a8e8438a960f690d4
Romain Sempé <romain@getlago.com>
2023-02-28 10:29:54 +0100

    feat(instant_charge): Add instant to Charge via GraphQL

--- app/graphql/types/charges/input.rb
+++ app/graphql/types/charges/input.rb
@@ -8,6 +8,7 @@ module Types
       argument :id, ID, required: false
       argument :billable_metric_id, ID, required: true
       argument :charge_model, Types::Charges::ChargeModelEnum, required: true
+      argument :instant, Boolean, required: false
 
       argument :properties, Types::Charges::PropertiesInput, required: false
       argument :group_properties, [Types::Charges::GroupPropertiesInput], required: false
