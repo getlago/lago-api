185bf2cb706113354332fff7d9b73cea539ce643
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-03-02 14:31:59 +0100

    chore(wallet_transaction): remove unused resolver and associated type

--- app/graphql/types/wallet_transactions/single_object.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  module WalletTransactions
-    class SingleObject < Types::WalletTransactions::Object
-      graphql_name 'WalletTransactionDetails'
-    end
-  end
-end
