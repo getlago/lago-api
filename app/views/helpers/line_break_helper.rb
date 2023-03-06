ece8c42dfa832b4962f935ae73f79f251f3e2a38
Jeremy Denquin <jeremy@getlago.com>
2023-03-06 18:10:20 +0100

    escape and html safe

--- app/views/helpers/line_break_helper.rb
+++ app/views/helpers/line_break_helper.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require 'rack/utils'
+
 class LineBreakHelper
   def self.break_lines(text)
-    text.to_s.gsub(/\n/, '<br/>').sanitize.html_safe
+    escaped_text = Rack::Utils.escape_html(text)
+    escaped_text.to_s.gsub(/\n/, '<br/>').html_safe
   end
 end
