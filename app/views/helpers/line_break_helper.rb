f007c04076d3937d58dabe6b5bf0d3f2ae16c88a
Jeremy Denquin <jeremy@getlago.com>
2023-03-06 18:11:04 +0100

    use erb escape

--- app/views/helpers/line_break_helper.rb
+++ app/views/helpers/line_break_helper.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-require 'rack/utils'
-
 class LineBreakHelper
   def self.break_lines(text)
-    escaped_text = Rack::Utils.escape_html(text)
+    escaped_text = ERB::Util.html_escape(text)
     escaped_text.to_s.gsub(/\n/, '<br/>').html_safe
   end
 end
