d7319c5e7b382111b8f4ddafd81de7f5a27b0ce3
Jeremy Denquin <jeremy@getlago.com>
2023-03-06 18:13:47 +0100

    ignore html safe warning

--- app/views/helpers/line_break_helper.rb
+++ app/views/helpers/line_break_helper.rb
@@ -3,6 +3,6 @@
 class LineBreakHelper
   def self.break_lines(text)
     escaped_text = ERB::Util.html_escape(text)
-    escaped_text.to_s.gsub(/\n/, '<br/>').html_safe
+    escaped_text.to_s.gsub(/\n/, '<br/>').html_safe # rubocop:disable Rails/OutputSafety
   end
 end
