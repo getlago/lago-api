fd903b3e6e3272bddf93305176ca4e1290f597bb
Vincent Pochet <vincent@getlago.com>
2023-03-07 14:23:12 +0100

    feat(instant_charge): Add event_id to fees for tracking instant fee source

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_145104) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_131524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -296,6 +296,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_145104) do
     t.uuid "invoiceable_id"
     t.integer "events_count"
     t.uuid "group_id"
+    t.string "instant_event_id"
     t.index ["applied_add_on_id"], name: "index_fees_on_applied_add_on_id"
     t.index ["charge_id"], name: "index_fees_on_charge_id"
     t.index ["group_id"], name: "index_fees_on_group_id"
