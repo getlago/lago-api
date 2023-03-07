fd84c0463c5e889be61fdab702f70d1cbb773a20
Jeremy Denquin <jdenquin@gmail.com>
2023-03-07 11:51:27 +0100

    last review

--- app/views/helpers/line_break_helper.rb
+++ app/views/helpers/line_break_helper.rb
@@ -2,7 +2,7 @@
 
 class LineBreakHelper
   def self.break_lines(text)
-    escaped_text = ERB::Util.html_escape(text)
-    escaped_text.to_s.gsub(/\n/, '<br/>').html_safe # rubocop:disable Rails/OutputSafety
+    escaped_text = ERB::Util.html_escape(text.to_s)
+    escaped_text.gsub(/\n/, '<br/>').html_safe # rubocop:disable Rails/OutputSafety
   end
 end
