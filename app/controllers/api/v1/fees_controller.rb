01407ce279c00214729012289d1982b8b4b2c8dc
Vincent Pochet <vincent@getlago.com>
2023-03-09 11:51:07 +0100

    fix(instant_charge): Fix scope for show fee route

--- app/controllers/api/v1/fees_controller.rb
+++ app/controllers/api/v1/fees_controller.rb
@@ -4,7 +4,13 @@ module Api
   module V1
     class FeesController < Api::BaseController
       def show
-        fee = current_organization.fees.find_by(id: params[:id])
+        # NOTE: instant fees might not be linked to any invoice, but add_on fees does not have any subscriptions
+        #       so we need a bit of logic to find the fee in the right organization scope
+        fee = Fee.left_joins(:invoice)
+          .left_joins(subscription: :customer)
+          .where('COALESCE(invoices.organization_id, customers.organization_id) = ?', current_organization.id)
+          .find_by(id: params[:id])
+
         return not_found_error(resource: 'fee') unless fee
 
         render(json: ::V1::FeeSerializer.new(fee, root_name: 'fee'))
