f099d051674d450034f005459c34f760e43a3d4b
Lovro Colic <lovro.colic.lc@gmail.com>
2023-03-20 16:43:36 +0100

    add graphql support for generating customer portal url

---
+++ app/services/customer_portal/generate_url_service.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module CustomerPortal
+  class GenerateUrlService < BaseService
+    def initialize(customer:)
+      @customer = customer
+
+      super
+    end
+
+    def call
+      return result.not_found_failure!(resource: 'customer') if customer.blank?
+
+      public_authenticator = ActiveSupport::MessageVerifier.new(ENV['SECRET_KEY_BASE'])
+      message = public_authenticator.generate(customer.id, expires_in: 12.hours)
+
+      result.url = "#{ENV['LAGO_FRONT_URL']}/customer-portal/#{message}"
+
+      result
+    end
+
+    private
+
+    attr_reader :customer
+  end
+end
