b5429bc98774a9b8bb86bd5ae7ce193d01fd89ad
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-03-29 16:46:07 +0200

    feat(password_reset): add resolver for single object

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,34 +9,35 @@ module Types
 
     field :current_user, resolver: Resolvers::CurrentUserResolver
 
-    field :add_ons, resolver: Resolvers::AddOnsResolver
     field :add_on, resolver: Resolvers::AddOnResolver
-    field :billable_metrics, resolver: Resolvers::BillableMetricsResolver
+    field :add_ons, resolver: Resolvers::AddOnsResolver
     field :billable_metric, resolver: Resolvers::BillableMetricResolver
-    field :coupons, resolver: Resolvers::CouponsResolver
+    field :billable_metrics, resolver: Resolvers::BillableMetricsResolver
     field :coupon, resolver: Resolvers::CouponResolver
+    field :coupons, resolver: Resolvers::CouponsResolver
     field :credit_note, resolver: Resolvers::CreditNoteResolver
-    field :customer_credit_notes, resolver: Resolvers::CustomerCreditNotesResolver
-    field :invoice_credit_notes, resolver: Resolvers::InvoiceCreditNotesResolver
-    field :customers, resolver: Resolvers::CustomersResolver
+    field :current_version, resolver: Resolvers::VersionResolver
     field :customer, resolver: Resolvers::CustomerResolver
-    field :events, resolver: Resolvers::EventsResolver
-    field :customer_usage, resolver: Resolvers::Customers::UsageResolver
+    field :customer_credit_notes, resolver: Resolvers::CustomerCreditNotesResolver
     field :customer_invoices, resolver: Resolvers::Customers::InvoicesResolver
     field :customer_portal_invoices, resolver: Resolvers::CustomerPortal::InvoicesResolver
     field :customer_portal_organization, resolver: Resolvers::CustomerPortal::OrganizationResolver
     field :customer_portal_user, resolver: Resolvers::CustomerPortal::CustomerResolver
-    field :organization, resolver: Resolvers::OrganizationResolver
-    field :plans, resolver: Resolvers::PlansResolver
-    field :plan, resolver: Resolvers::PlanResolver
-    field :current_version, resolver: Resolvers::VersionResolver
-    field :wallets, resolver: Resolvers::WalletsResolver
-    field :wallet_transactions, resolver: Resolvers::WalletTransactionsResolver
-    field :memberships, resolver: Resolvers::MembershipsResolver
-    field :invoice, resolver: Resolvers::InvoiceResolver
-    field :invoices, resolver: Resolvers::InvoicesResolver
+    field :customer_usage, resolver: Resolvers::Customers::UsageResolver
+    field :customers, resolver: Resolvers::CustomersResolver
+    field :events, resolver: Resolvers::EventsResolver
     field :invite, resolver: Resolvers::InviteResolver
     field :invites, resolver: Resolvers::InvitesResolver
+    field :invoice, resolver: Resolvers::InvoiceResolver
+    field :invoice_credit_notes, resolver: Resolvers::InvoiceCreditNotesResolver
+    field :invoices, resolver: Resolvers::InvoicesResolver
+    field :memberships, resolver: Resolvers::MembershipsResolver
+    field :organization, resolver: Resolvers::OrganizationResolver
+    field :password_reset, resolver: Resolvers::PasswordResetResolver
+    field :plan, resolver: Resolvers::PlanResolver
+    field :plans, resolver: Resolvers::PlansResolver
+    field :wallet_transactions, resolver: Resolvers::WalletTransactionsResolver
+    field :wallets, resolver: Resolvers::WalletsResolver
     field :webhooks, resolver: Resolvers::WebhooksResolver
   end
 end
