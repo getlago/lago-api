3d6f1eb82da7c2fbcb6debe51915888efa192d44
Romain Sempé <romain@getlago.com>
2023-04-13 10:50:27 +0200

    feat(min_charge): Add min_amount_cents to Charge for graphql

--- app/graphql/types/charges/input.rb
+++ app/graphql/types/charges/input.rb
@@ -9,6 +9,7 @@ module Types
       argument :billable_metric_id, ID, required: true
       argument :charge_model, Types::Charges::ChargeModelEnum, required: true
       argument :instant, Boolean, required: false
+      argument :min_amount_cents, GraphQL::Types::BigInt, required: false
 
       argument :properties, Types::Charges::PropertiesInput, required: false
       argument :group_properties, [Types::Charges::GroupPropertiesInput], required: false
