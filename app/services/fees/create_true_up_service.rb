919d569890bcef9cd2ea79c002750d60e63355a4
Romain Sempé <romain@getlago.com>
2023-04-13 21:14:39 +0200

    feat(min_charge): Add service to create the true-up fee

---
+++ app/services/fees/create_true_up_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Fees
+  class CreateTrueUpService < BaseService
+    def initialize(fee:)
+      @fee = fee
+      super
+    end
+
+    def call
+      return result unless fee
+      return result if fee.amount_cents >= charge.min_amount_cents
+
+      true_up_fee = fee.dup.tap do |f|
+        f.amount_cents = charge.min_amount_cents - fee.amount_cents
+        f.units = 1
+        f.events_count = 0
+        f.group_id = nil
+      end
+      true_up_fee.compute_vat
+
+      result.true_up_fee = true_up_fee
+      result
+    end
+
+    private
+
+    attr_reader :fee
+
+    delegate :charge, to: :fee
+  end
+end
