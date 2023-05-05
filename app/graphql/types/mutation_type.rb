19e73b613fe2776ca1f4e00cbd29acd0dffc1acb
Romain Sempé <romain@getlago.com>
2023-05-05 11:16:36 +0200

    feat(tax_rates): Add graphql mutation to delete a tax rate

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -74,6 +74,7 @@ module Types
     field :reset_password, mutation: Mutations::PasswordResets::Reset
 
     field :create_tax_rate, mutation: Mutations::TaxRates::Create
+    field :destroy_tax_rate, mutation: Mutations::TaxRates::Destroy
     field :update_tax_rate, mutation: Mutations::TaxRates::Update
 
     field :retry_webhook, mutation: Mutations::Webhooks::Retry
