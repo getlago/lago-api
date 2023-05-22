d92a98063a4c8a43be38b2c24581bf1dd3cfa65b
Jeremy Denquin <jeremy@getlago.com>
2023-05-22 16:44:29 +0200

    add migration to ensure no duplicates

--- db/migrate/20230522091400_add_unique_index_on_memberships.rb
+++ db/migrate/20230522091400_add_unique_index_on_memberships.rb
@@ -2,6 +2,9 @@
 
 class AddUniqueIndexOnMemberships < ActiveRecord::Migration[7.0]
   def change
+    LagoApi::Application.load_tasks
+    Rake::Task['memberships:revoke_duplicates'].invoke
+
     add_index :memberships, [:user_id, :organization_id], unique: true, where: 'revoked_at IS NULL'
   end
 end
