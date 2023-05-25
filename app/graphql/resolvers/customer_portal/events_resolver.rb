38c7606df0746048a915e9b4513c493bb0f4f2da
logan <logan@deemos.com>
2023-05-26 00:08:54 +0800

    update events_resolver.rb

---
+++ app/graphql/resolvers/customer_portal/events_resolver.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module CustomerPortal
+    class EventsResolver < GraphQL::Schema::Resolver
+      include AuthenticableCustomerPortalUser
+  
+      description 'Query events for a customer'
+  
+      argument :limit, Integer, required: false
+      argument :page, Integer, required: false
+  
+      type Types::Events::Object.collection_type, null: true
+  
+      def resolve(page: nil, limit: nil)
+
+        current_organizaton = context[:customer_portal_user].organization
+        current_customer = context[:customer_portal_user].id
+
+        current_organizaton.events
+          .order(timestamp: :desc)
+          .includes(:customer)
+          .joins('LEFT OUTER JOIN billable_metrics ON billable_metrics.code = events.code')
+          .where(billable_metrics: { deleted_at: nil })
+          .where(
+            billable_metrics: { customer_id: current_customer }
+          )
+          .select(
+            [
+              'events.*',
+              'billable_metrics.name as billable_metric_name',
+              'billable_metrics.field_name as billable_metric_field_name',
+            ].join(','),
+          )
+          .page(page)
+          .per(limit)
+      end
+    end
+  end
+end
