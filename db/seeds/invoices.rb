86cae007c230909c48bd344e49f79d794779a3b6
Romain Sempé <romain@getlago.com>
2023-05-25 14:35:32 +0200

    feat(tax): Fix seeds

--- db/seeds/invoices.rb
+++ db/seeds/invoices.rb
@@ -26,8 +26,7 @@ Invoice.all.find_each do |invoice|
     total_amount_cents: amount,
     total_amount_currency: fee.amount_currency,
     issuing_date: Time.current.to_date,
-    vat_amount_currency: fee.amount_currency,
-    vat_amount_cents: fee.vat_amount_cents,
+    taxes_amount_cents: fee.vat_amount_cents,
   )
 
   credit_note.items.create!(
