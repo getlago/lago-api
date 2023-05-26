5ff03e2752f2ac4fa0f7bebb764cfd18f8432540
Vincent Pochet <vincent@getlago.com>
2023-05-26 09:24:30 +0200

    fix(coupons): Ignore applied coupons for deleted customers (#1054)

---
+++ spec/queries/applied_coupons_query_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AppliedCouponsQuery, type: :query do
+  subject(:applied_coupons_query) { described_class.new(organization:, pagination:, filters:) }
+
+  let(:organization) { create(:organization) }
+  let(:pagination) { BaseQuery::Pagination.new }
+  let(:filters) { BaseQuery::Filters.new(query_filters) }
+
+  let(:query_filters) { {} }
+
+  let(:customer) { create(:customer, organization:) }
+  let(:coupon) { create(:coupon, organization:) }
+
+  let(:applied_coupon) { create(:applied_coupon, coupon:, customer:) }
+
+  before { applied_coupon }
+
+  describe 'call' do
+    it 'returns a list of applied_coupons' do
+      result = applied_coupons_query.call
+
+      aggregate_failures do
+        expect(result).to be_success
+        expect(result.applied_coupons.count).to eq(1)
+        expect(result.applied_coupons).to eq([applied_coupon])
+      end
+    end
+
+    context 'when customer is deleted' do
+      let(:customer) { create(:customer, :deleted, organization:) }
+
+      it 'filters the applied_coupons' do
+        result = applied_coupons_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.applied_coupons.count).to eq(0)
+        end
+      end
+    end
+
+    context 'with pagination' do
+      let(:pagination) { BaseQuery::Pagination.new(page: 2, limit: 10) }
+
+      it 'applies the pagination' do
+        result = applied_coupons_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.applied_coupons.count).to eq(0)
+          expect(result.applied_coupons.current_page).to eq(2)
+        end
+      end
+    end
+
+    context 'with customer filter' do
+      let(:query_filters) { { external_customer_id: customer.external_id } }
+
+      it 'applies the filter' do
+        result = applied_coupons_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.applied_coupons.count).to eq(1)
+        end
+      end
+    end
+
+    context 'with status filter' do
+      let(:query_filters) { { status: 'terminated' } }
+
+      it 'applies the filter' do
+        result = applied_coupons_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.applied_coupons.count).to eq(0)
+        end
+      end
+    end
+  end
+end
