ec9de42b6c1f348c67429bb945f39b8c86c00692
Romain Sempé <romain@getlago.com>
2023-05-30 14:38:55 +0200

    feat(tax): Extract applied tax fields into a graphql interface

--- app/graphql/types/credit_notes/applied_taxes/object.rb
+++ app/graphql/types/credit_notes/applied_taxes/object.rb
@@ -5,22 +5,9 @@ module Types
     module AppliedTaxes
       class Object < Types::BaseObject
         graphql_name 'CreditNoteAppliedTax'
-
-        field :id, ID, null: false
+        implements Types::Taxes::AppliedTax
 
         field :credit_note, Types::CreditNotes::Object, null: false
-        field :tax, Types::Taxes::Object, null: false
-
-        field :tax_code, String, null: false
-        field :tax_description, String, null: true
-        field :tax_name, String, null: false
-        field :tax_rate, Float, null: false
-
-        field :amount_cents, GraphQL::Types::BigInt, null: false
-        field :amount_currency, Types::CurrencyEnum, null: false
-
-        field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-        field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
       end
     end
   end
