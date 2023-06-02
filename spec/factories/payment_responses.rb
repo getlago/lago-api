8bc82f73ff2e14a1154af75f9d9a46acbff62764
Ivan Novosad <ivan.novosad@mobility46.se>
2023-06-02 18:10:52 +0200

    feat(adyen payments): Fix rubocop warnings in payment responses factory

--- spec/factories/payment_responses.rb
+++ spec/factories/payment_responses.rb
@@ -2,85 +2,85 @@ FactoryBot.define do
   sequence :adyen_payments_response do
     OpenStruct.new(
       response: {
-        "additionalData" => {
-            "recurringProcessingModel" => "UnscheduledCardOnFile"
+        'additionalData' => {
+          'recurringProcessingModel' => 'UnscheduledCardOnFile',
         },
-        "pspReference" => "#{SecureRandom.uuid}",
-        "resultCode" => "Authorised",
-        "amount" => {
-            "currency" => "USD",
-            "value" => 1000
+        'pspReference' => SecureRandom.uuid,
+        'resultCode' => 'Authorised',
+        'amount' => {
+          'currency' => 'USD',
+          'value' => 1000,
         },
-        "merchantReference" => "#{SecureRandom.uuid}",
-        "paymentMethod" => {
-            "brand" => "amex",
-            "type" => "scheme"
-        }
-      }
+        'merchantReference' => SecureRandom.uuid,
+        'paymentMethod' => {
+          'brand' => 'amex',
+          'type' => 'scheme',
+        },
+      },
     )
   end
 
   sequence :adyen_payment_links_response do
     OpenStruct.new(
       response: {
-        "amount" => {
-            "currency" => "EUR",
-            "value" => 0
+        'amount' => {
+          'currency' => 'EUR',
+          'value' => 0,
         },
-        "expiresAt" => "2023-05-19T10:00:19+02:00",
-        "merchantAccount" => "#{SecureRandom.uuid}",
-        "recurringProcessingModel" => "UnscheduledCardOnFile",
-        "reference" => "#{SecureRandom.uuid}",
-        "reusable" => false,
-        "shopperReference" => "#{SecureRandom.uuid}",
-        "storePaymentMethodMode" => "enabled",
-        "id" => "#{SecureRandom.uuid}",
-        "status" => "active",
-        "url" => "https://test.adyen.link/test"
-      }
+        'expiresAt' => '2023-05-19T10:00:19+02:00',
+        'merchantAccount' => SecureRandom.uuid,
+        'recurringProcessingModel' => 'UnscheduledCardOnFile',
+        'reference' => SecureRandom.uuid,
+        'reusable' => false,
+        'shopperReference' => SecureRandom.uuid,
+        'storePaymentMethodMode' => 'enabled',
+        'id' => SecureRandom.uuid,
+        'status' => 'active',
+        'url' => 'https://test.adyen.link/test',
+      },
     )
   end
 
   sequence :adyen_payment_methods_response do
     OpenStruct.new(
       response: {
-        "paymentMethods" => [
+        'paymentMethods' => [
           {
-            "brands" => [
-              "amex",
-              "bcmc",
-              "cartebancaire",
-              "mc",
-              "visa",
-              "visadankort"
+            'brands' => [
+              'amex',
+              'bcmc',
+              'cartebancaire',
+              'mc',
+              'visa',
+              'visadankort',
             ],
-            "name" => "Credit Card",
-            "type" => "scheme"
-          }
+            'name' => 'Credit Card',
+            'type' => 'scheme',
+          },
         ],
-        "storedPaymentMethods" => [
+        'storedPaymentMethods' => [
           {
-            "brand" => "visa",
-            "expiryMonth" => "03",
-            "expiryYear" => "30",
-            "holderName" => "Checkout Shopper PlaceHolder",
-            "id" => "#{SecureRandom.uuid}",
-            "lastFour" => "1234",
-            "name" => "VISA",
-            "networkTxReference" => "#{SecureRandom.uuid}",
-            "supportedRecurringProcessingModels" => [
-                "CardOnFile",
-                "Subscription",
-                "UnscheduledCardOnFile"
+            'brand' => 'visa',
+            'expiryMonth' => '03',
+            'expiryYear' => '30',
+            'holderName' => 'Checkout Shopper PlaceHolder',
+            'id' => SecureRandom.uuid,
+            'lastFour' => '1234',
+            'name' => 'VISA',
+            'networkTxReference' => SecureRandom.uuid,
+            'supportedRecurringProcessingModels' => [
+              'CardOnFile',
+              'Subscription',
+              'UnscheduledCardOnFile',
             ],
-            "supportedShopperInteractions" => [
-                "Ecommerce",
-                "ContAuth"
+            'supportedShopperInteractions' => [
+              'Ecommerce',
+              'ContAuth',
             ],
-            "type" => "scheme"
-          }
-        ]
-      }
+            'type' => 'scheme',
+          },
+        ],
+      },
     )
   end
 end
