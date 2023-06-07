a718b12b8bc656948ab11f0fd9d62add1fb4d4f8
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-06-07 13:03:20 +0200

    misc(schema) add new tax_identification_number attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_090325) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_164458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -270,6 +270,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_090325) do
     t.string "timezone"
     t.datetime "deleted_at"
     t.string "document_locale"
+    t.string "tax_identification_number"
     t.index ["deleted_at"], name: "index_customers_on_deleted_at"
     t.index ["external_id", "organization_id"], name: "index_customers_on_external_id_and_organization_id", unique: true, where: "(deleted_at IS NULL)"
     t.index ["organization_id"], name: "index_customers_on_organization_id"
@@ -506,6 +507,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_090325) do
     t.string "timezone", default: "UTC", null: false
     t.string "document_locale", default: "en", null: false
     t.string "email_settings", default: [], null: false, array: true
+    t.string "tax_identification_number"
     t.index ["api_key"], name: "index_organizations_on_api_key", unique: true
     t.check_constraint "invoice_grace_period >= 0", name: "check_organizations_on_invoice_grace_period"
   end
