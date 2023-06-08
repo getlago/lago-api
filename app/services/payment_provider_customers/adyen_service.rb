555a1db7690f289bda6bf0710a288cb69acbe85f
Ivan Novosad <ivan.novosad@mobility46.se>
2023-06-08 09:01:51 +0200

    feat(adyen fix): Fix adyen payment provider authorisation webhook

--- app/services/payment_provider_customers/adyen_service.rb
+++ app/services/payment_provider_customers/adyen_service.rb
@@ -45,10 +45,12 @@ module PaymentProviderCustomers
 
       return handle_missing_customer(shopper_reference) unless adyen_customer
 
-      status = (event['success'] == 'true') ? :succeeded : :failed
-      adyen_customer.update!(payment_method_id:, status:, provider_customer_id: shopper_reference)
-
-      SendWebhookJob.perform_later('customer.payment_provider_created', customer) if organization.webhook_url?
+      if event['success'] == 'true'
+        adyen_customer.update!(payment_method_id:, provider_customer_id: shopper_reference)
+        SendWebhookJob.perform_later('customer.payment_provider_created', customer) if organization.webhook_url?
+      else
+        deliver_error_webhook(Adyen::AdyenError.new(nil, nil, event['reason'], event['eventCode']))
+      end
 
       result.adyen_customer = adyen_customer
       result
