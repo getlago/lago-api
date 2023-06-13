1ae0bd4e51f78a3590a0ddc6e6620c5ed2be8273
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-06-13 16:21:02 +0200

    chore(customer) move create input in separated file

---
+++ app/graphql/types/customers/create_customer_input.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Types
+  module Customers
+    class CreateCustomerInput < BaseInputObject
+      description 'Create Customer input arguments'
+
+      argument :address_line1, String, required: false
+      argument :address_line2, String, required: false
+      argument :city, String, required: false
+      argument :country, Types::CountryCodeEnum, required: false
+      argument :currency, Types::CurrencyEnum, required: false
+      argument :email, String, required: false
+      argument :external_id, String, required: true
+      argument :invoice_grace_period, Integer, required: false
+      argument :legal_name, String, required: false
+      argument :legal_number, String, required: false
+      argument :logo_url, String, required: false
+      argument :name, String, required: true
+      argument :phone, String, required: false
+      argument :state, String, required: false
+      argument :timezone, Types::TimezoneEnum, required: false
+      argument :url, String, required: false
+      argument :vat_rate, Float, required: false
+      argument :zipcode, String, required: false
+
+      argument :metadata, [Types::Customers::Metadata::Input], required: false
+
+      argument :payment_provider, Types::PaymentProviders::ProviderTypeEnum, required: false
+      argument :provider_customer, Types::PaymentProviderCustomers::ProviderInput, required: false
+
+      argument :billing_configuration, Types::Customers::BillingConfigurationInput, required: false
+    end
+  end
+end
