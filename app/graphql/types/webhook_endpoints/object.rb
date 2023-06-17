43ec59785f10e13fe3834a0409afce86c421e2ec
Ivan Novosad <ivan.novosad@mobility46.se>
2023-06-17 22:18:37 +0200

    feat(multiple webhook endpoints): Add webhook endpoint type

---
+++ app/graphql/types/webhook_endpoints/object.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  module WebhookEndpoints
+    class Object < Types::BaseObject
+      graphql_name 'WebhookEndpoint'
+
+      field :id, ID, null: false
+      field :organization, Types::OrganizationType
+      field :webhook_url, String, null: false
+
+      field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+      field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    end
+  end
+end
