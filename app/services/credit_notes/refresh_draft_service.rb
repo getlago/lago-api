953481a736114e99f79f677f1f8382d94a641bff
Vincent Pochet <vincent@getlago.com>
2023-06-22 09:56:47 +0200

    feat(tax): Apply taxes and coupons before vat when refreshing a credit note (#1122)

--- app/services/credit_notes/refresh_draft_service.rb
+++ app/services/credit_notes/refresh_draft_service.rb
@@ -13,22 +13,26 @@ module CreditNotes
       result.credit_note = credit_note
       return result unless credit_note.draft?
 
+      credit_note.applied_taxes.destroy_all
       credit_note.items.update_all(fee_id: fee.id) # rubocop:disable Rails/SkipsModelValidations
 
-      amount_result = CreditNotes::ComputeAmountService.call(
+      taxes_result = CreditNotes::ApplyTaxesService.call(
         invoice: fee.invoice,
         items: credit_note.items,
       )
 
-      credit_note.precise_coupons_adjustment_amount_cents = amount_result.coupons_adjustment_amount_cents
-      credit_note.coupons_adjustment_amount_cents = amount_result.coupons_adjustment_amount_cents.round
-      credit_note.precise_taxes_amount_cents = amount_result.taxes_amount_cents
-      credit_note.taxes_amount_cents = amount_result.taxes_amount_cents.round
+      credit_note.precise_coupons_adjustment_amount_cents = taxes_result.coupons_adjustment_amount_cents
+      credit_note.coupons_adjustment_amount_cents = taxes_result.coupons_adjustment_amount_cents.round
+      credit_note.precise_taxes_amount_cents = taxes_result.taxes_amount_cents
+      credit_note.taxes_amount_cents = taxes_result.taxes_amount_cents.round
+      credit_note.taxes_rate = taxes_result.taxes_rate
+
+      taxes_result.applied_taxes.each { |applied_tax| credit_note.applied_taxes << applied_tax }
 
       credit_note.credit_amount_cents = (
         credit_note.items.sum(:precise_amount_cents).truncate(CreditNote::DB_PRECISION_SCALE) -
-        amount_result.coupons_adjustment_amount_cents +
-        amount_result.taxes_amount_cents
+        taxes_result.coupons_adjustment_amount_cents +
+        taxes_result.taxes_amount_cents
       ).round
 
       credit_note.balance_amount_cents = credit_note.credit_amount_cents
