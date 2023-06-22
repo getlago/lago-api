7b32c01064f22c46d5efeb9477166b476207affb
Vincent Pochet <vincent@getlago.com>
2023-06-22 11:27:17 +0200

    fix(credit_note): takes coupons into account when refreshing a credit note (#1126)

--- spec/services/credit_notes/refresh_draft_service_spec.rb
+++ spec/services/credit_notes/refresh_draft_service_spec.rb
@@ -5,10 +5,25 @@ require 'rails_helper'
 RSpec.describe CreditNotes::RefreshDraftService, type: :service do
   subject(:refresh_service) { described_class.new(credit_note:, fee:) }
 
+  let(:customer) { create(:customer) }
+  let(:organization) { customer.organization }
+
+  let(:invoice) do
+    create(
+      :invoice,
+      organization:,
+      customer:,
+      currency: 'EUR',
+      fees_amount_cents: 100,
+      total_amount_cents: 120,
+      coupons_amount_cents: 20,
+    )
+  end
+
   describe '#call' do
     let(:status) { :draft }
-    let(:credit_note) { create(:credit_note, status:) }
-    let(:fee) { create(:fee, taxes_rate: 0) }
+    let(:credit_note) { create(:credit_note, status:, invoice:) }
+    let(:fee) { create(:fee, invoice:, taxes_rate: 0) }
 
     before do
       create(:credit_note_item, credit_note:, fee: create(:fee, taxes_rate: 20))
@@ -29,9 +44,10 @@ RSpec.describe CreditNotes::RefreshDraftService, type: :service do
     it 'updates vat amounts of the credit note' do
       expect { refresh_service.call }
         .to change { credit_note.reload.taxes_amount_cents }.from(20).to(0)
-        .and change(credit_note, :credit_amount_cents).from(120).to(100)
-        .and change(credit_note, :balance_amount_cents).from(120).to(100)
-        .and change(credit_note, :total_amount_cents).from(120).to(100)
+        .and change(credit_note, :coupons_adjustment_amount_cents).from(0).to(20)
+        .and change(credit_note, :credit_amount_cents).from(120).to(80)
+        .and change(credit_note, :balance_amount_cents).from(120).to(80)
+        .and change(credit_note, :total_amount_cents).from(120).to(80)
     end
   end
 end
