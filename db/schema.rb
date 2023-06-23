43d915ae5cf7e3822be60ba49a125be567bb7014
Vincent Pochet <vincent@getlago.com>
2023-06-23 16:09:48 +0200

    feat(tax): Migrate taxes_rate for all resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_101701) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_120905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
