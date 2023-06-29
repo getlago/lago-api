9bb6f211c5400656c01ca942c2eaebab6682f2ba
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-06-29 14:38:58 +0200

    misc(schema) add description to coupon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_124005) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_100018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -153,6 +153,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_124005) do
     t.boolean "limited_plans", default: false, null: false
     t.datetime "deleted_at"
     t.boolean "limited_billable_metrics", default: false, null: false
+    t.text "description"
     t.index ["deleted_at"], name: "index_coupons_on_deleted_at"
     t.index ["organization_id", "code"], name: "index_coupons_on_organization_id_and_code", unique: true, where: "(deleted_at IS NULL)"
     t.index ["organization_id"], name: "index_coupons_on_organization_id"
