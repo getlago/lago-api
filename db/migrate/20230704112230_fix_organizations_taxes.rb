b17373eea639d2b91c66bd9a0ac6d2305ca7edee
Vincent Pochet <vincent@getlago.com>
2023-07-04 13:26:54 +0200

    fix(tax): Organization default vat_rate migration

---
+++ db/migrate/20230704112230_fix_organizations_taxes.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class FixOrganizationsTaxes < ActiveRecord::Migration[7.0]
+  def change
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE taxes
+          SET applied_to_organization = true
+          FROM organizations
+          WHERE organizations.id = taxes.organization_id
+            AND organizations.vat_rate = taxes.rate
+            AND applied_to_organization = false;
+        SQL
+      end
+    end
+  end
+end
