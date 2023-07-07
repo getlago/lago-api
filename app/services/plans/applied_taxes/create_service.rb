8fae92a3361db673b99d2919ca971ddfd47cb33e
Romain Sempé <romain@getlago.com>
2023-07-07 17:49:33 +0200

    feat(taxes): Refresh draft invoices only when applied tax is created

--- app/services/plans/applied_taxes/create_service.rb
+++ app/services/plans/applied_taxes/create_service.rb
@@ -13,8 +13,9 @@ module Plans
         return result.not_found_failure!(resource: 'plan') unless plan
         return result.not_found_failure!(resource: 'tax') unless tax
 
-        applied_tax = plan.applied_taxes.find_or_create_by!(tax:)
-        Invoices::RefreshBatchJob.perform_later(plan.invoices.draft.pluck(:id))
+        applied_tax = plan.applied_taxes.find_or_create_by!(tax:) do |_applied_tax|
+          Invoices::RefreshBatchJob.perform_later(plan.invoices.draft.pluck(:id))
+        end
 
         result.applied_tax = applied_tax
         result
