f587b88d82fbbaad59393e3ffb4536d6690cafa2
Jeremy Denquin <jeremy@getlago.com>
2023-07-12 17:44:57 +0200

    revert(subscriptions): revert subscriptions index filters

--- app/queries/subscriptions_query.rb
+++ app/queries/subscriptions_query.rb
@@ -2,8 +2,7 @@
 
 class SubscriptionsQuery < BaseQuery
   def call
-    subscriptions = paginate(organization.subscriptions)
-    subscriptions = with_status(subscriptions) if filters.status.present? && valid_status?
+    subscriptions = paginate(organization.subscriptions.active)
     subscriptions = subscriptions.order(started_at: :asc)
 
     subscriptions = with_external_customer(subscriptions) if filters.external_customer_id
@@ -19,13 +18,5 @@ class SubscriptionsQuery < BaseQuery
 
   def with_plan_code(scope)
     scope.joins(:plan).where(plans: { code: filters.plan_code })
-  end
-
-  def with_status(scope)
-    scope.where(status: filters.status)
-  end
-
-  def valid_status?
-    filters.status.all? { |s| Subscription.statuses.key?(s) }
   end
 end
