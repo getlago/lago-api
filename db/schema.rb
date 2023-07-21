8255623bafc3afe6b6a352f3a392a68908d1eb6f
Vincent Pochet <vincent@getlago.com>
2023-07-21 10:05:28 +0200

    fix(credit): Migrate v3 credits with wrong before taxes flag (#1187)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_090135) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_073114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
