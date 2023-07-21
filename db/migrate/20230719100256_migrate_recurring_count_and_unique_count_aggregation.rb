c91b7cab4faa2bdb32601de53a13159e5ebb51cb
Lovro Colic <lovro.colic.lc@gmail.com>
2023-07-21 17:44:46 +0200

    fix migration

--- db/migrate/20230719100256_migrate_recurring_count_and_unique_count_aggregation.rb
+++ db/migrate/20230719100256_migrate_recurring_count_and_unique_count_aggregation.rb
@@ -8,6 +8,7 @@ class MigrateRecurringCountAndUniqueCountAggregation < ActiveRecord::Migration[7
   class Event < ApplicationRecord; end
   class Plan < ApplicationRecord; end
   class Subscription < ApplicationRecord; end
+  class QuantifiedEvent < ApplicationRecord; end
 
   def change
     reversible do |dir|
@@ -21,26 +22,25 @@ class MigrateRecurringCountAndUniqueCountAggregation < ActiveRecord::Migration[7
             events.customer_id AS customer_id,
             events.properties AS properties,
             events.timestamp AS event_timestamp,
-            subscriptions.external_id AS subscription_external_id
+            subscriptions.external_id AS subscription_external_id,
+            billable_metrics.id AS billable_metric_id,
+            billable_metrics.field_name AS field_name
           FROM events
           INNER JOIN subscriptions ON subscriptions.id = events.subscription_id
           INNER JOIN billable_metrics ON billable_metrics.code = events.code
-          WHERE events.deleted_at IS NULL AND billable_metrics.aggregation_type = 3
+          WHERE events.deleted_at IS NULL
+            AND billable_metrics.aggregation_type = 3
+            AND billable_metrics.organization_id = events.organization_id
           ORDER BY event_timestamp ASC;
         SQL
 
         ApplicationRecord.connection.select_all(sql).each_with_object({}) do |row, _result|
-          matching_billable_metric = BillableMetric.find_by(
-            code: row['code'],
-            organization_id: row['organization_id'],
-          )
-
           existing_quantified_event =
             QuantifiedEvent.where(
               customer_id: row['customer_id'],
-              billable_metric_id: matching_billable_metric.id,
+              billable_metric_id: row['billable_metric_id'],
               external_subscription_id: row['subscription_external_id'],
-              external_id: JSON.parse(row['properties'])[matching_billable_metric.field_name.to_s],
+              external_id: JSON.parse(row['properties'])[row['field_name'].to_s],
             ).where(removed_at: nil).any?
 
           # There can only be one quantified event for certain external_id which guarantees uniqueness
@@ -48,9 +48,9 @@ class MigrateRecurringCountAndUniqueCountAggregation < ActiveRecord::Migration[7
 
           quantified_event = QuantifiedEvent.create!(
             customer_id: row['customer_id'],
-            billable_metric_id: matching_billable_metric.id,
+            billable_metric_id: row['billable_metric_id'],
             external_subscription_id: row['subscription_external_id'],
-            external_id: JSON.parse(row['properties'])[matching_billable_metric.field_name.to_s],
+            external_id: JSON.parse(row['properties'])[row['field_name'].to_s],
             properties: JSON.parse(row['properties']),
             added_at: row['event_timestamp'],
           )
@@ -61,90 +61,79 @@ class MigrateRecurringCountAndUniqueCountAggregation < ActiveRecord::Migration[7
         end
 
         # All charges that are related to recurring_count_agg should be prorated = true
-        sql = <<-SQL
-          SELECT
-            charges.id AS charge_id
-          FROM charges
-          INNER JOIN billable_metrics ON billable_metrics.id = charges.billable_metric_id
-          WHERE billable_metrics.aggregation_type = 4;
+        execute <<-SQL
+          WITH recurring_count_charges AS (
+            SELECT charges.id AS charge_id
+            FROM charges
+            INNER JOIN billable_metrics ON billable_metrics.id = charges.billable_metric_id
+            WHERE billable_metrics.aggregation_type = 4 -- recurring_count_agg
+          )
+
+          UPDATE charges
+          SET prorated = true
+          FROM recurring_count_charges
+          WHERE recurring_count_charges.charge_id = charges.id
         SQL
-        ApplicationRecord.connection.select_all(sql).each_with_object({}) do |row, _result|
-          charge = Charge.find_by(id: row['charge_id'])
-          charge.prorated = true
-          # Skipping validations since prorated is not allowed for graduated charge model, but we want to allow
-          # it for old charges where we will keep old calculation
-          charge.save!(validate: false)
-        end
 
         # Recurring count agg billable metrics should be recurring = true and aggregation type should be unique count
-        sql = <<-SQL
-          SELECT
-            billable_metrics.id AS billable_metric_id
-          FROM billable_metrics
-          WHERE billable_metrics.aggregation_type = 4;
+        execute <<-SQL
+          WITH recurring_count_billable_metrics AS (
+            SELECT billable_metrics.id AS billable_metric_id
+            FROM billable_metrics
+            WHERE billable_metrics.aggregation_type = 4 -- recurring_count_agg
+          )
+
+          UPDATE billable_metrics
+          SET recurring = true, aggregation_type = 3
+          FROM recurring_count_billable_metrics
+          WHERE recurring_count_billable_metrics.billable_metric_id = billable_metrics.id
         SQL
-        ApplicationRecord.connection.select_all(sql).each_with_object({}) do |row, _result|
-          billable_metric = BillableMetric.find_by(id: row['billable_metric_id'])
-          billable_metric.recurring = true
-          billable_metric.aggregation_type = 3 # Setting unique count aggregation
-          billable_metric.save!
-        end
 
         # If charge is pay_in_advance and aggregation type is SUM we need to set event metadata since this metadata
         # will be used in calculation of previous_event for further events in same period
-        sql = <<-SQL
-          SELECT
-            events.id AS event_id,
-            events.code AS code,
-            events.organization_id AS organization_id,
-            events.customer_id AS customer_id,
-            events.properties AS properties,
-            events.timestamp AS event_timestamp,
-            subscriptions.external_id AS subscription_external_id
-          FROM events
-          INNER JOIN subscriptions ON subscriptions.id = events.subscription_id
-          INNER JOIN plans ON plans.id = subscriptions.plan_id
-          INNER JOIN charges ON charges.deleted_at IS NULL AND charges.plan_id = plans.id
-          INNER JOIN billable_metrics on billable_metrics.code = events.code
-          WHERE events.deleted_at IS NULL AND charges.pay_in_advance = TRUE AND billable_metrics.aggregation_type = 1;
-        SQL
-        ApplicationRecord.connection.select_all(sql).each_with_object({}) do |row, _result|
-          billable_metric = BillableMetric.find_by(
-            code: row['code'],
-            organization_id: row['organization_id'],
+        execute <<-SQL
+          WITH sum_in_advance_events AS (
+            SELECT
+              events.id AS event_id,
+              events.properties ->> billable_metrics.field_name AS event_value
+            FROM events
+            INNER JOIN subscriptions ON subscriptions.id = events.subscription_id
+            INNER JOIN plans ON plans.id = subscriptions.plan_id
+            INNER JOIN charges ON charges.deleted_at IS NULL AND charges.plan_id = plans.id
+            INNER JOIN billable_metrics on billable_metrics.code = events.code
+            WHERE billable_metrics.organization_id = events.organization_id
+              AND events.deleted_at IS NULL
+              AND charges.pay_in_advance = TRUE
+              AND events.properties ->> billable_metrics.field_name IS NOT NULL
+              AND billable_metrics.aggregation_type = 1
           )
 
-          event = Event.find_by(id: row['event_id'])
-          value = event.properties.fetch(billable_metric.field_name, 0).to_s
-          event.metadata['current_aggregation'] = value
-          event.metadata['max_aggregation'] = value
-          event.save!
-        end
+          UPDATE events
+          SET metadata = jsonb_set(metadata, '{max_aggregation}', to_jsonb(event_value), true) ||
+                         jsonb_set(metadata, '{current_aggregation}', to_jsonb(event_value), true)
+          FROM sum_in_advance_events
+          WHERE sum_in_advance_events.event_id = events.id
+        SQL
 
         # If charge is pay_in_advance and aggregation type is UNIQUE COUNT we need to set event metadata since this
         # metadata will be used in calculation of previous event for further events in the same period
-        sql = <<-SQL
-          SELECT
-            events.id AS event_id,
-            events.code AS code,
-            events.organization_id AS organization_id,
-            events.customer_id AS customer_id,
-            events.properties AS properties,
-            events.timestamp AS event_timestamp,
-            subscriptions.external_id AS subscription_external_id
-          FROM events
-          INNER JOIN subscriptions ON subscriptions.id = events.subscription_id
-          INNER JOIN plans ON plans.id = subscriptions.plan_id
-          INNER JOIN charges ON charges.deleted_at IS NULL AND charges.plan_id = plans.id
-          INNER JOIN billable_metrics on billable_metrics.code = events.code
-          WHERE events.deleted_at IS NULL AND charges.pay_in_advance = TRUE AND billable_metrics.aggregation_type = 3;
+        execute <<-SQL
+          WITH unique_count_in_advance_events AS (
+            SELECT events.id AS event_id
+            FROM events
+            INNER JOIN subscriptions ON subscriptions.id = events.subscription_id
+            INNER JOIN plans ON plans.id = subscriptions.plan_id
+            INNER JOIN charges ON charges.deleted_at IS NULL AND charges.plan_id = plans.id
+            INNER JOIN billable_metrics on billable_metrics.code = events.code
+            WHERE events.deleted_at IS NULL AND charges.pay_in_advance = TRUE AND billable_metrics.aggregation_type = 3
+          )
+
+          UPDATE events
+          SET metadata = jsonb_set(metadata, '{max_aggregation}', '1'::jsonb, true) ||
+                         jsonb_set(metadata, '{current_aggregation}', '1'::jsonb, true)
+          FROM unique_count_in_advance_events
+          WHERE unique_count_in_advance_events.event_id = events.id
         SQL
-        ApplicationRecord.connection.select_all(sql).each_with_object({}) do |row, _result|
-          event = Event.find_by(id: row['event_id'])
-          event.metadata['current_aggregation'] = 1
-          event.metadata['max_aggregation'] = 1
-          event.save!
-        end
       end
     end
   end
