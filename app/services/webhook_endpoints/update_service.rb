9993d0d99ee3ffaaac1036500c56eccb93bdc7e3
Jeremy Denquin <jeremy@getlago.com>
2023-07-25 12:08:47 +0200

    add files

---
+++ app/services/webhook_endpoints/update_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module WebhookEndpoints
+  class UpdateService < BaseService
+    def initialize(id:, organization:, params:)
+      @id = id
+      @organization = organization
+      @params = params
+
+      super
+    end
+
+    def call
+      webhook_endpoint = organization.webhook_endpoints.find_by(id:)
+
+      return result.not_found_failure!(resource: 'webhook_endpoint') if webhook_endpoint.blank?
+
+      webhook_endpoint.update(
+        webhook_url: params[:webhook_url],
+      )
+
+      result.webhook_endpoint = webhook_endpoint
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    end
+
+    private
+
+    attr_reader :id, :organization, :params
+  end
+end
