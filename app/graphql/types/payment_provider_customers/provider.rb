451388dfdb71acd147c48a4bbe2f4590b3a64e8e
Jeremy Denquin <jeremy@getlago.com>
2023-07-27 18:18:19 +0200

    fix(stripe): set payment method to nullable on graphql

--- app/graphql/types/payment_provider_customers/provider.rb
+++ app/graphql/types/payment_provider_customers/provider.rb
@@ -7,7 +7,7 @@ module Types
 
       field :id, ID, null: false
       field :provider_customer_id, ID, null: true
-      field :provider_payment_methods, [Types::PaymentProviderCustomers::ProviderPaymentMethodsEnum], null: false
+      field :provider_payment_methods, [Types::PaymentProviderCustomers::ProviderPaymentMethodsEnum], null: true
       field :sync_with_provider, Boolean, null: true
     end
   end
