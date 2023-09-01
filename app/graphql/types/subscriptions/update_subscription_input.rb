c6aae73aa527be5f57d5b45672325449c093eacd
Lovro Colic <lovro.colic.lc@gmail.com>
2023-09-01 11:53:51 +0200

    reorder grapghql input

--- app/graphql/types/subscriptions/update_subscription_input.rb
+++ app/graphql/types/subscriptions/update_subscription_input.rb
@@ -5,8 +5,9 @@ module Types
     class UpdateSubscriptionInput < BaseInputObject
       description 'Update Subscription input arguments'
 
-      argument :ending_at, GraphQL::Types::ISO8601DateTime, required: false
       argument :id, ID, required: true
+
+      argument :ending_at, GraphQL::Types::ISO8601DateTime, required: false
       argument :name, String, required: false
       argument :subscription_at, GraphQL::Types::ISO8601DateTime, required: false
     end
