e77e53067f735f0843d6465a394f996b9702060d
Vincent Pochet <vincent@getlago.com>
2023-09-04 11:17:57 +0200

    fix(subscription): Activate should take care of past pending subscription and timezone

--- spec/services/subscriptions/activate_service_spec.rb
+++ spec/services/subscriptions/activate_service_spec.rb
@@ -34,21 +34,18 @@ RSpec.describe Subscriptions::ActivateService, type: :service do
     end
 
     context 'with customer timezone' do
-      let(:timestamp) { DateTime.parse('2022-10-21 00:30:00') }
+      let(:timestamp) { DateTime.parse('2023-08-24 00:07:00') }
       let(:pending_subscription) { pending_subscriptions.first }
 
       before do
-        active_subscription
-        future_pending_subscriptions
-
-        pending_subscription.customer.update!(timezone: 'America/New_York')
-        pending_subscription.update!(subscription_at: DateTime.parse('2022-10-21'))
+        pending_subscription.customer.update!(timezone: 'America/Bogota')
+        pending_subscription.update!(subscription_at: DateTime.parse('2023-08-24 04:17:00'))
       end
 
       it 'takes timezone into account' do
         expect { activate_service.activate_all_pending }
-          .to change(Subscription.pending, :count).by(-2)
-          .and change(Subscription.active, :count).by(2)
+          .to change(Subscription.pending, :count).by(-3)
+          .and change(Subscription.active, :count).by(3)
       end
     end
   end
