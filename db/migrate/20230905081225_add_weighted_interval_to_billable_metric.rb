67bd33b7dcf93f143ecc7c417d4554a1e4227984
Vincent Pochet <vincent@getlago.com>
2023-09-05 10:52:44 +0200

    feat(weighted-sum): Add new aggregation type to billable metric

---
+++ db/migrate/20230905081225_add_weighted_interval_to_billable_metric.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddWeightedIntervalToBillableMetric < ActiveRecord::Migration[7.0]
+  def change
+    create_enum :billable_metric_weighted_interval, %w[seconds]
+
+    change_table :billable_metrics do |t|
+      t.enum :weighted_interval, enum_type: 'billable_metric_weighted_interval', null: true
+    end
+  end
+end
