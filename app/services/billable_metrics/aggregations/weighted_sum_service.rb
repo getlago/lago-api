6c1f72558f15321f1c76056c138bff624abf1973
Vincent Pochet <vincent@getlago.com>
2023-09-05 18:21:13 +0200

    feat(weighted-sum): Starts implementing aggregation logic

--- app/services/billable_metrics/aggregations/weighted_sum_service.rb
+++ app/services/billable_metrics/aggregations/weighted_sum_service.rb
@@ -4,13 +4,87 @@ module BillableMetrics
   module Aggregations
     class WeightedSumService < BillableMetrics::Aggregations::BaseService
       def aggregate(options: {})
-        # TODO
-        result.aggregation = 0
+        result.aggregation = compute_aggregation
         result.current_usage_units = 0
         result.count = 0
         result.pay_in_advance_aggregation = BigDecimal(0)
         result.options = { running_total: 0 }
         result
+      end
+
+      private
+
+      def compute_aggregation
+        # query_result = ActiveRecord::Base.connection.select_all(aggregation_sql)
+        # query_result
+
+        # byebug
+
+        query_result = ActiveRecord::Base.connection.select_one(aggregation_sql)
+        query_result['aggregation']
+      end
+
+      def aggregation_sql
+        <<-SQL
+          WITH events_data AS (
+            (#{initial_value})
+            UNION
+            (#{
+              events_scope(from_datetime:, to_datetime:)
+                .select("timestamp, (#{sanitized_field_name})::numeric AS difference")
+                .to_sql
+            })
+            UNION
+            (#{end_of_period_value})
+          )
+
+          SELECT SUM(period_ratio) as aggregation
+          FROM (
+            SELECT
+              -- TODO: remove when finished
+              timestamp,
+              difference,
+              sum(difference) OVER (ORDER BY timestamp ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as cumul,
+              EXTRACT(epoch FROM lead(timestamp, 1, '#{to_datetime}') OVER (ORDER BY timestamp) - timestamp) AS second_duration,
+              --
+
+            (
+              -- NOTE: duration in seconds between current event and next one
+              -- TODO: takes weighted interval into account (+ group per interval in CTE ??)
+              CASE WHEN EXTRACT(EPOCH FROM LEAD(timestamp, 1, '#{to_datetime}') OVER (ORDER BY timestamp) - timestamp) = 0
+              THEN
+                0 -- NOTE: duration was null so usage is null
+              ELSE
+                -- NOTE: cumulative sum from previous events in the period
+                (sum(difference) OVER (ORDER BY timestamp ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW))
+                /
+                -- NOTE: duration in seconds between current event and next one - using end of period as final boundaries
+                EXTRACT(EPOCH FROM LEAD(timestamp, 1, '#{to_datetime}') OVER (ORDER BY timestamp) - timestamp)
+              END
+            ) AS period_ratio
+            FROM events_data
+          ) cumulated_ratios
+        SQL
+      end
+
+      def initial_value
+        initial_value = 0 # TODO: when recurring, get last cumul
+
+        <<-SQL
+          SELECT *
+          FROM (
+            VALUES (timestamp without time zone '#{from_datetime}', #{initial_value})
+          ) AS t(timestamp, difference)
+        SQL
+      end
+
+      def end_of_period_value
+        <<-SQL
+          SELECT *
+          FROM (
+            VALUES (timestamp without time zone '#{to_datetime}', 0)
+          ) AS t(timestamp, difference)
+        SQL
       end
     end
   end
---
+++ spec/services/billable_metrics/aggregations/weighted_sum_service_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BillableMetrics::Aggregations::WeightedSumService, type: :service do
+  subject(:aggregator) do
+    described_class.new(
+      billable_metric:,
+      subscription:,
+      group:,
+      boundaries: {
+        from_datetime:,
+        to_datetime:,
+      },
+    )
+  end
+
+  let(:subscription) { create(:subscription, started_at: DateTime.parse('2023-04-01 22:22:22')) }
+  let(:organization) { subscription.organization }
+  let(:customer) { subscription.customer }
+  let(:group) { nil }
+
+  let(:billable_metric) { create(:weighted_sum_billable_metric, organization:) }
+
+  let(:from_datetime) { DateTime.parse('2023-08-01 00:00:00.000') }
+  let(:to_datetime) { DateTime.parse('2023-08-31 23:59:59.999') }
+
+  let(:events_values) do
+    [
+      { timestamp: DateTime.parse('2023-08-01 00:00:00.000'), value: 2 },
+      { timestamp: DateTime.parse('2023-08-01 01:00:00'), value: 3 },
+      { timestamp: DateTime.parse('2023-08-01 01:30:00'), value: 1 },
+      { timestamp: DateTime.parse('2023-08-01 02:00:00'), value: -4 },
+      { timestamp: DateTime.parse('2023-08-01 04:00:00'), value: -2 },
+      { timestamp: DateTime.parse('2023-08-01 05:00:00'), value: 10 },
+      { timestamp: DateTime.parse('2023-08-01 05:30:00'), value: -10 },
+    ]
+  end
+
+  before do
+    events_values.each do |values|
+      create(
+        :event,
+        code: billable_metric.code,
+        subscription:,
+        timestamp: values[:timestamp],
+        properties: { value: values[:value] },
+      )
+    end
+  end
+
+  it 'aggregates the events' do
+    result = aggregator.aggregate
+
+    expect(result.aggregation.round(5).to_s).to eq('0.0125')
+  end
+
+  context 'with a single event' do
+    let(:events_values) do
+      [
+        { timestamp: DateTime.parse('2023-08-01 00:00:00.000'), value: 1000 },
+      ]
+    end
+
+    it 'aggregates the events' do
+      result = aggregator.aggregate
+
+      expect(result.aggregation.round(5).to_s).to eq('0.00037')
+    end
+  end
+
+  context 'with no events' do
+    let(:events_values) { [] }
+
+    it 'aggregates the events' do
+      result = aggregator.aggregate
+
+      expect(result.aggregation.round(5).to_s).to eq('0')
+    end
+  end
+end
