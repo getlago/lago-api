0ceb175866c06ffdfc6281645d112df4b803529a
Lovro Colic <lovro.colic.lc@gmail.com>
2023-09-06 12:55:18 +0200

    update latest aggregation service and add relates tests

--- app/services/billable_metrics/aggregations/latest_service.rb
+++ app/services/billable_metrics/aggregations/latest_service.rb
@@ -3,25 +3,33 @@
 module BillableMetrics
   module Aggregations
     class LatestService < BillableMetrics::Aggregations::BaseService
-      def aggregate(from_datetime:, to_datetime:, options: {})
-        events = events_scope(from_datetime:, to_datetime:)
-          .where("#{sanitized_field_name} IS NOT NULL").order(timestamp: :desc)
+      def aggregate(options: {})
+        latest_event = events.first
 
-        if events.count == 0
-          result.aggregation = 0
-          result.count = 0
-          result.options = options
-        else
-          latest_event = events.first
-
-          result.aggregation = latest_event&.properties&.fetch(billable_metric.field_name, 0)
-          result.count = events.count || 0
-          result.options = options
-        end
-
+        result.aggregation = compute_aggregation(latest_event)
+        result.count = events.count
+        result.options = options
         result
       rescue ActiveRecord::StatementInvalid => e
         result.service_failure!(code: 'aggregation_failure', message: e.message)
+      end
+
+      private
+
+      def events
+        @events ||=
+          events_scope(from_datetime:, to_datetime:)
+            .where("#{sanitized_field_name} IS NOT NULL")
+            .reorder(timestamp: :desc)
+      end
+
+      def compute_aggregation(latest_event)
+        if latest_event.present?
+          value = latest_event.properties.fetch(billable_metric.field_name, 0).to_s
+          BigDecimal(value).negative? ? BigDecimal(0) : BigDecimal(value)
+        else
+          0
+        end
       end
     end
   end
