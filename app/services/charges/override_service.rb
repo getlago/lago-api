c5d41f119ec470f5463b939f1e6043fb6ba9ff5d
Romain Sempé <romain@getlago.com>
2023-09-18 15:38:48 +0200

    feat(override-plan): Service for overriding a charge

---
+++ app/services/charges/override_service.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Charges
+  class OverrideService < BaseService
+    def initialize(charge:, params:)
+      @charge = charge
+      @params = params
+
+      super
+    end
+
+    def call
+      return result unless License.premium?
+
+      ActiveRecord::Base.transaction do
+        new_charge = charge.dup.tap do |c|
+          c.properties = params[:properties] if params.key?(:properties)
+          c.min_amount_cents = params[:min_amount_cents] if params.key?(:min_amount_cents)
+          # c.invoice_display_name = params[:invoice_display_name] if params.key?(:invoice_display_name)
+          c.group_properties = charge.group_properties.map(&:dup)
+          c.plan_id = params[:plan_id]
+        end
+        new_charge.save!
+
+        if params.key?(:group_properties)
+          group_result = GroupProperties::CreateOrUpdateBatchService.call(
+            charge: new_charge,
+            properties_params: params[:group_properties],
+          )
+          return group_result if group_result.error
+        end
+
+        if params.key?(:tax_codes)
+          taxes_result = Charges::ApplyTaxesService.call(charge: new_charge, tax_codes: params[:tax_codes])
+          taxes_result.raise_if_error!
+        end
+
+        result.charge = new_charge
+      end
+
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    end
+
+    private
+
+    attr_reader :charge, :params
+  end
+end
---
+++ spec/services/charges/override_service_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Charges::OverrideService, type: :service do
+  subject(:override_service) { described_class.new(charge:, params:) }
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+
+  describe '#call' do
+    let(:billable_metric) { create(:billable_metric, organization:) }
+    let(:group) { create(:group, billable_metric:) }
+    let(:group2) { create(:group, billable_metric:) }
+    let(:tax) { create(:tax, organization:) }
+
+    let(:charge) do
+      create(
+        :standard_charge,
+        billable_metric:,
+        properties: { amount: '300' },
+        group_properties: [
+          build(
+            :group_property,
+            group:,
+            values: { amount: '10', amount_currency: 'EUR' },
+          ),
+          build(
+            :group_property,
+            group: group2,
+            values: { amount: '20', amount_currency: 'EUR' },
+          ),
+        ],
+      )
+    end
+
+    let(:plan) { create(:plan, organization:) }
+    let(:params) do
+      {
+        id: charge.id,
+        plan_id: plan.id,
+        min_amount_cents: 1000,
+        properties: { amount: '200' },
+        tax_codes: [tax.code],
+        group_properties: [
+          {
+            group_id: group.id,
+            values: { amount: '100' },
+          },
+        ],
+      }
+    end
+
+    before { charge }
+
+    context 'when lago freemium' do
+      it 'returns without overriding the charge' do
+        expect { override_service.call }.not_to change(Charge, :count)
+      end
+    end
+
+    context 'when lago premium' do
+      around { |test| lago_premium!(&test) }
+
+      it 'creates a charge based on the given charge', :aggregate_failures do
+        applied_tax = create(:charge_applied_tax, charge:)
+
+        expect(charge.group_properties.count).to eq(2)
+        expect(charge.taxes).to contain_exactly(applied_tax.tax)
+
+        expect { override_service.call }.to change(Charge, :count).by(1)
+
+        charge = Charge.order(:created_at).last
+        expect(charge).to have_attributes(
+          amount_currency: charge.amount_currency,
+          billable_metric_id: charge.billable_metric.id,
+          charge_model: charge.charge_model,
+          invoiceable: charge.invoiceable,
+          pay_in_advance: charge.pay_in_advance,
+          prorated: charge.prorated,
+          # Overriden attributes
+          plan_id: plan.id,
+          min_amount_cents: 1000,
+          properties: { 'amount' => '200' },
+        )
+        expect(charge.group_properties.count).to eq(1)
+        expect(charge.group_properties.with_discarded.discarded.count).to eq(1)
+        expect(charge.group_properties.first).to have_attributes(
+          {
+            group_id: group.id,
+            values: { 'amount' => '100' },
+          },
+        )
+        expect(charge.taxes).to contain_exactly(tax)
+      end
+    end
+  end
+end
