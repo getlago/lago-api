a30a8edc06408b041dee74a14bd5fc21621bc992
Romain Sempé <romain@getlago.com>
2023-09-20 15:07:57 +0200

    fix: Run migration without a transaction

--- db/migrate/20230920083133_add_gin_index_to_events.rb
+++ db/migrate/20230920083133_add_gin_index_to_events.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddGinIndexToEvents < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_index(:events, :properties, using: 'gin', opclass: :jsonb_path_ops, algorithm: :concurrently)
   end
