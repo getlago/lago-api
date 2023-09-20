15b817b40bfa27f711c6a6942539819f9db67347
Vincent Pochet <vincent@getlago.com>
2023-09-20 11:28:05 +0200

    misc: Add concurrently algo to new indexes

--- db/migrate/20230918090426_add_index_to_events.rb
+++ db/migrate/20230918090426_add_index_to_events.rb
@@ -2,7 +2,7 @@
 
 class AddIndexToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_index :events, %w[subscription_id code timestamp], where: 'deleted_at IS NULL'
+    add_index :events, %w[subscription_id code timestamp], where: 'deleted_at IS NULL', algorithm: :concurrently
     remove_index :events, %w[subscription_id code]
   end
 end
--- db/migrate/20230920083133_add_gin_index_to_events.rb
+++ db/migrate/20230920083133_add_gin_index_to_events.rb
@@ -2,6 +2,6 @@
 
 class AddGinIndexToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_index(:events, :properties, using: 'gin', opclass: :jsonb_path_ops)
+    add_index(:events, :properties, using: 'gin', opclass: :jsonb_path_ops, algorithm: :concurrently)
   end
 end
