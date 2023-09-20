8d4198e8b3fe0dec5d61fc83c7ae47614d9cf27a
Vincent Pochet <vincent@getlago.com>
2023-09-20 19:45:09 +0200

    fix: concurent nmigrations should not be transactional (#1332)

--- db/migrate/20230918090426_add_index_to_events.rb
+++ db/migrate/20230918090426_add_index_to_events.rb
@@ -2,6 +2,8 @@
 
 class AddIndexToEvents < ActiveRecord::Migration[7.0]
   def change
+    disable_ddl_transaction!
+
     add_index :events, %w[subscription_id code timestamp], where: 'deleted_at IS NULL', algorithm: :concurrently
     remove_index :events, %w[subscription_id code]
   end
