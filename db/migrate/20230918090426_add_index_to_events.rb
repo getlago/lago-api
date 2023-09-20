34af6a8072696521ce7e3430ca27a5af23e96b83
Vincent Pochet <vincent@getlago.com>
2023-09-20 19:56:27 +0200

    fix: migration fix

--- db/migrate/20230918090426_add_index_to_events.rb
+++ db/migrate/20230918090426_add_index_to_events.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class AddIndexToEvents < ActiveRecord::Migration[7.0]
-  def change
-    disable_ddl_transaction!
+  disable_ddl_transaction!
 
+  def change
     add_index :events, %w[subscription_id code timestamp], where: 'deleted_at IS NULL', algorithm: :concurrently
     remove_index :events, %w[subscription_id code]
   end
