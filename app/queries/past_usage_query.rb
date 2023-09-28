a6815d21b48a95ea3583f4b54cb1bb9cdd820841
Vincent Pochet <vincent@getlago.com>
2023-09-28 15:23:41 +0200

    feat(past_usage): Allow filtering by billable metric code and periods_count

--- app/queries/past_usage_query.rb
+++ app/queries/past_usage_query.rb
@@ -5,7 +5,13 @@ class PastUsageQuery < BaseQuery
     validate_filters
     return result if result.error.present?
 
-    result.usage = query
+    result.usage = query.map do |invoice_subscription|
+      OpenStruct.new(
+        invoice_subscription:,
+        fees: fees_query(invoice_subscription.invoice),
+      )
+    end
+
     result
   end
 
@@ -18,7 +24,16 @@ class PastUsageQuery < BaseQuery
       .order(from_datetime: :desc)
       .includes(:invoice)
 
-    paginate(base_query)
+    base_query = paginate(base_query)
+    base_query = base_query.limit(filters.periods_count) if filters.periods_count
+    base_query
+  end
+
+  def fees_query(invoice)
+    query = invoice.fees.charge
+    return query unless filters.billable_metric_code
+
+    query.joins(:charge).where(charges: { billable_metric_id: billable_metric.id })
   end
 
   def validate_filters
@@ -29,11 +44,19 @@ class PastUsageQuery < BaseQuery
       )
     end
 
-    return if filters.external_subscription_id.present?
+    if filters.external_subscription_id.blank?
+      return result.single_validation_failure!(
+        field: :external_subscription_id,
+        error_code: 'value_is_mandatory',
+      )
+    end
 
-    result.single_validation_failure!(
-      field: :external_subscription_id,
-      error_code: 'value_is_mandatory',
-    )
+    return if filters.billable_metric_code.blank?
+
+    result.not_found_failure!(resource: 'billable_metric') if billable_metric.blank?
+  end
+
+  def billable_metric
+    @billable_metric ||= organization.billable_metrics.find_by(code: filters.billable_metric_code)
   end
 end
