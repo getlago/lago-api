6ada8abb5ac91ffcb372259a4a82cd7deeb26f3d
Lovro Colic <lovro.colic.lc@gmail.com>
2023-10-30 13:48:30 +0100

    fix linter issues

--- db/migrate/20231027144917_add_ready_for_draft_invoices_refresh_to_organization.rb
+++ db/migrate/20231027144917_add_ready_for_draft_invoices_refresh_to_organization.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddReadyForDraftInvoicesRefreshToOrganization < ActiveRecord::Migration[7.0]
   def change
     add_column :organizations, :ready_for_draft_invoices_refresh, :boolean, null: false, default: true
