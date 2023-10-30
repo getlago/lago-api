3bc48dc0b19312599dc49e89905daf28a8806161
Vincent Pochet <vincent@getlago.com>
2023-10-30 13:49:14 +0100

    Update app/validators/timezone_validator.rb
    Co-authored-by: Ivan Novosad <ivannovosad@users.noreply.github.com>

--- app/validators/timezone_validator.rb
+++ app/validators/timezone_validator.rb
@@ -8,8 +8,6 @@ class TimezoneValidator < ActiveModel::EachValidator
   protected
 
   def valid?(value)
-    return true if value == 'UTC'
-
-    value && ActiveSupport::TimeZone::MAPPING.value?(value)
+    value == 'UTC' || ActiveSupport::TimeZone::MAPPING.value?(value)
   end
 end
