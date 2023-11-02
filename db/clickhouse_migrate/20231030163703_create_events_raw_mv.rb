998ffa59cd80c25b68e6f3fab6b8705a4cf2be67
Vincent Pochet <vincent@getlago.com>
2023-11-02 15:15:46 +0100

    feat(clickhouse): Add events_raw_mv to fill events_raw (#1427)

---
+++ db/clickhouse_migrate/20231030163703_create_events_raw_mv.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CreateEventsRawMv < ActiveRecord::Migration[7.0]
+  def change
+    sql = <<-SQL
+      SELECT
+        organization_id,
+        external_customer_id,
+        external_subscription_id,
+        transaction_id,
+        timestamp,
+        code,
+        cast(JSONExtractKeysAndValuesRaw(properties), 'Map(String, String)') as properties
+      FROM events_raw_queue
+    SQL
+
+    create_view :events_raw_mv, materialized: true, as: sql, to: 'events_raw'
+  end
+end
