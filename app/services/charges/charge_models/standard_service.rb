e24df5d03c2fd96db9497d9caa73fa3f0ad68d5c
Romain Sempé <romain@getlago.com>
2023-11-02 15:06:15 +0100

    feat(unit-price): Set precise unit amount to standard charge model

--- app/services/charges/charge_models/standard_service.rb
+++ app/services/charges/charge_models/standard_service.rb
@@ -8,6 +8,13 @@ module Charges
       def compute_amount
         (units * BigDecimal(properties['amount']))
       end
+
+      def unit_amount
+        total_units = aggregation_result.full_units_number || units
+        return 0 if total_units.zero?
+
+        compute_amount / total_units
+      end
     end
   end
 end
--- spec/services/charges/charge_models/standard_service_spec.rb
+++ spec/services/charges/charge_models/standard_service_spec.rb
@@ -14,24 +14,27 @@ RSpec.describe Charges::ChargeModels::StandardService, type: :service do
   before do
     aggregation_result.aggregation = aggregation
     aggregation_result.total_aggregated_units = total_aggregated_units if total_aggregated_units
+    aggregation_result.full_units_number = full_units_number if full_units_number
   end
 
   let(:aggregation_result) { BaseService::Result.new }
   let(:aggregation) { 10 }
   let(:total_aggregated_units) { nil }
+  let(:full_units_number) { nil }
 
   let(:charge) do
     create(
       :standard_charge,
       charge_model: 'standard',
       properties: {
-        amount: '500',
+        amount: '5.12345',
       },
     )
   end
 
   it 'applies the charge model to the value' do
-    expect(apply_standard_service.amount).to eq(5000)
+    expect(apply_standard_service.amount).to eq(51.2345)
+    expect(apply_standard_service.unit_amount).to eq(5.12345)
   end
 
   context 'when aggregation result contains total_aggregated_units' do
@@ -39,6 +42,14 @@ RSpec.describe Charges::ChargeModels::StandardService, type: :service do
 
     it 'assigns the total_aggregated_units to the result' do
       expect(apply_standard_service.total_aggregated_units).to eq(total_aggregated_units)
+    end
+  end
+
+  context 'when aggregation result contains full_units_number' do
+    let(:full_units_number) { 100 }
+
+    it 'applies the charge model to the value' do
+      expect(apply_standard_service.unit_amount).to eq(0.512345)
     end
   end
 end
