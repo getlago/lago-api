30808acc806c2b7e81f19a1003fe7fc0af7718ba
Romain Sempé <romain@getlago.com>
2023-11-07 11:42:09 +0100

    fix: Ensure deletion of expected events

--- spec/jobs/billable_metrics/delete_events_job_spec.rb
+++ spec/jobs/billable_metrics/delete_events_job_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe BillableMetrics::DeleteEventsJob, type: :job, transaction: false 
 
   it 'deletes related events' do
     create(:standard_charge, plan: subscription.plan, billable_metric:)
+    not_impacted_event = create(:event, subscription_id: subscription.id)
     event = create(:event, code: billable_metric.code, subscription_id: subscription.id)
     quantified_event = create(:quantified_event, billable_metric:)
 
@@ -15,6 +16,8 @@ RSpec.describe BillableMetrics::DeleteEventsJob, type: :job, transaction: false 
       expect { described_class.perform_now(billable_metric) }
         .to change { event.reload.deleted_at }.from(nil).to(Time.current)
         .and change { quantified_event.reload.deleted_at }.from(nil).to(Time.current)
+
+      expect(not_impacted_event.reload.deleted_at).to be_nil
     end
   end
 end
