fe163498a0f685e8d6e13fb5b2052e387464f52e
Jeremy Denquin <jeremy@getlago.com>
2023-11-15 16:41:38 +0100

    fix pronto again, thanks ruby lsp :)

--- app/services/customers/generate_checkout_url_service.rb
+++ app/services/customers/generate_checkout_url_service.rb
@@ -11,10 +11,13 @@ module Customers
 
     def call
       return result.not_found_failure!(resource: 'customer') if customer.blank?
-      return result.service_failure!(
-        code: 400,
-        message: 'no payment provider linked to this customer'
-      ) if provider_customer.blank?
+
+      if provider_customer.blank?
+        return result.service_failure!(
+          code: 400,
+          message: 'no payment provider linked to this customer',
+        )
+      end
 
       provider_customer.service.generate_checkout_url(send_webhook: false)
     end
