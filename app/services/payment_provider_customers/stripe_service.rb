b787a76944024af6a73e08a0e423af551989452e
Jeremy Denquin <jeremy@getlago.com>
2023-11-15 16:35:22 +0100

    fix condition for stripe webhook

--- app/services/payment_provider_customers/stripe_service.rb
+++ app/services/payment_provider_customers/stripe_service.rb
@@ -93,7 +93,7 @@ module PaymentProviderCustomers
     end
 
     def generate_checkout_url(send_webhook: true)
-      return result unless customer.organization.webhook_endpoints.any?
+      return result unless (customer.organization.webhook_endpoints.any? || !send_webhook)
 
       res = Stripe::Checkout::Session.create(
         checkout_link_params,
