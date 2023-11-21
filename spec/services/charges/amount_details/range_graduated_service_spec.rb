dfdd2e21d75ef2d1827ee32ee44aec30419ad7ed
Romain Sempé <romain@getlago.com>
2023-11-21 14:14:16 +0100

    feat(unit-price): Rename unit amounts to be consistent with volume

--- spec/services/charges/amount_details/range_graduated_service_spec.rb
+++ spec/services/charges/amount_details/range_graduated_service_spec.rb
@@ -21,9 +21,9 @@ RSpec.describe Charges::AmountDetails::RangeGraduatedService, type: :service do
         from_value: 0,
         to_value: 10,
         flat_amount: 2,
-        unit_amount: 10,
+        per_unit_amount: 10,
         units: 10,
-        total_amount: 100,
+        per_unit_total_amount: 100,
         total_with_flat_amount: 102,
       },
     )
@@ -45,9 +45,9 @@ RSpec.describe Charges::AmountDetails::RangeGraduatedService, type: :service do
           from_value: 11,
           to_value: 20,
           flat_amount: 1,
-          unit_amount: 8,
+          per_unit_amount: 8,
           units: 5,
-          total_amount: 40,
+          per_unit_total_amount: 40,
           total_with_flat_amount: 41,
         },
       )
