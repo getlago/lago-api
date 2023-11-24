5cc12d6200a78b771d7a8a23c988802110eadbff
Ivan Novosad <ivan@getlago.com>
2023-11-24 11:19:39 +0100

    fix(analytics): Fix Gross revenue query

--- app/models/analytics/gross_revenue.rb
+++ app/models/analytics/gross_revenue.rb
@@ -44,63 +44,69 @@ module Analytics
         sql = <<~SQL.squish
           WITH organization_creation_date AS (
             SELECT
-                DATE_TRUNC('month', o.created_at) AS start_month
+              DATE_TRUNC('month', o.created_at) AS start_month
             FROM organizations o
             WHERE o.id = :organization_id
           ),
           all_months AS (
             SELECT
-                generate_series(
-                    (SELECT start_month FROM organization_creation_date),
-                    DATE_TRUNC('month', CURRENT_DATE + INTERVAL '10 years'),
-                    interval '1 month'
-                ) AS month
+              generate_series(
+                (SELECT start_month FROM organization_creation_date),
+                DATE_TRUNC('month', CURRENT_DATE + INTERVAL '10 years'),
+                interval '1 month'
+              ) AS month
           ),
           issued_invoices AS (
             SELECT
-                i.issuing_date,
-                i.total_amount_cents::float AS amount_cents,
-                i.currency
+              i.issuing_date,
+              i.total_amount_cents::float AS amount_cents,
+              i.currency,
+              COALESCE(SUM(refund_amount_cents::float),0) AS total_refund_amount_cents
             FROM invoices i
             LEFT JOIN customers c ON i.customer_id = c.id
+            LEFT JOIN credit_notes cn ON cn.invoice_id = i.id
             WHERE i.organization_id = :organization_id
-                AND i.status = 1
-                #{and_external_customer_id_sql}
+            AND i.status = 1
+            #{and_external_customer_id_sql}
+            GROUP BY i.issuing_date, i.total_amount_cents, i.currency
           ),
           instant_charges AS (
             SELECT
-                f.created_at AS issuing_date,
-                f.amount_cents AS amount_cents,
-                f.amount_currency AS currency
+              f.created_at AS issuing_date,
+              f.amount_cents AS amount_cents,
+              f.amount_currency AS currency,
+              0 AS total_refund_amount_cents
             FROM fees f
             LEFT JOIN subscriptions s ON f.subscription_id = s.id
             LEFT JOIN customers c ON c.id = s.customer_id
             WHERE c.organization_id = :organization_id
-                AND f.invoice_id IS NULL
-                AND f.pay_in_advance IS TRUE
-                #{and_external_customer_id_sql}
+            AND f.invoice_id IS NULL
+            AND f.pay_in_advance IS TRUE
+            #{and_external_customer_id_sql}
           ),
           combined_data AS (
             SELECT
-                DATE_TRUNC('month', issuing_date) AS month,
-                currency,
-                COALESCE(SUM(amount_cents), 0) AS amount_cents
+              DATE_TRUNC('month', issuing_date) AS month,
+              currency,
+              COALESCE(SUM(amount_cents), 0) AS amount_cents,
+              COALESCE(SUM(total_refund_amount_cents), 0) AS total_refund_amount_cents
             FROM (
-                SELECT * FROM issued_invoices
-                UNION ALL
-                SELECT * FROM instant_charges
+              SELECT * FROM issued_invoices
+              UNION ALL
+              SELECT * FROM instant_charges
             ) AS gross_revenue
-            GROUP BY month, currency
+            GROUP BY month, currency, total_refund_amount_cents
           )
           SELECT
             am.month,
-            cd.amount_cents,
-            #{select_currency_sql}
+            cd.currency,
+            SUM(cd.amount_cents - cd.total_refund_amount_cents) AS amount_cents
           FROM all_months am
           LEFT JOIN combined_data cd ON am.month = cd.month
           WHERE am.month <= DATE_TRUNC('month', CURRENT_DATE)
           #{and_months_sql}
           #{and_currency_sql}
+          GROUP BY am.month, cd.currency
           ORDER BY am.month;
         SQL
 
