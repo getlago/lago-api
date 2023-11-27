5af245a417d5dc761a31d14dca8a6f03f54c2186
LovroColic <lovro.colic.lc@gmail.com>
2023-11-27 17:37:24 +0100

    feat(invoice-numbering): Invoice numbering db changes (#1495)
    * add db changes
    
    * fix migration
    
    * fix linter issues

---
+++ db/migrate/20231123105540_add_fields_for_new_document_numbering.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class AddFieldsForNewDocumentNumbering < ActiveRecord::Migration[7.0]
+  def change
+    change_table :organizations, bulk: true do |t|
+      t.integer :document_numbering, default: 0, null: false
+      t.string :document_number_prefix, null: true
+    end
+    add_column :invoices, :organization_sequential_id, :integer, null: false, default: 0
+
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE organizations
+          SET document_number_prefix = UPPER(CAST(SUBSTRING(name, 1, 3) AS VARCHAR)) || '-' || UPPER(CAST(RIGHT(id::TEXT, 4) AS VARCHAR))
+          WHERE document_number_prefix IS NULL;
+        SQL
+
+        execute <<-SQL
+					WITH ordered_organization_invoices AS (
+				    SELECT
+              invoices.id AS invoice_id,
+              ROW_NUMBER() OVER (PARTITION BY invoices.organization_id, DATE_PART('month', invoices.created_at) ORDER BY invoices.created_at ASC) AS rn
+				    FROM invoices
+					)
+
+					UPDATE invoices
+					SET organization_sequential_id = ordered_organization_invoices.rn
+					FROM ordered_organization_invoices
+					WHERE invoices.organization_sequential_id = 0
+            AND ordered_organization_invoices.invoice_id = invoices.id;
+        SQL
+      end
+    end
+
+    change_column_null :organizations, :document_number_prefix, null: false
+  end
+end
