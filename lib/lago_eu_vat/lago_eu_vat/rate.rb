e05c3fc714d92ff5176c027211ba645f058e4af3
Jeremy Denquin <jeremy@getlago.com>
2023-12-04 15:47:25 +0100

    fix pronto

--- lib/lago_eu_vat/lago_eu_vat/rate.rb
+++ lib/lago_eu_vat/lago_eu_vat/rate.rb
@@ -3,7 +3,7 @@
 module LagoEuVat
   class Rate
     def initialize
-      file_path = Rails.root.join('lib', 'lago_eu_vat', 'lago_eu_vat', 'eu_vat_rates.json')
+      file_path = Rails.root.join('lib/lago_eu_vat/lago_eu_vat/eu_vat_rates.json')
       json_file = File.read(file_path)
       @json_countries_rates = JSON.parse(json_file)['items']
     end
@@ -14,9 +14,11 @@ module LagoEuVat
 
     def country_rates(country_code:)
       # NOTE: country rates are ordered by date, so we select the most recent applicable
-      json_countries_rates[country_code].select do |period|
+      country_rates = json_countries_rates[country_code].select do |period|
         Time.zone.now >= DateTime.parse(period['effective_from'])
-      end&.first.fetch('rates')
+      end
+      
+      country_rates&.first.fetch('rates')
     end
 
     private
