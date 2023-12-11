cd2cd09d25f0e68bdf1f4ef9ca43562a4e980a8f
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-12-11 15:48:20 +0100

    misc(fee) expose description in type (#1529)

--- app/graphql/types/fees/object.rb
+++ app/graphql/types/fees/object.rb
@@ -8,6 +8,7 @@ module Types
 
       field :charge, Types::Charges::Object, null: true
       field :currency, Types::CurrencyEnum, null: false
+      field :description, String, null: true
       field :group_name, String, null: true
       field :invoice_display_name, String, null: true
       field :invoice_name, String, null: true
