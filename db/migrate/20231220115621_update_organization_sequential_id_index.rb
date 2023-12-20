2e7d8e316808f0f666cce4b3436d1360462d80c7
Lovro Colic <lovro.colic.lc@gmail.com>
2023-12-20 13:39:53 +0100

    fix linter issues

--- db/migrate/20231220115621_update_organization_sequential_id_index.rb
+++ db/migrate/20231220115621_update_organization_sequential_id_index.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class UpdateOrganizationSequentialIdIndex < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
-    remove_index :invoices, name: :unique_organization_sequential_id
+    remove_index :invoices,
+                 "organization_id, organization_sequential_id, date_trunc('month'::text, created_at)",
+                 name: :unique_organization_sequential_id
     add_index :invoices,
               "organization_id, organization_sequential_id, (date_trunc('month', created_at)::date)",
               name: 'unique_organization_sequential_id',
