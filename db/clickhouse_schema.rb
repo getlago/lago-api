8688d4d2f0f8a762f4b67af94bbf9814d604b766
Jeremy Denquin <jeremy@getlago.com>
2023-12-21 15:20:19 +0100

    fix clickhouse schema

--- db/clickhouse_schema.rb
+++ db/clickhouse_schema.rb
@@ -25,8 +25,8 @@ ClickhouseActiverecord::Schema.define(version: 2023_10_30_163703) do
   end
 
   # TABLE: events_raw_queue
-  # SQL: CREATE TABLE default.events_raw_queue ( `organization_id` String, `external_customer_id` String, `external_subscription_id` String, `transaction_id` String, `timestamp` DateTime, `code` String, `properties` String ) ENGINE = Kafka SETTINGS kafka_broker_list = 'redpanda:9092', kafka_topic_list = 'events-raw', kafka_group_name = 'clickhouse', kafka_format = 'JSONEachRow'
-  create_table "events_raw_queue", id: false, options: "Kafka SETTINGS kafka_broker_list = 'redpanda:9092', kafka_topic_list = 'events-raw', kafka_group_name = 'clickhouse', kafka_format = 'JSONEachRow'", force: :cascade do |t|
+  # SQL: CREATE TABLE default.events_raw_queue ( `organization_id` String, `external_customer_id` String, `external_subscription_id` String, `transaction_id` String, `timestamp` DateTime, `code` String, `properties` String ) ENGINE = Kafka SETTINGS kafka_broker_list = '*****', kafka_topic_list = 'events-raw', kafka_group_name = 'clickhouse', kafka_format = 'JSONEachRow'
+  create_table "events_raw_queue", id: false, options: "Kafka SETTINGS kafka_broker_list = '*****', kafka_topic_list = 'events-raw', kafka_group_name = 'clickhouse', kafka_format = 'JSONEachRow'", force: :cascade do |t|
     t.string "organization_id", null: false
     t.string "external_customer_id", null: false
     t.string "external_subscription_id", null: false
