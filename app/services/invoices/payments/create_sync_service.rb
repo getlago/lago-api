03c36aea7a278aff6fd7e3c323d728cc81bcf356
trontram <txtron2012@gmail.com>
2023-12-25 16:23:08 +0700

    support sync subcription flow

---
+++ app/services/invoices/payments/create_sync_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Invoices
+  module Payments
+    class CreateSyncService < CreateService
+      def call
+        case payment_provider
+        when :stripe
+          Invoices::Payments::StripeCreateJob.perform_now(invoice)
+        when :gocardless
+          Invoices::Payments::GocardlessCreateJob.perform_now(invoice)
+        when :adyen
+          Invoices::Payments::AdyenCreateJob.perform_now(invoice)
+        end
+      end
+    end
+  end
+end
