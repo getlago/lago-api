b228224ea6748461f3d127a3e01419b297685038
LovroColic <lovro.colic.lc@gmail.com>
2023-12-26 11:57:07 +0100

    feat (edit-draft-invoice): add adjusted fee mutation (#1568)
    * setup create adjusted fee flow
    
    * add mutation spec
    
    * fix linter issues

--- app/models/adjusted_fee.rb
+++ app/models/adjusted_fee.rb
@@ -2,7 +2,9 @@
 
 class AdjustedFee < ApplicationRecord
   belongs_to :invoice
+  belongs_to :subscription
   belongs_to :fee, optional: true
+  belongs_to :charge, optional: true
 
   enum fee_type: Fee::FEE_TYPES
 end
---
+++ spec/graphql/mutations/adjusted_fees/create_spec.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Mutations::AdjustedFees::Create, type: :graphql do
+  around { |test| lago_premium!(&test) }
+
+  let(:membership) { create(:membership) }
+  let(:fee) { create(:charge_fee) }
+  let(:input) do
+    {
+      feeId: fee.id,
+      units: 4,
+      unitAmountCents: 1000,
+      invoiceDisplayName: 'Hello',
+    }
+  end
+
+  let(:mutation) do
+    <<-GQL
+      mutation($input: CreateAdjustedFeeInput!) {
+        createAdjustedFee(input: $input) {
+          id,
+          units,
+          invoiceDisplayName
+        }
+      }
+    GQL
+  end
+
+  before { fee.invoice.draft! }
+
+  it 'creates an adjusted fee' do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: membership.organization,
+      query: mutation,
+      variables: { input: },
+    )
+
+    expect(result['data']['createAdjustedFee']['id']).to eq(fee.id)
+  end
+
+  context 'with finalized invoice' do
+    before { fee.invoice.finalized! }
+
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        current_organization: membership.organization,
+        query: mutation,
+        variables: { input: },
+      )
+
+      expect_forbidden_error(result)
+    end
+  end
+
+  context 'without current user' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_organization: membership.organization,
+        query: mutation,
+        variables: { input: },
+      )
+
+      expect_unauthorized_error(result)
+    end
+  end
+
+  context 'without current organization' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        query: mutation,
+        variables: { input: },
+      )
+
+      expect_forbidden_error(result)
+    end
+  end
+end
