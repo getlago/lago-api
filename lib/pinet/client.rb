63d73148ba913629cf19beddab12f460e5078d9c
trontram <txtron2012@gmail.com>
2024-01-02 22:52:59 +0700

    fix style

--- lib/pinet/client.rb
+++ lib/pinet/client.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Pinet
   class Client
     PINET_API_URL = ENV.fetch('PINET_API_URL', 'https://api.pinpayments.com/1/charges')
@@ -13,12 +14,14 @@ module Pinet
 
     def charge(charge_body)
       # Fake response for testing purposes
-      OpenStruct.new({
-        id: SecureRandom.uuid,
-        amount: charge_body[:amount],
-        currency: charge_body[:currency],
-        status: 'succeeded',
-      })
+      OpenStruct.new(
+        {
+          id: SecureRandom.uuid,
+          amount: charge_body[:amount],
+          currency: charge_body[:currency],
+          status: 'succeeded',
+        },
+      )
     end
   end
 end
