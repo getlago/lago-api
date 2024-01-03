4fae0a61cda404c599a3c1a323627c9429948062
Lovro Colic <lovro.colic.lc@gmail.com>
2024-01-03 14:23:09 +0100

    fix linter issues

--- db/migrate/20240103125624_add_group_id_to_adjusted_fees.rb
+++ db/migrate/20240103125624_add_group_id_to_adjusted_fees.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddGroupIdToAdjustedFees < ActiveRecord::Migration[7.0]
   def change
     add_reference :adjusted_fees, :group, type: :uuid, null: true, index: true, foreign_key: true
