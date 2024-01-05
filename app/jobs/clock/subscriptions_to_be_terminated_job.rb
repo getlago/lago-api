49b9d9f274e2f7c85a71d745eeae3c52c9e6f4b3
Edward Poot <edwardmp@gmail.com>
2024-01-05 14:05:06 +0100

    Update app/jobs/clock/subscriptions_to_be_terminated_job.rb
    Co-authored-by: Vincent Pochet <vincent@getlago.com>

--- app/jobs/clock/subscriptions_to_be_terminated_job.rb
+++ app/jobs/clock/subscriptions_to_be_terminated_job.rb
@@ -25,11 +25,11 @@ module Clock
     private
 
     def sent_at_dates
-      # The alert will be sent 15 and 45 days before the subscription is terminated by default.
-      # You can override the default by setting below env var.
-      # E.g. LAGO_SUBSCRIPTION_TERMINATION_ALERT_SENT_AT_DAYS=1,15,45 will cause it
-      # to be sent at 1, 15, 45 days before subscription terminates, respectively.
-      sent_at_days_config = ENV.fetch("LAGO_SUBSCRIPTION_TERMINATION_ALERT_SENT_AT_DAYS", "15,45")
+      # NOTE: The alert will be sent 15 and 45 days before the subscription is terminated by default.
+      #       You can override the default by setting below env var.
+      #       E.g. LAGO_SUBSCRIPTION_TERMINATION_ALERT_SENT_AT_DAYS=1,15,45 will cause it
+      #       to be sent at 1, 15, 45 days before subscription terminates, respectively.
+      sent_at_days_config = ENV.fetch('LAGO_SUBSCRIPTION_TERMINATION_ALERT_SENT_AT_DAYS', '15,45')
       sent_at_days_config.split(',').map { |day_string| Time.current + day_string.to_i.days }.map(&:to_date)
     end
   end
