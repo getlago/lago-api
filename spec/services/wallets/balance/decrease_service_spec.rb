de659c222d684740476787b85ee2d1e03ca83007
Romain Sempé <romain@getlago.com>
2024-01-19 16:54:08 +0100

    feat(prepaid-credits): Add ongoing attributes to increase and decrease wallet

--- spec/services/wallets/balance/decrease_service_spec.rb
+++ spec/services/wallets/balance/decrease_service_spec.rb
@@ -5,7 +5,15 @@ require 'rails_helper'
 RSpec.describe Wallets::Balance::DecreaseService, type: :service do
   subject(:create_service) { described_class.new(wallet:, credits_amount:) }
 
-  let(:wallet) { create(:wallet, balance_cents: 1000, credits_balance: 10.0) }
+  let(:wallet) do
+    create(
+      :wallet,
+      balance_cents: 1000,
+      ongoing_balance_cents: 800,
+      credits_balance: 10.0,
+      credits_ongoing_balance: 8.0,
+    )
+  end
   let(:credits_amount) { BigDecimal('4.5') }
 
   before { wallet }
@@ -14,7 +22,9 @@ RSpec.describe Wallets::Balance::DecreaseService, type: :service do
     it 'updates wallet balance' do
       expect { create_service.call }
         .to change(wallet.reload, :balance_cents).from(1000).to(550)
+        .and change(wallet, :ongoing_balance_cents).from(800).to(350)
         .and change(wallet, :credits_balance).from(10.0).to(5.5)
+        .and change(wallet, :credits_ongoing_balance).from(8.0).to(3.5)
     end
 
     it 'updates wallet consumed status' do
