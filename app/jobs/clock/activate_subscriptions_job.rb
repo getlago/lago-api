4c2e135865d4efa099bd72e7951e2e42a12f70fc
Vincent Pochet <vincent@getlago.com>
2024-02-02 15:47:39 +0100

    fix: Ensure subscription activation clock job cannot be enqueued twice (#1666)

--- app/jobs/clock/activate_subscriptions_job.rb
+++ app/jobs/clock/activate_subscriptions_job.rb
@@ -4,6 +4,8 @@ module Clock
   class ActivateSubscriptionsJob < ApplicationJob
     queue_as 'clock'
 
+    unique :until_executed, on_conflict: :log
+
     def perform
       Subscriptions::ActivateService.new(timestamp: Time.current.to_i).activate_all_pending
     end
