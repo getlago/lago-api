44321e4f122ba9b414a843ac0f166b6d2b056573
Lovro Colic <lovro.colic.lc@gmail.com>
2024-02-05 19:01:52 +0100

    fix linter issues

--- db/migrate/20240205160647_remove_organization_sequential_id_index_from_invoices.rb
+++ db/migrate/20240205160647_remove_organization_sequential_id_index_from_invoices.rb
@@ -2,6 +2,8 @@
 
 class RemoveOrganizationSequentialIdIndexFromInvoices < ActiveRecord::Migration[7.0]
   def change
-    remove_index :invoices, name: 'unique_organization_sequential_id'
+    remove_index :invoices,
+                 "organization_id, organization_sequential_id, (date_trunc('month', created_at)::date)",
+                 name: 'unique_organization_sequential_id'
   end
 end
