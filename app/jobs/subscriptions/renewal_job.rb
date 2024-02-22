d35e887a2706e856b0e761052dcabcaedf66aa17
TronTram <txtron2012@gmail.com>
2024-02-22 17:38:58 +0700

    Fix syntax error in RenewalJob

--- app/jobs/subscriptions/renewal_job.rb
+++ app/jobs/subscriptions/renewal_job.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module Subscriptions
   class RenewalJob < ApplicationJob
-    queue_as "billing"
+    queue_as 'billing'
 
     def perform(timebased_event)
-      result = Subscriptions::RenewalService.new(timebased_event: timebased_event, async: false).call
+      result = Subscriptions::RenewalService.new(timebased_event:, async: false).call
 
       result.raise_if_error!
     end
