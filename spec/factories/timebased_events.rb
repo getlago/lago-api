d53fe7d81a5cf3d47e89fffdffb20e025723151c
TronTram <txtron2012@gmail.com>
2024-02-22 17:48:14 +0700

    fix linter

--- spec/factories/timebased_events.rb
+++ spec/factories/timebased_events.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :timebased_event do
     organization { nil }
     invoice_subscription { nil }
     event_type { 1 }
-    timestamp { "2024-02-21 15:01:59" }
-    external_customer_id { "MyString" }
-    external_subscription_id { "MyString" }
-    metadata { "" }
+    timestamp { '2024-02-21 15:01:59' }
+    external_customer_id { 'MyString' }
+    external_subscription_id { 'MyString' }
+    metadata { '' }
   end
 end
