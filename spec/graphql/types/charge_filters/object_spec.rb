8753cc0f9500a84ed4756babf99640f4fef0fe99
Vincent Pochet <vincent@getlago.com>
2024-02-29 17:13:14 +0100

    feat(filter): Expose charge filters into GraphQL API

---
+++ spec/graphql/types/charge_filters/object_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Types::ChargeFilters::Object do
+  subject { described_class }
+
+  it { is_expected.to have_field(:id).of_type('ID!') }
+  it { is_expected.to have_field(:invoice_display_name).of_type('String') }
+  it { is_expected.to have_field(:properties).of_type('Properties!') }
+  it { is_expected.to have_field(:values).of_type('ChargeFilterValues!') }
+end
