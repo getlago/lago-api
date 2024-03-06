88dfd4bfa1fac0f0e2efc90a1b454d1477b6ff09
Long Truong <ngoclong31120@gmail.com>
2024-03-06 15:02:18 +0700

    feat(charge): Refactor create_sync_service.rb: Remove unused methods

--- app/services/events/create_sync_service.rb
+++ app/services/events/create_sync_service.rb
@@ -28,41 +28,7 @@ module Events
     rescue ActiveRecord::RecordInvalid => e
       result.record_validation_failure!(record: e.record)
     rescue ActiveRecord::RecordNotUnique
-      result.single_validation_failure!(field: :transaction_id, error_code: "value_already_exist")
-    end
-
-    def subscription
-      @subscription ||= Subscription.find_by(external_id: params[:external_subscription_id])
-    end
-
-    def charge
-      @charge ||= find_charge
-    end
-
-    def find_charge
-      plan = subscription&.plan
-      billable_metric = organization.billable_metrics.find_by(code: params[:code])
-
-      Charge.where(plan:, billable_metric:).first if plan && billable_metric
-    end
-
-    def is_charge_group?
-      # NOTE: Currently there is only one type of charge group,
-      #       so we can just check if the charge is a package_group
-      charge&.charge_model == 'package_group'
-    end
-
-    def current_package_count
-      charge_group = charge&.charge_group
-
-      if charge_group && subscription
-        usage_charge_group = UsageChargeGroup.find_by(
-          charge_group_id: charge_group.id,
-          subscription_id: subscription.id,
-        )
-      end
-
-      usage_charge_group&.current_package_count || 1
+      result.single_validation_failure!(field: :transaction_id, error_code: 'value_already_exist')
     end
   end
 end
