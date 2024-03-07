2613099d733b2f2a4ac5b8b6e8c4dde384721c50
Long Truong <ngoclong31120@gmail.com>
2024-03-07 15:37:20 +0700

    feat(charge): rubocop styling

--- db/migrate/20240307050020_remove_properties_from_usage_charge_groups.rb
+++ db/migrate/20240307050020_remove_properties_from_usage_charge_groups.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemovePropertiesFromUsageChargeGroups < ActiveRecord::Migration[7.0]
   def change
     remove_column :usage_charge_groups, :properties, :jsonb
--- db/migrate/20240307050026_add_properties_to_charge_groups.rb
+++ db/migrate/20240307050026_add_properties_to_charge_groups.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPropertiesToChargeGroups < ActiveRecord::Migration[7.0]
   def change
     add_column :charge_groups, :properties, :jsonb, null: false, default: {}
