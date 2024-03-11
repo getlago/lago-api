2f36065932f68c884b97aaa210e5813a5c692227
TronTram <txtron2012@gmail.com>
2024-03-11 09:11:52 +0700

    Refactor time-based service to use base datetime and add block time

--- app/services/subscriptions/dates/timebased_service.rb
+++ app/services/subscriptions/dates/timebased_service.rb
@@ -4,31 +4,64 @@ module Subscriptions
   module Dates
     class TimebasedService < Subscriptions::DatesService
       def from_datetime
-        billing_at
+        base_datetime
       end
 
       def to_datetime
-        billing_at + block_time_in_minutes.minutes
+        add_block_time(from_datetime)
       end
 
       def charges_from_datetime
-        billing_at
+        base_datetime
       end
 
       def charges_to_datetime
-        billing_at + block_time_in_minutes.minutes
+        add_block_time(charges_from_datetime)
       end
 
       private
 
+      def base_datetime
+        if charge_group_type == Utils::Constants::CHARGE_GROUP_TYPES[:PACKAGE_TIMEBASED_GROUP]
+          latest_timebased_event.timestamp
+        else
+          billing_at
+        end
+      end
+
+      def add_block_time(datetime)
+        datetime + block_time_in_minutes.minutes
+      end
+
       def block_time_in_minutes
-        @block_time_in_minutes = subscription
+        @block_time_in_minutes ||= subscription
           .plan
           .charges
           .where(charge_model: :timebased)
           .first
           .properties['block_time_in_minutes']
           .to_i
+      end
+
+      def latest_timebased_event
+        @latest_timebased_event ||= Utils::TimebasedEventFinderService.new(
+          subscription:,
+          timestamp: billing_at,
+        ).latest_timebased_event
+      end
+
+      def charge_group
+        @charge_group ||= subscription
+          .plan
+          .charges.where(charge_model: :package_group)
+          .first
+          &.charge_group
+      end
+
+      def charge_group_type
+        return nil unless charge_group
+
+        @charge_group_type ||= Utils::ChargeGroupTypeDeterminerService.new(charge_group).call
       end
     end
   end
