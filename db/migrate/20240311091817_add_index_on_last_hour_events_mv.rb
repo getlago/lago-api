e0156a0359162245404251b8a52b4d83d47cdda7
Vincent Pochet <vincent@getlago.com>
2024-03-11 11:54:10 +0100

    misc: Event index should not block migration

--- db/migrate/20240311091817_add_index_on_last_hour_events_mv.rb
+++ db/migrate/20240311091817_add_index_on_last_hour_events_mv.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddIndexOnLastHourEventsMv < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_index :last_hour_events_mv, :organization_id
   end
