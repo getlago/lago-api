a1d2505e2250684339d7cf922e308db0da9f51ed
Ivan Novosad <ivannovosad@users.noreply.github.com>
2024-03-14 12:07:55 +0100

    feat(min-spend): Add commitment-related methods to models (#1784)

--- spec/models/invoice_subscription_spec.rb
+++ spec/models/invoice_subscription_spec.rb
@@ -129,4 +129,80 @@ RSpec.describe InvoiceSubscription, type: :model do
       expect(invoice_subscription.subscription_amount_currency).to eq(subscription.plan.amount_currency)
     end
   end
+
+  describe '#previous_invoice_subscription' do
+    subject(:previous_invoice_subscription_call) { invoice_subscription.previous_invoice_subscription }
+
+    context 'when it has previous invoice subscription' do
+      let(:previous_invoice_subscription) do
+        create(
+          :invoice_subscription,
+          subscription: invoice_subscription.subscription,
+          from_datetime: invoice_subscription.from_datetime - 1.year,
+          to_datetime: invoice_subscription.to_datetime - 1.year,
+          charges_from_datetime: invoice_subscription.charges_from_datetime - 1.year,
+          charges_to_datetime: invoice_subscription.charges_to_datetime - 1.year,
+        )
+      end
+
+      before do
+        previous_invoice_subscription
+
+        create(
+          :fee,
+          subscription: previous_invoice_subscription.subscription,
+          invoice: previous_invoice_subscription.invoice,
+          amount_cents: 857, # prorated
+        )
+      end
+
+      it 'returns previous invoice subscription' do
+        expect(previous_invoice_subscription_call).to eq(previous_invoice_subscription)
+      end
+    end
+
+    context 'when there is a previous invoice subscription for different subscription' do
+      let(:previous_invoice_subscription) do
+        create(
+          :invoice_subscription,
+          from_datetime: invoice_subscription.from_datetime - 1.year,
+          to_datetime: invoice_subscription.to_datetime - 1.year,
+          charges_from_datetime: invoice_subscription.charges_from_datetime - 1.year,
+          charges_to_datetime: invoice_subscription.charges_to_datetime - 1.year,
+        )
+      end
+
+      before do
+        previous_invoice_subscription
+
+        create(
+          :invoice_subscription,
+          from_datetime: invoice_subscription.from_datetime - 2.years,
+          to_datetime: invoice_subscription.to_datetime - 2.years,
+          charges_from_datetime: invoice_subscription.charges_from_datetime - 2.years,
+          charges_to_datetime: invoice_subscription.charges_to_datetime - 2.years,
+        )
+      end
+
+      it 'returns nil' do
+        expect(previous_invoice_subscription_call).to be(nil)
+      end
+    end
+
+    context 'when it has no previous invoice subscription' do
+      before do
+        create(
+          :invoice_subscription,
+          from_datetime: invoice_subscription.from_datetime + 1.year,
+          to_datetime: invoice_subscription.to_datetime + 1.year,
+          charges_from_datetime: invoice_subscription.charges_from_datetime + 1.year,
+          charges_to_datetime: invoice_subscription.charges_to_datetime + 1.year,
+        )
+      end
+
+      it 'returns nil' do
+        expect(previous_invoice_subscription_call).to be(nil)
+      end
+    end
+  end
 end
