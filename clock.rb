dd15a8934c4d15353336363c19c56618ce8e1250
Vincent Pochet <vincent@getlago.com>
2024-03-14 11:20:24 +0100

    fix: Handle error in clockwork post validation (#1786)

--- clock.rb
+++ clock.rb
@@ -64,5 +64,7 @@ module Clockwork
 
   every(1.hour, 'schedule:post_validate_events', at: '*:05') do
     Clock::EventsValidationJob.perform_later
+  rescue StandardError => e
+    Sentry.capture_exception(e)
   end
 end
