c02877a013ff86d4ed3a30dbeaa42142f1fdef3b
Karl Kloppenborg <k@rl.ag>
2024-03-26 19:14:38 +1100

    feat: adding SENTRY_ENVIRONMENT to sentry config (#1781)
    * feat: adding SENTRY_ENVIRONMENT to allow people to specify multiple instances in sentry
    * chore: default sentry env to Rails.env when env SENTRY_ENVIRONMENT is not set

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,6 @@ if ENV['SENTRY_DSN']
     config.dsn = ENV['SENTRY_DSN']
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.traces_sample_rate = 0
+    config.environment = ENV['SENTRY_ENVIRONMENT'] || Rails.env
   end
 end
