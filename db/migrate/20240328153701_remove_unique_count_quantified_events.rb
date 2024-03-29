29966dd39c8056f11d5faa5f39dd5293c3c7ddf4
Vincent Pochet <vincent@getlago.com>
2024-03-29 11:55:01 +0100

    misc(unique-count): Remove deprecated QuantifiedEvents (#1825)

---
+++ db/migrate/20240328153701_remove_unique_count_quantified_events.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class RemoveUniqueCountQuantifiedEvents < ActiveRecord::Migration[7.0]
+  def up
+    sql = <<~SQL
+      DELETE FROM quantified_events
+      WHERE billable_metric_id IN (
+        SELECT id
+        FROM billable_metrics
+        WHERE aggregation_type = 3
+      )
+    SQL
+
+    execute(sql)
+  end
+
+  def down; end
+end
