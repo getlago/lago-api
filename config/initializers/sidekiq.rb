d295ed6212c4406857365149527a2c588ba2b2be
Jérémy Denquin <jdenquin@users.noreply.github.com>
2024-04-02 16:43:14 +0200

    misc(sidekiq): use JSON Sidekiq Logger (#1826)
    * misc(sidekiq): Improve Sidekiq Logger
    
    * update config for server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -14,12 +14,14 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
-  config.logger = Rails.logger
+  config.logger = Sidekiq::Logger.new($stdout)
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
   config[:max_retries] = 0
   config[:dead_max_jobs] = ENV.fetch('LAGO_SIDEKIQ_MAX_DEAD_JOBS', 100_000).to_i
 end
 
 Sidekiq.configure_client do |config|
   config.redis = redis_config
-  config.logger = Rails.logger
+  config.logger = Sidekiq::Logger.new($stdout)
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
 end
