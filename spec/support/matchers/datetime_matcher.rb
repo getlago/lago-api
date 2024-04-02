b5768060ab280a7661646c5439042ff1acc988e4
Vincent Pochet <vincent@getlago.com>
2024-04-02 21:56:01 +0200

    fix(invoices): Fix boundaries when in advance anniversary

--- spec/support/matchers/datetime_matcher.rb
+++ spec/support/matchers/datetime_matcher.rb
@@ -2,8 +2,8 @@
 
 RSpec::Matchers.define :match_datetime do |expectation|
   match do |subject|
-    subject = subject.to_datetime.change(usec: 0) if subject.is_a?(String)
-    expectation = DateTime.parse(expectation) if expectation.is_a?(String)
+    subject = Time.zone.parse(subject).change(usec: 0) if subject.is_a?(String)
+    expectation = Time.zone.parse(expectation) if expectation.is_a?(String)
 
     subject.change(usec: 0) == expectation.change(usec: 0)
   end
