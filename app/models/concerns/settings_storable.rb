925393c0420d3f0df37b5c1e139b925b4d161eeb
Ivan Novosad <ivan@getlago.com>
2024-04-08 11:08:57 +0200

    feat(integrations-mutations): Add settings_accessors and secrets_accessors methods

--- app/models/concerns/settings_storable.rb
+++ app/models/concerns/settings_storable.rb
@@ -3,6 +3,20 @@
 module SettingsStorable
   extend ActiveSupport::Concern
 
+  class_methods do
+    def settings_accessors(*method_names)
+      method_names.each do |name|
+        define_method(name) do
+          get_from_settings(name.to_s)
+        end
+
+        define_method("#{name}=".to_sym) do |value|
+          push_to_settings(key: name.to_s, value:)
+        end
+      end
+    end
+  end
+
   def push_to_settings(key:, value:)
     self.settings ||= {}
     settings[key] = value
--- app/models/integrations/netsuite_integration.rb
+++ app/models/integrations/netsuite_integration.rb
@@ -4,68 +4,7 @@ module Integrations
   class NetsuiteIntegration < BaseIntegration
     validates :connection_id, :client_secret, :client_id, :account_id, presence: true
 
-    def connection_id=(connection_id)
-      push_to_secrets(key: 'connection_id', value: connection_id)
-    end
-
-    def connection_id
-      get_from_secrets('connection_id')
-    end
-
-    def client_secret=(client_secret)
-      push_to_secrets(key: 'client_secret', value: client_secret)
-    end
-
-    def client_secret
-      get_from_secrets('client_secret')
-    end
-
-    def account_id=(value)
-      push_to_settings(key: 'account_id', value:)
-    end
-
-    def account_id
-      get_from_settings('account_id')
-    end
-
-    def client_id=(value)
-      push_to_settings(key: 'client_id', value:)
-    end
-
-    def client_id
-      get_from_settings('client_id')
-    end
-
-    def sync_credit_notes=(value)
-      push_to_settings(key: 'sync_credit_notes', value:)
-    end
-
-    def sync_credit_notes
-      get_from_settings('sync_credit_notes')
-    end
-
-    def sync_invoices=(value)
-      push_to_settings(key: 'sync_invoices', value:)
-    end
-
-    def sync_invoices
-      get_from_settings('sync_invoices')
-    end
-
-    def sync_payments=(value)
-      push_to_settings(key: 'sync_payments', value:)
-    end
-
-    def sync_payments
-      get_from_settings('sync_payments')
-    end
-
-    def sync_sales_orders=(value)
-      push_to_settings(key: 'sync_sales_orders', value:)
-    end
-
-    def sync_sales_orders
-      get_from_settings('sync_sales_orders')
-    end
+    settings_accessors :client_id, :account_id, :sync_credit_notes, :sync_invoices, :sync_payments, :sync_sales_orders
+    secrets_accessors :connection_id, :client_secret
   end
 end
