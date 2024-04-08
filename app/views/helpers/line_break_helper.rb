430485df1e836425e01a58f3bf091c86584eb6e1
Julien Bourdeau <julien@julienbourdeau.com>
2024-04-08 11:27:53 +0200

    fix(pdf): Remove extra blank link in custom footer

--- app/views/helpers/line_break_helper.rb
+++ app/views/helpers/line_break_helper.rb
@@ -3,6 +3,6 @@
 class LineBreakHelper
   def self.break_lines(text)
     escaped_text = ERB::Util.html_escape(text.to_s)
-    escaped_text.gsub(/\n/, '<br/>').html_safe # rubocop:disable Rails/OutputSafety
+    escaped_text.split("\n").reject(&:blank?).join('<br/>').html_safe # rubocop:disable Rails/OutputSafety
   end
 end
