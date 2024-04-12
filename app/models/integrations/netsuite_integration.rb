235b5df4848beec2ed1e82a625265faf68757dd3
LovroColic <lovro.colic.lc@gmail.com>
2024-04-12 14:41:33 +0200

    fix (integrations): add account id setter with needed transformations (#1865)
    ## Context
    
    Currently Lago does not support accounting integrations
    
    ## Description
    
    This PR fixes storing account id. We need to store it with necessary
    string transformations

--- app/models/integrations/netsuite_integration.rb
+++ app/models/integrations/netsuite_integration.rb
@@ -5,12 +5,19 @@ module Integrations
     validates :connection_id, :client_secret, :client_id, :account_id, presence: true
 
     settings_accessors :client_id,
-                       :account_id,
                        :sync_credit_notes,
                        :sync_invoices,
                        :sync_payments,
                        :sync_sales_orders,
                        :script_endpoint_url
     secrets_accessors :connection_id, :client_secret
+
+    def account_id=(value)
+      push_to_settings(key: 'account_id', value: value&.downcase&.strip&.split(' ')&.join('-'))
+    end
+
+    def account_id
+      get_from_settings('account_id')
+    end
   end
 end
