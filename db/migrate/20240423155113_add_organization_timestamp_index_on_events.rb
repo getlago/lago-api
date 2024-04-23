f51501fd060569154e4d5fa6f4b71ec8adf971c8
Jeremy Denquin <jeremy@getlago.com>
2024-04-23 18:00:51 +0200

    ddl transaction!

--- db/migrate/20240423155113_add_organization_timestamp_index_on_events.rb
+++ db/migrate/20240423155113_add_organization_timestamp_index_on_events.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddOrganizationTimestampIndexOnEvents < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_index :events, %i[organization_id timestamp], where: 'deleted_at IS NULL'
   end
