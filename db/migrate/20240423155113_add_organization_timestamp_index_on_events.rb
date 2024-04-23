18f71e977928354ca7d3e0e7e758418a39d8ff9f
Jeremy Denquin <jeremy@getlago.com>
2024-04-23 17:58:30 +0200

    add files

---
+++ db/migrate/20240423155113_add_organization_timestamp_index_on_events.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddOrganizationTimestampIndexOnEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_index :events, %i[organization_id timestamp], where: 'deleted_at IS NULL'
+  end
+end
