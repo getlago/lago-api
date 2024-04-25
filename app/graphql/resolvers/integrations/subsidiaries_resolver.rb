727a38038b9cfb31828bfeec4919c1284df152b9
LovroColic <lovro.colic.lc@gmail.com>
2024-04-25 14:59:58 +0200

    feat (accounting-integrations): add logic and graphql support for fetching subsidiaries list (#1935)
    ## Context
    
    Currently Lago does not support accounting integrations
    
    ## Description
    
    This PR covers fetching subsidiaries from Lago's integration aggregator.
    
    List of subsidiaries is needed on the UI in order to allow user to
    select customer's subsidiary. Subsidiary is required field in customer
    create and update actions.
    
    There is small amount of subsidiaries (and it is used only on one place
    in the UI) so we will return it to the UI on the fly from accounting
    aggregator.

---
+++ app/graphql/resolvers/integrations/subsidiaries_resolver.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module Integrations
+    class SubsidiariesResolver < Resolvers::BaseResolver
+      include AuthenticableApiUser
+      include RequiredOrganization
+
+      description 'Query integration subsidiaries'
+
+      argument :integration_id, ID, required: false
+
+      type Types::Integrations::Subsidiaries::Object.collection_type, null: true
+
+      def resolve(integration_id: nil)
+        integration = current_organization.integrations.find(integration_id)
+
+        result = ::Integrations::Aggregator::SubsidiariesService.call(integration:)
+
+        result.subsidiaries
+      end
+    end
+  end
+end
---
+++ spec/graphql/resolvers/integrations/subsidiaries_resolver_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Resolvers::Integrations::SubsidiariesResolver, type: :graphql do
+  let(:query) do
+    <<~GQL
+      query($integrationId: ID!) {
+        integrationSubsidiaries(integrationId: $integrationId) {
+           collection { externalId externalName }
+        }
+      }
+    GQL
+  end
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:integration) { create(:netsuite_integration, organization:) }
+  let(:lago_client) { instance_double(LagoHttpClient::Client) }
+  let(:subsidiaries_endpoint) { 'https://api.nango.dev/v1/netsuite/subsidiaries' }
+
+  let(:headers) do
+    {
+      'Connection-Id' => integration.connection_id,
+      'Authorization' => 'Bearer ',
+      'Provider-Config-Key' => 'netsuite',
+    }
+  end
+
+  let(:aggregator_response) do
+    path = Rails.root.join('spec/fixtures/integration_aggregator/subsidiaries_response.json')
+    JSON.parse(File.read(path))
+  end
+
+  before do
+    allow(LagoHttpClient::Client).to receive(:new)
+      .with(subsidiaries_endpoint)
+      .and_return(lago_client)
+    allow(lago_client).to receive(:get)
+      .with(headers:)
+      .and_return(aggregator_response)
+  end
+
+  it 'returns a list of subsidiaries' do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: organization,
+      query:,
+      variables: { integrationId: integration.id },
+    )
+
+    subsidiaries = result['data']['integrationSubsidiaries']
+
+    aggregate_failures do
+      expect(subsidiaries['collection'].count).to eq(4)
+      expect(subsidiaries['collection'].first['externalId']).to eq('1')
+    end
+  end
+
+  context 'without current organization' do
+    it 'returns an error' do
+      result = execute_graphql(
+        current_user: membership.user,
+        query:,
+        variables: { integrationId: integration.id },
+      )
+
+      expect_graphql_error(
+        result:,
+        message: 'Missing organization id',
+      )
+    end
+  end
+end
