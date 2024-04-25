310a5b5da93c55a38eecd1f7a6c9ede70af6ed86
Jeremy Denquin <jeremy@getlago.com>
2024-04-25 16:50:46 +0200

    feat(okta): Authorize URL with invite

--- app/graphql/mutations/auth/okta/authorize.rb
+++ app/graphql/mutations/auth/okta/authorize.rb
@@ -7,11 +7,12 @@ module Mutations
         graphql_name 'OktaAuthorize'
 
         argument :email, String, required: true
+        argument :invite_token, String, required: false
 
         type Types::Auth::Okta::Authorize
 
-        def resolve(email:)
-          result = ::Auth::Okta::AuthorizeService.call(email:)
+        def resolve(email:, invite_token: nil)
+          result = ::Auth::Okta::AuthorizeService.call(email:, invite_token:)
           result.success? ? result : result_error(result)
         end
       end
--- spec/services/auth/okta/authorize_service_spec.rb
+++ spec/services/auth/okta/authorize_service_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe Auth::Okta::AuthorizeService do
 
   let(:organization) { create(:organization) }
   let(:okta_integration) { create(:okta_integration) }
+  let(:email) { "foo@#{okta_integration.domain}" }
 
   before { okta_integration }
 
   describe '#authorize' do
-    let(:email) { "foo@#{okta_integration.domain}" }
-
     it 'returns an authorize url' do
       result = service.call
 
@@ -22,17 +21,59 @@ RSpec.describe Auth::Okta::AuthorizeService do
         expect(result.url).to include(okta_integration.client_id)
       end
     end
-  end
 
-  context 'when domain is not configured with an integration' do
-    let(:email) { 'foo@bar.com' }
+    context 'when domain is not configured with an integration' do
+      let(:email) { 'foo@bar.com' }
 
-    it 'returns a failure result' do
-      result = service.call
+      it 'returns a failure result' do
+        result = service.call
 
-      aggregate_failures do
-        expect(result).not_to be_success
-        expect(result.error.messages.values.flatten).to include('domain_not_configured')
+        aggregate_failures do
+          expect(result).not_to be_success
+          expect(result.error.messages.values.flatten).to include('domain_not_configured')
+        end
+      end
+    end
+
+    context 'with invite token' do
+      subject(:service) { described_class.new(email:, invite_token: invite.token) }
+
+      let(:invite) { create(:invite, email:) }
+
+      it 'returns an authorize url' do
+        result = service.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.url).to include(okta_integration.organization_name.downcase)
+          expect(result.url).to include(okta_integration.client_id)
+        end
+      end
+
+      context 'when invite email is different from the email' do
+        let(:invite) { create(:invite, email: 'foo@b.com') }
+
+        it 'returns a failure result' do
+          result = service.call
+
+          aggregate_failures do
+            expect(result).not_to be_success
+            expect(result.error.messages.values.flatten).to include('invite_email_mistmatch')
+          end
+        end
+      end
+
+      context 'when pending invite does not exists' do
+        let(:invite) { create(:invite, email:, status: :accepted) }
+
+        it 'returns a failure result' do
+          result = service.call
+
+          aggregate_failures do
+            expect(result).not_to be_success
+            expect(result.error.messages.values.flatten).to include('invite_not_found')
+          end
+        end
       end
     end
   end
