148ba61611de4652b9dd88d70971cc303596e122
Jérémy Denquin <jdenquin@users.noreply.github.com>
2024-04-26 12:01:03 +0200

    feat(okta): Add Login Logic (#1889)
    - Add okta login service and logic

---
+++ app/services/auth/okta/login_service.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+module Auth
+  module Okta
+    class LoginService < BaseService
+      def initialize(code:, state:)
+        @code = code
+        @state = state
+
+        super
+      end
+
+      def call
+        check_state
+        check_okta_integration
+
+        query_okta_access_token
+        check_userinfo
+
+        find_or_create_user
+        find_or_create_membership
+
+        UsersService.new.new_token(result.user)
+      rescue Auth::Okta::ValidationError => e
+        result.single_validation_failure!(error_code: e.message)
+        result
+      rescue LagoHttpClient::HttpError
+        result.single_validation_failure!(error_code: 'okta_request_error')
+        result
+      end
+
+      private
+
+      attr_reader :code, :state
+
+      def check_state
+        email = Rails.cache.read(state)
+        raise Auth::Okta::ValidationError, 'state_not_found' if email.blank?
+
+        Rails.cache.delete(state)
+
+        result.email = email
+      end
+
+      def check_okta_integration
+        okta_integration = ::Integrations::OktaIntegration
+          .where('settings->>\'domain\' IS NOT NULL')
+          .where('settings->>\'domain\' = ?', result.email.split('@').last)
+          .first
+
+        raise Auth::Okta::ValidationError, 'domain_not_configured' if okta_integration.blank?
+
+        result.okta_integration = okta_integration
+      end
+
+      def query_okta_access_token
+        params = {
+          client_id: result.okta_integration.client_id,
+          client_secret: result.okta_integration.client_secret,
+          grant_type: 'authorization_code',
+          code:,
+          redirect_uri: "#{ENV['LAGO_FRONT_URL']}/auth/okta/callback",
+        }
+
+        token_client = LagoHttpClient::Client.new("https://#{result.okta_integration.organization_name.downcase}.okta.com/oauth2/default/v1/token")
+        response = token_client.post(params, {})
+        result.okta_access_token = response['access_token']
+      end
+
+      def check_userinfo
+        userinfo_client = LagoHttpClient::Client.new("https://#{result.okta_integration.organization_name.downcase}.okta.com/oauth2/default/v1/userinfo")
+        userinfo_headers = { 'Authorization' => "Bearer #{result.okta_access_token}" }
+        response = userinfo_client.get(headers: userinfo_headers)
+
+        raise Auth::Okta::ValidationError, 'okta_userinfo_error' if response['email'] != result.email
+
+        result.userinfo = response
+      end
+
+      def find_or_create_user
+        user = User.find_or_initialize_by(email: result.email)
+
+        if user.new_record?
+          user.password = SecureRandom.hex(16)
+          user.save!
+        end
+
+        result.user = user
+      end
+
+      def find_or_create_membership
+        result.user.memberships.find_or_create_by(organization_id: result.okta_integration.organization_id)
+      end
+    end
+
+    class ValidationError < StandardError; end
+  end
+end
---
+++ spec/services/auth/okta/login_service_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Auth::Okta::LoginService do
+  let(:service) { described_class.new(code: 'code', state:) }
+  let(:okta_integration) { create(:okta_integration, domain: 'bar.com', organization_name: 'foo') }
+  let(:lago_http_client) { instance_double(LagoHttpClient::Client) }
+  let(:okta_token_response) { OpenStruct.new(body: { 'access_token': 'access_token' }) }
+  let(:okta_userinfo_response) { OpenStruct.new({ 'email': 'foo@bar.com' }) }
+  let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+  let(:state) { SecureRandom.uuid }
+
+  before do
+    okta_integration
+
+    allow(Rails).to receive(:cache).and_return(memory_store)
+    memory_store.write(state, 'foo@bar.com')
+
+    allow(LagoHttpClient::Client).to receive(:new).and_return(lago_http_client)
+    allow(lago_http_client).to receive(:post).and_return(okta_token_response)
+    allow(lago_http_client).to receive(:get).and_return(okta_userinfo_response)
+  end
+
+  describe '#call' do
+    it 'creates user, membership and authenticate user' do
+      result = service.call
+
+      aggregate_failures do
+        expect(result).to be_success
+        expect(result.user.email).to eq('foo@bar.com')
+        expect(result.token).to be_present
+      end
+    end
+
+    context 'when state is not found' do
+      before do
+        Rails.cache.clear
+      end
+
+      it 'returns error' do
+        result = service.call
+
+        aggregate_failures do
+          expect(result).not_to be_success
+          expect(result.error.messages.values.flatten).to include('state_not_found')
+        end
+      end
+    end
+
+    context 'when domain is not configured with an integration' do
+      let(:okta_integration) { nil }
+
+      it 'returns error' do
+        result = service.call
+
+        aggregate_failures do
+          expect(result).not_to be_success
+          expect(result.error.messages.values.flatten).to include('domain_not_configured')
+        end
+      end
+    end
+
+    context 'when okta userinfo email is different from the state one' do
+      let(:okta_userinfo_response) { OpenStruct.new({ 'email': 'foo@test.com' }) }
+
+      it 'returns error' do
+        result = service.call
+
+        aggregate_failures do
+          expect(result).not_to be_success
+          expect(result.error.messages.values.flatten).to include('okta_userinfo_error')
+        end
+      end
+    end
+
+    context 'when user already exists' do
+      let(:user) { create(:user, email: 'foo@bar.com') }
+
+      before { user }
+
+      it 'does not create a new user' do
+        expect { service.call }.not_to change(User, :count)
+      end
+    end
+
+    context 'when membership already exists' do
+      let(:user) { create(:user, email: 'foo@bar.com') }
+      let(:membership) { create(:membership, user:, organization: okta_integration.organization) }
+
+      before { membership }
+
+      it 'does not create a new membership' do
+        expect { service.call }.not_to change(Membership, :count)
+      end
+    end
+  end
+end
