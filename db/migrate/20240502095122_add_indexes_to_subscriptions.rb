7600dc55f7efad62e7e1c09315650d307ef72cf8
Vincent Pochet <vincent@getlago.com>
2024-05-02 16:48:26 +0200

    misc(subscription): Add indexes to speedup billing (#1979)
    ## Context
    
    Lago is facing some performance issues when it has to generate a lot of
    invoices on a single day.
    
    ## Description
    
    This PR:
    - Add 2 indexes
      - `subscriptions#started_at` and `subscriptions#ending_at`
    - `invoice_subscriptions#subscription_id`,
    `invoice_subscriptions#from_datetime`,
    `invoice_subscriptions#to_datetime`

---
+++ db/migrate/20240502095122_add_indexes_to_subscriptions.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AddIndexesToSubscriptions < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def change
+    add_index :subscriptions, [:started_at, :ending_at]
+    add_index :invoice_subscriptions,
+              [:subscription_id, :from_datetime, :to_datetime],
+              name: 'index_invoice_subscriptions_boundaries',
+              algorithm: :concurrently
+  end
+end
