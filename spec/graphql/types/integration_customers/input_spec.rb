8127c8ef8f27f4a703d814a8d89056189de553df
Ivan Novosad <ivannovosad@users.noreply.github.com>
2024-05-06 17:12:55 +0200

    feat(integrations): Add integration and integration_code to customer input (#1989)
    ## Roadmap Task
    
    👉  https://getlago.canny.io/feature-requests/p/integration-with-netsuite
    
    ## Context
    
    Currently Lago does not support accounting integrations
    
    ## Description
    
    This PR adds `integration` and `integration_code` to integration
    customer input and adds this input to customer inputs.

--- spec/graphql/types/integration_customers/input_spec.rb
+++ spec/graphql/types/integration_customers/input_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Types::IntegrationCustomers::Input do
   subject { described_class }
 
   it { is_expected.to accept_argument(:external_customer_id).of_type('String') }
+  it { is_expected.to accept_argument(:integration).of_type('IntegrationTypeEnum') }
+  it { is_expected.to accept_argument(:integration_code).of_type('String') }
   it { is_expected.to accept_argument(:subsidiary_id).of_type('String') }
   it { is_expected.to accept_argument(:sync_with_provider).of_type('Boolean') }
 end
