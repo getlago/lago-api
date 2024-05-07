bab818b264db58156358ab9f339a9c4648ff17c0
Ivan Novosad <ivan@getlago.com>
2024-05-07 11:05:58 +0200

    feat(integrations:) Add/rename graphql fields

--- app/graphql/types/integration_customers/netsuite.rb
+++ app/graphql/types/integration_customers/netsuite.rb
@@ -7,6 +7,8 @@ module Types
 
       field :external_customer_id, String, null: true
       field :id, ID, null: false
+      field :integration, Types::Integrations::IntegrationTypeEnum, null: true
+      field :integration_code, String, null: true
       field :subsidiary_id, String, null: true
       field :sync_with_provider, Boolean, null: true
     end
--- spec/graphql/types/integration_customers/netsuite_spec.rb
+++ spec/graphql/types/integration_customers/netsuite_spec.rb
@@ -5,8 +5,10 @@ require 'rails_helper'
 RSpec.describe Types::IntegrationCustomers::Netsuite do
   subject { described_class }
 
-  it { is_expected.to have_field(:id).of_type('ID!') }
   it { is_expected.to have_field(:external_customer_id).of_type('String') }
+  it { is_expected.to have_field(:id).of_type('ID!') }
+  it { is_expected.to have_field(:integration).of_type('IntegrationTypeEnum') }
+  it { is_expected.to have_field(:integration_code).of_type('String') }
   it { is_expected.to have_field(:subsidiary_id).of_type('String') }
   it { is_expected.to have_field(:sync_with_provider).of_type('Boolean') }
 end
