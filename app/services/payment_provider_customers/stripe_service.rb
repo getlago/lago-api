964e44f0a64a810afa8b0b5fc58ff3e76706c981
Romain Sempé <romain@getlago.com>
2024-05-10 14:30:54 +0200

    misc: Do not try to generate checkout url with deleted customer

--- app/services/payment_provider_customers/stripe_service.rb
+++ app/services/payment_provider_customers/stripe_service.rb
@@ -113,6 +113,7 @@ module PaymentProviderCustomers
     end
 
     def generate_checkout_url(send_webhook: true)
+      return result unless customer # NOTE: Customer is nil when deleted.
       return result unless customer.organization.webhook_endpoints.any? || !send_webhook || !payment_provider(customer)
 
       res = Stripe::Checkout::Session.create(
