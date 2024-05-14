b9da6fb1b4195abe42a97d511da128504dd4490a
Romain Sempé <romain@getlago.com>
2024-05-14 10:07:56 +0200

    feat(wallet): Accept negative values for threshold_credits

--- app/services/wallets/validate_service.rb
+++ app/services/wallets/validate_service.rb
@@ -79,7 +79,7 @@ module Wallets
       end
 
       if recurring_rule[:rule_type]&.to_s == 'threshold' &&
-          ::Validators::DecimalAmountService.new(recurring_rule[:threshold_credits]).valid_amount?
+          ::Validators::DecimalAmountService.new(recurring_rule[:threshold_credits]).valid_decimal?
 
         return true
       end
