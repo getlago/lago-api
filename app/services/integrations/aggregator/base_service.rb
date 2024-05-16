03ec5a22161776497433061ce7d42603125db843
Lovro Colic <lovro.colic.lc@gmail.com>
2024-05-16 17:30:06 +0200

    add base setup for syncing invoice with integration

--- app/services/integrations/aggregator/base_service.rb
+++ app/services/integrations/aggregator/base_service.rb
@@ -45,6 +45,40 @@ module Integrations
         }
       end
 
+      def tax_item
+        @tax_item ||= integration.integration_collection_mappings.where(mapping_type: :tax)&.first
+      end
+
+      def commitment_item
+        @tax_item ||= integration.integration_collection_mappings.where(mapping_type: :minimum_commitment)&.first
+      end
+
+      def subscription_item
+        @tax_item ||= integration.integration_collection_mappings.where(mapping_type: :subscription_fee)&.first
+      end
+
+      def coupon_item
+        @tax_item ||= integration.integration_collection_mappings.where(mapping_type: :coupon)&.first
+      end
+
+      def credit_item
+        @tax_item ||= integration.integration_collection_mappings.where(mapping_type: :prepaid_credit)&.first
+      end
+
+      def credit_note_item
+        @tax_item ||= integration.integration_collection_mappings.where(mapping_type: :credit_note)&.first
+      end
+
+      def fallback_item
+        @fallback_item ||= integration.integration_collection_mappings.where(mapping_type: :fallback_item)&.first
+      end
+
+      def amount(amount_cents)
+        currency = invoice.total_amount.currency
+
+        amount_cents.round.fdiv(currency.subunit_to_unit)
+      end
+
       def secret_key
         ENV['NANGO_SECRET_KEY']
       end
---
+++ app/services/integrations/aggregator/invoices/create_service.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+module Integrations
+  module Aggregator
+    module Invoices
+      class CreateService < Integrations::Aggregator::BaseService
+        def initialize(invoice:)
+          @invoice = invoice
+
+          super(integration:)
+        end
+
+        def action_path
+          "v1/#{provider}/invoices"
+        end
+
+        def call
+          return unless integration
+          return unless invoice.finalized?
+          return unless integration.sync_invoices
+          return unless fallback_item
+
+          response = http_client.post_with_response(payload, headers)
+          result.external_invoice_id = JSON.parse(response.body)
+
+          result
+        end
+
+        private
+
+        attr_reader :invoice
+
+        delegate :customer, to: :invoice
+
+        def headers
+          {
+            'Connection-Id' => integration.connection_id,
+            'Authorization' => "Bearer #{secret_key}",
+            'Provider-Config-Key' => provider,
+          }
+        end
+
+        def integration
+          return nil unless integration_customer
+
+          integration_customer&.integration
+        end
+
+        def integration_customer
+          @integration_customer ||= customer.integration_customers&.first
+        end
+
+        def billable_metric_item(fee)
+          @tax_item ||=
+            integration
+              .integration_mappings
+              .find_by(mappable_type: 'BillableMetric', mappable_id: fee.billable_metric.id)
+        end
+
+        def add_on_item(fee)
+          @tax_item ||=
+            integration
+              .integration_mappings
+              .find_by(mappable_type: 'AddOn', mappable_id: fee.add_on.id)
+        end
+
+        def item(fee)
+          mapped_item = if fee.charge?
+            billable_metric_item(fee)
+          elsif fee.add_on?
+            add_on_item(fee)
+          elsif fee.credit?
+            credit_item
+          elsif fee.commitment?
+            commitment_item
+          elsif fee.subscription?
+            subscription_item
+          end
+
+          {
+            'item' => mapped_item.external_id,
+            'account' => mapped_item.external_account_code,
+            'quantity' => fee.units,
+            'rate' => fee.precise_unit_amount,
+          }
+        end
+
+        def discounts
+          output = []
+
+          if invoice.coupons_amount_cents > 0
+            output << {
+              'item' => coupon_item.external_id,
+              'account' => coupon_item.external_account_code,
+              'quantity' => 1,
+              'rate' => -(amount(invoice.coupons_amount_cents)),
+            }
+          end
+
+          if invoice.prepaid_credit_amount_cents > 0
+            output << {
+              'item' => credit_item.external_id,
+              'account' => credit_item.external_account_code,
+              'quantity' => 1,
+              'rate' => -(amount(invoice.prepaid_credit_amount_cents)),
+            }
+          end
+
+          if invoice.credit_notes_amount_cents > 0
+            output << {
+              'item' => credit_note_item.external_id,
+              'account' => credit_note_item.external_account_code,
+              'quantity' => 1,
+              'rate' => -(amount(invoice.credit_notes_amount_cents)),
+            }
+          end
+
+          output
+        end
+
+        def payload
+          {
+            'type' => 'invoice',
+            'isDynamic' => false,
+            'columns' => {
+              'tranid' => invoice.id,
+              'entity' => integration_customer.external_customer_id,
+              'istaxable' => true,
+              'taxitem' => tax_item.external_id || fallback_item.external_id,
+              'taxamountoverride' => amount(invoice.taxes_amount_cents),
+              'otherrefnum' => invoice.number,
+              'custbody_lago_id' => invoice.id,
+              'custbody_ava_disable_tax_calculation' => true,
+            },
+            'lines' => [
+              {
+                'sublistId' => 'item',
+                'lineItems' => invoice.fees.map { |fee| item(fee) } + discounts
+              },
+            ],
+          }
+        end
+      end
+    end
+  end
+end
