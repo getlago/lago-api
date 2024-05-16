820ae9371a18a08e5efe6c1742be3269c0860974
Julien Bourdeau <julien@julienbourdeau.com>
2024-05-16 16:12:36 +0200

    feat(email): Use `img` tag to display Lago logo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,4 +4,10 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   self.delivery_job = SendEmailJob
+
+  before_action :set_shared_variables
+
+  def set_shared_variables
+    @lago_logo_url = "https://assets.getlago.com/lago-logo-email.png"
+  end
 end
