f2c546d2bfc82a50f4c6aa671f3f67501cbc6278
LovroColic <lovro.colic.lc@gmail.com>
2024-05-16 10:28:10 +0200

    fix(integration-customers): update integration customers graphql input (#2014)
    ## Context
    
    Currently Lago does not support accounting integrations
    
    ## Description
    
    This PR updates Input integration customer object for graphql side

--- spec/graphql/types/integration_customers/input_spec.rb
+++ spec/graphql/types/integration_customers/input_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Types::IntegrationCustomers::Input do
   subject { described_class }
 
+  it { is_expected.to accept_argument(:id).of_type('ID') }
   it { is_expected.to accept_argument(:external_customer_id).of_type('String') }
   it { is_expected.to accept_argument(:integration_type).of_type('IntegrationTypeEnum') }
   it { is_expected.to accept_argument(:integration_code).of_type('String') }
