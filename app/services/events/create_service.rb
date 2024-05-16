160a1d79c249490b2474148ea120efb92f225270
Jeremy Denquin <jeremy@getlago.com>
2024-05-16 17:04:00 +0200

    misc(events): Do not save on PG when using clickhouse

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -20,12 +20,13 @@ module Events
       event.properties = params[:properties] || {}
       event.metadata = metadata || {}
       event.timestamp = Time.zone.at(params[:timestamp] ? params[:timestamp].to_f : timestamp)
-      event.save!
+
+      event.save! unless organization.clickhouse_aggregation?
 
       result.event = event
 
       produce_kafka_event(event)
-      Events::PostProcessJob.perform_later(event:)
+      Events::PostProcessJob.perform_later(event:) unless organization.clickhouse_aggregation?
 
       result
     rescue ActiveRecord::RecordInvalid => e
