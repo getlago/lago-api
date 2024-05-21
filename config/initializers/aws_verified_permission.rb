281689fb18627c1f0d5e1412f2df52c628de8456
ghun131 <hungdh131@gmail.com>
2024-05-21 10:50:08 +0700

    feat: remove old verified permission init

--- config/initializers/aws_verified_permission.rb
+++
@@ -1,6 +0,0 @@
-require 'aws-sdk-verifiedpermissions' # require the specific AWS SDK you're using
-
-$aws_avp_client = Aws.config.update({
-  region: 'ap-southeast-1',
-  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID', nil), ENV.fetch('AWS_SECRET_ACCESS_KEY', nil))
-})
