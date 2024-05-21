3b385b7ea1b8bca4c9b9f51c912f7e973e017b36
Julien Bourdeau <julien@julienbourdeau.com>
2024-05-21 09:54:49 +0200

    edit new test

--- spec/services/webhooks/payment_providers/error_service_spec.rb
+++ spec/services/webhooks/payment_providers/error_service_spec.rb
@@ -9,27 +9,5 @@ RSpec.describe Webhooks::PaymentProviders::ErrorService do
   let(:organization) { create(:organization) }
   let(:webhook_options) { {provider_error: {message: 'message', error_code: 'code', source: 'stripe', action: 'payment_provider.register_webhook'}} }
 
-  describe '.call' do
-    let(:lago_client) { instance_double(LagoHttpClient::Client) }
-
-    before do
-      allow(LagoHttpClient::Client).to receive(:new)
-        .with(organization.webhook_endpoints.first.webhook_url)
-        .and_return(lago_client)
-      allow(lago_client).to receive(:post_with_response)
-    end
-
-    it 'builds payload with payment_provider.error webhook type' do
-      webhook_service.call
-
-      aggregate_failures do
-        expect(LagoHttpClient::Client).to have_received(:new)
-          .with(organization.webhook_endpoints.first.webhook_url)
-        expect(lago_client).to have_received(:post_with_response) do |payload|
-          expect(payload[:webhook_type]).to eq('payment_provider.error')
-          expect(payload[:object_type]).to eq('payment_provider_error')
-        end
-      end
-    end
-  end
+  it_behaves_like 'creates webhook', 'payment_provider.error', 'payment_provider_error'
 end
