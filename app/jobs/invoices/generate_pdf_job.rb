664ed525ddaf5898a61ad19b1557689d11d9038b
Vincent Pochet <vincent@getlago.com>
2024-05-23 17:10:48 +0200

    misc: Auto retry PDF generate jobs in case of HTTP errors

--- app/jobs/invoices/generate_pdf_job.rb
+++ app/jobs/invoices/generate_pdf_job.rb
@@ -4,6 +4,8 @@ module Invoices
   class GeneratePdfJob < ApplicationJob
     queue_as 'invoices'
 
+    retry_on LagoHttpClient::HttpError, wait: :exponentially_longer, attempts: 6
+
     def perform(invoice)
       result = Invoices::GeneratePdfService.call(invoice:, context: 'api')
       result.raise_if_error!
