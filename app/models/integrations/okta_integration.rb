292f980a12a6381c3de464882dac7adc8c4237a7
Jeremy Denquin <jeremy@getlago.com>
2024-05-23 11:44:43 +0200

    feat(okta): Add Okta integration uniqueness

--- app/models/integrations/okta_integration.rb
+++ app/models/integrations/okta_integration.rb
@@ -3,8 +3,20 @@
 module Integrations
   class OktaIntegration < BaseIntegration
     validates :client_secret, :client_id, :domain, :organization_name, presence: true
+    validate :domain_uniqueness
 
     settings_accessors :client_id, :domain, :organization_name
     secrets_accessors :client_secret
+
+    private
+
+    def domain_uniqueness
+      okta_integration = ::Integrations::OktaIntegration
+          .where('settings->>\'domain\' IS NOT NULL')
+          .where('settings->>\'domain\' = ?', domain)
+          .first
+      
+      errors.add(:domain, 'domain_not_unique') if okta_integration.present?
+    end
   end
 end
---
+++ spec/models/integrations/okta_integration_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Integrations::OktaIntegration, type: :model do
+  subject(:okta_integration) { build(:okta_integration) }
+
+  it { is_expected.to validate_presence_of(:domain) }
+  it { is_expected.to validate_presence_of(:organization_name) }
+  it { is_expected.to validate_presence_of(:client_id) }
+  it { is_expected.to validate_presence_of(:client_secret) }
+
+  describe 'validations' do
+    it 'validates uniqueness of domain' do
+      expect(okta_integration).to be_valid
+    end
+
+    context 'when domain already exists' do
+      before { create(:okta_integration) }
+
+      it 'does not validate the record' do
+        expect(okta_integration).not_to be_valid
+        expect(okta_integration.errors).to include(:domain)
+      end
+    end
+  end
+end
