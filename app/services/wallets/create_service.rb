2e130eea7fca8a6f68e632bcc7a141a998c0ea0a
Romain Sempé <romain@getlago.com>
2024-05-29 10:45:47 +0200

    feat(wallet): Extract creation of wallet transaction rule

--- app/services/wallets/create_service.rb
+++ app/services/wallets/create_service.rb
@@ -23,13 +23,8 @@ module Wallets
         wallet.currency = wallet.customer.currency
         wallet.save!
 
-        if args[:recurring_transaction_rules] && License.premium?
-          create_recurring_transaction_rule(
-            recurring_transaction_rules: args[:recurring_transaction_rules],
-            paid_credits: args[:paid_credits],
-            granted_credits: args[:granted_credits],
-            wallet:,
-          )
+        if args[:recurring_transaction_rules]
+          Wallets::RecurringTransactionRules::CreateService.call(wallet:, wallet_params: args)
         end
       end
 
@@ -54,21 +49,6 @@ module Wallets
 
     def valid?(**args)
       Wallets::ValidateService.new(result, **args).valid?
-    end
-
-    def create_recurring_transaction_rule(recurring_transaction_rules:, paid_credits:, granted_credits:, wallet:)
-      recurring_rule = recurring_transaction_rules.first
-
-      RecurringTransactionRule.create!(
-        wallet:,
-        paid_credits:,
-        granted_credits:,
-        threshold_credits: recurring_rule[:threshold_credits] || '0.0',
-        interval: recurring_rule[:interval],
-        method: recurring_rule[:method] || 'fixed',
-        target_ongoing_balance: recurring_rule[:target_ongoing_balance],
-        trigger: recurring_rule[:trigger].to_s
-      )
     end
   end
 end
