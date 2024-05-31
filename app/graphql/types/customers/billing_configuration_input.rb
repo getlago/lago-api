a7cd82d328cde45b23afd2f9ae699c55ad217f98
Julien Bourdeau <julien@julienbourdeau.com>
2024-05-31 14:49:51 +0200

    update customer settings permissions

--- app/graphql/types/customers/billing_configuration_input.rb
+++ app/graphql/types/customers/billing_configuration_input.rb
@@ -5,7 +5,7 @@ module Types
     class BillingConfigurationInput < BaseInputObject
       graphql_name 'CustomerBillingConfigurationInput'
 
-      argument :document_locale, String, required: false
+      argument :document_locale, String, required: false, permissions: %w[customers:create customers:update customer_settings:update:lang]
     end
   end
 end
--- app/graphql/types/customers/update_customer_input.rb
+++ app/graphql/types/customers/update_customer_input.rb
@@ -7,35 +7,37 @@ module Types
 
       argument :id, ID, required: true
 
-      argument :address_line1, String, required: false
-      argument :address_line2, String, required: false
-      argument :city, String, required: false
-      argument :country, Types::CountryCodeEnum, required: false
-      argument :currency, Types::CurrencyEnum, required: false
-      argument :email, String, required: false
-      argument :external_id, String, required: true
-      argument :external_salesforce_id, String, required: false
-      argument :invoice_grace_period, Integer, required: false
-      argument :legal_name, String, required: false
-      argument :legal_number, String, required: false
-      argument :logo_url, String, required: false
-      argument :name, String, required: true
-      argument :net_payment_term, Integer, required: false
-      argument :phone, String, required: false
-      argument :state, String, required: false
-      argument :tax_codes, [String], required: false
-      argument :tax_identification_number, String, required: false
-      argument :timezone, Types::TimezoneEnum, required: false
-      argument :url, String, required: false
-      argument :zipcode, String, required: false
+      argument :address_line1, String, required: false, permission: 'customers:update'
+      argument :address_line2, String, required: false, permission: 'customers:update'
+      argument :city, String, required: false, permission: 'customers:update'
+      argument :country, Types::CountryCodeEnum, required: false, permission: 'customers:update'
+      argument :currency, Types::CurrencyEnum, required: false, permission: 'customers:update'
+      argument :email, String, required: false, permission: 'customers:update'
+      argument :external_id, String, required: true, permission: 'customers:update'
+      argument :external_salesforce_id, String, required: false, permission: 'customers:update'
+      argument :legal_name, String, required: false, permission: 'customers:update'
+      argument :legal_number, String, required: false, permission: 'customers:update'
+      argument :logo_url, String, required: false, permission: 'customers:update'
+      argument :name, String, required: true, permission: 'customers:update'
+      argument :phone, String, required: false, permission: 'customers:update'
+      argument :state, String, required: false, permission: 'customers:update'
+      argument :tax_identification_number, String, required: false, permission: 'customers:update'
+      argument :timezone, Types::TimezoneEnum, required: false, permission: 'customers:update'
+      argument :url, String, required: false, permission: 'customers:update'
+      argument :zipcode, String, required: false, permission: 'customers:update'
 
-      argument :metadata, [Types::Customers::Metadata::Input], required: false
+      argument :metadata, [Types::Customers::Metadata::Input], required: false, permission: 'customers:update'
 
-      argument :payment_provider, Types::PaymentProviders::ProviderTypeEnum, required: false
-      argument :payment_provider_code, String, required: false
-      argument :provider_customer, Types::PaymentProviderCustomers::ProviderInput, required: false
+      argument :payment_provider, Types::PaymentProviders::ProviderTypeEnum, required: false, permission: 'customers:update'
+      argument :payment_provider_code, String, required: false, permission: 'customers:update'
+      argument :provider_customer, Types::PaymentProviderCustomers::ProviderInput, required: false, permission: 'customers:update'
 
-      argument :integration_customer, Types::IntegrationCustomers::Input, required: false
+      argument :integration_customer, Types::IntegrationCustomers::Input, required: false, permission: 'customers:update'
+
+      # Customer settings
+      argument :invoice_grace_period, Integer, required: false, permissions: %w[customer_settings:update:grace_period customers:update]
+      argument :net_payment_term, Integer, required: false, permissions: %w[customer_settings:update:payment_terms customers:update]
+      argument :tax_codes, [String], required: false, permissions: %w[customer_settings:update:tax_rates customers:update]
 
       argument :billing_configuration, Types::Customers::BillingConfigurationInput, required: false
     end
