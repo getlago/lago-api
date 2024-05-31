c1ef5214ba5de105aba177f1f51344d28b631577
Lovro Colic <lovro.colic.lc@gmail.com>
2024-05-31 12:24:48 +0200

    add support for anrok create mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -120,6 +120,8 @@ module Types
     field :create_okta_integration, mutation: Mutations::Integrations::Okta::Create
     field :update_okta_integration, mutation: Mutations::Integrations::Okta::Update
 
+    field :create_anrok_integration, mutation: Mutations::Integrations::Anrok::Create
+
     field :okta_accept_invite, mutation: Mutations::Auth::Okta::AcceptInvite
     field :okta_authorize, mutation: Mutations::Auth::Okta::Authorize
     field :okta_login, mutation: Mutations::Auth::Okta::Login
---
+++ app/services/integrations/anrok/create_service.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Integrations
+  module Anrok
+    class CreateService < BaseService
+      def call(**args)
+        organization = Organization.find_by(id: args[:organization_id])
+
+        unless organization.premium_integrations.include?('anrok')
+          return result.not_allowed_failure!(code: 'premium_integration_missing')
+        end
+
+        integration = Integrations::AnrokIntegration.new(
+          organization:,
+          name: args[:name],
+          code: args[:code],
+          api_key: args[:api_key],
+        )
+
+        integration.save!
+
+        result.integration = integration
+        result
+      rescue ActiveRecord::RecordInvalid => e
+        result.record_validation_failure!(record: e.record)
+      end
+    end
+  end
+end
