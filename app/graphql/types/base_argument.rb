3b8d9816163161aa0b72afb96ce5d04d9ab2a6e5
Julien Bourdeau <julien@julienbourdeau.com>
2024-05-31 14:49:28 +0200

    multiple permissions for graphql arguments

--- app/graphql/types/base_argument.rb
+++ app/graphql/types/base_argument.rb
@@ -4,8 +4,13 @@ module Types
   class BaseArgument < GraphQL::Schema::Argument
     attr_reader :permissions
 
-    def initialize(*args, permission: nil, **kwargs, &block)
-      @permissions = [permission].compact
+    def initialize(*args, permission: nil, permissions: nil, **kwargs, &block)
+      @permissions = if permission
+        [permission].compact
+      elsif permissions
+        Array.wrap(permissions).compact
+      end
+
       super(*args, **kwargs, &block)
     end
   end
