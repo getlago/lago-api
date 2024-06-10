e6827995eb92e7f2c0f90ab8e3d72ef6a90f7783
Vincent Pochet <vincent@getlago.com>
2024-06-10 14:14:24 +0200

    fix(stripe): Enable auto retry for APIConnectionError on payment job

--- app/jobs/invoices/payments/stripe_create_job.rb
+++ app/jobs/invoices/payments/stripe_create_job.rb
@@ -8,6 +8,7 @@ module Invoices
       unique :until_executed
 
       retry_on Stripe::RateLimitError, wait: :exponentially_longer, attempts: 6
+      retry_on Stripe::APIConnectionError, wait: :exponentially_longer, attempts: 6
 
       def perform(invoice)
         result = Invoices::Payments::StripeService.new(invoice).create
