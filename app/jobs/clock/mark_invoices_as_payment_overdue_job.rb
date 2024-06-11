ca38d0a2c53980ecd3d1dc61a9c5adfb95b4f453
Romain Sempé <romain@getlago.com>
2024-06-11 14:58:36 +0200

    feat(dunning): Send webhook invoice.payment_overdue

--- app/jobs/clock/mark_invoices_as_payment_overdue_job.rb
+++ app/jobs/clock/mark_invoices_as_payment_overdue_job.rb
@@ -13,6 +13,7 @@ module Clock
         .where(payment_due_date: ...Time.current)
         .find_each do |invoice|
           invoice.update!(payment_overdue: true)
+          SendWebhookJob.perform_later('invoice.payment_overdue', invoice)
         end
     end
   end
