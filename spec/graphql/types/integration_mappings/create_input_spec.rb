1843a41e6d292e28745effd1e5e611c2dbee4c5a
LovroColic <lovro.colic.lc@gmail.com>
2024-06-11 16:38:24 +0200

    fix (integrations): enable optional code when creating mapping object (#2164)
    ## Context
    
    Currently Lago does not support tax integrations
    
    ## Description
    
    When creating Anrok mapping object `code` is not required so we need to
    change our generic gql mapping definitions

--- spec/graphql/types/integration_mappings/create_input_spec.rb
+++ spec/graphql/types/integration_mappings/create_input_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Types::IntegrationMappings::CreateInput do
   it { is_expected.to accept_argument(:integration_id).of_type('ID!') }
   it { is_expected.to accept_argument(:mappable_id).of_type('ID!') }
   it { is_expected.to accept_argument(:mappable_type).of_type('MappableTypeEnum!') }
-  it { is_expected.to accept_argument(:external_account_code).of_type('String!') }
+  it { is_expected.to accept_argument(:external_account_code).of_type('String') }
   it { is_expected.to accept_argument(:external_id).of_type('String!') }
   it { is_expected.to accept_argument(:external_name).of_type('String') }
 end
--- spec/graphql/types/integration_mappings/object_spec.rb
+++ spec/graphql/types/integration_mappings/object_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Types::IntegrationMappings::Object do
   it { is_expected.to have_field(:integration_id).of_type('ID!') }
   it { is_expected.to have_field(:mappable_id).of_type('ID!') }
   it { is_expected.to have_field(:mappable_type).of_type('MappableTypeEnum!') }
-  it { is_expected.to have_field(:external_account_code).of_type('String!') }
+  it { is_expected.to have_field(:external_account_code).of_type('String') }
   it { is_expected.to have_field(:external_id).of_type('String!') }
   it { is_expected.to have_field(:external_name).of_type('String') }
 end
