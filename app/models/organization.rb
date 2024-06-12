b52904511127f306df330f3fdf28c51afc60757b
Ivan Novosad <ivan@getlago.com>
2024-06-12 17:14:38 +0200

    feat(xero): Add xero to Organization::INTEGRATIONS and to IntegrationTypeEnum

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -47,7 +47,7 @@ class Organization < ApplicationRecord
     :per_organization
   ].freeze
 
-  INTEGRATIONS = %w[netsuite okta anrok].freeze
+  INTEGRATIONS = %w[netsuite okta anrok xero].freeze
 
   enum document_numbering: DOCUMENT_NUMBERINGS
 
