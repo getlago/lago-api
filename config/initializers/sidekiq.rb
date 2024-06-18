3198e2c7d91b4a1d9d4016e97fc83ea5cbe1852d
Jérémy Denquin <jdenquin@users.noreply.github.com>
2024-06-18 16:08:39 +0200

    misc(sidekiq): Add new monitoring system for k8s liveness probe (#2186)
    ## Description
    
    - `sidekiqmon` cause a lot of issues depending on Redis configuration
    - Uses a very simple approach to check Sidekiq to Redis networking

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'socket'
+
+LIVENESS_PORT = 8080
+
 redis_config = {
   url: ENV['REDIS_URL'],
   pool_timeout: 5,
@@ -23,7 +27,47 @@ Sidekiq.configure_server do |config|
   config.logger = Sidekiq::Logger.new($stdout)
   config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
   config[:max_retries] = 0
-  config[:dead_max_jobs] = ENV.fetch('LAGO_SIDEKIQ_MAX_DEAD_JOBS', 100_000).to_i
+  config[:dead_max_jobs] = ENV.fetch("LAGO_SIDEKIQ_MAX_DEAD_JOBS", 100_000).to_i
+  config.on(:startup) do
+    Sidekiq.logger.info "Starting liveness server on #{LIVENESS_PORT}"
+    Thread.start do
+      server = TCPServer.new("localhost", LIVENESS_PORT)
+      loop do
+        Thread.start(server.accept) do |socket|
+          request = socket.gets
+          sidekiq_response = ::Sidekiq.redis { |r| r.ping }
+
+          if sidekiq_response.eql?("PONG")
+            response = "Live!\n"
+            socket.print "HTTP/1.1 200 OK\r\n" \
+                       "Content-Type: text/plain\r\n" \
+                       "Content-Length: #{response.bytesize}\r\n" \
+                       "Connection: close\r\n"
+          else
+            response = "Sidekiq is not ready: Sidekiq.redis.ping returned #{request.inspect} instead of PONG\n"
+            Sidekiq.logger.error(response)
+            socket.print "HTTP/1.1 404 OK\r\n" \
+                       "Content-Type: text/plain\r\n" \
+                       "Content-Length: #{response.bytesize}\r\n" \
+                       "Connection: close\r\n"
+          end
+          socket.print "\r\n"
+          socket.print response
+          socket.close
+        rescue
+          response = "Sidekiq is not ready\n"
+          Sidekiq.logger.error(response)
+          socket.print "HTTP/1.1 404 OK\r\n" \
+                       "Content-Type: text/plain\r\n" \
+                       "Content-Length: #{response.bytesize}\r\n" \
+                       "Connection: close\r\n"
+          socket.print "\r\n"
+          socket.print response
+          socket.close
+        end
+      end
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
