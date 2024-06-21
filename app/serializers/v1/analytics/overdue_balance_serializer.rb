b0b37d9399664bd42b5da3396997213e52bbf93c
Romain Sempé <romain@getlago.com>
2024-06-21 14:58:15 +0200

    feat(dunning): Update overdue balance to have lago_invoice_ids

--- app/serializers/v1/analytics/overdue_balance_serializer.rb
+++ app/serializers/v1/analytics/overdue_balance_serializer.rb
@@ -8,7 +8,7 @@ module V1
           month: model["month"],
           amount_cents: model["amount_cents"],
           currency: model["currency"],
-          invoice_ids: JSON.parse(model["invoice_ids"])
+          lago_invoice_ids: JSON.parse(model["invoice_ids"])
         }
       end
     end
