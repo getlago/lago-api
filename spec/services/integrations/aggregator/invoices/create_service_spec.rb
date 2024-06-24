db80deb81d485481eebf896891535eb2bfcea459
Ivan Novosad <ivan@getlago.com>
2024-06-24 11:57:41 +0200

    fix(integrations): Fix creating an invoice when there's http error

--- spec/services/integrations/aggregator/invoices/create_service_spec.rb
+++ spec/services/integrations/aggregator/invoices/create_service_spec.rb
@@ -354,6 +354,10 @@ RSpec.describe Integrations::Aggregator::Invoices::CreateService do
           end.not_to raise_error(http_error)
         end
 
+        it 'returns result' do
+          expect(service_call).to be_a(BaseService::Result)
+        end
+
         it 'enqueues a SendWebhookJob' do
           expect { service_call }.to have_enqueued_job(SendWebhookJob)
         end
