2a48cc21c8a85d178638121b1533308ff9d6a252
Julien Bourdeau <julien@julienbourdeau.com>
2024-06-24 15:18:09 +0200

    revove `pending` invoice

--- app/jobs/bill_non_invoiceable_fees_job.rb
+++ app/jobs/bill_non_invoiceable_fees_job.rb
@@ -6,7 +6,7 @@ class BillNonInvoiceableFeesJob < ApplicationJob
   retry_on Sequenced::SequenceError, ActiveJob::DeserializationError
 
   def perform(subscriptions, billing_at)
-    result = Invoices::RegroupFeesService.call(subscriptions:, billing_at:)
+    result = Invoices::AdvanceChargesService.call(subscriptions:, billing_at:)
     result.raise_if_error!
   end
 end
