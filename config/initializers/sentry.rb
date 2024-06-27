83bc694f3ffedc68551bd9a0ccdf30f719923522
Vincent Pochet <vincent@getlago.com>
2024-06-27 11:01:19 +0200

    hotfix(sentry): Use fallback for version number

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 if ENV['SENTRY_DSN']
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.release = LAGO_VERSION.number
+    config.release = LagoUtils::Version.call(default: Rails.env).number
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.traces_sample_rate = 0
     config.traces_sample_rate = ENV['SENTRY_TRACES_SAMPLE_RATE'].to_f || 0
