e42183609ac5501890c12baa2ed964206c84599e
Julien Bourdeau <julien@julienbourdeau.com>
2024-06-27 11:48:15 +0200

    chore(factories): Don't use Faker names for random codes

--- spec/factories/add_ons.rb
+++ spec/factories/add_ons.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     organization
     name { Faker::Name.name }
     invoice_display_name { Faker::Fantasy::Tolkien.location }
-    code { Faker::Name.first_name }
+    code { Faker::Alphanumeric.alphanumeric(number: 10) }
     description { 'test description' }
     amount_cents { 200 }
     amount_currency { 'EUR' }
