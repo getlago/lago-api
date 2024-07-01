09d1d574d9137445827976c21c8a1ab47307390a
Vincent Pochet <vincent@getlago.com>
2024-07-01 17:46:04 +0200

    misc(event): Ensure updating charge filter is touching the related charge

--- db/migrate/20240701083355_create_billable_metrics_grouped_charges.rb
+++ db/migrate/20240701083355_create_billable_metrics_grouped_charges.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CreateBillableMetricsGroupedCharges < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
   def change
     create_view :billable_metrics_grouped_charges
   end
