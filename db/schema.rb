9cf1706525ef48ae19f626d208d29387d9db5090
Julien Bourdeau <julien@julienbourdeau.com>
2024-07-04 11:33:55 +0200

    dump schemas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_083355) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_02_081109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -225,6 +225,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_01_083355) do
     t.boolean "invoiceable", default: true, null: false
     t.boolean "prorated", default: false, null: false
     t.string "invoice_display_name"
+    t.integer "regroup_paid_fees"
     t.index ["billable_metric_id"], name: "index_charges_on_billable_metric_id"
     t.index ["deleted_at"], name: "index_charges_on_deleted_at"
     t.index ["plan_id"], name: "index_charges_on_plan_id"
