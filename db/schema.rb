d0aa9d516792e06962c1da1433e9c68f2846b636
Julien Bourdeau <julien@julienbourdeau.com>
2024-07-05 12:31:34 +0200

    feat(papertrail): Add Lago Version to papertrail metadata (#2240)
    ## Description
    
    Every time a change is made to a model, we store the changes in a
    `versions` table managed by the [papertrail
    gem](https://github.com/paper-trail-gem/paper_trail).
    
    This PR adds the version of Lago to help debugging, especially self
    hosted versions.
    
    Only possible because of [this
    PR](https://github.com/getlago/lago-api/pull/2204).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_02_081109) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_03_061352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -999,6 +999,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_081109) do
     t.jsonb "object"
     t.jsonb "object_changes"
     t.datetime "created_at"
+    t.string "lago_version"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
