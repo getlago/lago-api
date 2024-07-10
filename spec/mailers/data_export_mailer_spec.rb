07d82131b187b38d4cd8c86abe2fc0e1d7eba634
Ancor Cruz <hello@ancorcruz.com>
2024-07-10 11:23:42 +0100

    Extend DataExport mailer to handle invoice fees exports
    also, enhance mailer specs

--- spec/mailers/data_export_mailer_spec.rb
+++ spec/mailers/data_export_mailer_spec.rb
@@ -9,9 +9,27 @@ RSpec.describe DataExportMailer, type: :mailer do
 
   describe '#completed' do
     let(:mailer) { data_export_mailer.with(data_export:).completed }
+    let(:file_url) { "https://api.lago.dev/rails/active_storage/blobs/redirect/eyJf" }
+
+    before do
+      allow(data_export).to receive(:file_url).and_return(file_url)
+    end
 
     specify do
       expect(mailer.to).to eq([data_export.user.email])
+      expect(mailer.subject).to eq("Your Lago invoices export in ready!")
+      expect(mailer.body.encoded).to match("Your invoices export is ready!")
+      expect(mailer.body.encoded).to match("will be available for 7 days")
+      expect(mailer.body.encoded).to match(data_export.file_url)
+    end
+
+    context "when the resource type is invoice_fees" do
+      let(:data_export) { create(:data_export, :completed, resource_type: 'invoice_fees') }
+
+      specify do
+        expect(mailer.subject).to eq("Your Lago invoice fees export in ready!")
+        expect(mailer.body.encoded).to match("Your invoice fees export is ready!")
+      end
     end
 
     context 'when data export is expired' do
