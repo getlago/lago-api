a79ea3955e8eb1e76e7a2e517aaf247e717eb999
brunomiguelpinto <darkymiguel@gmail.com>
2024-07-10 18:08:14 +0100

    fix: add disable_ddl_transaction (#2274)

--- db/migrate/20240611074215_add_payment_overdue_to_invoices.rb
+++ db/migrate/20240611074215_add_payment_overdue_to_invoices.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class AddPaymentOverdueToInvoices < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_column :invoices, :payment_overdue, :boolean, default: false
-    add_index :invoices, :payment_overdue
+    add_index :invoices, :payment_overdue, algorithm: :concurrently
 
     reversible do |dir|
       dir.up do
         # Set existing invoices as payment_overdue
         execute <<-SQL
           UPDATE invoices
-            SET payment_overdue = true
-            WHERE status = 1 -- finalized
+          SET payment_overdue = true
+          WHERE status = 1 -- finalized
             AND payment_status != 1 -- not succeeded
             AND payment_dispute_lost_at IS NULL -- not lost dispute
             AND payment_due_date < NOW(); -- due date is in the past
