4870ebef8fceb5c680f4f1a3797b1ecc44d45bb5
brunomiguelpinto <darkymiguel@gmail.com>
2024-07-10 15:56:22 +0100

    fix: disable transaction on migration (#2273)

--- db/migrate/20240701184757_add_deleted_at_to_fees.rb
+++ db/migrate/20240701184757_add_deleted_at_to_fees.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class AddDeletedAtToFees < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
   def change
     add_column :fees, :deleted_at, :datetime
-    add_index :fees, :deleted_at
+    add_index :fees, :deleted_at, algorithm: :concurrently
   end
 end
