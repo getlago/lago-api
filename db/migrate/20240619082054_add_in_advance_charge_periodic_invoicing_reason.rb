cd0addc830313759cb91ab95e660cdd111f54374
Miguel Pinto <darkymiguel@gmail.com>
2024-07-10 16:01:45 +0100

    fix: add disable_ddl_transaction

--- db/migrate/20240619082054_add_in_advance_charge_periodic_invoicing_reason.rb
+++ db/migrate/20240619082054_add_in_advance_charge_periodic_invoicing_reason.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AddInAdvanceChargePeriodicInvoicingReason < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
   def up
     execute <<-SQL
       ALTER TYPE subscription_invoicing_reason ADD VALUE IF NOT EXISTS 'in_advance_charge_periodic';
