5aadcff69b4bebcba10374ba5b26d885bc6319c9
Ancor Cruz <hello@ancorcruz.com>
2024-07-11 07:42:09 +0100

    Namespace DataExports graphQL enum types

--- spec/graphql/types/data_exports/object_spec.rb
+++ spec/graphql/types/data_exports/object_spec.rb
@@ -6,5 +6,5 @@ RSpec.describe Types::DataExports::Object do
   subject { described_class }
 
   it { is_expected.to have_field(:id).of_type('ID!') }
-  it { is_expected.to have_field(:status).of_type('StatusEnum!') }
+  it { is_expected.to have_field(:status).of_type('DataExportStatusEnum!') }
 end
