e14c3e34909578b558442fe8aa8738dea6aa4a0b
Vincent Pochet <vincent@getlago.com>
2024-07-11 16:22:19 +0200

    misc: Add LAGO_CLICKHOUSE_MIGRATIONS_ENABLED env (#2281)
    ## Description
    
    This PR introduce a new `LAGO_CLICKHOUSE_MIGRATIONS_ENABLED` environment
    variable to enable or disable the Clickhouse migration. This variable
    completes the existing `LAGO_CLICKHOUSE_ENABLED` that enable the
    Clickhouse event stores in API
    
    It should fix some issue we had recently in deploy on US/EU clusters
    with migration performed at startup

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,10 +7,10 @@ fi
 
 rm -f ./tmp/pids/server.pid
 
-if [ -v LAGO_CLICKHOUSE_ENABLED ] && [ "$LAGO_CLICKHOUSE_ENABLED" == "true" ]
+if [ -v LAGO_CLICKHOUSE_MIGRATIONS_ENABLED ] && [ "$LAGO_CLICKHOUSE_MIGRATIONS_ENABLED" == "true" ]
 then
   bundle exec rails db:migrate:primary
-  bundle exec rake db:migrate:clickhouse
+  bundle exec rails db:migrate:clickhouse
 else
   bundle exec rails db:migrate
 fi
