6bbe1980a9f2fa1f086937737d4a3d50a17d6983
Miguel Pinto <darkymiguel@gmail.com>
2024-07-12 11:21:20 +0100

    feat: update the migration

--- db/migrate/20240712090133_add_index_to_fees.rb
+++ db/migrate/20240712090133_add_index_to_fees.rb
@@ -5,7 +5,6 @@ class AddIndexToFees < ActiveRecord::Migration[7.1]
 
   def change
     add_index :fees, [:charge_id, :invoice_id],
-      name: 'index_fees_on_charge_id_and_invoice_id',
       where: 'deleted_at IS NULL',
       algorithm: :concurrently,
       if_not_exists: true
