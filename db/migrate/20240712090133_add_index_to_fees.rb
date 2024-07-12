c844bf9f9bcccb52b188af2dd2dcf558f377b76c
Miguel Pinto <darkymiguel@gmail.com>
2024-07-12 10:23:37 +0100

    feat: update the schema

--- db/migrate/20240712090133_add_index_to_fees.rb
+++ db/migrate/20240712090133_add_index_to_fees.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 class AddIndexToFees < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    add_index :fees, [:charge_id, :invoice_id],
-              name: 'index_fees_on_charge_id_and_invoice_id',
-              where: 'deleted_at IS NULL'
+    unless index_exists?(:fees, [:charge_id, :invoice_id], name: 'index_fees_on_charge_id_and_invoice_id', where: 'deleted_at IS NULL')
+      add_index :fees, [:charge_id, :invoice_id],
+        name: 'index_fees_on_charge_id_and_invoice_id',
+        where: 'deleted_at IS NULL'
+    end
   end
 end
