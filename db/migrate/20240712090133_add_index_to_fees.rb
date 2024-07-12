807b48a675de08a9ca969230ec625358687a035e
Miguel Pinto <darkymiguel@gmail.com>
2024-07-12 11:13:24 +0100

    feat: update the migration

--- db/migrate/20240712090133_add_index_to_fees.rb
+++ db/migrate/20240712090133_add_index_to_fees.rb
@@ -4,10 +4,10 @@ class AddIndexToFees < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    unless index_exists?(:fees, [:charge_id, :invoice_id], name: 'index_fees_on_charge_id_and_invoice_id', where: 'deleted_at IS NULL')
-      add_index :fees, [:charge_id, :invoice_id],
-        name: 'index_fees_on_charge_id_and_invoice_id',
-        where: 'deleted_at IS NULL'
-    end
+    add_index :fees, [:charge_id, :invoice_id],
+      name: 'index_fees_on_charge_id_and_invoice_id',
+      where: 'deleted_at IS NULL',
+      algorithm: :concurrently,
+      if_not_exists: true
   end
 end
