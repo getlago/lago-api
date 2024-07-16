f428d9909234a6ab9ff8563745887b43f0b937d9
Jeremy Denquin <jdenquin@gmail.com>
2024-07-16 17:43:32 +0200

    add migration

---
+++ db/migrate/20240716153753_add_index_on_fees_pay_in_advance_event_transaction_id.rb
@@ -0,0 +1,10 @@
+class AddIndexOnFeesPayInAdvanceEventTransactionId < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :fees, :pay_in_advance_event_transaction_id,
+      where: 'deleted_at IS NULL',
+      algorithm: :concurrently,
+      if_not_exists: true
+  end
+end
