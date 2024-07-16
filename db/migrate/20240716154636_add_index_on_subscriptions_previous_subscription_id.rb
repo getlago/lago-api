86a975f3a244b6116a859a4d6d722e83192b47b0
Jeremy Denquin <jdenquin@gmail.com>
2024-07-16 17:51:27 +0200

    fix(subscriptions): Add missing index on previous_subscription_id and status

---
+++ db/migrate/20240716154636_add_index_on_subscriptions_previous_subscription_id.rb
@@ -0,0 +1,9 @@
+class AddIndexOnSubscriptionsPreviousSubscriptionId < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :subscriptions, [:previous_subscription_id, :status],
+      algorithm: :concurrently,
+      if_not_exists: true
+  end
+end
