bfac55b4e0d20d8e2101e6c25dfcaec4f02b4677
Ancor Cruz <hello@ancorcruz.com>
2024-07-19 23:08:05 +0100

    Migrate IntegrationItemsQuery object to new design

--- spec/queries/integration_items_query_spec.rb
+++ spec/queries/integration_items_query_spec.rb
@@ -3,9 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe IntegrationItemsQuery, type: :query do
-  subject(:integration_items_query) { described_class.new(organization:) }
+  subject(:result) do
+    described_class.call(organization:, search_term:, pagination:, filters:)
+  end
 
-  let(:query_filters) { {} }
+  let(:pagination) { nil }
+  let(:search_term) { nil }
+  let(:filters) { {} }
   let(:membership) { create(:membership) }
   let(:organization) { membership.organization }
 
@@ -18,12 +22,6 @@ RSpec.describe IntegrationItemsQuery, type: :query do
   let(:integration_item_third) { create(:integration_item, integration: integration_third) }
   let(:integration_item_fourth) { create(:integration_item, external_name: 'Findme', integration:) }
 
-  let(:service_call) do
-    integration_items_query.call(integration_id: integration.id, search_term:, page: 1, limit: 10, filters:)
-  end
-
-  let(:search_term) { nil }
-
   before do
     integration_item_first
     integration_item_second
@@ -31,16 +29,43 @@ RSpec.describe IntegrationItemsQuery, type: :query do
     integration_item_fourth
   end
 
-  context 'when filters are empty' do
-    let(:filters) { {} }
+  it 'returns all integration items of an organization' do
+    returned_ids = result.integration_items.pluck(:id)
+
+    aggregate_failures do
+      expect(result).to be_success
+      expect(returned_ids.count).to eq(3)
+      expect(returned_ids).to include(integration_item_first.id)
+      expect(returned_ids).to include(integration_item_second.id)
+      expect(returned_ids).not_to include(integration_item_third.id)
+      expect(returned_ids).to include(integration_item_fourth.id)
+    end
+  end
+
+  context 'with pagination' do
+    let(:pagination) { {page: 2, limit: 2} }
+
+    it 'applies the pagination' do
+      aggregate_failures do
+        expect(result).to be_success
+        expect(result.integration_items.count).to eq(1)
+        expect(result.integration_items.current_page).to eq(2)
+        expect(result.integration_items.prev_page).to eq(1)
+        expect(result.integration_items.next_page).to be_nil
+        expect(result.integration_items.total_pages).to eq(2)
+        expect(result.integration_items.total_count).to eq(3)
+      end
+    end
+  end
+
+  context 'when filtering by integration_id' do
+    let(:filters) { {integration_id: integration.id} }
 
     it 'returns all integration items of an integration' do
-      result = service_call
-
       returned_ids = result.integration_items.pluck(:id)
 
       aggregate_failures do
-        expect(result.integration_items.count).to eq(2)
+        expect(returned_ids.count).to eq(2)
         expect(returned_ids).to include(integration_item_first.id)
         expect(returned_ids).not_to include(integration_item_second.id)
         expect(returned_ids).not_to include(integration_item_third.id)
@@ -53,12 +78,10 @@ RSpec.describe IntegrationItemsQuery, type: :query do
     let(:filters) { {item_type: 'tax'} }
 
     it 'returns one integration item' do
-      result = service_call
-
       returned_ids = result.integration_items.pluck(:id)
 
       aggregate_failures do
-        expect(result.integration_items.count).to eq(1)
+        expect(returned_ids.count).to eq(1)
         expect(returned_ids).to include(integration_item_first.id)
         expect(returned_ids).not_to include(integration_item_second.id)
         expect(returned_ids).not_to include(integration_item_third.id)
@@ -69,15 +92,12 @@ RSpec.describe IntegrationItemsQuery, type: :query do
 
   context 'when searching by name' do
     let(:search_term) { 'Findme' }
-    let(:filters) { {} }
 
     it 'returns one integration item' do
-      result = service_call
-
       returned_ids = result.integration_items.pluck(:id)
 
       aggregate_failures do
-        expect(result.integration_items.count).to eq(1)
+        expect(returned_ids.count).to eq(1)
         expect(returned_ids).not_to include(integration_item_first.id)
         expect(returned_ids).not_to include(integration_item_second.id)
         expect(returned_ids).not_to include(integration_item_third.id)
