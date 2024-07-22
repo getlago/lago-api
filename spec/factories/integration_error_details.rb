48d7d17c4ec31ebc22507a3f43042ae1121e56de
Anna Velentsevich <annavelentsevich@Annas-MBP-3.station>
2024-07-22 16:06:38 +0200

    feat: rename integration_error_detail model to error_detail

--- spec/factories/integration_error_details.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :integration_error_detail do
-    association :owner, factory: %i[invoice].sample
-    association :error_producer, factory: :anrok_integration
-  end
-end
