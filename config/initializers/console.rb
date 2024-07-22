82e6b54045ee1e93700012a6812d911cbf1f3820
Julien Bourdeau <julien@julienbourdeau.com>
2024-07-22 18:18:00 +0200

    feat(console): Hard delete invoice in console

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -10,4 +10,23 @@ module Rails::ConsoleMethods
       raise "Don't know how to resolve this ¯\\_(ツ)_/¯. Please provide a valid email or Global ID."
     end
   end
+
+  def delete_invoice(id)
+    invoice = Invoice.find(id)
+    puts "Retrieved invoice #{invoice.id} from organization #{invoice.organization.name}"
+
+    puts "Deleting invoice #{invoice.id}..."
+    ActiveRecord::Base.transaction do
+      invoice.taxes.destroy_all
+      invoice.fees.destroy_all
+      invoice.destroy
+    end
+
+    begin
+      invoice.reload
+      puts "Invoice #{id} could not be deleted. Please try again."
+    rescue ActiveRecord::RecordNotFound
+      puts "Invoice #{id} has been successfully deleted."
+    end
+  end
 end
