a15c3d4f0aa67264b2e426440b2b79e4c5b2c984
Anna Velentsevich <annavelentsevich@Annas-MBP-3.station>
2024-07-22 09:31:47 +0200

    feat: rename integration relation on integration_error_details to error_producer

--- app/models/integration_error_detail.rb
+++ app/models/integration_error_detail.rb
@@ -4,6 +4,6 @@ class IntegrationErrorDetail < ApplicationRecord
   include Discard::Model
   self.discard_column = :deleted_at
 
-  belongs_to :integration, class_name: 'Integrations::BaseIntegration'
+  belongs_to :error_producer, polymorphic: true
   belongs_to :owner, polymorphic: true
 end
