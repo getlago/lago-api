02e0cf07994f0aafdb995845dac2d41f93ecb00a
Anna Velentsevich <annavelentsevich@Annas-MBP-3.station>
2024-07-22 11:14:59 +0200

    feat: better definition of integration_error_details factory

--- spec/models/integration_error_detail_spec.rb
+++ spec/models/integration_error_detail_spec.rb
@@ -3,6 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe IntegrationErrorDetail, type: :model do
-  it { is_expected.to belong_to(:integration) }
+  it { is_expected.to belong_to(:error_producer) }
   it { is_expected.to belong_to(:owner) }
 end
