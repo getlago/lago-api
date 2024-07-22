fca6fd72b6df3c412f2f50cf9115b4047b76b247
Anna Velentsevich <annavelentsevich@Annas-MBP-3.station>
2024-07-22 09:07:13 +0200

    feat: found better name for the abstraction

--- db/migrate/20240718105718_create_integration_error_details.rb
+++ db/migrate/20240718105718_create_integration_error_details.rb
@@ -4,7 +4,7 @@ class CreateIntegrationErrorDetails < ActiveRecord::Migration[7.1]
   def change
     create_table :integration_error_details, id: :uuid do |t|
       t.references :owner, type: :uuid, polymorphic: true, null: false, index: true
-      t.references :integration, type: :uuid, polymorphic: true, index: true, null: false
+      t.references :error_producer, type: :uuid, polymorphic: true, index: true, null: false
       t.jsonb :details, null: false, default: {}
       t.datetime :deleted_at, index: true
 
