c0149b30188c345936e42ccbf95f85d8aa97d214
LovroColic <lovro.colic.lc@gmail.com>
2024-07-22 15:43:34 +0200

    feat (tax-integration): add mutation for retrying failed invoice (#2317)
    ## Context
    
    Currently tax integration is being implemented
    
    ## Description
    
    This PR adds mutation that allows retrying failed invoice

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -85,6 +85,7 @@ module Types
     field :lose_invoice_dispute, mutation: Mutations::Invoices::LoseDispute
     field :refresh_invoice, mutation: Mutations::Invoices::Refresh
     field :retry_all_invoice_payments, mutation: Mutations::Invoices::RetryAllPayments
+    field :retry_invoice, mutation: Mutations::Invoices::Retry
     field :retry_invoice_payment, mutation: Mutations::Invoices::RetryPayment
     field :update_invoice, mutation: Mutations::Invoices::Update
     field :void_invoice, mutation: Mutations::Invoices::Void
