9d4de66b539adb2905bb307a4d6551c7ee9632b0
Anna Velentsevich <annvelents@gmail.com>
2024-07-23 18:07:35 +0200

    feat: fix linter

--- app/models/integrations/anrok_integration.rb
+++ app/models/integrations/anrok_integration.rb
@@ -3,8 +3,8 @@
 module Integrations
   class AnrokIntegration < BaseIntegration
     has_many :error_details, -> { where({error_details: {error_code: 'tax_error'}}) },
-             primary_key: :organization_id,
-             foreign_key: :organization_id
+      primary_key: :organization_id,
+      foreign_key: :organization_id
 
     validates :connection_id, :api_key, presence: true
 
