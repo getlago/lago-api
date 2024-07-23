d7e1ac30e28353adb1423a3a19010aa0f5ddf74c
Julien Bourdeau <julien@julienbourdeau.com>
2024-07-23 15:45:16 +0200

    refactor(wallet): change ApplyPaidCreditsService argument

--- spec/services/wallets/apply_paid_credits_service_spec.rb
+++ spec/services/wallets/apply_paid_credits_service_spec.rb
@@ -6,40 +6,25 @@ RSpec.describe Wallets::ApplyPaidCreditsService, type: :service do
   subject(:service) { described_class.new }
 
   describe '.call' do
-    let(:invoice) { create(:invoice, customer:, organization: customer.organization) }
-    let(:customer) { create(:customer) }
-    let(:subscription) { create(:subscription, customer:) }
-    let(:wallet) { create(:wallet, customer:, balance_cents: 1000, credits_balance: 10.0) }
+    let(:wallet) { create(:wallet, balance_cents: 1000, credits_balance: 10.0) }
     let(:wallet_transaction) do
       create(:wallet_transaction, wallet:, amount: 15.0, credit_amount: 15.0, status: 'pending')
-    end
-    let(:fee) do
-      create(
-        :fee,
-        fee_type: 'credit',
-        invoiceable_type: 'WalletTransaction',
-        invoiceable_id: wallet_transaction.id,
-        invoice:
-      )
     end
 
     before do
       wallet_transaction
-      fee
-      subscription
-      invoice.update(invoice_type: 'credit')
     end
 
     it 'updates wallet balance' do
-      service.call(invoice)
+      service.call(wallet_transaction)
 
       expect(wallet.reload.balance_cents).to eq 2500
     end
 
     it 'settles the wallet transaction' do
-      service.call(invoice)
+      result = service.call(wallet_transaction)
 
-      expect(wallet_transaction.reload.status).to eq('settled')
+      expect(result.wallet_transaction.status).to eq('settled')
     end
   end
 end
