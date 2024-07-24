dc129f28f137e1c4051d6dba81c827a702cde93f
Anna Velentsevich <annvelents@gmail.com>
2024-07-24 13:30:19 +0200

    unite two migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_085326) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_150221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
