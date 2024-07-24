3700d598158e4ecf8478b336e584b60dc8bca8f7
Anna Velentsevich <annvelents@gmail.com>
2024-07-24 14:47:43 +0200

    remove renaming of the class

--- app/graphql/types/error_details/error_codes_enum.rb
+++ app/graphql/types/error_details/error_codes_enum.rb
@@ -3,7 +3,6 @@
 module Types
   module ErrorDetails
     class ErrorCodesEnum < Types::BaseEnum
-      graphql_name 'ErrorCodesEnum'
 
       ErrorDetail::ERROR_CODES.each do |code|
         value code
