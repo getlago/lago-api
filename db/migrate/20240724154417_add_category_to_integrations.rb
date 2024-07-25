5893e435acb8d3e63c66edebece47e750156814f
Anna Velentsevich <annvelents@gmail.com>
2024-07-25 11:05:36 +0200

    add migration to add category to integrations

---
+++ db/migrate/20240724154417_add_category_to_integrations.rb
@@ -0,0 +1,36 @@
+class AddCategoryToIntegrations < ActiveRecord::Migration[7.1]
+  module Integrations
+    class BaseIntegration < ApplicationRecord
+      self.table_name = 'integrations'
+      INTEGRATION_CATEGORIES = %w[system accounting tax_provider]
+      enum category: INTEGRATION_CATEGORIES
+    end
+
+    class AnrokIntegration < BaseIntegration
+    end
+
+    class NetsuiteIntegration < BaseIntegration
+    end
+
+    class XeroIntegration < BaseIntegration
+    end
+
+    class OktaIntegration < BaseIntegration
+    end
+  end
+
+  def up
+    add_column :integrations, :category, :integer
+    add_index :integrations, :category
+
+    Integrations::AnrokIntegration.update_all(category: 'tax_provider')
+    Integrations::NetsuiteIntegration.update_all(category: 'accounting')
+    Integrations::XeroIntegration.update_all(category: 'accounting')
+    Integrations::OktaIntegration.update_all(category: 'system')
+  end
+
+  def down
+    remove_index :integrations, :category
+    remove_column :integrations, :category
+  end
+end
