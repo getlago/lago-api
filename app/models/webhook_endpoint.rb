423d94913a05c129bd18aea9f2b66b61f7b1677b
Ancor Cruz <hello@ancorcruz.com>
2024-07-25 19:36:08 +0100

    Migrate WebhookEndpointsQuery object to new design
    Add WebhookEndpointsQuery tests as this class was not tested,
    and fix search by search terms as it was not working,
    also drop legacy `ids` filter

--- app/models/webhook_endpoint.rb
+++ app/models/webhook_endpoint.rb
@@ -17,6 +17,10 @@ class WebhookEndpoint < ApplicationRecord
 
   enum signature_algo: SIGNATURE_ALGOS
 
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[webhook_url]
+  end
+
   private
 
   def max_webhook_endpoints
