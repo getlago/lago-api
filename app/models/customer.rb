3327258c60cd6c0f7d7b9138b2c28b68e0254a3f
Anna Velentsevich <annvelents@gmail.com>
2024-07-29 09:53:40 +0200

    fix linter error

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -71,7 +71,7 @@ class Customer < ApplicationRecord
   validates :payment_provider, inclusion: {in: PAYMENT_PROVIDERS}, allow_nil: true
   validates :timezone, timezone: true, allow_nil: true
 
-  alias :provider_customer :payment_provider_customer
+  alias_method :provider_customer, :payment_provider_customer
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[id name external_id email]
