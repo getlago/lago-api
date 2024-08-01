5e59b9269e891b78f5aa35c96c1f30d50ab4d1e7
Toon Willems <m@toonwillems.be>
2024-08-01 17:21:08 +0200

    Setup more aggressive autovacuum_vacuum_scale_factor setting for events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_154334) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_142242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
