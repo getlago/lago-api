86cf55103f2b46a2effe868e83f6f45a652dcaa4
Ancor Cruz <hello@ancorcruz.com>
2024-08-01 18:12:23 +0100

    fix(export invoices): Re arrange CSV column order, add new columns and fix row order by issuing date (#2348)
    ## Roadmap Task
    
    👉
    https://getlago.canny.io/feature-requests/p/ability-to-export-data-from-the-user-interface
    
    ## Context
    
    Customers need some extra data in the invoices export we did not
    anticipate.
    
    ## Description
    
    - Add the `fees_amount_cents` column in the CSV export
    - Fix the invoice header typo (invoice instead of `invoince`)
    - Add a `customer_name` column, after the `customer_external_id` name
    - Invoices list must be sorted by `issuing_date DESC` (newest invoice at
    the top)
    - Adapt the order of columns (in the exact order). This list of columns
    must start after the file_url one:
    1. `fees_amount_cents`
    2. `coupons_amount_cents`
    3. `taxes_amount_cents`
    4. `credit_notes_amount_cents`
    5. `prepaid_credit_amount_cents`
    6. `total_amount_cents`
    
    In order to achieve the CSV row order by `issuing_date DESC` we can not
    use rails `find_each` or `find_in_batches` as this batching solution
    limits the ordering by the records primary key (asc or desc). We will
    use `each` over the whole result, if workers are impacted with
    performance or memory usage we will have to implement pagination through
    the query object capabilities.

--- app/services/data_exports/csv/invoices.rb
+++ app/services/data_exports/csv/invoices.rb
@@ -17,7 +17,7 @@ module DataExports
         ::CSV.generate(headers: true) do |csv|
           csv << headers
 
-          query.invoices.find_each do |invoice|
+          query.invoices.each do |invoice|
             serialized_invoice = serializer_klass
               .new(invoice, includes: %i[customer])
               .serialize
@@ -28,19 +28,21 @@ module DataExports
               serialized_invoice[:issuing_date],
               serialized_invoice[:customer][:lago_id],
               serialized_invoice[:customer][:external_id],
+              serialized_invoice[:customer][:name],
               serialized_invoice[:customer][:country],
               serialized_invoice[:customer][:tax_identification_number],
               serialized_invoice[:number],
-              serialized_invoice[:total_amount_cents],
-              serialized_invoice[:currency],
               serialized_invoice[:invoice_type],
               serialized_invoice[:payment_status],
               serialized_invoice[:status],
               serialized_invoice[:file_url],
+              serialized_invoice[:currency],
+              serialized_invoice[:fees_amount_cents],
+              serialized_invoice[:coupons_amount_cents],
               serialized_invoice[:taxes_amount_cents],
               serialized_invoice[:credit_notes_amount_cents],
               serialized_invoice[:prepaid_credit_amount_cents],
-              serialized_invoice[:coupons_amount_cents],
+              serialized_invoice[:total_amount_cents],
               serialized_invoice[:payment_due_date],
               serialized_invoice[:payment_dispute_lost_at],
               serialized_invoice[:payment_overdue]
@@ -62,19 +64,21 @@ module DataExports
           issuing_date
           customer_lago_id
           customer_external_id
+          customer_name
           customer_country
           customer_tax_identification_number
-          invoince_number
-          total_amount_cents
-          currency
+          invoice_number
           invoice_type
           payment_status
           status
           file_url
+          currency
+          fees_amount_cents
+          coupons_amount_cents
           taxes_amount_cents
           credit_notes_amount_cents
           prepaid_credit_amount_cents
-          coupons_amount_cents
+          total_amount_cents
           payment_due_date
           payment_dispute_lost_at
           payment_overdue
@@ -86,7 +90,7 @@ module DataExports
         filters = resource_query.except("search_term")
 
         InvoicesQuery.call(
-          organization: organization,
+          organization:,
           pagination: nil,
           search_term:,
           filters:
