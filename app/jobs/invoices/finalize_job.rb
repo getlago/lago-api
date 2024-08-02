599194c81486faaf63fe098d988cd1d69f349728
Julien Bourdeau <julien@julienbourdeau.com>
2024-08-02 12:40:21 +0200

    feat(billing): Retry finalize jobs on Sequence error

--- app/jobs/invoices/finalize_job.rb
+++ app/jobs/invoices/finalize_job.rb
@@ -4,6 +4,8 @@ module Invoices
   class FinalizeJob < ApplicationJob
     queue_as 'invoices'
 
+    retry_on Sequenced::SequenceError, wait: :polynomially_longer
+
     def perform(invoice)
       Invoices::RefreshDraftAndFinalizeService.call(invoice:)
     end
