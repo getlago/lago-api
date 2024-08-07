168f7ba06d4645ba764493ceddaf8ca3ce65b1da
Jeremy Denquin <jdenquin@gmail.com>
2024-08-07 16:16:17 +0200

    add spec for lib

--- spec/lib/lago_eu_vat/rate_spec.rb
+++ spec/lib/lago_eu_vat/rate_spec.rb
@@ -15,10 +15,13 @@ RSpec.describe LagoEuVat::Rate do
 
   describe '.country_rate' do
     it 'returns all applicable rates for a country' do
-      fr_rates = rates.country_rates(country_code: 'FR')
+      fr_taxes = rates.country_rates(country_code: 'FR')
+      fr_rates = fr_taxes[:rates]
+      fr_exceptions = fr_taxes[:exceptions]
 
       aggregate_failures do
         expect(fr_rates['standard']).to eq(20)
+        expect(fr_exceptions.count).to eq(5)
       end
     end
   end
