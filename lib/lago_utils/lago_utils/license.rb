f79506a63600cb1afdac20d26b968fde5d0791fd
Abdussamad Bello <abdussamadbello4real@hotmail.co.uk>
2024-08-09 00:24:38 +0100

    feat(lago_utils): prem

--- lib/lago_utils/lago_utils/license.rb
+++ lib/lago_utils/lago_utils/license.rb
@@ -4,16 +4,11 @@ module LagoUtils
   class License
     def initialize(url)
       @url = url
-      @premium = false
+      @premium = true
     end
 
     def verify
-      return if ENV['LAGO_LICENSE'].blank?
-
-      http_client = LagoHttpClient::Client.new("#{url}/verify/#{ENV["LAGO_LICENSE"]}")
-      response = http_client.get
-
-      @premium = response['valid']
+      @premium = true
     end
 
     def premium?
