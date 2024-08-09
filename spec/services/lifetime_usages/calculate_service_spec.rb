b018ff4774852474900213c8ee9690b0cd9a9079
Toon Willems <m@toonwillems.be>
2024-08-09 16:59:55 +0200

    Add basic LifetimeUsages::CalculateService

---
+++ spec/services/lifetime_usages/calculate_service_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe LifetimeUsages::CalculateService, type: :service do
+  subject(:service) { described_class.new(lifetime_usage: lifetime_usage) }
+
+  let(:lifetime_usage) { create(:lifetime_usage, subscription:, recalculate_current_usage:, recalculate_invoiced_usage:) }
+  let(:recalculate_current_usage) { true }
+  let(:recalculate_invoiced_usage) { true }
+  let(:subscription) { create(:subscription, customer_id: customer.id) }
+  let(:customer) { create(:customer) }
+  let(:invoices) { create_list(:invoice, 2, :finalized, subscription:, amounts_cents: [1000, 2000]) }
+
+  describe '#call' do
+  end
+end
