706d3afcc51f193cf66623af28e4bf821b8d5dda
LovroColic <lovro.colic.lc@gmail.com>
2024-08-09 13:04:34 +0200

    feat (tax-integrations): add support for negate action (#2389)
    ## Context
    
    Currently integration with tax provider Anrok is being added
    
    ## Description
    
    This service adds logic for sending new request to Nango.
    
    New action triggers `negate` action in Anrok.
    
    This action should be called if voided operation cannot be perform (in
    case invoice is already reported to the tax authority)

---
+++ app/services/integrations/aggregator/taxes/invoices/negate_service.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Integrations
+  module Aggregator
+    module Taxes
+      module Invoices
+        class NegateService < BaseService
+          def action_path
+            "v1/#{provider}/negate_invoices"
+          end
+
+          def call
+            return result unless integration
+            return result unless integration.type == 'Integrations::AnrokIntegration'
+
+            response = http_client.post_with_response(payload, headers)
+            body = JSON.parse(response.body)
+
+            process_void_response(body)
+
+            result
+          rescue LagoHttpClient::HttpError => e
+            code = code(e)
+            message = message(e)
+
+            result.service_failure!(code:, message:)
+          end
+
+          private
+
+          def payload
+            [
+              {
+                'id' => invoice.id,
+                'voided_id' => "#{invoice.id}_voided"
+              }
+            ]
+          end
+        end
+      end
+    end
+  end
+end
