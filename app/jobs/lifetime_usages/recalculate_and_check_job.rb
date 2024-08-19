47a7a97774d935d8892f76ab468b4aed560e9214
Toon Willems <m@toonwillems.be>
2024-08-19 12:17:32 +0200

    Add RecalculateAndCheckService

---
+++ app/jobs/lifetime_usages/recalculate_and_check_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module LifetimeUsages
+  class RecalculateAndCheckJob < ApplicationJob
+    unique :until_executed, on_conflict: :log
+
+    def perform(lifetime_usage)
+      LifetimeUsages::RecalculateAndCheckService.call(lifetime_usage)
+    end
+  end
+end
