96a9c0a37898287992376f3500407c02c287f3d0
Romain Sempé <romain@getlago.com>
2024-08-20 13:18:12 +0200

    feat(dunning): Create payment request

--- spec/services/payment_requests/create_service_spec.rb
+++ spec/services/payment_requests/create_service_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe PaymentRequests::CreateService, type: :service do
   let(:params) do
     {
       external_customer_id: customer.external_id,
+      email: "john.doe@example.com",
       lago_invoice_ids: [first_invoice.id, second_invoice.id]
     }
   end
@@ -27,6 +28,23 @@ RSpec.describe PaymentRequests::CreateService, type: :service do
       expect { create_service.call }
         .to change { first_invoice.reload.payable_group }.from(nil).to(be_a(PayableGroup))
         .and change { second_invoice.reload.payable_group }.from(nil).to(be_a(PayableGroup))
+    end
+
+    it "creates a payment request" do
+      expect { create_service.call }.to change { customer.payment_requests.count }.by(1)
+    end
+
+    it "returns the payment request", :aggregate_failures do
+      result = create_service.call
+
+      expect(result.payment_request).to be_a(PaymentRequest)
+      expect(result.payment_request).to have_attributes(
+        organization:,
+        customer:,
+        amount_cents: first_invoice.total_amount_cents + second_invoice.total_amount_cents,
+        amount_currency: "EUR",
+        email: "john.doe@example.com"
+      )
     end
   end
 end
