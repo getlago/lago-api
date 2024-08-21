979f74f283e7b7d1c0316eafbc181a8e29d9ec17
Romain Sempé <romain@getlago.com>
2024-08-21 15:40:43 +0200

    fix: Remove conflict with a built-in method on graphql

--- app/graphql/types/wallets/recurring_transaction_rules/object.rb
+++ app/graphql/types/wallets/recurring_transaction_rules/object.rb
@@ -12,14 +12,16 @@ module Types
         field :granted_credits, String, null: false
         field :interval, Types::Wallets::RecurringTransactionRules::IntervalEnum, null: true
         field :invoice_requires_successful_payment, Boolean, null: false
-        field :method, Types::Wallets::RecurringTransactionRules::MethodEnum, null: false
+        field :method, Types::Wallets::RecurringTransactionRules::MethodEnum, null: false, resolver_method: :resolver_method
         field :paid_credits, String, null: false
         field :started_at, GraphQL::Types::ISO8601DateTime, null: true
         field :target_ongoing_balance, String, null: true
         field :threshold_credits, String, null: true
         field :trigger, Types::Wallets::RecurringTransactionRules::TriggerEnum, null: false
 
-        delegate :method, to: :object
+        def resolver_method
+          object.method
+        end
       end
     end
   end
