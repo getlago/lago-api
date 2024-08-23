b5675fb22b0b3054a7ed28cf1a5975ab38d33869
Anna Velentsevich <annvelents@gmail.com>
2024-08-23 17:37:15 +0200

    add fields to graphql response on applied taxes

--- app/graphql/types/invoices/applied_taxes/object.rb
+++ app/graphql/types/invoices/applied_taxes/object.rb
@@ -7,9 +7,10 @@ module Types
         graphql_name 'InvoiceAppliedTax'
         implements Types::Taxes::AppliedTax
 
-        field :invoice, Types::Invoices::Object, null: false
-
+        field :applied_on_whole_invoice?, GraphQL::Types::Boolean, null: false
         field :fees_amount_cents, GraphQL::Types::BigInt, null: false
+        field :invoice, Types::Invoices::Object, null: false
+        field :tax_code, String, null: false
       end
     end
   end
