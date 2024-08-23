b66f683ca16ca198ee0aa46e15fcb70057861417
Ancor Cruz <hello@ancorcruz.com>
2024-08-23 14:32:09 +0100

    fix migration

--- db/migrate/20240822142524_add_payment_retry_columns_to_payment_requests.rb
+++ db/migrate/20240822142524_add_payment_retry_columns_to_payment_requests.rb
@@ -1,8 +1,10 @@
 class AddPaymentRetryColumnsToPaymentRequests < ActiveRecord::Migration[7.1]
   def change
-    change_table :payment_requests, bulk: true do |t|
-      t.integer :payment_attempts, default: 0, null: false
-      t.boolean :ready_for_payment_processing, default: true, null: false
+    safety_assured do
+      change_table :payment_requests, bulk: true do |t|
+        t.integer :payment_attempts, default: 0, null: false
+        t.boolean :ready_for_payment_processing, default: true, null: false
+      end
     end
   end
 end
