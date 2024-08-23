06a4ac3e5b66c2da7d462af8704f5e81edb068a5
Miguel Pinto <darkymiguel@gmail.com>
2024-08-23 20:04:28 +0100

    feat: add finalize_zero_amount to customers

--- db/migrate/20240823170532_add_finalize_zero_amount_invoice_to_customers.rb
+++ db/migrate/20240823170532_add_finalize_zero_amount_invoice_to_customers.rb
@@ -2,6 +2,6 @@
 
 class AddFinalizeZeroAmountInvoiceToCustomers < ActiveRecord::Migration[7.1]
   def change
-    add_column :customers, :finalize_zero_amount_invoice, :integer, default: 0, null: false
+    add_column :customers, :finalize_zero_amount_invoice, :integer
   end
 end
