f1099461dc3a4c70204425d9d0ec08367d41aeb1
Anna Velentsevich <annvelents@gmail.com>
2024-08-26 10:33:58 +0200

    remove redundant field

--- app/graphql/types/invoices/applied_taxes/object.rb
+++ app/graphql/types/invoices/applied_taxes/object.rb
@@ -10,7 +10,6 @@ module Types
         field :applied_on_whole_invoice?, GraphQL::Types::Boolean, null: false
         field :fees_amount_cents, GraphQL::Types::BigInt, null: false
         field :invoice, Types::Invoices::Object, null: false
-        field :tax_code, String, null: false
       end
     end
   end
