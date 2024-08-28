30c1836a362adbd5cb8f96db376b6767c54f6649
Anna Velentsevich <annvelents@gmail.com>
2024-08-28 15:38:12 +0200

    Feat(anrok): GraphQL add enum type for tax codes that are applicable on whole invoice (#2499)

--- app/graphql/types/invoices/applied_taxes/object.rb
+++ app/graphql/types/invoices/applied_taxes/object.rb
@@ -8,8 +8,13 @@ module Types
         implements Types::Taxes::AppliedTax
 
         field :applied_on_whole_invoice, GraphQL::Types::Boolean, null: false, method: :applied_on_whole_invoice?
+        field :enumed_tax_code, Types::Invoices::AppliedTaxes::WholeInvoiceApplicableTaxCodeEnum, null: true
         field :fees_amount_cents, GraphQL::Types::BigInt, null: false
         field :invoice, Types::Invoices::Object, null: false
+
+        def enumed_tax_code
+          object.tax_code if object.applied_on_whole_invoice?
+        end
       end
     end
   end
