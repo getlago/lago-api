3096ec037a01cffd1a7e21d23b13b813e1631b84
Romain Sempé <romain@getlago.com>
2024-08-29 17:49:17 +0200

    feat(dunning): Add customerId to invoices graphql resolver

--- app/graphql/resolvers/invoices_resolver.rb
+++ app/graphql/resolvers/invoices_resolver.rb
@@ -11,6 +11,7 @@ module Resolvers
 
     argument :currency, Types::CurrencyEnum, required: false
     argument :customer_external_id, String, required: false
+    argument :customer_id, ID, required: false, description: 'Uniq ID of the customer'
     argument :invoice_type, [Types::Invoices::InvoiceTypeEnum], required: false
     argument :issuing_date_from, GraphQL::Types::ISO8601Date, required: false
     argument :issuing_date_to, GraphQL::Types::ISO8601Date, required: false
@@ -27,6 +28,7 @@ module Resolvers
     def resolve( # rubocop:disable Metrics/ParameterLists
       currency: nil,
       customer_external_id: nil,
+      customer_id: nil,
       invoice_type: nil,
       issuing_date_from: nil,
       issuing_date_to: nil,
@@ -49,6 +51,7 @@ module Resolvers
           status:,
           currency:,
           customer_external_id:,
+          customer_id:,
           invoice_type:,
           issuing_date_from:,
           issuing_date_to:
