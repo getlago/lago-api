67852d614bacec23d5c52d35df805b11c3581975
Anna Velentsevich <annvelents@gmail.com>
2024-08-29 16:04:02 +0200

    add enumed_tax_code to tax breakdown serialization

--- app/graphql/types/integrations/anrok_objects/breakdown_object.rb
+++ app/graphql/types/integrations/anrok_objects/breakdown_object.rb
@@ -6,6 +6,7 @@ module Types
       class BreakdownObject < Types::BaseObject
         graphql_name 'AnrokBreakdownObject'
 
+        field :enumed_tax_code, Types::Invoices::AppliedTaxes::WholeInvoiceApplicableTaxCodeEnum, null: true
         field :name, String, null: true
         field :rate, GraphQL::Types::Float, null: true
         field :tax_amount, GraphQL::Types::BigInt, null: true
@@ -13,6 +14,14 @@ module Types
 
         def rate
           BigDecimal(object.rate)
+        end
+
+        def tax_code
+          object.name&.parameterize(separator: '_')
+        end
+
+        def enumed_tax_code
+          tax_code if Invoice::AppliedTax::TAX_CODES_APPLICABLE_ON_WHOLE_INVOICE.include?(tax_code)
         end
       end
     end
