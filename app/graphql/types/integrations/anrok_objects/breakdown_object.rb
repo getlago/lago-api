4ad16114238ae5b43ba835776ca14b06f0cc1ac3
Anna Velentsevich <annvelents@gmail.com>
2024-08-30 14:43:11 +0200

    Feat(anrok): add enumed_tax_code to tax breakdown serialization (#2512)
    to display addon taxes the same way when creating a one-off invoice as
    when we show fee taxes in all the other invoices, FE needs
    enumed_tax_code to be sent in the same format as for Invoice::AppliedTax
    (which is usually created when creating an invoice)
    
    ---------
    
    Co-authored-by: Ancor Cruz <hello@ancorcruz.com>

--- app/graphql/types/integrations/anrok_objects/breakdown_object.rb
+++ app/graphql/types/integrations/anrok_objects/breakdown_object.rb
@@ -6,6 +6,9 @@ module Types
       class BreakdownObject < Types::BaseObject
         graphql_name 'AnrokBreakdownObject'
 
+        # we need to show how this tax will behave when invoice is generated - will it be applied
+        # on whole invoice specific rule or just a normal tax
+        field :enumed_tax_code, Types::Invoices::AppliedTaxes::WholeInvoiceApplicableTaxCodeEnum, null: true
         field :name, String, null: true
         field :rate, GraphQL::Types::Float, null: true
         field :tax_amount, GraphQL::Types::BigInt, null: true
@@ -13,6 +16,14 @@ module Types
 
         def rate
           BigDecimal(object.rate)
+        end
+
+        def tax_code
+          @tax_code ||= object.name&.parameterize(separator: '_')
+        end
+
+        def enumed_tax_code
+          tax_code if Invoice::AppliedTax::TAX_CODES_APPLICABLE_ON_WHOLE_INVOICE.include?(tax_code)
         end
       end
     end
