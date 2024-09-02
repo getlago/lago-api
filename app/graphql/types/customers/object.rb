4ce459d66641da4a7bafdf272ee4ebd155d530cb
Miguel Pinto <darkymiguel@gmail.com>
2024-09-02 19:08:04 +0100

    feat: add enum type

--- app/graphql/types/customers/object.rb
+++ app/graphql/types/customers/object.rb
@@ -83,7 +83,7 @@ module Types
         description 'Check if customer attributes are editable'
       end
 
-      field :finalize_zero_amount_invoice, String, null: true, description: 'Options for handling invoices with a zero total amount.'
+      field :finalize_zero_amount_invoice, Types::Customers::FinalizeZeroAmountInvoiceEnum, null: true, description: 'Options for handling invoices with a zero total amount.'
 
       def invoices
         object.invoices.visible.order(created_at: :desc)
