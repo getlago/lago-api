8a3b25a4f94a24fdd3db9df8e525e34afc62f1bc
Ivan Novosad <ivan@getlago.com>
2024-09-04 09:01:10 +0200

    feat(precise_amount): Fix precise_amount_cents in taxes services

--- app/services/fees/apply_provider_taxes_service.rb
+++ app/services/fees/apply_provider_taxes_service.rb
@@ -14,6 +14,7 @@ module Fees
       return result if fee.applied_taxes.any?
 
       applied_taxes_amount_cents = 0
+      applied_precise_taxes_amount_cents = 0.to_d
       applied_taxes_rate = 0
 
       fee_taxes.tax_breakdown.each do |tax|
@@ -28,19 +29,22 @@ module Fees
         )
         fee.applied_taxes << applied_tax
 
-        tax_amount_cents = (fee.sub_total_excluding_taxes_amount_cents * tax_rate).fdiv(100.to_d)
+        tax_amount_cents = (fee.sub_total_excluding_taxes_amount_cents * tax_rate).fdiv(100)
+        tax_precise_amount_cents = (fee.sub_total_excluding_taxes_precise_amount_cents * tax_rate).fdiv(100.to_d)
+
         applied_tax.amount_cents = tax_amount_cents.round
-        applied_tax.precise_amount_cents = tax_amount_cents
+        applied_tax.precise_amount_cents = tax_precise_amount_cents
         applied_tax.save! if fee.persisted?
 
         applied_taxes_amount_cents += tax_amount_cents
+        applied_precise_taxes_amount_cents += tax_precise_amount_cents
         applied_taxes_rate += tax_rate
 
         result.applied_taxes << applied_tax
       end
 
       fee.taxes_amount_cents = applied_taxes_amount_cents.round
-      fee.taxes_precise_amount_cents = applied_taxes_amount_cents
+      fee.taxes_precise_amount_cents = applied_precise_taxes_amount_cents
       fee.taxes_rate = applied_taxes_rate
 
       result
