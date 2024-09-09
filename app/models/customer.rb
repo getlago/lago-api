c5349a99befb2cbbe214e6980248fd15d915d75f
Miguel Pinto <darkymiguel@gmail.com>
2024-09-09 17:21:20 +0100

    feat: added firstname and last name to queries

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -79,7 +79,7 @@ class Customer < ApplicationRecord
   validates :email, email: true, if: :email?
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[id name external_id email]
+    %w[id name firstname lastname external_id email]
   end
 
   def display_name
