257a42b056fff5533af63096cc654ce27ff50aef
Miguel Pinto <darkymiguel@gmail.com>
2024-09-10 14:47:48 +0100

    feat: dont send webhooks for 0 invoices

--- app/services/invoices/create_pay_in_advance_charge_service.rb
+++ app/services/invoices/create_pay_in_advance_charge_service.rb
@@ -47,13 +47,14 @@ module Invoices
         invoice.save!
       end
 
-      Utils::SegmentTrack.invoice_created(invoice)
-
-      deliver_webhooks
-      GeneratePdfAndNotifyJob.perform_later(invoice:, email: should_deliver_email?)
-      Integrations::Aggregator::Invoices::CreateJob.perform_later(invoice:) if invoice.should_sync_invoice?
-      Integrations::Aggregator::SalesOrders::CreateJob.perform_later(invoice:) if invoice.should_sync_sales_order?
-      Invoices::Payments::CreateService.new(invoice).call
+      unless invoice.closed?
+        Utils::SegmentTrack.invoice_created(invoice)
+        deliver_webhooks
+        GeneratePdfAndNotifyJob.perform_later(invoice:, email: should_deliver_email?)
+        Integrations::Aggregator::Invoices::CreateJob.perform_later(invoice:) if invoice.should_sync_invoice?
+        Integrations::Aggregator::SalesOrders::CreateJob.perform_later(invoice:) if invoice.should_sync_sales_order?
+        Invoices::Payments::CreateService.new(invoice).call
+      end
 
       result
     rescue ActiveRecord::RecordInvalid => e
