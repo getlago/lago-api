784911b7c76d2b1a3ce5119ede5bafadd5ebeafe
Ivan Novosad <ivan@getlago.com>
2024-09-10 18:16:21 +0100

    misc: Add id field to graphql charge spec

--- spec/graphql/types/customers/usage/charge_spec.rb
+++ spec/graphql/types/customers/usage/charge_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Types::Customers::Usage::Charge do
   subject { described_class }
 
+  it { is_expected.to have_field(:id).of_type('ID!') }
   it { is_expected.to have_field(:amount_cents).of_type('BigInt!') }
   it { is_expected.to have_field(:events_count).of_type('Int!') }
   it { is_expected.to have_field(:units).of_type('Float!') }
