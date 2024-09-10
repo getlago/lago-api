4bcdfcb8864aa1f1ccd3267d4689441cd35198fb
Anna Velentsevich <annvelents@gmail.com>
2024-09-10 18:47:43 +0100

    add test for applying taxes on credit notes when taxes are provided by a service

--- spec/factories/invoice_applied_taxes.rb
+++ spec/factories/invoice_applied_taxes.rb
@@ -10,5 +10,16 @@ FactoryBot.define do
     tax_rate { 20.0 }
     amount_cents { 200 }
     amount_currency { 'EUR' }
+
+    transient do
+      provider_tax_breakdown_object { nil }
+    end
+
+    trait :with_provider_tax do
+      tax_description { provider_tax_breakdown_object.type }
+      tax_code { provider_tax_breakdown_object.name.parameterize(separator: '_') }
+      tax_name { provider_tax_breakdown_object.name }
+      tax_rate {  provider_tax_breakdown_object.rate }
+    end
   end
 end
