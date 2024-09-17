0e40d2a848d71e78e1b64cbbc595448c579809e8
Anna Velentsevich <annvelents@gmail.com>
2024-09-17 19:06:34 +0200

    add CreditNotes::CalculateItemsAvailableAmountsService

---
+++ app/services/credit_notes/calculate_items_available_amounts_service.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module CreditNotes
+  class CalculateItemsAvailableAmountsService < BaseService
+    def initialize(credit_note:)
+      @credit_note = credit_note
+
+      super
+    end
+
+    def call
+      clear_items_available_amounts = calculate_items_sum_without_taxes
+      result.available_amounts = split_sum_into_items(clear_items_available_amounts)
+      result
+    end
+
+    private
+
+    attr_reader :credit_note
+
+    # balance_amount_cents includes items_sum + their taxes,
+    # but taxes_rate is the percentage, not decimal
+    def calculate_items_sum_without_taxes
+      (credit_note.balance_amount_cents / (100 + credit_note.taxes_rate)) * 100
+    end
+
+    def split_sum_into_items(clear_items_available_amounts)
+      item_values = {}
+      credit_note.items.each do |item|
+        item_weight = item.amount_cents / credit_note_original_amount_without_taxes.to_f
+        item_values[item.id] = item_weight * clear_items_available_amounts
+      end
+      item_values
+    end
+
+    def credit_note_original_amount_without_taxes
+      @credit_note_original_amount_without_taxes ||= credit_note.credit_amount_cents - credit_note.taxes_amount_cents
+    end
+  end
+end
---
+++ spec/services/credit_notes/calculate_items_available_amounts_service_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CreditNotes::CalculateItemsAvailableAmountsService, type: :service do
+  subject(:credit_service) { described_class.new(credit_note: ) }
+
+  let(:credit_note) do
+    create(:credit_note, balance_amount_cents: 60000, credit_amount_cents: 60000, taxes_amount_cents: 10000, taxes_rate: 20)
+  end
+  let(:item1) { create(:credit_note_item, credit_note: credit_note, amount_cents: 20000) }
+  let(:item2) { create(:credit_note_item, credit_note: credit_note, amount_cents: 30000) }
+
+  before do
+    item1
+    item2
+    credit_note.reload
+  end
+
+  context 'when credit_note has balance_amount_cents equal to taxes_amount_cents' do
+    it 'calculates items available amounts' do
+      result = credit_service.call
+      expect(result.available_amounts).to eq({ item1.id => 20000, item2.id => 30000 })
+    end
+  end
+
+  context 'when credit_note has balance_amount_cents not equal to taxes_amount_cents' do
+    let(:credit_note) do
+      create(:credit_note, balance_amount_cents: 36000, credit_amount_cents: 60000, taxes_amount_cents: 10000, taxes_rate: 20)
+    end
+
+    it 'calculates items available amounts' do
+      result = credit_service.call
+      expect(result.available_amounts).to eq({ item1.id => 12000, item2.id => 18000 })
+    end
+  end
+end
