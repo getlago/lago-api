cdcdca6e33c61dbe7a952480d78b74303f996c34
Romain Sempé <romain@getlago.com>
2024-09-23 11:42:09 +0200

    feat(dunning): Add subscriptions resolver to customer portal

---
+++ app/graphql/resolvers/customer_portal/subscriptions_resolver.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module CustomerPortal
+    class SubscriptionsResolver < Resolvers::BaseResolver
+      include AuthenticableCustomerPortalUser
+
+      description "Query customer portal subscriptions"
+
+      argument :limit, Integer, required: false
+      argument :page, Integer, required: false
+      argument :plan_code, String, required: false
+      argument :status, [Types::Subscriptions::StatusTypeEnum], required: false
+
+      type Types::Subscriptions::Object.collection_type, null: false
+
+      def resolve(page: nil, limit: nil, plan_code: nil, status: nil)
+        result = SubscriptionsQuery.call(
+          organization: context[:customer_portal_user],
+          pagination: {page:, limit:},
+          filters: {
+            external_customer_id: context[:customer_portal_user].external_id,
+            plan_code:,
+            status:
+          }
+        )
+
+        result.subscriptions
+      end
+    end
+  end
+end
---
+++ spec/graphql/resolvers/customer_portal/subscriptions_resolver_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Resolvers::CustomerPortal::SubscriptionsResolver, type: :graphql do
+  let(:query) do
+    <<~GQL
+      query {
+        customerPortalSubscriptions(limit: 5, planCode: "#{plan.code}", status: [active]) {
+            collection { id externalId plan { code } }
+            metadata { currentPage, totalCount }
+        }
+      }
+    GQL
+  end
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:customer) { create(:customer, organization:) }
+  let(:plan) { create(:plan, organization:) }
+  let(:active_subscription) { create(:subscription, customer:, plan:) }
+  let(:terminated_subscription) { create(:subscription, :terminated, customer:, plan:) }
+
+  before do
+    active_subscription
+    terminated_subscription
+  end
+
+  it_behaves_like "requires a customer portal user"
+
+  it "returns a list of subscriptions" do
+    result = execute_graphql(customer_portal_user: customer, query:)
+
+    subscriptions_response = result["data"]["customerPortalSubscriptions"]
+
+    aggregate_failures do
+      expect(subscriptions_response["collection"].pluck("id")).to contain_exactly(active_subscription.id)
+      expect(subscriptions_response["metadata"]["currentPage"]).to eq(1)
+      expect(subscriptions_response["metadata"]["totalCount"]).to eq(1)
+    end
+  end
+
+  context "with filter on status" do
+    let(:query) do
+      <<~GQL
+        query($status: [StatusTypeEnum!]) {
+          customerPortalSubscriptions(status: $status) {
+            collection { id }
+            metadata { currentPage, totalCount }
+          }
+        }
+      GQL
+    end
+
+    it "only returns draft invoice" do
+      result = execute_graphql(
+        customer_portal_user: customer,
+        query:,
+        variables: {status: ["terminated"]}
+      )
+
+      subscriptions_response = result["data"]["customerPortalSubscriptions"]
+
+      aggregate_failures do
+        expect(subscriptions_response["collection"].first["id"]).to eq(terminated_subscription.id)
+        expect(subscriptions_response["metadata"]["totalCount"]).to eq(1)
+      end
+    end
+  end
+
+  context "without customer portal user" do
+    it "returns an error" do
+      result = execute_graphql(query:)
+      expect_unauthorized_error(result)
+    end
+  end
+end
