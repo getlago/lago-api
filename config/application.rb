e9a13714167f9af5fc13130387a7e809f89b335f
Vincent Pochet <vincent@getlago.com>
2024-09-23 16:24:22 +0200

    misc: Allign env variable with documentation

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,9 @@ module LagoApi
 
     # Configuration for active record encryption
     config.active_record.encryption.hash_digest_class = OpenSSL::Digest::SHA1
-    config.active_record.encryption.primary_key = ENV['ENCRYPTION_PRIMARY_KEY']
-    config.active_record.encryption.deterministic_key = ENV['ENCRYPTION_DETERMINISTIC_KEY']
-    config.active_record.encryption.key_derivation_salt = ENV['ENCRYPTION_KEY_DERIVATION_SALT']
+    config.active_record.encryption.primary_key = ENV['ENCRYPTION_PRIMARY_KEY'] || ENV['LAGO_ENCRYPTION_PRIMARY_KEY']
+    config.active_record.encryption.deterministic_key = ENV['ENCRYPTION_DETERMINISTIC_KEY'] || ENV['LAGO_ENCRYPTION_DETERMINISTIC_KEY']
+    config.active_record.encryption.key_derivation_salt = ENV['ENCRYPTION_KEY_DERIVATION_SALT'] || ENV['LAGO_ENCRYPTION_KEY_DERIVATION_SALT']
 
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.available_locales = %i[en fr nb de it es sv]
