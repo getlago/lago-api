8ce3d431cc4e1c978e6809431dfbbc25d0330a10
Preeti Yuankrathok <preetisatit@gmail.com>
2024-09-26 21:33:43 +0700

    feat(event): Add event list API (#2601)
    ## Roadmap Task
    
    👉 https://getlago.canny.io/feature-requests/p/retrieve-and-filter-events
    
    ## Context
    
    Currently, REST API does not provide the endpoint to list all events (as
    in the Developer -> Events page). This pull request aim to expose the
    event list to the REST API to let the user retrieve and filter events.
    
    ## Description
    
    This pull request add supports for:  
    - Adding `GET /api/v1/events` endpoint
    - Filtering events by `code`, `external_subscription_id`,
    `timestamp_from`, and `timestamp_to`
    
    I'm also planning to checkout the front-end side to add these kinds of
    filter into the UI later as well.

---
+++ app/queries/events_query.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class EventsQuery < BaseQuery
+  def call
+    events = organization.events
+    events = paginate(events)
+    events = events.order(created_at: :desc)
+
+    events = with_code(events) if filters.code
+    events = with_external_subscription_id(events) if filters.external_subscription_id
+    events = with_timestamp_range(events) if filters.timestamp_from || filters.timestamp_to
+
+    result.events = events
+    result
+  rescue BaseService::FailedResult
+    result
+  end
+
+  private
+
+  def with_code(scope)
+    scope.where(code: filters.code)
+  end
+
+  def with_external_subscription_id(scope)
+    scope.where(external_subscription_id: filters.external_subscription_id)
+  end
+
+  def with_timestamp_range(scope)
+    scope = scope.where(timestamp: timestamp_from..) if filters.timestamp_from
+    scope = scope.where(timestamp: ..timestamp_to) if filters.timestamp_to
+    scope
+  end
+
+  def timestamp_from
+    @timestamp_from ||= parse_datetime_filter(:timestamp_from)
+  end
+
+  def timestamp_to
+    @timestamp_to ||= parse_datetime_filter(:timestamp_to)
+  end
+end
---
+++ spec/queries/events_query_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EventsQuery, type: :query do
+  subject(:events_query) { described_class.new(organization:, pagination:, filters:) }
+
+  let(:organization) { create(:organization) }
+  let(:pagination) { nil }
+  let(:filters) { {} }
+
+  let(:event) { create(:event, timestamp: 1.day.ago.to_date, organization:) }
+
+  before { event }
+
+  describe 'call' do
+    it 'returns a list of events' do
+      result = events_query.call
+
+      aggregate_failures do
+        expect(result).to be_success
+        expect(result.events.count).to eq(1)
+        expect(result.events).to eq([event])
+      end
+    end
+
+    context 'with pagination' do
+      let(:pagination) { {page: 2, limit: 10} }
+
+      it 'applies the pagination' do
+        result = events_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.events.count).to eq(0)
+          expect(result.events.current_page).to eq(2)
+        end
+      end
+    end
+
+    context 'with code filter' do
+      let(:event2) { create(:event, organization:) }
+      let(:filters) { {code: event.code} }
+
+      before { event2 }
+
+      it 'applies the filter' do
+        result = events_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.events.count).to eq(1)
+        end
+      end
+    end
+
+    context 'with external subscription id filter' do
+      let(:event2) { create(:event, organization:) }
+      let(:filters) { {external_subscription_id: event.external_subscription_id} }
+
+      before { event2 }
+
+      it 'applies the filter' do
+        result = events_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.events.count).to eq(1)
+        end
+      end
+    end
+
+    context 'with timestamp from filter' do
+      let(:filters) {
+        {
+          timestamp_from: 2.days.ago.iso8601.to_date.to_s,
+          timestamp_to: Date.tomorrow.iso8601.to_date.to_s
+        }
+      }
+
+      it 'applies the filter' do
+        result = events_query.call
+
+        aggregate_failures do
+          expect(result).to be_success
+          expect(result.events.count).to eq(1)
+        end
+      end
+    end
+  end
+end
