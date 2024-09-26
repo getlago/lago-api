592b8dca5eb095cde425fc3e2da7809605615f29
Lovro Colic <lovro.colic.lc@gmail.com>
2024-09-26 09:35:57 +0200

    fix linter issues

--- app/graphql/types/invoices/applied_taxes/object.rb
+++ app/graphql/types/invoices/applied_taxes/object.rb
@@ -10,8 +10,8 @@ module Types
         field :applied_on_whole_invoice, GraphQL::Types::Boolean, null: false, method: :applied_on_whole_invoice?
         field :enumed_tax_code, Types::Invoices::AppliedTaxes::WholeInvoiceApplicableTaxCodeEnum, null: true
         field :fees_amount_cents, GraphQL::Types::BigInt, null: false
-        field :taxable_base_amount_cents, GraphQL::Types::BigInt, null: false
         field :invoice, Types::Invoices::Object, null: false
+        field :taxable_base_amount_cents, GraphQL::Types::BigInt, null: false
 
         def enumed_tax_code
           object.tax_code if object.applied_on_whole_invoice?
