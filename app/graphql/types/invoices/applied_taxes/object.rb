f0e6fd915ea68de19bc3071b14581988a461ed75
Lovro Colic <lovro.colic.lc@gmail.com>
2024-09-27 10:56:19 +0200

    update gql schema

--- app/graphql/types/invoices/applied_taxes/object.rb
+++ app/graphql/types/invoices/applied_taxes/object.rb
@@ -11,14 +11,10 @@ module Types
         field :enumed_tax_code, Types::Invoices::AppliedTaxes::WholeInvoiceApplicableTaxCodeEnum, null: true
         field :fees_amount_cents, GraphQL::Types::BigInt, null: false
         field :invoice, Types::Invoices::Object, null: false
-        field :taxable_base_amount_cents, GraphQL::Types::BigInt, null: false
+        field :taxable_amount_cents, GraphQL::Types::BigInt, null: false
 
         def enumed_tax_code
           object.tax_code if object.applied_on_whole_invoice?
-        end
-
-        def taxable_base_amount_cents
-          object.taxable_amount_cents
         end
       end
     end
