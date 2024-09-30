f168da25cea155856f0fb3a96f09064e8af56079
Toon Willems <m@toonwillems.be>
2024-09-30 15:06:33 +0200

    format numbers with 6 significant digits

--- app/views/helpers/money_helper.rb
+++ app/views/helpers/money_helper.rb
@@ -10,6 +10,12 @@ class MoneyHelper
   end
 
   def self.format_with_precision(amount_cents, currency)
+    amount_cents = if amount_cents < 1
+      BigDecimal("%.6g" % amount_cents)
+    else
+      amount_cents.round(6)
+    end
+
     Utils::MoneyWithPrecision.from_amount(amount_cents, currency).format(
       format: I18n.t('money.format'),
       decimal_mark: I18n.t('money.decimal_mark'),
