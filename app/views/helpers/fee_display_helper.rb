d72c3245701ee13501b3100e362fd6ad33dd5e0f
Ivan Novosad <ivan@getlago.com>
2024-10-02 15:52:18 +0200

    fix(invoice-template): Fix grouped_by_display helper method

--- app/views/helpers/fee_display_helper.rb
+++ app/views/helpers/fee_display_helper.rb
@@ -2,9 +2,7 @@
 
 class FeeDisplayHelper
   def self.grouped_by_display(fee)
-    return '' unless fee.charge?
-    return '' if fee.charge.properties['grouped_by'].blank?
-    return '' if fee.grouped_by.values.compact.blank?
+    return '' if !fee.charge? || fee.grouped_by.values.compact.blank?
 
     " • #{fee.grouped_by.values.compact.join(" • ")}"
   end
