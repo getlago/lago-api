05dddc26d58582feab2647835df6ad5ba1629105
Toon Willems <m@toonwillems.be>
2024-10-03 11:24:55 +0200

    Don't execute RefreshDraft service when the ready_to_be_refreshed is not true

--- app/jobs/invoices/refresh_draft_job.rb
+++ app/jobs/invoices/refresh_draft_job.rb
@@ -4,9 +4,12 @@ module Invoices
   class RefreshDraftJob < ApplicationJob
     queue_as 'invoices'
 
-    unique :until_executed, on_conflict: :log
+    unique :until_executed, on_conflict: :log, lock_ttl: 6.hours
 
     def perform(invoice)
+      # if this has already been set to false, we can skip the job
+      return unless invoice.ready_to_be_refreshed?
+
       ::Invoices::RefreshDraftService.call(invoice:)
     end
   end
