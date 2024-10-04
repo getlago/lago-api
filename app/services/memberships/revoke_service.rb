c0b4a99fc2b5c499683740c4259e72bb227d5964
Vincent Pochet <vincent@getlago.com>
2024-10-04 09:58:02 +0200

    fix(membership): Ensure membership is in current organization when revoking

--- app/services/memberships/revoke_service.rb
+++ app/services/memberships/revoke_service.rb
@@ -2,10 +2,16 @@
 
 module Memberships
   class RevokeService < BaseService
-    def call(id)
-      membership = Membership.find_by(id:)
+    def initialize(user:, membership:)
+      @user = user
+      @membership = membership
+
+      super
+    end
+
+    def call
       return result.not_found_failure!(resource: 'membership') unless membership
-      return result.not_allowed_failure!(code: 'cannot_revoke_own_membership') if result.user.id == membership.user.id
+      return result.not_allowed_failure!(code: 'cannot_revoke_own_membership') if user.id == membership.user.id
       return result.not_allowed_failure!(code: 'last_admin') if membership.organization.memberships.admin.count == 1 && membership.admin?
 
       membership.mark_as_revoked!
@@ -13,5 +19,9 @@ module Memberships
       result.membership = membership
       result
     end
+
+    private
+
+    attr_reader :user, :membership
   end
 end
