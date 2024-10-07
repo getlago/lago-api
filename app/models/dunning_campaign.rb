df83b22b6f3860ac8c1865b2ec14095d0615ed27
Romain Sempé <romain@getlago.com>
2024-10-07 11:22:27 +0200

    feat(dunning): Add DunningCampaign model

---
+++ app/models/dunning_campaign.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class DunningCampaign < ApplicationRecord
+  include PaperTrailTraceable
+
+  belongs_to :organization
+
+  validates :name, presence: true
+  validates :days_between_attempts, numericality: {greater_than: 0}
+  validates :max_attempts, numericality: {greater_than: 0}
+  validates :code, uniqueness: {scope: :organization_id}
+end
+
+# == Schema Information
+#
+# Table name: dunning_campaigns
+#
+#  id                      :uuid             not null, primary key
+#  applied_to_organization :boolean          default(FALSE), not null
+#  code                    :string           not null
+#  days_between_attempts   :integer          default(1), not null
+#  description             :text
+#  max_attempts            :integer          default(1), not null
+#  name                    :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  organization_id         :uuid             not null
+#
+# Indexes
+#
+#  index_dunning_campaigns_on_organization_id           (organization_id)
+#  index_dunning_campaigns_on_organization_id_and_code  (organization_id,code) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organization_id => organizations.id)
+#
---
+++ spec/models/dunning_campaign_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe DunningCampaign, type: :model do
+  subject(:dunning_campaign) { create(:dunning_campaign) }
+
+  it_behaves_like "paper_trail traceable"
+
+  it { is_expected.to belong_to(:organization) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
+  it { is_expected.to validate_numericality_of(:days_between_attempts).is_greater_than(0)}
+  it { is_expected.to validate_numericality_of(:max_attempts).is_greater_than(0)}
+
+  it { is_expected.to validate_uniqueness_of(:code).scoped_to(:organization_id) }
+end
