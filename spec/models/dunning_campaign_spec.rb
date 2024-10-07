f688ffdb28135d2a2be1a0d0923f588804fd2175
Romain Sempé <romain@getlago.com>
2024-10-07 14:04:36 +0200

    feat(dunning): Organization has many dunning campaigns

--- spec/models/dunning_campaign_spec.rb
+++ spec/models/dunning_campaign_spec.rb
@@ -8,11 +8,12 @@ RSpec.describe DunningCampaign, type: :model do
   it_behaves_like "paper_trail traceable"
 
   it { is_expected.to belong_to(:organization) }
+  it { is_expected.to have_many(:thresholds).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of(:name) }
 
-  it { is_expected.to validate_numericality_of(:days_between_attempts).is_greater_than(0)}
-  it { is_expected.to validate_numericality_of(:max_attempts).is_greater_than(0)}
+  it { is_expected.to validate_numericality_of(:days_between_attempts).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:max_attempts).is_greater_than(0) }
 
   it { is_expected.to validate_uniqueness_of(:code).scoped_to(:organization_id) }
 end
