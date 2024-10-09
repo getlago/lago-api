9557c55dd60d25923af2dc4fb2a68619e7384880
Ivan Novosad <ivan@getlago.com>
2024-10-09 17:01:17 +0200

    feat(hubspot): Add GraphQL for hubspot customers

--- app/graphql/types/integrations/hubspot/targeted_objects_enum.rb
+++ app/graphql/types/integrations/hubspot/targeted_objects_enum.rb
@@ -4,6 +4,8 @@ module Types
   module Integrations
     class Hubspot
       class TargetedObjectsEnum < Types::BaseEnum
+        graphql_name 'HubspotTargetedObjectsEnum'
+
         ::Integrations::HubspotIntegration::TARGETED_OBJECTS.each do |type|
           value type
         end
