4714bd821e84fdf1f38fed323cd5fd640706f411
Ivan Novosad <ivan@getlago.com>
2024-10-09 17:01:17 +0200

    feat(hubspot): Add GraphQL for hubspot customers

--- spec/graphql/types/integrations/hubspot_spec.rb
+++ spec/graphql/types/integrations/hubspot_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Types::Integrations::Hubspot do
 
   it { is_expected.to have_field(:code).of_type('String!') }
   it { is_expected.to have_field(:connection_id).of_type('ID!') }
-  it { is_expected.to have_field(:default_targeted_object).of_type('TargetedObjectsEnum!') }
+  it { is_expected.to have_field(:default_targeted_object).of_type('HubspotTargetedObjectsEnum!') }
   it { is_expected.to have_field(:name).of_type('String!') }
   it { is_expected.to have_field(:private_app_token).of_type('String!') }
 
