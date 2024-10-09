fc80ca492c7093ffe33b7c1b8faebe40d761c21f
Ivan Novosad <ivan@getlago.com>
2024-10-09 16:35:45 +0200

    feat(hubspot): Add hubspot payloads for contacts and companies

---
+++ spec/services/integrations/aggregator/companies/payloads/factory_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Integrations::Aggregator::Companies::Payloads::Factory do
+  let(:customer) { integration_customer.customer }
+  let(:integration) { integration_customer.integration }
+  let(:subsidiary_id) { '123' }
+
+  describe '.new_instance' do
+    subject(:new_instance_call) do
+      described_class.new_instance(integration:, customer:, integration_customer:, subsidiary_id:)
+    end
+
+    context 'when customer is a hubspot customer' do
+      let(:integration_customer) { FactoryBot.create(:hubspot_customer, customer:) }
+      let(:customer) { FactoryBot.create(:customer) }
+
+      context 'when customer is an individual' do
+        let(:customer_type) { 'individual' }
+
+        it 'returns payload' do
+          expect(subject).to be_a(Integrations::Aggregator::Contacts::Payloads::Hubspot)
+        end
+      end
+
+      context 'when customer is a company' do
+        let(:customer_type) { ['company', nil].sample }
+
+        it 'returns payload' do
+          expect(subject).to be_a(Integrations::Aggregator::Companies::Payloads::Hubspot)
+        end
+      end
+    end
+  end
+end
