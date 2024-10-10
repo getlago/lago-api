87836da7f26b7814172bb73615034c8aa41964e2
Lovro Colic <lovro.colic.lc@gmail.com>
2024-10-10 11:37:26 +0200

    improve handling rc iupon wallet update

--- app/services/wallets/apply_paid_credits_service.rb
+++ app/services/wallets/apply_paid_credits_service.rb
@@ -11,9 +11,11 @@ module Wallets
       return result unless wallet_transaction
       return result if wallet_transaction.status == 'settled'
 
-      WalletTransactions::SettleService.new(wallet_transaction:).call
-      Wallets::Balance::IncreaseService
-        .new(wallet: wallet_transaction.wallet, credits_amount: wallet_transaction.credit_amount).call
+      ActiveRecord::Base.transaction do
+        WalletTransactions::SettleService.new(wallet_transaction:).call
+        Wallets::Balance::IncreaseService
+          .new(wallet: wallet_transaction.wallet, credits_amount: wallet_transaction.credit_amount).call
+      end
 
       result.wallet_transaction = wallet_transaction
       result
--- app/services/wallets/balance/increase_service.rb
+++ app/services/wallets/balance/increase_service.rb
@@ -3,51 +3,34 @@
 module Wallets
   module Balance
     class IncreaseService < BaseService
-      MAX_RETRIES = 5
-
       def initialize(wallet:, credits_amount:, reset_consumed_credits: false)
         super
 
         @wallet = wallet
         @credits_amount = credits_amount
         @reset_consumed_credits = reset_consumed_credits
-        @retries = 0
       end
 
       def call
-        ActiveRecord::Base.transaction do
-          currency = wallet.balance.currency
-          amount_cents = wallet.rate_amount * credits_amount * currency.subunit_to_unit
+        currency = wallet.balance.currency
+        amount_cents = wallet.rate_amount * credits_amount * currency.subunit_to_unit
 
-          update_params = {
-            balance_cents: wallet.balance_cents + amount_cents,
-            credits_balance: wallet.credits_balance + credits_amount,
-            last_balance_sync_at: Time.current
-          }
+        update_params = {
+          balance_cents: wallet.balance_cents + amount_cents,
+          credits_balance: wallet.credits_balance + credits_amount,
+          last_balance_sync_at: Time.current
+        }
 
-          if reset_consumed_credits
-            update_params[:consumed_credits] = [0.0, wallet.consumed_credits - credits_amount].max
-            update_params[:consumed_amount_cents] = [0, wallet.consumed_amount_cents - amount_cents].max
-          end
-
-          wallet.update!(update_params)
-          Wallets::Balance::RefreshOngoingService.call(wallet:)
+        if reset_consumed_credits
+          update_params[:consumed_credits] = [0.0, wallet.consumed_credits - credits_amount].max
+          update_params[:consumed_amount_cents] = [0, wallet.consumed_amount_cents - amount_cents].max
         end
+
+        wallet.update!(update_params)
+        Wallets::Balance::RefreshOngoingService.call(wallet:)
 
         result.wallet = wallet
         result
-      rescue ActiveRecord::StaleObjectError
-        @retries += 1
-
-        if @retries <= MAX_RETRIES
-          sleep(0.5)
-
-          wallet.reload
-
-          retry
-        end
-
-        result.service_failure!(code: 'race_condition_error', message: '')
       end
 
       private
