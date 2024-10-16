05a4930b6bdad458c70d0c7b9c12634de7886479
Ivan Novosad <ivan@getlago.com>
2024-10-16 11:30:20 +0200

    feat(hubspot): Fix company update payload

--- app/services/integrations/aggregator/companies/payloads/hubspot.rb
+++ app/services/integrations/aggregator/companies/payloads/hubspot.rb
@@ -21,14 +21,17 @@ module Integrations
 
           def update_body
             {
-              'properties' => {
-                'name' => customer.name,
-                'domain' => customer.url,
-                'lago_customer_id' => customer.id,
-                'lago_customer_external_id' => customer.external_id,
-                'lago_billing_email' => customer.email,
-                'lago_tax_identification_number' => customer.tax_identification_number,
-                'lago_customer_link' => customer_url
+              'contactId' => integration_customer.external_customer_id,
+              'input' => {
+                'properties' => {
+                  'name' => customer.name,
+                  'domain' => customer.url,
+                  'lago_customer_id' => customer.id,
+                  'lago_customer_external_id' => customer.external_id,
+                  'lago_billing_email' => customer.email,
+                  'lago_tax_identification_number' => customer.tax_identification_number,
+                  'lago_customer_link' => customer_url
+                }
               }
             }
           end
