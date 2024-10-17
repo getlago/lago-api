dcb3e0ccc55894aca5593e46e3520ced5ed4a61f
Anna Velentsevich <annvelents@gmail.com>
2024-10-17 11:34:52 +0200

    fix linter

--- app/graphql/types/invoices/object.rb
+++ app/graphql/types/invoices/object.rb
@@ -42,8 +42,8 @@ module Types
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-      field :available_to_credit_amount_cents, GraphQL::Types::BigInt, null: false
       field :associated_active_wallet_present, Boolean, null: false
+      field :available_to_credit_amount_cents, GraphQL::Types::BigInt, null: false
       field :creditable_amount_cents, GraphQL::Types::BigInt, null: false
       field :refundable_amount_cents, GraphQL::Types::BigInt, null: false
 
