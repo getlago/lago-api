99c94c7dedf2be2b239cbf59624d94bd526f2f86
Lovro Colic <lovro.colic.lc@gmail.com>
2024-10-18 18:08:55 +0200

    fix spec

--- spec/jobs/charges/destroy_job_spec.rb
+++ spec/jobs/charges/destroy_job_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Charges::DestroyJob, type: :job do
-  let(:charge) { create(:charge) }
+  let(:charge) { create(:standard_charge) }
 
   before do
     allow(Charges::DestroyService).to receive(:call).with(charge:).and_return(BaseService::Result.new)
   end
 
   it 'calls the service' do
-    described_class.perform_now(plan:, params:)
+    described_class.perform_now(charge:)
 
     expect(Charges::DestroyService).to have_received(:call)
   end
