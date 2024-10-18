71a3f55d866c72acc4d728aa2400a1206863dadf
Ancor Cruz <hello@ancorcruz.com>
2024-10-18 14:49:04 +0100

    Add auto_dunning premium integration to organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -52,7 +52,7 @@ class Organization < ApplicationRecord
     :per_organization
   ].freeze
 
-  INTEGRATIONS = %w[netsuite okta anrok xero progressive_billing hubspot].freeze
+  INTEGRATIONS = %w[netsuite okta anrok xero progressive_billing hubspot auto_dunning].freeze
   PREMIUM_INTEGRATIONS = INTEGRATIONS - %w[anrok]
 
   enum document_numbering: DOCUMENT_NUMBERINGS
