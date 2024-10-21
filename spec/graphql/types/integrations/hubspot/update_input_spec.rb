7b1562a495f3759157c4755cd8a7b8f6b2bef88a
Ivan Novosad <ivannovosad@users.noreply.github.com>
2024-10-21 16:38:51 +0200

    Feat hubspot customers graphql (#2673)
    ## Roadmap Task
    
    👉  https://getlago.canny.io/feature-requests/p/integration-with-hubspot
    
    ## Context
    
    When creating or updating customers in Lago, we must sync them to
    Hubspot.
    Depending on if Lago customer is either company or individual, we need
    to call different services (Nango endpoints).
    
    ## Description
    
    This PR adds GraphQL types that are needed for syncing customers to
    Hubspot.

--- spec/graphql/types/integrations/hubspot/update_input_spec.rb
+++ spec/graphql/types/integrations/hubspot/update_input_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Types::Integrations::Hubspot::UpdateInput do
   it { is_expected.to accept_argument(:code).of_type('String') }
   it { is_expected.to accept_argument(:name).of_type('String') }
   it { is_expected.to accept_argument(:connection_id).of_type('String') }
-  it { is_expected.to accept_argument(:default_targeted_object).of_type('TargetedObjectsEnum') }
+  it { is_expected.to accept_argument(:default_targeted_object).of_type('HubspotTargetedObjectsEnum') }
   it { is_expected.to accept_argument(:sync_invoices).of_type('Boolean') }
   it { is_expected.to accept_argument(:sync_subscriptions).of_type('Boolean') }
 end
--- spec/graphql/types/integrations/hubspot_spec.rb
+++ spec/graphql/types/integrations/hubspot_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe Types::Integrations::Hubspot do
 
   it { is_expected.to have_field(:code).of_type('String!') }
   it { is_expected.to have_field(:connection_id).of_type('ID!') }
-  it { is_expected.to have_field(:default_targeted_object).of_type('TargetedObjectsEnum!') }
+  it { is_expected.to have_field(:default_targeted_object).of_type('HubspotTargetedObjectsEnum!') }
   it { is_expected.to have_field(:name).of_type('String!') }
+  it { is_expected.to have_field(:portal_id).of_type('String') }
 
   it { is_expected.to have_field(:sync_invoices).of_type('Boolean') }
   it { is_expected.to have_field(:sync_subscriptions).of_type('Boolean') }
