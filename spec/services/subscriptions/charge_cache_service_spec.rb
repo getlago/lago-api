b99e3e3a0cd35a154058f0334c9daa2cd4d24f7b
Vincent Pochet <vincent@getlago.com>
2024-10-21 10:20:22 +0200

    misc(usage): Scope usage caching to the charge filter level (#2678)
    ## Context
    
    Today caching for usage data is scope to the charge.
    When an event is received for a specific billable metric code will
    invalidate the cache for all charges attached to it for the customer's
    plan. The cache will leave until the end of the billing period or until
    a new event is received for the same billable metric code and
    subscription.
    This logic avoid recomputing the complete charge aggregation every time
    the customer usage is requested.
    
    This logic was fine until the introduction of charge filters.
    Aggregation for filters are done one by one for every filter, and stored
    in cache using the same charge cache key (ie: all filtered aggregations
    are grouped inside the same charge cached bucket). When an event
    matching a filter is received it invalidate the cache for all filters,
    meaning that when fetching the usage we have to resegregate it for every
    single filter even if nothing new was received for the majority of them.
    
    ## Description
    
    This PR refactors some parts of the usage computation and adds a new
    caching logic to scope the caching up to the charge filter when
    applicable and keeping the actual caching when no filters are attached
    to the charge.

---
+++ spec/services/subscriptions/charge_cache_service_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Subscriptions::ChargeCacheService, type: :service do
+  subject(:cache_service) { described_class.new(subscription:, charge:, charge_filter:) }
+
+  let(:subscription) { create(:subscription) }
+  let(:charge) { create(:standard_charge, plan: subscription.plan) }
+  let(:charge_filter) { nil }
+
+  describe '#cache_key' do
+    it 'returns the cache key' do
+      expect(cache_service.cache_key)
+        .to eq("charge-usage/#{charge.id}/#{subscription.id}/#{charge.updated_at.iso8601}")
+    end
+
+    context 'with a charge filter' do
+      let(:charge_filter) { create(:charge_filter) }
+
+      it 'returns the cache key with the charge filter' do
+        expect(cache_service.cache_key)
+          .to eq("charge-usage/#{charge.id}/#{subscription.id}/#{charge.updated_at.iso8601}/#{charge_filter.id}/#{charge_filter.updated_at.iso8601}")
+      end
+    end
+  end
+
+  describe '#expire_cache' do
+    it 'deletes the cached value' do
+      allow(Rails.cache).to receive(:delete).with(cache_service.cache_key)
+
+      cache_service.expire_cache
+
+      expect(Rails.cache).to have_received(:delete).with(cache_service.cache_key)
+    end
+  end
+end
