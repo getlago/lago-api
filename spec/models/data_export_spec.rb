1cc74407f86c608b75f390c2a962dda8880ac703
Toon Willems <m@toonwillems.be>
2024-10-24 11:23:45 +0200

    Add DataExportPart model

--- spec/models/data_export_spec.rb
+++ spec/models/data_export_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe DataExport, type: :model do
   it { is_expected.to belong_to(:organization) }
   it { is_expected.to belong_to(:membership) }
+  it { is_expected.to have_many(:data_export_parts) }
 
   it { is_expected.to validate_presence_of(:format) }
   it { is_expected.to validate_presence_of(:resource_type) }
