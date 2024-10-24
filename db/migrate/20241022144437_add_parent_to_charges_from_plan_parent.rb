3494655f4f33ef42aa0827a74adfc3db0fbfdfc1
Anna Velentsevich <annvelents@gmail.com>
2024-10-24 18:09:28 +0200

    Speed up migration

--- db/migrate/20241022144437_add_parent_to_charges_from_plan_parent.rb
+++ db/migrate/20241022144437_add_parent_to_charges_from_plan_parent.rb
@@ -1,20 +1,25 @@
 # frozen_string_literal: true
 #
 class AddParentToChargesFromPlanParent < ActiveRecord::Migration[7.1]
-  def change
+  def up
     now = Time.current
     Plan.where.not(parent_id: nil).includes(:charges, parent: :charges).find_each do |plan|
       parent = plan.parent
       next if plan.charges.empty? || parent.charges.empty?
 
       plan.charges.each do |child_charge|
-        parent_charges = parent.charges.where(charge_model: child_charge.charge_model, properties: child_charge.properties)
+        parent_charges = parent.charges.select do |charge|
+          charge.charge_model == child_charge.charge_model && charge.properties == child_charge.properties
+        end
         next if parent_charges.length != 1
 
-        child_charge.update(parent_id: parent_charges[0].id)
+        child_charge.update_columns(parent_id: parent_charges[0].id)
       end
     end
     puts '=' * 80
-    puts 'Migration took: ' + (Time.current - now).to_s
+    puts 'Migration took: ' + (Time.current - now).to_s + ' seconds'
+  end
+
+  def down
   end
 end
