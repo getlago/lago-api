b106976a5a6f08cc9f39d48749928a3756202355
Anna Velentsevich <annvelents@gmail.com>
2024-10-24 18:15:13 +0200

    fix linter

--- db/migrate/20241022144437_add_parent_to_charges_from_plan_parent.rb
+++ db/migrate/20241022144437_add_parent_to_charges_from_plan_parent.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 class AddParentToChargesFromPlanParent < ActiveRecord::Migration[7.1]
   def up
     now = Time.current
@@ -8,16 +8,18 @@ class AddParentToChargesFromPlanParent < ActiveRecord::Migration[7.1]
       next if plan.charges.empty? || parent.charges.empty?
 
       plan.charges.each do |child_charge|
+        next if child_charge.parent_id.present?
+
         parent_charges = parent.charges.select do |charge|
           charge.charge_model == child_charge.charge_model && charge.properties == child_charge.properties
         end
         next if parent_charges.length != 1
 
-        child_charge.update_columns(parent_id: parent_charges[0].id)
+        child_charge.update_columns(parent_id: parent_charges[0].id) # rubocop:disable Rails/SkipsModelValidations
       end
     end
-    puts '=' * 80
-    puts 'Migration took: ' + (Time.current - now).to_s + ' seconds'
+    Rails.logger.info('=' * 80)
+    Rails.logger.info('Migration took: ' + (Time.current - now).to_s + ' seconds')
   end
 
   def down
