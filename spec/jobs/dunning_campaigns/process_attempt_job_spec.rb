3abe8c3d21060a6df6cb6b5d4e82baecf2ab47c4
Ancor Cruz <hello@ancorcruz.com>
2024-10-29 13:41:53 +0000

    Add DunningCampaigns ProcessAttemptJob
    to kickoff attempt execution for each matching customer

---
+++ spec/jobs/dunning_campaigns/process_attempt_job_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DunningCampaigns::ProcessAttemptJob, type: :job do
+  let(:result) { BaseService::Result.new }
+  let(:customer) { build :customer }
+  let(:dunning_campaign_threshold) { build :dunning_campaign_threshold }
+
+  before do
+    allow(DunningCampaigns::ProcessAttemptService)
+      .to receive(:call)
+      .and_return(result)
+  end
+
+  it "calls DunningCampaigns::ProcessAttemptService" do
+    described_class.perform_now(customer:, dunning_campaign_threshold:)
+
+    expect(DunningCampaigns::ProcessAttemptService)
+      .to have_received(:call)
+      .with(customer:, dunning_campaign_threshold:)
+  end
+end
