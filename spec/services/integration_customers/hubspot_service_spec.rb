cfa83f7e2f287b028dad707eb0e3b4c683ad1a90
Miguel Pinto <darkymiguel@gmail.com>
2024-10-31 00:17:30 +0000

    feat: added specs

--- spec/services/integration_customers/hubspot_service_spec.rb
+++ spec/services/integration_customers/hubspot_service_spec.rb
@@ -3,13 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe IntegrationCustomers::HubspotService, type: :service do
-  let(:integration) { create(:xero_integration, organization:) }
+  let(:integration) { create(:hubspot_integration, organization:) }
   let(:organization) { membership.organization }
   let(:membership) { create(:membership) }
   let(:customer) { create(:customer, organization:, customer_type: 'individual') }
+  let(:targeted_object) { 'contacts' }
 
   describe '#create' do
-    subject(:service_call) { described_class.new(integration:, customer:, subsidiary_id: nil, targeted_object: 'contacts').create }
+    subject(:service_call) { described_class.new(integration:, customer:, subsidiary_id: nil, targeted_object:).create }
 
     let(:contact_id) { SecureRandom.uuid }
     let(:create_result) do
@@ -45,6 +46,53 @@ RSpec.describe IntegrationCustomers::HubspotService, type: :service do
 
     it 'creates integration customer' do
       expect { service_call }.to change(IntegrationCustomers::HubspotCustomer, :count).by(1)
+    end
+
+    context "with targeted_object" do
+      let(:customer) { create(:customer, organization:, customer_type:) }
+      let(:customer_type) { "individual" }
+
+      context 'when params[:targeted_object] is specified as "contacts"' do
+        let(:targeted_object) { 'contacts' }
+
+        it 'uses Integrations::Aggregator::Contacts::CreateService' do
+          allow(Integrations::Aggregator::Contacts::CreateService).to receive(:call).and_return(create_result)
+          service_call
+          expect(Integrations::Aggregator::Contacts::CreateService).to have_received(:call)
+        end
+      end
+
+      context 'when params[:targeted_object] is specified as "companies"' do
+        let(:targeted_object) { 'companies' }
+
+        it 'uses Integrations::Aggregator::Companies::CreateService' do
+          allow(Integrations::Aggregator::Companies::CreateService).to receive(:call).and_return(create_result)
+          service_call
+          expect(Integrations::Aggregator::Companies::CreateService).to have_received(:call)
+        end
+      end
+
+      context 'when params[:targeted_object] is not specified and customer is an individual' do
+        let(:targeted_object) { nil }
+        let(:customer) { create(:customer, organization:, customer_type: 'individual') }
+
+        it 'defaults to Integrations::Aggregator::Contacts::CreateService' do
+          allow(Integrations::Aggregator::Contacts::CreateService).to receive(:call).and_return(create_result)
+          service_call
+          expect(Integrations::Aggregator::Contacts::CreateService).to have_received(:call)
+        end
+      end
+
+      context 'when params[:targeted_object] is not specified and customer is a company' do
+        let(:targeted_object) { nil }
+        let(:customer) { create(:customer, organization:, customer_type: 'company') }
+
+        it 'defaults to Integrations::Aggregator::Companies::CreateService' do
+          allow(Integrations::Aggregator::Companies::CreateService).to receive(:call).and_return(create_result)
+          service_call
+          expect(Integrations::Aggregator::Companies::CreateService).to have_received(:call)
+        end
+      end
     end
   end
 end
