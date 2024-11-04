c1e3b2495ca315110be8cc5c7ff092bb8564430a
Toon Willems <m@toonwillems.be>
2024-11-04 10:31:27 +0100

    feat(InvoiceError) - Improve create_for handling  (#2770)
    ## description
    
    If an invoice already has an invoice error, and still fails to generate.
    We'd have a duplicate primary key violation.
    we'd rather simply update the existing error object.

--- app/models/invoice_error.rb
+++ app/models/invoice_error.rb
@@ -4,12 +4,14 @@ class InvoiceError < ApplicationRecord
   # NOTE! Invoice errors will have the same id as the invoice they belong to.
   def self.create_for(invoice:, error:)
     return unless invoice
-
-    create(id: invoice.id,
+    instance = find_or_create_by(id: invoice.id)
+    instance.update(
       backtrace: error.backtrace,
       error: error.inspect.to_json,
       invoice: invoice.to_json(except: :file),
-      subscriptions: invoice.subscriptions.to_json)
+      subscriptions: invoice.subscriptions.to_json
+    )
+    instance
   end
 end
 
