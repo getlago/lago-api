2ba3f1d3e43447ba09cfa83534cd0e15f2744346
jdenquin <jdenquin@gmail.com>
2024-11-05 16:16:20 +0100

    misc(cors): Support new LAGO_DOMAIN

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,6 +14,8 @@ Rails.application.config.middleware.insert_before(0, Rack::Cors) do
       end
 
       origins frontend_origin
+    elsif ENV.key?('LAGO_DOMAIN')
+      origins ENV['LAGO_DOMAIN']
     elsif Rails.env.development?
       origins 'app.lago.dev', 'api', 'lago.ngrok.dev'
     end
