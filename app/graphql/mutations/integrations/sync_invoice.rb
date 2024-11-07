7a0fec2938d7c5d9e59f12ca2df8234ff07523b1
Miguel Pinto <darkymiguel@gmail.com>
2024-11-07 11:43:05 +0000

    feat: added webhook

--- app/graphql/mutations/integrations/sync_invoice.rb
+++ app/graphql/mutations/integrations/sync_invoice.rb
@@ -18,7 +18,7 @@ module Mutations
       def resolve(**args)
         invoice = current_organization.invoices.find_by(id: args[:invoice_id])
 
-        result = ::Integrations::Aggregator::Invoices::CreateService.call_async(invoice:)
+        result = ::Integrations::Aggregator::Invoices::CreateService.call_async(invoice:, send_webhook: true)
         result.success? ? result.invoice_id : result_error(result)
         result
       end
