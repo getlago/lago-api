eeb68dce06c66e9d8d3b9f9dcb45ccfc7cca61c7
Miguel Pinto <darkymiguel@gmail.com>
2024-11-07 12:34:11 +0000

    feat: add graphql mutation

---
+++ app/graphql/mutations/integrations/salesforce/sync_invoice.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Integrations
+    module Salesforce
+      class SyncInvoice < BaseMutation
+        include AuthenticableApiUser
+        include RequiredOrganization
+
+        REQUIRED_PERMISSION = 'organization:integrations:update'
+
+        graphql_name 'SyncSalesforceInvoice'
+        description 'Sync Salesforce invoice'
+
+        input_object_class Types::Integrations::SyncInvoiceInput
+
+        field :invoice_id, ID, null: true
+
+        def resolve(**args)
+          SendWebhookJob.perform_later('invoice.resynced', invoice)
+        end
+      end
+    end
+  end
+end
