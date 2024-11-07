9d3e95cf57876142855353c1a4773d64197d1821
Toon Willems <m@toonwillems.be>
2024-11-07 10:39:10 +0100

    Bug(DataExport) - update DEFAULT_BATCH_SIZE of ExportResourcesJob

--- app/jobs/data_exports/export_resources_job.rb
+++ app/jobs/data_exports/export_resources_job.rb
@@ -4,7 +4,7 @@ module DataExports
   class ExportResourcesJob < ApplicationJob
     queue_as :default
 
-    DEFAULT_BATCH_SIZE = 100
+    DEFAULT_BATCH_SIZE = 20
 
     def perform(data_export, batch_size: DEFAULT_BATCH_SIZE)
       ExportResourcesService.call(data_export:, batch_size:).raise_if_error!
