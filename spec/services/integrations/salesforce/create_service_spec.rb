9df5d0926b1d8a39ed986dcb07328a58bb6b61a6
Ivan Novosad <ivannovosad@users.noreply.github.com>
2024-11-08 10:17:44 +0100

    Feat salesforce services (#2795)
    ## Roadmap Task
    
    👉
    https://getlago.canny.io/feature-requests/p/sync-lago-customers-with-salesforce
    
    ## Context
    
    We need to be able to create a Salesforce integration in Lago and sync
    Lago customers to Salesforce using this integration details.
    
    ## Description
    
    This PR adds `create` and `update` services to store the integration.
    They will be used by the GraphQL mutations.

---
+++ spec/services/integrations/salesforce/create_service_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Integrations::Salesforce::CreateService, type: :service do
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+
+  describe '#call' do
+    subject(:service_call) { described_class.call(params: create_args) }
+
+    let(:name) { 'Salesforce 1' }
+
+    let(:create_args) do
+      {
+        name:,
+        code: 'salesforce',
+        organization_id: organization.id,
+        instance_id: 'Instance1'
+      }
+    end
+
+    context 'without premium license' do
+      it 'does not create an integration' do
+        expect { service_call }.not_to change(Integrations::SalesforceIntegration, :count)
+      end
+
+      it 'returns an error' do
+        result = service_call
+
+        aggregate_failures do
+          expect(result).not_to be_success
+          expect(result.error).to be_a(BaseService::MethodNotAllowedFailure)
+        end
+      end
+    end
+
+    context 'with premium license' do
+      context 'with salesforce premium integration not present' do
+        it 'returns an error' do
+          result = service_call
+
+          aggregate_failures do
+            expect(result).not_to be_success
+            expect(result.error).to be_a(BaseService::MethodNotAllowedFailure)
+          end
+        end
+      end
+
+      context 'with salesforce premium integration present' do
+        before { organization.update!(premium_integrations: ['salesforce']) }
+
+        context 'without validation errors' do
+          it 'creates an integration' do
+            expect { service_call }.to change(Integrations::SalesforceIntegration, :count).by(1)
+
+            integration = Integrations::SalesforceIntegration.order(:created_at).last
+            expect(integration.instance_id).to eq('Instance1')
+          end
+        end
+
+        context 'with validation error' do
+          let(:name) { nil }
+
+          it 'returns an error' do
+            result = service_call
+
+            aggregate_failures do
+              expect(result).not_to be_success
+              expect(result.error).to be_a(BaseService::ValidationFailure)
+              expect(result.error.messages[:name]).to eq(['value_is_mandatory'])
+            end
+          end
+        end
+      end
+    end
+  end
+end
