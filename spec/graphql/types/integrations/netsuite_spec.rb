5fd4445cd4a8d0a48dc7d6bcae51460575e2d64d
Miguel Pinto <darkymiguel@gmail.com>
2024-11-12 18:47:22 +0000

    feat: fixed specs

--- spec/graphql/types/integrations/netsuite_spec.rb
+++ spec/graphql/types/integrations/netsuite_spec.rb
@@ -19,5 +19,5 @@ RSpec.describe Types::Integrations::Netsuite do
   it { is_expected.to have_field(:sync_credit_notes).of_type('Boolean') }
   it { is_expected.to have_field(:sync_invoices).of_type('Boolean') }
   it { is_expected.to have_field(:sync_payments).of_type('Boolean') }
-  it { is_expected.to have_field(:sync_sales_orders).of_type('Boolean') }
+
 end
