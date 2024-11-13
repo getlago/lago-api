7f10dbaeb39ac94d10243edd3884d5cf562a40ba
Toon Willems <m@toonwillems.be>
2024-11-13 09:22:52 +0100

    fix BillableMetrics::DeleteEventsJob

--- spec/jobs/billable_metrics/delete_events_job_spec.rb
+++ spec/jobs/billable_metrics/delete_events_job_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe BillableMetrics::DeleteEventsJob, type: :job, transaction: false 
       expect(not_impacted_event.reload.deleted_at).to be_nil
     end
   end
+
+  it 'deletes new-style external_subscription based events' do
+    create(:standard_charge, plan: subscription.plan, billable_metric:)
+    not_impacted_event = create(:event, external_subscription_id: SecureRandom.uuid, organization_id: billable_metric.organization_id)
+    event = create(:event, code: billable_metric.code, external_subscription_id: subscription.external_id, organization_id: billable_metric.organization_id)
+
+    freeze_time do
+      expect { described_class.perform_now(billable_metric) }
+        .to change { event.reload.deleted_at }.from(nil).to(Time.current)
+
+      expect(not_impacted_event.reload.deleted_at).to be_nil
+    end
+  end
 end
