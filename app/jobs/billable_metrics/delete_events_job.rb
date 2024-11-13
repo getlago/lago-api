aac08953923feda70c8ad68f7032f2179444cd49
Toon Willems <m@toonwillems.be>
2024-11-13 09:56:14 +0100

    bug(BillableMetric) - BillableMetrics::DeleteEventsJob remove new-style events as well (#2810)
    ## Description
    
    when switching events over to use `external_subscription_id` we forgot
    to adapt this job to also remove those events.
    I decided to also keep the old query in there as it might be that a very
    old billable metric is removed at some point in the future.

--- app/jobs/billable_metrics/delete_events_job.rb
+++ app/jobs/billable_metrics/delete_events_job.rb
@@ -9,11 +9,21 @@ module BillableMetrics
 
       deleted_at = Time.current
 
+      # Delete events having an old-style `subscription_id`
       Event.where(
         code: metric.code,
         subscription_id: Charge.with_discarded
           .where(billable_metric_id: metric.id)
           .joins(plan: :subscriptions).pluck('subscriptions.id')
+      ).update_all(deleted_at:) # rubocop:disable Rails/SkipsModelValidations
+
+      # Delete events using the new `external_subscription_id`
+      Event.where(
+        organization_id: metric.organization.id,
+        code: metric.code,
+        external_subscription_id: Charge.with_discarded
+          .where(billable_metric_id: metric.id)
+          .joins(plan: :subscriptions).pluck('subscriptions.external_id')
       ).update_all(deleted_at:) # rubocop:disable Rails/SkipsModelValidations
     end
   end
