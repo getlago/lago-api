cdeac9a468fd5b874e417f843aed067a6091c395
Ancor Cruz <hello@ancorcruz.com>
2024-11-19 11:44:53 +0000

    feat(dunning): Add deleted_at to dunning campaign threshold (#2831)
    ## Roadmap Task
    
    👉 https://getlago.canny.io/feature-requests/p/set-up-payment-retry-logic
    
    👉
    https://getlago.canny.io/feature-requests/p/send-reminders-for-overdue-invoices
    
     ## Context
    
    We want to automate dunning process so that our users don't have to look
    at each customer to maximize their chances of being paid retrying
    payments of overdue balances and sending email reminders.
    
    We are extending dunning campaigns management to edit and delete
    campaigns.
    
     ## Description
    
    This change adds capabilities to soft delete dunning campaign thresholds

--- app/models/dunning_campaign_threshold.rb
+++ app/models/dunning_campaign_threshold.rb
@@ -3,12 +3,16 @@
 class DunningCampaignThreshold < ApplicationRecord
   include Currencies
   include PaperTrailTraceable
+  include Discard::Model
+  self.discard_column = :deleted_at
 
   belongs_to :dunning_campaign
 
   validates :amount_cents, numericality: {greater_than_or_equal_to: 0}
   validates :currency, inclusion: {in: currency_list}
-  validates :currency, uniqueness: {scope: :dunning_campaign_id}
+  validates :currency, uniqueness: {scope: :dunning_campaign_id}, unless: :deleted_at
+
+  default_scope -> { kept }
 end
 
 # == Schema Information
@@ -18,13 +22,15 @@ end
 #  id                  :uuid             not null, primary key
 #  amount_cents        :bigint           not null
 #  currency            :string           not null
+#  deleted_at          :datetime
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #  dunning_campaign_id :uuid             not null
 #
 # Indexes
 #
-#  idx_on_dunning_campaign_id_currency_fbf233b2ae            (dunning_campaign_id,currency) UNIQUE
+#  idx_on_dunning_campaign_id_currency_fbf233b2ae            (dunning_campaign_id,currency) UNIQUE WHERE (deleted_at IS NULL)
+#  index_dunning_campaign_thresholds_on_deleted_at           (deleted_at)
 #  index_dunning_campaign_thresholds_on_dunning_campaign_id  (dunning_campaign_id)
 #
 # Foreign Keys
