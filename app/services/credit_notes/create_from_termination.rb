4bebf8b0e90b89e6ad65189403f745647bf99e62
Toon Willems <m@toonwillems.be>
2024-11-20 18:39:02 +0100

    Use creditable_amount_cents rather than duplicating the logic

--- app/services/credit_notes/create_from_termination.rb
+++ app/services/credit_notes/create_from_termination.rb
@@ -19,11 +19,8 @@ module CreditNotes
       # NOTE: In some cases, if the fee was already prorated (in case of multiple upgrade) the amount
       #       could be greater than the last subscription fee amount.
       #       In that case, we have to use the last subscription fee amount
-      amount = last_subscription_fee.amount_cents if amount > last_subscription_fee.amount_cents
+      amount = last_subscription_fee.creditable_amount_cents if amount > last_subscription_fee.creditable_amount_cents
 
-      # NOTE: if credit notes were already issued on the fee,
-      #       we have to deduct them from the prorated amount
-      amount -= last_subscription_fee.credit_note_items.sum(:amount_cents)
       return result unless amount.positive?
 
       CreditNotes::CreateService.new(
