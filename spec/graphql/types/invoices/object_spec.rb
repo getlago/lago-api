8f28fa1d9c3e8b3b3ff229d288f2aa85e8a73a53
Miguel Pinto <darkymiguel@gmail.com>
2024-11-21 11:29:14 +0000

    fix: fix namespace issue

--- spec/graphql/types/invoices/object_spec.rb
+++ spec/graphql/types/invoices/object_spec.rb
@@ -52,8 +52,10 @@ RSpec.describe Types::Invoices::Object do
   it { is_expected.to have_field(:invoice_subscriptions).of_type('[InvoiceSubscription!]') }
   it { is_expected.to have_field(:subscriptions).of_type('[Subscription!]') }
 
-  it { is_expected.to have_field(:external_crm_integration_id).of_type('String') }
+  it { is_expected.to have_field(:external_hubspot_integration_id).of_type('String') }
+  it { is_expected.to have_field(:external_salesforce_integration_id).of_type('String') }
   it { is_expected.to have_field(:external_integration_id).of_type('String') }
-  it { is_expected.to have_field(:integration_crm_syncable).of_type('Boolean!') }
+  it { is_expected.to have_field(:integration_hubspot_syncable).of_type('Boolean!') }
+  it { is_expected.to have_field(:integration_salesforce_syncable).of_type('Boolean!') }
   it { is_expected.to have_field(:integration_syncable).of_type('Boolean!') }
 end
