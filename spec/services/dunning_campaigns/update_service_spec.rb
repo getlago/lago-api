df1da0e207d1f461558ef70474c6b22bcb047b45
Ancor Cruz <hello@ancorcruz.com>
2024-11-22 13:19:32 +0000

    feat(dunning): extend DunningCampaigns::UpdateService update all fields (#2836)
    ## Roadmap
    
    👉 https://getlago.canny.io/feature-requests/p/set-up-payment-retry-logic
    
    👉
    https://getlago.canny.io/feature-requests/p/send-reminders-for-overdue-invoices
    
     ## Context
    
    We want to automate dunning process so that our users don't have to look
    at each customer to maximize their chances of being paid retrying
    payments of overdue balances and sending email reminders.
    
    We are extending dunning campaigns management to edit and delete
    campaigns.
    
     ## Description
    
    This change enables UpdateService to update dunning campaign data and
    its thresholds.

--- spec/services/dunning_campaigns/update_service_spec.rb
+++ spec/services/dunning_campaigns/update_service_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe DunningCampaigns::UpdateService, type: :service do
 
   let(:params) { {applied_to_organization: false} }
 
-  describe "#call" do
+  describe "#call", :aggregate_failures do
     subject(:result) { update_service.call }
 
     before do
@@ -21,7 +21,7 @@ RSpec.describe DunningCampaigns::UpdateService, type: :service do
     end
 
     context "when lago freemium" do
-      it 'returns an error', :aggregate_failures do
+      it 'returns an error' do
         expect(result).not_to be_success
         expect(result.error).to be_a(BaseService::ForbiddenFailure)
       end
@@ -35,7 +35,7 @@ RSpec.describe DunningCampaigns::UpdateService, type: :service do
       around { |test| lago_premium!(&test) }
 
       context "when no auto_dunning premium integration" do
-        it 'returns an error', :aggregate_failures do
+        it 'returns an error' do
           expect(result).not_to be_success
           expect(result.error).to be_a(BaseService::ForbiddenFailure)
         end
@@ -48,6 +48,65 @@ RSpec.describe DunningCampaigns::UpdateService, type: :service do
       context "when auto_dunning premium integration" do
         let(:organization) do
           create(:organization, premium_integrations: ["auto_dunning"])
+        end
+
+        let(:dunning_campaign_threshold) do
+          create(:dunning_campaign_threshold, dunning_campaign:)
+        end
+
+        let(:params) do
+          {
+            name: "Updated Dunning Campaign",
+            code: "updated-dunning-campaign",
+            days_between_attempts: Faker::Number.number(digits: 2),
+            max_attempts: Faker::Number.number(digits: 2),
+            description: "Updated Dunning Campaign Description",
+            thresholds: thresholds_input
+          }
+        end
+
+        let(:thresholds_input) do
+          [
+            {
+              id: dunning_campaign_threshold.id,
+              amount_cents: 999_99,
+              currency: "GBP"
+            },
+            {
+              amount_cents: 5_55,
+              currency: "CHF"
+            }
+          ]
+        end
+
+        it "updates the dunning campaign" do
+          expect(result).to be_success
+          expect(result.dunning_campaign.name).to eq(params[:name])
+          expect(result.dunning_campaign.code).to eq(params[:code])
+          expect(result.dunning_campaign.days_between_attempts).to eq(params[:days_between_attempts])
+          expect(result.dunning_campaign.max_attempts).to eq(params[:max_attempts])
+          expect(result.dunning_campaign.description).to eq(params[:description])
+
+          expect(result.dunning_campaign.thresholds.count).to eq(2)
+          expect(result.dunning_campaign.thresholds.find(dunning_campaign_threshold.id))
+            .to have_attributes({amount_cents: 999_99, currency: "GBP"})
+          expect(result.dunning_campaign.thresholds.where.not(id: dunning_campaign_threshold.id).first)
+            .to have_attributes({amount_cents: 5_55, currency: "CHF"})
+        end
+
+        context "when the input does not include a thresholds" do
+          let(:dunning_campaign_threshold_to_be_deleted) do
+            create(:dunning_campaign_threshold, dunning_campaign:, currency: "EUR")
+          end
+
+          before { dunning_campaign_threshold_to_be_deleted }
+
+          it "deletes the thresholds not in the input" do
+            expect(result).to be_success
+            expect(result.dunning_campaign.thresholds.count).to eq(2)
+            expect(result.dunning_campaign.thresholds.find_by(id: dunning_campaign_threshold_to_be_deleted.id)).to be_nil
+            expect(dunning_campaign_threshold_to_be_deleted.reload).to be_discarded
+          end
         end
 
         context "with applied_to_organization false" do
@@ -98,8 +157,9 @@ RSpec.describe DunningCampaigns::UpdateService, type: :service do
 
         context "with no dunning campaign record" do
           let(:dunning_campaign) { nil }
+          let(:thresholds_input) { nil }
 
-          it "returns a failure", :aggregate_failures do
+          it "returns a failure" do
             expect(result).not_to be_success
             expect(result.error).to be_a(BaseService::NotFoundFailure)
             expect(result.error.message).to eq("dunning_campaign_not_found")
