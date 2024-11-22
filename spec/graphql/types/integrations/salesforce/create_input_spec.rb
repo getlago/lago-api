22aba21c8c250f35ddbf035310b09df1bd434365
Miguel Pinto <darkymiguel@gmail.com>
2024-11-22 11:53:10 +0000

    fix: namespace changes on subscription

--- spec/graphql/types/integrations/salesforce/create_input_spec.rb
+++ spec/graphql/types/integrations/salesforce/create_input_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Types::Integrations::Salesforce::CreateInput do
   subject { described_class }
 
+  it { is_expected.to accept_argument(:code).of_type('String!') }
   it { is_expected.to accept_argument(:name).of_type('String!') }
   it { is_expected.to accept_argument(:instance_id).of_type('String!') }
 end
