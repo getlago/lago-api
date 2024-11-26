4e91c83b096920dda937120b4e6683e9c5ef4838
Ancor Cruz <hello@ancorcruz.com>
2024-11-26 19:44:49 +0000

    Stripe paymnent request update payment status...
    reset dunning campaing status counters when payment is successful

--- app/services/payment_requests/payments/stripe_service.rb
+++ app/services/payment_requests/payments/stripe_service.rb
@@ -114,6 +114,7 @@ module PaymentRequests
         payment_status = payable_payment_status(status)
         update_payable_payment_status(payment_status:, processing:)
         update_invoices_payment_status(payment_status:, processing:)
+        reset_customer_dunning_campaign_status(payment_status)
 
         PaymentRequestMailer.with(payment_request: payment.payable).requested.deliver_later if result.payable.payment_failed?
 
@@ -337,6 +338,17 @@ module PaymentRequests
 
       def payment_status_succeeded?(payment_status)
         payment_status.to_sym == :succeeded
+      end
+
+      def reset_customer_dunning_campaign_status(payment_status)
+        return unless payment_status_succeeded?(payment_status)
+        return unless payable.try(:dunning_campaign)
+
+        customer.update!(
+          dunning_campaign_completed: false,
+          last_dunning_campaign_attempt: 0,
+          last_dunning_campaign_attempt_at: nil
+        )
       end
     end
   end
