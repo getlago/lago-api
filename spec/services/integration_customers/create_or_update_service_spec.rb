627ece668ba04d8cc7b8fd0d3ad2f78103a0d21c
Miguel Pinto <darkymiguel@gmail.com>
2024-11-27 12:15:31 +0000

    fix: added specs

--- spec/services/integration_customers/create_or_update_service_spec.rb
+++ spec/services/integration_customers/create_or_update_service_spec.rb
@@ -267,7 +267,7 @@ RSpec.describe IntegrationCustomers::CreateOrUpdateService, type: :service do
         end
 
         it 'processes the job immediately' do
-          aggregate_failures 'job processing and enqueuing' do
+          aggregate_failures do
             expect { service_call }.to change(IntegrationCustomers::BaseCustomer, :count).by(1)
             expect { service_call }.not_to have_enqueued_job(IntegrationCustomers::CreateJob)
           end
