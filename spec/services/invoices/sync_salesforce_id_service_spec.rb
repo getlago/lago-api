c43afdf0e61c5a2c65d24d226f03a65eaa8bf792
Miguel Pinto <darkymiguel@gmail.com>
2024-11-28 13:46:48 +0000

    feat: add specs

--- spec/services/invoices/sync_salesforce_id_service_spec.rb
+++ spec/services/invoices/sync_salesforce_id_service_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Invoices::SyncSalesforceIdService, type: :service do
-  subject(:sync_salesforce_id_service) { described_class.new(invoice:, params:) }
+  subject(:service_call) { sync_salesforce_id_service.call }
 
+  let(:sync_salesforce_id_service) { described_class.new(invoice:, params:) }
   let(:organization) { create(:organization) }
-  let(:customer) { create(:customer, organization:) }
-  let(:integration) { create(:salesforce_integration, organization:) }
+  let(:invoice) { create(:invoice, organization:) }
   let(:params) { {} }
 
   describe '#call' do
@@ -15,7 +15,7 @@ RSpec.describe Invoices::SyncSalesforceIdService, type: :service do
       let(:invoice) { nil }
 
       it 'returns an error' do
-        result = sync_salesforce_id_service.call
+        result = service_call
 
         expect(result).not_to be_success
         expect(result.error.error_code).to eq('invoice_not_found')
@@ -23,11 +23,8 @@ RSpec.describe Invoices::SyncSalesforceIdService, type: :service do
     end
 
     context 'when the integration is nil' do
-      let(:invoice) { create(:invoice, organization:) }
-      #let!(:integration) { create(:salesforce_integration, organization:) }
-
       it 'returns an error' do
-        result = sync_salesforce_id_service.call
+        result = service_call
 
         expect(result).not_to be_success
         expect(result.error.error_code).to eq('integration_not_found')
@@ -35,7 +32,26 @@ RSpec.describe Invoices::SyncSalesforceIdService, type: :service do
     end
 
     context 'when the integration resource does not exist' do
-      xit 'creates a new integration resource' do
+      let(:integration) { create(:salesforce_integration, organization:) }
+      let(:params) do
+        {
+          integration_code: integration.code,
+          external_id: '1234'
+        }
+      end
+
+      it 'creates a new integration resource' do
+        expect { service_call }.to change(IntegrationResource, :count).by(1)
+
+        result = service_call
+        expect(result).to be_success
+        expect(result.invoice).to eq(invoice)
+
+        integration_resource = IntegrationResource.last
+        expect(integration_resource.integration).to eq(integration)
+        expect(integration_resource.external_id).to eq(params[:external_id])
+        expect(integration_resource.syncable).to eq(invoice)
+        expect(integration_resource.resource_type).to eq('invoice')
       end
     end
   end
