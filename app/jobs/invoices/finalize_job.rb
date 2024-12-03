0cbf43f51d69aa42ea4636d8ed77a17b046ffdb0
LovroColic <lovro.colic.lc@gmail.com>
2024-12-03 16:15:17 +0100

    fix: update applied prepaid credits service exit case (#2901)
    ## Context
    
    Prepaid credits should be applied on invoice only once. This condition
    should be basic check in the depending service.
    
    ## Description
    
    Currently we are just returning result instead of proper error for the
    described case, which leads to errors that are hard to track

--- app/jobs/invoices/finalize_job.rb
+++ app/jobs/invoices/finalize_job.rb
@@ -4,6 +4,8 @@ module Invoices
   class FinalizeJob < ApplicationJob
     queue_as 'invoices'
 
+    unique :until_executed, on_conflict: :log
+
     retry_on Sequenced::SequenceError, wait: :polynomially_longer
 
     def perform(invoice)
