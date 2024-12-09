1a74ce905570be2add2784e171d2d5f6db1416a4
Anna Velentsevich <annvelents@gmail.com>
2024-12-09 14:44:34 +0100

    add invoice_custom_section update mutation

---
+++ app/graphql/mutations/invoice_custom_sections/update.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Mutations
+  module InvoiceCustomSections
+    class Update < BaseMutation
+      include AuthenticableApiUser
+      include RequiredOrganization
+
+      REQUIRED_PERMISSION = 'invoice_custom_sections:update'
+
+      graphql_name 'UpdateInvoiceCustomSection'
+      description 'Updates an InvoiceCustomSection'
+
+      input_object_class Types::InvoiceCustomSections::UpdateInput
+
+      type Types::InvoiceCustomSections::Object
+
+      def resolve(**args)
+        selected = args.delete(:selected) || false
+        invoice_custom_section = ::InvoiceCustomSection.find(args.delete(:id))
+        result = ::InvoiceCustomSections::UpdateService.call(
+          invoice_custom_section: , update_params: args.to_h, selected: selected
+        )
+
+        result.success? ? result.invoice_custom_section : result_error(result)
+      end
+    end
+  end
+end
---
+++ spec/graphql/types/invoice_custom_sections/update_input_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Types::InvoiceCustomSections::CreateInput do
+  subject { described_class }
+
+  it { is_expected.to accept_argument(:id).of_type('ID!') }
+
+  it { is_expected.to accept_argument(:description).of_type('String') }
+  it { is_expected.to accept_argument(:details).of_type('String') }
+  it { is_expected.to accept_argument(:display_name).of_type('String') }
+  it { is_expected.to accept_argument(:name).of_type('String!') }
+  it { is_expected.to accept_argument(:selected).of_type('Boolean') }
+end
