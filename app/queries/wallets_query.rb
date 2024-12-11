90d2f95cf3d242b15bd9125981d0fd1a3685635f
Ancor Cruz <hello@ancorcruz.com>
2024-12-11 08:36:49 +0000

    Ensure consistent order for Wallets API endpoing response
    add wallets query object with filter validation

---
+++ app/queries/wallets_query.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class WalletsQuery < BaseQuery
+  def call
+    validate_filters
+    return result if result.error.present?
+
+    wallets = base_scope
+    wallets = paginate(wallets)
+    wallets = apply_consistent_ordering(wallets)
+
+    wallets = with_external_customer_id(wallets) if filters.external_customer_id
+
+    result.wallets = wallets
+    result
+  end
+
+  private
+
+  def base_scope
+    organization.wallets
+  end
+
+  def with_external_customer_id(scope)
+    scope.joins(:customer).where(customers: {external_id: filters.external_customer_id})
+  end
+
+  def validate_filters
+    if filters.to_h.key? :external_customer_id
+      result.not_found_failure!(resource: "customer") unless customer_exists?
+    end
+  end
+
+  def customer_exists?
+    organization.customers.exists?(external_id: filters.external_customer_id)
+  end
+end
