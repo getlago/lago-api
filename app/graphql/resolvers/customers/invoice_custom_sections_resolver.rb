8d66ef18b9daf2e579abef6a539caa4721dcbb02
Anna Velentsevich <annvelents@gmail.com>
2024-12-11 18:13:11 +0100

    WIP adding customer::InvoiceCustomSectionsResolver

--- app/graphql/resolvers/customers/invoice_custom_sections_resolver.rb
+++ app/graphql/resolvers/customers/invoice_custom_sections_resolver.rb
@@ -3,6 +3,20 @@
 module Resolvers
   module Customers
     class InvoiceCustomSectionsResolver < Resolvers::BaseResolver
+      description 'Query selected invoice_custom_sections of a customer'
+
+      argument :limit, Integer, required: false
+      argument :page, Integer, required: false
+      argument :customer_id, type: ID, required: true
+
+      type Types::InvoiceCustomSections::Object.collection_type, null: true
+      def resolve(customer_id:, page: nil, limit: nil)
+        customer = current_organization.customers.find(customer_id)
+        customer.applicable_invoice_custom_sections.page(page).per(limit)
+      rescue ActiveRecord::RecordNotFound
+        not_found_error(resource: 'invoice')
+      end
+
     end
   end
 end
