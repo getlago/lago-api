45264f2b2689d8569e6f5987e79dac026469500f
Anna Velentsevich <annvelents@gmail.com>
2024-12-11 11:28:55 +0100

    add single invoice_custom_section resolver

--- spec/graphql/mutations/invoice_custom_sections/destroy_spec.rb
+++ spec/graphql/mutations/invoice_custom_sections/destroy_spec.rb
@@ -3,14 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Mutations::InvoiceCustomSections::Destroy, type: :graphql do
-  let(:sbj) {
-    execute_graphql(
-      current_user: membership.user,
-      current_organization: membership.organization,
-      query: mutation,
-      variables: {input: {id: invoice_custom_section.id}}
-    )
-  }
   let(:membership) { create(:membership) }
   let(:organization) { membership.organization }
   let(:invoice_custom_section) { create(:invoice_custom_section, organization:) }
@@ -25,7 +17,14 @@ RSpec.describe Mutations::InvoiceCustomSections::Destroy, type: :graphql do
 
   before { invoice_custom_section }
 
-  it 'destroys the invoice_custom_section' do
-    expect { sbj }.to change(InvoiceCustomSection, :count).by(-1)
+  it 'destroys am invoice_custom_section' do
+    expect do
+      execute_graphql(
+        current_user: membership.user,
+        current_organization: organization,
+        query: mutation,
+        variables: {input: {id: invoice_custom_section.id}}
+      )
+    end.to change(InvoiceCustomSection, :count).by(-1)
   end
 end
