c94eb7a8d16e5245a14c1c767a813ab0ecd1294b
Toon Willems <m@toonwillems.be>
2024-12-11 13:54:00 +0100

    set trace host

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -4,6 +4,8 @@ if ENV["LAGO_DATADOG_ENABLED"] == "true"
 
   require 'datadog/auto_instrument'
   Datadog.configure do |c|
+    c.agent.host = 'datadog-agent.datadog.svc.cluster.local'
+    c.agent.port = 8126
   end
 
 end
