475c39fa66dc02b895e2e4f4fb3fb037cb83b00c
Anna Velentsevich <annvelents@gmail.com>
2024-12-13 15:08:38 +0100

    Add InvoiceCustomSections::UpdateService (#2924)
    ## Context
    
    We need services to update invoice_custom_sections
    
    ## Description
    
    - Added service to update invoice_custom_section
    - Added service to deselect invoice_custom_section

---
+++ spec/services/invoice_custom_sections/update_service_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe InvoiceCustomSections::UpdateService do
+  subject(:service_result) { described_class.call(invoice_custom_section:, update_params:, selected:) }
+
+  let(:organization) { create(:organization) }
+  let(:invoice_custom_section) { create(:invoice_custom_section, organization: organization) }
+  let(:update_params) { {name: 'Updated Name'} }
+  let(:selected) { true }
+
+  before do
+    allow(InvoiceCustomSections::SelectService).to receive(:call).and_call_original
+    allow(InvoiceCustomSections::DeselectService).to receive(:call).and_call_original
+  end
+
+  describe '#call' do
+    context 'when update is successful' do
+      it 'updates the invoice custom section' do
+        result = service_result
+
+        expect(result).to be_success
+        expect(result.invoice_custom_section.name).to eq('Updated Name')
+        expect(InvoiceCustomSections::SelectService).to have_received(:call)
+          .with(section: invoice_custom_section, organization: organization)
+      end
+
+      context 'when pass selected as false' do
+        let(:selected) { false }
+
+        it 'calls DeselectService when selected is false' do
+          service_result
+          expect(InvoiceCustomSections::DeselectService).to have_received(:call)
+            .with(section: invoice_custom_section, organization: organization)
+        end
+      end
+    end
+
+    context 'when update fails' do
+      let(:update_params) { {name: nil} }
+
+      it 'handles validation errors' do
+        expect(service_result).not_to be_success
+        expect(service_result.error).to be_a(BaseService::ValidationFailure)
+        expect(service_result.error.messages[:name]).to eq(["value_is_mandatory"])
+      end
+    end
+  end
+end
