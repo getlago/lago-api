a94ff2aa2757dd863fa9c4256fdaccc77716d977
Anna Velentsevich <annvelents@gmail.com>
2024-12-13 11:46:53 +0100

    fix linter

--- app/graphql/resolvers/customers/invoice_custom_sections_resolver.rb
+++ app/graphql/resolvers/customers/invoice_custom_sections_resolver.rb
@@ -6,9 +6,9 @@ module Resolvers
       include RequiredOrganization
       description 'Query selected invoice_custom_sections of a customer'
 
+      argument :customer_id, type: ID, required: true
       argument :limit, Integer, required: false
       argument :page, Integer, required: false
-      argument :customer_id, type: ID, required: true
 
       type Types::InvoiceCustomSections::Object.collection_type, null: true
       def resolve(customer_id:, page: nil, limit: nil)
