e8a31892159a9e5b60f3c9ec8c921e233488b73d
LovroColic <lovro.colic.lc@gmail.com>
2024-12-13 11:16:38 +0100

    fix (grace_period): refactor how issuing date is set (#2928)
    ## Context
    
    When billing job is triggered in the past, `issuing_date` is not set
    correctly.
    
    ## Description
    
    `issuing_date` is set based on the billing job timestamp and grace
    period.
    
    However, when we check if it is correct time to finalize invoice or in
    services that set grace period we calculate issuing date again based on
    `invoice.created_at`. If `invoice.created_at` is different than billing
    day timestamp, we could end-up with invalid issuing date. This
    especially affects when the draft invoice is finalized.
    
    @vincent-pochet @rsempe Can you see any downside of this approach?

--- app/services/customers/update_invoice_grace_period_service.rb
+++ app/services/customers/update_invoice_grace_period_service.rb
@@ -4,26 +4,30 @@ module Customers
   class UpdateInvoiceGracePeriodService < BaseService
     def initialize(customer:, grace_period:)
       @customer = customer
-      @grace_period = grace_period
+      @grace_period = grace_period.to_i
       super
     end
 
     def call
-      if grace_period != customer.invoice_grace_period
+      old_grace_period = customer.invoice_grace_period.to_i
+      old_applicable_grace_period = customer.applicable_invoice_grace_period.to_i
+
+      if grace_period != old_grace_period
         customer.invoice_grace_period = grace_period
         customer.save!
 
-        # NOTE: Finalize related draft invoices.
-        customer.invoices.ready_to_be_finalized.each do |invoice|
-          Invoices::RefreshDraftAndFinalizeService.call(invoice:)
-        end
+        grace_period_diff = customer.applicable_invoice_grace_period.to_i - old_applicable_grace_period
 
         # NOTE: Update issuing_date on draft invoices.
-        customer.invoices.draft.each do |invoice|
-          invoice.update!(
-            issuing_date: grace_period_issuing_date(invoice),
-            payment_due_date: grace_period_payment_due_date(invoice)
-          )
+        customer.invoices.draft.find_each do |invoice|
+          invoice.issuing_date = invoice.issuing_date + grace_period_diff.days
+          invoice.payment_due_date = grace_period_payment_due_date(invoice)
+          invoice.save!
+        end
+
+        # NOTE: Finalize related draft invoices.
+        customer.invoices.ready_to_be_finalized.find_each do |invoice|
+          Invoices::RefreshDraftAndFinalizeService.call(invoice:)
         end
       end
 
@@ -35,16 +39,8 @@ module Customers
 
     attr_reader :customer, :grace_period
 
-    def invoice_created_at(invoice)
-      invoice.created_at.in_time_zone(customer.applicable_timezone).to_date
-    end
-
-    def grace_period_issuing_date(invoice)
-      invoice_created_at(invoice) + customer.applicable_invoice_grace_period.days
-    end
-
     def grace_period_payment_due_date(invoice)
-      grace_period_issuing_date(invoice) + customer.applicable_net_payment_term.days
+      invoice.issuing_date + customer.applicable_net_payment_term.days
     end
   end
 end
