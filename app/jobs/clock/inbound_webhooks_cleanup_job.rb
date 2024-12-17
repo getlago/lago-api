fa20a50627161381a5c991e143cc75d6b7da3c6a
Ancor Cruz <hello@ancorcruz.com>
2024-12-17 13:56:54 +0000

    Add clock job to clean old (older than 90 days) inbound webhooks from DB

---
+++ app/jobs/clock/inbound_webhooks_cleanup_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Clock
+  class InboundWebhooksCleanupJob < ApplicationJob
+    include SentryCronConcern
+
+    queue_as 'clock'
+
+    def perform
+      InboundWebhook.where('updated_at < ?', 90.days.ago).destroy_all
+    end
+  end
+end
