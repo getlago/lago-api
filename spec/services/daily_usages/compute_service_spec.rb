46161943a79d2d0eb354efce6029a3230170801b
Romain Sempé <romain@getlago.com>
2024-12-19 16:44:06 +0100

    feat(data): Set usage_date when computing daily usage

--- spec/services/daily_usages/compute_service_spec.rb
+++ spec/services/daily_usages/compute_service_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe DailyUsages::ComputeService, type: :service do
   subject(:compute_service) { described_class.new(subscription:, timestamp:) }
@@ -12,10 +12,10 @@ RSpec.describe DailyUsages::ComputeService, type: :service do
     create(:subscription, :calendar, customer:, plan:, started_at: 1.year.ago, subscription_at: 1.year.ago)
   end
 
-  let(:timestamp) { Time.zone.parse('2024-10-22 00:05:00') }
+  let(:timestamp) { Time.zone.parse("2024-10-22 00:05:00") }
 
-  describe '#call' do
-    it 'creates a daily usage', aggregate_failures: true do
+  describe "#call" do
+    it "creates a daily usage", aggregate_failures: true do
       travel_to(timestamp) do
         expect { compute_service.call }.to change(DailyUsage, :count).by(1)
 
@@ -26,7 +26,8 @@ RSpec.describe DailyUsages::ComputeService, type: :service do
           subscription_id: subscription.id,
           external_subscription_id: subscription.external_id,
           usage: Hash,
-          usage_diff: Hash
+          usage_diff: Hash,
+          usage_date: timestamp.to_date - 1.day
         )
         expect(daily_usage.refreshed_at).to match_datetime(timestamp)
         expect(daily_usage.from_datetime).to match_datetime(timestamp.beginning_of_month)
@@ -34,28 +35,28 @@ RSpec.describe DailyUsages::ComputeService, type: :service do
       end
     end
 
-    context 'when a daily usage already exists' do
+    context "when a daily usage already exists" do
       let(:existing_daily_usage) do
         create(:daily_usage, subscription:, organization:, customer:, refreshed_at: timestamp)
       end
 
       before { existing_daily_usage }
 
-      it 'returns the existing daily usage', aggregate_failure: true do
+      it "returns the existing daily usage", aggregate_failure: true do
         result = compute_service.call
 
         expect(result).to be_success
         expect(result.daily_usage).to eq(existing_daily_usage)
       end
 
-      context 'when the organization has a timezone' do
-        let(:organization) { create(:organization, timezone: 'America/Sao_Paulo') }
+      context "when the organization has a timezone" do
+        let(:organization) { create(:organization, timezone: "America/Sao_Paulo") }
 
         let(:existing_daily_usage) do
           create(:daily_usage, subscription:, organization:, customer:, refreshed_at: timestamp - 4.hours)
         end
 
-        it 'takes the timezone into account' do
+        it "takes the timezone into account" do
           result = compute_service.call
 
           expect(result).to be_success
@@ -63,14 +64,14 @@ RSpec.describe DailyUsages::ComputeService, type: :service do
         end
       end
 
-      context 'when the customer has a timezone' do
-        let(:customer) { create(:customer, organization:, timezone: 'America/Sao_Paulo') }
+      context "when the customer has a timezone" do
+        let(:customer) { create(:customer, organization:, timezone: "America/Sao_Paulo") }
 
         let(:existing_daily_usage) do
           create(:daily_usage, subscription:, organization:, customer:, refreshed_at: timestamp - 4.hours)
         end
 
-        it 'takes the timezone into account' do
+        it "takes the timezone into account" do
           result = compute_service.call
 
           expect(result).to be_success
@@ -79,26 +80,26 @@ RSpec.describe DailyUsages::ComputeService, type: :service do
       end
     end
 
-    context 'when timestamp is on subscription billing day' do
+    context "when timestamp is on subscription billing day" do
       let(:subscription) do
         create(:subscription, :anniversary, customer:, plan:, started_at: 1.year.ago, subscription_at: 1.year.ago)
       end
 
       let(:timestamp) { subscription.subscription_at + 1.year }
 
-      it 'does not create a daily usage' do
+      it "does not create a daily usage" do
         expect { compute_service.call }.not_to change(DailyUsage, :count)
       end
     end
 
-    context 'when subscription is terminated after the timestamp' do
+    context "when subscription is terminated after the timestamp" do
       let(:subscription) do
         create(:subscription, :terminated, :calendar, customer:, plan:, started_at: 1.year.ago)
       end
 
       let(:timestamp) { subscription.terminated_at - 1.day }
 
-      it 'creates a daily usage', aggregate_failures: true do
+      it "creates a daily usage", aggregate_failures: true do
         result = compute_service.call
 
         expect(result).to be_success
@@ -110,7 +111,8 @@ RSpec.describe DailyUsages::ComputeService, type: :service do
           subscription_id: subscription.id,
           external_subscription_id: subscription.external_id,
           usage: Hash,
-          usage_diff: Hash
+          usage_diff: Hash,
+          usage_date: timestamp.to_date - 1.day
         )
         expect(daily_usage.refreshed_at).to match_datetime(timestamp)
         expect(daily_usage.from_datetime).to match_datetime(timestamp.beginning_of_month)
