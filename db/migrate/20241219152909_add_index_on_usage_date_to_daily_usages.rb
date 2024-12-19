465187d02d62bfc064dc4aa7715275617d321b7e
Romain Sempé <romain@getlago.com>
2024-12-19 17:00:04 +0100

    feat(data): Set usage_date when filling daily usage history

--- db/migrate/20241219152909_add_index_on_usage_date_to_daily_usages.rb
+++ db/migrate/20241219152909_add_index_on_usage_date_to_daily_usages.rb
@@ -5,7 +5,6 @@ class AddIndexOnUsageDateToDailyUsages < ActiveRecord::Migration[7.1]
 
   def change
     safety_assured do
-      remove_column :daily_usages, :timezone
       add_index :daily_usages, :usage_date, algorithm: :concurrently
     end
   end
