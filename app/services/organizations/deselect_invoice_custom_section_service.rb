ec0ca775414fb3bf406433b985087cd880d18044
Anna Velentsevich <annvelents@gmail.com>
2024-12-20 15:58:18 +0100

    finish refactoring

--- app/services/organizations/deselect_invoice_custom_section_service.rb
+++ app/services/organizations/deselect_invoice_custom_section_service.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
 module Organizations
-    class DeselectInvoiceCustomSectionService < BaseService
-      def initialize(section:)
-        @section = section
-        @organization = section.organization
-        super
-      end
+  class DeselectInvoiceCustomSectionService < BaseService
+    def initialize(section:)
+      @section = section
+      @organization = section.organization
+      super
+    end
 
-      def call
-        deselect_for_organization
-        result
-      end
+    def call
+      deselect_for_organization
+      result
+    end
 
-      private
+    private
 
-      attr_reader :section, :organization
+    attr_reader :section, :organization
 
-      def deselect_for_organization
-        return unless organization.selected_invoice_custom_sections.include?(section)
+    def deselect_for_organization
+      return unless organization.selected_invoice_custom_sections.include?(section)
 
-        InvoiceCustomSectionSelection.where(
-          organization_id: organization.id, invoice_custom_section_id: section.id
-        ).destroy_all
-        result.section = section
-      end
+      InvoiceCustomSectionSelection.where(
+        organization_id: organization.id, invoice_custom_section_id: section.id
+      ).destroy_all
+      result.section = section
     end
   end
 end
