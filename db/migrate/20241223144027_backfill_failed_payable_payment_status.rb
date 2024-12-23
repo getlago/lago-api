090c93a50a10179c44ef30ea9f380255faafcabc
brunomiguelpinto <darkymiguel@gmail.com>
2024-12-23 15:06:00 +0000

    feat: mark all failed on the new column (#2997)
    ## Context
    
    Mark all the failed payments on the new column payable_payment_status

---
+++ db/migrate/20241223144027_backfill_failed_payable_payment_status.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class BackfillFailedPayablePaymentStatus < ActiveRecord::Migration[7.1]
+  def change
+    Payment.where(status: 'failed', payable_payment_status: nil)
+      .update_all(payable_payment_status: 'failed') # rubocop:disable Rails/SkipsModelValidations
+  end
+end
