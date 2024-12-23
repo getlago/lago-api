a49cb8e94a3591076115f433c64fcbe291527cbc
brunomiguelpinto <darkymiguel@gmail.com>
2024-12-23 12:53:08 +0000

    fix: fix all pending states (#2996)
    ## Context
    
    Mark all payments in status processing pending_submission as failed when
    they are duplicates only keep one pending that should be the must recent
    one

--- db/migrate/20241220095049_backfill_payable_payment_status.rb
+++ db/migrate/20241220095049_backfill_payable_payment_status.rb
@@ -3,21 +3,37 @@
 class BackfillPayablePaymentStatus < ActiveRecord::Migration[7.1]
   def change
     # clean all duplicate data
-    # Find `payable_id`s with duplicate "processing" payments
-    duplicate_payables = Payment.where(status: 'processing')
+    # Find `payable_id`s with duplicate_statuses payments
+
+    # Define statuses to clean up duplicates
+    duplicate_statuses = %w[
+      processing
+      requires_capture
+      requires_action
+      requires_confirmation
+      requires_payment_method
+      pending_customer_approval
+      pending_submission
+      submitted
+      confirmed
+      AuthorisedPending
+      Received
+    ]
+
+    duplicate_payables = Payment.where(status: duplicate_statuses)
       .group(:payable_id)
       .having('COUNT(id) > 1')
       .pluck(:payable_id)
 
     # clean the duplicates
     duplicate_payables.each do |payable_id|
-      # Find the most recent "processing"
-      latest_pending_payment = Payment.where(status: 'processing', payable_id: payable_id)
+      # Find the most recent with duplicate_statuses
+      latest_pending_payment = Payment.where(status: duplicate_statuses, payable_id: payable_id)
         .order(created_at: :desc)
         .first
 
-      # Update all other "processing" payments for this `payable_id` to "failed"
-      Payment.where(status: 'processing', payable_id: payable_id)
+      # Update all other duplicate_statuses payments for this `payable_id` to "failed"
+      Payment.where(status: duplicate_statuses, payable_id: payable_id)
         .where.not(id: latest_pending_payment.id)
         .update_all(status: 'failed') # rubocop:disable Rails/SkipsModelValidations
     end
