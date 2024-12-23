6f71999dd4c97e352c830f9104874e2da2a185b3
Ancor Cruz <hello@ancorcruz.com>
2024-12-23 14:32:21 +0000

    Update stripe handle incoming webhook
    only contruct event as paylaoad is already validated.

--- app/services/payment_providers/stripe/handle_incoming_webhook_service.rb
+++ app/services/payment_providers/stripe/handle_incoming_webhook_service.rb
@@ -12,36 +12,28 @@ module PaymentProviders
       end
 
       def call
-        payment_provider_result = PaymentProviders::FindService.call(
-          organization_id:,
-          code:,
-          payment_provider_type: "stripe"
-        )
-
-        return payment_provider_result unless payment_provider_result.success?
-
-        event = ::Stripe::Webhook.construct_event(
-          payload,
-          signature,
-          payment_provider_result.payment_provider&.webhook_secret
-        )
-
         PaymentProviders::Stripe::HandleEventJob.perform_later(
-          organization: payment_provider_result.payment_provider.organization,
-          event: event.to_json
+          organization:,
+          event: stripe_event.to_json
         )
 
-        result.event = event
+        result.event = stripe_event
         result
       rescue JSON::ParserError
         result.service_failure!(code: "webhook_error", message: "Invalid payload")
-      rescue ::Stripe::SignatureVerificationError
-        result.service_failure!(code: "webhook_error", message: "Invalid signature")
       end
 
       private
 
-      def_delegators :@inbound_webhook, :code, :organization_id, :payload, :signature
+      def_delegators :@inbound_webhook, :organization, :payload
+
+      def stripe_event
+        @stripe_event ||= ::Stripe::Event.construct_from(json_payload)
+      end
+
+      def json_payload
+        JSON.parse(payload, symbolize_names: true)
+      end
     end
   end
 end
--- spec/services/payment_providers/stripe/handle_incoming_webhook_service_spec.rb
+++ spec/services/payment_providers/stripe/handle_incoming_webhook_service_spec.rb
@@ -5,11 +5,7 @@ require "rails_helper"
 RSpec.describe PaymentProviders::Stripe::HandleIncomingWebhookService, type: :service do
   subject(:result) { described_class.call(inbound_webhook:) }
 
-  let(:inbound_webhook) { create :inbound_webhook, organization:, code: }
-  let(:code) { nil }
-  let(:membership) { create(:membership) }
-  let(:organization) { membership.organization }
-  let(:stripe_provider) { create(:stripe_provider, organization:) }
+  let(:inbound_webhook) { create :inbound_webhook }
   let(:event_result) { Stripe::Event.construct_from(event) }
 
   let(:event) do
@@ -17,42 +13,20 @@ RSpec.describe PaymentProviders::Stripe::HandleIncomingWebhookService, type: :se
     JSON.parse(File.read(path))
   end
 
-  before { stripe_provider }
-
   it "checks the webhook" do
-    allow(::Stripe::Webhook).to receive(:construct_event)
-      .and_return(event_result)
-
     expect(result).to be_success
     expect(result.event).to eq(event_result)
     expect(PaymentProviders::Stripe::HandleEventJob).to have_been_enqueued
   end
 
   context "when failing to parse payload" do
-    it "returns an error" do
-      allow(::Stripe::Webhook).to receive(:construct_event)
-        .and_raise(JSON::ParserError)
+    let(:event) { "invalid" }
 
+    it "returns an error" do
       expect(result).not_to be_success
       expect(result.error).to be_a(BaseService::ServiceFailure)
       expect(result.error.code).to eq("webhook_error")
       expect(result.error.error_message).to eq("Invalid payload")
-    end
-  end
-
-  context "when failing to validate the signature" do
-    it "returns an error" do
-      allow(::Stripe::Webhook).to receive(:construct_event)
-        .and_raise(
-          ::Stripe::SignatureVerificationError.new(
-            "error", "signature", http_body: event.to_json
-          )
-        )
-
-      expect(result).not_to be_success
-      expect(result.error).to be_a(BaseService::ServiceFailure)
-      expect(result.error.code).to eq("webhook_error")
-      expect(result.error.error_message).to eq("Invalid signature")
     end
   end
 end
