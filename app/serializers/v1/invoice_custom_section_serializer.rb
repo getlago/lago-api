f41637810b4a69ccdb3c0d249eeacc3f8e553eb6
Anna Velentsevich <annvelents@gmail.com>
2024-12-26 05:24:16 +0100

    add controller for invoice_custom_sections

---
+++ app/serializers/v1/invoice_custom_section_serializer.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module V1
+  class InvoiceCustomSectionSerializer < ModelSerializer
+    def serialize
+      {
+        lago_id: model.id,
+        code: model.code,
+        name: model.name,
+        description: model.description,
+        details: model.details,
+        display_name: model.display_name,
+        selected_for_organization: model.selected_for_organization?,
+        organization: {
+          lago_id: model.organization_id,
+        }
+      }
+    end
+  end
+end
