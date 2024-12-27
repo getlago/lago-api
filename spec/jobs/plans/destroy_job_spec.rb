95d8286bd39ab289ae84c7bf35b930ad63cade50
Ancor Cruz <hello@ancorcruz.com>
2024-12-27 12:44:53 +0000

    fix linter

--- spec/jobs/plans/destroy_job_spec.rb
+++ spec/jobs/plans/destroy_job_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Plans::DestroyJob, type: :job do
       expect do
         described_class.perform_later(plan)
         described_class.perform_later(plan)
-      end.to change { enqueued_jobs.count }.by(1)
+      end.to change(enqueued_jobs, :count).by(1)
     end
   end
 
@@ -66,4 +66,3 @@ RSpec.describe Plans::DestroyJob, type: :job do
     end
   end
 end
-
