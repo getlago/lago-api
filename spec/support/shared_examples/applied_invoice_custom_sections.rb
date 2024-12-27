6bcaeffa9438c3c148c1f48a0aee3d1f42699b80
Anna Velentsevich <annvelents@gmail.com>
2024-12-27 17:08:46 +0100

    wip adding tests for applying invoice custom sections

---
+++ spec/support/shared_examples/applied_invoice_custom_sections.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'applies invoice_custom_sections' do
+  let(:invoice_custom_sections) { create_list(:invoice_custom_section, 4, organization:)}
+
+  before do
+    organization.selected_invoice_custom_sections = invoice_custom_sections[2..3]
+  end
+
+  context 'there customer has :skip_invoice_custom_sections flag' do
+    before { customer.update(skip_invoice_custom_sections: true) }
+
+    it 'doesn\'t create applied_invoice_custom_section' do
+      expect { service_call }.not_to change { AppliedInvoiceCustomSection.count }
+    end
+  end
+
+  context 'when customer follows organizations invoice_custom_sections' do
+    it 'creates applied_invoice_custom_sections' do
+      result = service_call
+      invoice = result.invoice
+      expect(invoice.applied_invoice_custom_sections.pluck(:code)).to match_array(organization.selected_invoice_custom_sections.pluck(:code))
+    end
+  end
+end
