0ada1ba253ba0c6946ee0e25ca835599ab0fb4b9
Anna Velentsevich <annvelents@gmail.com>
2024-12-30 14:48:01 +0100

    fix linter

--- spec/support/shared_examples/applied_invoice_custom_sections.rb
+++ spec/support/shared_examples/applied_invoice_custom_sections.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'applies invoice_custom_sections' do
-  let(:invoice_custom_sections) { create_list(:invoice_custom_section, 4, organization:)}
+  let(:invoice_custom_sections) { create_list(:invoice_custom_section, 4, organization:) }
 
   before do
     organization.selected_invoice_custom_sections = invoice_custom_sections[2..3]
   end
 
-  context 'there customer has :skip_invoice_custom_sections flag' do
+  context 'when the customer has :skip_invoice_custom_sections flag' do
     before { customer.update(skip_invoice_custom_sections: true) }
 
     it 'doesn\'t create applied_invoice_custom_section' do
-      expect { service_call }.not_to change { AppliedInvoiceCustomSection.count }
+      expect { service_call }.not_to change(AppliedInvoiceCustomSection, :count)
     end
   end
 
