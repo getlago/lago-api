f9b4c0da48a40a072c8ca666c07197d04d59e5e6
Miguel Pinto <darkymiguel@gmail.com>
2025-01-02 17:17:08 +0000

    feat: added specs

--- spec/services/webhooks/invoices/resynced_service_spec.rb
+++ spec/services/webhooks/invoices/resynced_service_spec.rb
@@ -11,5 +11,19 @@ RSpec.describe Webhooks::Invoices::ResyncedService do
 
   describe '.call' do
     it_behaves_like 'creates webhook', 'invoice.resynced', 'invoice'
+
+    it 'calls the InvoiceSerializer with integration_customers included' do
+      serializer_instance = instance_double(V1::InvoiceSerializer)
+      allow(V1::InvoiceSerializer).to receive(:new).and_return(serializer_instance)
+      allow(serializer_instance).to receive(:serialize).and_return({})
+
+      webhook_service.call
+
+      expect(V1::InvoiceSerializer).to have_received(:new).with(
+        invoice,
+        root_name: 'invoice',
+        includes: array_including(:customer, :integration_customers, :subscriptions, :fees, :credits, :applied_taxes)
+      )
+    end
   end
 end
