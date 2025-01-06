ee4ca6e86a2f6f238e66f7e73bac84917d75c909
Romain Sempé <romain@getlago.com>
2025-01-06 16:35:11 +0100

    fix(fee): Prevent creating fee for an incorrect billing period (#3026)
    The goal of this PR is to ensure the fee is created for the correct
    billing period.
    By checking invoice subscription boundaries and comparing them to the
    fee timestamp.

--- app/services/invoices/advance_charges_service.rb
+++ app/services/invoices/advance_charges_service.rb
@@ -50,6 +50,7 @@ module Invoices
         invoice.invoice_subscriptions.each do |is|
           is.subscription.fees
             .where(invoice: nil, payment_status: :succeeded)
+            .where("properties->>'timestamp' <= ?", is.charges_to_datetime)
             .update_all(invoice_id: invoice.id) # rubocop:disable Rails/SkipsModelValidations
         end
 
