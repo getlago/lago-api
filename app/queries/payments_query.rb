77e4d08fe406385a4375f05efddf7ba2fae2597d
Miguel Pinto <darkymiguel@gmail.com>
2025-01-08 21:54:44 +0000

    feat: rubocop

--- app/queries/payments_query.rb
+++ app/queries/payments_query.rb
@@ -2,7 +2,6 @@
 
 class PaymentsQuery < BaseQuery
   def call
-    pp Payment.for_organization(organization).to_sql
     payments = Payment.for_organization(organization)
     payments = paginate(payments)
     payments = apply_consistent_ordering(payments)
