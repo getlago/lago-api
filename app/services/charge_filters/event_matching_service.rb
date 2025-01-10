ad926b65714debb90ab75bcd75d5895cf0cfd0d0
Romain Sempé <romain@getlago.com>
2025-01-10 10:43:15 +0100

    fix: Cast filters to string on ingestion (#3044)
    When creating an event, it was ignored because the type has been sent as
    an integer and should be sent as a string.
    
    The goal of this PR is to convert it to string so that it can be taken
    into consideration in the current usage.

--- app/services/charge_filters/event_matching_service.rb
+++ app/services/charge_filters/event_matching_service.rb
@@ -13,7 +13,7 @@ module ChargeFilters
       # NOTE: Find all filters matching event properties
       matching_filters = filters.select do |filter|
         filter.to_h.all? do |key, values|
-          applicable_event_properties.key?(key) && applicable_event_properties[key].in?(values)
+          applicable_event_properties.key?(key) && applicable_event_properties[key].to_s.in?(values)
         end
       end
 
