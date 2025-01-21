ce8dff909d0674563d397f7dd65cceb894cb5b54
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-01-21 11:09:29 +0200

    Add invoices preview context service

---
+++ app/services/invoices/preview_context_service.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+module Invoices
+  class PreviewContextService < BaseService
+    def initialize(organization, params:)
+      @organization = organization
+      @params = params.presence || {}
+      super(nil)
+    end
+
+    def call
+      result.customer = find_or_build_customer
+      result.subscription = build_subscription
+      result.applied_coupons = find_or_build_applied_coupons
+      result
+    rescue ActiveRecord::RecordNotFound => exception
+      result.not_found_failure!(resource: exception.model.demodulize.underscore)
+    ensure
+      result
+    end
+
+    private
+
+    attr_reader :params, :organization
+
+    def find_or_build_customer
+      customer_params = params[:customer] || {}
+
+      if customer_params.key?(:external_id)
+        organization.customers.find_by!(external_id: customer_params[:external_id])
+      else
+        Customer.new(
+          name: customer_params[:name],
+          organization: organization,
+          tax_identification_number: customer_params[:tax_identification_number],
+          currency: customer_params[:currency],
+          shipping_address_line1: customer_params.dig(:shipping_address, :address_line1),
+          shipping_address_line2: customer_params.dig(:shipping_address, :address_line2),
+          shipping_city: customer_params.dig(:shipping_address, :city),
+          shipping_zipcode: customer_params.dig(:shipping_address, :zipcode),
+          shipping_state: customer_params.dig(:shipping_address, :state),
+          shipping_country: customer_params.dig(:shipping_address, :country)
+        ) do |customer|
+          customer.integration_customers = Array(customer_params[:integration_customers]).map do |integration_params|
+            build_customer_integration(customer, integration_params)
+          end
+        end
+      end
+    end
+
+    def build_customer_integration(customer, attrs)
+      integration_class = integration_type(attrs[:integration_type]).constantize
+      integration = integration_class.find_by!(code: attrs[:integration_code], organization:)
+
+      integration_customer_class = IntegrationCustomers::BaseCustomer.customer_type(attrs[:integration_type]).constantize
+      integration_customer_class.new(integration:, customer:)
+    end
+
+    def build_subscription
+      return if !plan || !result.customer
+
+      billing_time = if Subscription::BILLING_TIME.include?(params[:billing_time]&.to_sym)
+        params[:billing_time]
+      else
+        'calendar'
+      end
+
+      Subscription.new(
+        customer: result.customer,
+        plan:,
+        subscription_at: params[:subscription_at].presence || Time.current,
+        started_at: params[:subscription_at].presence || Time.current,
+        billing_time:,
+        created_at: Time.current,
+        updated_at: Time.current
+      )
+    end
+
+    def plan
+      organization.plans.find_by!(code: params[:plan_code])
+    end
+
+    def find_or_build_applied_coupons
+      applied_coupons = result.customer.applied_coupons.presence
+
+      applied_coupons || Array(params[:coupons]).map do |coupon_attr|
+        coupon = coupon_attr.key?(:code) && organization.coupons.find_by(code: coupon_attr[:code])
+        coupon || Coupon.new(coupon_attr)
+      end.map { |coupon| AppliedCoupon.new(coupon:, customer: result.customer) }
+    end
+
+    def integration_type(type)
+      case type
+      when 'anrok'
+        'Integrations::AnrokIntegration'
+      when 'xero'
+        'Integrations::XeroIntegration'
+      when 'hubspot'
+        'Integrations::HubspotIntegration'
+      when 'salesforce'
+        'Integrations::SalesforceIntegration'
+      else
+        raise(NotImplementedError)
+      end
+    end
+  end
+end
---
+++ spec/services/invoices/preview_context_service_spec.rb
@@ -0,0 +1,304 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Invoices::PreviewContextService, type: :service do
+  let(:result) { described_class.call(organization, params:) }
+
+  describe "#call" do
+    let(:organization) { create(:organization) }
+    let(:plan) { create(:plan, organization:) }
+    let(:customer) { create(:customer, organization:) }
+
+    let(:params) do
+      {
+        customer: {external_id: customer.external_id},
+        plan_code: plan.code,
+        coupons: []
+      }
+    end
+
+    before do
+      create(:coupon, organization:) { |coupon| customer.coupons << coupon }
+    end
+
+    it "assigns customer, plan, and applied coupons to result" do
+      expect(result)
+        .to be_success
+        .and have_attributes(customer:, subscription: Subscription, applied_coupons: customer.applied_coupons)
+    end
+  end
+
+  describe "#customer" do
+    subject { result.customer }
+
+    let(:organization) { create(:organization) }
+
+    before { create(:customer, organization:) }
+
+    context "when customer external id is provided" do
+      let(:params) do
+        {
+          customer: {external_id:}
+        }
+      end
+
+      context "when customer matching external id exists in organization" do
+        let(:customer) { create(:customer, organization:) }
+        let(:external_id) { customer.external_id }
+
+        it "returns customer" do
+          expect(subject).to eq customer
+        end
+      end
+
+      context "when customer matching external id does not exist in organization" do
+        let(:external_id) { SecureRandom.uuid }
+
+        it "returns nil" do
+          expect(result).not_to be_success
+          expect(result.error).to be_a(BaseService::NotFoundFailure)
+          expect(result.error.error_code).to eq("customer_not_found")
+
+          expect(subject).to be_nil
+        end
+      end
+    end
+
+    context "when customer external id is omitted" do
+      let(:params) do
+        {
+          customer: {
+            name: "Mislav M",
+            tax_identification_number: "123",
+            currency: "EUR",
+            shipping_address: {
+              address_line1: "Rue de Tax",
+              address_line2: nil,
+              state: nil,
+              city: "Paris",
+              zipcode: "75011",
+              country: "IT"
+            },
+            integration_customers:
+          }
+        }
+      end
+
+      context "when integration customers params passed" do
+        let(:integration_customers) do
+          [
+            {
+              integration_type: "anrok",
+              integration_code: "code"
+            }
+          ]
+        end
+
+        context "when integration matching params exists" do
+          let(:expected_attributes) do
+            params[:customer].tap do |hash|
+              hash[:integration_customers] = [IntegrationCustomers::AnrokCustomer]
+            end
+          end
+
+          before { create(:anrok_integration, organization:, code: "code") }
+
+          it "returns new customer build from params including integration customers" do
+            expect(subject)
+              .to be_present
+              .and be_new_record
+              .and have_attributes(expected_attributes)
+          end
+        end
+
+        context "when integration matching params does not exist" do
+          it "returns nil" do
+            expect(result).not_to be_success
+            expect(result.error).to be_a(BaseService::NotFoundFailure)
+            expect(result.error.error_code).to eq("anrok_integration_not_found")
+
+            expect(subject).to be_nil
+          end
+        end
+      end
+
+      context "when integration customers params is omitted or empty" do
+        let(:integration_customers) { [] }
+        let(:expected_attributes) { params[:customer] }
+
+        it "returns new customer build from params" do
+          expect(subject)
+            .to be_present
+            .and be_new_record
+            .and have_attributes(expected_attributes)
+        end
+      end
+    end
+  end
+
+  describe "#subscription" do
+    subject { result.subscription }
+
+    let(:organization) { customer.organization }
+    let(:customer) { create(:customer) }
+
+    let(:params) do
+      {
+        customer: {external_id: customer.external_id},
+        plan_code: plan&.code,
+        subscription_at: subscription_at&.iso8601,
+        billing_time:
+      }
+    end
+
+    context "when plan matching provided code exists" do
+      let(:plan) { create(:plan, organization:) }
+
+      before { freeze_time }
+
+      context "when billing time and subscription date are present" do
+        let(:subscription_at) { generate(:past_date) }
+        let(:billing_time) { 'anniversary' }
+
+        it "returns new subscription with provided params" do
+          expect(subject)
+            .to be_a(Subscription)
+            .and have_attributes(
+              customer:,
+              plan:,
+              subscription_at: subscription_at,
+              started_at: subscription_at,
+              billing_time: params[:billing_time]
+            )
+        end
+      end
+
+      context "when billing time and subscription date are missing" do
+        let(:subscription_at) { nil }
+        let(:billing_time) { nil }
+
+        it "returns new subscription with default values for subscription date and billing time" do
+          expect(subject)
+            .to be_a(Subscription)
+            .and have_attributes(
+              customer:,
+              plan:,
+              subscription_at: Time.current,
+              started_at: Time.current,
+              billing_time: 'calendar'
+            )
+        end
+      end
+    end
+
+    context "when plan matching provided code does not exist" do
+      let(:plan) { nil }
+      let(:subscription_at) { nil }
+      let(:billing_time) { nil }
+
+      it "returns nil" do
+        expect(result).not_to be_success
+        expect(result.error).to be_a(BaseService::NotFoundFailure)
+        expect(result.error.error_code).to eq("plan_not_found")
+
+        expect(subject).to be_nil
+      end
+    end
+  end
+
+  describe "#applied_coupons" do
+    subject { result.applied_coupons }
+
+    let(:organization) { create(:organization) }
+    let(:plan) { create(:plan, organization:) }
+
+    let(:params) do
+      {
+        customer: customer_params,
+        plan_code: plan.code,
+        coupons: coupon_params
+      }
+    end
+
+    context "when customer has applied coupons" do
+      let(:customer_params) { {external_id: customer.external_id} }
+      let(:customer) { create(:customer, organization:) }
+
+      before do
+        create(:coupon, organization:) { |coupon| customer.coupons << coupon }
+      end
+
+      context "when coupons are provided" do
+        let(:coupon_params) do
+          [
+            {
+              code: coupon.code
+            },
+            {
+              code: "coupon_preview",
+              name: "coupon_preview",
+              coupon_type: "percentage",
+              amount_cents: 1200,
+              amount_currency: "EUR",
+              percentage_rate: 1
+            }
+          ]
+        end
+
+        let(:coupon) { create(:coupon, organization:) }
+
+        it "returns customer's applied coupons" do
+          expect(subject).to be_present.and eq customer.applied_coupons
+        end
+      end
+
+      context "when coupons are empty" do
+        let(:coupon_params) { [] }
+
+        it "returns customer's applied coupons" do
+          expect(subject).to be_present.and eq customer.applied_coupons
+        end
+      end
+    end
+
+    context "when customer has no applied coupons" do
+      let(:customer_params) { {name: Faker::Name.name} }
+
+      context "when coupons are provided" do
+        let(:coupon_params) do
+          [
+            {
+              code: coupon.code
+            },
+            {
+              code: "coupon_preview",
+              name: "coupon_preview",
+              coupon_type: "percentage",
+              amount_cents: 1200,
+              amount_currency: "EUR",
+              percentage_rate: 1
+            }
+          ]
+        end
+
+        let(:coupon) { create(:coupon, organization:) }
+
+        it "returns applied coupons build from provided params" do
+          expect(subject).to be_a(Array).and match_array([AppliedCoupon, AppliedCoupon])
+
+          expect(subject.map { |ac| ac.coupon.code })
+            .to match_array coupon_params.map { |i| i[:code] }
+        end
+      end
+
+      context "when coupons are empty" do
+        let(:coupon_params) { [] }
+
+        it "returns empty collection" do
+          expect(subject).to be_empty
+        end
+      end
+    end
+  end
+end
