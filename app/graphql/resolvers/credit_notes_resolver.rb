ea6e9d7f3d4f1c488cd508aabf0af905c7e73f5b
Ancor Cruz <hello@ancorcruz.com>
2025-01-22 15:53:58 +0000

    Add self_billed filter to credit notes resolver

--- app/graphql/resolvers/credit_notes_resolver.rb
+++ app/graphql/resolvers/credit_notes_resolver.rb
@@ -9,6 +9,11 @@ module Resolvers
 
     description 'Query credit notes'
 
+    argument :search_term, String, required: false
+
+    argument :limit, Integer, required: false
+    argument :page, Integer, required: false
+
     argument :amount_from, Integer, required: false
     argument :amount_to, Integer, required: false
     argument :credit_status, [Types::CreditNotes::CreditStatusTypeEnum], required: false
@@ -18,11 +23,9 @@ module Resolvers
     argument :invoice_number, String, required: false
     argument :issuing_date_from, GraphQL::Types::ISO8601Date, required: false
     argument :issuing_date_to, GraphQL::Types::ISO8601Date, required: false
-    argument :limit, Integer, required: false
-    argument :page, Integer, required: false
     argument :reason, [Types::CreditNotes::ReasonTypeEnum], required: false
     argument :refund_status, [Types::CreditNotes::RefundStatusTypeEnum], required: false
-    argument :search_term, String, required: false
+    argument :self_billed, Boolean, required: false
 
     type Types::CreditNotes::Object.collection_type, null: false
 
@@ -41,7 +44,8 @@ module Resolvers
           issuing_date_from: args[:issuing_date_from],
           issuing_date_to: args[:issuing_date_to],
           reason: args[:reason],
-          refund_status: args[:refund_status]
+          refund_status: args[:refund_status],
+          self_billed: args[:self_billed]
         },
         pagination: {
           page: args[:page],
