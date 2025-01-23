688961e598f9e927a47c2e471b5845f36b9ebd44
Ivan Novosad <ivan@getlago.com>
2025-01-23 17:21:04 +0100

    feat(manual-payments): Add payment resolver, and filter by customer external_id

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -61,6 +61,7 @@ module Types
     field :organization, resolver: Resolvers::OrganizationResolver
     field :overdue_balances, resolver: Resolvers::Analytics::OverdueBalancesResolver
     field :password_reset, resolver: Resolvers::PasswordResetResolver
+    field :payment, resolver: Resolvers::PaymentResolver
     field :payment_provider, resolver: Resolvers::PaymentProviderResolver
     field :payment_providers, resolver: Resolvers::PaymentProvidersResolver
     field :payment_requests, resolver: Resolvers::PaymentRequestsResolver
