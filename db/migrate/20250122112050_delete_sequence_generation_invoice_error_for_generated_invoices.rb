7892557d5c622672099e3561803ae1c2f524871c
Anna Velentsevich <annvelents@gmail.com>
2025-01-23 12:15:44 +0100

    use local constant to define invoices generated statuses

--- db/migrate/20250122112050_delete_sequence_generation_invoice_error_for_generated_invoices.rb
+++ db/migrate/20250122112050_delete_sequence_generation_invoice_error_for_generated_invoices.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class DeleteSequenceGenerationInvoiceErrorForGeneratedInvoices < ActiveRecord::Migration[7.1]
+  GENERATED_INVOICE_STATUSES = %w[finalized closed].freeze
+
   def change
     InvoiceError.joins("LEFT JOIN invoices ON invoices.id = invoice_errors.id")
       .where("invoice_errors.backtrace LIKE ?", "%generate_organization_sequential_id%")
-      .where(invoices: {status: Invoice::GENERATED_INVOICE_STATUSES}).find_each do |ie|
+      .where(invoices: {status: GENERATED_INVOICE_STATUSES}).find_each do |ie|
       ie.delete
     end
   end
