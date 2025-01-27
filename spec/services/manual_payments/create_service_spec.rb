e4794b9420b44e3a202aa9b799542ae0e96fa64b
Miguel Pinto <darkymiguel@gmail.com>
2025-01-27 15:02:17 +0000

    feat: added spec

--- spec/services/manual_payments/create_service_spec.rb
+++ spec/services/manual_payments/create_service_spec.rb
@@ -103,6 +103,20 @@ RSpec.describe ManualPayments::CreateService, type: :service do
           end
         end
 
+        context "when paid_at format is valid but different format" do
+          let(:paid_at) { "2024-01-20" }
+
+          it "creates a payment with valid date" do
+            result = service.call
+
+            aggregate_failures do
+              expect(result).to be_success
+              expect(result.payment.payment_type).to eq("manual")
+              expect(result.payment.created_at).to eq(paid_at)
+            end
+          end
+        end
+
         context "when payment amount cents is smaller than invoice remaining amount cents" do
           let(:amount_cents) { 2000 }
 
