b9442c35bb6f19c21406c85cb45f4e67a8a1b774
Julien Bourdeau <julien@julienbourdeau.com>
2025-01-27 16:37:07 +0100

    feat(boleto): Introduce Boleto as a valid Stripe Payment Method

--- spec/graphql/types/customers/customer_type_enum_spec.rb
+++ spec/graphql/types/customers/customer_type_enum_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Types::Customers::CustomerTypeEnum do
-  it 'enumerizes the correct values' do
+  it 'enumerates the correct values' do
     expect(described_class.values.keys).to match_array(%w[company individual])
   end
 end
