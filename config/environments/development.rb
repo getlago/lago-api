28709c55da9ef3ec9c126190f302601bb9cf8333
Ancor Cruz <hello@ancorcruz.com>
2025-02-05 08:32:58 +0000

    linter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
-  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new($stdout)
     .tap { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
