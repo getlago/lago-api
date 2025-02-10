c8e55446d08398c43d0513c23f7038da509cb4f9
Toon Willems <m@toonwillems.be>
2025-02-10 12:43:10 +0100

    Bug(BatchEvent): add validation error propagation to batch event service

--- app/services/events/create_batch_service.rb
+++ app/services/events/create_batch_service.rb
@@ -50,10 +50,11 @@ module Events
         event.timestamp = Time.zone.at(event_params[:timestamp] ? Float(event_params[:timestamp]) : timestamp)
         event.precise_total_amount_cents = event_params[:precise_total_amount_cents]
 
-        CalculateExpressionService.call(organization:, event:).raise_if_error!
+        expression_result = CalculateExpressionService.call(organization:, event:)
+        result.errors[index] = expression_result.error.message unless expression_result.success?
 
         result.events.push(event)
-        result.errors = result.errors.merge({index => event.errors.messages}) unless event.valid?
+        result.errors[index] = event.errors.messages unless event.valid?
       rescue ArgumentError
         result.errors = result.errors.merge({index => {timestamp: ['invalid_format']}})
       end
