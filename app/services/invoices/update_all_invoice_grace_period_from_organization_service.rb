e67722441f5e1f3a10df4d4bb1792aae135673bc
Toon Willems <m@toonwillems.be>
2025-02-10 10:51:17 +0100

    bug(GracePeriod) - Make grace period update async for increased reliability (#3154)
    ## Description
    
    The grace period update service was not built to deal with many (>100k)
    draft invoices. This means that some issuing_dates could be set
    incorrectly if the request timed out. This PR moves the update to an
    asynchronous job.
    
    For idempotency reasons it's important to store the applied_grace_period
    on the invoice, this way we can be certain that we're not running the
    job twice.

---
+++ app/services/invoices/update_all_invoice_grace_period_from_organization_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Invoices
+  class UpdateAllInvoiceGracePeriodFromOrganizationService < BaseService
+    def initialize(organization:, old_grace_period:)
+      @organization = organization
+      @old_grace_period = old_grace_period
+
+      super
+    end
+
+    def call
+      organization.invoices.draft.find_each do |invoice|
+        Invoices::UpdateGracePeriodFromOrganizationJob.perform_later(invoice, old_grace_period)
+      end
+
+      result
+    end
+
+    private
+
+    attr_reader :organization, :old_grace_period
+  end
+end
