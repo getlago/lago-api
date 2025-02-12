3fdb342ebdafeff07f9d41ecf74fa9a136ee73ec
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-12 15:21:11 +0100

    send wallet_transaction.payment_failure if invoice is visible (not only open)

--- spec/services/invoices/payments/deliver_error_webhook_service_spec.rb
+++ spec/services/invoices/payments/deliver_error_webhook_service_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe Invoices::Payments::DeliverErrorWebhookService, type: :service do
   subject(:webhook_service) { described_class.new(invoice, params) }
 
-  let(:invoice) { create(:invoice) }
   let(:params) do
     {
       'provider_customer_id' => 'customer',
@@ -17,24 +16,53 @@ RSpec.describe Invoices::Payments::DeliverErrorWebhookService, type: :service do
   end
 
   describe '.call_async' do
-    it 'enqueues a job to send an invoice payment failure webhook' do
-      expect do
-        webhook_service.call_async
-      end.to have_enqueued_job(SendWebhookJob).with('invoice.payment_failure', invoice, params)
-    end
+    context "when invoice is visible?" do
+      let(:invoice) { create(:invoice, invoice_type: :subscription, status: :finalized) }
 
-    context 'when the invoice is credit? and open?' do
-      let(:invoice) do
-        invoice = create(:invoice, :credit, status: :open)
-        create(:fee, fee_type: :credit, invoice: invoice, invoiceable: wallet_transaction)
-        invoice
-      end
-      let(:wallet_transaction) { create(:wallet_transaction) }
-
-      it 'enqueues a job to send a wallet transaction payment failure webhook' do
+      it 'enqueues a job to send an invoice payment failure webhook' do
         expect do
           webhook_service.call_async
-        end.to have_enqueued_job(SendWebhookJob).with('wallet_transaction.payment_failure', wallet_transaction, params)
+        end.to have_enqueued_job(SendWebhookJob).once.and(
+          have_enqueued_job(SendWebhookJob).with('invoice.payment_failure', invoice, params)
+        )
+      end
+    end
+
+    context 'when invoice is invisible' do
+      let(:invoice) { create(:invoice, invoice_type: :credit, status: :generating) }
+
+      it 'does not send the invoice payment failure webhook' do
+        expect do
+          webhook_service.call_async
+        end.not_to have_enqueued_job(SendWebhookJob)
+      end
+    end
+
+    context 'when the invoice is credit?' do
+      let(:fee) { create(:fee, fee_type: :credit, invoice: invoice, invoiceable: create(:wallet_transaction)) }
+
+      before { fee }
+
+      context 'when the invoice is open?' do
+        let(:invoice) { create(:invoice, :credit, status: :open) }
+
+        it 'enqueues a job to send a wallet transaction and an invoice payment failure webhook' do
+          expect do
+            webhook_service.call_async
+          end.to have_enqueued_job(SendWebhookJob).once
+            .and(have_enqueued_job(SendWebhookJob).with('wallet_transaction.payment_failure', WalletTransaction, params))
+        end
+      end
+
+      context 'when the invoice is visible?' do
+        let(:invoice) { create(:invoice, :credit, status: :finalized) }
+
+        it 'enqueues a job to send an invoice payment failure webhook' do
+          expect do
+            webhook_service.call_async
+          end.to have_enqueued_job(SendWebhookJob).with('wallet_transaction.payment_failure', WalletTransaction, params)
+            .and(have_enqueued_job(SendWebhookJob).with('invoice.payment_failure', invoice, params))
+        end
       end
     end
   end
