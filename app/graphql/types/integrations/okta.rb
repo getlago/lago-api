28d26b31c6a22a4ff052afd6b41577a6e725f333
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-18 06:43:50 -0800

    feat(gql): Introduce ObfuscatedStringType

--- app/graphql/types/integrations/okta.rb
+++ app/graphql/types/integrations/okta.rb
@@ -6,18 +6,12 @@ module Types
       graphql_name "OktaIntegration"
 
       field :client_id, String, null: true
-      field :client_secret, String, null: true
+      field :client_secret, ObfuscatedStringType, null: true
       field :code, String, null: false
       field :domain, String, null: false
       field :id, ID, null: false
       field :name, String, null: false
       field :organization_name, String, null: false
-
-      # NOTE: Client secret is a sensitive information. It should not be sent back to the
-      #       front end application. Instead we send an obfuscated value
-      def client_secret
-        "#{"•" * 8}…#{object.client_secret[-3..]}"
-      end
     end
   end
 end
--- spec/graphql/types/payment_providers/stripe_spec.rb
+++ spec/graphql/types/payment_providers/stripe_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Types::PaymentProviders::Stripe do
     expect(subject).to have_field(:code).of_type("String!")
     expect(subject).to have_field(:name).of_type("String!")
 
-    expect(subject).to have_field(:secret_key).of_type("String").with_permission("organization:integrations:view")
+    expect(subject).to have_field(:secret_key).of_type("ObfuscatedString").with_permission("organization:integrations:view")
     expect(subject).to have_field(:success_redirect_url).of_type("String").with_permission("organization:integrations:view")
   end
 end
