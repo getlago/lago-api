3a9e766f5734abf98ccee81b339993041e00ee7f
Ivan Novosad <ivan@getlago.com>
2025-02-18 09:53:55 -0800

    fix(rounding): Change rounding default to 6 significant digits

--- spec/views/helpers/rounding_helper_spec.rb
+++ spec/views/helpers/rounding_helper_spec.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe RoundingHelper do
   subject(:helper) { described_class }
 
-  describe '.round_decimal_part' do
-    it 'rounds the decimal part to the specified significant figures' do
-      expect(helper.round_decimal_part(123.456789123, 6)).to eq('123.456789')
+  describe ".round_decimal_part" do
+    it "rounds the decimal part to the specified significant figures" do
+      expect(helper.round_decimal_part(123.456789123, 4)).to eq("123.4568")
     end
 
-    it 'rounds the decimal part to the default significant figures when not specified' do
-      expect(helper.round_decimal_part(123.456789123)).to eq('123.4568')
+    it "rounds the decimal part to the default significant figures when not specified" do
+      expect(helper.round_decimal_part(123.456789123)).to eq("123.456789")
     end
 
-    it 'returns the integer part as a string when there is no decimal part' do
-      expect(helper.round_decimal_part(123)).to eq('123')
+    it "returns the integer part as a string when there is no decimal part" do
+      expect(helper.round_decimal_part(123)).to eq("123")
     end
 
-    it 'handles numbers with leading zeros in the decimal part' do
-      expect(helper.round_decimal_part(123.0000456789)).to eq('123.0')
+    it "handles numbers with leading zeros in the decimal part" do
+      expect(helper.round_decimal_part(123.0000456789)).to eq("123.000046")
     end
 
-    it 'handles very small decimal parts correctly' do
-      expect(helper.round_decimal_part(123.000000000456789)).to eq('123.0')
+    it "handles very small decimal parts correctly" do
+      expect(helper.round_decimal_part(123.000000000456789)).to eq("123.0")
     end
 
-    it 'handles very small decimal parts only correctly' do
-      expect(helper.round_decimal_part(0.0000000009)).to eq('0.0000000009')
+    it "handles very small decimal parts only correctly" do
+      expect(helper.round_decimal_part(0.0000000009)).to eq("0.0000000009")
     end
 
-    it 'handles negative numbers correctly' do
-      expect(helper.round_decimal_part(-123.456789123)).to eq('-123.4568')
+    it "handles negative numbers correctly" do
+      expect(helper.round_decimal_part(-123.456789123)).to eq("-123.456789")
     end
   end
 end
