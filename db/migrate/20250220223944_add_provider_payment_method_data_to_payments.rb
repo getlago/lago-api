21f72be957583a7008b313aed8dd7e50a2255dc7
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-19 15:15:40 -0800

    add column

---
+++ db/migrate/20250220223944_add_provider_payment_method_data_to_payments.rb
@@ -0,0 +1,5 @@
+class AddProviderPaymentMethodDataToPayments < ActiveRecord::Migration[7.1]
+  def change
+    add_column :payments, :provider_payment_method_data, :jsonb, null: false, default: {}
+  end
+end
