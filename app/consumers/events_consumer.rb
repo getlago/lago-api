6b99ea01220f310651e17e1a6e0a0d5a392f0ac1
Toon Willems <m@toonwillems.be>
2025-02-19 16:40:12 +0100

    try using a barrier

--- app/consumers/events_consumer.rb
+++ app/consumers/events_consumer.rb
@@ -6,46 +6,47 @@ require "async/semaphore"
 class EventsConsumer < ApplicationConsumer
   def consume
     Rails.logger.info "Polled #{messages.size} messages from kafka"
+    Async do
+      barrier = Async::Barrier.new
+      messages.each do |message|
+        barrier.async do
+          event_payload = message.payload
 
-    barrier = Async::Barrier.new
-    semaphore = Async::Semaphore.new(100, parent: barrier)
-    messages.each do |message|
-      semaphore.async do
-        event_payload = message.payload
+          event = Events::CommonFactory.new_instance source: event_payload
 
-        event = Events::CommonFactory.new_instance source: event_payload
+          # make sure Billable metric exists
+          bm = BillableMetric.find_by(code: event.code, organization_id: event.organization_id)
+          if !bm
+            # dispatch_to_dlq(message)
+            next
+          end
 
-        # make sure Billable metric exists
-        bm = BillableMetric.find_by(code: event.code, organization_id: event.organization_id)
-        if !bm
-          # dispatch_to_dlq(message)
-          next
+          # Evaluate expression
+          expression_result = Events::CalculateExpressionService.call(organization: event.organization, event:)
+          if expression_result.failure?
+            # dispatch_to_dlq(message)
+            next
+          end
+
+          # Check pay in advance
+          if event_payload["source"] != "http_ruby" && bm.charges.pay_in_advance.exists?
+            Events::PayInAdvanceKafkaJob.perform_later(event.as_json)
+          end
+
+          # fill in value with the extracted value
+          value = event.properties[bm.field_name]
+          event_payload["value"] = value.to_s
+
+          producer.produce_async(
+            topic: ENV["LAGO_KAFKA_ENRICHED_EVENTS_TOPIC"],
+            key: "#{event.organization_id}-#{event.external_subscription_id}-#{event.code}",
+            payload: event_payload.to_json
+          )
         end
-
-        # Evaluate expression
-        expression_result = Events::CalculateExpressionService.call(organization: event.organization, event:)
-        if expression_result.failure?
-          # dispatch_to_dlq(message)
-          next
-        end
-
-        # Check pay in advance
-        if event_payload["source"] != "http_ruby" && bm.charges.pay_in_advance.exists?
-          Events::PayInAdvanceKafkaJob.perform_later(event.as_json)
-        end
-
-        # fill in value with the extracted value
-        value = event.properties[bm.field_name]
-        event_payload["value"] = value.to_s
-
-        producer.produce_async(
-          topic: ENV["LAGO_KAFKA_ENRICHED_EVENTS_TOPIC"],
-          key: "#{event.organization_id}-#{event.external_subscription_id}-#{event.code}",
-          payload: event_payload.to_json
-        )
       end
+      barrier.wait
     end
-    barrier.wait
+
     Rails.logger.info "Done processing"
   end
 end
