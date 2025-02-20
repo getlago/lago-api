b6fe16cf1b1566107a30062e62cca0f97d5e4c56
Anna Velentsevich <annvelents@gmail.com>
2025-02-20 10:17:19 +0100

    better creation of default values between org and billing entities

--- spec/factories/billing_entities.rb
+++ spec/factories/billing_entities.rb
@@ -2,13 +2,17 @@
 
 FactoryBot.define do
   factory :billing_entity do
-    organization
     name { Faker::Company.name }
     code { "entity_#{SecureRandom.uuid}" }
     default_currency { "USD" }
 
     email { Faker::Internet.email }
     email_settings { ["invoice.finalized", "credit_note.created"] }
+
+    after :build do |billing_entity, values|
+      billing_entity.is_default = true if values.organization&.billing_entities&.where(is_default: true).blank?
+      billing_entity.organization = build(:organization, billing_entities: [billing_entity]) if values.organization.blank?
+    end
 
     trait :default do
       is_default { true }
