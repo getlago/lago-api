28a9dd9d977e7d472f3d4630e7ae117efd4bba19
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-20 23:08:45 +0100

    chore(spec): Merging rails_helper into spec_helper (#3208)
    ## Description
    
    Currently, we have both `spec_helper.rb` and `rails_helper.rb`.
    
    My main goal was to merge them so we have only one place defining all
    the before/after actions. I realized that we could get rid of the
    rails_helper file entirely, so we don't have to require it manually
    anymore.
    
    Any reason you'd like to keep it?

--- spec/support/pdf_helper.rb
+++ spec/support/pdf_helper.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  config.before(:each, :scenarios) do
-    stub_pdf_generation
-  end
-end
-
 module PdfHelper
   def stub_pdf_generation(response_body = nil, status = 200)
     response_body ||= File.read(Rails.root.join("spec/fixtures/blank.pdf"))
