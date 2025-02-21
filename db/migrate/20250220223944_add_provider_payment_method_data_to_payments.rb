24ddd19bc4a20bba38340f3d40cfbef91068e4b8
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-21 11:53:35 -0800

    STRIPE: save payment methods after payment and refactor webhooks service

--- db/migrate/20250220223944_add_provider_payment_method_data_to_payments.rb
+++ db/migrate/20250220223944_add_provider_payment_method_data_to_payments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddProviderPaymentMethodDataToPayments < ActiveRecord::Migration[7.1]
   def change
     add_column :payments, :provider_payment_method_data, :jsonb, null: false, default: {}
