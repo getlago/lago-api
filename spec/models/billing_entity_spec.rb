4cc59ddfd7041ba0fed825b7824c36364092f001
Anna Velentsevich <annvelents@gmail.com>
2025-02-26 09:41:54 +0100

    Feat(multi-entities): add validations for billing_entity (#3237)
    ## Context
    
    Since mostly billing entity just follow the logic of Organization, we
    need to repeat most of the validations we currently have on the
    organization

--- spec/models/billing_entity_spec.rb
+++ spec/models/billing_entity_spec.rb
@@ -43,4 +43,133 @@ RSpec.describe BillingEntity, type: :model do
       expect(record_3).to be_valid
     end
   end
+
+  describe "Validations" do
+    let(:billing_entity) { build(:billing_entity) }
+
+    it "is valid with valid attributes" do
+      expect(billing_entity).to be_valid
+    end
+
+    it "is not valid without name" do
+      billing_entity.name = nil
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is invalid with invalid email" do
+      billing_entity.email = "foo.bar"
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is invalid with invalid country" do
+      billing_entity.country = "ZWX"
+      expect(billing_entity).not_to be_valid
+
+      billing_entity.country = ""
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "validates the language code" do
+      billing_entity.document_locale = nil
+      expect(billing_entity).not_to be_valid
+
+      billing_entity.document_locale = "en"
+      expect(billing_entity).to be_valid
+
+      billing_entity.document_locale = "foo"
+      expect(billing_entity).not_to be_valid
+
+      billing_entity.document_locale = ""
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is invalid with invalid invoice footer" do
+      billing_entity.invoice_footer = SecureRandom.alphanumeric(601)
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is valid with logo" do
+      billing_entity.logo.attach(
+        io: File.open(Rails.root.join("spec/factories/images/logo.png")),
+        content_type: "image/png",
+        filename: "logo"
+      )
+      expect(billing_entity).to be_valid
+    end
+
+    it "is invalid with too big logo" do
+      billing_entity.logo.attach(
+        io: File.open(Rails.root.join("spec/factories/images/big_sized_logo.jpg")),
+        content_type: "image/jpeg",
+        filename: "logo"
+      )
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is invalid with unsupported logo content type" do
+      billing_entity.logo.attach(
+        io: File.open(Rails.root.join("spec/factories/images/logo.gif")),
+        content_type: "image/gif",
+        filename: "logo"
+      )
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is invalid with invalid timezone" do
+      billing_entity.timezone = "foo"
+      expect(billing_entity).not_to be_valid
+    end
+
+    it "is valid with email_settings" do
+      billing_entity.email_settings = ["invoice.finalized", "credit_note.created"]
+      expect(billing_entity).to be_valid
+    end
+
+    it "is invalid with non permitted email_settings value" do
+      billing_entity.email_settings = ["email.not_permitted"]
+
+      expect(billing_entity).not_to be_valid
+      expect(billing_entity.errors.first.attribute).to eq(:email_settings)
+      expect(billing_entity.errors.first.type).to eq(:unsupported_value)
+    end
+
+    it "dont allow finalize_zero_amount_invoice with null value" do
+      expect(billing_entity.finalize_zero_amount_invoice).to eq true
+      billing_entity.finalize_zero_amount_invoice = nil
+
+      expect(billing_entity).not_to be_valid
+    end
+  end
+
+  describe "#save" do
+    subject { billing_entity.save! }
+
+    context "with a new record" do
+      let(:billing_entity) { build(:billing_entity) }
+
+      it "sets document number prefix of billing_entity" do
+        subject
+
+        expect(billing_entity.document_number_prefix)
+          .to eq "#{billing_entity.name.first(3).upcase}-#{billing_entity.id.last(4).upcase}"
+      end
+
+      context "when document number prefix is already set" do
+        it "does not change existing document number prefix of billing_entity" do
+          billing_entity.document_number_prefix = "ABC-1234"
+          subject
+
+          expect(billing_entity.document_number_prefix).to eq "ABC-1234"
+        end
+      end
+    end
+
+    context "with a persisted record" do
+      let(:billing_entity) { create(:billing_entity) }
+
+      it "does not change document number prefix of billing_entity" do
+        expect { subject }.not_to change(billing_entity, :document_number_prefix)
+      end
+    end
+  end
 end
