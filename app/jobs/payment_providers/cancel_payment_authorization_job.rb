7d6b2b06a7da7f4ac2bb9be3407b73b8260f6f55
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-26 16:22:44 -0800

    add some tests

--- app/jobs/payment_providers/cancel_payment_authorization_job.rb
+++ app/jobs/payment_providers/cancel_payment_authorization_job.rb
@@ -5,11 +5,13 @@ module PaymentProviders
     queue_as "providers"
 
     def perform(payment_provider:, id:)
-      case payment_provider.payment_type.to_s
+      provider_name = payment_provider.payment_type.to_s
+      
+      case provider_name
       when "stripe"
         ::Stripe::PaymentIntent.cancel(id, {}, api_key: payment_provider.secret_key)
       else
-        raise NotImplementedError.new("Cancelling payment authorization not implemented for #{provider}")
+        raise NotImplementedError.new("Cancelling payment authorization not implemented for #{provider_name}")
       end
     end
   end
