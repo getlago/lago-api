c9f087f0ab35a3aa5c3276fbf0f3c0f86dddfb3c
Anna Velentsevich <annvelents@gmail.com>
2025-03-03 09:16:36 +0100

    remove test for is_default validations

--- spec/models/billing_entity_spec.rb
+++ spec/models/billing_entity_spec.rb
@@ -23,27 +23,6 @@ RSpec.describe BillingEntity, type: :model do
   it { is_expected.to have_many(:applied_taxes).dependent(:destroy) }
   it { is_expected.to have_many(:taxes).through(:applied_taxes) }
 
-  describe "is_default validation" do
-    let(:organization) { create :organization }
-
-    it "validates uniqueness of organization_id for is_default excluding deleted and archived records" do
-      # by default an organization is built with a default billing entity
-      expect(organization.default_billing_entity.discard!).to be true
-      archived_record = create(:billing_entity, :default, :archived, organization:)
-      expect(archived_record).to be_valid
-
-      record_1 = create(:billing_entity, :default, organization:)
-      expect(record_1).to be_valid
-
-      record_2 = build(:billing_entity, :default, organization:)
-      expect(record_2).not_to be_valid
-      expect(record_2.errors[:is_default]).to include("value_already_exist")
-
-      record_3 = build(:billing_entity, :default)
-      expect(record_3).to be_valid
-    end
-  end
-
   describe "Validations" do
     let(:billing_entity) { build(:billing_entity) }
 
