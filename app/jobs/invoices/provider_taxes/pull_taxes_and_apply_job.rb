401efe4dcd7b2f96afc075990ce230fc87ff1c8f
LovroColic <lovro.colic.lc@gmail.com>
2025-03-03 18:19:59 +0100

    fix: retry Net::ReadTimeout for pulling taxes (#3283)
    ## Context
    
    Integration aggregator often returns Net::ReadTimeout
    
    ## Description
    
    This PR auto retries mentioned error

--- app/jobs/invoices/provider_taxes/pull_taxes_and_apply_job.rb
+++ app/jobs/invoices/provider_taxes/pull_taxes_and_apply_job.rb
@@ -7,6 +7,7 @@ module Invoices
 
       retry_on BaseService::ThrottlingError, wait: :polynomially_longer, attempts: 25
       retry_on LagoHttpClient::HttpError, wait: :polynomially_longer, attempts: 6
+      retry_on Net::ReadTimeout, wait: :polynomially_longer, attempts: 6
 
       def perform(invoice:)
         Invoices::ProviderTaxes::PullTaxesAndApplyService.call!(invoice:)
