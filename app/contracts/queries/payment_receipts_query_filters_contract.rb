acc21f8013fc5dc813cc9dd7e4c3f50ad3e85c8a
Ivan Novosad <ivan@getlago.com>
2025-03-03 16:06:24 +0100

    feat(payment-receipts): Refactor contracts

--- app/contracts/queries/payment_receipts_query_filters_contract.rb
+++ app/contracts/queries/payment_receipts_query_filters_contract.rb
@@ -2,11 +2,9 @@
 
 module Queries
   class PaymentReceiptsQueryFiltersContract < Dry::Validation::Contract
-    UUID_REGEX = /\A[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\z/
-
     params do
       required(:filters).hash do
-        optional(:invoice_id).maybe(:string, format?: UUID_REGEX)
+        optional(:invoice_id).maybe(:string, format?: Regex::UUID)
       end
     end
   end
--- app/contracts/queries/payments_query_filters_contract.rb
+++ app/contracts/queries/payments_query_filters_contract.rb
@@ -2,11 +2,9 @@
 
 module Queries
   class PaymentsQueryFiltersContract < Dry::Validation::Contract
-    UUID_REGEX = /\A[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\z/
-
     params do
       required(:filters).hash do
-        optional(:invoice_id).maybe(:string, format?: UUID_REGEX)
+        optional(:invoice_id).maybe(:string, format?: Regex::UUID)
         optional(:external_customer_id).maybe(:string)
       end
     end
