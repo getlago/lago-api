da5a6d38add4e8a7f1c7e926d47bd32e4ab957eb
Vincent Pochet <vincent@getlago.com>
2025-03-04 17:38:50 +0100

    fix(kafka): Ensure timestamp is pushed as float

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -57,7 +57,7 @@ module Events
           external_subscription_id: event.external_subscription_id,
           transaction_id: event.transaction_id,
           # NOTE: Removes trailing 'Z' to allow clickhouse parsing
-          timestamp: event.timestamp.to_i,
+          timestamp: event.timestamp.to_f.to_s,
           code: event.code,
           # NOTE: Default value to 0.0 is required for clickhouse parsing
           precise_total_amount_cents: event.precise_total_amount_cents.present? ? event.precise_total_amount_cents.to_s : "0.0",
