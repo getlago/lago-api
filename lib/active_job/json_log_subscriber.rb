ad6be32821dfacbc554ea87b57961186e64e5972
Jérémy Denquin <jdenquin@users.noreply.github.com>
2025-03-05 11:29:41 +0100

    chore(logs): Improve logging to json (#3272)
    New Active Job logging format 
    
    ```
    {:event=>"perform", :status=>"error", :job=>"Invoices::GeneratePdfAndNotifyJob", :duration=>528.95, :job_id=>"11c160df-7edb-45fe-9ab5-31c7db66e97e", :queue=>"invoices", :exception=>{:class=>"Socket::ResolutionError", :message=>"Failed to open TCP connection to pdf:3000 (getaddrinfo: Name or service not known)"}}
    ```

---
+++ lib/active_job/json_log_subscriber.rb
@@ -0,0 +1,267 @@
+# frozen_string_literal: true
+
+require "active_support/subscriber"
+require "active_job/log_subscriber"
+
+module ActiveJob
+  class JsonLogSubscriber < ActiveSupport::LogSubscriber # :nodoc:
+    # rubocop:disable ThreadSafety/ClassAndModuleAttributes
+    class_attribute :backtrace_cleaner, default: ActiveSupport::BacktraceCleaner.new
+    # rubocop:enable ThreadSafety/ClassAndModuleAttributes
+
+    def enqueue(event)
+      job = event.payload[:job]
+      ex = event.payload[:exception_object] || job.enqueue_error
+
+      if ex
+        error do
+          {
+            event: "enqueue",
+            status: "error",
+            job: job.class.name,
+            queue: job.queue_name,
+            exception: {
+              class: ex.class.name,
+              message: ex.message
+            }
+          }
+        end
+      elsif event.payload[:aborted]
+        info do
+          {
+            event: "enqueue",
+            status: "aborted",
+            job: job.class.name,
+            queue: job.queue_name
+          }
+        end
+      else
+        info do
+          {
+            event: "enqueue",
+            status: "success",
+            job: job.class.name,
+            job_id: job.job_id,
+            queue: job.queue_name,
+            arguments: args_info(job)
+          }
+        end
+      end
+    end
+    subscribe_log_level :enqueue, :info
+
+    def enqueue_at(event)
+      job = event.payload[:job]
+      ex = event.payload[:exception_object] || job.enqueue_error
+
+      if ex
+        error do
+          {
+            event: "enqueue",
+            status: "error",
+            job: job.class.name,
+            queue: job.queue_name,
+            exception: {
+              class: ex.class.name,
+              message: ex.message
+            }
+          }
+        end
+      elsif event.payload[:aborted]
+        info do
+          {
+            event: "enqueue",
+            status: "aborted",
+            job: job.class.name,
+            queue: job.queue_name
+          }
+        end
+      else
+        info do
+          {
+            event: "enqueue",
+            enqueued_at: scheduled_at(event),
+            status: "success",
+            job: job.class.name,
+            job_id: job.job_id,
+            queue: job.queue_name,
+            arguments: args_info(job)
+          }
+        end
+      end
+    end
+    subscribe_log_level :enqueue_at, :info
+
+    # NOTE: We do not use batch enqueue at LAGO
+    # this has to be added if we use it finally
+    # def enqueue_all; end
+
+    def perform_start(event)
+      info do
+        job = event.payload[:job]
+
+        message = {
+          event: "perform",
+          status: "start",
+          job: job.class.name,
+          job_id: job.job_id,
+          arguments: args_info(job),
+          queue: job.queue_name
+        }
+
+        job.enqueued_at ? message.merge(enqueued_at: job.enqueued_at.utc) : message
+      end
+    end
+    subscribe_log_level :perform_start, :info
+
+    def perform(event)
+      job = event.payload[:job]
+      ex = event.payload[:exception_object]
+
+      if ex
+        error do
+          {
+            event: "perform",
+            status: "error",
+            job: job.class.name,
+            duration: event.duration.round(2),
+            job_id: job.job_id,
+            queue: job.queue_name,
+            exception: {
+              class: ex.class.name,
+              message: ex.message
+            }
+          }
+        end
+      elsif event.payload[:aborted]
+        info do
+          {
+            event: "perform",
+            status: "aborted",
+            job: job.class.name,
+            duration: event.duration.round(2),
+            job_id: job.job_id,
+            queue: job.queue_name
+          }
+        end
+      else
+        info do
+          {
+            event: "perform",
+            status: "success",
+            job: job.class.name,
+            duration: event.duration.round(2),
+            job_id: job.job_id,
+            queue: job.queue_name
+          }
+        end
+      end
+    end
+    subscribe_log_level :perform, :info
+
+    def enqueue_retry(event)
+      job = event.payload[:job]
+      ex = event.payload[:error]
+      wait = event.payload[:wait]
+
+      info do
+        if ex
+          {
+            event: "retry",
+            status: "error",
+            job: job.class.name,
+            job_id: job.job_id,
+            execution: job.executions,
+            wait: wait.to_i,
+            exception: {
+              class: ex.class.name,
+              message: ex.message
+            }
+          }
+        else
+          {
+            event: "retry",
+            status: "success",
+            job: job.class.name,
+            job_id: job.job_id,
+            execution: job.executions,
+            wait: wait.to_i
+          }
+        end
+      end
+    end
+    subscribe_log_level :enqueue_retry, :info
+
+    def retry_stopped(event)
+      job = event.payload[:job]
+      ex = event.payload[:error]
+
+      error do
+        {
+          event: "retry",
+          status: "stopped",
+          job: job.class.name,
+          job_id: job.job_id,
+          queue: job.queue_name,
+          retries: job.executions,
+          exception: {
+            class: ex.class.name,
+            message: ex.message
+          }
+        }
+      end
+    end
+    subscribe_log_level :retry_stopped, :error
+
+    def discard(event)
+      job = event.payload[:job]
+      ex = event.payload[:error]
+
+      error do
+        {
+          event: "discard",
+          status: "error",
+          job: job.class.name,
+          job_id: job.job_id,
+          exception: {
+            class: ex.class.name,
+            message: ex.message
+          }
+        }
+      end
+    end
+    subscribe_log_level :discard, :error
+
+    private
+
+    def args_info(job)
+      if job.class.log_arguments? && job.arguments.any?
+        job.arguments.map { |arg| format(arg).inspect }.join(", ")
+      else
+        {}
+      end
+    end
+
+    def format(arg)
+      case arg
+      when Hash
+        arg.transform_values { |value| format(value) }
+      when Array
+        arg.map { |value| format(value) }
+      when GlobalID::Identification
+        # rubocop:disable Style/RescueModifier
+        arg.to_global_id rescue arg
+        # rubocop:enable Style/RescueModifier
+      else
+        arg
+      end
+    end
+
+    def scheduled_at(event)
+      Time.at(event.payload[:job].scheduled_at).utc
+    end
+  end
+end
+
+ActiveJob::LogSubscriber.detach_from :active_job
+ActiveJob::JsonLogSubscriber.attach_to :active_job
