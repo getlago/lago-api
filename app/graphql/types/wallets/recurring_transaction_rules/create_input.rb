72b08a0bd89821b5143d8394f78315afe03a99ce
Miguel Pinto <darkymiguel@gmail.com>
2025-03-07 10:34:44 +0000

    feat: add expiration_at

--- app/graphql/types/wallets/recurring_transaction_rules/create_input.rb
+++ app/graphql/types/wallets/recurring_transaction_rules/create_input.rb
@@ -6,6 +6,7 @@ module Types
       class CreateInput < Types::BaseInputObject
         graphql_name "CreateRecurringTransactionRuleInput"
 
+        argument :expiration_at, GraphQL::Types::ISO8601DateTime, required: false
         argument :granted_credits, String, required: false
         argument :interval, Types::Wallets::RecurringTransactionRules::IntervalEnum, required: false
         argument :invoice_requires_successful_payment, Boolean, required: false
