84fa4b90b3c14245a12ab0cd620104cb5b80d7f4
Miguel Pinto <darkymiguel@gmail.com>
2025-03-07 11:11:50 +0000

    feat: add expiration_at

--- spec/graphql/types/wallets/recurring_transaction_rules/create_input_spec.rb
+++ spec/graphql/types/wallets/recurring_transaction_rules/create_input_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe Types::Wallets::RecurringTransactionRules::CreateInput do
   subject { described_class }
 
   it do
+    expect(subject).to accept_argument(:expiration_at).of_type("ISO8601DateTime")
+    expect(subject).to accept_argument(:granted_credits).of_type("String")
     expect(subject).to accept_argument(:interval).of_type("RecurringTransactionIntervalEnum")
     expect(subject).to accept_argument(:method).of_type("RecurringTransactionMethodEnum")
+    expect(subject).to accept_argument(:paid_credits).of_type("String")
     expect(subject).to accept_argument(:started_at).of_type("ISO8601DateTime")
     expect(subject).to accept_argument(:target_ongoing_balance).of_type("String")
-    expect(subject).to accept_argument(:trigger).of_type("RecurringTransactionTriggerEnum!")
     expect(subject).to accept_argument(:threshold_credits).of_type("String")
+    expect(subject).to accept_argument(:trigger).of_type("RecurringTransactionTriggerEnum!")
   end
 end
--- spec/graphql/types/wallets/recurring_transaction_rules/object_spec.rb
+++ spec/graphql/types/wallets/recurring_transaction_rules/object_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Types::Wallets::RecurringTransactionRules::Object do
     expect(subject).to have_field(:method).of_type("RecurringTransactionMethodEnum!")
     expect(subject).to have_field(:trigger).of_type("RecurringTransactionTriggerEnum!")
     expect(subject).to have_field(:interval).of_type("RecurringTransactionIntervalEnum")
-
+    expect(subject).to have_field(:expiration_at).of_type("ISO8601DateTime")
     expect(subject).to have_field(:started_at).of_type("ISO8601DateTime")
     expect(subject).to have_field(:target_ongoing_balance).of_type("String")
     expect(subject).to have_field(:threshold_credits).of_type("String")
