0b704f7c471af25a0b36afda9ab0d55285ea72e5
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-03-11 14:19:45 +0200

    misc(invoice-preview): Refactor subscription handling logic (#3296)
    ## Context
    
    `SubscriptionsService` became even more complicated with brining support
    for upgrade/downgrade cases.
    To better prepare for those changes breaking existing service into
    smaller, more focused on doing single thing services with better test
    coverage.
    Extracted only refactoring part for easier review process. In next PR
    I'll add support for upgrade and downgrade cases.
    
    ## Description
    
    Now `SubscriptionsService` responsible only for identifying context and
    calling another smaller service to produce proper data state.

---
+++ app/services/invoices/preview/build_subscription_service.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Invoices
+  module Preview
+    class BuildSubscriptionService < BaseService
+      Result = BaseResult[:subscriptions]
+
+      def initialize(customer:, params:)
+        @customer = customer
+        @params = params.presence || {}
+        super
+      end
+
+      def call
+        return result.not_found_failure!(resource: "customer") unless customer
+        return result.not_found_failure!(resource: "plan") unless plan
+
+        result.subscriptions = [build_subscription]
+        result
+      end
+
+      private
+
+      attr_reader :customer, :params
+
+      delegate :organization, to: :customer
+
+      def build_subscription
+        Subscription.new(
+          customer:,
+          plan:,
+          subscription_at: params[:subscription_at].presence || Time.current,
+          started_at: params[:subscription_at].presence || Time.current,
+          billing_time:,
+          created_at: params[:subscription_at].presence || Time.current,
+          updated_at: Time.current
+        )
+      end
+
+      def billing_time
+        if Subscription::BILLING_TIME.include?(params[:billing_time]&.to_sym)
+          params[:billing_time]
+        else
+          "calendar"
+        end
+      end
+
+      def plan
+        @plan ||= organization.plans.find_by(code: params[:plan_code])
+      end
+    end
+  end
+end
