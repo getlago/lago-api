4c21522ec92b4f9caca11d0e8a12678603564745
Julien Bourdeau <julien@julienbourdeau.com>
2025-03-13 18:34:49 +0100

    improve and test

--- app/services/payment_provider_customers/stripe/retrieve_latest_payment_method_service.rb
+++ app/services/payment_provider_customers/stripe/retrieve_latest_payment_method_service.rb
@@ -11,15 +11,39 @@ module PaymentProviderCustomers
       end
 
       def call
-        # We use limit: 10 just in case for some (wrong) reason the customer has a very high number of payment method
-        list = ::Stripe::Customer.list_payment_methods(provider_customer.provider_customer_id, {limit: 10}, api_key:)
-        result.payment_method_id = list.data.filter { _1.type == "card" }.max_by { _1.created }.id
+        # First, we try to get the customer default payment method
+        payment_method_id = begin
+          customer = ::Stripe::Customer.retrieve(provider_customer.provider_customer_id, request_options)
+          customer["invoice_settings"]["default_payment_method"]
+        rescue
+          nil
+        end
+
+        # If no default, we'll try to get the latest card
+        if payment_method_id.blank?
+          payment_method_id = begin
+            # We use limit: 10 just in case for some (wrong) reason the customer has a very high number of payment method
+            list = ::Stripe::Customer.list_payment_methods(provider_customer.provider_customer_id, {limit: 10}, request_options)
+            list.data.filter { _1.type == "card" }.max_by { _1.created }.id
+          rescue
+            nil
+          end
+        end
+
+        result.payment_method_id = payment_method_id
         result
       end
 
       private
 
-      attr_reader :provider_customer, :payment_method_id
+      attr_reader :provider_customer
+
+      def request_options
+        {
+          api_key:,
+          stripe_version: "2024-09-30.acacia"
+        }
+      end
 
       def api_key
         provider_customer.payment_provider.secret_key
