4602c0d6843c50383489aad04369905b5db82a55
Anna Velentsevich <annvelents@gmail.com>
2025-03-13 17:01:20 +0100

    do not count billed usage in ongoing_credits

--- app/services/wallets/balance/refresh_ongoing_service.rb
+++ app/services/wallets/balance/refresh_ongoing_service.rb
@@ -16,14 +16,14 @@ module Wallets
 
           {
             total_usage_amount_cents: invoice.total_amount.to_f * wallet.ongoing_balance.currency.subunit_to_unit,
-            pay_in_advance_usage_amount_cents: billed_usage_amount_cents(invoice)
+            billed_usage_amount_cents: billed_usage_amount_cents(invoice)
           }
         end
 
         total_usage_amount_cents = usage_amount_cents.sum { |e| e[:total_usage_amount_cents] }
-        pay_in_advance_usage_amount_cents = usage_amount_cents.sum { |e| e[:pay_in_advance_usage_amount_cents] }
+        billed_usage_amount_cents = usage_amount_cents.sum { |e| e[:billed_usage_amount_cents] }
 
-        Wallets::Balance::UpdateOngoingService.call(wallet:, total_usage_amount_cents:, pay_in_advance_usage_amount_cents:).raise_if_error!
+        Wallets::Balance::UpdateOngoingService.call(wallet:, total_usage_amount_cents:, billed_usage_amount_cents:).raise_if_error!
 
         result.wallet = wallet
         result
@@ -36,9 +36,13 @@ module Wallets
       delegate :customer, to: :wallet
 
       def billed_usage_amount_cents(invoice)
+        paid_in_advance_fees = invoice.fees.select { |f| f.charge.pay_in_advance? && f.charge.invoiceable? }
         invoice.prepaid_credit_amount_cents +
           invoice.total_paid_amount_cents +
-          invoice.fees.select { |f| f.charge.pay_in_advance? && f.charge.invoiceable? }.sum(&:amount_cents)
+          # In invoice total that is returned from CustomerUsageService in total_usage the taxes are included
+          # so if the fees ae already paid, we should exclude fees AND their taxes
+          paid_in_advance_fees.sum(&:amount_cents) +
+          paid_in_advance_fees.sum(&:taxes_amount_cents)
       end
     end
   end
