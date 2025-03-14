66ad0b1290e71a0df971414b848d342aa604bf28
Ivan Novosad <ivan@getlago.com>
2025-03-14 15:43:56 +0100

    fix(rounding): Refactor RoundingHelper.round_decimal_part

--- app/views/helpers/rounding_helper.rb
+++ app/views/helpers/rounding_helper.rb
@@ -4,9 +4,7 @@ class RoundingHelper
   def self.round_decimal_part(num, decimal_sig_figs = 6)
     return BigDecimal("%.#{decimal_sig_figs}g" % num).to_s if num.abs < 1
 
-    rounded_decimal_part = BigDecimal(num.to_s).frac.round(decimal_sig_figs)
-    rounded_number = num.round(decimal_sig_figs)
-
-    rounded_decimal_part.zero? ? rounded_number.to_i.to_s : rounded_number.to_s
+    rounded = BigDecimal(num.to_s).round(decimal_sig_figs)
+    rounded.frac.zero? ? rounded.to_i.to_s : rounded.to_s
   end
 end
