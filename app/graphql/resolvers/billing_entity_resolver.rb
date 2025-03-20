a5af664330be00ee946f4ca78a708b8b3de4fccd
Anna Velentsevich <annvelents@gmail.com>
2025-03-20 10:57:28 +0100

    Add billing_entity endpoints

---
+++ app/graphql/resolvers/billing_entity_resolver.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class BillingEntityResolver < Resolvers::BaseResolver
+    include AuthenticableApiUser
+    include RequiredOrganization
+
+    REQUIRED_PERMISSION = "billing_entity:view"
+
+    description "Query a single billing_entity of an organization"
+
+    argument :id, ID, required: true, description: "Uniq ID of the billing_entity"
+
+    type Types::BillingEntity::Object, null: true
+
+    def resolve(id: nil)
+      current_organization.all_billing_entities.find(id)
+    rescue ActiveRecord::RecordNotFound
+      not_found_error(resource: "billing_entity")
+    end
+  end
+end
