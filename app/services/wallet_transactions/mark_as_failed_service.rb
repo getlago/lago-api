4574807ec314900036236d35c02c0b91b94c06a1
Miguel Pinto <darkymiguel@gmail.com>
2025-03-20 10:08:30 +0000

    feat: fix failing specs

--- app/services/wallet_transactions/mark_as_failed_service.rb
+++ app/services/wallet_transactions/mark_as_failed_service.rb
@@ -4,16 +4,14 @@ module WalletTransactions
   class MarkAsFailedService < BaseService
     def initialize(wallet_transaction:)
       @wallet_transaction = wallet_transaction
-      super()
+      super
     end
 
     def call
       return result unless wallet_transaction
       return result if wallet_transaction.status == "failed"
 
-      ActiveRecord::Base.transaction do
-        wallet_transaction.mark_as_failed!
-      end
+      wallet_transaction.mark_as_failed!
       SendWebhookJob.perform_later("wallet_transaction.updated", wallet_transaction)
       result.wallet_transaction = wallet_transaction
       result
