b6dda2816bf98ec6884dd211cad3306a0dcd6b87
Jeremy Denquin <jdenquin@gmail.com>
2025-03-21 17:55:47 +0100

    chore(sentry): Add strackprof profiling with sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
-if ENV["SENTRY_DSN"]
+if ENV["SENTRY_DSN"].present?
+  require "sentry-ruby"
+  require "sentry-rails"
+  require "sentry-sidekiq"
+  require "stackprof"
+
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
     config.release = LagoUtils::Version.call(default: Rails.env).number
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    config.traces_sample_rate = 0
     config.traces_sample_rate = ENV["SENTRY_TRACES_SAMPLE_RATE"].to_f || 0
+    config.profiles_sample_rate = ENV["SENTRY_PROFILES_SAMPLE_RATE"].to_f || 0
     config.environment = ENV["SENTRY_ENVIRONMENT"] || Rails.env
   end
 end
