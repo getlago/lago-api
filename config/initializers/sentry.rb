3ab2959dd4218ef0be6e30f409a2b0236e17bc4d
Jeremy Denquin <jdenquin@gmail.com>
2025-03-21 18:25:43 +0100

    require stackprof only if profiling is enabled

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,14 +4,18 @@ if ENV["SENTRY_DSN"].present?
   require "sentry-ruby"
   require "sentry-rails"
   require "sentry-sidekiq"
-  require "stackprof"
+  require "stackprof" if ENV["SENTRY_ENABLE_PROFILING"].present?
 
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
     config.release = LagoUtils::Version.call(default: Rails.env).number
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.traces_sample_rate = ENV["SENTRY_TRACES_SAMPLE_RATE"].to_f || 0
-    config.profiles_sample_rate = ENV["SENTRY_PROFILES_SAMPLE_RATE"].to_f || 0
+
+    if ENV["SENTRY_ENABLE_PROFILING"].present?
+      config.profiles_sample_rate = ENV["SENTRY_PROFILES_SAMPLE_RATE"].to_f || 0
+    end
+
     config.environment = ENV["SENTRY_ENVIRONMENT"] || Rails.env
   end
 end
