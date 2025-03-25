171736e1fe3952df48d00ebe318a84e2889a4f01
Ancor Cruz <hello@ancorcruz.com>
2025-03-25 18:46:34 +0000

    fix linter issues

--- db/migrate/20250325145324_assign_customers_to_billing_entities.rb
+++ db/migrate/20250325145324_assign_customers_to_billing_entities.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
 class AssignCustomersToBillingEntities < ActiveRecord::Migration[7.2]
   def change
     # NOTE: ensure first billing entity has the same id as the organization to ease the migration to multi entities.
     BillingEntity.where("id != organization_id").find_in_batches(batch_size: 1000) do |batch|
-      BillingEntity.where(id: batch.pluck(:id)).update_all("id = organization_id")
+      BillingEntity.where(id: batch.pluck(:id))
+        .update_all("id = organization_id") # rubocop:disable Rails/SkipsModelValidations
     end
 
     # NOTE: Update all customers to have the same billing_entity_id as organization_id
     Customer.where("billing_entity_id != organization_id").or(Customer.where(billing_entity_id: nil)).find_in_batches(batch_size: 1000) do |batch|
-      Customer.where(id: batch.pluck(:id)).update_all("billing_entity_id = organization_id")
+      Customer.where(id: batch.pluck(:id))
+        .update_all("billing_entity_id = organization_id") # rubocop:disable Rails/SkipsModelValidations
     end
   end
 end
