a54e2a3b9f8b6b2191442fd3fa7872e072cd0098
Julien Bourdeau <julien@julienbourdeau.com>
2025-03-26 15:26:34 +0100

    spec

--- spec/jobs/payments/update_payment_method_data_job_spec.rb
+++ spec/jobs/payments/update_payment_method_data_job_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Payments::UpdatePaymentMethodDataJob, type: :job do
   let(:provider_payment_id) { "pi_123" }
   let(:provider_payment_method_id) { "pm_001" }
 
-  it "calls the create service" do
+  it "calls the service" do
     create(:payment, provider_payment_id:)
 
     allow(Payments::UpdatePaymentMethodDataService)
@@ -15,5 +15,17 @@ RSpec.describe Payments::UpdatePaymentMethodDataJob, type: :job do
     described_class.perform_now(provider_payment_id: "pi_123", provider_payment_method_id:)
 
     expect(Payments::UpdatePaymentMethodDataService).to have_received(:call!)
+  end
+
+  context "when payment is not found" do
+    it "does not call the service" do
+      allow(Payments::UpdatePaymentMethodDataService).to receive(:call!)
+
+      expect {
+        described_class.perform_now(provider_payment_id: "pi_123", provider_payment_method_id:)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+
+      expect(Payments::UpdatePaymentMethodDataService).not_to have_received(:call!)
+    end
   end
 end
