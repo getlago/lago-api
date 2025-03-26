f56185db47c32dfe3b9532d7a9ce99cb837f17ed
Julien Bourdeau <julien@julienbourdeau.com>
2025-03-26 14:46:43 +0100

    retry rate limit

--- app/jobs/payments/update_payment_method_data_job.rb
+++ app/jobs/payments/update_payment_method_data_job.rb
@@ -4,6 +4,8 @@ module Payments
   class UpdatePaymentMethodDataJob < ApplicationJob
     queue_as "default"
 
+    retry_on ::Stripe::RateLimitError, wait: :exponentially_longer, attempts: 5
+
     def perform(provider_payment_id:, provider_payment_method_id:)
       payment = ::Payment.find_by!(provider_payment_id: provider_payment_id)
       ::Payments::UpdatePaymentMethodDataService.call!(payment:, provider_payment_method_id:)
