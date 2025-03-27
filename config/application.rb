d3ecff9c6054d5086a49da4af28cad430417f9b0
Ivan Novosad <ivannovosad@users.noreply.github.com>
2025-03-27 16:49:25 +0100

    Misc dump sql (#3349)
    ## Description
    
    This PR handles the switch from `schema.rb` dump to `structure.sql`
    which is needed for storing procedures, triggers, etc.
    
    ---------
    
    Co-authored-by: Vincent Pochet <vincent@getlago.com>
    Co-authored-by: Toon Willems <m@toonwillems.be>

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module LagoApi
     config.active_record.encryption.primary_key = ENV["ENCRYPTION_PRIMARY_KEY"] || ENV["LAGO_ENCRYPTION_PRIMARY_KEY"]
     config.active_record.encryption.deterministic_key = ENV["ENCRYPTION_DETERMINISTIC_KEY"] || ENV["LAGO_ENCRYPTION_DETERMINISTIC_KEY"]
     config.active_record.encryption.key_derivation_salt = ENV["ENCRYPTION_KEY_DERIVATION_SALT"] || ENV["LAGO_ENCRYPTION_KEY_DERIVATION_SALT"]
+    config.active_record.schema_format = :sql
+    ActiveRecord::Tasks::DatabaseTasks.structure_dump_flags = ["--clean", "--if-exists", "--no-comments"]
 
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
     config.i18n.available_locales = %i[en fr nb de it es sv]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,10 +48,9 @@ ActiveJob::Uniqueness.test_mode!
 
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
+# Checks for pending migrations
 begin
-  ActiveRecord::Migration.maintain_test_schema!
+  ActiveRecord::Migration.check_all_pending!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
