fe3a0f4f1b62d9d654cbb54abbf7f67542e31e38
Vincent Pochet <vincent@getlago.com>
2025-03-27 10:41:28 +0100

    feat(processor): Add flag subscription refresh logic

--- spec/clockwork_spec.rb
+++ spec/clockwork_spec.rb
@@ -240,4 +240,25 @@ describe Clockwork do
       expect(Clock::InboundWebhooksRetryJob).to have_been_enqueued
     end
   end
+
+  describe "schedule:refresh_flagged_subscriptions" do
+    let(:job) { "schedule:refresh_flagged_subscriptions" }
+    let(:start_time) { Time.zone.parse("2025-03-27T00:05:00") }
+    let(:end_time) { Time.zone.parse("2025-03-27T00:06:00") }
+
+    it "enqueue a retry inbound webhooks job" do
+      Clockwork::Test.run(
+        file: clock_file,
+        start_time:,
+        end_time:,
+        tick_speed: 1.minute
+      )
+
+      expect(Clockwork::Test).to be_ran_job(job)
+      expect(Clockwork::Test.times_run(job)).to eq(1)
+
+      Clockwork::Test.block_for(job).call
+      expect(Clock::ConsumeSubscriptionRefreshedQueueJob).to have_been_enqueued
+    end
+  end
 end
---
+++ spec/services/subscriptions/consume_subscription_refreshed_queue_service_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Subscriptions::ConsumeSubscriptionRefreshedQueueService do
+  subject(:flag_service) { described_class.new }
+
+  let(:redis_client) { instance_double(Redis) }
+
+  let(:values) { ["#{SecureRandom.uuid}:#{SecureRandom.uuid}", "#{SecureRandom.uuid}:#{SecureRandom.uuid}"] }
+  let(:loop_values) { [values, []] }
+
+  before do
+    allow(Redis).to receive(:new).and_return(redis_client)
+    allow(redis_client).to receive(:srandmember)
+      .with(described_class::REDIS_STORE_NAME, described_class::BATCH_SIZE)
+      .and_return(*loop_values)
+
+    allow(redis_client).to receive(:srem)
+  end
+
+  describe "#call" do
+    it "flags all subscriptions as refreshed" do
+      result = flag_service.call
+
+      expect(result).to be_success
+      expect(Subscriptions::FlagRefreshedJob).to have_been_enqueued.twice
+    end
+
+    context "with multiple batches" do
+      let(:loop_values) { [values, values, []] }
+
+      it "flags all subscriptions as refreshed" do
+        result = flag_service.call
+
+        expect(result).to be_success
+        expect(Subscriptions::FlagRefreshedJob).to have_been_enqueued.exactly(4).times
+      end
+    end
+
+    context "with no subscriptions" do
+      let(:loop_values) { [[]] }
+
+      it "does not flag any subscriptions as refreshed" do
+        result = flag_service.call
+
+        expect(result).to be_success
+        expect(Subscriptions::FlagRefreshedJob).not_to have_been_enqueued
+      end
+    end
+
+    context "when timeout is reached" do
+      let(:start_time) { Time.current }
+
+      before do
+        allow(Time).to receive(:current).and_return(
+          start_time,
+          start_time + described_class::PROCESSING_TIMEOUT + 1.second
+        )
+      end
+
+      it "flags all subscriptions as refreshed" do
+        result = flag_service.call
+
+        expect(result).to be_success
+        expect(Subscriptions::FlagRefreshedJob).not_to have_been_enqueued
+      end
+    end
+  end
+end
