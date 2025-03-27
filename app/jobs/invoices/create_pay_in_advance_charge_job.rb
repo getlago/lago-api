6e91c3bdeecfd9fafd4b76ee5812a1f808131e18
Anna Velentsevich <annvelents@gmail.com>
2025-03-27 10:54:52 +0100

    Lock uniq jobs for charge and subscription

--- app/jobs/invoices/create_pay_in_advance_charge_job.rb
+++ app/jobs/invoices/create_pay_in_advance_charge_job.rb
@@ -37,7 +37,7 @@ module Invoices
     def lock_key_arguments
       args = arguments.first
       event = Events::CommonFactory.new_instance(source: args[:event])
-      [args[:charge], event.organization_id, event.external_subscription_id, event.transaction_id]
+      [args[:charge], event.organization_id, event.external_subscription_id]
     end
 
     private
