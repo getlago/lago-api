21c5f7d4146b7451551011889e911ae861442f1c
Ancor Cruz <hello@ancorcruz.com>
2025-03-28 09:18:20 +0000

    Fix linter issue on migration

--- db/migrate/20250327130155_remove_default_billing_entity_sequential_id_on_invoices.rb
+++ db/migrate/20250327130155_remove_default_billing_entity_sequential_id_on_invoices.rb
@@ -7,9 +7,9 @@ class RemoveDefaultBillingEntitySequentialIdOnInvoices < ActiveRecord::Migration
     change_column_default :invoices, :billing_entity_sequential_id, from: 0, to: nil
 
     Invoice.in_batches(of: 1000) do |batch|
-      batch.update_all(
+      batch.update_all( # rubocop:disable Rails/SkipsModelValidations
         "billing_entity_sequential_id = CASE WHEN organization_sequential_id = 0 THEN NULL ELSE organization_sequential_id END"
-      ) # rubocop:disable Rails/SkipsModelValidations
+      )
     end
   end
 end
