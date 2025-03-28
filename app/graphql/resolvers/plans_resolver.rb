ac2bd9674dda44a9763b46a81f2f0846a58b0c5e
Romain Sempé <romain@getlago.com>
2025-03-28 14:57:17 +0100

    feat: Add deleted records to customers and plans resolvers

--- app/graphql/resolvers/plans_resolver.rb
+++ app/graphql/resolvers/plans_resolver.rb
@@ -12,13 +12,17 @@ module Resolvers
     argument :limit, Integer, required: false
     argument :page, Integer, required: false
     argument :search_term, String, required: false
+    argument :with_deleted, Boolean, required: false
 
     type Types::Plans::Object.collection_type, null: false
 
-    def resolve(page: nil, limit: nil, search_term: nil)
+    def resolve(page: nil, limit: nil, search_term: nil, with_deleted: nil)
       result = PlansQuery.call(
         organization: current_organization,
         search_term:,
+        filters: {
+          with_deleted:
+        },
         pagination: {
           page:,
           limit:
