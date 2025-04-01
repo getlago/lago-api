ec1176e3528ca7fc6e857289b3127be62eae160f
Ancor Cruz <hello@ancorcruz.com>
2025-04-01 15:09:49 +0100

    Add lock_key_arguments to ensure job uniqueness

--- app/jobs/invoices/prepaid_credit_job.rb
+++ app/jobs/invoices/prepaid_credit_job.rb
@@ -7,6 +7,13 @@ module Invoices
     retry_on ActiveRecord::StaleObjectError, wait: :polynomially_longer, attempts: 6
     unique :until_executed, on_conflict: :log
 
+    def lock_key_arguments
+      invoice = arguments.first
+      payment_status = arguments.second || :succeeded
+
+      [invoice, payment_status.to_sym]
+    end
+
     def perform(invoice, payment_status = :succeeded)  # Default to :succeeded for old jobs
       wallet_transaction = invoice.fees.find_by(fee_type: "credit")&.invoiceable
 
