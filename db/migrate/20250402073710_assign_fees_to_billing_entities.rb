663cdffcd047442aaccf7f53a87d2819da9c68e7
Anna Velentsevich <annvelents@gmail.com>
2025-04-02 12:01:42 +0200

    use bg job to migrate fees

--- db/migrate/20250402073710_assign_fees_to_billing_entities.rb
+++ db/migrate/20250402073710_assign_fees_to_billing_entities.rb
@@ -2,10 +2,7 @@
 
 class AssignFeesToBillingEntities < ActiveRecord::Migration[7.2]
   def up
-    Fee.where(billing_entity_id: nil).find_in_batches(batch_size: 5000) do |batch|
-      Fee.where(id: batch.pluck(:id))
-        .update_all("billing_entity_id = organization_id") # rubocop:disable Rails/SkipsModelValidations
-    end
+    Migrate::PopulateFeesWithBillingEntityJob.perform_later
   end
 
   def down
