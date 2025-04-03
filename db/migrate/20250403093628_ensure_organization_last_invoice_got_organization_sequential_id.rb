fd57652db71276aa0fc8928917087b32265f85e3
Ancor Cruz <hello@ancorcruz.com>
2025-04-03 12:24:10 +0100

    fix linter

--- db/migrate/20250403093628_ensure_organization_last_invoice_got_organization_sequential_id.rb
+++ db/migrate/20250403093628_ensure_organization_last_invoice_got_organization_sequential_id.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class EnsureOrganizationLastInvoiceGotOrganizationSequentialId < ActiveRecord::Migration[7.2]
   def change
     Migrations::InvoicesOrganizationSequentialIdFixer.call
--- lib/migrations/invoices_organization_sequential_id_fixer.rb
+++ lib/migrations/invoices_organization_sequential_id_fixer.rb
@@ -26,7 +26,7 @@ module Migrations
         next if last_organization_sequential_id == invoices_count
 
         last_invoice = organization.invoices.non_self_billed.with_generated_number.order(created_at: :desc).limit(1)
-        last_invoice.update_all(organization_sequential_id: invoices_count)
+        last_invoice.update_all(organization_sequential_id: invoices_count) # rubocop:disable Rails/SkipsModelValidations
       end
     end
   end
