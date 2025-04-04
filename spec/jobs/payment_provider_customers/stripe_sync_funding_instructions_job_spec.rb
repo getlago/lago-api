f54e5c4ebbb5543f61beb349cd0325f493bff7d5
Miguel Pinto <darkymiguel@gmail.com>
2025-04-04 12:08:06 +0100

    feat: added type to the service

--- spec/jobs/payment_provider_customers/stripe_sync_funding_instructions_job_spec.rb
+++ spec/jobs/payment_provider_customers/stripe_sync_funding_instructions_job_spec.rb
@@ -3,41 +3,21 @@
 require "rails_helper"
 
 RSpec.describe PaymentProviderCustomers::StripeSyncFundingInstructionsJob, type: :job do
-  subject(:job) { described_class }
+  subject(:stripe_sync_funding_instructions_job) { described_class }
 
   let(:stripe_customer) { create(:stripe_customer) }
-  let(:service_instance) { instance_double(PaymentProviderCustomers::Stripe::SyncFundingInstructionsService) }
-  let(:result) { instance_double(BaseService::Result, raise_if_error!: true) }
+  let(:stripe_service) { instance_double(PaymentProviderCustomers::Stripe::SyncFundingInstructionsService) }
 
-  before do
-    allow(PaymentProviderCustomers::Stripe::SyncFundingInstructionsService)
-      .to receive(:new)
+  it "calls the funding instructions sync service" do
+    allow(PaymentProviderCustomers::Stripe::SyncFundingInstructionsService).to receive(:new)
       .with(stripe_customer)
-      .and_return(service_instance)
+      .and_return(stripe_service)
+    allow(stripe_service).to receive(:call)
+      .and_return(BaseService::Result.new)
 
-    allow(service_instance).to receive(:call).and_return(result)
-  end
+    stripe_sync_funding_instructions_job.perform_now(stripe_customer)
 
-  it "calls the Stripe SyncFundingInstructionsService and raises if error" do
-    job.perform_now(stripe_customer)
-
-    expect(PaymentProviderCustomers::Stripe::SyncFundingInstructionsService).to have_received(:new).with(stripe_customer)
-    expect(service_instance).to have_received(:call)
-    expect(result).to have_received(:raise_if_error!)
-  end
-
-  context "when an UnauthorizedFailure is raised" do
-    before do
-      allow(result).to receive(:raise_if_error!).and_raise(BaseService::UnauthorizedFailure.new("unauthorized"))
-      allow(Rails.logger).to receive(:warn)
-    end
-
-    it "rescues the error and logs a warning" do
-      expect {
-        job.perform_now(stripe_customer)
-      }.not_to raise_error
-
-      expect(Rails.logger).to have_received(:warn).with("unauthorized")
-    end
+    expect(PaymentProviderCustomers::Stripe::SyncFundingInstructionsService).to have_received(:new)
+    expect(stripe_service).to have_received(:call)
   end
 end
