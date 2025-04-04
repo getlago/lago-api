de5a2ee320db4d323fde51f9a5feed11a7d0b709
Anna Velentsevich <annvelents@gmail.com>
2025-04-04 17:23:02 +0200

    fix structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -6248,7 +6248,6 @@ SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20250402135038'),
-('20250402133749'),
 ('20250402113844'),
 ('20250325162648'),
 ('20250325145324'),
