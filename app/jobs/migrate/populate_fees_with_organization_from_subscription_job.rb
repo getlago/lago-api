3f29c873d4999e061df23448a3515bfd6d13abbd
Anna Velentsevich <annvelents@gmail.com>
2025-04-04 16:04:27 +0200

    add fees population that do not have invoice_id

---
+++ app/jobs/migrate/populate_fees_with_organization_from_subscription_job.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Migrate
+  class PopulateFeesWithOrganizationFromSubscriptionJob < ApplicationJob
+    queue_as :low_priority
+    unique :until_executed
+
+    BATCH_SIZE = 1000
+
+    def perform(batch_number = 1)
+      Rails.logger.info("batch number: #{batch_number}")
+      batch = Fee.unscoped.where(organization_id: nil).where.not(subscription_id: nil)
+                 .joins(subscription: :customer).limit(BATCH_SIZE)
+
+      if batch.exists?
+        # rubocop:disable Rails/SkipsModelValidations
+        batch.update_all(
+          "organization_id = (SELECT customers.organization_id FROM subscriptions
+                            JOIN customers ON customers.id = subscriptions.customer_id
+                            WHERE subscriptions.id = fees.subscription_id),
+           billing_entity_id = (SELECT customers.organization_id FROM subscriptions
+                              JOIN customers ON customers.id = subscriptions.customer_id
+                              WHERE subscriptions.id = fees.subscription_id)"
+        )
+
+        # Queue the next batch
+        self.class.perform_later(batch_number + 1)
+      else
+        Rails.logger.info("Finished the execution")
+      end
+    end
+
+    def lock_key_arguments
+      [arguments]
+    end
+  end
+end
