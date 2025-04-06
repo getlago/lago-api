b0f4172668bf7cc856cf3c6bb64a291151a11685
Anna Velentsevich <annvelents@gmail.com>
2025-04-06 11:29:20 +0200

    rename one migration

--- db/migrate/20250402150920_add_non_null_to_invoices_billing_entity_id.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddNonNullToInvoicesBillingEntityId < ActiveRecord::Migration[7.2]
-  def change
-    add_check_constraint :invoices, "billing_entity_id IS NOT NULL", name: "invoices_billing_entity_id_null", validate: false
-  end
-end
