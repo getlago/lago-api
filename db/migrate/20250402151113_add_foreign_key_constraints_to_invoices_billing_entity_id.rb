5c2502b405d1568b3a76790b5f80b52960328e68
Anna Velentsevich <annvelents@gmail.com>
2025-04-07 14:44:12 +0200

    fix linter

--- db/migrate/20250402151113_add_foreign_key_constraints_to_invoices_billing_entity_id.rb
+++ db/migrate/20250402151113_add_foreign_key_constraints_to_invoices_billing_entity_id.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class AddForeignKeyConstraintsToInvoicesBillingEntityId < ActiveRecord::Migration[7.2]
-
   def up
     # Add foreign key without validation first
     add_foreign_key :invoices, :billing_entities, validate: false
