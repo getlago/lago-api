e8056c14f3960a8f7efd646d6e14f8565c41818e
Lovro Colic <lovro.colic.lc@gmail.com>
2025-04-07 12:04:42 +0200

    add support for avalara mapping

---
+++ app/models/integration_mappings/avalara_mapping.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module IntegrationMappings
+  class AvalaraMapping < BaseMapping
+  end
+end
+
+# == Schema Information
+#
+# Table name: integration_mappings
+#
+#  id             :uuid             not null, primary key
+#  mappable_type  :string           not null
+#  settings       :jsonb            not null
+#  type           :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  integration_id :uuid             not null
+#  mappable_id    :uuid             not null
+#
+# Indexes
+#
+#  index_integration_mappings_on_integration_id  (integration_id)
+#  index_integration_mappings_on_mappable        (mappable_type,mappable_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (integration_id => integrations.id)
+#
