ef510e0c17418614594f3051f6ecd7aeb2e7994f
Miguel Pinto <darkymiguel@gmail.com>
2025-04-08 12:12:28 +0100

    feat: added type to the service

--- spec/services/payment_provider_customers/stripe/sync_funding_instructions_service_spec.rb
+++ spec/services/payment_provider_customers/stripe/sync_funding_instructions_service_spec.rb
@@ -3,22 +3,53 @@
 require "rails_helper"
 
 RSpec.describe PaymentProviderCustomers::Stripe::SyncFundingInstructionsService do
+  subject(:update_service) { described_class.new(stripe_customer) }
+
   let(:organization) { create(:organization) }
-  let(:customer) { create(:customer, organization:) }
+  let(:customer) { create(:customer, organization:, currency: "USD") }
   let(:provider_customer_id) { "cus_Rw5Qso78STEap3" }
-  let(:provider_customer) { create(:stripe_customer, customer:, provider_customer_id:, payment_provider: create(:stripe_provider, organization:), payment_method_id: nil) }
+  let(:stripe_customer) { create(:stripe_customer, customer:, provider_payment_methods:) }
+  let(:provider_payment_methods) { %w[customer_balance] }
 
   describe "#call" do
-    # TODO working on it
-    # context "when customer has a default payment method in Stripe" do
-    #   it do
-    #     stub_request(:get, %r{/v1/customers/#{provider_customer_id}$}).and_return(
-    #       status: 200, body: File.read(Rails.root.join("spec/fixtures/stripe/customer_with_default_payment_method.json"))
-    #     )
-    #
-    #     result = subject.call
-    #     expect(result.payment_method_id).to eq "pm_1R2DFsQ8iJWBZFaMw3LLbR0r"
-    #   end
-    # end
+    context "when customer is not eligible" do
+      let(:provider_payment_methods) { %w[card] }
+
+      it "returns a successful result without doing anything" do
+        expect(::Stripe::Customer).not_to receive(:create_funding_instructions)
+        result = update_service.call
+        expect(result).to be_success
+      end
+    end
+
+    context "when customer is eligible and everything is valid and section does not yet exist" do
+      let(:funding_instructions) do
+        double("FundingInstructions", bank_transfer: double(to_hash: {some: "details"}))
+      end
+
+      let(:formatter_service_result) { double(details: "formatted bank details") }
+      let(:invoice_custom_section) { create(:invoice_custom_section, organization:) }
+
+      before do
+        allow(stripe_customer.payment_provider).to receive(:secret_key).and_return("sk_test_123")
+        allow(::Stripe::Customer).to receive(:create_funding_instructions)
+          .and_return(funding_instructions)
+        allow(InvoiceCustomSections::FundingInstructionsFormatterService).to receive(:call)
+           .and_return(formatter_service_result)
+        allow(InvoiceCustomSections::CreateService).to receive(:call)
+           .and_return(double(invoice_custom_section: invoice_custom_section))
+        allow(Customers::ManageInvoiceCustomSectionsService).to receive(:call)
+      end
+
+      it "creates the section and returns success" do
+        result = update_service.call
+
+        expect(result).to be_success
+        expect(::Stripe::Customer).to have_received(:create_funding_instructions)
+        expect(InvoiceCustomSections::FundingInstructionsFormatterService).to have_received(:call)
+        expect(InvoiceCustomSections::CreateService).to have_received(:call)
+        expect(Customers::ManageInvoiceCustomSectionsService).to have_received(:call)
+      end
+    end
   end
 end
