9d72d163843b4e6944b808e19ccb52c47d29c040
Miguel Pinto <darkymiguel@gmail.com>
2025-04-08 10:38:39 +0100

    feat: added type to the service

--- spec/graphql/resolvers/invoice_custom_sections_resolver_spec.rb
+++ spec/graphql/resolvers/invoice_custom_sections_resolver_spec.rb
@@ -26,8 +26,7 @@ RSpec.describe Resolvers::InvoiceCustomSectionsResolver, type: :graphql do
       create(:invoice_custom_section, organization:, name: "a"),
       create(:invoice_custom_section, organization:, name: "z"),
       create(:invoice_custom_section, organization:, name: "n"),
-      create(:invoice_custom_section, organization:, name: "not show", section_type: "system_generated"),
-      create(:invoice_custom_section, organization:, name: "not show2", section_type: "system_generated")
+      create(:invoice_custom_section, organization:, name: "not show", section_type: "system_generated")
     ]
   end
 
