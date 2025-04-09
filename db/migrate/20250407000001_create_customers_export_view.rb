3ec27bdf1509ce7ee1c7de9b3776a1ad863f4579
Jeremy Denquin <jeremy@getlago.com>
2025-04-09 17:17:56 +0200

    add fees view

--- db/migrate/20250407000001_create_customers_export_view.rb
+++ db/migrate/20250407000001_create_customers_export_view.rb
@@ -5,11 +5,23 @@ class CreateCustomersExportView < ActiveRecord::Migration[7.0]
     create_view :exports_customers, version: 1
     create_view :exports_billable_metrics, version: 1
     create_view :exports_plans, version: 1
+
+    create_view :customers_export_view, version: 1, materialized: true
+    add_index :customers_export_view, :lago_id, unique: true
+    add_index :customers_export_view, :external_id, unique: true
+
+    create_view :fees_export_view, version: 1
   end
 
   def down
     drop_view :exports_billable_metrics, if_exists: true
     drop_view :exports_customers, if_exists: true
     drop_view :exports_plans, if_exists: true
+
+    drop_view :fees_export_view, revert_to_version: 1
+
+    remove_index :customers_export_view, :external_id
+    remove_index :customers_export_view, :lago_id
+    drop_view :customers_export_view, materialized: true, revert_to_version: 1
   end
 end
