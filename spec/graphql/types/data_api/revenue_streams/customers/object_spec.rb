f576d36d14cb26afe887e22a48c4df0ddd15460d
Ivan Novosad <ivannovosad@users.noreply.github.com>
2025-04-09 16:38:08 +0200

    fix(analytics): Fix null customer name (#3464)
    ## Description
    
    RevenueStreams::Customer customer_name can be null - that fixes the
    customer loading bug.

--- spec/graphql/types/data_api/revenue_streams/customers/object_spec.rb
+++ spec/graphql/types/data_api/revenue_streams/customers/object_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Types::DataApi::RevenueStreams::Customers::Object do
     expect(subject).to have_field(:customer_id).of_type("ID!")
     expect(subject).to have_field(:customer_deleted_at).of_type("ISO8601DateTime")
     expect(subject).to have_field(:external_customer_id).of_type("String!")
-    expect(subject).to have_field(:customer_name).of_type("String!")
+    expect(subject).to have_field(:customer_name).of_type("String")
     expect(subject).to have_field(:amount_currency).of_type("CurrencyEnum!")
     expect(subject).to have_field(:gross_revenue_amount_cents).of_type("BigInt!")
     expect(subject).to have_field(:gross_revenue_share).of_type("Float!")
