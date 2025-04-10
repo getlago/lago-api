1be0de913b5d13bb0bdf1acf35822acf1e1387b7
Vincent Pochet <vincent@getlago.com>
2025-04-10 17:09:14 +0200

    misc(plans): Extract plan GraphQL arguments in types

--- app/graphql/mutations/plans/update.rb
+++ app/graphql/mutations/plans/update.rb
@@ -10,24 +10,7 @@ module Mutations
       graphql_name "UpdatePlan"
       description "Updates an existing Plan"
 
-      argument :amount_cents, GraphQL::Types::BigInt, required: true
-      argument :amount_currency, Types::CurrencyEnum, required: true
-      argument :bill_charges_monthly, Boolean, required: false
-      argument :cascade_updates, Boolean, required: false
-      argument :code, String, required: true
-      argument :description, String, required: false
-      argument :id, String, required: true
-      argument :interval, Types::Plans::IntervalEnum, required: true
-      argument :invoice_display_name, String, required: false
-      argument :name, String, required: true
-      argument :pay_in_advance, Boolean, required: true
-      argument :tax_codes, [String], required: false
-      argument :trial_period, Float, required: false
-
-      argument :charges, [Types::Charges::Input]
-      argument :minimum_commitment, Types::Commitments::Input, required: false
-      argument :usage_thresholds, [Types::UsageThresholds::Input], required: false
-
+      input_object_class Types::Plans::UpdateInput
       type Types::Plans::Object
 
       def resolve(**args)
