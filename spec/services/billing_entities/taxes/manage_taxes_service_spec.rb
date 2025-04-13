642de339fe8be1436c8be222facf297949906a15
Anna Velentsevich <annvelents@gmail.com>
2025-04-13 12:49:18 +0200

    fix linter

--- spec/services/billing_entities/taxes/manage_taxes_service_spec.rb
+++ spec/services/billing_entities/taxes/manage_taxes_service_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe BillingEntities::Taxes::ManageTaxesService do
   let(:tax1) { create(:tax, organization:, code: "TAX_CODE_1") }
   let(:tax2) { create(:tax, organization:, code: "TAX_CODE_2") }
 
-  
   describe "#call" do
     context "when sending tax codes" do
       let(:tax_codes) { ["TAX_CODE_1", "TAX_CODE_2"] }
@@ -38,6 +37,7 @@ RSpec.describe BillingEntities::Taxes::ManageTaxesService do
 
       context "when billing_entity had another tax applied" do
         let(:tax3) { create(:tax, organization:, code: "TAX_CODE_3") }
+
         before do
           billing_entity.taxes << tax3
         end
@@ -48,23 +48,23 @@ RSpec.describe BillingEntities::Taxes::ManageTaxesService do
           expect(billing_entity.taxes).to eq([tax1, tax2])
         end
       end
-      
+
       context "when tax codes contain duplicates" do
         let(:tax_codes) { ["TAX_CODE_1", "TAX_CODE_2", "TAX_CODE_1"] }
-        
+
         it "applies each tax only once" do
           service.call
-          
+
           expect(billing_entity.taxes).to eq([tax1, tax2])
         end
       end
-      
+
       context "when tax codes have different case" do
         let(:tax_codes) { ["tax_code_1", "TAX_CODE_2"] }
-        
+
         it "matches tax codes case-insensitively" do
           service.call
-          
+
           expect(billing_entity.taxes).to eq([tax1, tax2])
         end
       end
@@ -77,32 +77,31 @@ RSpec.describe BillingEntities::Taxes::ManageTaxesService do
         billing_entity.taxes = [tax1, tax2]
       end
 
-
       it "removes taxes from the billing entity" do
         service.call
 
         expect(billing_entity.taxes).to be_empty
       end
     end
-    
+
     context "when tax_codes is nil" do
       let(:tax_codes) { nil }
-      
+
       before do
         billing_entity.taxes = [tax1, tax2]
       end
-      
+
       it "removes taxes from the billing entity" do
         service.call
-        
+
         expect(billing_entity.taxes).to be_empty
       end
     end
-    
+
     context "when billing_entity is invalid" do
       let(:billing_entity) { nil }
       let(:tax_codes) { ["TAX_CODE_1"] }
-      
+
       it "returns a not_found_failure" do
         result = service.call
         expect(result.error).to be_a(BaseService::NotFoundFailure)
