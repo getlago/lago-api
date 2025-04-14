46b7381b0b22ba16c1d4d293364a9c915fa91ec5
Julien Bourdeau <julien@julienbourdeau.com>
2025-04-14 16:09:23 +0200

    link to PR

--- app/jobs/payments/update_payment_method_data_job.rb
+++ app/jobs/payments/update_payment_method_data_job.rb
@@ -4,7 +4,8 @@ module Payments
   class UpdatePaymentMethodDataJob < ApplicationJob
     queue_as "default"
 
-    # NOTE: Even if the perform method is protected against running this job multple time
+    # NOTE: Even if the service is protected against running multiple time, this job must be unique.
+    #       https://github.com/getlago/lago-api/pull/3490
     unique :until_executed
 
     retry_on ::Stripe::RateLimitError, wait: :polynomially_longer, attempts: 5
