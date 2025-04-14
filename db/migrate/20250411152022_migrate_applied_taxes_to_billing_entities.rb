4b7d71bd05972d59ab4eba626c667fc048441d15
Ancor Cruz <hello@ancorcruz.com>
2025-04-14 13:42:29 +0100

    feat(multientities): Migrate organization applied taxes.
    Creates a billing entity applied tax record for each organization tax
    with applied_to_organization = true

---
+++ db/migrate/20250411152022_migrate_applied_taxes_to_billing_entities.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class MigrateAppliedTaxesToBillingEntities < ActiveRecord::Migration[7.2]
+  def up
+    Organization.find_each do |organization|
+      Tax.where(organization_id: organization.id, applied_to_organization: true).find_each do |tax|
+        BillingEntity::AppliedTax.find_or_create_by!(
+          billing_entity_id: organization.id,
+          tax_id: tax.id
+        )
+      end
+    end
+  end
+end
