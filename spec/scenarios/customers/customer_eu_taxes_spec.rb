521024e3ccfc870668203bb4db3fcf11d704ef1c
Julien Bourdeau <julien@julienbourdeau.com>
2025-04-14 14:13:39 +0200

    chore(spec): Send `customer.vies_check` webhook if error (#3472)
    ## Description
    
    Adds scenario tests about customer taxes and EU tax management. Ensure a
    webhook is sent in case of success or failure.
    
    This ensure the webhook is sent in case of error
    
    When using Valvat, the gem will perform format check via Regex before
    calling the API. If the format is invalid, the API isn't called.
    
    In the webhook pay load the `customer.vies_check` hash has multple form.
    
    **Notice that valvat is now autloaded with Bundler. I don't know why it
    was done manually (since the gem was added).**
    
    ### When valid
    
    The response from the VIES API is returned, like before
    
    ### When format is invalid
    
    ```json
    {
      "valid": false,
      "valid_format": false,
    }
    ```
    
    ### When format is valid but number doesn't exist
    
    ```json
    {
      "valid": false,
      "valid_format": true
    }
    ```
    
    
    ### When the API returned an error (timeout, rate limit and so on)
    
    ```json
    {
      "valid": false,
      "valid_format": true,
      "error": "The  web service returned the error: <ERROR FROM VIES>"
    }
    ```
    
    The error message prefix is from valvat.

---
+++ spec/scenarios/customers/customer_eu_taxes_spec.rb
@@ -0,0 +1,235 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Add customer-specific taxes", :scenarios, type: :request do
+  let(:organization) { create(:organization, country: "FR", eu_tax_management: false, billing_entities: [create(:billing_entity, country: "FR")]) }
+  let(:plan) { create(:plan, organization:, pay_in_advance: true, amount_cents: 149_00) }
+
+  let(:american_attributes) do
+    {
+      name: "John",
+      country: "US",
+      address_line1: "123 Main St",
+      address_line2: "",
+      state: "Colorado",
+      city: "Denver",
+      zipcode: "80095",
+      currency: "USD"
+    }
+  end
+
+  let(:french_attributes) do
+    {
+      name: "Jean",
+      country: "FR",
+      address_line1: "123 Avenue du General",
+      address_line2: "",
+      state: "",
+      city: "Paris",
+      zipcode: "75018",
+      currency: "EUR"
+    }
+  end
+
+  let(:italian_attributes) do
+    {
+      country: "IT",
+      address_line1: "123 Via Marconi",
+      address_line2: "",
+      state: "",
+      city: "Roma",
+      zipcode: "00146",
+      currency: "EUR"
+    }
+  end
+
+  def enable_eu_tax_management!
+    Organizations::UpdateService.call!(organization:, params: {eu_tax_management: true})
+  end
+
+  include_context "with webhook tracking"
+
+  context "when customer are created after the feature was enabled" do
+    it "create taxes" do
+      enable_eu_tax_management!
+
+      create_or_update_customer(american_attributes.merge(external_id: "user_usa_123"))
+      expect(Customer.find_by(external_id: "user_usa_123").taxes.sole.code).to eq "lago_eu_tax_exempt"
+
+      create_or_update_customer(french_attributes.merge(external_id: "user_fr_123"))
+      expect(Customer.find_by(external_id: "user_fr_123").taxes.sole.code).to eq "lago_eu_fr_standard"
+
+      create_or_update_customer(italian_attributes.merge(external_id: "user_it_123"))
+      expect(Customer.find_by(external_id: "user_it_123").taxes.sole.code).to eq "lago_eu_it_standard"
+
+      webhooks_sent.clear
+      # Update customer to provide an INVALID EU VAT identifier
+      # Nothing changes and no API call is made
+      create_or_update_customer(external_id: "user_it_123", tax_identification_number: "IT123")
+      expect(Customer.find_by(external_id: "user_it_123").taxes.reload.sole.code).to eq "lago_eu_it_standard"
+      expect(webhooks_sent.first { _1["webhook_type"] == "customer.vies_check" }.dig("customer", "vies_check")).to eq({
+        "valid" => false,
+        "valid_format" => false
+      })
+
+      webhooks_sent.clear
+      # Update customer to provide a valid EU VAT identifier
+      # A call is made to VIES api, we mock the service rather than the HTTP call because it's a SOAP API
+      # This customer now have 0% vat
+      mock_vies_check!("IT12345678901")
+      create_or_update_customer(external_id: "user_it_123", tax_identification_number: "IT12345678901")
+      expect(Customer.find_by(external_id: "user_it_123").taxes.reload.sole.code).to eq "lago_eu_reverse_charge"
+      expect(webhooks_sent.first { _1["webhook_type"] == "customer.vies_check" }.dig("customer", "vies_check")).to eq({
+        "countryCode" => "IT",
+        "vatNumber" => "IT12345678901"
+      })
+
+      mock_vies_check!("FR12345678901")
+      create_or_update_customer(external_id: "user_fr_123", tax_identification_number: "FR12345678901")
+      expect(Customer.find_by(external_id: "user_fr_123").taxes.sole.code).to eq "lago_eu_reverse_charge"
+
+      customer = Customer.find_by(external_id: "user_it_123")
+      # If I had a custom tax for this Customer
+      # It removes the automatic VAT
+      create_tax(name: "Banking rates", code: "banking_rates", rate: 1.3)
+      create_or_update_customer(external_id: customer.external_id, tax_codes: ["banking_rates"])
+      expect(customer.taxes.sole.code).to eq "banking_rates"
+
+      # Make an invoice with this tax
+      addon = create(:add_on, code: :test, organization:)
+      create_one_off_invoice(customer, [addon], taxes: ["banking_rates"])
+      expect(customer.invoices.sole.taxes.sole.code).to eq "banking_rates"
+
+      # Then, remove the tax_identification_number for the customer
+      # The custom tax is overridden by the default VAT of the country, even if an invoice used the previous taxes
+      create_or_update_customer(external_id: customer.external_id, tax_identification_number: nil)
+      expect(customer.taxes.sole.code).to eq "lago_eu_it_standard"
+    end
+  end
+
+  context "when VIES returns an error" do
+    it "does not change taxes but send the webhook" do
+      enable_eu_tax_management!
+
+      create_or_update_customer(french_attributes.merge(external_id: "user_fr_123"))
+      expect(Customer.find_by(external_id: "user_fr_123").taxes.sole.code).to eq "lago_eu_fr_standard"
+
+      webhooks_sent.clear
+      vat_number = "FR12345678901"
+      allow_any_instance_of(Valvat).to receive(:exists?) # rubocop:disable RSpec/AnyInstance
+        .and_raise(::Valvat::RateLimitError.new("rate limit exceeded", Valvat::Lookup::VIES))
+
+      create_or_update_customer(external_id: "user_fr_123", tax_identification_number: vat_number)
+
+      expect(Customer.find_by(external_id: "user_fr_123").taxes.reload.sole.code).to eq "lago_eu_fr_standard"
+      expect(webhooks_sent.first { _1["webhook_type"] == "customer.vies_check" }.dig("customer", "vies_check")).to eq({
+        "valid" => false,
+        "valid_format" => true,
+        "error" => "The VIES web service returned the error: rate limit exceeded"
+      })
+    end
+  end
+
+  context "when customer are created before the feature was enabled" do
+    it "does not create taxes until the customer is updated" do
+      create_or_update_customer(american_attributes.merge(external_id: "user_usa_123"))
+      expect(Customer.find_by(external_id: "user_usa_123").taxes).to be_empty
+
+      enable_eu_tax_management!
+      expect(Customer.find_by(external_id: "user_usa_123").taxes).to be_empty
+
+      create_or_update_customer(external_id: "user_usa_123", tax_identification_number: "US-111") # Not EU VAT
+      expect(Customer.find_by(external_id: "user_usa_123").taxes.sole.code).to eq "lago_eu_tax_exempt"
+    end
+  end
+
+  context "when customer changes country" do
+    it "updates taxes" do
+      enable_eu_tax_management!
+
+      create_or_update_customer(french_attributes.merge(external_id: "user_moving"))
+      customer = Customer.find_by(external_id: "user_moving")
+      expect(customer.reload.taxes.sole.code).to eq "lago_eu_fr_standard"
+
+      create_or_update_customer(external_id: customer.external_id, country: "DE")
+      expect(customer.reload.taxes.sole.code).to eq "lago_eu_de_standard"
+    end
+  end
+
+  context "when customer have an invoice with other taxes" do
+    it "does not affect the customer taxes" do
+      enable_eu_tax_management!
+
+      create_or_update_customer(italian_attributes.merge(external_id: "user_it_123"))
+      customer = Customer.find_by(external_id: "user_it_123")
+      expect(customer.taxes.sole.code).to eq "lago_eu_it_standard"
+
+      # Make an invoice with another tax
+      create_tax(name: "Banking rates", code: "banking_rates", rate: 1.3)
+      addon = create(:add_on, code: :test, organization:)
+      create_one_off_invoice(customer, [addon], taxes: ["banking_rates"])
+      expect(customer.invoices.sole.taxes.sole.code).to eq "banking_rates"
+
+      # The customer tax is unaffected
+      expect(customer.taxes.sole.code).to eq "lago_eu_it_standard"
+    end
+  end
+
+  context "when organization has a default tax" do
+    it "does not affect the customer taxes" do
+      enable_eu_tax_management!
+      organization.taxes.where(code: "lago_eu_fr_standard").update!(applied_to_organization: true)
+
+      mock_vies_check!("IT12345678901")
+      create_or_update_customer(italian_attributes.merge(
+        external_id: "user_it_123", tax_identification_number: "IT12345678901"
+      ))
+      customer = Customer.find_by(external_id: "user_it_123")
+      expect(customer.taxes.reload.sole.code).to eq "lago_eu_reverse_charge"
+
+      create_subscription({
+        external_customer_id: customer.external_id,
+        external_id: "sub_#{customer.external_id}",
+        plan_code: plan.code
+      })
+
+      # The default organization tax is not applied
+      expect(customer.invoices.sole.taxes.sole.code).to eq "lago_eu_reverse_charge"
+    end
+  end
+
+  context "when charge has a dedicated tax" do
+    it "does not affect the customer taxes" do
+      enable_eu_tax_management!
+      billable_metric = create(:billable_metric, organization:, field_name: "item_id")
+      create(:standard_charge, :pay_in_advance, billable_metric:, plan:, taxes: [Tax.find_by(code: "lago_eu_fr_standard")])
+
+      mock_vies_check!("IT12345678901")
+      create_or_update_customer(italian_attributes.merge(
+        external_id: "user_it_123", tax_identification_number: "IT12345678901"
+      ))
+      customer = Customer.find_by(external_id: "user_it_123")
+      expect(customer.taxes.reload.sole.code).to eq "lago_eu_reverse_charge"
+
+      create_subscription({
+        external_customer_id: customer.external_id,
+        external_id: "sub_#{customer.external_id}",
+        plan_code: plan.code
+      })
+      expect(customer.invoices.sole.taxes.sole.code).to eq "lago_eu_reverse_charge"
+
+      create_event({
+        code: billable_metric.code,
+        transaction_id: SecureRandom.uuid,
+        external_subscription_id: "sub_#{customer.external_id}"
+      })
+
+      # The Advance fee charge has the charge taxes even if the customer has a different tax
+      advance_fee_invoice = customer.invoices.order(created_at: :desc).first
+      expect(advance_fee_invoice.taxes.sole.code).to eq "lago_eu_fr_standard"
+      expect(advance_fee_invoice.fees.charge.sole.taxes.sole.code).to eq "lago_eu_fr_standard"
+      expect(customer.taxes.reload.sole.code).to eq "lago_eu_reverse_charge"
+    end
+  end
+end
--- spec/serializers/v1/customer_serializer_spec.rb
+++ spec/serializers/v1/customer_serializer_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe ::V1::CustomerSerializer do
     described_class.new(customer, root_name: "customer", includes: %i[taxes integration_customers applicable_invoice_custom_sections])
   end
 
+  let(:result) { JSON.parse(serializer.to_json) }
   let(:customer) { create(:customer, :with_salesforce_integration) }
   let(:metadata) { create(:customer_metadata, customer:) }
   let(:tax) { create(:tax, organization: customer.organization) }
@@ -21,55 +22,51 @@ RSpec.describe ::V1::CustomerSerializer do
   end
 
   it "serializes the object" do
-    result = JSON.parse(serializer.to_json)
-
-    aggregate_failures do
-      expect(result["customer"]["lago_id"]).to eq(customer.id)
-      expect(result["customer"]["billing_entity_code"]).to eq(customer.billing_entity.code)
-      expect(result["customer"]["external_id"]).to eq(customer.external_id)
-      expect(result["customer"]["account_type"]).to eq(customer.account_type)
-      expect(result["customer"]["name"]).to eq(customer.name)
-      expect(result["customer"]["firstname"]).to eq(customer.firstname)
-      expect(result["customer"]["lastname"]).to eq(customer.lastname)
-      expect(result["customer"]["customer_type"]).to eq(customer.customer_type)
-      expect(result["customer"]["sequential_id"]).to eq(customer.sequential_id)
-      expect(result["customer"]["slug"]).to eq(customer.slug)
-      expect(result["customer"]["created_at"]).to eq(customer.created_at.iso8601)
-      expect(result["customer"]["updated_at"]).to eq(customer.updated_at.iso8601)
-      expect(result["customer"]["country"]).to eq(customer.country)
-      expect(result["customer"]["address_line1"]).to eq(customer.address_line1)
-      expect(result["customer"]["address_line2"]).to eq(customer.address_line2)
-      expect(result["customer"]["state"]).to eq(customer.state)
-      expect(result["customer"]["zipcode"]).to eq(customer.zipcode)
-      expect(result["customer"]["email"]).to eq(customer.email)
-      expect(result["customer"]["city"]).to eq(customer.city)
-      expect(result["customer"]["url"]).to eq(customer.url)
-      expect(result["customer"]["phone"]).to eq(customer.phone)
-      expect(result["customer"]["logo_url"]).to eq(customer.logo_url)
-      expect(result["customer"]["legal_name"]).to eq(customer.legal_name)
-      expect(result["customer"]["legal_number"]).to eq(customer.legal_number)
-      expect(result["customer"]["currency"]).to eq(customer.currency)
-      expect(result["customer"]["timezone"]).to eq(customer.timezone)
-      expect(result["customer"]["applicable_timezone"]).to eq(customer.applicable_timezone)
-      expect(result["customer"]["net_payment_term"]).to eq(customer.net_payment_term)
-      expect(result["customer"]["finalize_zero_amount_invoice"]).to eq(customer.finalize_zero_amount_invoice)
-      expect(result["customer"]["billing_configuration"]["payment_provider"]).to eq(customer.payment_provider)
-      expect(result["customer"]["billing_configuration"]["payment_provider_code"]).to eq(customer.payment_provider_code)
-      expect(result["customer"]["billing_configuration"]["invoice_grace_period"]).to eq(customer.invoice_grace_period)
-      expect(result["customer"]["billing_configuration"]["document_locale"]).to eq(customer.document_locale)
-      expect(result["customer"]["shipping_address"]["address_line1"]).to eq("test1")
-      expect(result["customer"]["shipping_address"]["city"]).to eq("Paris")
-      expect(result["customer"]["shipping_address"]["zipcode"]).to eq("002")
-      expect(result["customer"]["metadata"].first["lago_id"]).to eq(metadata.id)
-      expect(result["customer"]["metadata"].first["key"]).to eq(metadata.key)
-      expect(result["customer"]["metadata"].first["value"]).to eq(metadata.value)
-      expect(result["customer"]["metadata"].first["display_in_invoice"]).to eq(metadata.display_in_invoice)
-      expect(result["customer"]["tax_identification_number"]).to eq(customer.tax_identification_number)
-      expect(result["customer"]["taxes"].count).to eq(1)
-      expect(result["customer"]["integration_customers"].count).to eq(1)
-      expect(result["customer"]["applicable_invoice_custom_sections"].count).to eq(1)
-      expect(result["customer"]["skip_invoice_custom_sections"]).to eq(false)
-    end
+    expect(result["customer"]["lago_id"]).to eq(customer.id)
+    expect(result["customer"]["billing_entity_code"]).to eq(customer.billing_entity.code)
+    expect(result["customer"]["external_id"]).to eq(customer.external_id)
+    expect(result["customer"]["account_type"]).to eq(customer.account_type)
+    expect(result["customer"]["name"]).to eq(customer.name)
+    expect(result["customer"]["firstname"]).to eq(customer.firstname)
+    expect(result["customer"]["lastname"]).to eq(customer.lastname)
+    expect(result["customer"]["customer_type"]).to eq(customer.customer_type)
+    expect(result["customer"]["sequential_id"]).to eq(customer.sequential_id)
+    expect(result["customer"]["slug"]).to eq(customer.slug)
+    expect(result["customer"]["created_at"]).to eq(customer.created_at.iso8601)
+    expect(result["customer"]["updated_at"]).to eq(customer.updated_at.iso8601)
+    expect(result["customer"]["country"]).to eq(customer.country)
+    expect(result["customer"]["address_line1"]).to eq(customer.address_line1)
+    expect(result["customer"]["address_line2"]).to eq(customer.address_line2)
+    expect(result["customer"]["state"]).to eq(customer.state)
+    expect(result["customer"]["zipcode"]).to eq(customer.zipcode)
+    expect(result["customer"]["email"]).to eq(customer.email)
+    expect(result["customer"]["city"]).to eq(customer.city)
+    expect(result["customer"]["url"]).to eq(customer.url)
+    expect(result["customer"]["phone"]).to eq(customer.phone)
+    expect(result["customer"]["logo_url"]).to eq(customer.logo_url)
+    expect(result["customer"]["legal_name"]).to eq(customer.legal_name)
+    expect(result["customer"]["legal_number"]).to eq(customer.legal_number)
+    expect(result["customer"]["currency"]).to eq(customer.currency)
+    expect(result["customer"]["timezone"]).to eq(customer.timezone)
+    expect(result["customer"]["applicable_timezone"]).to eq(customer.applicable_timezone)
+    expect(result["customer"]["net_payment_term"]).to eq(customer.net_payment_term)
+    expect(result["customer"]["finalize_zero_amount_invoice"]).to eq(customer.finalize_zero_amount_invoice)
+    expect(result["customer"]["billing_configuration"]["payment_provider"]).to eq(customer.payment_provider)
+    expect(result["customer"]["billing_configuration"]["payment_provider_code"]).to eq(customer.payment_provider_code)
+    expect(result["customer"]["billing_configuration"]["invoice_grace_period"]).to eq(customer.invoice_grace_period)
+    expect(result["customer"]["billing_configuration"]["document_locale"]).to eq(customer.document_locale)
+    expect(result["customer"]["shipping_address"]["address_line1"]).to eq("test1")
+    expect(result["customer"]["shipping_address"]["city"]).to eq("Paris")
+    expect(result["customer"]["shipping_address"]["zipcode"]).to eq("002")
+    expect(result["customer"]["metadata"].first["lago_id"]).to eq(metadata.id)
+    expect(result["customer"]["metadata"].first["key"]).to eq(metadata.key)
+    expect(result["customer"]["metadata"].first["value"]).to eq(metadata.value)
+    expect(result["customer"]["metadata"].first["display_in_invoice"]).to eq(metadata.display_in_invoice)
+    expect(result["customer"]["tax_identification_number"]).to eq(customer.tax_identification_number)
+    expect(result["customer"]["taxes"].count).to eq(1)
+    expect(result["customer"]["integration_customers"].count).to eq(1)
+    expect(result["customer"]["applicable_invoice_custom_sections"].count).to eq(1)
+    expect(result["customer"]["skip_invoice_custom_sections"]).to eq(false)
   end
 
   context "with a stripe customer" do
@@ -81,9 +78,18 @@ RSpec.describe ::V1::CustomerSerializer do
     end
 
     it "serializes the object" do
-      result = JSON.parse(serializer.to_json)
       expect(result["customer"]["billing_configuration"]["provider_customer_id"]).to eq(stripe_customer.provider_customer_id)
       expect(result["customer"]["billing_configuration"]["provider_payment_methods"]).to eq(stripe_customer.provider_payment_methods)
+    end
+  end
+
+  context "with a VIES check" do
+    subject(:serializer) { described_class.new(customer, root_name: "customer", includes: %i[vies_check], vies_check: {custom_hash: "yes"}) }
+
+    let(:customer) { create(:customer, :with_salesforce_integration, tax_identification_number: "IT12345678901") }
+
+    it "adds vies_check to customer" do
+      expect(result["customer"]["vies_check"]).to eq({"custom_hash" => "yes"})
     end
   end
 end
