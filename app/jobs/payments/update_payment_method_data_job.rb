259dccfe5a4c56813f8e184463be71907ecae0fc
Julien Bourdeau <julien@julienbourdeau.com>
2025-04-14 14:59:58 +0200

    unique job

--- app/jobs/payments/update_payment_method_data_job.rb
+++ app/jobs/payments/update_payment_method_data_job.rb
@@ -4,6 +4,9 @@ module Payments
   class UpdatePaymentMethodDataJob < ApplicationJob
     queue_as "default"
 
+    # NOTE: Even if the perform method is protected against running this job multple time
+    unique :until_executed
+
     retry_on ::Stripe::RateLimitError, wait: :polynomially_longer, attempts: 5
 
     def perform(payment:, provider_payment_method_id:)
