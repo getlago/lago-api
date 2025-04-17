0362dffc85e0b77e4e7f4f057f86fd9b395c3508
Mario Diniz <mariohenriquediniz@gmail.com>
2025-04-17 12:06:26 -0300

    fixing linter and dumping the graphql schema

--- app/graphql/types/activity_logs/object.rb
+++ app/graphql/types/activity_logs/object.rb
@@ -10,13 +10,13 @@ module Types
       field :activity_source, Types::ActivityLogs::ActivitySourceTypeEnum, null: false
       field :activity_type, String, null: false
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+      field :external_customer_id, String
+      field :external_subscription_id, String
       field :logged_at, GraphQL::Types::ISO8601DateTime, null: false
       field :organization, Types::Organizations::OrganizationType
       field :resource_changes, String
       field :resource_id, String, null: false
       field :resource_type, String, null: false
-      field :external_customer_id, String
-      field :external_subscription_id, String
       field :user, Types::UserType
     end
   end
