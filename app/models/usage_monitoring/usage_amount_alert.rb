c9d4198c97525092177480df2e3df33f33243ae4
Julien Bourdeau <julien@julienbourdeau.com>
2025-04-18 11:03:35 +0200

    alert have charge, not billable_metric !

--- app/models/usage_monitoring/usage_amount_alert.rb
+++ app/models/usage_monitoring/usage_amount_alert.rb
@@ -36,20 +36,20 @@ end
 #  previous_value           :decimal(30, 5)   default(0.0), not null
 #  created_at               :datetime         not null
 #  updated_at               :datetime         not null
-#  billable_metric_id       :uuid
+#  charge_id                :uuid
 #  organization_id          :uuid             not null
 #  subscription_external_id :string           not null
 #
 # Indexes
 #
-#  index_alerts_unique_per_type_per_customer                  (subscription_external_id,organization_id,alert_type) UNIQUE WHERE ((billable_metric_id IS NULL) AND (deleted_at IS NULL))
-#  index_alerts_unique_per_type_per_customer_with_bm          (subscription_external_id,organization_id,alert_type,billable_metric_id) UNIQUE WHERE ((billable_metric_id IS NOT NULL) AND (deleted_at IS NULL))
-#  index_usage_monitoring_alerts_on_billable_metric_id        (billable_metric_id)
+#  idx_alerts_unique_per_type_per_customer                    (subscription_external_id,organization_id,alert_type) UNIQUE WHERE ((charge_id IS NULL) AND (deleted_at IS NULL))
+#  idx_alerts_unique_per_type_per_customer_with_charge        (subscription_external_id,organization_id,alert_type,charge_id) UNIQUE WHERE ((charge_id IS NOT NULL) AND (deleted_at IS NULL))
+#  index_usage_monitoring_alerts_on_charge_id                 (charge_id)
 #  index_usage_monitoring_alerts_on_organization_id           (organization_id)
 #  index_usage_monitoring_alerts_on_subscription_external_id  (subscription_external_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (billable_metric_id => billable_metrics.id)
+#  fk_rails_...  (charge_id => charges.id)
 #  fk_rails_...  (organization_id => organizations.id)
 #
--- db/migrate/20250418092036_create_usage_monitoring_alerts.rb
+++ db/migrate/20250418092036_create_usage_monitoring_alerts.rb
@@ -5,7 +5,7 @@ class CreateUsageMonitoringAlerts < ActiveRecord::Migration[7.2]
     create_table :usage_monitoring_alerts, id: :uuid do |t|
       t.references :organization, type: :uuid, foreign_key: true, null: false, index: true
       t.string :subscription_external_id, null: false, index: true
-      t.references :billable_metric, type: :uuid, foreign_key: true, null: true, index: true # TODO: Is index needed?
+      t.references :charge, type: :uuid, foreign_key: true, null: true, index: true # TODO: Is index needed?
       t.string :alert_type, null: false # Rails STI
       t.numeric :previous_value, precision: 30, scale: 5, null: false, default: 0
       t.datetime :last_processed_at
@@ -15,12 +15,12 @@ class CreateUsageMonitoringAlerts < ActiveRecord::Migration[7.2]
 
       t.index %w[subscription_external_id organization_id alert_type],
         unique: true,
-        name: "index_alerts_unique_per_type_per_customer",
-        where: "(billable_metric_id IS NULL AND deleted_at IS NULL)"
-      t.index %w[subscription_external_id organization_id alert_type billable_metric_id],
+        name: "idx_alerts_unique_per_type_per_customer",
+        where: "(charge_id IS NULL AND deleted_at IS NULL)"
+      t.index %w[subscription_external_id organization_id alert_type charge_id],
         unique: true,
-        name: "index_alerts_unique_per_type_per_customer_with_bm",
-        where: "(billable_metric_id IS NOT NULL AND deleted_at IS NULL)"
+        name: "idx_alerts_unique_per_type_per_customer_with_charge",
+        where: "(charge_id IS NOT NULL AND deleted_at IS NULL)"
     end
 
     create_table :usage_monitoring_alert_thresholds, id: :uuid do |t|
