3c527f098f3a051cd3db1d8240ccf6fa920726f8
Anna Velentsevich <annvelents@gmail.com>
2025-04-22 11:19:10 +0200

    Feat/multientities 4 graphql taxes (#3420)
    ## Context
    
    Added graphql endpoints to apply and remove taxes

---
+++ spec/graphql/resolvers/billing_entity_taxes_resolver_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Resolvers::BillingEntityTaxesResolver, type: :graphql do
+  let(:query) do
+    <<~GQL
+      query($billing_entity_id: ID!) {
+        billingEntityTaxes(billingEntityId: $billing_entity_id) {
+          collection {
+            id
+            code
+            name
+            rate
+            description
+          }
+        }
+      }
+    GQL
+  end
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:billing_entity) { organization.default_billing_entity }
+  let(:tax1) { create(:tax, organization: organization) }
+  let(:tax2) { create(:tax, organization: organization) }
+
+  before do
+    billing_entity.taxes << [tax1, tax2]
+  end
+
+  it "returns billing entity taxes" do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: organization,
+      permissions: ["billing_entities:view"],
+      query:,
+      variables: {billing_entity_id: billing_entity.id}
+    )
+
+    taxes_data = result["data"]["billingEntityTaxes"]["collection"]
+    expect(taxes_data.count).to eq(2)
+    expect(taxes_data.map { |t| t["id"] }).to include(tax1.id, tax2.id)
+  end
+
+  it "returns error if billing entity is not found" do
+    result = execute_graphql(
+      current_user: membership.user,
+      current_organization: organization,
+      permissions: ["billing_entities:view"],
+      query:,
+      variables: {billing_entity_id: "invalid"}
+    )
+
+    expect_graphql_error(result:, message: "Resource not found")
+  end
+end
