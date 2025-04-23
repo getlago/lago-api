5cae72a0cb18e0f6f707e0424f9aba3a6a4c7a27
Ancor Cruz <hello@ancorcruz.com>
2025-04-23 08:49:25 +0100

    Fix tests

--- spec/services/customers/update_invoice_payment_due_date_service_spec.rb
+++ spec/services/customers/update_invoice_payment_due_date_service_spec.rb
@@ -7,13 +7,22 @@ RSpec.describe Customers::UpdateInvoicePaymentDueDateService, type: :service do
 
   let(:membership) { create(:membership) }
   let(:organization) { membership.organization }
-  let(:customer) { create(:customer, organization:, net_payment_term: customer_net_payment_term) }
+  let(:billing_entity) { create(:billing_entity, organization:, net_payment_term: 15) }
+  let(:customer) { create(:customer, organization:, billing_entity:, net_payment_term: customer_net_payment_term) }
   let(:net_payment_term) { 30 }
   let(:customer_net_payment_term) { nil }
 
   describe "#call" do
     let(:draft_invoice) do
-      create(:invoice, status: :draft, customer:, issuing_date: DateTime.parse("21 Jun 2022"), net_payment_term: customer.applicable_net_payment_term, organization:)
+      create(
+        :invoice,
+        organization:,
+        billing_entity:,
+        customer:,
+        status: :draft,
+        issuing_date: DateTime.parse("21 Jun 2022"),
+        net_payment_term: customer.applicable_net_payment_term
+      )
     end
 
     before do
@@ -30,20 +39,16 @@ RSpec.describe Customers::UpdateInvoicePaymentDueDateService, type: :service do
       let(:customer_net_payment_term) { 20 }
       let(:net_payment_term) { nil }
 
-      before do
-        organization.update(net_payment_term: 15)
-      end
-
-      it "sets the payment_due_date of the draft_invoice to the org level value" do
+      it "sets the payment_due_date of the draft_invoice to the billing entity level value" do
         expect { update_service.call }.to change { draft_invoice.reload.payment_due_date }
           .from(DateTime.parse("21 Jun 2022"))
-          .to(DateTime.parse("21 Jun 2022") + organization.net_payment_term.days)
+          .to(DateTime.parse("21 Jun 2022") + billing_entity.net_payment_term.days)
       end
 
       it "sets the net_payment_term of the draft_invoice to the org level value" do
         expect { update_service.call }.to change { draft_invoice.reload.net_payment_term }
           .from(customer_net_payment_term)
-          .to(organization.net_payment_term)
+          .to(billing_entity.net_payment_term)
       end
     end
   end
