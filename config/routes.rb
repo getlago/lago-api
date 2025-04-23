7c7e1d5b7214b6d79f2bd56f191e6e22379cd458
Anna Velentsevich <annvelents@gmail.com>
2025-04-23 14:25:35 +0200

    Feat(multientities): add api endpoint and service to manage taxes all together (#3483)
    ## Context
    
    We want to allow to manage applied taxes on billing_entity via API
    
    ## Description
    
    instead of having manually to send "taxes_to_remove" and then
    "taxes_to_apply", we've
    - introduced a new service `BillinEntities::Taxes::ManageService`
    - and new endpoint: `billing_entities#manage_taxes`

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ Rails.application.routes.draw do
         get :overdue_balance, to: "overdue_balances#index", as: :overdue_balance
       end
 
-      resources :billing_entities, param: :code, only: %i[index show]
+      resources :billing_entities, param: :code, only: %i[index show] do
+        post :manage_taxes, on: :member
+      end
 
       resources :customers, param: :external_id, only: %i[create index show destroy] do
         get :portal_url
