d1112d1cbab13f8d759267dc9406852d13f09afe
Ivan Novosad <ivan@getlago.com>
2025-04-24 13:44:25 +0200

    feat(usages): Improve specs

--- app/controllers/api/v1/data_api/usages_controller.rb
+++ app/controllers/api/v1/data_api/usages_controller.rb
@@ -5,7 +5,7 @@ module Api
     module DataApi
       class UsagesController < Api::V1::DataApi::BaseController
         def index
-          @result = ::DataApi::UsagesService.call(current_organization, **filters)
+          @result = ::DataApi::UsagesService.call(current_organization, **filter_params)
 
           if @result.success?
             render_result(@result)
@@ -20,21 +20,21 @@ module Api
           render(json: {"usages" => result.usages}.to_json)
         end
 
-        def filters
-          {
-            time_granularity: params[:time_granularity],
-            currency: params[:currency],
-            from_date: params[:from_date],
-            to_date: params[:to_date],
-            customer_type: params[:customer_type],
-            external_customer_id: params[:external_customer_id],
-            customer_country: params[:customer_country],
-            external_subscription_id: params[:external_subscription_id],
-            plan_code: params[:plan_code],
-            billable_metric_code: params[:billable_metric_code],
-            grouped_by: params[:grouped_by],
-            filter_values: params[:filter_values]
-          }.compact
+        def filter_params
+          params.permit(
+            :time_granularity,
+            :currency,
+            :from_date,
+            :to_date,
+            :customer_type,
+            :external_customer_id,
+            :customer_country,
+            :external_subscription_id,
+            :plan_code,
+            :billable_metric_code,
+            :grouped_by,
+            filter_values: []
+          ).to_h.compact
         end
       end
     end
