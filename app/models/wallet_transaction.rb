3409895343bad9784b63c2314f28ffad4968107c
Vincent Pochet <vincent@getlago.com>
2025-04-25 15:17:58 +0200

    chore(organization): Add organization_id to wallet_transactions table

--- app/models/wallet_transaction.rb
+++ app/models/wallet_transaction.rb
@@ -4,6 +4,7 @@ class WalletTransaction < ApplicationRecord
   include PaperTrailTraceable
 
   belongs_to :wallet
+  belongs_to :organization, optional: true
 
   # these two relationships are populated only for outbound transactions
   belongs_to :invoice, optional: true
@@ -74,17 +75,20 @@ end
 #  updated_at                          :datetime         not null
 #  credit_note_id                      :uuid
 #  invoice_id                          :uuid
+#  organization_id                     :uuid
 #  wallet_id                           :uuid             not null
 #
 # Indexes
 #
-#  index_wallet_transactions_on_credit_note_id  (credit_note_id)
-#  index_wallet_transactions_on_invoice_id      (invoice_id)
-#  index_wallet_transactions_on_wallet_id       (wallet_id)
+#  index_wallet_transactions_on_credit_note_id   (credit_note_id)
+#  index_wallet_transactions_on_invoice_id       (invoice_id)
+#  index_wallet_transactions_on_organization_id  (organization_id)
+#  index_wallet_transactions_on_wallet_id        (wallet_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (credit_note_id => credit_notes.id)
 #  fk_rails_...  (invoice_id => invoices.id)
+#  fk_rails_...  (organization_id => organizations.id)
 #  fk_rails_...  (wallet_id => wallets.id)
 #
