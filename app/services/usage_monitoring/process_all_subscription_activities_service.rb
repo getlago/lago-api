ef45599bab4268f848f0870c5955ce3ee7ade91a
Julien Bourdeau <julien@julienbourdeau.com>
2025-04-25 07:13:44 +0200

    process_all_subscription_activities_service

---
+++ app/services/usage_monitoring/process_all_subscription_activities_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module UsageMonitoring
+  class ProcessAllSubscriptionActivitiesService < BaseService
+    Result = BaseResult
+
+    def call
+      # NOTE: If we need to handle different delays per organization, this would be done here.
+      #     This is why it's a dedicated service and not just done in the job
+
+      Organization.with_activity_tracking.select(:id).find_each do |organization|
+        ProcessOrganizationSubscriptionActivitiesJob.perform_later(organization)
+      end
+
+      result
+    end
+  end
+end
---
+++ spec/services/usage_monitoring/process_all_subscription_activities_service_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UsageMonitoring::ProcessAllSubscriptionActivitiesService, type: :service do
+  describe "#call" do
+    subject(:service) { described_class.new }
+
+    before do
+      allow(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob).to receive(:perform_later)
+    end
+
+    it "enqueues ProcessOrganizationSubscriptionActivitiesJob for organizations with activity tracking" do
+      organization1 = create(:organization, premium_integrations: Organization::INTEGRATIONS_TRACKING_ACTIVITY)
+      organization2 = create(:organization, premium_integrations: Organization::INTEGRATIONS_TRACKING_ACTIVITY)
+      organization3 = create(:organization, premium_integrations: ["salesforce"]) # No activity tracking
+
+      result = service.call
+
+      expect(result).to be_success
+      expect(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob).to have_received(:perform_later).with(organization1)
+      expect(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob).to have_received(:perform_later).with(organization2)
+      expect(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob).not_to have_received(:perform_later).with(organization3)
+    end
+
+    context "when there are no organizations with activity tracking" do
+      it "does not enqueue any jobs" do
+        create(:organization, premium_integrations: ["salesforce"])
+
+        service.call
+
+        expect(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob).not_to receive(:perform_later)
+      end
+    end
+  end
+end
