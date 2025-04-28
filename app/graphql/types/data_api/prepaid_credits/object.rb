1303a51c7367cc7a50ff2ea1e6889d12f0adf563
Ivan Novosad <ivannovosad@users.noreply.github.com>
2025-04-28 16:03:52 +0200

    fix(data-api): Fix prepaid credits data types (#3551)
    ## Description
    
    This PR fixes the data types for multiple prepaid credits attributes.

--- app/graphql/types/data_api/prepaid_credits/object.rb
+++ app/graphql/types/data_api/prepaid_credits/object.rb
@@ -8,15 +8,15 @@ module Types
 
         field :amount_currency, Types::CurrencyEnum, null: false
 
-        field :consumed_amount, GraphQL::Types::BigInt, null: false
-        field :offered_amount, GraphQL::Types::BigInt, null: false
-        field :purchased_amount, GraphQL::Types::BigInt, null: false
-        field :voided_amount, GraphQL::Types::BigInt, null: false
+        field :consumed_amount, Float, null: false
+        field :offered_amount, Float, null: false
+        field :purchased_amount, Float, null: false
+        field :voided_amount, Float, null: false
 
-        field :consumed_credits_quantity, GraphQL::Types::BigInt, null: false
-        field :offered_credits_quantity, GraphQL::Types::BigInt, null: false
-        field :purchased_credits_quantity, GraphQL::Types::BigInt, null: false
-        field :voided_credits_quantity, GraphQL::Types::BigInt, null: false
+        field :consumed_credits_quantity, Float, null: false
+        field :offered_credits_quantity, Float, null: false
+        field :purchased_credits_quantity, Float, null: false
+        field :voided_credits_quantity, Float, null: false
 
         field :end_of_period_dt, GraphQL::Types::ISO8601Date, null: false
         field :start_of_period_dt, GraphQL::Types::ISO8601Date, null: false
