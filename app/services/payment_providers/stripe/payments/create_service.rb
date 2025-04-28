364cc418efdaf79c06cca1dd1cd22005b147d1fe
Julien Bourdeau <julien@julienbourdeau.com>
2025-04-28 15:28:51 +0200

    feat(error): Add original_error to FailedResult (#3457)
    ## Context
    
    The system swallows too much details about original error.
    
    ## Description
    
    When introducing payment pre-auth when creating subscription, I
    introduced a new `ProviderError` which adds `error_details` to the API
    response. This was done in isolation just for this feature.
    
    I'm trying to bring this `error_details` to the webhooks too.

--- app/services/payment_providers/stripe/payments/create_service.rb
+++ app/services/payment_providers/stripe/payments/create_service.rb
@@ -207,7 +207,7 @@ module PaymentProviders
           # stripe may return us a Stripe::CardError error if payment_intent was created, but it's processing failed, in this case error would contain payment_intent id
           payment.update!(status: :failed, payable_payment_status:, provider_payment_id: error.error&.payment_intent&.id)
 
-          result.service_failure!(code: "stripe_error", message: error.message)
+          result.service_failure!(code: "stripe_error", message: error.message, error:)
         end
       end
     end
