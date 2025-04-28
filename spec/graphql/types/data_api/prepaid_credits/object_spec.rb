164c1abd165f1b4b497d09ea1cbd4830e32e67c4
Ivan Novosad <ivannovosad@users.noreply.github.com>
2025-04-28 16:03:52 +0200

    fix(data-api): Fix prepaid credits data types (#3551)
    ## Description
    
    This PR fixes the data types for multiple prepaid credits attributes.

--- spec/graphql/types/data_api/prepaid_credits/object_spec.rb
+++ spec/graphql/types/data_api/prepaid_credits/object_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe Types::DataApi::PrepaidCredits::Object do
 
     expect(subject).to have_field(:amount_currency).of_type("CurrencyEnum!")
 
-    expect(subject).to have_field(:purchased_amount).of_type("BigInt!")
-    expect(subject).to have_field(:offered_amount).of_type("BigInt!")
-    expect(subject).to have_field(:consumed_amount).of_type("BigInt!")
-    expect(subject).to have_field(:voided_amount).of_type("BigInt!")
+    expect(subject).to have_field(:purchased_amount).of_type("Float!")
+    expect(subject).to have_field(:offered_amount).of_type("Float!")
+    expect(subject).to have_field(:consumed_amount).of_type("Float!")
+    expect(subject).to have_field(:voided_amount).of_type("Float!")
 
-    expect(subject).to have_field(:purchased_credits_quantity).of_type("BigInt!")
-    expect(subject).to have_field(:offered_credits_quantity).of_type("BigInt!")
-    expect(subject).to have_field(:consumed_credits_quantity).of_type("BigInt!")
-    expect(subject).to have_field(:voided_credits_quantity).of_type("BigInt!")
+    expect(subject).to have_field(:purchased_credits_quantity).of_type("Float!")
+    expect(subject).to have_field(:offered_credits_quantity).of_type("Float!")
+    expect(subject).to have_field(:consumed_credits_quantity).of_type("Float!")
+    expect(subject).to have_field(:voided_credits_quantity).of_type("Float!")
 
     expect(subject).to have_field(:end_of_period_dt).of_type("ISO8601Date!")
     expect(subject).to have_field(:start_of_period_dt).of_type("ISO8601Date!")
