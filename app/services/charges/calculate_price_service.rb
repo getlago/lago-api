18bbb90e65ab70c86d3647161b09d98571d03e50
Ivan Novosad <ivan@getlago.com>
2025-04-30 14:50:48 +0200

    feat(forecast): WIP - Calculate prices for billable metric and units

---
+++ app/services/charges/calculate_price_service.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+module Charges
+  class CalculatePriceService < BaseService
+    Result = BaseResult[:charge_amount_cents, :subscription_amount_cents, :total_amount_cents]
+
+    def initialize(billable_metric:, subscription:, date:, units:)
+      @billable_metric = billable_metric
+      @subscription = subscription
+      @date = date
+      @units = units
+
+      super
+    end
+
+    def call
+      result.charge_amount_cents = calculate_charge_amount
+      result.subscription_amount_cents = plan.amount_cents
+      result.total_amount_cents = result.charge_amount_cents + result.subscription_amount_cents
+      result
+    end
+
+    private
+
+    attr_reader :billable_metric, :subscription, :date, :units
+
+    delegate :plan, to: :subscription
+    delegate :customer, to: :subscription
+
+    def calculate_charge_amount
+      charge = subscription.plan.charges.find_by(billable_metric:)
+      return 0 unless charge
+
+      # For past dates, get the last active charge version
+      # For current/future dates, use the current charge
+      if date.to_time < Time.current
+        version = charge.versions.where("created_at <= ?", date.to_time).order(created_at: :desc).first
+        return 0 unless version
+
+        object = if version.event == "create"
+          version.item
+        else
+          version.reify
+        end
+
+        properties = object.properties.presence || Charges::BuildDefaultPropertiesService.call(object.charge_model)
+      else
+        properties = charge.properties.presence || Charges::BuildDefaultPropertiesService.call(charge.charge_model)
+      end
+
+      properties = Charges::FilterChargeModelPropertiesService.call(charge:, properties:).properties
+
+      charge_model = ChargeModelFactory.new_instance(
+        charge:,
+        aggregation_result: build_aggregation_result,
+        properties:
+      )
+
+      charge_model.apply.amount
+    end
+
+    def build_aggregation_result
+      OpenStruct.new(
+        aggregation: units,
+        total_aggregated_units: units,
+        current_usage_units: units
+      )
+    end
+  end
+end
