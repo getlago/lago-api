b3750f784ceef8b5728ad20cba095d8a6549f2c5
Toon Willems <m@toonwillems.be>
2025-04-30 16:37:31 +0200

    Revert "chore(sentry): Add strackprof profiling with sentry (#3366)" (#3568)

--- config/initializers/newrelic.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-require "newrelic_rpm" if ENV["NEW_RELIC_KEY"].present?
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 if ENV["SENTRY_DSN"].present?
-  require "stackprof" if ENV["SENTRY_ENABLE_PROFILING"].present?
-  require "sentry-ruby"
-  require "sentry-rails"
-  require "sentry-sidekiq"
-
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
     config.release = LagoUtils::Version.call(default: Rails.env).number
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.traces_sample_rate = 0
     config.traces_sample_rate = ENV["SENTRY_TRACES_SAMPLE_RATE"].to_f || 0
-
-    if ENV["SENTRY_ENABLE_PROFILING"].present?
-      config.profiles_sample_rate = ENV["SENTRY_PROFILES_SAMPLE_RATE"].to_f || 0
-    end
-
     config.environment = ENV["SENTRY_ENVIRONMENT"] || Rails.env
   end
 end
