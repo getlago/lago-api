dd225bb5c935700a7a9a80ce9f1c6ae886aa3970
Ancor Cruz <hello@ancorcruz.com>
2025-04-30 08:53:33 +0100

    fix billing entity billing configuration and email settings

--- app/graphql/types/billing_entities/update_input.rb
+++ app/graphql/types/billing_entities/update_input.rb
@@ -32,8 +32,8 @@ module Types
       argument :document_number_prefix, String, required: false
       argument :document_numbering, Types::BillingEntities::DocumentNumberingEnum, required: false
 
-      argument :billing_configuration, Types::BillingEntities::BillingConfigurationInput, required: false, permission: "organization:invoices:view"
-      argument :email_settings, [Types::BillingEntities::EmailSettingsEnum], required: false, permission: "organization:emails:view"
+      argument :billing_configuration, Types::BillingEntities::BillingConfigurationInput, required: false, permission: "billing_entities:invoices:view"
+      argument :email_settings, [Types::BillingEntities::EmailSettingsEnum], required: false, permission: "billing_entities:emails:view"
       argument :finalize_zero_amount_invoice, Boolean, required: false
     end
   end
