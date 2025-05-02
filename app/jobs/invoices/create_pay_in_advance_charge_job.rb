31289594a7338876ff27d54cb74cec1134c186a1
Toon Willems <m@toonwillems.be>
2025-05-02 11:00:48 +0200

    Simplify pay in advance error handling

--- app/jobs/invoices/create_pay_in_advance_charge_job.rb
+++ app/jobs/invoices/create_pay_in_advance_charge_job.rb
@@ -18,21 +18,13 @@ module Invoices
     unique :until_executed, on_conflict: :log
 
     def perform(charge:, event:, timestamp:, invoice: nil)
-      result = Invoices::CreatePayInAdvanceChargeService.call(charge:, event:, timestamp:, invoice:)
+      result = Invoices::CreatePayInAdvanceChargeService.call(charge:, event:, timestamp:)
       return if result.success?
       # NOTE: We don't want a dead job for failed invoice due to the tax reason.
       #       This invoice should be in failed status and can be retried.
       return if tax_error?(result)
 
-      result.raise_if_error! if invoice || result.invoice.nil? || !result.invoice.generating?
-
-      # NOTE: retry the job, but don't pass the invoice as it's not persisted.
-      self.class.set(wait: 3.seconds).perform_later(
-        charge:,
-        event:,
-        timestamp:,
-        invoice: nil
-      )
+      result.raise_if_error!
     end
 
     def lock_key_arguments
