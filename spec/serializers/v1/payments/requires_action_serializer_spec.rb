90ff70d2d58cf8e611afc3a225f223c9d697b2a3
Ancor Cruz <hello@ancorcruz.com>
2025-05-09 12:06:22 +0100

    misc(webhooks): Add lago_id to payment.requires_action webhook payload
    lago_id was missing and it is required to identify the payment for later
    request to endpoint `GET /api/v1/payments/{lago_id}` to collect more
    payment information.

--- spec/serializers/v1/payments/requires_action_serializer_spec.rb
+++ spec/serializers/v1/payments/requires_action_serializer_spec.rb
@@ -16,16 +16,15 @@ RSpec.describe ::V1::Payments::RequiresActionSerializer do
   it "serializes the object" do
     result = JSON.parse(serializer.to_json)
 
-    aggregate_failures do
-      expect(result["payment"]["lago_payable_id"]).to eq(payment.payable.id)
-      expect(result["payment"]["lago_customer_id"]).to eq(payment.payable.customer.id)
-      expect(result["payment"]["status"]).to eq(payment.status)
-      expect(result["payment"]["external_customer_id"]).to eq(payment.payable.customer.external_id)
-      expect(result["payment"]["provider_customer_id"]).to eq(payment.payment_provider_customer.id)
-      expect(result["payment"]["payment_provider_code"]).to eq(payment.payment_provider.code)
-      expect(result["payment"]["payment_provider_type"]).to eq(payment.payment_provider.type)
-      expect(result["payment"]["provider_payment_id"]).to eq(payment.provider_payment_id)
-      expect(result["payment"]["next_action"]).to eq(payment.provider_payment_data)
-    end
+    expect(result["payment"]["lago_id"]).to eq(payment.id)
+    expect(result["payment"]["lago_payable_id"]).to eq(payment.payable.id)
+    expect(result["payment"]["lago_customer_id"]).to eq(payment.payable.customer.id)
+    expect(result["payment"]["status"]).to eq(payment.status)
+    expect(result["payment"]["external_customer_id"]).to eq(payment.payable.customer.external_id)
+    expect(result["payment"]["provider_customer_id"]).to eq(payment.payment_provider_customer.id)
+    expect(result["payment"]["payment_provider_code"]).to eq(payment.payment_provider.code)
+    expect(result["payment"]["payment_provider_type"]).to eq(payment.payment_provider.type)
+    expect(result["payment"]["provider_payment_id"]).to eq(payment.provider_payment_id)
+    expect(result["payment"]["next_action"]).to eq(payment.provider_payment_data)
   end
 end
