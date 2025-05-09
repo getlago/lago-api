c4b3d637323dd9096e94131ed17a812e94f2d5b7
Miguel Pinto <darkymiguel@gmail.com>
2025-05-09 12:55:39 +0100

    feat: wip

--- app/services/credit_notes/create_service.rb
+++ app/services/credit_notes/create_service.rb
@@ -38,7 +38,7 @@ module CreditNotes
           reason:,
           description:,
           credit_status: "available",
-          status: invoice.status
+          status: (invoice.status == "voided") ? "finalized" : invoice.status
         )
 
         credit_note.save! if context != :preview
