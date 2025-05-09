f61f631f4970129760b557dc9f8a2a93ae5879c3
Anna Velentsevich <annvelents@gmail.com>
2025-05-09 11:19:12 +0200

    change the migration way

--- app/jobs/database_migrations/populate_invoices_billing_entity_sequential_id_job.rb
+++ app/jobs/database_migrations/populate_invoices_billing_entity_sequential_id_job.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
-class PopulateInvoicesBillingEntitySequentialIdJob < ApplicationJob
-  queue_as :low_priority
-  unique :until_executed
+module DatabaseMigrations
+  class PopulateInvoicesBillingEntitySequentialIdJob < ApplicationJob
+    queue_as :low_priority
+    unique :until_executed
 
-  BATCH_SIZE = 1000
+    BATCH_SIZE = 1000
 
-  def perform(batch_number = 1)
-    batch = Invoice.where(billing_entity_sequential_id: nil).where.not(organization_sequential_id: nil).limit(BATCH_SIZE)
+    def perform(batch_number = 1)
+      batch = Invoice.where("organization_sequential_id != 0")
+        .where("billing_entity_sequential_id != organization_sequential_id")
+        .limit(BATCH_SIZE)
 
-    if batch.exists?
-      # rubocop:disable Rails/SkipsModelValidations
-      batch.update_all("billing_entity_sequential_id = organization_sequential_id")
-      # rubocop:enable Rails/SkipsModelValidations
+      if batch.exists?
+        # rubocop:disable Rails/SkipsModelValidations
+        batch.update_all("billing_entity_sequential_id = organization_sequential_id")
+        # rubocop:enable Rails/SkipsModelValidations
 
-      # Queue the next batch
-      self.class.perform_later(batch_number + 1)
-    else
-      Rails.logger.info("Finished the execution")
+        # Queue the next batch
+        self.class.perform_later(batch_number + 1)
+      else
+        Rails.logger.info("Finished the execution")
+      end
     end
-  end
 
-  def lock_key_arguments
-    [arguments]
+    def lock_key_arguments
+      [arguments]
+    end
   end
 end
