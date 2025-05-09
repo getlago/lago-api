743fc080a9ed79a62f74995e30df683d2b73c2de
Romain Sempé <romain@getlago.com>
2025-05-09 10:11:44 +0200

    feat(audit_logs): Activity log when deleting a plan

--- app/services/plans/destroy_service.rb
+++ app/services/plans/destroy_service.rb
@@ -7,6 +7,11 @@ module Plans
       super
     end
 
+    activity_loggable(
+      action: "plan.deleted",
+      record: -> { plan }
+    )
+
     def call
       return result.not_found_failure!(resource: "plan") unless plan
 
