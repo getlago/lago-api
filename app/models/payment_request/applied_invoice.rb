86cb19fd97b93ed1496aeb8dfb9e5cf6f6a1588c
Vincent Pochet <vincent@getlago.com>
2025-05-12 11:59:59 +0200

    chore(organization): Add organization_id to invoices_payment_requests table (#3596)
    ## Context
    
    This PR is part of the epic to add `organization_id` on all tables of
    the application
    
    ## Description
    
    The current one is adding the field to the `fees_taxes` table.
    For now the field allows null values. A job to back-fill it has been
    added.
    In a later pull request the not null constraint will be added

--- app/models/payment_request/applied_invoice.rb
+++ app/models/payment_request/applied_invoice.rb
@@ -6,6 +6,7 @@ class PaymentRequest
 
     belongs_to :invoice
     belongs_to :payment_request
+    belongs_to :organization, optional: true
   end
 end
 
@@ -17,16 +18,19 @@ end
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  invoice_id         :uuid             not null
+#  organization_id    :uuid
 #  payment_request_id :uuid             not null
 #
 # Indexes
 #
 #  idx_on_invoice_id_payment_request_id_aa550779a4        (invoice_id,payment_request_id) UNIQUE
 #  index_invoices_payment_requests_on_invoice_id          (invoice_id)
+#  index_invoices_payment_requests_on_organization_id     (organization_id)
 #  index_invoices_payment_requests_on_payment_request_id  (payment_request_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (invoice_id => invoices.id)
+#  fk_rails_...  (organization_id => organizations.id)
 #  fk_rails_...  (payment_request_id => payment_requests.id)
 #
