47ab804d1df4a5cc1d94b9018e227cf36b51bbe0
Ancor Cruz <hello@ancorcruz.com>
2025-05-15 10:10:26 +0100

    fix factory

--- spec/factories/payment_receipts.rb
+++ spec/factories/payment_receipts.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     number { Faker::Alphanumeric.alphanumeric(number: 12) }
     payment
     organization
+    billing_entity { organization&.default_billing_entity || association(:billing_entity) }
   end
 end
