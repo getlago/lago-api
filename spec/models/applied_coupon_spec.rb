42156e1aa96e771526bd882e1a96ff48e1cb9fc5
Miguel Pinto <darkymiguel@gmail.com>
2025-05-15 12:55:15 +0100

    feat: added specs

--- spec/models/applied_coupon_spec.rb
+++ spec/models/applied_coupon_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe AppliedCoupon, type: :model do
       expect(applied_coupon.remaining_amount).to eq(40)
     end
   end
+
+  describe "#mark_as_voided!" do
+    let(:applied_coupon) { build(:applied_coupon, status: :active) }
+
+    it "sets the status to voided and updates voided_at timestamp" do
+      travel_to(Time.zone.local(2025, 1, 1, 15, 0)) do
+        applied_coupon.mark_as_voided!
+        expect(applied_coupon.voided_at).to eq(Time.current)
+        expect(applied_coupon.status.to_sym).to eq(:voided)
+      end
+    end
+  end
 end
