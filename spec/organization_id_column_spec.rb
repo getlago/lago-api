4bd64c08bc16789235cb26e4d2e6fa09092a88e8
Vincent Pochet <vincent@getlago.com>
2025-05-16 14:56:18 +0200

    chore(organization): Add organization_id to integration_mappings table (#3661)
    ## Context
    
    This PR is part of the epic to add `organization_id` on all tables of
    the application
    
    ## Description
    
    The current one is adding the field to the `integration_mappings` table.
    For now the field allows null values. A job to back-fill it has been
    added.
    In a later pull request the not null constraint will be added
    
    Co-authored-by: Anna Velentsevich <annvelents@gmail.com>

--- spec/organization_id_column_spec.rb
+++ spec/organization_id_column_spec.rb
@@ -29,7 +29,6 @@ Rspec.describe "All tables must have an organization_id" do
       charge_filter_values
       integration_collection_mappings
       integration_items
-      integration_mappings
       recurring_transaction_rules
       refunds
       versions
