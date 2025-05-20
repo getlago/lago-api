2f3beb7cf773dc738a313cc0a3f406b3720ba03b
Mario Diniz <mariohenriquediniz@gmail.com>
2025-05-20 11:40:57 -0300

    feat(audit-logs):extract mark invoice payment overdue service (#3686)
    This PR extract the action to mark an invoice as payment overdue to a
    service.

---
+++ app/services/invoices/payments/mark_overdue_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Invoices
+  module Payments
+    class MarkOverdueService < BaseService
+      def initialize(invoice:)
+        @invoice = invoice
+
+        super
+      end
+
+      def call
+        return result.not_found_failure!(resource: "invoice") unless invoice
+        return result.not_allowed_failure!(code: "invoice_not_finalized") unless invoice.finalized?
+        return result.not_allowed_failure!(code: "invoice_payment_already_succeeded") if invoice.payment_succeeded?
+        return result.not_allowed_failure!(code: "invoice_due_date_in_future") if invoice.payment_due_date > Time.current
+        return result.not_allowed_failure!(code: "invoice_dispute_lost") if invoice.payment_dispute_lost_at
+
+        invoice.update!(payment_overdue: true)
+
+        result.invoice = invoice
+
+        SendWebhookJob.perform_later("invoice.payment_overdue", invoice)
+        result
+      end
+
+      private
+
+      attr_reader :invoice
+    end
+  end
+end
