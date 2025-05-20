b990ba7acfb64cf7c63151c6b6fc5a47fa86fc0e
Ivan Novosad <ivan@getlago.com>
2025-05-20 16:04:30 +0200

    fix(daily-usages): Retry job on ActiveRecordError

--- app/jobs/daily_usages/compute_job.rb
+++ app/jobs/daily_usages/compute_job.rb
@@ -4,6 +4,8 @@ module DailyUsages
   class ComputeJob < ApplicationJob
     queue_as "low_priority"
 
+    retry_on ActiveRecord::ActiveRecordError, wait: :polynomially_longer, attempts: 6
+
     def perform(subscription, timestamp:)
       DailyUsages::ComputeService.call(subscription:, timestamp:).raise_if_error!
     end
