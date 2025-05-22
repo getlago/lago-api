ea49ef7db6ff7bb682e34a353078bafe8c185f8d
Lovro Colic <lovro.colic.lc@gmail.com>
2025-05-22 16:22:18 +0200

    fix linter

--- db/migrate/20250522132106_add_precise_credit_columns_to_fees.rb
+++ db/migrate/20250522132106_add_precise_credit_columns_to_fees.rb
@@ -2,7 +2,11 @@
 
 class AddPreciseCreditColumnsToFees < ActiveRecord::Migration[8.0]
   def change
-    add_column :fees, :precise_progressive_credits_amount_cents, :decimal, precision: 30, scale: 5, null: false, default: 0
-    add_column :fees, :precise_credit_notes_amount_cents, :decimal, precision: 30, scale: 5, null: false, default: 0
+    safety_assured do
+      change_table :fees, bulk: true do |t|
+        t.decimal :precise_progressive_credits_amount_cents, precision: 30, scale: 5, null: false, default: 0
+        t.decimal :precise_credit_notes_amount_cents, precision: 30, scale: 5, null: false, default: 0
+      end
+    end
   end
 end
