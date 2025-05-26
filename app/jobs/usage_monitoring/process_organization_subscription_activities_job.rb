a9eb33e54048e84d0489378006f57e3bef261b8e
Julien Bourdeau <julien@julienbourdeau.com>
2025-05-26 16:47:40 +0200

    fix(alerting): Better All SubscriptionActivity job (#3714)
    ## Context
    
    Alerting is not a premium integration, any premium organization can use
    it.
    
    ## Description
    
    Instead of enqueueing a job for all organizations, we use the db to get
    a list of organization and a count of jobs.
    The previous log is moved the clock jobs and more importantly: it's
    formatted.

--- app/jobs/usage_monitoring/process_organization_subscription_activities_job.rb
+++ app/jobs/usage_monitoring/process_organization_subscription_activities_job.rb
@@ -8,13 +8,7 @@ module UsageMonitoring
       return unless License.premium?
 
       organization = Organization.find(organization_id)
-      result = UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService.call!(organization:)
-
-      if result.nb_jobs_enqueued > 0
-        Rails.logger.info(
-          "[#{organization.id}] ProcessOrganizationSubscriptionActivitiesService enqueued #{result.nb_jobs_enqueued} jobs"
-        )
-      end
+      UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService.call!(organization:)
     end
   end
 end
--- spec/jobs/usage_monitoring/process_organization_subscription_activities_job_spec.rb
+++ spec/jobs/usage_monitoring/process_organization_subscription_activities_job_spec.rb
@@ -4,11 +4,9 @@ require "rails_helper"
 
 RSpec.describe UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob, type: :job do
   let(:organization) { create(:organization) }
-  let(:result_double) { instance_double("UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService::Result", nb_jobs_enqueued: 5) }
 
   before do
-    allow(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService).to receive(:call!).and_return(result_double)
-    allow(Rails.logger).to receive(:info)
+    allow(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService).to receive(:call!)
   end
 
   context "when license is premium" do
@@ -18,18 +16,12 @@ RSpec.describe UsageMonitoring::ProcessOrganizationSubscriptionActivitiesJob, ty
       described_class.perform_now(organization.id)
       expect(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService).to have_received(:call!).with(organization:)
     end
-
-    it "logs the number of jobs enqueued" do
-      described_class.perform_now(organization.id)
-      expect(Rails.logger).to have_received(:info).with("[#{organization.id}] ProcessOrganizationSubscriptionActivitiesService enqueued 5 jobs")
-    end
   end
 
   context "when license is not premium" do
     it "does not call the service or log" do
       described_class.perform_now(organization.id)
       expect(UsageMonitoring::ProcessOrganizationSubscriptionActivitiesService).not_to have_received(:call!)
-      expect(Rails.logger).not_to have_received(:info)
     end
   end
 end
