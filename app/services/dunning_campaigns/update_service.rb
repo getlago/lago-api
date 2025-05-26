7ace13afc513a8d56ad8be4235993f5f99e2c4b2
Anna Velentsevich <annvelents@gmail.com>
2025-05-26 16:16:33 +0200

    BillingEntity:applied_dunning_campaign to follow dunning campaign applied on org

--- app/services/dunning_campaigns/update_service.rb
+++ app/services/dunning_campaigns/update_service.rb
@@ -102,6 +102,7 @@ module DunningCampaigns
         .update_all(applied_to_organization: false) # rubocop:disable Rails/SkipsModelValidations
 
       organization.reset_customers_last_dunning_campaign_attempt
+      organization.default_billing_entity.update(applied_dunning_campaign: dunning_campaign)
 
       customers_fallback_campaign.update_all( # rubocop:disable Rails/SkipsModelValidations
         last_dunning_campaign_attempt_at: nil,
