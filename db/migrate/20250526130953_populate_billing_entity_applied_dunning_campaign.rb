7002222b597e6f6abfbde1e57fdc55e2cb89d6c5
Anna Velentsevich <annvelents@gmail.com>
2025-05-27 10:48:21 +0200

    fix linter

--- db/migrate/20250526130953_populate_billing_entity_applied_dunning_campaign.rb
+++ db/migrate/20250526130953_populate_billing_entity_applied_dunning_campaign.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 class PopulateBillingEntityAppliedDunningCampaign < ActiveRecord::Migration[8.0]
-  class DunningCampaign < ActiveRecord::Base
-    self.table_name = "dunning_campaigns"
-    belongs_to :organization, class_name: "Organization"
-  end
-
   def up
+    # rubocop:disable Rails/SkipsModelValidations
     DunningCampaign.where(applied_to_organization: true).find_each do |dunning_campaign|
       BillingEntity.where(id: dunning_campaign.organization_id).update_all(applied_dunning_campaign_id: dunning_campaign.id)
     end
+    # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
