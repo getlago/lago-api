0f8c0f68d3b4c1360b19079ccff656d107144ac8
Anna Velentsevich <annvelents@gmail.com>
2025-05-27 10:00:06 +0200

    add tests for update dunning campaign service

--- app/services/dunning_campaigns/update_service.rb
+++ app/services/dunning_campaigns/update_service.rb
@@ -102,7 +102,11 @@ module DunningCampaigns
         .update_all(applied_to_organization: false) # rubocop:disable Rails/SkipsModelValidations
 
       organization.reset_customers_last_dunning_campaign_attempt
-      organization.default_billing_entity.update(applied_dunning_campaign: dunning_campaign)
+      if dunning_campaign.applied_to_organization
+        organization.default_billing_entity.update(applied_dunning_campaign: dunning_campaign)
+      else
+        organization.default_billing_entity.update(applied_dunning_campaign: nil)
+      end
 
       new_applied_dunning_campaign = dunning_campaign.applied_to_organization ? dunning_campaign : nil
       organization.default_billing_entity.update!(applied_dunning_campaign: new_applied_dunning_campaign)
