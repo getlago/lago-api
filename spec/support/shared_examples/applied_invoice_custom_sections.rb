64edab502bac0525836921064224cb2c8352436b
Ancor Cruz <hello@ancorcruz.com>
2025-05-27 19:29:40 +0100

    Fix shared example

--- spec/support/shared_examples/applied_invoice_custom_sections.rb
+++ spec/support/shared_examples/applied_invoice_custom_sections.rb
@@ -4,22 +4,24 @@ RSpec.shared_examples "applies invoice_custom_sections" do
   let(:invoice_custom_sections) { create_list(:invoice_custom_section, 4, organization:) }
 
   before do
-    organization.selected_invoice_custom_sections = invoice_custom_sections[2..3]
+    invoice_custom_sections[2..3].each do |section|
+      create(:billing_entity_applied_invoice_custom_section, organization:, billing_entity:, invoice_custom_section: section)
+    end
   end
 
   context "when the customer has :skip_invoice_custom_sections flag" do
-    before { customer.update(skip_invoice_custom_sections: true) }
+    before { customer.update!(skip_invoice_custom_sections: true) }
 
     it "doesn't create applied_invoice_custom_section" do
       expect { service_call }.not_to change(AppliedInvoiceCustomSection, :count)
     end
   end
 
-  context "when customer follows organizations invoice_custom_sections" do
+  context "when customer follows billing_entity invoice_custom_sections" do
     it "creates applied_invoice_custom_sections" do
       result = service_call
       invoice = result.invoice
-      expect(invoice.applied_invoice_custom_sections.pluck(:code)).to match_array(organization.selected_invoice_custom_sections.pluck(:code))
+      expect(invoice.applied_invoice_custom_sections.pluck(:code)).to match_array(billing_entity.selected_invoice_custom_sections.pluck(:code))
     end
   end
 end
