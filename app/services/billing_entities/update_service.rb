132f77923a65b8ca7069df9b67c7fe6697847fa5
Ancor Cruz <hello@ancorcruz.com>
2025-05-28 12:49:32 +0100

    Make billing entity update service run within a transaction
    If something fails (like manage invoice custom sections) it should not
    commit changes to the DB.

--- app/services/billing_entities/update_service.rb
+++ app/services/billing_entities/update_service.rb
@@ -29,63 +29,65 @@ module BillingEntities
       billing_entity.country = params[:country]&.upcase if params.key?(:country)
       billing_entity.default_currency = params[:default_currency]&.upcase if params.key?(:default_currency)
 
-      if params.key?(:document_numbering)
-        # TODO: remove when we do not support document_numbering per organization
-        document_numbering = (params[:document_numbering] == "per_customer") ? "per_customer" : "per_billing_entity"
+      ActiveRecord::Base.transaction do
+        if params.key?(:document_numbering)
+          # TODO: remove when we do not support document_numbering per organization
+          document_numbering = (params[:document_numbering] == "per_customer") ? "per_customer" : "per_billing_entity"
 
-        BillingEntities::ChangeInvoiceNumberingService.call(
-          billing_entity:,
-          document_numbering:
-        )
-      end
-
-      billing_entity.document_number_prefix = params[:document_number_prefix] if params.key?(:document_number_prefix)
-      billing_entity.finalize_zero_amount_invoice = params[:finalize_zero_amount_invoice] if params.key?(:finalize_zero_amount_invoice)
-
-      billing = params[:billing_configuration]&.to_h || {}
-      billing_entity.invoice_footer = billing[:invoice_footer] if billing.key?(:invoice_footer)
-      billing_entity.document_locale = billing[:document_locale] if billing.key?(:document_locale)
-
-      handle_eu_tax_management if params.key?(:eu_tax_management)
-
-      if License.premium? && billing.key?(:invoice_grace_period)
-        BillingEntities::UpdateInvoiceGracePeriodService.call(
-          billing_entity:,
-          grace_period: billing[:invoice_grace_period]
-        )
-      end
-
-      if params.key?(:net_payment_term)
-        # note: this service only assigns new net_payment_term to the billing_entity but doesn't save it
-        BillingEntities::UpdateInvoicePaymentDueDateService.call(
-          billing_entity:,
-          net_payment_term: params[:net_payment_term]
-        )
-      end
-
-      if params.key?(:tax_codes)
-        BillingEntities::Taxes::ManageTaxesService.call!(billing_entity:, tax_codes: params[:tax_codes])
-      end
-
-      if params.key?(:invoice_custom_section_ids)
-        existing_section_ids = billing_entity.selected_invoice_custom_sections.ids
-        new_section_ids = params[:invoice_custom_section_ids]
-
-        billing_entity.applied_invoice_custom_sections.where.not(invoice_custom_section_id: new_section_ids).destroy_all
-
-        sections_to_create = new_section_ids - existing_section_ids
-        sections_to_create.each do |invoice_custom_section_id|
-          billing_entity.applied_invoice_custom_sections.create!(
-            invoice_custom_section_id:,
-            organization_id: billing_entity.organization_id
+          BillingEntities::ChangeInvoiceNumberingService.call(
+            billing_entity:,
+            document_numbering:
           )
         end
+
+        billing_entity.document_number_prefix = params[:document_number_prefix] if params.key?(:document_number_prefix)
+        billing_entity.finalize_zero_amount_invoice = params[:finalize_zero_amount_invoice] if params.key?(:finalize_zero_amount_invoice)
+
+        billing = params[:billing_configuration]&.to_h || {}
+        billing_entity.invoice_footer = billing[:invoice_footer] if billing.key?(:invoice_footer)
+        billing_entity.document_locale = billing[:document_locale] if billing.key?(:document_locale)
+
+        handle_eu_tax_management if params.key?(:eu_tax_management)
+
+        if License.premium? && billing.key?(:invoice_grace_period)
+          BillingEntities::UpdateInvoiceGracePeriodService.call(
+            billing_entity:,
+            grace_period: billing[:invoice_grace_period]
+          )
+        end
+
+        if params.key?(:net_payment_term)
+          # note: this service only assigns new net_payment_term to the billing_entity but doesn't save it
+          BillingEntities::UpdateInvoicePaymentDueDateService.call(
+            billing_entity:,
+            net_payment_term: params[:net_payment_term]
+          )
+        end
+
+        if params.key?(:tax_codes)
+          BillingEntities::Taxes::ManageTaxesService.call!(billing_entity:, tax_codes: params[:tax_codes])
+        end
+
+        if params.key?(:invoice_custom_section_ids)
+          existing_section_ids = billing_entity.selected_invoice_custom_sections.ids
+          new_section_ids = params[:invoice_custom_section_ids]
+
+          billing_entity.applied_invoice_custom_sections.where.not(invoice_custom_section_id: new_section_ids).destroy_all
+
+          sections_to_create = new_section_ids - existing_section_ids
+          sections_to_create.each do |invoice_custom_section_id|
+            billing_entity.applied_invoice_custom_sections.create!(
+              invoice_custom_section_id:,
+              organization_id: billing_entity.organization_id
+            )
+          end
+        end
+
+        assign_premium_attributes
+        handle_base64_logo if params.key?(:logo)
+
+        billing_entity.save!
       end
-
-      assign_premium_attributes
-      handle_base64_logo if params.key?(:logo)
-
-      billing_entity.save!
 
       result.billing_entity = billing_entity
       result
