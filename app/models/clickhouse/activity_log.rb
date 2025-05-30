9906abdf5c4270f412e2f5bb105ada210a82ea00
Romain Sempé <romain@getlago.com>
2025-05-30 14:58:52 +0200

    feat(audit-logs): Include deleted associations

--- app/models/clickhouse/activity_log.rb
+++ app/models/clickhouse/activity_log.rb
@@ -9,6 +9,7 @@ module Clickhouse
     belongs_to :resource, polymorphic: true
 
     belongs_to :customer,
+      -> { with_discarded },
       primary_key: :external_id,
       foreign_key: :external_customer_id,
       optional: true
@@ -17,6 +18,8 @@ module Clickhouse
       primary_key: :external_id,
       foreign_key: :external_subscription_id,
       optional: true
+
+    RESOURCE_TYPES_WITH_DISCARDED = %w[BillableMetric Plan Customer BillingEntity Coupon].freeze
 
     RESOURCE_TYPES = {
       billable_metric: "BillableMetric",
@@ -90,6 +93,18 @@ module Clickhouse
 
     def subscription
       organization.subscriptions.find_by(external_id: external_subscription_id)
+    end
+
+    # TODO: Remove this once we have soft deletion everywhere
+    def resource
+      return nil if resource_type.blank? || resource_id.blank?
+
+      klass = resource_type.safe_constantize
+      if RESOURCE_TYPES_WITH_DISCARDED.include?(resource_type)
+        klass.with_discarded.find_by(organization_id: organization.id, id: resource_id)
+      else
+        klass.find_by(organization_id: organization.id, id: resource_id)
+      end
     end
 
     private
