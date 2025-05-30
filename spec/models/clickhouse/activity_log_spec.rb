674735acd60e0d4f63771078d13a19d9d11c9a02
Mario Diniz <mariohenriquediniz@gmail.com>
2025-05-30 12:19:13 -0300

    query user even when discarded

--- spec/models/clickhouse/activity_log_spec.rb
+++ spec/models/clickhouse/activity_log_spec.rb
@@ -9,34 +9,12 @@ RSpec.describe Clickhouse::ActivityLog, type: :model, clickhouse: true do
   it { is_expected.to belong_to(:resource) }
   it { is_expected.to belong_to(:customer).optional }
   it { is_expected.to belong_to(:subscription).optional }
+  it { is_expected.to belong_to(:user).optional }
+  it { is_expected.to belong_to(:api_key).optional }
 
   describe "#ensure_activity_id" do
     it "sets the activity_id if it is not set" do
       expect(activity_log.activity_id).to be_present
-    end
-  end
-
-  describe "#user" do
-    it "returns the user" do
-      expect(activity_log.user).to be_a(User)
-    end
-  end
-
-  describe "#api_key" do
-    it "returns the api key" do
-      expect(activity_log.api_key).to be_a(ApiKey)
-    end
-  end
-
-  describe "#customer" do
-    it "returns the customer" do
-      expect(activity_log.customer).to be_a(Customer)
-    end
-  end
-
-  describe "#subscription" do
-    it "returns the subscription" do
-      expect(activity_log.subscription).to be_a(Subscription)
     end
   end
 end
