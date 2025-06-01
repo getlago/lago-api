4e9d82018fc5fa5a84c8b2de68c48724ea6953a5
Miguel Pinto <darkymiguel@gmail.com>
2025-06-01 19:18:04 +0100

    feat: void and generate credit note

--- app/graphql/types/invoices/void_invoice_input.rb
+++ app/graphql/types/invoices/void_invoice_input.rb
@@ -5,10 +5,10 @@ module Types
     class VoidInvoiceInput < Types::BaseInputObject
       description "Void Invoice input arguments"
 
-      argument :id, ID, required: true
-      argument :generate_credit_note, Boolean, required: false
-      argument :refund_amount, GraphQL::Types::BigInt, required: false
       argument :credit_amount, GraphQL::Types::BigInt, required: false
+      argument :generate_credit_note, Boolean, required: false
+      argument :id, ID, required: true
+      argument :refund_amount, GraphQL::Types::BigInt, required: false
     end
   end
 end
