2029e44cdb39bbdc2f577c9706de8c13f2d0bcc5
Julien Bourdeau <julien@julienbourdeau.com>
2025-06-04 11:57:18 +0200

    feat(alerting): new field for alert.triggered webhook payload (#3779)
    As I'm documenting this `alert.triggered` webhook I realize there was
    some field that could be better.
    * `subscription_external_id` is how you can retrieve subscription via
    the API (instead of lago_id)
    * `organization_id` in case you receive webhooks from multiple
    organizations? It should be everywhere.
    
    I also edited the seeder to seed some TriggeredAlert.

--- spec/serializers/v1/usage_monitoring/triggered_alert_serializer_spec.rb
+++ spec/serializers/v1/usage_monitoring/triggered_alert_serializer_spec.rb
@@ -18,8 +18,10 @@ RSpec.describe ::V1::UsageMonitoring::TriggeredAlertSerializer do
 
       payload = result["triggered_alert"]
       expect(payload["lago_id"]).to eq(triggered_alert.id)
+      expect(payload["lago_organization_id"]).to eq(triggered_alert.organization_id)
       expect(payload["lago_alert_id"]).to eq(triggered_alert.alert.id)
       expect(payload["lago_subscription_id"]).to eq(triggered_alert.subscription.id)
+      expect(payload["subscription_external_id"]).to eq("ext-id")
       expect(payload["billable_metric_code"]).to be_nil
       expect(payload["alert_name"]).to eq("General Alert")
       expect(payload["alert_code"]).to eq("first")
