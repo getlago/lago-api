c9c6be399ffaedf858376dee68739f45c29354cf
Julien Bourdeau <julien@julienbourdeau.com>
2025-06-04 15:12:13 +0200

    fix(migrations): Disable transaction for wallet_transaction migration

--- db/migrate/20250512081332_add_lock_version_to_wallet_transactions.rb
+++ db/migrate/20250512081332_add_lock_version_to_wallet_transactions.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddLockVersionToWalletTransactions < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def change
     add_column :wallet_transactions, :lock_version, :integer, default: 0, null: false
   end
