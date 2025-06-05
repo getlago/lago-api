08e8e00c23d27c0662776102b92187704242e3d9
Miguel Pinto <darkymiguel@gmail.com>
2025-06-05 16:46:50 +0100

    feat: add specs

--- spec/requests/api/v1/invoices_controller_spec.rb
+++ spec/requests/api/v1/invoices_controller_spec.rb
@@ -826,6 +826,7 @@ RSpec.describe Api::V1::InvoicesController, type: :request do
       let(:credit_amount) { 0 }
       let(:refund_amount) { 0 }
       let(:params) { {generate_credit_note: true, credit_amount: credit_amount, refund_amount: refund_amount} }
+
       around { |test| lago_premium!(&test) }
 
       it "calls the void service with all parameters" do
