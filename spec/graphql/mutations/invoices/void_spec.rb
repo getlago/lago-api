f0c9e6cae0d487cf70e19378088c9bc42b6367f7
Miguel Pinto <darkymiguel@gmail.com>
2025-06-06 11:05:19 +0100

    feat: add specs

--- spec/graphql/mutations/invoices/void_spec.rb
+++ spec/graphql/mutations/invoices/void_spec.rb
@@ -85,10 +85,8 @@ RSpec.describe Mutations::Invoices::Void, type: :graphql do
 
       result_data = result["data"]["voidInvoice"]
 
-      aggregate_failures do
-        expect(result_data["id"]).to be_present
-        expect(result_data["status"]).to eq("voided")
-      end
+      expect(result_data["id"]).to be_present
+      expect(result_data["status"]).to eq("voided")
     end
   end
 end
