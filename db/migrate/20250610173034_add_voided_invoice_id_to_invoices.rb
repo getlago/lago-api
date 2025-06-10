e1d65fb2fc7d2f3d1aa144ec2aa20c6adc0bc0b0
Diego Charles <diegocharles@me.com>
2025-06-10 14:33:17 -0300

    [Migration] Create invoices#voided_invoice_id reference

--- db/migrate/20250610173034_add_voided_invoice_id_to_invoices.rb
+++ db/migrate/20250610173034_add_voided_invoice_id_to_invoices.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class AddVoidedInvoiceIdToInvoices < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
-
   def change
-    add_reference :invoices, :voided_invoice, type: :uuid, index: {algorithm: :concurrently}
+    safety_assured do
+      add_reference :invoices, :voided_invoice, type: :uuid, foreign_key: {to_table: :invoices}, index: true
+    end
   end
 end
