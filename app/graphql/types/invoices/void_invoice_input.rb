ed4207af6e4f3a7b37ee7df3ebbd3513a6dde9bf
Miguel Pinto <darkymiguel@gmail.com>
2025-06-10 14:25:46 +0100

    feat: add scenario specs

--- app/graphql/types/invoices/void_invoice_input.rb
+++ app/graphql/types/invoices/void_invoice_input.rb
@@ -4,10 +4,10 @@ module Types
   module Invoices
     class VoidInvoiceInput < Types::BaseInputObject
       description "Void Invoice input arguments"
+      argument :id, ID, required: true
 
       argument :credit_amount, GraphQL::Types::BigInt, required: false
       argument :generate_credit_note, Boolean, required: false
-      argument :id, ID, required: true
       argument :refund_amount, GraphQL::Types::BigInt, required: false
     end
   end
