e278e4a6b84c6488bf66bcaf34db94f487bc421d
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-06-11 10:19:16 +0300

    Add PricingUnitUsage model

---
+++ spec/factories/pricing_unit_usages.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :pricing_unit_usage do
+    fee
+    pricing_unit
+    short_name { pricing_unit.short_name }
+    amount_cents { 200 }
+    precise_amount_cents { 200.0000000001 }
+    unit_amount_cents { 10 }
+    conversion_rate { 1.0 }
+  end
+end
