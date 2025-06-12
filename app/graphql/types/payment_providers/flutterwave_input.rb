0eee5ab6c6a6fc8eb401f456a7d8b0e491c06803
willymwai <willymwaid92@gmail.com>
2025-06-12 16:18:23 +0300

    feat(payment-providers): Add public and secret key fields to Flutterwave provider and update input arguments

--- app/graphql/types/payment_providers/flutterwave_input.rb
+++ app/graphql/types/payment_providers/flutterwave_input.rb
@@ -4,13 +4,12 @@ module Types
   module PaymentProviders
     class FlutterwaveInput < BaseInputObject
       description "Flutterwave input arguments"
-
-      argument :public_key, String, required: true
-      argument :secret_key, String, required: true
-      argument :encryption_key, String, required: true
       argument :code, String, required: true
+      argument :encryption_key, String, required: true
       argument :name, String, required: true
       argument :production, Boolean, required: false, default_value: false
+      argument :public_key, String, required: true
+      argument :secret_key, String, required: true
       argument :success_redirect_url, String, required: false
     end
   end
