910a82ad5f105bbe3b8d6a0ac3ee7bf77a86ebab
Julien Bourdeau <julien@julienbourdeau.com>
2025-06-12 11:19:11 +0200

    feat(stripe): Fix Stripe.api_version to `2025-04-30.basil`

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Stripe.api_version = ENV.fetch("STRIPE_API_VERSION", "2025-04-30.basil")
+
 # Lago uses the key from PaymentProvider.secret_key because each org should have their own keys
 # In development, we always use our sandbox key, and in the console we might need to use
 # the Stripe client directly (ex: ::Stripe::Customer.retrieve('cus_xxx')) which throws
