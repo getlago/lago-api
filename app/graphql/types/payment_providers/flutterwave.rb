6b7b6ead81438a802a0f5e65c6cb929bd74eee87
willymwai <willymwaid92@gmail.com>
2025-06-12 15:58:26 +0300

    feat(payment-providers): Add Flutterwave payment provider integration

---
+++ app/graphql/types/payment_providers/flutterwave.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Types
+  module PaymentProviders
+    class Flutterwave < Types::BaseObject
+      graphql_name "FlutterwaveProvider"
+
+      field :code, String, null: false
+      field :id, ID, null: false
+      field :name, String, null: false
+
+      field :public_key, ObfuscatedStringType, null: true, permission: "organization:integrations:view"
+      field :secret_key, ObfuscatedStringType, null: true, permission: "organization:integrations:view"
+      field :encryption_key, ObfuscatedStringType, null: true, permission: "organization:integrations:view"
+      field :production, Boolean, null: true, permission: "organization:integrations:view"
+      field :success_redirect_url, String, null: true, permission: "organization:integrations:view"
+    end
+  end
+end
