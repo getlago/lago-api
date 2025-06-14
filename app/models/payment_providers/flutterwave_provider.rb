49a02c4d4aeedfb9ef13f031ea078621f4bbe14b
willymwai <willymwaid92@gmail.com>
2025-06-14 08:08:27 +0300

    feat(payment-providers): Refactor Flutterwave provider by removing unused fields and updating input arguments

--- app/models/payment_providers/flutterwave_provider.rb
+++ app/models/payment_providers/flutterwave_provider.rb
@@ -5,21 +5,17 @@ module PaymentProviders
     FlutterwavePayment = Data.define(:id, :status, :metadata)
 
     SUCCESS_REDIRECT_URL = "https://www.flutterwave.com/ng"
-    API_VERSION = "v3"
-    BASE_URL = "https://api.flutterwave.com/v3"
-    SANDBOX_URL = "https://ravesandboxapi.flutterwave.com/v3"
+    API_URL = "https://api.flutterwave.com/v3"
 
     PROCESSING_STATUSES = %w[pending].freeze
     SUCCESS_STATUSES = %w[successful].freeze
     FAILED_STATUSES = %w[failed cancelled].freeze
 
-    validates :public_key, presence: true
     validates :secret_key, presence: true
-    validates :encryption_key, presence: true
     validates :success_redirect_url, url: true, allow_nil: true, length: {maximum: 1024}
 
-    secrets_accessors :public_key, :secret_key, :encryption_key
-    settings_accessors :production, :webhook_secret
+    secrets_accessors :secret_key
+    settings_accessors :webhook_secret
 
     before_create :generate_webhook_secret
 
@@ -28,11 +24,7 @@ module PaymentProviders
     end
 
     def api_url
-      production? ? BASE_URL : SANDBOX_URL
-    end
-
-    def production?
-      ActiveModel::Type::Boolean.new.cast(production)
+      API_URL
     end
 
     private
--- spec/graphql/types/payment_providers/flutterwave_spec.rb
+++ spec/graphql/types/payment_providers/flutterwave_spec.rb
@@ -9,11 +9,7 @@ RSpec.describe Types::PaymentProviders::Flutterwave do
     expect(subject).to have_field(:id).of_type("ID!")
     expect(subject).to have_field(:code).of_type("String!")
     expect(subject).to have_field(:name).of_type("String!")
-
-    expect(subject).to have_field(:public_key).of_type("ObfuscatedString").with_permission("organization:integrations:view")
     expect(subject).to have_field(:secret_key).of_type("ObfuscatedString").with_permission("organization:integrations:view")
-    expect(subject).to have_field(:encryption_key).of_type("ObfuscatedString").with_permission("organization:integrations:view")
-    expect(subject).to have_field(:production).of_type("Boolean").with_permission("organization:integrations:view")
     expect(subject).to have_field(:success_redirect_url).of_type("String").with_permission("organization:integrations:view")
     expect(subject).to have_field(:webhook_secret).of_type("String").with_permission("organization:integrations:view")
   end
