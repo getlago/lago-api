0b75e81ffaf68a8cab2b436045ebfc99450479d0
Mario Diniz <mariohenriquediniz@gmail.com>
2025-06-16 17:49:24 -0300

    Api endpoints api logs (#3816)
    Api Logs endpoints.
    
    ---------
    
    Co-authored-by: Romain Sempé <romain@getlago.com>

--- spec/controllers/concerns/api_loggable_spec.rb
+++ spec/controllers/concerns/api_loggable_spec.rb
@@ -36,4 +36,27 @@ RSpec.describe ApiLoggable, type: :controller do
       end
     end
   end
+
+  context "with skip_audit_logs!" do
+    # rubocop:disable RSpec/DescribedClass
+    controller(ApplicationController) do
+      include ApiLoggable
+      skip_audit_logs!
+
+      def index
+        render json: :ok
+      end
+    end
+    # rubocop:enable RSpec/DescribedClass
+
+    [:get, :post, :put, :delete].each do |method|
+      context "when method is #{method}" do
+        it "does not produce api log" do
+          send(method, :index)
+
+          expect(Utils::ApiLog).not_to have_received(:produce)
+        end
+      end
+    end
+  end
 end
