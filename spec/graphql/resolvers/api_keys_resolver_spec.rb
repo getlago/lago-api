c72646bede199a07af85972cf8ec6456a97e8e6d
Yohan Robert <yohan@getlago.com>
2025-06-16 14:39:22 +0200

    fix(api-key): Fix pagination in API keys GraphQL resolver
    We used the following query:
    
    ```ruby
    current_organization.api_keys.order(created_at: :asc).page(page).limit(limit)
    ```
    
    which resulted in the following SQL:
    
    ```sql
    SELECT "api_keys"."id",
      "api_keys"."organization_id",
      "api_keys"."value",
      "api_keys"."created_at",
      "api_keys"."updated_at",
      "api_keys"."expires_at",
      "api_keys"."last_used_at",
      "api_keys"."name",
      "api_keys"."permissions"
    FROM "api_keys"
    WHERE (
        expires_at IS NULL
        OR expires_at > '2025-06-16 12:41:11.750724'
      )
      AND "api_keys"."organization_id" = '...'
    ORDER BY "api_keys"."created_at" ASC
    LIMIT 5 OFFSET 25
    ```
    
    This is due to Kaminari's way of handling pagination, which requires the `page` and `per` methods to be called in a specific order. `per` will then properly override the `limit` and `offset`. If `per` is not called, Kaminari will define the `OFFSET` using the `page` and default page size.

--- spec/graphql/resolvers/api_keys_resolver_spec.rb
+++ spec/graphql/resolvers/api_keys_resolver_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Resolvers::ApiKeysResolver, type: :graphql do
   let(:query) do
     <<~GQL
       query {
-        apiKeys(limit: 1, page: 2) {
+        apiKeys(limit: 1, page: 1) {
           collection { id value createdAt }
           metadata { currentPage, totalCount totalPages }
         }
@@ -26,9 +26,7 @@ RSpec.describe Resolvers::ApiKeysResolver, type: :graphql do
   let(:organization) { create(:api_key).organization }
   let(:membership) { create(:membership, organization:) }
   let(:required_permission) { "developers:keys:manage" }
-  let!(:api_key) { create(:api_key, organization:) }
-
-  before { create(:api_key) }
+  let(:api_key) { membership.organization.api_keys.first }
 
   it_behaves_like "requires current user"
   it_behaves_like "requires current organization"
@@ -37,13 +35,43 @@ RSpec.describe Resolvers::ApiKeysResolver, type: :graphql do
   it "returns a list of api keys" do
     api_key_response = result["data"]["apiKeys"]
 
-    aggregate_failures do
-      expect(api_key_response["collection"].first["id"]).to eq(api_key.id)
-      expect(api_key_response["collection"].first["value"]).to eq("••••••••" + api_key.value.last(3))
-      expect(api_key_response["collection"].first["createdAt"]).to eq(api_key.created_at.iso8601)
+    expect(api_key_response["collection"].first["id"]).to eq(api_key.id)
+    expect(api_key_response["collection"].first["value"]).to eq("••••••••" + api_key.value.last(3))
+    expect(api_key_response["collection"].first["createdAt"]).to eq(api_key.created_at.iso8601)
+
+    expect(api_key_response["metadata"]["currentPage"]).to eq(1)
+    expect(api_key_response["metadata"]["totalCount"]).to eq(1)
+    expect(api_key_response["metadata"]["totalPages"]).to eq(1)
+  end
+
+  context "when pagination is provided" do
+    let(:query) do
+      <<~GQL
+        query {
+          apiKeys(limit: 2, page: 2) {
+            collection { id value name createdAt }
+            metadata { currentPage, totalCount totalPages}
+          }
+        }
+      GQL
+    end
+
+    before do
+      3.times do |i|
+        create(:api_key, organization: membership.organization, created_at: Time.zone.now - 10.days + i.days, name: "API Key #{i + 1}")
+      end
+    end
+
+    it "returns a list of api keys" do
+      api_key_response = result["data"]["apiKeys"]
+
+      collection = api_key_response["collection"]
+
+      expect(collection.size).to eq(2)
+      expect(collection.map { |api_key| api_key["name"] }).to eq(["API Key 3", "API Key"])
 
       expect(api_key_response["metadata"]["currentPage"]).to eq(2)
-      expect(api_key_response["metadata"]["totalCount"]).to eq(2)
+      expect(api_key_response["metadata"]["totalCount"]).to eq(4)
       expect(api_key_response["metadata"]["totalPages"]).to eq(2)
     end
   end
