b51969644c83982d9656f8ef8734eb62c93e5306
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-06-16 15:16:30 +0300

    Adjust invoice PDF to display amounts in pricing units

--- app/views/helpers/fee_display_helper.rb
+++ app/views/helpers/fee_display_helper.rb
@@ -17,15 +17,37 @@ class FeeDisplayHelper
 
   def self.format_with_precision(fee, amount)
     casted_amount = BigDecimal(amount)
-    MoneyHelper.format_with_precision(casted_amount, fee.currency)
+
+    if fee.pricing_unit_usage
+      MoneyHelper.format_pricing_unit_with_precision(
+        casted_amount,
+        fee.pricing_unit_usage.currency
+      )
+    else
+      MoneyHelper.format_with_precision(casted_amount, fee.currency)
+    end
   end
 
   def self.format_as_currency(fee, amount)
-    money = amount.to_money(fee.currency)
-    MoneyHelper.format(money)
+    if fee.pricing_unit_usage
+      MoneyHelper.format_pricing_unit(
+        BigDecimal(amount),
+        fee.pricing_unit_usage.currency
+      )
+    else
+      money = amount.to_money(fee.currency)
+      MoneyHelper.format(money)
+    end
   end
 
   def self.format_amount(fee)
-    MoneyHelper.format(fee.amount)
+    if fee.pricing_unit_usage
+      MoneyHelper.format_pricing_unit(
+        fee.pricing_unit_usage.amount_cents.to_d / 100,
+        fee.pricing_unit_usage.currency
+      )
+    else
+      MoneyHelper.format(fee.amount)
+    end
   end
 end
