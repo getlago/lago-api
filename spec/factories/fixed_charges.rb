201acc55c66a8c5137ed2db1cd9fdafac4a49932
Ancor Cruz <hello@ancorcruz.com>
2025-06-18 11:10:04 +0100

    fixed charge factory

--- spec/factories/fixed_charges.rb
+++ spec/factories/fixed_charges.rb
@@ -1,19 +1,32 @@
 FactoryBot.define do
   factory :fixed_charge do
-    organization { nil }
-    billing_entity { nil }
-    plan { nil }
-    add_on { nil }
-    parent { nil }
-    charge_model { "MyString" }
-    properties { "" }
-    invoice_display_name { "MyString" }
-    pay_in_advance { false }
-    prorated { false }
-    recurring { false }
+    organization { plan&.organization || association(:organization) }
+    billing_entity { organization&.default_billing_entity || association(:billing_entity) }
+    plan
+    add_on
+    invoice_display_name { Faker::Fantasy::Tolkien.location }
     billing_period_duration { 1 }
-    billing_period_duration_unit { "MyString" }
-    trial_period { 1 }
+    billing_period_duration_unit { "month" }
+    trial_period { 0 }
     untis { 1 }
+
+    trait :standard do
+      charge_model { "standard" }
+      properties do
+        {}
+      end
+    end
+
+    trait :pay_in_advance do
+      pay_in_advance { true }
+    end
+
+    trait :prorated do
+      prorated { true }
+    end
+
+    trait :recurring do
+      recurring { true }
+    end
   end
 end
