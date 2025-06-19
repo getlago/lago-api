f97e4bef3940f1a6b34bbf17c95a7fcf40f0b830
Julien Bourdeau <julien@julienbourdeau.com>
2025-06-19 07:57:10 +0200

    all basic models

---
+++ app/models/feature_entitlement_value.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class FeatureEntitlementValue < ApplicationRecord
+  belongs_to :organization
+  belongs_to :feature_entitlement
+  belongs_to :privilege
+end
+
+# == Schema Information
+#
+# Table name: feature_entitlement_values
+#
+#  id                     :uuid             not null, primary key
+#  deleted_at             :datetime
+#  value                  :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  feature_entitlement_id :uuid             not null
+#  organization_id        :uuid             not null
+#  privilege_id           :uuid             not null
+#
+# Indexes
+#
+#  idx_on_privilege_id_feature_entitlement_id_1e0b74b1a6       (privilege_id,feature_entitlement_id) WHERE (deleted_at IS NULL)
+#  index_feature_entitlement_values_on_feature_entitlement_id  (feature_entitlement_id)
+#  index_feature_entitlement_values_on_organization_id         (organization_id)
+#  index_feature_entitlement_values_on_privilege_id            (privilege_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (feature_entitlement_id => feature_entitlements.id)
+#  fk_rails_...  (organization_id => organizations.id)
+#  fk_rails_...  (privilege_id => privileges.id)
+#
--- db/seeds/03_entitlements.rb
+++ db/seeds/03_entitlements.rb
@@ -2,7 +2,7 @@
 
 organization ||= Organization.find_by(name: "Hooli")
 
-Plan.create_with(
+plan = Plan.create_with(
   interval: "monthly", pay_in_advance: false, amount_cents: 49_00, amount_currency: "EUR"
 ).find_or_create_by!(organization:, name: "Premium Plan", code: "premium_plan")
 
@@ -38,15 +38,18 @@ seats = Feature.create_with(
   description: "Number of users of the account"
 ).find_or_create_by!(organization:, code: "seats")
 
-# PrivilegeValue.where(privilege: seats.privileges).delete_all
+FeatureEntitlementValue.where(privilege: seats.privileges).delete_all
 seats.privileges.delete_all
 
 max = seats.privileges.create!(organization:, code: "max", name: "Maximum", value_type: "integer")
 max_admins = seats.privileges.create!(organization:, code: "max_admins", name: "Max Admins", value_type: "integer")
 
-# PrivilegeValue.create!(organization:, privilege: min, plan:, value: 3) # Plan defaults
-# PrivilegeValue.create!(organization:, privilege: max, plan:, value: 20) # Plan defaults
-# PrivilegeValue.create!(organization:, privilege: max, subscription_external_id: sub.external_id, value: 99) # Subscription override
+FeatureEntitlement.where(organization:, feature: seats, plan:).delete_all
+fe = FeatureEntitlement.create!(organization:, feature: seats, plan:)
+FeatureEntitlementValue.create!(organization:, feature_entitlement: fe, privilege: max, value: 20) # Plan defaults
+FeatureEntitlementValue.create!(organization:, feature_entitlement: fe, privilege: max_admins, value: 3) # Plan defaults
+fe_sub = FeatureEntitlement.create!(organization:, feature: seats, subscription_external_id: sub.external_id)
+FeatureEntitlementValue.create!(organization:, privilege: max, feature_entitlement: fe_sub, value: 99) # Subscription override
 
 # Analytics API
 analytics_api_feature = Feature.create_with(
@@ -54,11 +57,28 @@ analytics_api_feature = Feature.create_with(
   description: "Access to all analytics data via REST API"
 ).find_or_create_by!(organization:, code: "analytics_api")
 
-# PrivilegeValue.where(privilege: analytics_api_feature.privileges).delete_all
 analytics_api_feature.privileges.delete_all
 
-analytics = analytics_api_feature.privileges.create!(organization:, code: "enabled", value_type: "boolean")
-# PrivilegeValue.create!(organization:, privilege: analytics, plan:, value: true)
-# PrivilegeValue.create!(organization:, privilege: analytics, subscription_external_id: sub.external_id, value: false)
+FeatureEntitlement.where(organization:, feature: analytics_api_feature, plan:).delete_all
+FeatureEntitlement.create!(organization:, feature: analytics_api_feature, plan:)
 
-pp max_admins, max, analytics
+# Salesforce
+salesforce = Feature.create_with(
+  name: "Salesforce Integration"
+).find_or_create_by!(organization:, code: "salesforce")
+
+salesforce.privileges.delete_all
+
+FeatureEntitlement.where(organization:, feature: salesforce).delete_all
+FeatureEntitlement.create!(organization:, feature: salesforce, subscription_external_id: sub.external_id)
+
+pp max_admins, max, salesforce
+
+# Premium Support
+support = Feature.create_with(
+  name: "Premium Support"
+).find_or_create_by!(organization:, code: "premium_support")
+
+FeatureEntitlement.where(organization:, feature: support, plan:).delete_all
+FeatureEntitlement.create!(organization:, feature: support, plan:)
+SubscriptionFeatureRemoval.create!(organization:, feature: support, subscription_external_id: sub.external_id)
