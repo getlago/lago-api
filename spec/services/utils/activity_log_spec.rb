0279f64b4de6a9aae86cc0f9a84cfe9d47a39968
Yohan Robert <7434196+groyoh@users.noreply.github.com>
2025-06-23 10:21:30 +0200

    fix(invoice): Fix race-condition in invoice update jobs (#3851)
    ## Context
    
    When doing a manual payment, it will update the invoice payment status. When doing so, we're supposed to do a few things asynchronously like sending a webhook.
    
    Unfortunately, the way it is working right now, our server will sometimes process those asynchronous jobs before the database transaction ends, and therefore before we actually update the invoice. We end up with not up-to-date data within those different actions.
    
    For instance, the webhook might be triggered with `payment_status: 'pending'` in the payload instead of `payment_status: 'succeeded'`.
    
    ## Description
    
    This commit fixes this issue by ensuring the jobs run after the transaction is committed. This is done by using `AfterCommitEverywhere.after_commit` callbacks.
    
    To reduce the risk of mistakes, two new class methods were added:
    
    - `ApplicationJob.perform_after_commit`
    - `Utils::ActivityLog.produce_after_commit`
    
    Doing this, we make the intention clear that the job should run after the transaction is committed.
    
    Test were added for the new methods, and existing tests were updated to properly test that jobs are enqueued after the transaction is committed. To do so, a `expect {}.to enqueue_after_commit` matcher was added.

--- spec/services/utils/activity_log_spec.rb
+++ spec/services/utils/activity_log_spec.rb
@@ -6,56 +6,75 @@ RSpec.describe Utils::ActivityLog, type: :service do
   let(:membership) { create(:membership) }
   let(:api_key) { create(:api_key) }
 
+  let(:organization) { create(:organization) }
+  let(:coupon) { create(:coupon, organization:) }
+  let(:karafka_producer) { instance_double(WaterDrop::Producer) }
+
+  let(:serialized_coupon) do
+    {topic: "activity_logs",
+     key: "#{organization.id}--activity-id",
+     payload: {
+       activity_source: "api",
+       api_key_id: api_key.id,
+       user_id: nil,
+       activity_type: "coupon.created",
+       activity_id: "activity-id",
+       logged_at: Time.current.iso8601[...-1],
+       created_at: Time.current.iso8601[...-1],
+       resource_id: coupon.id,
+       resource_type: "Coupon",
+       organization_id: organization.id,
+       activity_object: V1::CouponSerializer.new(coupon).serialize,
+       activity_object_changes: {},
+       external_customer_id: nil,
+       external_subscription_id: nil
+     }.to_json}
+  end
+
   before do
     allow(CurrentContext).to receive(:membership).and_return(membership.id)
     allow(CurrentContext).to receive(:api_key_id).and_return(api_key.id)
     allow(CurrentContext).to receive(:source).and_return("api")
     travel_to(Time.zone.parse("2023-03-22 12:00:00"))
+
+    allow(Karafka).to receive(:producer).and_return(karafka_producer)
+    allow(karafka_producer).to receive(:produce_async)
+  end
+
+  around do |example|
+    if example.metadata[:kafka_configured]
+      ENV["LAGO_KAFKA_BOOTSTRAP_SERVERS"] = "kafka"
+      ENV["LAGO_KAFKA_ACTIVITY_LOGS_TOPIC"] = "activity_logs"
+    end
+    example.run
+  ensure
+    ENV["LAGO_KAFKA_BOOTSTRAP_SERVERS"] = nil
+    ENV["LAGO_KAFKA_ACTIVITY_LOGS_TOPIC"] = nil
+  end
+
+  describe ".produce_after_commit" do
+    context "when kafka is configured", :kafka_configured do
+      it "produces the event on kafka after the commit" do
+        ApplicationRecord.transaction do
+          activity_log.produce_after_commit(coupon, "coupon.created", activity_id: "activity-id") { BaseService::Result.new }
+
+          expect(karafka_producer).not_to have_received(:produce_async)
+        end
+
+        expect(karafka_producer).to have_received(:produce_async).with(
+          **serialized_coupon
+        )
+      end
+    end
   end
 
   describe ".produce" do
-    let(:organization) { create(:organization) }
-    let(:coupon) { create(:coupon, organization:) }
-    let(:karafka_producer) { instance_double(WaterDrop::Producer) }
-
-    before do
-      allow(Karafka).to receive(:producer).and_return(karafka_producer)
-      allow(karafka_producer).to receive(:produce_async)
-    end
-
-    context "when kafka is configured" do
-      before do
-        ENV["LAGO_KAFKA_BOOTSTRAP_SERVERS"] = "kafka"
-        ENV["LAGO_KAFKA_ACTIVITY_LOGS_TOPIC"] = "activity_logs"
-      end
-
-      after do
-        ENV["LAGO_KAFKA_BOOTSTRAP_SERVERS"] = nil
-        ENV["LAGO_KAFKA_ACTIVITY_LOGS_TOPIC"] = nil
-      end
-
+    context "when kafka is configured", :kafka_configured do
       it "produces the event on kafka" do
         activity_log.produce(coupon, "coupon.created", activity_id: "activity-id") { BaseService::Result.new }
 
         expect(karafka_producer).to have_received(:produce_async).with(
-          topic: "activity_logs",
-          key: "#{organization.id}--activity-id",
-          payload: {
-            activity_source: "api",
-            api_key_id: api_key.id,
-            user_id: nil,
-            activity_type: "coupon.created",
-            activity_id: "activity-id",
-            logged_at: Time.current.iso8601[...-1],
-            created_at: Time.current.iso8601[...-1],
-            resource_id: coupon.id,
-            resource_type: "Coupon",
-            organization_id: organization.id,
-            activity_object: V1::CouponSerializer.new(coupon).serialize,
-            activity_object_changes: {},
-            external_customer_id: nil,
-            external_subscription_id: nil
-          }.to_json
+          **serialized_coupon
         )
       end
 
