d4f7302a740ece0928c20bc76b0a73abfd960c32
Diego Charles <diegocharles@me.com>
2025-06-23 13:32:08 -0300

    Modify CreateGeneratingInvoiceService adding voided_invoice_id arg

---
+++ app/graphql/mutations/invoices/regenerate_from_voided.rb
@@ -0,0 +1,27 @@
+# app/graphql/mutations/invoices/regenerate_from_voided.rb
+module Mutations
+  module Invoices
+    class RegenerateFromVoided < BaseMutation
+      include AuthenticableApiUser
+      include RequiredOrganization
+
+      REQUIRED_PERMISSION = "invoices:update"
+
+      graphql_name "RegenerateInvoice"
+      description "Regenerate an invoice from a voided invoice"
+
+      argument :voided_invoice_id, ID, required: true
+      argument :fees, [Types::Invoices::FeeInput], required: true
+
+      type Types::Invoices::Object
+
+      def resolve(voided_invoice_id:, fees:)
+        invoice = current_organization.invoices.visible.find_by(id: voided_invoice_id)
+
+        result = ::Invoices::RegenerateService.new(invoice:, fees:).call
+
+        result.success? ? result.invoice : result_error(result)
+       end
+    end
+  end
+end
