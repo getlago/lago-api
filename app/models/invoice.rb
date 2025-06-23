6591c187faefa1ebbf86d53c4ff3dec6badcd3a6
Charles <diegocharles@me.com>
2025-06-23 13:12:10 -0300

    migration: create invoices#void_invoice_id references (#3815)
    ## Roadmap Task
    
    
    https://www.notion.so/getlago/BE-Dive-In-Regenerate-Voided-Invoice-208ef63110d280158976c5d15a23dfd7
    
    ## Context
    
    Create the voided_invoice_id association on the invoices table. 
    
    ## Description
    
    This allows to associate the regenerated invoice from its voided
    invoice.
    
    ---------
    
    Co-authored-by: Miguel Pinto <darkymiguel@gmail.com>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -573,6 +573,7 @@ end
 #  organization_id                         :uuid             not null
 #  organization_sequential_id              :integer          default(0), not null
 #  sequential_id                           :integer
+#  voided_invoice_id                       :uuid
 #
 # Indexes
 #
@@ -589,6 +590,7 @@ end
 #  index_invoices_on_self_billed                                   (self_billed)
 #  index_invoices_on_sequential_id                                 (sequential_id)
 #  index_invoices_on_status                                        (status)
+#  index_invoices_on_voided_invoice_id                             (voided_invoice_id)
 #
 # Foreign Keys
 #
