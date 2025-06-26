3d79a01a6acf03b802267963216c6f7d361ec523
Miguel Pinto <darkymiguel@gmail.com>
2025-06-26 19:00:05 +0100

    feat: added migration to update all webhooks

---
+++ db/migrate/20250626175249_refresh_stripe_webhooks.rb
@@ -0,0 +1,12 @@
+class RefreshStripeWebhooks < ActiveRecord::Migration[7.1]
+  # Stripe calls are external; avoid wrapping in a DB transaction.
+  disable_ddl_transaction!
+
+  def up
+    PaymentProviders::StripeProvider.unscoped.find_each do |stripe_provider|
+      next unless stripe_provider.secret_key
+
+      PaymentProviders::Stripe::RefreshWebhookJob.perform_later(stripe_provider)
+    end
+  end
+end
