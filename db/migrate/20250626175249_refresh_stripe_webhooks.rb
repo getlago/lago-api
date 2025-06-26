50cb844c1b02e31f19bcce67cf46bc02d4abc687
Miguel Pinto <darkymiguel@gmail.com>
2025-06-26 19:17:45 +0100

    feat: rubocop

--- db/migrate/20250626175249_refresh_stripe_webhooks.rb
+++ db/migrate/20250626175249_refresh_stripe_webhooks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RefreshStripeWebhooks < ActiveRecord::Migration[7.1]
   # Stripe calls are external; avoid wrapping in a DB transaction.
   disable_ddl_transaction!
