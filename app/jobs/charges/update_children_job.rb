2e9b5663ad201f6603b02772ea9f341626b6c9a2
Lovro Colic <lovro.colic.lc@gmail.com>
2025-07-01 17:08:55 +0200

    adjust batch size for cascade feature

--- app/jobs/charges/update_children_job.rb
+++ app/jobs/charges/update_children_job.rb
@@ -8,7 +8,7 @@ module Charges
       charge = Charge.find_by(id: old_parent_attrs["id"])
       return unless charge
 
-      charge.children.order(created_at: :asc).pluck(:id).each_slice(100) do |child_ids|
+      charge.children.order(created_at: :asc).pluck(:id).each_slice(20) do |child_ids|
         Charges::UpdateChildrenBatchJob.perform_later(
           child_ids:,
           params:,
