getlago/lago-api

commit 0fad050fb9b888ec43f8eb59240898ea6a7425f9

fix(wallet_transaction): Ensure consistency when creating wallet transaction (#3895)
## Context

An issue has been identifier on the `POST /v1/wallet_transactions` used
to create a transaction on a wallet.

In some situation if the same wallet is being refreshed in a separate
process (job, parallel call to the endpoint), an
`ActiveRecord::StaleObjectError` can be raised when running an update on
the model. The main reason is that the wallet model is using an
optimistic locking approach to avoid inconsistencies.

The error is raised and it leads to an HTTP 500 response error returned
by the API. An other problem appears here: the process to create the
wallet transaction is not transactional, meaning that a wallet
transaction might have been persisted in the database without impacting
the wallet balance...

## Description

This PR:
- Wrap the logic in a single database transaction making sure nothing is
persisted if something goes wrong in the process
- Adds an auto retry logic when an `ActiveRecord::StaleObjectError` is
raised

app/services/wallet_transactions/create_from_params_service.rb
@@ -2,16 +2,22 @@
 
 module WalletTransactions
   class CreateFromParamsService < ::BaseService
+    MAX_WALLET_UPDATE_ATTEMPTS = 5
+
     Result = BaseResult[:current_wallet, :wallet_transactions]
 
     def initialize(organization:, params:)
       @organization = organization
       @params = params
 
+      @update_attempts = 0
+
       super
     end
 
     def call
+      @update_attempts += 1
+
       # Normalize metadata
       params[:metadata] = [] if params[:metadata] == {}
       return result unless valid? # NOTE: validator sets result.current_wallet
@@ -26,33 +32,35 @@ module WalletTransactions
       end
       wallet = result.current_wallet
 
-      if params[:paid_credits]
-        transaction = handle_paid_credits(
-          wallet:,
-          credits_amount: BigDecimal(params[:paid_credits]).floor(5),
-          invoice_requires_successful_payment:
-        )
-        wallet_transactions << transaction
-      end
+      ActiveRecord::Base.transaction do
+        if params[:paid_credits]
+          transaction = handle_paid_credits(
+            wallet:,
+            credits_amount: BigDecimal(params[:paid_credits]).floor(5),
+            invoice_requires_successful_payment:
+          )
+          wallet_transactions << transaction
+        end
 
-      if params[:granted_credits]
-        transaction = handle_granted_credits(
-          wallet:,
-          credits_amount: BigDecimal(params[:granted_credits]).floor(5),
-          reset_consumed_credits: ActiveModel::Type::Boolean.new.cast(params[:reset_consumed_credits]),
-          invoice_requires_successful_payment:
-        )
-        wallet_transactions << transaction
-      end
+        if params[:granted_credits]
+          transaction = handle_granted_credits(
+            wallet:,
+            credits_amount: BigDecimal(params[:granted_credits]).floor(5),
+            reset_consumed_credits: ActiveModel::Type::Boolean.new.cast(params[:reset_consumed_credits]),
+            invoice_requires_successful_payment:
+          )
+          wallet_transactions << transaction
+        end
 
-      if params[:voided_credits]
-        wallet_credit = WalletCredit.new(wallet:, credit_amount: BigDecimal(params[:voided_credits]).floor(5), invoiceable: false)
-        void_result = WalletTransactions::VoidService.call(
-          wallet:,
-          wallet_credit:,
-          from_source: source, metadata:
-        )
-        wallet_transactions << void_result.wallet_transaction
+        if params[:voided_credits]
+          wallet_credit = WalletCredit.new(wallet:, credit_amount: BigDecimal(params[:voided_credits]).floor(5), invoiceable: false)
+          void_result = WalletTransactions::VoidService.call(
+            wallet:,
+            wallet_credit:,
+            from_source: source, metadata:
+          )
+          wallet_transactions << void_result.wallet_transaction
+        end
       end
 
       transactions = wallet_transactions.compact
@@ -65,6 +73,14 @@ module WalletTransactions
 
       result.wallet_transactions = transactions
       result
+    rescue ActiveRecord::StaleObjectError
+      if @update_attempts <= MAX_WALLET_UPDATE_ATTEMPTS
+        sleep(rand(0.1..0.5))
+        result.current_wallet.reload # Make sure the wallet is reloaded before retrying
+        retry
+      end
+
+      raise
     end
 
     private

app/services/wallets/balance/increase_service.rb
@@ -3,6 +3,8 @@
 module Wallets
   module Balance
     class IncreaseService < BaseService
+      Result = BaseResult[:wallet]
+
       def initialize(wallet:, wallet_transaction:, reset_consumed_credits: false)
         super
 

commit 4a5e510e4f47e38c7beaae9e525ee775925a3d1d

fix(wallet_transaction): Enqueue BillPaidCreditJob after commit (#3906)
## Context
This PR fixes a regression introduced by
https://github.com/getlago/lago-api/pull/3895

The wallet transaction processing was performed in a single database
transaction. I the process a `BillPaidCreditJob` is enqueued. Since it's
now in the DB transaction, it leads in some situation to an
`ActiveJob::DeserializationError`.

## Description

This PR fixes the situation by enqueuing the job after the the commit of
the DB transaction

app/services/wallet_transactions/create_from_params_service.rb
@@ -102,7 +102,7 @@ module WalletTransactions
         metadata:
       ).wallet_transaction
 
-      BillPaidCreditJob.perform_later(wallet_transaction, Time.current.to_i)
+      BillPaidCreditJob.perform_after_commit(wallet_transaction, Time.current.to_i)
 
       wallet_transaction
     end