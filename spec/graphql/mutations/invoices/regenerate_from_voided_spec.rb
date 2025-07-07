0f16cffedd8c6ab30ce85de53f871509a5ddca73
Diego Charles <diegocharles@me.com>
2025-07-07 14:04:05 -0300

    Refactor the specs

--- spec/graphql/mutations/invoices/regenerate_from_voided_spec.rb
+++ spec/graphql/mutations/invoices/regenerate_from_voided_spec.rb
@@ -5,21 +5,19 @@ require "rails_helper"
 RSpec.describe Mutations::Invoices::RegenerateFromVoided, type: :graphql do
   let(:required_permission) { "invoices:update" }
   let(:organization) { create(:organization) }
-  let(:membership) { create(:membership, organization: organization) }
+  let(:membership) { create(:membership, organization:) }
   let(:user) { membership.user }
-  let(:voided_invoice) { create(:invoice, :voided, organization: organization) }
-  let!(:fee) { create(:fee, invoice: voided_invoice, organization: organization) }
+  let!(:add_on) { create(:add_on, organization:) }
   let(:fees) do
     [{
       id: fee.id,
-      add_on_id: nil,
+      addOnId: add_on.id,
       description: "Updated description",
-      invoice_display_name: "Updated display name",
+      invoiceDisplayName: "Updated display name",
       units: 5.0,
-      unit_amount_cents: 1000
+      unitAmountCents: 1000
     }]
   end
-
   let(:mutation) do
     <<~GQL
       mutation ($input: RegenerateInvoiceInput!) {
@@ -38,30 +36,58 @@ RSpec.describe Mutations::Invoices::RegenerateFromVoided, type: :graphql do
   it_behaves_like "requires current organization"
   it_behaves_like "requires permission", "invoices:update"
 
-  it "regenerates an invoice from a voided invoice (success)" do
-    result = execute_graphql(
-      current_organization: organization,
-      current_user: user,
-      permissions: required_permission,
-      query: mutation,
-      variables: {
-        input: {
-          voidedInvoiceId: voided_invoice.id,
-          fees: fees
+  shared_examples "regenerates invoice with expected status" do |expected_status|
+    it "regenerates an invoice with status #{expected_status}" do
+      result = execute_graphql(
+        current_organization: organization,
+        current_user: user,
+        permissions: required_permission,
+        query: mutation,
+        variables: {
+          input: {
+            voidedInvoiceId: voided_invoice.id,
+            fees: fees
+          }
         }
-      }
-    )
-
-    result_data = result["data"]["regenerateFromVoided"]
-    aggregate_failures do
-      expect(result["errors"]).to be_nil
-      expect(result_data["id"]).to be_present
-      expect(result_data["status"]).to eq("draft")
-      expect(result_data["fees"].length).to eq(1)
+      )
+      result_data = result.dig("data", "regenerateFromVoided")
+      aggregate_failures do
+        expect(result.dig("errors")).to be_nil
+        expect(result_data.dig("id")).to be_present
+        expect(result_data.dig("status")).to eq(expected_status)
+        expect(result_data.dig("fees")&.size).to eq(1)
+      end
     end
   end
 
+  context "when customer has grace period" do
+    let(:customer) { create(:customer, organization:, invoice_grace_period: 1) }
+    let(:voided_invoice) { create(:invoice, :voided, organization:, customer:) }
+    let(:fee) { create(:fee, invoice: voided_invoice, organization:, add_on:) }
+
+    include_examples "regenerates invoice with expected status", "draft"
+  end
+
+  context "when customer has no grace period" do
+    let(:customer) { create(:customer, organization:, invoice_grace_period: 0) }
+    let(:voided_invoice) { create(:invoice, :voided, organization:, customer:) }
+    let(:fee) { create(:fee, invoice: voided_invoice, organization:, add_on:) }
+
+    include_examples "regenerates invoice with expected status", "finalized"
+  end
+
   it "returns an error if the invoice is not found or not voided (failure)" do
+    customer = create(:customer, organization:)
+    voided_invoice = create(:invoice, :voided, organization:, customer:)
+    fee = create(:fee, invoice: voided_invoice, organization:, add_on:)
+    fees = [{
+      id: fee.id,
+      addOnId: add_on.id,
+      description: "Updated description",
+      invoiceDisplayName: "Updated display name",
+      units: 5.0,
+      unitAmountCents: 1000
+    }]
     result = execute_graphql(
       current_organization: organization,
       current_user: user,
@@ -74,10 +100,10 @@ RSpec.describe Mutations::Invoices::RegenerateFromVoided, type: :graphql do
         }
       }
     )
-    expect(result["data"]["regenerateFromVoided"]).to be_nil
-    expect(result["errors"]).to be_present
+    expect(result.dig("data", "regenerateFromVoided")).to be_nil
+    expect(result.dig("errors")).to be_present
 
-    non_voided_invoice = create(:invoice, status: :finalized, organization: organization, customer: voided_invoice.customer)
+    non_voided_invoice = create(:invoice, status: :finalized, organization:, customer:)
     result2 = execute_graphql(
       current_organization: organization,
       current_user: user,
@@ -90,7 +116,7 @@ RSpec.describe Mutations::Invoices::RegenerateFromVoided, type: :graphql do
         }
       }
     )
-    expect(result2["data"]["regenerateFromVoided"]).to be_nil
-    expect(result2["errors"]).to be_present
+    expect(result2.dig("data", "regenerateFromVoided")).to be_nil
+    expect(result2.dig("errors")).to be_present
   end
 end
