140fa857c9fb413be5fcd92c528cf1acb17bc031
Ancor Cruz <hello@ancorcruz.com>
2025-07-08 12:29:50 +0100

    we have a migration that already adds source to events

--- db/migrate/20250707100400_add_source_to_events.rb
+++ db/migrate/20250707100400_add_source_to_events.rb
@@ -4,10 +4,10 @@ class AddSourceToEvents < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_column :events, :source, :string, default: 'usage', null: false
-    
-    add_index :events, :source, algorithm: :concurrently
-    add_index :events, [:organization_id, :source], algorithm: :concurrently
-    add_index :events, [:external_subscription_id, :source], algorithm: :concurrently
+    # add_column :events, :source, :string, default: 'usage', null: false
+
+    # add_index :events, :source, algorithm: :concurrently
+    # add_index :events, [:organization_id, :source], algorithm: :concurrently
+    # add_index :events, [:external_subscription_id, :source], algorithm: :concurrently
   end
 end 
