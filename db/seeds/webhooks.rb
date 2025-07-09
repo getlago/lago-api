5e874148e8275197948151a6c7fc8af8a9a0f986
Yohan Robert <7434196+groyoh@users.noreply.github.com>
2025-07-09 11:47:22 +0200

    chore(webhook): Update webhook endpoint created in seeds to a working one (#3951)
    ## Context
    
    https://github.com/getlago/lago/commit/1f9c64111519a59e5318491eecfff9f38683365b added a [`webhook-tester`](https://github.com/tarampampam/webhook-tester) container to the dev environment.
    
    ## Description
    
    This commit updates the URL of webhook endpoint created in seeds to match the URL of the new container.

--- db/seeds/webhooks.rb
+++ db/seeds/webhooks.rb
@@ -6,7 +6,7 @@ require "factory_bot_rails"
 organization = Organization.find_or_create_by!(name: "Hooli")
 ApiKey.find_or_create_by!(organization:)
 
-webhook_endpoint = WebhookEndpoint.find_or_create_by!(organization:, webhook_url: "http://test.lago.dev/webhook")
+webhook_endpoint = WebhookEndpoint.find_or_create_by!(organization:, webhook_url: "http://webhook/11111111-2222-3333-4444-555555555555")
 
 3.times do
   FactoryBot.create(:webhook, :succeeded, organization:, webhook_endpoint:)
