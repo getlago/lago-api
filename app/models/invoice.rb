0f2c1f92142e03ca5c39e03849e5f2621391629f
Ancor Cruz <hello@ancorcruz.com>
2025-07-10 20:22:41 +0100

    WIP: include fixed charge fees in invoice PDF (V4)

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -211,6 +211,15 @@ class Invoice < ApplicationRecord
       .any?
   end
 
+  def existing_fixed_charge_fees_in_interval?(subscription_id:, fixed_charge_in_advance: false)
+    subscription_fees(subscription_id)
+      .fixed_charge
+      .positive_units # could we invoice fees with negative units?
+      .joins(:fixed_charge)
+      .where(fixed_charge: {pay_in_advance: fixed_charge_in_advance})
+      .any?
+  end
+
   def recurring_fees(subscription_id)
     subscription_fees(subscription_id)
       .joins(charge: :billable_metric)
