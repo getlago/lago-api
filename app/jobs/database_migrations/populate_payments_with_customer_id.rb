bc994c843a508d733ee889a4285871a21cd3b3cf
Anna Velentsevich <annvelents@gmail.com>
2025-07-11 15:29:14 +0200

    use select to upgrade

--- app/jobs/database_migrations/populate_payments_with_customer_id.rb
+++ app/jobs/database_migrations/populate_payments_with_customer_id.rb
@@ -10,16 +10,14 @@ module DatabaseMigrations
     def perform(batch_number = 1)
       # rubocop:disable Rails/SkipsModelValidations
       Payment.where(payable_type: "Invoice", customer_id: nil)
-        .joins("LEFT JOIN invoices ON invoices.id = payments.payable_id AND payments.payable_type = 'Invoice'")
-        .where("payments.customer_id IS NULL AND invoices.customer_id IS NOT NULL")
+        .where("payable_id IN (SELECT id FROM invoices WHERE customer_id IS NOT NULL)")
         .limit(BATCH_SIZE)
-        .update_all("customer_id = invoices.customer_id")
+        .update_all("customer_id = (SELECT customer_id FROM invoices WHERE invoices.id = payments.payable_id)")
 
       Payment.where(payable_type: "PaymentRequest", customer_id: nil)
-        .joins("LEFT JOIN payment_requests ON payment_requests.id = payments.payable_id AND payments.payable_type = 'PaymentRequest'")
-        .where("payments.customer_id IS NULL AND payment_requests.customer_id IS NOT NULL")
+        .where("payable_id IN (SELECT id FROM payment_requests WHERE customer_id IS NOT NULL)")
         .limit(BATCH_SIZE)
-        .update_all("customer_id = payment_requests.customer_id")
+        .update_all("customer_id = (SELECT customer_id FROM payment_requests WHERE payment_requests.id = payments.payable_id)")
       # rubocop:enable Rails/SkipsModelValidations
 
       # Queue the next batch
