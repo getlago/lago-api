18dee041445e05bc97a861ff882b0d3a36e1711e
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-07-11 12:45:28 +0300

    Improve support of displaying unit amount and add tests

--- spec/views/helpers/charge_display_helper_spec.rb
+++ spec/views/helpers/charge_display_helper_spec.rb
@@ -11,8 +11,18 @@ RSpec.describe ChargeDisplayHelper do
     let(:plan) { create(:plan, amount_currency: "USD") }
     let(:charge) { create(:standard_charge, plan:, min_amount_cents: 500) }
 
-    it "returns the min amount with the appropriate currency symbol" do
-      expect(subject).to eq "$5.00"
+    context "when charge does not have applied pricing unit" do
+      it "returns the min amount with the appropriate currency symbol" do
+        expect(subject).to eq "$5.00"
+      end
+    end
+
+    context "when charge has applied pricing unit" do
+      let!(:applied_pricing_unit) { create(:applied_pricing_unit, pricing_unitable: charge) }
+
+      it "returns the min amount with the pricing unit's short name" do
+        expect(subject).to eq "5.00 #{applied_pricing_unit.pricing_unit.short_name}"
+      end
     end
   end
 end
