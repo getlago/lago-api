cda88f4a7074878cf05058202590dd1d41b439d6
Anna Velentsevich <annvelents@gmail.com>
2025-07-11 10:37:44 +0200

    fix linter

--- app/jobs/database_migrations/populate_payments_with_customer_id.rb
+++ app/jobs/database_migrations/populate_payments_with_customer_id.rb
@@ -8,6 +8,7 @@ module DatabaseMigrations
     BATCH_SIZE = 1000
 
     def perform(batch_number = 1)
+      # rubocop:disable Rails/SkipsModelValidations
       Payment.where(payable_type: "Invoice", customer_id: nil)
         .limit(BATCH_SIZE)
         .update_all("customer_id = (SELECT customer_id FROM invoices WHERE invoices.id = payments.payable_id)")
@@ -15,6 +16,7 @@ module DatabaseMigrations
       Payment.where(payable_type: "PaymentRequest", customer_id: nil)
         .limit(BATCH_SIZE)
         .update_all("customer_id = (SELECT customer_id FROM payment_requests WHERE payment_requests.id = payments.payable_id)")
+      # rubocop:enable Rails/SkipsModelValidations
 
       # Queue the next batch
       self.class.perform_later(batch_number + 1) if Payment.where(customer_id: nil).exists?
