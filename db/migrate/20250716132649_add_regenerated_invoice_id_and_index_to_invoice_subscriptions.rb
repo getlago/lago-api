4ebc9e81765455881bb08647211f0e934e676bf3
Miguel Pinto <darkymiguel@gmail.com>
2025-07-16 14:57:58 +0100

    feat: added regenerated_invoice_id

--- db/migrate/20250716132649_add_regenerated_invoice_id_and_index_to_invoice_subscriptions.rb
+++ db/migrate/20250716132649_add_regenerated_invoice_id_and_index_to_invoice_subscriptions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRegeneratedInvoiceIdAndIndexToInvoiceSubscriptions < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
@@ -5,10 +7,10 @@ class AddRegeneratedInvoiceIdAndIndexToInvoiceSubscriptions < ActiveRecord::Migr
     add_column :invoice_subscriptions, :regenerated_invoice_id, :uuid
 
     add_index :invoice_subscriptions,
-              [:subscription_id, :invoicing_reason],
-              unique: true,
-              name: :index_unique_terminating_invoice_subscription,
-              where: "invoicing_reason = 'subscription_terminating' AND regenerated_invoice_id IS NULL",
-              algorithm: :concurrently
+      [:subscription_id, :invoicing_reason],
+      unique: true,
+      name: :index_unique_terminating_invoice_subscription,
+      where: "invoicing_reason = 'subscription_terminating' AND regenerated_invoice_id IS NULL",
+      algorithm: :concurrently
   end
 end
