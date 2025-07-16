d2e538794308f25e2998e01226151079fd7ebd80
Anna Velentsevich <annvelents@gmail.com>
2025-07-16 18:44:25 +0200

    update fixed_charge with more things from poc

--- app/models/fixed_charge.rb
+++ app/models/fixed_charge.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class FixedCharge < ApplicationRecord
+  include PaperTrailTraceable
   include Discard::Model
+
   self.discard_column = :deleted_at
 
   belongs_to :organization
