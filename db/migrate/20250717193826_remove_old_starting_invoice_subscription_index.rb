b2d075eb7eb05489d7e3211237829e7f98d6d6a3
Miguel Pinto <darkymiguel@gmail.com>
2025-07-17 20:40:18 +0100

    feat: added adjusted_fee texes

---
+++ db/migrate/20250717193826_remove_old_starting_invoice_subscription_index.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class RemoveOldStartingInvoiceSubscriptionIndex < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
+  def up
+    remove_index :invoice_subscriptions, name: :index_unique_starting_subscription_invoice
+  end
+
+  def down
+    add_index :invoice_subscriptions,
+              [:subscription_id, :invoicing_reason],
+              unique: true,
+              name: :index_unique_starting_subscription_invoice,
+              where: "invoicing_reason = 'subscription_starting'",
+              algorithm: :concurrently
+  end
+end
