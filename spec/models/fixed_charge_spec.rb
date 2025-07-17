e43b24e0316a67d64b01a1ef57cb8d8cbd823207
Anna Velentsevich <annvelents@gmail.com>
2025-07-17 12:25:13 +0200

    fix linter

--- spec/models/fixed_charge_spec.rb
+++ spec/models/fixed_charge_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe FixedCharge, type: :model do
   subject { build(:fixed_charge) }
+
   it_behaves_like "paper_trail traceable"
 
   it { expect(described_class).to be_soft_deletable }
