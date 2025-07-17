b03b693cb21dae968c81b102a29f2d08ea10fa68
Miguel Pinto <darkymiguel@gmail.com>
2025-07-17 17:50:17 +0100

    feat: added regenerated_invoice_id

--- app/services/invoices/regenerate_from_voided_service.rb
+++ app/services/invoices/regenerate_from_voided_service.rb
@@ -23,7 +23,7 @@ module Invoices
         invoice = create_regenerated_invoice
         create_invoice_subscription(invoice) if invoice.invoice_type == "subscription"
         process_fees(invoice)
-        adjust_feea(invoice)
+        adjust_fees(invoice)
         Invoices::ApplyInvoiceCustomSectionsService.call(invoice: invoice)
         Invoices::ComputeAmountsFromFees.call(invoice: invoice)
         Invoices::TransitionToFinalStatusService.call(invoice: invoice)
@@ -61,25 +61,46 @@ module Invoices
       end
     end
 
-    def adjust_feea(invoice)
+    def adjust_fees(invoice)
       invoice.fees.each do |fee|
-        adjusted_fee = fee.adjusted_fee
-        fee.invoice_display_name = fee.adjusted_fee.invoice_display_name if adjusted_fee.invoice_display_name.present?
-        byebug
-        fee.charge_id = adjusted_fee.charge_id if adjusted_fee.charge_id.present?
-        fee.subscription_id = adjusted_fee.subscription_id if adjusted_fee.subscription_id.present?
+        if fee.fee_type == "charge"
+          result = Fees::InitFromAdjustedChargeFeeService.call(
+            adjusted_fee: fee.adjusted_fee,
+            boundaries: fee.properties,
+            properties: fee.charge.properties
+          )
+          adjusted_fee = result.fee
+          attrs = adjusted_fee.attributes.slice(
+            "invoice_display_name",
+            "charge_id",
+            "subscription_id",
+            "units",
+            "unit_amount_cents",
+            "precise_unit_amount",
+            "amount_cents",
+            "precise_amount_cents",
+            "amount_details"
+          )
+          fee.assign_attributes(attrs)
+          fee.save!
+        else
+          adjusted_fee = fee.adjusted_fee
+          fee.invoice_display_name = fee.adjusted_fee.invoice_display_name if adjusted_fee.invoice_display_name.present?
+          fee.charge_id = adjusted_fee.charge_id if adjusted_fee.charge_id.present?
+          fee.subscription_id = adjusted_fee.subscription_id if adjusted_fee.subscription_id.present?
+          fee.units = adjusted_fee.units if adjusted_fee.units.present?
 
-        fee.units = adjusted_fee.units if adjusted_fee.units.present?
+          units = fee.units
+          subunit = invoice.total_amount.currency.subunit_to_unit
 
+          unit_precise_amount_cents = adjusted_fee.unit_precise_amount_cents || fee.charge
 
-        units = fee.units
-        subunit = invoice.total_amount.currency.subunit_to_unit
-        unit_precise_amount_cents = adjusted_fee.unit_precise_amount_cents
-        fee.unit_amount_cents = unit_precise_amount_cents.round
-        fee.precise_unit_amount = unit_precise_amount_cents.to_d / subunit
-        fee.amount_cents = (units * unit_precise_amount_cents).round
-        fee.precise_amount_cents = units * unit_precise_amount_cents
-        fee.save!
+          fee.unit_amount_cents = unit_precise_amount_cents.round
+          fee.precise_unit_amount = unit_precise_amount_cents.to_d / subunit
+          fee.amount_cents = (units * unit_precise_amount_cents).round
+          fee.precise_amount_cents = units * unit_precise_amount_cents
+          fee.save!
+        end
       end
     end
 
@@ -94,11 +115,10 @@ module Invoices
         adjusted_fee_params = {
           invoice_display_name: fee_params[:invoice_display_name],
           units: fee_params[:units],
-          unit_precise_amount: fee_params[:unit_amount_cents],
           charge_id: fee_params[:charge_id],
           subscription_id: fee_params[:subscription_id]
         }
-
+        adjusted_fee_params[:unit_precise_amount] = fee_params[:unit_amount_cents] if fee_params[:unit_amount_cents].present?
         adjusted_fee_params[:fee_id] = dep_fee.id if dep_fee
 
         AdjustedFees::CreateService.call(
