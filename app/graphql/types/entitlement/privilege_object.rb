d3a02a9f98f138a429efce3c01d21bb05edb8e20
Julien Bourdeau <julien@julienbourdeau.com>
2025-07-21 17:24:03 +0200

    feat(entitlements): Restore privilege ids in gql (#4036)
    ID is used to know if the privilege exists and is being edited or if
    it's new.

--- app/graphql/types/entitlement/privilege_object.rb
+++ app/graphql/types/entitlement/privilege_object.rb
@@ -3,6 +3,8 @@
 module Types
   module Entitlement
     class PrivilegeObject < Types::BaseObject
+      field :id, ID, null: false
+
       field :code, String, null: false
       field :config, Types::Entitlement::PrivilegeConfigObject, null: false
       field :name, String, null: true
