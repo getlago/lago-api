12916e4bc4c9f9df2768023fccf4dc2106e8bcbf
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-07-23 14:46:57 +0300

    feat(pricing-units): Adjust PDF for pricing unit (#3867)
    ## Context
    
    Adding support for showing fee's amount in the PDF in pricing units.
    
    ## Description
    
    Adjust invoice PDF template to support amounts in pricing units

--- app/views/helpers/money_helper.rb
+++ app/views/helpers/money_helper.rb
@@ -2,6 +2,7 @@
 
 class MoneyHelper
   SYMBOLS_CURRENCIES = %w[$ € £ ¥].freeze
+  DEFAULT_STUB_CURRENCY = "USD"
 
   def self.format(money)
     money&.format(
@@ -15,6 +16,15 @@ class MoneyHelper
     amount_cents = normalize_precision(amount_cents)
     money = Utils::MoneyWithPrecision.from_amount(amount_cents, currency)
     format(money)
+  end
+
+  def self.format_pricing_unit(amount_cents, currency)
+    format_with_custom_currency(amount_cents, currency.short_name)
+  end
+
+  def self.format_pricing_unit_with_precision(amount_cents, currency)
+    amount_cents = normalize_precision(amount_cents)
+    format_with_custom_currency(amount_cents, currency.short_name)
   end
 
   def self.currency_format(money_currency)
@@ -31,5 +41,14 @@ class MoneyHelper
     else
       amount_cents.round(6)
     end
+  end
+
+  def self.format_with_custom_currency(amount, currency_code)
+    stub = Utils::MoneyWithPrecision.from_amount(amount, DEFAULT_STUB_CURRENCY)
+    stub.format(
+      format: "%n #{currency_code}",
+      decimal_mark: I18n.t("money.decimal_mark"),
+      thousands_separator: I18n.t("money.thousands_separator")
+    )
   end
 end
