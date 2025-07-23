7f2d9c5bd3a779a778b1eaf1e304f1dd67a1e109
Julien Bourdeau <julien@julienbourdeau.com>
2025-07-23 13:35:08 +0200

    fix(entitlements): Remove update resource routes (#4058)
    This endpoint is specifically defined with PATCH line 51.

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Rails.application.routes.draw do
       resources :subscriptions, only: %i[create update show index], param: :external_id do
         resource :lifetime_usage, only: %i[show update], controller: "subscriptions/lifetime_usages"
         resources :alerts, only: %i[create index update show destroy], param: :code, controller: "subscriptions/alerts"
-        resources :entitlements, only: %i[index update destroy], param: :code, code: /.*/, controller: "subscriptions/entitlements" do
+        resources :entitlements, only: %i[index destroy], param: :code, code: /.*/, controller: "subscriptions/entitlements" do
           resources :privileges, only: %i[destroy], param: :code, code: /.*/, controller: "subscriptions/entitlements/privileges"
           post :remove, on: :member
           post :restore, on: :member
