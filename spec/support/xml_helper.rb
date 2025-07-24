f4be3f11dedab2c667fabad526205fdb96d19bd4
Mario Diniz <mariohenriquediniz@gmail.com>
2025-07-24 18:06:48 -0300

    feat(e-invoicing): Add tests to FacturX::Create::Context and Header

---
+++ spec/support/xml_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module XMLHelper
+  NAMESPACES = {
+    factur_x: EInvoice::FacturX::Create::Builder::ROOT_NAMESPACES
+  }
+
+  def xml_document(ns)
+    Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
+      xml.TestRoot(NAMESPACES[ns]) do
+        yield xml
+      end
+    end.doc
+  end
+
+  def xml_fragment(ns, &block)
+    xml_document(ns, &block).root.children.to_xml(
+      save_with: Nokogiri::XML::Node::SaveOptions::NO_DECLARATION
+    )
+  end
+end
