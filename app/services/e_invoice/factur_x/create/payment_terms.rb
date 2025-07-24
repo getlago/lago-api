caa2500bcf6cfedaee64f590a293ecf60b45bb4c
Mario Diniz <mariohenriquediniz@gmail.com>
2025-07-24 13:00:28 -0300

    feat(e-invoicing): PaymentTerms and MonetarySummation tags

---
+++ app/services/e_invoice/factur_x/create/payment_terms.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module EInvoice
+  module FacturX
+    module Create
+      class PaymentTerms < Builder
+        def call
+          xml.comment "Payment Terms"
+          xml['ram'].SpecifiedTradePaymentTerms do
+            xml['ram'].Description "Net payment term: 0 days"
+            xml['ram'].DueDateDateTime do
+              xml['udt'].DateTimeString formatted_date("20250626".to_date), "format" => 102
+            end
+          end
+        end
+      end
+    end
+  end
+end
