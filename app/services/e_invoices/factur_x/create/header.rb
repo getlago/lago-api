c69a330ac4d20ce8e54fcc84df114d2b6d08dfae
Mario Diniz <mariohenriquediniz@gmail.com>
2025-07-25 15:22:39 -0300

    feat(e-invoicing): set constants

--- app/services/e_invoices/factur_x/create/header.rb
+++ app/services/e_invoices/factur_x/create/header.rb
@@ -3,14 +3,16 @@
 module EInvoices
   module FacturX
     module Create
+      COMMERCIAL_INVOICE = 380
+
       class Header < Builder
         def call
           xml.comment "Exchange Document Header"
           xml["rsm"].ExchangedDocument do
             xml["ram"].ID invoice.number
-            xml["ram"].TypeCode 380
+            xml["ram"].TypeCode COMMERCIAL_INVOICE
             xml["ram"].IssueDateTime do
-              xml["udt"].DateTimeString formatted_date(invoice.issuing_date), "format" => 102
+              xml["udt"].DateTimeString formatted_date(invoice.issuing_date), format: YYMMDD
             end
             xml["ram"].IncludedNote do
               xml["ram"].Content "Invoice ID: #{invoice.id}"
