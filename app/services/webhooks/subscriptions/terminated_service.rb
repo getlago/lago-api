1f4720294cff82d4db13bd59bd520f7ee1edb8a3
Yohan Robert <yohan@getlago.com>
2025-07-25 15:08:26 +0200

    chore(api): Improve subscription APIs after OpenAPI review

--- app/services/webhooks/subscriptions/terminated_service.rb
+++ app/services/webhooks/subscriptions/terminated_service.rb
@@ -2,23 +2,11 @@
 
 module Webhooks
   module Subscriptions
-    class TerminatedService < Webhooks::BaseService
+    class TerminatedService < BaseService
       private
-
-      def object_serializer
-        ::V1::SubscriptionSerializer.new(
-          object,
-          root_name: "subscription",
-          includes: %i[plan customer]
-        )
-      end
 
       def webhook_type
         "subscription.terminated"
-      end
-
-      def object_type
-        "subscription"
       end
     end
   end
