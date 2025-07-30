a41d40215210a294a55cb6ed0dd29bf6f35c3894
Mario Diniz <mariohenriquediniz@gmail.com>
2025-07-30 12:16:47 -0300

    feat(e-invoicing): SpecifiedTradeSettlementHeaderMonetarySummation tag test

--- spec/services/e_invoices/factur_x/create_service_spec.rb
+++ spec/services/e_invoices/factur_x/create_service_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe EInvoices::FacturX::CreateService, type: :service do
@@ -6,6 +8,7 @@ RSpec.describe EInvoices::FacturX::CreateService, type: :service do
 
   describe "#call" do
     it "builds the XML and writes it to a file" do
+      # rubocop:disable RSpec/MessageSpies
       expect(Nokogiri::XML::Builder).to receive(:new).with(encoding: "UTF-8")
         .and_yield(xml_builder_double).and_return(xml_builder_double)
 
@@ -15,6 +18,7 @@ RSpec.describe EInvoices::FacturX::CreateService, type: :service do
       expect(File).to receive(:write).with("output.xml", "<xml>content</xml>")
 
       described_class.new(invoice:).call
+      # rubocop:enable RSpec/MessageSpies
     end
   end
 end
