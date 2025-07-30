ec071de4a619c62a3cdb99fbb4c4672bd8ff932c
jdenquin <jdenquin@gmail.com>
2025-07-30 17:52:00 +0200

    remove isolation_level for spec check

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module LagoApi
     config.api_only = true
     config.active_job.queue_adapter = :sidekiq
 
-    config.active_support.isolation_level = :fiber
+    # config.active_support.isolation_level = :fiber
 
     # Configuration for active record encryption
     config.active_record.encryption.hash_digest_class = OpenSSL::Digest::SHA1
