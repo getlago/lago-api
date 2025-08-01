93d7379920a929a07e22f723dc4e68a832ec7300
Miguel Pinto <darkymiguel@gmail.com>
2025-08-01 09:43:56 +0100

    feat: added test

--- db/migrate/20250731144632_add_scoped_index_to_starting_invoice_subscription.rb
+++ db/migrate/20250731144632_add_scoped_index_to_starting_invoice_subscription.rb
@@ -9,6 +9,7 @@ class AddScopedIndexToStartingInvoiceSubscription < ActiveRecord::Migration[8.0]
       unique: true,
       name: :index_unique_starting_invoice_subscription,
       where: "invoicing_reason = 'subscription_starting' AND regenerated_invoice_id IS NULL",
-      algorithm: :concurrently
+      algorithm: :concurrently,
+      if_not_exists: true
   end
 end
--- db/migrate/20250731145640_remove_old_starting_invoice_subscription_index.rb
+++ db/migrate/20250731145640_remove_old_starting_invoice_subscription_index.rb
@@ -4,7 +4,9 @@ class RemoveOldStartingInvoiceSubscriptionIndex < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def up
-    remove_index :invoice_subscriptions, name: :index_unique_starting_subscription_invoice
+    if index_exists?(:invoice_subscriptions, nil, name: :index_unique_starting_subscription_invoice)
+      remove_index :invoice_subscriptions, name: :index_unique_starting_subscription_invoice
+    end
   end
 
   def down
@@ -13,6 +15,7 @@ class RemoveOldStartingInvoiceSubscriptionIndex < ActiveRecord::Migration[8.0]
       unique: true,
       name: :index_unique_starting_subscription_invoice,
       where: "invoicing_reason = 'subscription_starting'",
-      algorithm: :concurrently
+      algorithm: :concurrently,
+      if_not_exists: true
   end
 end
