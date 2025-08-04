4c6c6f5342adea73e9bf4521f69e679d08ecfcd2
jdenquin <jdenquin@gmail.com>
2025-08-04 15:33:11 +0200

    fix linter

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -3,7 +3,7 @@
 require "yabeda"
 
 Yabeda.configure do
-    default_tag :service, "lago-api"
-    default_tag :environment, Rails.env
-    default_tag :version, ENV["LAGO_VERSION"] || "unknown"
+  default_tag :service, ENV["OTEL_SERVICE_NAME"] || "lago-api"
+  default_tag :environment, Rails.env
+  default_tag :version, ENV["LAGO_VERSION"] || "unknown"
 end
