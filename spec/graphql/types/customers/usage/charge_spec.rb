6bd69d8eb23f3e00a44d8d70a0e4d6fefe0be395
Murillo Parreira <murparreira@gmail.com>
2025-08-04 11:34:35 -0300

    feat(projected-usage): add projected usage data to the current usage endpoint (#3905)
    ## Context
    
    We offer a endpoint and a UI for the current usage of the customers of
    our users. They can see in real time the consumption of each feature for
    the current period. However, there is no way to calculate the projected
    usage. So we need to add this feature on our platform.
    Here is the [spec
    document](https://www.notion.so/getlago/Spec-Projected-usage-for-current-periods-218ef63110d280df877bfcb0be4045c4).
    Here is the [dive in
    document](https://www.notion.so/getlago/BE-Dive-In-Projected-usage-for-current-periods-WIP-21cef63110d280318540fe4310df8048).
    
    ## Description
    
    There are many changes in this PR:
    
    - Add a service for calculating projected usage data based on the fees
    and the boundary dates.
    - Change the serializer for the API to use this new service to calculate
    the projected usage values added to the response.
    - Change the graphql types to use this new service to calculate the
    projected usage values added to the response.
    - Made several specs to test the behavior and modified existing ones,
    because there were changes to an existing endpoint.

--- spec/graphql/types/customers/usage/charge_spec.rb
+++ spec/graphql/types/customers/usage/charge_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe Types::Customers::Usage::Charge do
   it do
     expect(subject).to have_field(:id).of_type("ID!")
     expect(subject).to have_field(:amount_cents).of_type("BigInt!")
+    expect(subject).to have_field(:projected_amount_cents).of_type("BigInt!")
     expect(subject).to have_field(:events_count).of_type("Int!")
     expect(subject).to have_field(:units).of_type("Float!")
+    expect(subject).to have_field(:projected_units).of_type("Float!")
     expect(subject).to have_field(:billable_metric).of_type("BillableMetric!")
     expect(subject).to have_field(:charge).of_type("Charge!")
     expect(subject).to have_field(:grouped_usage).of_type("[GroupedChargeUsage!]!")
