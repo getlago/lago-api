daf56d839e1d03f96c9a7fe60be93be23d747220
Ancor Cruz <hello@ancorcruz.com>
2025-08-08 12:15:27 +0100

    Feat(fixed_charges): Add FixedCharges::CreateService (#4113)
    ## Roadmap Task
    
    👉
    https://getlago.canny.io/feature-requests/p/allow-add-ons-to-be-added-to-subscription-invoices
    
    👉
    https://getlago.canny.io/feature-requests/p/define-quantities-for-plan-charges
    
     ## Context
    
     What is the current situation?
    
     **Option 1:** User has to create a one off invoice alongside the
     subscription, it will create 2 different invoices.
    
     **Option 2:** User can add a recurring billable metric and use event to
     have this fee invoice on subscription renewal, but it won’t appear on
     the first billing subscription.
    
     What problem are we trying to solve?
    
     At subscription creation or afterward, there is no clear way to invoice
     a fixed fee that is not tied to events, aside from the subscription fee
     itself. This fee could be either a one-time charge or a recurring one.
    
     ## Description
    
     Add `FixedCharges::CreateService` to handle fixed charge creation:
    
     * accepts add_on id and code
     * apply default properties based on fixed charge's charge model
     * filter properties based on the charge model
     * apply taxes to the fixed charge

---
+++ app/services/fixed_charges/create_service.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+module FixedCharges
+  class CreateService < BaseService
+    Result = BaseResult[:fixed_charge]
+
+    def initialize(plan:, params:)
+      @plan = plan
+      @params = params
+
+      super
+    end
+
+    def call
+      return result.not_found_failure!(resource: "plan") unless plan
+
+      ActiveRecord::Base.transaction do
+        fixed_charge = plan.fixed_charges.new(
+          organization_id: plan.organization_id,
+          add_on_id: add_on.id,
+          invoice_display_name: params[:invoice_display_name],
+          charge_model: params[:charge_model],
+          parent_id: params[:parent_id],
+          pay_in_advance: params[:pay_in_advance] || false,
+          prorated: params[:prorated] || false,
+          units: params[:units] || 0
+        )
+
+        properties = params[:properties].presence || ChargeModels::BuildDefaultPropertiesService.call(fixed_charge.charge_model)
+        fixed_charge.properties = ChargeModels::FilterPropertiesService.call(
+          chargeable: fixed_charge,
+          properties:
+        ).properties
+
+        fixed_charge.save!
+
+        if params[:tax_codes]
+          taxes_result = FixedCharges::ApplyTaxesService.call(fixed_charge:, tax_codes: params[:tax_codes])
+          taxes_result.raise_if_error!
+        end
+
+        result.fixed_charge = fixed_charge
+      end
+
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    rescue ActiveRecord::RecordNotFound => e
+      result.not_found_failure!(resource: e.model.underscore)
+    rescue BaseService::FailedResult => e
+      e.result
+    end
+
+    private
+
+    attr_reader :plan, :params
+
+    delegate :organization, to: :plan
+
+    def add_on
+      if params[:add_on_id].present?
+        organization.add_ons.find(params[:add_on_id])
+      elsif params[:add_on_code].present?
+        organization.add_ons.find_by!(code: params[:add_on_code])
+      else
+        raise ArgumentError, "Either add_on_id or add_on_code must be provided"
+      end
+    end
+  end
+end
