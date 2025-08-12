ab13a4bce1e25707efdcb0f1be39b3d5cda14623
Lovro Colic <lovro.colic.lc@gmail.com>
2025-08-12 15:21:31 +0200

    fix spec

--- spec/graphql/types/invoices/fee_input_spec.rb
+++ spec/graphql/types/invoices/fee_input_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Types::Invoices::FeeInput do
     expect(subject).to accept_argument(:tax_codes).of_type("[String!]")
     expect(subject).to accept_argument(:unit_amount_cents).of_type("BigInt")
     expect(subject).to accept_argument(:units).of_type("Float")
-    expect(subject).to accept_argument(:from_datetime).of_type("ISO8601DateTime")
-    expect(subject).to accept_argument(:to_datetime).of_type("ISO8601DateTime")
+    expect(subject).to accept_argument(:from_datetime).of_type("ISO8601DateTime!")
+    expect(subject).to accept_argument(:to_datetime).of_type("ISO8601DateTime!")
   end
 end
