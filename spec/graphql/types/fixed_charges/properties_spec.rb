8e0ea76f8cbdd5b7d4025249c01352a2f892a2f5
Ancor Cruz <hello@ancorcruz.com>
2025-08-12 08:30:41 +0100

    fix test

--- spec/graphql/types/fixed_charges/properties_spec.rb
+++ spec/graphql/types/fixed_charges/properties_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Types::FixedCharges::Properties do
   subject { described_class }
 
   it { is_expected.to have_field(:amount).of_type("String") }
-  it { is_expected.to have_field(:graduated_ranges).of_type("[GraduatedRangeInput!]") }
-  it { is_expected.to have_field(:volume_ranges).of_type("[VolumeRangeInput!]") }
+  it { is_expected.to have_field(:graduated_ranges).of_type("[GraduatedRange!]") }
+  it { is_expected.to have_field(:volume_ranges).of_type("[VolumeRange!]") }
 end
