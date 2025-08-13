26f0bcc8cb9b72dfb51a0c4bc3d4ef0ad2f22516
Ancor Cruz <hello@ancorcruz.com>
2025-08-13 10:20:25 +0100

    Migrate Clock::RefreshWalletsOngoingBalance for job overrides
    job accepts organization,
    if organization is provided only apply the refresh for wallets of the
      given organization
    if organization is not provided apply wallet refresh for all wallets but
      the ones belonging to organizations with a JobScheduledOverride
      enabled for this job class.

--- app/jobs/clock/refresh_wallets_ongoing_balance_job.rb
+++ app/jobs/clock/refresh_wallets_ongoing_balance_job.rb
@@ -4,10 +4,23 @@ module Clock
   class RefreshWalletsOngoingBalanceJob < ClockJob
     unique :until_executed, on_conflict: :log
 
-    def perform
+    def perform(organization: nil)
       return unless License.premium?
 
-      Wallet.active.ready_to_be_refreshed.find_each do |wallet|
+      wallets = Wallet.active.ready_to_be_refreshed
+
+      if organization
+        wallets = wallets.where(organization_id: organization.id)
+      else
+        excluded_organization_ids = JobScheduleOverride
+          .enabled
+          .where(job_name: self.class.name)
+          .pluck(:organization_id)
+
+        wallets = wallets.where.not(organization_id: excluded_organization_ids) if excluded_organization_ids.any?
+      end
+
+      wallets.find_each do |wallet|
         Wallets::RefreshOngoingBalanceJob.perform_later(wallet)
       end
     end
