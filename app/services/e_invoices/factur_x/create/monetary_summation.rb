ca457164662072d4c6ad0f1703c0d7a6adead435
Mario Diniz <mariohenriquediniz@gmail.com>
2025-08-13 11:19:56 -0300

    FacturX XML into invoice PDFs (#4114)
    FacturX implementation for `FR` country.
    
    ---------
    
    Co-authored-by: Romain Sempé <romain@getlago.com>

---
+++ app/services/e_invoices/factur_x/create/monetary_summation.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module EInvoices
+  module FacturX
+    module Create
+      class MonetarySummation < Builder
+        def call
+          xml.comment "Monetary Summation"
+          xml["ram"].SpecifiedTradeSettlementHeaderMonetarySummation do
+            xml["ram"].LineTotalAmount format_number(invoice.fees_amount)
+            xml["ram"].ChargeTotalAmount format_number(0)
+            xml["ram"].AllowanceTotalAmount format_number(invoice.coupons_amount)
+            xml["ram"].TaxBasisTotalAmount format_number(invoice.sub_total_excluding_taxes_amount)
+            xml["ram"].TaxTotalAmount format_number(invoice.taxes_amount), currencyID: invoice.currency
+            xml["ram"].GrandTotalAmount format_number(invoice.sub_total_including_taxes_amount)
+            xml["ram"].TotalPrepaidAmount format_number(prepaid_amount)
+            xml["ram"].DuePayableAmount format_number(due_payable_amount)
+          end
+        end
+
+        private
+
+        def prepaid_amount
+          invoice.prepaid_credit_amount + invoice.credit_notes_amount
+        end
+
+        def due_payable_amount
+          invoice.total_amount - prepaid_amount
+        end
+      end
+    end
+  end
+end
