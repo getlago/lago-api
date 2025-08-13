275723f84154293f9978c8792d1be0b6ac9a8214
Ancor Cruz <hello@ancorcruz.com>
2025-08-13 10:20:25 +0100

    Migrate Clock::RefreshWalletsOngoingBalance for job overrides
    job accepts organization,
    if organization is provided only apply the refresh for wallets of the
      given organization
    if organization is not provided apply wallet refresh for all wallets but
      the ones belonging to organizations with a JobScheduledOverride
      enabled for this job class.

--- spec/jobs/clock/refresh_wallets_ongoing_balance_job_spec.rb
+++ spec/jobs/clock/refresh_wallets_ongoing_balance_job_spec.rb
@@ -38,6 +38,44 @@ describe Clock::RefreshWalletsOngoingBalanceJob, job: true do
           expect(Wallets::RefreshOngoingBalanceJob).not_to have_been_enqueued.with(wallet)
         end
       end
+
+      context "with organization" do
+        let(:other_organization_wallet) { create(:wallet, ready_to_be_refreshed: true) }
+
+        before { other_organization_wallet }
+
+        it "refreshes only wallets for the given organization" do
+          described_class.perform_now(organization:)
+
+          expect(Wallets::RefreshOngoingBalanceJob).to have_been_enqueued.with(wallet)
+          expect(Wallets::RefreshOngoingBalanceJob).not_to have_been_enqueued.with(other_organization_wallet)
+        end
+      end
+
+      context "when JobOverride is enabled for the wallet organization" do
+        let(:job_schedule_override) do
+          create(
+            :job_schedule_override,
+            organization:,
+            job_name: described_class.name
+          )
+        end
+
+        let(:other_organization_wallet) do
+          create(:wallet, ready_to_be_refreshed: true)
+        end
+
+        before do
+          other_organization_wallet
+          job_schedule_override
+        end
+
+        it "does not call the refresh service for the wallet" do
+          described_class.perform_now
+          expect(Wallets::RefreshOngoingBalanceJob).not_to have_been_enqueued.with(wallet)
+          expect(Wallets::RefreshOngoingBalanceJob).to have_been_enqueued.with(other_organization_wallet)
+        end
+      end
     end
   end
 end
