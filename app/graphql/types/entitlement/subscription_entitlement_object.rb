2ffeebb81de60cb328a1d7107b509c3c815a3494
Julien Bourdeau <julien@julienbourdeau.com>
2025-08-14 17:57:14 +0200

    remove feature field :grim:

--- app/graphql/types/entitlement/subscription_entitlement_object.rb
+++ app/graphql/types/entitlement/subscription_entitlement_object.rb
@@ -9,13 +9,6 @@ module Types
       field :description, String, null: true
       field :name, String, null: false
       field :privileges, [SubscriptionEntitlementPrivilegeObject], null: false
-
-      # TODO: Remove to avoid N+1, all feature attributes are already part of the SubscriptionEntitlement
-      field :feature, Types::Entitlement::FeatureObject, null: false
-
-      def privileges
-        object.privileges.sort_by(&:created_at)
-      end
     end
   end
 end
