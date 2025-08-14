42b0d73266eb8ca3a9e9f4f03eca40748f0f7cc2
Mario Diniz <mariohenriquediniz@gmail.com>
2025-08-14 11:09:40 -0300

    feat(e-invoices): Delivery tag for UBL

--- spec/services/e_invoices/ubl/create/builder_spec.rb
+++ spec/services/e_invoices/ubl/create/builder_spec.rb
@@ -9,7 +9,17 @@ RSpec.describe EInvoices::Ubl::Create::Builder, type: :service do
     end
   end
 
-  let(:invoice) { create(:invoice) }
+  let(:invoice_subscription) { create(:invoice_subscription, :boundaries, invoice:, subscription: subscription) }
+  let(:subscription) { create(:subscription, started_at: "2025-03-16".to_date) }
+  let(:invoice) { create(:invoice, total_amount_cents: 30_00, currency: "USD", coupons_amount_cents: 1000) }
+  let(:fee) { create(:fee, invoice:, amount_cents: 10000, taxes_rate: 20.00) }
+  let(:invoice_applied_tax) { create(:invoice_applied_tax, invoice:, tax_rate: 20.00) }
+
+  before do
+    fee
+    invoice_subscription
+    invoice_applied_tax
+  end
 
   shared_examples "xml section" do |section|
     it "contains the section tag" do
@@ -31,7 +41,8 @@ RSpec.describe EInvoices::Ubl::Create::Builder, type: :service do
       ubl_info: {name: "UBL Version and Customization", xpath: "//cbc:UBLVersionID"},
       invoice_header: {name: "Invoice Header Information", xpath: "//cbc:ID"},
       invoice_seller: {name: "Supplier Party", xpath: "//cac:AccountingSupplierParty"},
-      invoice_customer: {name: "Customer Party", xpath: "//cac:AccountingCustomerParty"}
+      invoice_customer: {name: "Customer Party", xpath: "//cac:AccountingCustomerParty"},
+      delivery: {name: "Delivery Information", xpath: "//cac:Delivery"}
     }.each do |reference, section|
       it_behaves_like "xml section", section
     end
