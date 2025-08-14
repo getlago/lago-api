7612d927d3676440ffcffbee316e7902e9de146d
Mario Diniz <mariohenriquediniz@gmail.com>
2025-08-14 12:56:33 -0300

    feat(e-invoices): Refactor TradeSettlementPayment tag for FacturX

--- app/services/e_invoices/base_service.rb
+++ app/services/e_invoices/base_service.rb
@@ -10,6 +10,12 @@ module EInvoices
     # https://service.unece.org/trade/untdid/d00a/tred/tred5153.htm
     VAT = "VAT"
 
+    # You can see more payments codes UNTDID 4461 here
+    # https://service.unece.org/trade/untdid/d21b/tred/tred4461.htm
+    STANDARD = 1
+    PREPAID = 57
+    CREDIT_NOTE = 97
+
     def initialize(invoice:)
       super
 
@@ -43,6 +49,36 @@ module EInvoices
           ::Subscriptions::DatesService.new_instance(subscription, Time.current, current_usage: true)
             .charges_from_datetime
         end.min
+      end
+    end
+
+    def payment_information
+      case type
+      when STANDARD
+        payment_label
+      when PREPAID, CREDIT_NOTE
+        I18n.t("invoice.e_invoicing.payment_information", payment_label:, currency: invoice.currency, amount:)
+      end
+    end
+
+    def payment_label
+      case type
+      when STANDARD
+        I18n.t("invoice.e_invoicing.standard_payment")
+      when PREPAID
+        I18n.t("invoice.prepaid_credits")
+      when CREDIT_NOTE
+        I18n.t("invoice.credit_notes")
+      end
+    end
+
+    def build_payments(invoice, &block)
+      {
+        STANDARD => invoice.total_due_amount,
+        PREPAID => invoice.prepaid_credit_amount,
+        CREDIT_NOTE => invoice.credit_notes_amount
+      }.each do |type, amount|
+        yield(type, amount) if amount.positive?
       end
     end
   end
--- app/services/e_invoices/factur_x/create/trade_settlement_payment.rb
+++ app/services/e_invoices/factur_x/create/trade_settlement_payment.rb
@@ -4,12 +4,6 @@ module EInvoices
   module FacturX
     module Create
       class TradeSettlementPayment < Builder
-        # You can see more codes UNTDID 4461 here
-        # https://service.unece.org/trade/untdid/d21b/tred/tred4461.htm
-        STANDARD = 1
-        PREPAID = 57
-        CREDIT_NOTE = 97
-
         def initialize(xml:, invoice:, type:, amount: nil)
           @type = type
           @amount = amount
@@ -20,33 +14,13 @@ module EInvoices
           xml.comment "Payment Means: #{payment_label}"
           xml["ram"].SpecifiedTradeSettlementPaymentMeans do
             xml["ram"].TypeCode type
-            xml["ram"].Information information
+            xml["ram"].Information payment_information
           end
         end
 
         private
 
         attr_accessor :type, :amount
-
-        def information
-          case type
-          when STANDARD
-            payment_label
-          when PREPAID, CREDIT_NOTE
-            I18n.t("invoice.e_invoicing.payment_information", payment_label:, currency: invoice.currency, amount:)
-          end
-        end
-
-        def payment_label
-          case type
-          when STANDARD
-            I18n.t("invoice.e_invoicing.standard_payment")
-          when PREPAID
-            I18n.t("invoice.prepaid_credits")
-          when CREDIT_NOTE
-            I18n.t("invoice.credit_notes")
-          end
-        end
       end
     end
   end
