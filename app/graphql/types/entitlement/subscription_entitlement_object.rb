558592cce6316ecae3c960b3b6b4dc832fda7992
Julien Bourdeau <julien@julienbourdeau.com>
2025-08-14 17:46:34 +0200

    sort

--- app/graphql/types/entitlement/subscription_entitlement_object.rb
+++ app/graphql/types/entitlement/subscription_entitlement_object.rb
@@ -7,20 +7,14 @@ module Types
 
       field :code, String, null: false
       field :description, String, null: true
-      field :feature, Types::Entitlement::FeatureObject, null: false
       field :name, String, null: false
       field :privileges, [SubscriptionEntitlementPrivilegeObject], null: false
 
-      def code
-        object.feature.code
-      end
+      # TODO: Remove to avoid N+1, all feature attributes are already part of the SubscriptionEntitlement
+      field :feature, Types::Entitlement::FeatureObject, null: false
 
-      def name
-        object.feature.name
-      end
-
-      def description
-        object.feature.description
+      def privileges
+        object.privileges.sort_by(&:created_at)
       end
     end
   end
