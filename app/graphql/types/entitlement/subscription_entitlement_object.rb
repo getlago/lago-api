ba4b5d15957b21e2b9fd08aa656e6c56bb2a982e
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-08-14 11:53:18 -0300

    misc(entitlement): expose feature on SubscriptionEntitlementObject

--- app/graphql/types/entitlement/subscription_entitlement_object.rb
+++ app/graphql/types/entitlement/subscription_entitlement_object.rb
@@ -7,8 +7,21 @@ module Types
 
       field :code, String, null: false
       field :description, String, null: true
+      field :feature, Types::Entitlement::FeatureObject, null: false
       field :name, String, null: false
       field :privileges, [SubscriptionEntitlementPrivilegeObject], null: false
+
+      def code
+        object.feature.code
+      end
+
+      def name
+        object.feature.name
+      end
+
+      def description
+        object.feature.description
+      end
     end
   end
 end
