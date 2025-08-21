b4f8e2c3dd96be5f6c18edad918e9ecb8f5db887
Mario Diniz <mariohenriquediniz@gmail.com>
2025-08-21 10:50:10 -0300

    feat(e-invoices): Mutations::Invoices::DownloadXml for graphql

---
+++ spec/graphql/mutations/invoices/download_xml_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Mutations::Invoices::DownloadXml, type: :graphql do
+  let(:required_permission) { "invoices:view" }
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:customer) { create(:customer, organization:) }
+  let(:invoice) { create(:invoice, customer:, organization:) }
+
+  let(:mutation) do
+    <<~GQL
+      mutation($input: DownloadXmlInvoiceInput!) {
+        downloadInvoiceXml(input: $input) {
+          id
+        }
+      }
+    GQL
+  end
+
+  it_behaves_like "requires current user"
+  it_behaves_like "requires current organization"
+  it_behaves_like "requires permission", "invoices:view"
+
+  it "generates the XML for the given invoice" do
+    freeze_time do
+      result = execute_graphql(
+        current_user: membership.user,
+        current_organization: organization,
+        permissions: required_permission,
+        query: mutation,
+        variables: {
+          input: {id: invoice.id}
+        }
+      )
+      result_data = result["data"]["downloadInvoiceXml"]
+
+      aggregate_failures do
+        expect(result_data["id"]).to be_present
+      end
+    end
+  end
+end
