275e26d61f4e2e161df23449e7172e7085f9e092
Julien Bourdeau <julien@julienbourdeau.com>
2025-08-21 09:37:11 +0200

    subscription entitlements resolver

---
+++ app/graphql/resolvers/entitlement/subscription_entitlements_resolver.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module Entitlement
+    class SubscriptionEntitlementsResolver < Resolvers::BaseResolver
+      include AuthenticableApiUser
+      include RequiredOrganization
+
+      REQUIRED_PERMISSION = "subscriptions:view"
+
+      description "Query entitlements of a subscriptions"
+
+      argument :subscription_id, ID, required: true
+
+      type Types::Entitlement::SubscriptionEntitlementObject.collection_type, null: false
+
+      def resolve(**args)
+        raise forbidden_error(code: "feature_unavailable") unless License.premium?
+
+        subscription = current_organization.subscriptions.find(args[:subscription_id])
+
+        ::Entitlement::SubscriptionEntitlement.for_subscription(subscription)
+      rescue ActiveRecord::RecordNotFound
+        not_found_error(resource: "subscription")
+      end
+    end
+  end
+end
