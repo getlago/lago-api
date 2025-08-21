fdc3e8b65f74966a9a1a5b2bcfee3ceb312227e6
Mario Diniz <mariohenriquediniz@gmail.com>
2025-08-21 11:29:18 -0300

    Ubl/e invoices (#4145)
    This PR adds the UBL for eInvoices

---
+++ app/services/e_invoices/ubl/create/builder.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module EInvoices
+  module Ubl
+    module Create
+      class Builder < CreateService
+        ROOT_NAMESPACES = {
+          "xmlns" => "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2",
+          "xmlns:cac" => "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",
+          "xmlns:cbc" => "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+        }.freeze
+
+        DATEFORMAT = "%Y-%m-%d"
+
+        def initialize(xml:, invoice: nil)
+          @xml = xml
+          @invoice = invoice
+        end
+
+        def call
+          xml.Invoice(ROOT_NAMESPACES) do
+            xml.comment "UBL Version and Customization"
+            xml["cbc"].UBLVersionID "2.1"
+            xml["cbc"].CustomizationID "urn:cen.eu:en16931:2017"
+
+            Header.call(xml:, invoice:)
+            SupplierParty.call(xml:, invoice:)
+            CustomerParty.call(xml:, invoice:)
+            Delivery.call(xml:, invoice:)
+            PaymentMeans.call(xml:, invoice:, type: STANDARD, amount: invoice.total_due_amount)
+            PaymentTerms.call(xml:, invoice:)
+
+            allowance_charges do |tax_rate, amount|
+              AllowanceCharge.call(xml:, invoice:, tax_rate:, amount:)
+            end
+
+            xml.comment "Tax Total Information"
+            xml["cac"].TaxTotal do
+              xml["cbc"].TaxAmount \
+                format_number(Money.new(invoice.applied_taxes.sum(:amount_cents))),
+                currencyID: invoice.currency
+
+              applied_taxes do |applied_tax|
+                TaxSubtotal.call(xml:, invoice:, applied_tax:)
+              end
+            end
+
+            MonetaryTotal.call(xml:, invoice:)
+
+            line_items do |fee, line_id|
+              LineItem.call(xml:, fee:, line_id:)
+            end
+          end
+        end
+
+        protected
+
+        attr_accessor :xml, :invoice
+      end
+    end
+  end
+end
