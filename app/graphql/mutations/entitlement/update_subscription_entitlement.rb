1d9c4ec2f71823931bdb349cef09ab75af4ced49
Julien Bourdeau <julien@julienbourdeau.com>
2025-08-21 15:37:25 +0200

    new better service to update feature

---
+++ app/graphql/mutations/entitlement/update_subscription_entitlement.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Entitlement
+    class UpdateSubscriptionEntitlement < BaseMutation
+      include AuthenticableApiUser
+      include RequiredOrganization
+
+      REQUIRED_PERMISSION = "subscriptions:update"
+
+      description "Updates a subscription entitlement"
+
+      argument :subscription_id, ID, required: true
+
+      argument :entitlement, Types::Entitlement::EntitlementInput, required: true
+
+      type Types::Entitlement::SubscriptionEntitlementObject
+
+      def resolve(subscription_id:, entitlement: nil)
+        subscription = current_organization.subscriptions.find_by(id: subscription_id)
+
+        result = ::Entitlement::SubscriptionEntitlementCreateOrUpdateService.call(
+          subscription:,
+          feature_code: entitlement[:feature_code],
+          privilege_params: entitlement[:privileges]&.map { [it.privilege_code, it.value] }.to_h
+        )
+
+        result.success? ? result.entitlement : result_error(result)
+      end
+    end
+  end
+end
