a2ebb50239906af5168158d1f44ea34a67b51af2
Oleksandr Chebotarov <oleksandr@getlago.com>
2025-08-22 10:57:29 +0300

    Refactor wallet transaction's void service

--- app/services/wallet_transactions/void_service.rb
+++ app/services/wallet_transactions/void_service.rb
@@ -2,13 +2,10 @@
 
 module WalletTransactions
   class VoidService < BaseService
-    def initialize(wallet:, wallet_credit:, from_source: :manual, metadata: {}, credit_note_id: nil, priority: 50)
+    def initialize(wallet:, wallet_credit:, **transaction_params)
       @wallet = wallet
       @wallet_credit = wallet_credit
-      @from_source = from_source
-      @metadata = metadata
-      @credit_note_id = credit_note_id
-      @priority = priority
+      @transaction_params = transaction_params
 
       super
     end
@@ -18,16 +15,13 @@ module WalletTransactions
 
       ActiveRecord::Base.transaction do
         wallet_transaction = CreateService.call!(
+          **transaction_params,
           wallet:,
           wallet_credit:,
           transaction_type: :outbound,
           status: :settled,
           settled_at: Time.current,
-          source: from_source,
-          transaction_status: :voided,
-          metadata:,
-          credit_note_id:,
-          priority:
+          transaction_status: :voided
         ).wallet_transaction
         Wallets::Balance::DecreaseService.new(wallet:, wallet_transaction:).call
         result.wallet_transaction = wallet_transaction
@@ -38,6 +32,6 @@ module WalletTransactions
 
     private
 
-    attr_reader :wallet, :wallet_credit, :from_source, :metadata, :credit_note_id, :priority
+    attr_reader :wallet, :wallet_credit, :transaction_params
   end
 end
