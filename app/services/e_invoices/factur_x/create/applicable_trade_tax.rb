229df93eada710640785c0417fabeef2c8a58c6c
Mario Diniz <mariohenriquediniz@gmail.com>
2025-08-25 13:19:22 -0300

    feat(e-invoices): When fee has more than one tax rate

--- app/services/e_invoices/factur_x/create/applicable_trade_tax.rb
+++ app/services/e_invoices/factur_x/create/applicable_trade_tax.rb
@@ -4,30 +4,33 @@ module EInvoices
   module FacturX
     module Create
       class ApplicableTradeTax < Builder
-        def initialize(xml:, invoice:, applied_tax:)
-          @applied_tax = applied_tax
+        def initialize(xml:, invoice:, tax_rate:, amount:, tax:)
+          @tax_rate = tax_rate
+          @amount = amount
+          @tax = tax
+
           super(xml:, invoice:)
         end
 
         def call
-          xml.comment "Tax Information #{percent(applied_tax.tax_rate)} #{VAT}"
+          xml.comment "Tax Information #{percent(tax_rate)} #{VAT}"
           xml["ram"].ApplicableTradeTax do
-            xml["ram"].CalculatedAmount format_number(applied_tax.amount)
+            xml["ram"].CalculatedAmount format_number(tax)
             xml["ram"].TypeCode VAT
-            xml["ram"].BasisAmount format_number(applied_tax.fees_amount)
-            applied_tax_category_code = tax_category_code(type: invoice.invoice_type, tax_rate: applied_tax.tax_rate)
-            xml["ram"].CategoryCode applied_tax_category_code
-            if applied_tax_category_code == O_CATEGORY
+            xml["ram"].BasisAmount format_number(amount)
+            tax_category_code = tax_category_code(type: invoice.invoice_type, tax_rate: tax_rate)
+            xml["ram"].CategoryCode tax_category_code
+            if tax_category_code == O_CATEGORY
               xml["ram"].ExemptionReasonCode O_VAT_EXEMPTION
             else
-              xml["ram"].RateApplicablePercent format_number(applied_tax.tax_rate)
+              xml["ram"].RateApplicablePercent format_number(tax_rate)
             end
           end
         end
 
         private
 
-        attr_accessor :applied_tax
+        attr_accessor :tax_rate, :amount, :tax
       end
     end
   end
