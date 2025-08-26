d0fd03665805d5fbbdf5a62b6dcd6af3efa1ed68
Anna Velentsevich <annvelents@gmail.com>
2025-08-26 12:08:46 +0200

    Feat(fixed_charges): add fixed_charges_units_override service (#4175)
    ## Context
    
    we're implementing a mechanism to override units of a fixed charge for a
    subscription without creating a plan override. It's done through
    `SubscriptionFixedChargeUnitsOverride`. This PR creates service that'll
    be used to create these records
    
    ## Description
    
    - Added `SubscriptionFixedChargeUnitsOverride`

---
+++ app/services/subscriptions/fixed_charge_unit_override_service.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Subscriptions
+  class FixedChargeUnitOverrideService < BaseService
+    Result = BaseResult[:fixed_charge_unit_override]
+
+    def initialize(subscription:, fixed_charge:, units:)
+      @subscription = subscription
+      @fixed_charge = fixed_charge
+      @units = units
+
+      super()
+    end
+
+    def call
+      return result.not_found_failure!(resource: "subscription") unless subscription
+
+      result.fixed_charge_unit_override = build_fixed_charge_unit_override
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    end
+
+    private
+
+    attr_reader :subscription, :fixed_charge, :units
+
+    def build_fixed_charge_unit_override
+      SubscriptionFixedChargeUnitsOverride.create!(
+        organization: subscription.organization,
+        billing_entity: subscription.billing_entity,
+        subscription:,
+        fixed_charge:,
+        units:
+      )
+    end
+  end
+end
