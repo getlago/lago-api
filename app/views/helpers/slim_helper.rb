5aed1a40230df58b05884612b2e6bf30f548a4a1
Murillo Parreira <murparreira@gmail.com>
2025-08-27 15:57:10 -0300

    fix(invoice-pdf): created a helper method to format the address based on the country

--- app/views/helpers/slim_helper.rb
+++ app/views/helpers/slim_helper.rb
@@ -11,4 +11,46 @@ class SlimHelper
       )
     end.render(context, **locals)
   end
+
+  def self.format_address(address_line1, address_line2, city, state, zipcode, country_code)
+    country = ISO3166::Country.new(country_code)&.common_name
+
+    case country_code&.upcase
+    when "DE", "FR", "IT"
+      [
+        address_line1,
+        address_line2,
+        [zipcode, city].compact.join(" "),
+        country
+      ].compact.reject(&:empty?)
+    when "US", "CA", "GB", "AU"
+      [
+        address_line1,
+        address_line2,
+        [city, [state, zipcode].compact.join(" ")].compact.join(", "),
+        country
+      ].compact.reject(&:empty?)
+    when "BR"
+      [
+        address_line1,
+        address_line2,
+        [city, state].compact.join(" - ") + (zipcode.present? ? ", #{zipcode}" : ""),
+        country
+      ].compact.reject(&:empty?)
+    when "ES"
+      [
+        address_line1,
+        address_line2,
+        [zipcode, [city, state].compact.join(", ")].compact.join(" "),
+        country
+      ].compact.reject(&:empty?)
+    else
+      [
+        address_line1,
+        address_line2,
+        [city, [state, zipcode].compact.join(" ")].compact.join(", "),
+        country
+      ].compact.reject(&:empty?)
+    end
+  end
 end
