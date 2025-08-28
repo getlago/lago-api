cce71bad70c5211a7440c5f9c286821296e05ac5
Anna Velentsevich <annvelents@gmail.com>
2025-08-28 11:38:34 +0200

    remove units_override creation

--- app/services/subscriptions/create_service.rb
+++ app/services/subscriptions/create_service.rb
@@ -116,7 +116,6 @@ module Subscriptions
       else
         new_subscription.mark_as_active!
       end
-      create_fixed_charge_units_override(new_subscription) if overrides_only_fixed_charge_units?
 
       if should_be_billed_today?(new_subscription)
         # NOTE: Since job is launched from inside a db transaction
@@ -174,7 +173,6 @@ module Subscriptions
         billing_time: current_subscription.billing_time,
         ending_at: params.key?(:ending_at) ? params[:ending_at] : current_subscription.ending_at
       )
-      create_fixed_charge_units_override(new_subscription) if overrides_only_fixed_charge_units?
 
       after_commit do
         SendWebhookJob.perform_later("subscription.updated", current_subscription)
@@ -226,26 +224,7 @@ module Subscriptions
     end
 
     def plan_for_override_params(plan)
-      if overrides_only_fixed_charge_units?
-        return plan
-      end
-
       Plans::OverrideService.call(plan:, params: params[:plan_overrides].to_h.with_indifferent_access).plan
-    end
-
-    def overrides_only_fixed_charge_units?
-      params[:plan_overrides] && params[:plan_overrides].keys == [:fixed_charges] &&
-        params[:plan_overrides][:fixed_charges].map(&:keys).flatten.uniq.sort == [:id, :units]
-    end
-
-    def create_fixed_charge_units_override(subscription)
-      params[:plan_overrides][:fixed_charges].each do |fixed_charge|
-        FixedChargeUnitOverrideService.call!(
-          subscription:,
-          fixed_charge: subscription.plan.fixed_charges.find(fixed_charge[:id]),
-          units: fixed_charge[:units]
-        )
-      end
     end
   end
 end
