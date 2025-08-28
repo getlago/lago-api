ca3af16587992b03930bd71b7488c84fde4202d7
Vincent Pochet <vincent@getlago.com>
2025-08-28 15:57:13 +0200

    fix(event): Kafka payload for batch events (#4220)
    ## Context
    
    Kafka payload for raw events is built in two places, one in the
    `Events::CreateService` and one in `Events::CreateBatchService` this
    issue is that the one from the batch service is lacking the `metadata`
    fields.
    
    This field is a hash used by the events processor to find if the event
    is already post processed in the API. If not and if the event matched an
    in advance charge, a message is produced in the
    `events_charged_in_advance` topic.
    
    As the metadata was missing the event processor was considering that the
    event was not post-processed by the API even if it was, leading to a
    double processing and a duplicated fee...
    
    ## Description
    
    This PR makes sure that both `Events::CreateService` and
    `Events::CreateBatchService` uses the same Kafka producer service to
    remove the duplicated payload handling.
    
    NOTE: The duplication should have been avoided by the unique index
    defined at Posgres level but another bug have been identified at this
    level and will be fixed in a second PR.

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -47,30 +47,7 @@ module Events
     attr_reader :organization, :params, :timestamp, :metadata
 
     def produce_kafka_event(event)
-      return if ENV["LAGO_KAFKA_BOOTSTRAP_SERVERS"].blank?
-      return if ENV["LAGO_KAFKA_RAW_EVENTS_TOPIC"].blank?
-
-      Karafka.producer.produce_async(
-        topic: ENV["LAGO_KAFKA_RAW_EVENTS_TOPIC"],
-        key: "#{organization.id}-#{event.external_subscription_id}",
-        payload: {
-          organization_id: organization.id,
-          external_customer_id: event.external_customer_id,
-          external_subscription_id: event.external_subscription_id,
-          transaction_id: event.transaction_id,
-          # NOTE: Removes trailing 'Z' to allow clickhouse parsing
-          timestamp: event.timestamp.to_f.to_s,
-          code: event.code,
-          # NOTE: Default value to 0.0 is required for clickhouse parsing
-          precise_total_amount_cents: event.precise_total_amount_cents.present? ? event.precise_total_amount_cents.to_s : "0.0",
-          properties: event.properties,
-          ingested_at: Time.zone.now.iso8601[...-1],
-          source: "http_ruby",
-          source_metadata: {
-            api_post_processed: !organization.clickhouse_events_store?
-          }
-        }.to_json
-      )
+      Events::KafkaProducerService.call!(event:, organization:)
     end
   end
 end
