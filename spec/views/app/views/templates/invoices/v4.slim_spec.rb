bbdabe38d87405ec47362df568526adae3f750f8
Yohan Robert <7434196+groyoh@users.noreply.github.com>
2025-09-05 17:11:16 +0200

    test(invoice): Introduce invoice rendering tests (#4259)
    ## Context
    
    I am currently working on a task that modifies the behavior of the invoice rendering for `credit` invoices. I noticed there were no spec for the rendering of those invoices. So I decided to add some tests and ended up extracting the logic into this PR.
    
    ## Description
    
    In order to test the invoice rendering, I relied on the same code used by the `Utils::PdfGenerator`: `Slim::Template.new(template).render(invoice)`. The test is also using `build_stubbed` factory with constant data to have fast and consistent test setup. 
    
    In order to make assertions on the view, I used the [`rspec-snapshots`](https://github.com/levinmr/rspec-snapshot) which serialize the HTML content into a snapshot on the first run and compares with the snapshot on the next runs. This allows us to easily diff the HTML changes and ensure there's no breaking change in the rendering.
    
    One issue I faced though was that:
    
    1. Slim rendering was not properly pretty printing tables:
    
       ```html
              <div class="mb-24">
                <h2 class="title-2 mb-4">
                  $10.50
                </h2>
                <div class="body-1">
                  Due Sep 04, 2025
                </div>
              </div>
              <div class="invoice-resume mb-24 overflow-auto">
                <table class="invoice-resume-table" width="100%"><tr><td class="body-2">Item</td><td class="body-2">Units</td><td class="body-2">Unit price</td><td class="body-2">Amount</td></tr><tr><td class="body-1">Prepaid credits - Premium Wallet</td><td class="body-2">10.5</td><td class="body-2">1.0</td><td class="body-2">$10.50</td></tr></table><table class="total-table" width="100%"><tr><td class="body-2"></td><td class="body-1">Total</td><td class="body-1">$10.50</td></tr></table>
              </div>
              
              <p class="body-3 mb-24">
                
              </p>
        ```
    
    2. Nokogiri was not pretty printing with indentations:
    
        ```html
        <table class="total-table" width="100%"><tr>
        <td class="body-2"></td>
        <td class="body-1">Total</td>
        <td class="body-1">$10.50</td>
        </tr></table>
              </div>
              
              <p class="body-3 mb-24">
                
              </p>
              <div class="powered-by">
        <span class="body-2">Powered by  </span><img alt="Lago Logo" src="lago-logo-invoice.png">
        </div>
        ```
        
    So I had to combine Slim with [`htmlbeautifier`](https://github.com/threedaymonk/htmlbeautifier/) gem to have a proper pretty print.

---
+++ spec/views/app/views/templates/invoices/v4.slim_spec.rb
@@ -0,0 +1,166 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# This spec relies on `rspec-snapshot` gem (https://github.com/levinmr/rspec-snapshot) in order to serialize and compare
+# the rendered invoice HTML.
+#
+# To update a snapshot, either delete it, or run the tests with `UPDATE_SNAPSHOTS=true` environment variable.
+
+RSpec.describe "invoices/v4/_credit.slim", type: :view do
+  subject(:rendered_template) do
+    # We have to use both `pretty: true` and `HtmlBeautifier.beautify` to ensure proper formatting which eases the
+    # snapshot diff review.
+    html = Slim::Template.new(template, 1, pretty: true).render(invoice)
+    HtmlBeautifier.beautify(html, stop_on_errors: true)
+  end
+
+  let(:template) { Rails.root.join("app/views/templates/invoices/v4.slim") }
+  let(:invoice) do
+    build_stubbed(
+      :invoice,
+      :credit,
+      organization: organization,
+      billing_entity: billing_entity,
+      customer: customer,
+      number: "LAGO-202509-001",
+      payment_due_date: Date.parse("2025-09-04"),
+      issuing_date: Date.parse("2025-09-04"),
+      total_amount_cents: 1050,
+      currency: "USD",
+      fees: [fee]
+    )
+  end
+  # Static organization data for consistent rendering
+  let(:organization) do
+    build_stubbed(
+      :organization,
+      name: "ACME Corporation",
+      default_currency: "USD",
+      country: "US"
+    )
+  end
+
+  # Static billing entity data for consistent rendering
+  let(:billing_entity) do
+    build_stubbed(
+      :billing_entity,
+      organization: organization,
+      name: "ACME Corporation",
+      email: "billing@acme.com",
+      address_line1: "123 Business St",
+      address_line2: "Suite 100",
+      city: "San Francisco",
+      state: "CA",
+      zipcode: "94105",
+      country: "US"
+    )
+  end
+  # Static customer data
+  let(:customer) do
+    build_stubbed(
+      :customer,
+      organization: organization,
+      firstname: nil,
+      lastname: nil,
+      name: "John Doe",
+      legal_name: "John Doe",
+      legal_number: "1234567890",
+      external_id: "customer_123",
+      email: "john.doe@example.com",
+      address_line1: "456 Customer Ave",
+      address_line2: "Apt 202",
+      city: "New York",
+      state: "NY",
+      zipcode: "10001",
+      country: "US",
+      phone: "+1-555-123-4567"
+    )
+  end
+
+  # Static wallet data
+  let(:wallet) do
+    build_stubbed(
+      :wallet,
+      customer: customer,
+      name: wallet_name,
+      balance_currency: "USD",
+      rate_amount: BigDecimal("1.0")
+    )
+  end
+
+  # Static wallet transaction data
+  let(:wallet_transaction) do
+    build_stubbed(
+      :wallet_transaction,
+      wallet: wallet,
+      credit_amount: BigDecimal("10.50"),
+      amount: BigDecimal("10.50")
+    )
+  end
+
+  # Static fee data
+  let(:fee) do
+    build_stubbed(
+      :fee,
+      id: "87654321-0fed-cba9-8765-4321fedcba90",
+      fee_type: :credit,
+      invoiceable: wallet_transaction,
+      amount_cents: 1050,
+      amount_currency: "USD"
+    )
+  end
+
+  let(:wallet_name) { "Premium Wallet" }
+
+  before do
+    # Set locale to ensure consistent translations
+    I18n.locale = :en
+  end
+
+  def snapshot_name(metadata)
+    description =
+      if metadata[:description].empty?
+        # we have an "it { is_expected.to be something }" block
+        metadata[:scoped_id]
+      else
+        metadata[:description]
+      end
+    example_group =
+      if metadata.key?(:example_group)
+        metadata[:example_group]
+      else
+        metadata[:parent_example_group]
+      end
+
+    description = description.tr("/", "_").tr(" ", "_")
+    if example_group
+      [snapshot_name(example_group), description].join("/")
+    else
+      description
+    end
+  end
+
+  def expect_to_match_snapshot
+    snapshot_name = self.snapshot_name(RSpec.current_example.metadata)
+    expect(rendered_template).to match_snapshot("#{snapshot_name}.html")
+  end
+
+  context "when invoice_type is credit" do
+    context "when wallet has no name" do
+      let(:wallet_name) { nil }
+
+      it "renders correctly" do
+        expect_to_match_snapshot
+      end
+    end
+
+    context "when wallet has a name" do
+      let(:wallet_name) { "Premium Wallet" }
+
+      it "renders correctly" do
+        expect_to_match_snapshot
+      end
+    end
+  end
+end
