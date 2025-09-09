6d1f218103ee42d10881155be5999b78fcc42028
Ancor Cruz <hello@ancorcruz.com>
2025-09-09 14:24:26 +0100

    Ensure subscription fixed charge override works from UI
    graphQL

--- spec/graphql/types/subscriptions/fixed_charge_overrides_input_spec.rb
+++ spec/graphql/types/subscriptions/fixed_charge_overrides_input_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Types::Subscriptions::FixedChargeOverridesInput do
   subject { described_class }
 
   it { is_expected.to accept_argument(:id).of_type("ID") }
-  it { is_expected.to accept_argument(:add_on_id).of_type("ID!") }
+  it { is_expected.to accept_argument(:add_on_id).of_type("ID") }
   it { is_expected.to accept_argument(:apply_units_immediately).of_type("Boolean") }
   it { is_expected.to accept_argument(:invoice_display_name).of_type("String") }
   it { is_expected.to accept_argument(:properties).of_type("FixedChargePropertiesInput") }
