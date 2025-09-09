181f9dc1a74e91d785359b17a732f602e1248c94
Ancor Cruz <hello@ancorcruz.com>
2025-09-09 10:34:02 +0100

    Add specs for fixed charge override input
    also adds the apply units immediately field

--- app/graphql/types/subscriptions/fixed_charge_overrides_input.rb
+++ app/graphql/types/subscriptions/fixed_charge_overrides_input.rb
@@ -6,10 +6,11 @@ module Types
       argument :add_on_id, ID, required: true
       argument :id, ID, required: false
 
+      argument :apply_units_immediately, Boolean, required: false
       argument :invoice_display_name, String, required: false
       argument :properties, Types::FixedCharges::PropertiesInput, required: false
       argument :tax_codes, [String], required: false
-      argument :units, String, required: true
+      argument :units, String, required: false
     end
   end
 end
