4c6f2e583cf26571cf5e48f051847adf111668e2
Anna Velentsevich <annvelents@gmail.com>
2025-09-11 14:56:55 +0200

    give fixed_charges their own controller

---
+++ app/controllers/api/v1/subscriptions/base_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    module Subscriptions
+      class BaseController < Api::BaseController
+        before_action :find_subscription
+
+        private
+
+        attr_reader :subscription
+
+        def find_subscription
+          @subscription = current_organization.subscriptions
+            .order("terminated_at DESC NULLS FIRST, started_at DESC") # TODO: Confirm
+            .find_by!(
+              external_id: params[:subscription_external_id]
+            )
+        rescue ActiveRecord::RecordNotFound
+          not_found_error(resource: "subscription")
+        end
+      end
+    end
+  end
+end
