43c2377d7f22d906f3b6162d0a23caaee16ed498
Maxime Vidori <maxime.vidori@gmail.com>
2025-09-12 14:59:58 +0200

    chore(monitoring): Ignore health endpoint

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -6,4 +6,6 @@ Yabeda.configure do
   default_tag :service, ENV["OTEL_SERVICE_NAME"] || "lago-api"
   default_tag :environment, Rails.env
   default_tag :version, ENV["LAGO_VERSION"] || "unknown"
+
+  Yabeda::Rails.config.ignore_actions = ["ApplicationController#health"]
 end
