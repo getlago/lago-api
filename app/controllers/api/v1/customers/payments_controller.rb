52a95da3bc785eccd95aa7e6846c2340321bf8b8
Yohan Robert <yohan@getlago.com>
2025-09-12 10:53:07 +0200

    chore(payment): DRY payment index in controller and test

--- app/controllers/api/v1/customers/payments_controller.rb
+++ app/controllers/api/v1/customers/payments_controller.rb
@@ -4,28 +4,10 @@ module Api
   module V1
     module Customers
       class PaymentsController < BaseController
-        def index
-          result = PaymentsQuery.call(
-            organization: current_organization,
-            pagination: {
-              page: params[:page],
-              limit: params[:per_page] || PER_PAGE
-            },
-            filters: params.permit(:invoice_id).merge(external_customer_id: customer.external_id)
-          )
+        include PaymentIndex
 
-          if result.success?
-            render(
-              json: ::CollectionSerializer.new(
-                result.payments,
-                ::V1::PaymentSerializer,
-                collection_name: resource_name.pluralize,
-                meta: pagination_metadata(result.payments)
-              )
-            )
-          else
-            render_error_response(result)
-          end
+        def index
+          payment_index(customer_external_id: customer.external_id)
         end
 
         private
