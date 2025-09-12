a35f1617c0c37bee726fa4ddc8352448321bcde8
Anna Velentsevich <annvelents@gmail.com>
2025-09-12 12:06:42 +0200

    remove some fields for plan override fixed_charges

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -225,9 +225,7 @@ module Api
           ],
           fixed_charges: [
             :id,
-            :add_on_id,
             :invoice_display_name,
-            :charge_model,
             :units,
             :apply_units_immediately,
             properties: {},
