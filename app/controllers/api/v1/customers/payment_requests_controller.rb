af08bc70b2fb7ae398ab0ec0e4cc88db568e55e1
Yohan Robert <7434196+groyoh@users.noreply.github.com>
2025-09-12 13:47:12 +0200

    chore(payment-request): DRY payment request index in controller and test (#4305)
    ## Context
    
    https://github.com/getlago/lago-api/commit/b2befc225a914ef65009f96232aa38e80cebc58c introduce a `GET /api/v1/customer/:external_customer_id/payment_requests` endpoint. This commit was mainly a copy-paste from the `GET /api/v1/payment_requests` endpoints without the `external_customer_id` filter.
    
    The main issue here is that when updating one endpoint (adding a filter for instance), we might forgot to update the other one.
    
    ## Description
    
    This refactors the controller and test to reuse as much code as possible and keep both controllers in sync.

--- app/controllers/api/v1/customers/payment_requests_controller.rb
+++ app/controllers/api/v1/customers/payment_requests_controller.rb
@@ -4,29 +4,10 @@ module Api
   module V1
     module Customers
       class PaymentRequestsController < BaseController
-        def index
-          result = PaymentRequestsQuery.call(
-            organization: current_organization,
-            pagination: {
-              page: params[:page],
-              limit: params[:per_page] || PER_PAGE
-            },
-            filters: params.permit(:payment_status).merge(external_customer_id: customer.external_id)
-          )
+        include PaymentRequestIndex
 
-          if result.success?
-            render(
-              json: ::CollectionSerializer.new(
-                result.payment_requests.preload(:customer, :invoices),
-                ::V1::PaymentRequestSerializer,
-                collection_name: "payment_requests",
-                meta: pagination_metadata(result.payment_requests),
-                includes: %i[customer invoices]
-              )
-            )
-          else
-            render_error_response(result)
-          end
+        def index
+          payment_request_index(external_customer_id: customer.external_id)
         end
 
         private
