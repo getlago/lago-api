d303cace679dbced75f58ff5b20913a2d05676e7
Yohan Robert <7434196+groyoh@users.noreply.github.com>
2025-09-17 16:26:55 +0200

    feat(wallet-transaction): Add `transaction_name` to wallet transaction rules GraphQL types (#4288)
    ## Roadmap Task
    
    👉  https://getlago.canny.io/feature-requests/p/define-the-name-of-a-wallet-transaction-at-top-up
    
    ## Context
    
    Follow up of https://github.com/getlago/lago-api/commit/38261baa273514e1b79b8c9be5263f4ef21f0eed.
    
    ## Description
    
    This adds a `transaction_name` to the wallet transaction recurring rule GraphQL types that are then used to populate the name of the triggered wallet transactions.

--- app/graphql/types/wallets/recurring_transaction_rules/create_input.rb
+++ app/graphql/types/wallets/recurring_transaction_rules/create_input.rb
@@ -17,6 +17,7 @@ module Types
         argument :target_ongoing_balance, String, required: false
         argument :threshold_credits, String, required: false
         argument :transaction_metadata, [Types::Wallets::RecurringTransactionRules::TransactionMetadataInput], required: false
+        argument :transaction_name, String, required: false
         argument :trigger, Types::Wallets::RecurringTransactionRules::TriggerEnum, required: true
       end
     end
--- spec/factories/wallets.rb
+++ spec/factories/wallets.rb
@@ -15,5 +15,9 @@ FactoryBot.define do
     trait :terminated do
       status { "terminated" }
     end
+
+    trait :with_recurring_transaction_rules do
+      recurring_transaction_rules { [association(:recurring_transaction_rule)] }
+    end
   end
 end
