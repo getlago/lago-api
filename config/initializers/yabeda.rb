609f75bb9ca87a032aa2f50c83b8834474c9db29
Maxime Vidori <maxime.vidori@gmail.com>
2025-09-18 09:58:17 +0200

    feat(metrics): change latency bucket to Yabeda configuration
    This update introduces a new configuration for LONG_RUNNING_REQUEST_BUCKETS in the Yabeda initializer, defining specific time thresholds for monitoring long-running requests.

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -7,10 +7,11 @@ Prometheus::Client.config.data_store = Prometheus::Client::DataStores::DirectFil
   dir: "/tmp/prometheus/"
 )
 
+Yabeda::Rails.config.ignore_actions = ["ApplicationController#health"]
+Yabeda::Rails.config.buckets = [0.05, 0.1, 0.25, 0.5, 1, 5]
+
 Yabeda.configure do
   default_tag :service, ENV["OTEL_SERVICE_NAME"] || "lago-api"
   default_tag :environment, Rails.env
   default_tag :version, ENV["LAGO_VERSION"] || "unknown"
-
-  Yabeda::Rails.config.ignore_actions = ["ApplicationController#health"]
 end
