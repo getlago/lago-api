d49098adbad8f1de851636026cfdfecdcbfdb2f5
Vincent Pochet <vincent@getlago.com>
2025-09-18 13:57:09 +0200

    fix(progressive_billing): Avoid double parallel processing '03ba488831c50bfcb8959328de452778b048b56bc06da1ef6d9104ef9a598f7e'

--- spec/jobs/lifetime_usages/recalculate_and_check_job_spec.rb
+++ spec/jobs/lifetime_usages/recalculate_and_check_job_spec.rb
@@ -3,13 +3,38 @@
 require "rails_helper"
 
 RSpec.describe LifetimeUsages::RecalculateAndCheckJob do
-  let(:lifetime_usage) { create(:lifetime_usage) }
+  let(:organization) { create(:organization, :premium, premium_integrations:) }
+  let(:lifetime_usage) { create(:lifetime_usage, organization:) }
 
-  it "delegates to the RecalculateAndCheck service" do
+  let(:premium_integrations) { ["progressive_billing"] }
+
+  it "delegates to the Calculate service" do
     allow(LifetimeUsages::CalculateService).to receive(:call!)
-    allow(LifetimeUsages::CheckThresholdsService).to receive(:call)
+    allow(LifetimeUsages::CheckThresholdsService).to receive(:call!)
     described_class.perform_now(lifetime_usage)
-    expect(LifetimeUsages::CalculateService).to have_received(:call!).with(lifetime_usage:)
-    expect(LifetimeUsages::CheckThresholdsService).to have_received(:call).with(lifetime_usage:)
+    expect(LifetimeUsages::CalculateService).to have_received(:call!).with(lifetime_usage:, current_usage: nil)
+    expect(LifetimeUsages::CheckThresholdsService).not_to have_received(:call!)
+  end
+
+  context "when premium", :premium do
+    it "delegates to the RecalculateAndCheck service" do
+      allow(LifetimeUsages::CalculateService).to receive(:call!)
+      allow(LifetimeUsages::CheckThresholdsService).to receive(:call!)
+      described_class.perform_now(lifetime_usage)
+      expect(LifetimeUsages::CalculateService).to have_received(:call!).with(lifetime_usage:, current_usage: nil)
+      expect(LifetimeUsages::CheckThresholdsService).to have_received(:call!).with(lifetime_usage:)
+    end
+
+    context "when progressive billing is disabled" do
+      let(:premium_integrations) { [] }
+
+      it "delegates to the RecalculateAndCheck service" do
+        allow(LifetimeUsages::CalculateService).to receive(:call!)
+        allow(LifetimeUsages::CheckThresholdsService).to receive(:call!)
+        described_class.perform_now(lifetime_usage)
+        expect(LifetimeUsages::CalculateService).to have_received(:call!).with(lifetime_usage:, current_usage: nil)
+        expect(LifetimeUsages::CheckThresholdsService).not_to have_received(:call!)
+      end
+    end
   end
 end
