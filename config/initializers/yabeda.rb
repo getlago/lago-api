f5e17bc3b7aaf320f3be86a2015e922bd50b4d1c
Maxime Vidori <maxime.vidori@gmail.com>
2025-09-18 13:59:37 +0200

    fix(metrics): Support multi-process metrics (#4354)
    ## Context
    
    Metrics were not aggregated between processes, making calls to
    `/metrics` returning different values depending on the process
    responding to the request. This led to incorrect metrics report.
    
    ## Description
    
    Configure Prometheus data store in Yabeda initializer to handle
    multi-process:
    https://github.com/yabeda-rb/yabeda-prometheus?tab=readme-ov-file#multi-process-server-support

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -2,10 +2,16 @@
 
 require "yabeda"
 
+# https://github.com/yabeda-rb/yabeda-prometheus?tab=readme-ov-file#multi-process-server-support
+Prometheus::Client.config.data_store = Prometheus::Client::DataStores::DirectFileStore.new(
+  dir: "/tmp/prometheus/"
+)
+
+Yabeda::Rails.config.ignore_actions = ["ApplicationController#health"]
+Yabeda::Rails.config.buckets = [0.05, 0.1, 0.25, 0.5, 1, 5]
+
 Yabeda.configure do
   default_tag :service, ENV["OTEL_SERVICE_NAME"] || "lago-api"
   default_tag :environment, Rails.env
   default_tag :version, ENV["LAGO_VERSION"] || "unknown"
-
-  Yabeda::Rails.config.ignore_actions = ["ApplicationController#health"]
 end
