97cb890b68d92afb5eed047c8b6165d51474400f
Yohan Robert <7434196+groyoh@users.noreply.github.com>
2025-09-23 10:02:27 +0200

    test: Improve `match_html_snapshot` matcher (#4376)
    ## Context
    
    We rely on the `match_html_snapshot` to test the invoice rendering. One issue with it is that it includes a huge chunk of CSS and unnecessary `<head>` which makes the snapshot and diff bigger than necessary.
    
    ## Description
    
    The matcher will now strip the `<head>` and `<style>` tags by default and ensure each HTML tag is on a new line to ensure consistent formatting.

--- spec/support/matchers/snapshot_matcher.rb
+++ spec/support/matchers/snapshot_matcher.rb
@@ -23,11 +23,11 @@
 #   expect(rendered_template).to match_html_snapshot
 # end
 # ```
-RSpec::Matchers.define :match_html_snapshot do |name = nil|
+RSpec::Matchers.define :match_html_snapshot do |name = nil, strip_style: true, strip_head: true|
   match(notify_expectation_failures: true) do |html|
     name = [snapshot_name(RSpec.current_example.metadata), name].compact.join("/")
     name += ".html"
-    html = beautify(html)
+    html = beautify(html, strip_style: strip_style, strip_head: strip_head)
     expect(html).to match_snapshot(name)
   end
 
@@ -45,7 +45,20 @@ RSpec::Matchers.define :match_html_snapshot do |name = nil|
     end
   end
 
-  def beautify(html)
+  def beautify(html, strip_style: true, strip_head: true)
+    # Remove unnecessary styles and head tags
+    if strip_style
+      prev = nil
+      while html != prev
+        prev = html
+        html = html.gsub(%r{<style.*?>.*?</style>}m, "")
+      end
+    end
+    html = html.gsub(%r{<head>.*?</head>}m, "") if strip_head
+    # Make sure each HTML start tag is on a new line as the beautifier does not always do it
+    html = html.gsub(%r{><([^/])}, ">\n<\\1")
+    html = html.gsub(%r{(</.*?>)}, "\\1\n")
+    html = html.gsub(%r{/>}, "/>\n")
     # Ensure the HTML string is properly encoded as UTF-8, otherwise we won't be able to write the snapshot
     html = html.force_encoding("UTF-8") if html.encoding != Encoding::UTF_8
     HtmlBeautifier.beautify(html, stop_on_errors: true)
