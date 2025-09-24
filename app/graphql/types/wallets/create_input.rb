da74eace2169ddcda3672fa0c0fd4c4cdece1eae
brunomiguelpinto <darkymiguel@gmail.com>
2025-09-24 23:48:17 +0100

    Feat ongoing balance (#4252)
    ## Roadmap Task
    
    👉  https://getlago.canny.io/feature-requests/p/{{FEATURE_SLUG}}
    
    ## Context
    
    Include relevant motivation and context.
    
    ## Description
    
    Describe your changes in detail.
    
    List any dependencies that are required.

--- app/graphql/types/wallets/create_input.rb
+++ app/graphql/types/wallets/create_input.rb
@@ -11,8 +11,8 @@ module Types
       argument :granted_credits, String, required: true
       argument :invoice_requires_successful_payment, Boolean, required: false
       argument :name, String, required: false
+      argument :priority, Integer, required: false
       argument :paid_credits, String, required: true
-      argument :priority, Integer, required: true
       argument :rate_amount, String, required: true
 
       argument :ignore_paid_top_up_limits_on_creation, Boolean, required: false
