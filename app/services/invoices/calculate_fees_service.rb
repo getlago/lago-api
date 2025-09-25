ce36132b5e69e0b1a7fdb3cad31ffd44c351f967
Miguel Pinto <darkymiguel@gmail.com>
2025-09-25 22:26:14 +0100

    feat: add multiple wallet handle

--- app/services/invoices/calculate_fees_service.rb
+++ app/services/invoices/calculate_fees_service.rb
@@ -291,10 +291,10 @@ module Invoices
       true
     end
 
-    def wallet
-      return @wallet if @wallet
+    def wallets
+      return @wallets if @wallets
 
-      @wallet = customer.wallets.active.first
+      @wallets = customer.active_wallets_in_application_order
     end
 
     def should_create_credit_note_credit?
@@ -310,10 +310,9 @@ module Invoices
 
     def should_create_applied_prepaid_credit?
       return false if not_in_finalizing_process?
-      return false unless wallet&.active?
       return false unless invoice.total_amount_cents&.positive?
 
-      wallet.balance.positive?
+      wallets.any? { |w| w.balance.positive? }
     end
 
     def create_credit_note_credit
@@ -324,9 +323,7 @@ module Invoices
     end
 
     def create_applied_prepaid_credit
-      prepaid_credit_result = Credits::AppliedPrepaidCreditService.call(invoice:, wallet:)
-      prepaid_credit_result.raise_if_error!
-
+      prepaid_credit_result = Credits::AppliedPrepaidCreditsService.call!(invoice:, wallets:)
       refresh_amounts(credit_amount_cents: prepaid_credit_result.prepaid_credit_amount_cents)
     end
 
