5ff3ff6ab6f2a155b224473e731b2068bca667d6
Yohan Robert <yohan@getlago.com>
2025-09-30 14:55:43 +0200

    chore(integration): Remove unused `integration_collection_mappings` code

--- app/queries/integration_collection_mappings_query.rb
+++ app/queries/integration_collection_mappings_query.rb
@@ -2,14 +2,13 @@
 
 class IntegrationCollectionMappingsQuery < BaseQuery
   Result = BaseResult[:integration_collection_mappings]
-  Filters = BaseFilters[:integration_id, :mapping_type]
+  Filters = BaseFilters[:integration_id]
 
   def call
     integration_collection_mappings = paginate(base_scope)
     integration_collection_mappings = apply_consistent_ordering(integration_collection_mappings)
 
     integration_collection_mappings = with_integration_id(integration_collection_mappings) if filters.integration_id
-    integration_collection_mappings = with_mapping_type(integration_collection_mappings) if filters.mapping_type
 
     result.integration_collection_mappings = integration_collection_mappings
     result
@@ -24,9 +23,5 @@ class IntegrationCollectionMappingsQuery < BaseQuery
 
   def with_integration_id(scope)
     scope.where(integration_id: filters.integration_id)
-  end
-
-  def with_mapping_type(scope)
-    scope.where(mapping_type: filters.mapping_type)
   end
 end
