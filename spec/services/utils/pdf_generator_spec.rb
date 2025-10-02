48e9824465c3315ed0d948361377618ce8046228
Ancor Cruz <hello@ancorcruz.com>
2025-10-02 12:47:06 +0100

    fix test

--- spec/services/utils/pdf_generator_spec.rb
+++ spec/services/utils/pdf_generator_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe Utils::PdfGenerator do
   subject(:pdf_generator_service) { described_class.new(template: "invoices/v2", context: invoice) }
 
   let(:invoice) { create(:invoice) }
+  let(:customer_snapshot) { create(:customer_snapshot, invoice:) }
   let(:pdf_response) do
     File.read(Rails.root.join("spec/fixtures/blank.pdf"))
   end
 
   before do
+    customer_snapshot
+
     stub_request(:post, "#{ENV["LAGO_PDF_URL"]}/forms/chromium/convert/html")
       .to_return(body: pdf_response, status: 200)
   end
