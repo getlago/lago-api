45e716e94a50f609c2785f60be098bc72d79303c
Ancor Cruz <hello@ancorcruz.com>
2025-10-06 10:22:28 +0100

    Create Fixed Charge events on subscription creation when it is activated

--- app/services/subscriptions/create_service.rb
+++ app/services/subscriptions/create_service.rb
@@ -102,7 +102,6 @@ module Subscriptions
       sub.active? && sub.subscription_at.today? && plan.pay_in_advance? && !sub.in_trial_period?
     end
 
-    # TODO: Create fixed charge events for the subscription
     def create_subscription
       new_subscription = Subscription.new(
         organization_id: customer.organization_id,
@@ -163,12 +162,10 @@ module Subscriptions
       new_subscription
     end
 
-    # TODO: Create fixed charge events for the subscription
     def upgrade_subscription
       PlanUpgradeService.call!(current_subscription:, plan:, params:).subscription
     end
 
-    # TODO: Create fixed charge events for the subscription
     def downgrade_subscription
       if current_subscription.starting_in_the_future?
         update_pending_subscription
@@ -232,7 +229,6 @@ module Subscriptions
       old_plan.amount_currency != new_plan.amount_currency
     end
 
-    # TODO: Test this.... with fixed charge events...
     def update_pending_subscription
       current_subscription.plan = plan
       current_subscription.name = name if name.present?
@@ -251,7 +247,6 @@ module Subscriptions
         .order(started_at: :desc)
     end
 
-    # TODO: Test this.... with fixed charge events...
     def override_plan(plan)
       Plans::OverrideService.call(plan:, params: params[:plan_overrides].to_h.with_indifferent_access).plan
     end
