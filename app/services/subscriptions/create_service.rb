6bf1d0ee4c1bbb144a21295948962fd26d9a56b4
Ancor Cruz <hello@ancorcruz.com>
2025-10-06 10:22:28 +0100

    Create Fixed Charge events on subscription creation when it is activated

--- app/services/subscriptions/create_service.rb
+++ app/services/subscriptions/create_service.rb
@@ -97,7 +97,6 @@ module Subscriptions
       sub.active? && sub.subscription_at.today? && plan.pay_in_advance? && !sub.in_trial_period?
     end
 
-    # TODO: Create fixed charge events for the subscription
     def create_subscription
       new_subscription = Subscription.new(
         organization_id: customer.organization_id,
@@ -118,7 +117,12 @@ module Subscriptions
         new_subscription.mark_as_active!
       end
 
-      EmitFixedChargeEventsService.call!(subscriptions: [new_subscription], timestamp: new_subscription.subscription_at)
+      if new_subscription.active?
+        EmitFixedChargeEventsService.call!(
+          subscriptions: [new_subscription],
+          timestamp: new_subscription.started_at
+        )
+      end
 
       if should_be_billed_today?(new_subscription)
         # NOTE: Since job is launched from inside a db transaction
@@ -148,17 +152,10 @@ module Subscriptions
       new_subscription
     end
 
-    # TODO: Create fixed charge events for the subscription
     def upgrade_subscription
-      PlanUpgradeService.call!(current_subscription:, plan:, params:).subscription.tap do |upgrade_subscription|
-        EmitFixedChargeEventsService.call!(
-          subscriptions: [upgrade_subscription],
-          timestamp: upgrade_subscription.subscription_at
-        )
-      end
+      PlanUpgradeService.call!(current_subscription:, plan:, params:).subscription
     end
 
-    # TODO: Create fixed charge events for the subscription
     def downgrade_subscription
       if current_subscription.starting_in_the_future?
         update_pending_subscription
@@ -181,8 +178,6 @@ module Subscriptions
         billing_time: current_subscription.billing_time,
         ending_at: params.key?(:ending_at) ? params[:ending_at] : current_subscription.ending_at
       )
-
-      EmitFixedChargeEventsService.call!(subscriptions: [next_subscription], timestamp: next_subscription.subscription_at)
 
       after_commit do
         SendWebhookJob.perform_later("subscription.updated", current_subscription)
@@ -215,7 +210,6 @@ module Subscriptions
       old_plan.amount_currency != new_plan.amount_currency
     end
 
-    # TODO: Test this.... with fixed charge events...
     def update_pending_subscription
       current_subscription.plan = plan
       current_subscription.name = name if name.present?
@@ -234,7 +228,6 @@ module Subscriptions
         .order(started_at: :desc)
     end
 
-    # TODO: Test this.... with fixed charge events...
     def override_plan(plan)
       Plans::OverrideService.call(plan:, params: params[:plan_overrides].to_h.with_indifferent_access).plan
     end
