getlago/lago-api

commit b3131f433bfd38831a0a32c5deb1376659028902

test(event): Use a `shared_example` for Postgres and Clickhouse event stores (#4452)
## Context

While working on fix an issue with the Clickhouse event store, I refactored the Clickhouse event store tests to make it easier to review and test. This created a discrepancy between the Postgres and Clickhouse test setup.

## Description

This extracts the updated tests into a `shared_example` and re-use it for Postgres.

Note that this change showcased two minor issues:

1. Since we're running tests in transactions and the events are created in the `events` connection, they were not available through `ActiveRecord::Base.connection.select_one`. This was replaced by `Event.connection.select_one`.
2. The Postgres store returns grouped result with a timestamp as a `Time` object and the Clickhouse store returns it as `String`.

app/services/events/stores/postgres_store.rb
@@ -95,7 +95,7 @@ module Events
       def unique_count
         query = Events::Stores::Postgres::UniqueCountQuery.new(store: self)
         sql = ActiveRecord::Base.sanitize_sql_for_conditions([query.query])
-        result = ActiveRecord::Base.connection.select_one(sql)
+        result = Event.connection.select_one(sql)
 
         result["aggregation"]
       end

commit 5240dd187817aa51ab53e597dfb868d3218ea8e2

test(event): Revert test removal (#4459)
## Context

Due to a rebase, https://github.com/getlago/lago-api/commit/b3131f433bfd38831a0a32c5deb1376659028902 inadvertently removed a test added in https://github.com/getlago/lago-api/commit/95d9aff8d5749fee985d884f2db775e90e9f2569.

## Description

This adds the test back.

spec/services/events/stores/clickhouse_store_spec.rb
@@ -215,6 +215,19 @@ RSpec.describe Events::Stores::ClickhouseStore, clickhouse: {clean_before: true}
           organization_id: organization.id,
           external_subscription_id: subscription.external_id,
           code:,
+          timestamp: boundaries[:from_datetime] + 1.day,
+          properties: {
+            billable_metric.field_name => 3
+          },
+          value: "30",
+          decimal_value: 30
+        )
+
+        Clickhouse::EventsEnriched.create!(
+          transaction_id: SecureRandom.uuid,
+          organization_id: organization.id,
+          external_subscription_id: subscription.external_id,
+          code:,
           timestamp: (boundaries[:from_datetime] + 1.day).end_of_day,
           properties: {
             billable_metric.field_name => 2,
@@ -227,7 +240,10 @@ RSpec.describe Events::Stores::ClickhouseStore, clickhouse: {clean_before: true}
         event_store.aggregation_property = billable_metric.field_name
 
         result = event_store.prorated_unique_count_breakdown
-        expect(result.count).to eq(6)
+        expect(result.count).to eq(7)
+
+        # Ensure consistent ordering with 2 events with the same timestamp
+        expect(result.map { it["property"] }).to eq(%w[1 2 30 2 3 4 5])
 
         grouped_result = result.group_by { |r| r["property"] }
 