ee9c8ff55b93524f71dbccac8f6491cb805a7e9d
Anna Velentsevich <annvelents@gmail.com>
2025-10-09 19:33:27 +0200

    squashed commit

--- spec/factories/fixed_charges.rb
+++ spec/factories/fixed_charges.rb
@@ -9,6 +9,8 @@ FactoryBot.define do
     units { 1 }
     properties { {amount: Faker::Number.between(from: 100, to: 500).to_s} }
     invoice_display_name { Faker::Fantasy::Tolkien.location }
+    pay_in_advance { false }
+    prorated { false }
 
     trait :pay_in_advance do
       pay_in_advance { true }
