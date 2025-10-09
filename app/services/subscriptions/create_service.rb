39575c3e6eb0c3c5999bc91aa28b0e87980da197
Anna Velentsevich <annvelents@gmail.com>
2025-10-09 19:33:27 +0200

    squashed commit

--- app/services/subscriptions/create_service.rb
+++ app/services/subscriptions/create_service.rb
@@ -98,6 +98,7 @@ module Subscriptions
       plan.yearly_amount_cents < current_subscription.plan.yearly_amount_cents
     end
 
+    # we also need a test here...
     def should_be_billed_today?(sub)
       sub.active? && sub.subscription_at.today? &&
         sub.billed_on_activation? &&
@@ -142,6 +143,7 @@ module Subscriptions
         #       we must wait for it to be committed before processing the job.
         #       We do not set offset anymore but instead retry jobs
         after_commit do
+          # add test here for fixed charges
           BillSubscriptionJob.perform_later(
             [new_subscription],
             Time.zone.now.to_i,
