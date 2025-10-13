044539d134f64b6f86f13702551b000acb3aa2d0
LovroColic <lovro.colic.lc@gmail.com>
2025-10-13 12:31:30 +0200

    feat(multiple-payment-methods): add API support for listing customer payment methods (#4469)
    ## Context
    
    Currently in Lago, there can only be one payment provider customer with
    only one attached payment method.
    
    ## Description
    
    With this feature, it will be possible to add multiple payment methods
    per provider customer
    
    This PR adds API support for listing payment methods per customer

---
+++ spec/serializers/v1/payment_method_serializer_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ::V1::PaymentMethodSerializer do
+  subject(:serializer) do
+    described_class.new(
+      payment_method,
+      root_name: "payment_method"
+    )
+  end
+
+  let(:payment_method) { create(:payment_method) }
+
+  it "serializes the object" do
+    result = JSON.parse(serializer.to_json)
+
+    aggregate_failures do
+      expect(result["payment_method"]).to include(
+        "lago_id" => payment_method.id,
+        "is_default" => payment_method.is_default,
+        "payment_provider_code" => payment_method.payment_provider&.code,
+        "payment_provider_type" => "stripe",
+        "provider_method_id" => payment_method.provider_method_id,
+        "created_at" => payment_method.created_at.iso8601
+      )
+    end
+  end
+end
