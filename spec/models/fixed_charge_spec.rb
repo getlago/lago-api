23a842d9fb9481ce529f009d1b0449339890e468
Anna Velentsevich <annvelents@gmail.com>
2025-10-16 20:05:37 +0200

    squashed commit

--- spec/models/fixed_charge_spec.rb
+++ spec/models/fixed_charge_spec.rb
@@ -20,9 +20,41 @@ RSpec.describe FixedCharge do
 
   it { is_expected.to validate_numericality_of(:units).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of(:charge_model) }
-  it { is_expected.to validate_inclusion_of(:pay_in_advance).in_array([true, false]) }
-  it { is_expected.to validate_inclusion_of(:prorated).in_array([true, false]) }
+  it { is_expected.to validate_exclusion_of(:pay_in_advance).in_array([nil]) }
+  it { is_expected.to validate_exclusion_of(:prorated).in_array([nil]) }
   it { is_expected.to validate_presence_of(:properties) }
+
+  describe "scopes" do
+    let(:scoped) { create(:fixed_charge) }
+    let(:deleted) { create(:fixed_charge, :deleted) }
+    let(:pay_in_advance) { create(:fixed_charge, pay_in_advance: true) }
+    let(:pay_in_arrears) { create(:fixed_charge, pay_in_advance: false) }
+
+    before do
+      scoped
+      deleted
+      pay_in_advance
+      pay_in_arrears
+    end
+
+    describe ".all" do
+      it "returns all not deleted fixed charges" do
+        expect(described_class.all).to match_array([scoped, pay_in_advance, pay_in_arrears])
+      end
+    end
+
+    describe ".pay_in_advance" do
+      it "returns only pay_in_advance fixed charges" do
+        expect(described_class.pay_in_advance).to match_array([pay_in_advance])
+      end
+    end
+
+    describe ".pay_in_arrears" do
+      it "returns only pay_in_arrears fixed charges" do
+        expect(described_class.pay_in_arrears).to match_array([pay_in_arrears, scoped])
+      end
+    end
+  end
 
   describe "#equal_properties?" do
     let(:fixed_charge1) { build(:fixed_charge, properties: {amount: 100}) }
@@ -48,6 +80,37 @@ RSpec.describe FixedCharge do
 
       it "returns true if both charge model and properties are the same" do
         expect(fixed_charge1.equal_properties?(fixed_charge2)).to be true
+      end
+    end
+  end
+
+  describe "#included_in_next_subscription?" do
+    let(:add_on) { build(:add_on) }
+    let(:fixed_charge) { build(:fixed_charge, add_on:) }
+    let(:subscription) { create(:subscription, plan: fixed_charge.plan) }
+    let(:next_subscription) { create(:subscription, :with_previous_subscription, previous_subscription: subscription) }
+
+    context "when the fixed charge is included in the next subscription" do
+      before { next_subscription.plan.fixed_charges = [fixed_charge] }
+
+      it "returns true" do
+        expect(fixed_charge.included_in_next_subscription?(subscription)).to be true
+      end
+    end
+
+    context "when the fixed charge is not included in the next subscription" do
+      before { next_subscription.plan.fixed_charges = [] }
+
+      it "returns false" do
+        expect(fixed_charge.included_in_next_subscription?(subscription)).to be false
+      end
+    end
+
+    context "when there is no next subscription" do
+      let(:next_subscription) { nil }
+
+      it "returns false" do
+        expect(fixed_charge.included_in_next_subscription?(subscription)).to be false
       end
     end
   end
