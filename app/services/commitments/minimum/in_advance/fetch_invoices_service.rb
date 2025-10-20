56d629ff921737edfba1de25c3b830cb49428b84
Anna Velentsevich <annvelents@gmail.com>
2025-10-20 09:54:55 +0200

    fix min commitment, add tests

--- app/services/commitments/minimum/in_advance/fetch_invoices_service.rb
+++ app/services/commitments/minimum/in_advance/fetch_invoices_service.rb
@@ -37,13 +37,15 @@ module Commitments
             current_usage: true
           )
 
+          # note: current solution solves the problem for billed monthly charges and fixed charges. However, 
+          # we still have wrong calculation when we're issuing an invoice for the previous period, but it contains
+          # pay_in_advance fixed charges and charges (recurring, for example), because in invoice_subscription
+          # we'll have boundaries of previous billing period.
           invoice_ids_query = subscription
             .invoice_subscriptions
             .where(
-              "(charges_from_datetime >= ? AND charges_to_datetime <= ?)" \
-              "OR (fixed_charges_from_datetime >= ? AND fixed_charges_to_datetime <= ?)",
-              ds.previous_beginning_of_period,
-              ds.end_of_period,
+              "(GREATEST(charges_from_datetime, fixed_charges_from_datetime) >= ? " \
+              "AND GREATEST(charges_to_datetime, fixed_charges_to_datetime) <= ?)",
               ds.previous_beginning_of_period,
               ds.end_of_period
             ).select(:invoice_id)
