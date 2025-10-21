241f0b31c6854a6c08a368906f24d52145e787b1
Jeremy Denquin <jdenquin@gmail.com>
2025-10-21 12:20:49 +0200

    chore(karafka): Ensure karafka producer close

--- config/puma.rb
+++ config/puma.rb
@@ -45,6 +45,17 @@ pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 #
 workers ENV.fetch("WEB_CONCURRENCY", 0)
 
+# Ensure we flush and close Karafka producer when puma is shutting down
+if ENV.fetch("WEB_CONCURRENCY", 0).to_i > 1
+  before_worker_shutdown do
+    ::Karafka.producer.close
+  end
+else
+  @config.options[:events].after_stopped do
+    ::Karafka.producer.close
+  end
+end
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
