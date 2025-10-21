getlago/lago-api

commit 7aa074069298da6c094b26447cd3669995105e8d

chore(seeders): Rework all seeders (#4385)
## Context

We use seeders to seed dev environment.
We use `signup:seed_organization` to seed staging.

## Description

The hooli organization ID is now forced to
`11111111-2222-3333-4444-555555555555`.

The API key is hardcoded so re-seeding your env will not require you to
updated you script or Postman config.

The main goal is to:
- Re-order seeders
- Create expected stuff so other seeders can rely on it (like plans and
John Doe subscription)
- Use services when possible to create correct data (using factories
isn't always correct)

Other details:
- I have removed webhooks because we use services, it already creates
proper webhooks
- Credit notes were creating **very weird** data, now there is only one
for John Doe created with the service


Expected data:

key | value
-- | --
hooli org id | 11111111-2222-3333-4444-555555555555
hooli api key | lago_key-hooli-1234567890
customer external id | cust_john-doe
subscription external id | sub_john-doe-main


## Next steps

- Use `01_base.rb` instead of `signup:seed_organization` and maybe reuse
env variable to make base seeder customizable.
- Add Stripe payment provider seeder
- Update openapi to use these values in examples?


<img width="3682" height="2880" alt="CleanShot 2025-09-24 at 10 20
00@2x"
src="https://github.com/user-attachments/assets/149e70fc-f15f-407a-9bbe-d272003b2a70"
/>

<img width="3682" height="2880" alt="CleanShot 2025-09-24 at 10 19
29@2x"
src="https://github.com/user-attachments/assets/9ed98cfa-3099-43ef-bb3b-a300da886020"
/>

<img width="3682" height="2880" alt="CleanShot 2025-09-24 at 10 19
11@2x"
src="https://github.com/user-attachments/assets/aba92e76-e23a-4f5b-9968-cc1ed468cb04"
/>

db/seeds/21_events.rb
@@ -1,0 +1,50 @@
+# frozen_string_literal: true
+
+# NOTE: If hooli is not found, run 01_base.rb first
+@organization = Organization.find_by!(name: "Hooli")
+@customer = Customer.find_by!(external_id: "cust_john-doe")
+@sub = Subscription.find_by!(external_id: "sub_john-doe-main")
+sum_bm = BillableMetric.find_by!(organization: @organization, code: "sum_bm")
+count_bm = BillableMetric.find_by!(organization: @organization, code: "count_bm")
+
+def create_event(code:, time:)
+  Event.create!(
+    external_customer_id: @customer.external_id,
+    external_subscription_id: @sub.external_id,
+    organization_id: @organization.id,
+    transaction_id: "tr_#{SecureRandom.hex}",
+    timestamp: time - rand(0..12).seconds,
+    created_at: time,
+    code: code,
+    properties: {
+      custom_field: 10
+    },
+    metadata: {
+      user_agent: "Lago Python v0.1.5",
+      ip_address: Faker::Internet.ip_v4_address
+    }
+  )
+end
+
+# NOTE: Assigns valid events
+6.times do |offset|
+  5.times do
+    create_event(code: sum_bm.code, time: (offset.month - rand(1..20).days).ago)
+  end
+
+  2.times do
+    create_event(code: count_bm.code, time: (offset.month - rand(1..20).days).ago)
+  end
+end
+
+# NOTE: Assigns events missing custom property
+5.times do
+  event = create_event(code: sum_bm.code, time: rand(1..20).days.ago)
+  event.properties = {}
+  event.save!
+end
+
+# NOTE: Assigns events with invalid code
+5.times do
+  create_event(code: "foo", time: rand(1..20).days.ago)
+end

db/seeds/01_base.rb
@@ -7,15 +7,29 @@ require "factory_bot_rails"
 user = User.create_with(password: "ILoveLago")
   .find_or_create_by(email: "gavin@hooli.com")
 
-organization = Organization.find_or_create_by!(name: "Hooli")
-organization.premium_integrations = Organization::PREMIUM_INTEGRATIONS
-organization.save!
-billing_entity = BillingEntity.find_or_create_by!(organization: organization, name: "Hooli", code: "hooli")
-ApiKey.find_or_create_by!(organization:)
+organization = Organization.find_by(name: "Hooli")
+if organization.nil?
+  organization = Organization.create!(
+    id: "11111111-2222-3333-4444-555555555555",
+    name: "Hooli"
+  )
+end
+organization.update!({
+  premium_integrations: Organization::PREMIUM_INTEGRATIONS,
+  invoice_footer: "Hooli is a fictional company."
+})
+BillingEntity.find_or_create_by!(organization:, name: "Hooli", code: "hooli")
 Membership.find_or_create_by!(user:, organization:, role: :admin)
+WebhookEndpoint.find_or_create_by!(organization:, webhook_url: "http://webhook/#{organization.id}")
 
-# NOTE: define a billing model
-sum_metric = BillableMetric.find_or_create_by!(
+organization.api_keys.destroy_all
+organization.api_keys.create!(name: "Expired Key", expires_at: 1.day.ago, last_used_at: 36.hours.ago, permissions: {"customer" => ["read", "write"]})
+k = organization.api_keys.create!(name: "Hooli Key", permissions: ApiKey.default_permissions)
+k.update_columns(value: "lago_key-hooli-1234567890") # rubocop:disable Rails/SkipsModelValidations
+
+# == BillableMetrics
+
+sum_bm = BillableMetric.find_or_create_by!(
   organization:,
   aggregation_type: "sum_agg",
   name: "Sum BM",
@@ -23,7 +37,7 @@ sum_metric = BillableMetric.find_or_create_by!(
   field_name: "custom_field"
 )
 
-count_metric = BillableMetric.find_or_create_by!(
+count_bm = BillableMetric.find_or_create_by!(
   organization:,
   aggregation_type: "count_agg",
   name: "Count BM",
@@ -31,19 +45,9 @@ count_metric = BillableMetric.find_or_create_by!(
   field_name: "customer_field"
 )
 
-plan = Plan.create_with(
-  interval: "monthly",
-  pay_in_advance: false,
-  amount_cents: 100,
-  amount_currency: "EUR"
-).find_or_create_by!(
-  organization:,
-  name: "Standard Plan",
-  code: "standard_plan"
-)
+# == Taxes
 
-# Tax
-if !Tax.exists?(organization:, code: "lago_eu_fr_standard")
+unless Tax.exists?(organization:, code: "lago_eu_fr_standard")
   Taxes::CreateService.call!(
     organization:,
     params: {
@@ -55,246 +59,123 @@ if !Tax.exists?(organization:, code: "lago_eu_fr_standard")
   )
 end
 
-# Pay-in-advance plan
-if !Plan.exists?(organization:, code: "pay_in_advance_plan")
-  Plans::CreateService.call!(
-    organization_id: organization.id,
-    name: "Pay-in-advance Plan",
-    code: "pay_in_advance_plan",
-    interval: "monthly",
-    pay_in_advance: true,
-    amount_cents: 10000,
-    amount_currency: "EUR",
-    tax_codes: ["lago_eu_fr_standard"]
-  )
-end
+# == Addons
 
-# Add-on
-if !AddOn.exists?(organization:, code: "setup_fee")
+unless AddOn.exists?(organization:, code: "setup_fee")
   AddOns::CreateService.call!(
     organization_id: organization.id,
     name: "Setup Fee",
     code: "setup_fee",
     description: "Fee for setting up the subscription",
-    amount_cents: 10000,
+    amount_cents: 100_00,
     amount_currency: "EUR",
     tax_codes: ["lago_eu_fr_standard"]
   )
 end
 
-Charge.create_with(
-  organization:,
-  charge_model: "standard",
-  amount_currency: "EUR",
-  properties: {
-    amount: Faker::Number.between(from: 100, to: 500).to_s
-  }
-).find_or_create_by!(
-  plan:,
-  billable_metric: sum_metric
-)
-
-Charge.create_with(
-  organization:,
-  charge_model: "standard",
-  amount_currency: "EUR",
-  properties: {
-    amount: Faker::Number.between(from: 100, to: 500).to_s
-  }
-).find_or_create_by!(
-  plan:,
-  billable_metric: count_metric
-)
-
-# NOTE: define customers
-5.times do |i|
-  customer = Customer.create_with(
-    name: Faker::TvShows::SiliconValley.character,
-    country: Faker::Address.country_code,
-    address_line1: Faker::Address.street_address,
-    address_line2: Faker::Address.secondary_address,
-    state: Faker::Address.state,
-    zipcode: Faker::Address.zip_code,
-    email: Faker::Internet.email,
-    city: Faker::Address.city,
-    url: Faker::Internet.url,
-    phone: Faker::PhoneNumber.phone_number,
-    logo_url: Faker::Internet.url,
-    legal_name: Faker::Company.name,
-    legal_number: Faker::Company.duns_number,
-    currency: "EUR"
-  ).find_or_create_by!(
-    organization:,
-    billing_entity:,
-    external_id: "cust_#{i + 1}"
-  )
-
-  subscription_at = (Time.current - 6.months).beginning_of_month
-
-  sub = Subscription.create_with(
-    organization:,
-    started_at: subscription_at,
-    subscription_at:,
-    status: :active,
-    billing_time: :calendar
-  ).find_or_create_by!(
-    customer:,
-    external_id: "cust_#{SecureRandom.hex}",
-    plan:
-  )
-
-  next if Event.where(customer_id: customer.id).exists?
-
-  # NOTE: Assigns valid events
-  6.times do |offset|
-    5.times do
-      time = Time.current - offset.month - rand(1..20).days
-
-      Event.create!(
-        external_customer_id: customer.external_id,
-        external_subscription_id: sub.external_id,
-        organization_id: organization.id,
-        transaction_id: "tr-#{SecureRandom.hex}",
-        timestamp: time - rand(0..12).seconds,
-        created_at: time,
-        code: sum_metric.code,
-        properties: {
-          custom_field: 10
-        },
-        metadata: {
-          user_agent: "Lago Python v0.1.5",
-          ip_address: Faker::Internet.ip_v4_address
-        }
-      )
-    end
-
-    2.times do
-      time = Time.current - offset.month - rand(1..20).days
-
-      Event.create!(
-        external_customer_id: customer.external_id,
-        external_subscription_id: sub.external_id,
-        organization_id: organization.id,
-        transaction_id: "tr-#{SecureRandom.hex}",
-        timestamp: time - rand(0..12).seconds,
-        created_at: time,
-        code: count_metric.code,
-        metadata: {
-          user_agent: "Lago Python v0.1.5",
-          ip_address: Faker::Internet.ip_v4_address
-        }
-      )
-    end
-  end
-
-  # NOTE: Assigns events missing custom property
-  5.times do
-    time = Time.zone.now - rand(1..20).days
-
-    Event.create!(
-      external_customer_id: customer.external_id,
-      external_subscription_id: sub.external_id,
-      organization_id: organization.id,
-      transaction_id: "tr-#{SecureRandom.hex}",
-      timestamp: time - 120.seconds,
-      created_at: time,
-      code: sum_metric.code,
-      properties: {},
-      metadata: {
-        user_agent: "Lago Python v0.1.5",
-        ip_address: Faker::Internet.ip_v4_address
-      }
-    )
-  end
-
-  # NOTE: Assigns events with invalid code
-  5.times do
-    time = Time.zone.now - rand(1..20).days
-
-    Event.create!(
-      external_customer_id: customer.external_id,
-      external_subscription_id: sub.external_id,
-      organization_id: organization.id,
-      transaction_id: "tr-#{SecureRandom.hex}",
-      timestamp: time - 120.seconds,
-      created_at: time,
-      code: "foo",
-      properties: {},
-      metadata: {
-        user_agent: "Lago Python v0.1.5",
-        ip_address: Faker::Internet.ip_v4_address
-      }
-    )
-  end
-end
-
-first_customer = organization.customers.first
-
-wallet = Wallet.create!(
-  organization_id: organization.id,
-  customer: first_customer,
-  name: "My Active Wallet",
-  status: :active,
-  rate_amount: BigDecimal("1.00"),
-  balance: BigDecimal("100.00"),
-  ongoing_balance: BigDecimal("100.00"),
-  ongoing_usage_balance: BigDecimal("100.00"),
-  credits_balance: BigDecimal("100.00"),
-  credits_ongoing_balance: BigDecimal("100.00"),
-  credits_ongoing_usage_balance: BigDecimal("100.00"),
-  currency: "EUR"
-)
-
-Wallet.create!(
-  organization_id: organization.id,
-  customer: first_customer,
-  name: "My Terminated Wallet",
-  status: :terminated,
-  terminated_at: Time.zone.now - 3.days,
-  rate_amount: BigDecimal("1.00"),
-  balance: BigDecimal("86.00"),
-  ongoing_balance: BigDecimal("86.00"),
-  ongoing_usage_balance: BigDecimal("86.00"),
-  credits_balance: BigDecimal("86.00"),
-  credits_ongoing_balance: BigDecimal("86.00"),
-  credits_ongoing_usage_balance: BigDecimal("86.00"),
-  consumed_credits: BigDecimal("114.00"),
-  currency: "EUR"
-)
-
-3.times do
-  WalletTransaction.create!(
+unless AddOn.exists?(organization:, code: "setup_fee")
+  AddOns::CreateService.call!(
     organization_id: organization.id,
-    wallet:,
-    transaction_type: :outbound,
-    status: :settled,
-    amount: BigDecimal("10.00"),
-    credit_amount: BigDecimal("10.00"),
-    settled_at: Time.zone.now,
-    transaction_status: :invoiced
+    name: "Hour of Premium Support",
+    code: "support_hour",
+    description: "One hour of support from our experts",
+    amount_cents: 84_99,
+    amount_currency: "EUR",
+    tax_codes: ["lago_eu_fr_standard"]
   )
 end
 
-# NOTE: Regenerate events for the customers
-organization.customers.find_each do |customer|
-  5.times do
-    time = Time.zone.now
+# == Coupons
 
-    Event.create!(
-      external_customer_id: customer.external_id,
-      external_subscription_id: customer.active_subscriptions&.first&.external_id,
-      organization_id: organization.id,
-      transaction_id: "tr-#{SecureRandom.hex}",
-      timestamp: time - rand(0..24).hours,
-      created_at: time,
-      code: sum_metric.code,
-      properties: {
-        custom_field: 10
+unless Coupon.exists?(organization:, code: "20_percent_off")
+  Coupons::CreateService.call!(
+    organization_id: organization.id,
+    name: "20% off",
+    code: "20_percent_off",
+    coupon_type: "percentage",
+    percentage_rate: 20,
+    frequency: "forever",
+    expiration: "no_expiration"
+  )
+end
+
+unless Coupon.exists?(organization:, code: "10_euro_off")
+  Coupons::CreateService.call!(
+    organization_id: organization.id,
+    name: "10€ off",
+    code: "10_euro_off",
+    coupon_type: "fixed_amount",
+    amount_cents: 1000,
+    amount_currency: "EUR",
+    frequency: "forever",
+    expiration: "no_expiration"
+  )
+end
+
+# == Plans
+
+unless Plan.exists?(organization:, code: "standard_plan")
+  Plans::CreateService.call!(
+    organization_id: organization.id,
+    name: "Standard Plan",
+    code: "standard_plan",
+    interval: "monthly",
+    pay_in_advance: true,
+    amount_cents: 19_99,
+    amount_currency: "EUR",
+    tax_codes: ["lago_eu_fr_standard"],
+    charges: [
+      {
+        billable_metric_id: sum_bm.id,
+        charge_model: "standard",
+        amount_currency: "EUR",
+        pay_in_advance: false,
+        properties: {
+          amount: 100.to_s
+        }
       },
-      metadata: {
-        user_agent: "Lago Python v0.1.5",
-        ip_address: Faker::Internet.ip_v4_address
+      {
+        billable_metric_id: count_bm.id,
+        charge_model: "standard",
+        amount_currency: "EUR",
+        pay_in_advance: false,
+        properties: {
+          amount: 499.to_s
+        }
       }
-    )
-  end
+    ]
+  )
+end
+
+unless Plan.exists?(organization:, code: "premium_plan")
+  Plans::CreateService.call!(
+    organization_id: organization.id,
+    name: "Premium Plan",
+    code: "premium_plan",
+    interval: "monthly",
+    pay_in_advance: true,
+    amount_cents: 100_00,
+    amount_currency: "EUR",
+    tax_codes: ["lago_eu_fr_standard"],
+    charges: [
+      {
+        billable_metric_id: sum_bm.id,
+        charge_model: "standard",
+        amount_currency: "EUR",
+        pay_in_advance: false,
+        properties: {
+          amount: 30.to_s
+        }
+      },
+      {
+        billable_metric_id: count_bm.id,
+        charge_model: "standard",
+        amount_currency: "EUR",
+        pay_in_advance: false,
+        properties: {
+          amount: 399.to_s
+        }
+      }
+    ]
+  )
 end

db/seeds/02_john_doe.rb
@@ -1,0 +1,112 @@
+# frozen_string_literal: true
+
+# == John Doe
+
+# NOTE: If hooli is not found, run 01_base.rb first
+organization = Organization.find_by!(name: "Hooli")
+billing_entity = organization.default_billing_entity
+plan = Plan.find_by!(code: "premium_plan")
+
+customer_external_id = "cust_john-doe"
+sub_external_id = "sub_john-doe-main"
+started_at = 6.months.ago
+currency = "EUR"
+addon = AddOn.find_by!(code: "setup_fee")
+
+john_doe = Customer.create_with(
+  name: "John Doe",
+  country: "FR",
+  address_line1: Faker::Address.street_address,
+  address_line2: Faker::Address.secondary_address,
+  zipcode: Faker::Address.zip_code,
+  email: "john.doe@example.com",
+  city: Faker::Address.city,
+  url: Faker::Internet.url,
+  phone: Faker::PhoneNumber.phone_number,
+  logo_url: Faker::Internet.url,
+  legal_number: Faker::Company.duns_number,
+  currency:,
+  created_at: started_at
+).find_or_create_by!(
+  organization:,
+  billing_entity:,
+  external_id: customer_external_id
+)
+
+sub = john_doe.subscriptions.active.find_by(external_id: sub_external_id)
+unless sub
+  sub = Subscriptions::CreateService.call!(
+    customer: john_doe,
+    plan:,
+    params: {
+      name: "Main Subscription",
+      billing_time: :calendar,
+      subscription_at: started_at,
+      started_at: started_at,
+      external_id: sub_external_id
+    }
+  ).subscription
+  sub.update(created_at: started_at)
+end
+
+unless john_doe.wallets.active.exists?
+  params = {
+    customer: john_doe,
+    name: "Main wallet",
+    rate_amount: "3",
+    paid_top_up_min_amount_cents: 12_00
+  }
+
+  if License.premium?
+    params[:recurring_transaction_rules] = [
+      {
+        granted_credits: "10",
+        interval: :weekly,
+        method: :fixed,
+        expiration_at: 1.year.from_now,
+        trigger: :interval,
+        transaction_metadata: [{key: :origin, value: :seeder}],
+        transaction_name: "10 credits for free 🎁"
+      }
+    ]
+  end
+
+  Wallets::CreateService.call!(params:)
+
+  john_doe.wallets.create!(
+    organization:,
+    customer: john_doe,
+    name: "Terminated wallet",
+    rate_amount: "1",
+    status: :terminated,
+    terminated_at: 1.week.ago,
+    currency:
+  )
+end
+
+# == one-off invoice with credit note
+
+one_off = Invoices::CreateOneOffService.call!(
+  customer: john_doe,
+  currency:,
+  fees: [{
+    add_on_id: addon.id,
+    name: addon.name,
+    units: 2,
+    unit_amount_cents: addon.amount_cents,
+    tax_codes: ["lago_eu_fr_standard"]
+  }],
+  timestamp: started_at + 5.days,
+  skip_psp: true
+).invoice
+
+if License.premium?
+  CreditNotes::CreateService.call!(
+    invoice: one_off,
+    credit_amount_cents: 48_00,
+    description: "Generated by seeders",
+    items: [
+      fee_id: one_off.fees.sole.id, amount_cents: 40_00
+    ]
+  )
+end

db/seeds/04_coupons.rb
@@ -1,26 +1,0 @@
-# frozen_string_literal: true
-
-organization = Organization.find_by!(name: "Hooli")
-
-# Percentage coupon
-Coupons::CreateService.call!(
-  organization_id: organization.id,
-  name: "20% off",
-  code: "20_percent_off",
-  coupon_type: "percentage",
-  percentage_rate: 20,
-  frequency: "forever",
-  expiration: "no_expiration"
-)
-
-# Fixed amount coupon
-Coupons::CreateService.call!(
-  organization_id: organization.id,
-  name: "10€ off",
-  code: "10_euro_off",
-  coupon_type: "fixed_amount",
-  amount_cents: 1000,
-  amount_currency: "EUR",
-  frequency: "forever",
-  expiration: "no_expiration"
-)

db/seeds/05_entitlements.rb
@@ -1,51 +1,18 @@
 # frozen_string_literal: true
 
+return unless License.premium?
+
+# NOTE: If hooli is not found, run 01_base.rb first
+organization = Organization.find_by!(name: "Hooli")
+plan = Plan.find_by!(code: "premium_plan")
+sub = Subscription.find_by!(external_id: "sub_john-doe-main")
+
 def clean_up_feature!(feature)
   Entitlement::SubscriptionFeatureRemoval.where(feature: feature).with_discarded.delete_all
   Entitlement::SubscriptionFeatureRemoval.where(privilege: feature.privileges.with_discarded).with_discarded.delete_all
   Entitlement::EntitlementValue.where(privilege: feature.privileges.with_discarded).with_discarded.delete_all
   Entitlement::Entitlement.where(feature: feature).with_discarded.delete_all
   feature.privileges.with_discarded.delete_all
-end
-
-# Created in 01_base.rb
-organization ||= Organization.find_by!(name: "Hooli")
-plan = Plan.find_by!(code: "standard_plan")
-
-sub = Subscription.find_by(external_id: "sub_entitlement_80554965")
-if sub.nil?
-  customer = Customer.create!(
-    organization:,
-    billing_entity: organization.billing_entities.first,
-    external_id: "cust_#{SecureRandom.hex}",
-    name: Faker::TvShows::SiliconValley.character,
-    country: Faker::Address.country_code,
-    email: Faker::Internet.email,
-    currency: "EUR"
-  )
-  sub = Subscription.create!(
-    organization:,
-    external_id: "sub_entitlement_80554965",
-    started_at: Time.current,
-    subscription_at: Time.current,
-    status: :active,
-    billing_time: :calendar,
-    plan:,
-    customer:
-  )
-
-  unless Subscription.where(external_id: "sub_entitlement_NO_OVERRIDE", customer:).exists?
-    Subscription.create!(
-      organization:,
-      external_id: "sub_entitlement_NO_OVERRIDE",
-      started_at: Time.current,
-      subscription_at: Time.current,
-      status: :active,
-      billing_time: :calendar,
-      plan:,
-      customer:
-    )
-  end
 end
 
 # == seats - feature with privilege and subscription overrides

db/seeds/06_alerting.rb
@@ -1,46 +1,16 @@
 # frozen_string_literal: true
 
-organization ||= Organization.find_by(name: "Hooli")
+# NOTE: If hooli is not found, run 01_base.rb first
+organization = Organization.find_by!(name: "Hooli")
+sum_bm = BillableMetric.find_by!(organization:, code: "sum_bm")
+subscription = Subscription.find_by!(external_id: "sub_john-doe-main")
 
-# Create Plan with default alerts
-# The pay in advance charge helps us test alerting by sending events
-plan = Plan.create_with(
-  interval: "monthly", pay_in_advance: false, amount_cents: 49_00, amount_currency: "EUR"
-).find_or_create_by!(organization:, name: "Premium Plan", code: "premium_plan")
+existing_alerts = UsageMonitoring::Alert.where(organization:, subscription_external_id: subscription.external_id)
+UsageMonitoring::TriggeredAlert.where(alert: existing_alerts).delete_all
+UsageMonitoring::AlertThreshold.where(alert: existing_alerts).delete_all
+existing_alerts.delete_all
 
-ops_bm = BillableMetric.find_or_create_by!(
-  organization:,
-  aggregation_type: "sum_agg",
-  name: "Operations",
-  code: "ops",
-  field_name: "ops_count"
-)
-
-Charge.create_with(
-  organization:,
-  charge_model: "standard",
-  amount_currency: "EUR",
-  properties: {
-    amount: "500"
-  }
-).find_or_create_by!(plan:, billable_metric: ops_bm)
-
-def create_customer_with_sub(ext_id, plan:, organization:)
-  customer = FactoryBot.create(:customer, organization:, external_id: "cust_#{ext_id}")
-  customer.subscriptions.create!(
-    started_at: Time.current,
-    subscription_at: Time.current,
-    status: :active,
-    billing_time: :calendar,
-    plan:,
-    external_id: "sub_#{ext_id}",
-    organization:
-  )
-end
-
-subscription = create_customer_with_sub("alerting-#{SecureRandom.hex}", plan:, organization:)
-
-UsageMonitoring::CreateAlertService.call(organization:, subscription:, params: {
+UsageMonitoring::CreateAlertService.call!(organization:, subscription:, params: {
   alert_type: "current_usage_amount",
   code: "default",
   name: "Default Alert",
@@ -52,7 +22,7 @@ UsageMonitoring::CreateAlertService.call(organization:, subscription:, params: {
 })
 
 if License.premium?
-  alert = UsageMonitoring::CreateAlertService.call(organization:, subscription:, params: {
+  alert = UsageMonitoring::CreateAlertService.call!(organization:, subscription:, params: {
     alert_type: "lifetime_usage_amount",
     code: "total",
     thresholds: [
@@ -81,7 +51,7 @@ end
 
 bm_alert = UsageMonitoring::CreateAlertService.call(organization:, subscription:, params: {
   alert_type: "billable_metric_current_usage_amount",
-  billable_metric: ops_bm,
+  billable_metric: sum_bm,
   code: "ops",
   name: "Operations Alert",
   thresholds: [

db/seeds/20_subscriptions.rb
@@ -1,0 +1,41 @@
+# frozen_string_literal: true
+
+# NOTE: If hooli is not found, run 01_base.rb first
+organization = Organization.find_by!(name: "Hooli")
+billing_entity = organization.default_billing_entity
+plan = Plan.find_by!(code: "standard_plan")
+
+# Create and customers with subscriptions to add "noise" to the system
+5.times do |i|
+  customer = Customer.create_with(
+    name: Faker::TvShows::SiliconValley.character,
+    country: Faker::Address.country_code,
+    address_line1: Faker::Address.street_address,
+    address_line2: Faker::Address.secondary_address,
+    zipcode: Faker::Address.zip_code,
+    email: Faker::Internet.email,
+    city: Faker::Address.city,
+    legal_name: Faker::Company.name,
+    legal_number: Faker::Company.duns_number,
+    currency: "EUR"
+  ).find_or_create_by!(
+    organization:,
+    billing_entity:,
+    external_id: "cust_#{i + 1}"
+  )
+
+  subscription_at = 6.months.ago
+
+  Subscription.create_with(
+    organization:,
+    started_at: subscription_at,
+    subscription_at:,
+    status: :active,
+    billing_time: :calendar,
+    created_at: subscription_at
+  ).find_or_create_by!(
+    customer:,
+    external_id: "sub_#{i + 1}",
+    plan:
+  )
+end

db/seeds/50_invoices.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+
+# NOTE: Generate invoices for the customers
+Subscription.all.find_each do |subscription|
+  invoice_count = (Time.current - subscription.subscription_at).fdiv(1.month).round
+
+  (0..invoice_count).each do |offset|
+    Invoices::SubscriptionService.call(
+      subscriptions: [subscription],
+      timestamp: Time.current - offset.months + 1.day,
+      invoicing_reason: :subscription_periodic
+    )
+  end
+end

commit 18afdd547dd2c40f21ea0db81bccf54789d3c888

chore(seeds): Update seeds to avoid events in future (#4512)
## Context

The seeds currently generate events with timestamp `(offset.month - rand(1..20).days).ago` when `offset` goes from `0` to `5`. This means that for offset `0`, the timestamp will be in the future. I believe this is a bug introduced in https://github.com/getlago/lago-api/commit/7aa074069298da6c094b26447cd3669995105e8d as we previously defined the timestamp as `Time.current - offset.month - rand(1..20).days`.

## Description

This changes the events creation to ensure all events are in the past.

db/seeds/21_events.rb
@@ -29,11 +29,11 @@ end
 # NOTE: Assigns valid events
 6.times do |offset|
   5.times do
-    create_event(code: sum_bm.code, time: (offset.month - rand(1..20).days).ago)
+    create_event(code: sum_bm.code, time: (offset.month + rand(1..20).days).ago)
   end
 
   2.times do
-    create_event(code: count_bm.code, time: (offset.month - rand(1..20).days).ago)
+    create_event(code: count_bm.code, time: (offset.month + rand(1..20).days).ago)
   end
 end
 