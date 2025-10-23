f1f7f06442d1786cb03c0a6f5bc0a00cb0d4c312
Anna Velentsevich <annvelents@gmail.com>
2025-10-23 11:33:44 +0200

    completely remove fixed_charges from fetching invoices for minimum commitment to not break a test

--- app/services/commitments/minimum/in_advance/fetch_invoices_service.rb
+++ app/services/commitments/minimum/in_advance/fetch_invoices_service.rb
@@ -43,10 +43,7 @@ module Commitments
           invoice_ids_query = subscription
             .invoice_subscriptions
             .where(
-              "(charges_from_datetime >= ? AND charges_to_datetime <= ?)" \
-              "OR (fixed_charges_from_datetime >= ? AND fixed_charges_to_datetime <= ?)",
-              ds.previous_beginning_of_period,
-              ds.end_of_period,
+              "(charges_from_datetime >= ? AND charges_to_datetime <= ?)",
               ds.previous_beginning_of_period,
               ds.end_of_period
             ).select(:invoice_id)
