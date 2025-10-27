c26f02f9e523de1e0e77cc06d46d88315bb561ee
Murillo <murparreira@gmail.com>
2025-10-27 10:10:05 -0300

    refactor: extract functionallity to service

--- app/controllers/data_api/v1/charges_controller.rb
+++ app/controllers/data_api/v1/charges_controller.rb
@@ -13,74 +13,16 @@ module DataApi
           return
         end
 
-        charge_ids = charges_data.map { |cd| cd[:charge_id] }.compact.uniq
-        charge_filter_ids = charges_data.map { |cd| cd[:charge_filter_id] }.compact.uniq.reject(&:blank?)
+        result = Charges::BulkForecastedUsageAmountService.call(
+          charges_data: charges_data
+        )
 
-        charges_lookup = Charge.where(id: charge_ids).index_by(&:id)
-        charge_filters_lookup = charge_filter_ids.any? ?
-                               ChargeFilter.where(id: charge_filter_ids).index_by(&:id) : {}
-
-        results = []
-        failed_charges = []
-
-        charges_data.each do |charge_data|
-          charge = charges_lookup[charge_data[:charge_id]]
-          charge_filter = charge_data[:charge_filter_id].present? ?
-                         charge_filters_lookup[charge_data[:charge_filter_id]] : nil
-          record_id = charge_data[:record_id]
-
-          unless charge
-            raise ActiveRecord::RecordNotFound, "Charge not found: #{charge_data[:charge_id]}"
-          end
-
-          if charge_data[:charge_filter_id].present? && !charge_filter
-            raise ActiveRecord::RecordNotFound, "ChargeFilter not found: #{charge_data[:charge_filter_id]}"
-          end
-
-          percentile_results = {}
-
-          [:units_conservative, :units_realistic, :units_optimistic].each do |percentile_key|
-            units = charge_data[percentile_key]
-            next unless units
-
-            result = Charges::CalculatePriceService.call(
-              units: units,
-              charge: charge,
-              charge_filter: charge_filter
-            )
-
-            if result.success?
-              suffix = percentile_key.to_s.gsub("units_", "")
-              percentile_results["charge_amount_cents_#{suffix}"] = result.charge_amount_cents * 100
-              percentile_results["subscription_amount_cents_#{suffix}"] = result.subscription_amount_cents * 100
-              percentile_results["total_amount_cents_#{suffix}"] = result.total_amount_cents * 100
-            end
-          end
-
-          results << {
-            record_id: record_id,
-            charge_id: charge_data[:charge_id],
-            charge_filter_id: charge_data[:charge_filter_id],
-            **percentile_results
-          }
-        rescue => e
-          failed_charges << {
-            record_id: record_id,
-            charge_id: charge_data[:charge_id],
-            error: e.message
-          }
-        end
-
-        response_data = {
-          results: results,
-          failed_charges: failed_charges,
-          processed_count: results.length,
-          failed_count: failed_charges.length
+        render json: {
+          results: result.results,
+          failed_charges: result.failed_charges,
+          processed_count: result.processed_count,
+          failed_count: result.failed_count
         }
-
-        Rails.logger.info "[ChargesController] Response summary: #{response_data.except(:results)}"
-
-        render json: response_data
       end
 
       def resource_name
