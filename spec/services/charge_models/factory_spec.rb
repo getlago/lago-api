4c659c15b6f86df035764b51762a412a074b482b
Vincent Pochet <vincent@getlago.com>
2025-10-27 14:36:20 +0100

    feat(estimate): Prepare Clickhouse event estimate (#4542)
    ## Context
    
    Organizations configured to use the Clickhouse event store are not able
    to use the `POST /api/v1/events/estimate_fees` endpoint as the logic
    relies on the creation of a temporary record in the `events` table of
    Postgres.
    
    This PR is part of a refactor allowing the estimate with clickhouse
    event stores and speeding up te estimate computation with postgres store
    by removing the need for persisting a temporary record.
    
    ## Description
    
    Since the change requires a lot of file update, it will be split in
    multiple parts.
    This first part:
    - Adds a new `persisted` attribute to the `Events::Common` structure,
    with a default value to true. It will allow us to mark define an event
    as "temporary"
    - Define the `==` method on the `BaseResult` class, to allow comparison
    of result states and values in specs
    - Extract the charge model class initialization for in advance billing
    from `Charges::ApplyPayInAdvanceChargeModelService` to
    `ChargeModels::Factory`

--- spec/services/charge_models/factory_spec.rb
+++ spec/services/charge_models/factory_spec.rb
@@ -101,4 +101,56 @@ RSpec.describe ChargeModels::Factory do
       end
     end
   end
+
+  describe ".in_advance_charge_model_class" do
+    let(:result) { factory.in_advance_charge_model_class(chargeable: charge) }
+
+    context "when chargeable is a charge" do
+      context "with standard charge model" do
+        it { expect(result).to eq(ChargeModels::StandardService) }
+      end
+
+      context "with graduated charge model" do
+        let(:charge) { build(:graduated_charge) }
+
+        it { expect(result).to eq(ChargeModels::GraduatedService) }
+      end
+
+      context "with graduated_percentage charge model" do
+        let(:charge) { build(:graduated_percentage_charge) }
+
+        it { expect(result).to eq(ChargeModels::GraduatedPercentageService) }
+      end
+
+      context "with package charge model" do
+        let(:charge) { build(:package_charge) }
+
+        it { expect(result).to eq(ChargeModels::PackageService) }
+      end
+
+      context "with percentage charge model" do
+        let(:charge) { build(:percentage_charge) }
+
+        it { expect(result).to eq(ChargeModels::PercentageService) }
+      end
+
+      context "with volume charge model" do
+        let(:charge) { build(:volume_charge) }
+
+        it { expect { result }.to raise_error(NotImplementedError) }
+      end
+
+      context "with dynamic charge model" do
+        let(:charge) { build(:dynamic_charge) }
+
+        it { expect(result).to eq(ChargeModels::DynamicService) }
+      end
+
+      context "with custom charge model" do
+        let(:charge) { build(:custom_charge) }
+
+        it { expect(result).to eq(ChargeModels::CustomService) }
+      end
+    end
+  end
 end
