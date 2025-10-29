4145cbdd6afcbfa5d07fdf012d51cfd273c82f34
Mario Diniz <mariohenriquediniz@gmail.com>
2025-10-29 15:55:21 -0300

    feat(e-invoices/credit_notes): Create CreditNotes::GenerateDocumentsJob

---
+++ app/jobs/credit_notes/generate_documents_job.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module CreditNotes
+  class GenerateDocumentsJob < ApplicationJob
+    queue_as "invoices"
+
+    def perform(credit_note)
+      CreditNotes::GenerateXmlService.call(credit_note:, context: "api").raise_if_error!
+      CreditNotes::GenerateService.call(credit_note:, context: "api").raise_if_error!
+    end
+  end
+end
