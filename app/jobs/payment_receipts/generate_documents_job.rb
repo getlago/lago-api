0952b9f6e4f1e713c5bdec9769757804d5155948
Mario Diniz <mariohenriquediniz@gmail.com>
2025-10-31 16:55:16 -0300

    feat(e-invoices/payments): Generate XMLs

---
+++ app/jobs/payment_receipts/generate_documents_job.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module PaymentReceipts
+  class GenerateDocumentsJob < ApplicationJob
+    queue_as do
+      if ActiveModel::Type::Boolean.new.cast(ENV["SIDEKIQ_PDFS"])
+        :pdfs
+      else
+        :low_priority
+      end
+    end
+
+    retry_on LagoHttpClient::HttpError, Errno::ECONNREFUSED, EOFError, wait: :polynomially_longer, attempts: 6
+
+    def perform(payment_receipt:, notify: false)
+      PaymentReceipts::GenerateXmlService.call(payment_receipt:).raise_if_error!
+      PaymentReceipts::GeneratePdfService.call(payment_receipt:).raise_if_error!
+
+      PaymentReceipts::NotifyJob.perform_later(payment_receipt:) if notify
+    end
+  end
+end
