getlago/lago-api

commit 91cdb1742a18ff1c69683da2835150032966a723

fix(redis): increase timeouts and retry counts, and reconnect counts, add retry_jitter to avoid lock contention (#3413)
## Context

Lago's RedisClient is currently configured to `reconnect_attempts` just
2 times, and redlock's `retry_count` is set to 0.

if Lago tries to connect to Redis and the connection is not stable at
the time of the connection, this will result in an error such as:
```
{"level":"error","event":"enqueue","status":"error","job":"Invoices::Payments::CreateJob","queue":"default","exception":{"class":"Redlock::LockAcquisitionError","message":"failed to acquire lock on 'Too many Redis errors prevented lock acquisition:\nRedisClient::ConnectionError: Broken pipe (rediss://redis-host:6379)'"}}
{"level":"error","event":"perform","status":"error","job":"BillSubscriptionJob","duration":557.02,"job_id":"ce1323cb-64e3-4a08-a81e-420fd6e3d345","queue":"billing","exception":{"class":"Redlock::LockAcquisitionError","message":"failed to acquire lock on 'Too many Redis errors prevented lock acquisition:\nRedisClient::ConnectionError: Broken pipe (rediss://redis-host:6379)'"}}
```

these errors will then lead to weird behaviour in the Lago front end and
API, such as showing random errors like:
`This invoice contains a tax error`

## Description

This PR optimizes the defaults and helps Lago successfully handle these,
thus increasing reliability, specifically it:

- increases `reconnect_attempts` from 2 to 4, so more reconnect attempts
are tried in case of failure.
- increase `retry_count` from 0 to 3, so more redlock retries attempts
are tried in case of failure.
- add a `retry_delay` of 200ms, to wait a bit before trying to reconnect
instead of trying immediately.
- add a `retry_jitter` of 50ms, to help avoid lock contention and
increase reliability.

This should result in more reliable Redis connections, with really no
downsize.

config/initializers/active_job_uniqueness.rb
@@ -4,8 +4,11 @@ ActiveJob::Uniqueness.configure do |config|
   config.lock_ttl = 1.hour
 
   config.redlock_options = {
-    retry_count: 0,
-    redis_timeout: 5
+    retry_count: 3,
+    redis_timeout: 5,
+    retry_delay: 200,
+    # random delay to avoid lock contention
+    retry_jitter: 50
   }
 
   if ENV["REDIS_PASSWORD"].present? && !ENV["REDIS_PASSWORD"].empty?
@@ -16,6 +19,6 @@ ActiveJob::Uniqueness.configure do |config|
       host = [host, uri.query].join("?")
     end
 
-    config.redlock_servers = [RedisClient.new(url: "#{uri.scheme}://:#{ENV["REDIS_PASSWORD"]}@#{host}:#{uri.port}", reconnect_attempts: 2)]
+    config.redlock_servers = [RedisClient.new(url: "#{uri.scheme}://:#{ENV["REDIS_PASSWORD"]}@#{host}:#{uri.port}", reconnect_attempts: 4)]
   end
 end

commit 31ce2008fbf156846224d3e8076dd2551d2d2138

fix(job): Fix `activejob-uniqueness` retry on error (#4538)
## Context

https://github.com/getlago/lago-api/commit/91cdb1742a18ff1c69683da2835150032966a723 introduced an issue. It increased the `retry_count` to `3`. Redlock retry logic actually behaves the same whether there's an error (network, Redis error, etc.) or whether it fails to lock because the resource is already locked. So whenever we try to enqueue a job for which there's already a job locked (enqueued or running), we’ll try 3 times to enqueue the job causing a ~0.5s delay. 

## Description

This fixes it by introducing a patch to the `Redlock::Client`.

I'll try and push a fix upstream to https://github.com/leandromoreira/redlock-rb/ later on.

Gemfile
@@ -8,6 +8,7 @@ ruby "3.4.7"
 # Core
 gem "aasm"
 gem "activejob-uniqueness", require: "active_job/uniqueness/sidekiq_patch"
+gem "redlock", "~> 2.0.6" # Used through `activejob-uniqueness`. It's pinned to 2.0.x because we patched the library to fix a bug.
 gem "active_storage_validations"
 gem "bootsnap", require: false
 gem "clockwork", require: false
@@ -16,7 +17,7 @@ gem "puma", "~> 6.5"
 gem "rails", "~> 8.0"
 gem "redis"
 gem "sidekiq"
-group :'sidekiq-pro', optional: true do
+group :"sidekiq-pro", optional: true do
   source "https://gems.contribsys.com/" do
     gem "sidekiq-pro"
   end

config/application.rb
@@ -52,5 +52,6 @@ module LagoApi
 end
 
 require_relative "../lib/active_job/uniqueness/strategies/until_executed_patch"
+require_relative "../lib/redlock/client_patch"
 require_relative "../lib/active_job/logging"
 require_relative "../lib/active_job/json_log_subscriber"

config/initializers/active_job_uniqueness.rb
@@ -4,11 +4,8 @@ ActiveJob::Uniqueness.configure do |config|
   config.lock_ttl = 1.hour
 
   config.redlock_options = {
-    retry_count: 3,
-    redis_timeout: 5,
-    retry_delay: 200,
-    # random delay to avoid lock contention
-    retry_jitter: 50
+    retry_count: 0,
+    redis_timeout: 5
   }
 
   redis_config = {

lib/redlock/client_patch.rb
@@ -1,0 +1,40 @@
+# frozen_string_literal: true
+
+require "redlock"
+require "redlock/client"
+
+# Redlock retry logic actually behaves the same whether there's an error (network, Redis error, etc.) or whether it
+# fails to lock because the resource is already locked. So, if we enable redlock retry, whenever we try to enqueue a job
+# for which there's already a job locked (enqueued or running), we’ll try 3 times to enqueue the job causing a ~0.5s
+# delay.
+#
+# This patch is used to work around this issue by retrying the lock acquisition only if there is a network error.
+module Redlock
+  module ClientPatch
+    def lock(resource, ttl, options = {}, &block)
+      with_retry_on_error do
+        super(resource, ttl, options, &block)
+      end
+    end
+
+    private
+
+    def with_retry_on_error(&block)
+      tries = Redlock::Client::DEFAULT_RETRY_COUNT + 1
+      error = nil
+      tries.times do |attempt_number|
+        # Wait a random delay before retrying.
+        attempt_retry_delay = (Redlock::Client::DEFAULT_RETRY_DELAY + rand(Redlock::Client::DEFAULT_RETRY_JITTER)).to_f / 1000
+        sleep(attempt_retry_delay) if attempt_number > 0
+
+        return yield
+      rescue Redlock::LockAcquisitionError => error
+        if attempt_number == tries - 1
+          raise error
+        end
+      end
+    end
+  end
+end
+
+Redlock::Client.prepend(Redlock::ClientPatch)

spec/lib/redlock/client_patch_spec.rb
@@ -1,0 +1,91 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Redlock::ClientPatch do
+  subject(:lock_manager) { Redlock::Client.new([redis_client]) }
+
+  let(:redis_url) { ENV["REDIS_URL"] }
+  let(:redis_client) do
+    redis_wrapper_class.new(RedisClient.new(url: redis_url))
+  end
+  let(:redis_wrapper_class) do
+    Class.new(SimpleDelegator) do
+      attr_reader :lock_attempts
+
+      def initialize(*args, **kwargs)
+        super
+        @lock_attempts = []
+      end
+
+      def reset_lock_attempts
+        @lock_attempts = []
+      end
+
+      def call(*args, **kwargs)
+        if args.first == "EVALSHA" && args[1] == Redlock::Scripts::LOCK_SCRIPT_SHA
+          @lock_attempts << Time.zone.now
+        end
+        super
+      end
+    end
+  end
+
+  let(:resource) { "test_resource_#{SecureRandom.hex(8)}" }
+  let(:ttl) { 10000 }
+  let(:options) { {retry_count: 0} }
+
+  describe "#lock" do
+    it "locks the resource" do
+      lock_info = lock_manager.lock(resource, ttl, options)
+
+      expect(lock_info).to match({validity: a_kind_of(Integer), resource: resource, value: a_kind_of(String)})
+
+      lock_info2 = lock_manager.lock(resource, ttl, options)
+      expect(lock_info2).to be false
+
+      lock_manager.unlock(lock_info)
+
+      lock_info3 = lock_manager.lock(resource, ttl, options)
+      expect(lock_info3).to match({validity: a_kind_of(Integer), resource: resource, value: a_kind_of(String)})
+    end
+
+    context "when lock acquisition fails" do
+      before do
+        lock_manager.lock(resource, ttl, options)
+        redis_client.reset_lock_attempts
+      end
+
+      it "does not retry" do
+        expect(lock_manager.lock(resource, ttl, options)).to be false
+        expect(redis_client.lock_attempts.length).to eq 1
+      end
+
+      context "when lock retry options is defined" do
+        it "retries to acquire the lock" do
+          expect(lock_manager.lock(resource, ttl, {retry_count: 3})).to be false
+
+          expect(redis_client.lock_attempts.length).to eq 4
+          3.times do |i|
+            expect(redis_client.lock_attempts[i + 1]).to be >= (redis_client.lock_attempts[i] + 0.2.seconds)
+          end
+        end
+      end
+    end
+
+    context "when connection to redis is lost" do
+      let(:redis_url) { "redis://localhost:22222" }
+
+      it "retries to acquire the lock" do
+        expect do
+          lock_manager.lock(resource, ttl, options)
+        end.to raise_error(Redlock::LockAcquisitionError)
+
+        expect(redis_client.lock_attempts.length).to eq 4
+        3.times do |i|
+          expect(redis_client.lock_attempts[i + 1]).to be >= (redis_client.lock_attempts[i] + 0.2.seconds)
+        end
+      end
+    end
+  end
+end