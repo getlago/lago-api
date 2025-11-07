c82c0dc0a611604a37e63d8fb917951b97dfb113
Julia <j.e.egorova@gmail.com>
2025-11-07 17:58:34 +0300

    Update past migrations to pass the db check

--- db/migrate/20250507110137_fix_billing_entity_document_numbering_prefix.rb
+++ db/migrate/20250507110137_fix_billing_entity_document_numbering_prefix.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class FixBillingEntityDocumentNumberingPrefix < ActiveRecord::Migration[8.0]
-  class BillingEntity < ApplicationRecord; end
+  class BillingEntity < ApplicationRecord
+    attribute :subscription_invoice_issuing_date_anchor, :string, default: "next_period_start"
+    attribute :subscription_invoice_issuing_date_adjustment, :string, default: "keep_anchor"
+  end
 
   def up
     # rubocop:disable Rails/SkipsModelValidations
