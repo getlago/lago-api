785c1d664ac31b2a0a3479445d481f8d674b8451
Yohan R. <7434196+groyoh@users.noreply.github.com>
2025-11-10 11:22:14 +0100

    test(integration): Update integration payloads tests (#4458)
    ## Context
    
    Currently, our customers cannot sync invoices to multiple accounting
    entities (subsidiaries) as our mapping is made at the organization
    level. However, in NetSuite, for instance, the tax and item mapping are
    made at the entity level. We need to allow our mapping to be defined at
    the entity level.
    
    ## Description
    
    This heavily tests the changes introduced in #4450 by relying on a
    `shared_example`.

--- spec/services/integrations/aggregator/taxes/invoices/payloads/anrok_spec.rb
+++ spec/services/integrations/aggregator/taxes/invoices/payloads/anrok_spec.rb
@@ -3,108 +3,60 @@
 require "rails_helper"
 
 RSpec.describe Integrations::Aggregator::Taxes::Invoices::Payloads::Anrok do
-  let(:integration) { create(:anrok_integration, organization:) }
-  let(:organization) { create(:organization) }
-  let(:integration_customer) { create(:anrok_customer, customer:, integration:) }
-  let(:customer) { create(:customer, organization:) }
-  let(:payload) { described_class.new(integration:, customer:, invoice:, integration_customer:, fees:) }
-  let(:add_on) { create(:add_on, organization:) }
-  let(:add_on_two) { create(:add_on, organization:) }
-  let(:current_time) { Time.current }
-  let(:fees) { invoice.fees }
-  let(:integration_collection_mapping1) do
-    create(
-      :netsuite_collection_mapping,
-      integration:,
-      mapping_type: :fallback_item,
-      settings: {external_id: "1", external_account_code: "11", external_name: ""}
-    )
-  end
-  let(:integration_mapping_add_on) do
-    create(
-      :netsuite_mapping,
-      integration:,
-      mappable_type: "AddOn",
-      mappable_id: add_on.id,
-      settings: {external_id: "m1", external_account_code: "m11", external_name: ""}
-    )
-  end
-  let(:invoice) do
-    create(
-      :invoice,
-      customer:,
-      organization:
-    )
-  end
-  let(:fee_add_on) do
-    create(
-      :fee,
-      invoice:,
-      add_on:,
-      created_at: current_time - 3.seconds
-    )
-  end
-  let(:fee_add_on_two) do
-    create(
-      :fee,
-      invoice:,
-      add_on: add_on_two,
-      created_at: current_time - 2.seconds
-    )
-  end
-
-  before do
-    integration_customer
-    integration_collection_mapping1
-    integration_mapping_add_on
-    fee_add_on
-    fee_add_on_two
-  end
-
   describe "#body" do
-    subject(:call) { payload.body }
+    subject(:payload) { described_class.new(integration:, customer:, invoice:, integration_customer:, fees:).body }
 
-    let(:payload_body) do
-      [
-        {
-          "issuing_date" => invoice.issuing_date,
-          "currency" => invoice.currency,
-          "contact" => {
-            "external_id" => integration_customer&.external_customer_id || customer.external_id,
-            "name" => customer.name,
-            "address_line_1" => customer.shipping_address_line1 || customer.address_line1,
-            "city" => customer.shipping_city || customer.city,
-            "zip" => customer.shipping_zipcode || customer.zipcode,
-            "country" => customer.shipping_country || customer.country,
-            "taxable" => customer.tax_identification_number.present?,
-            "tax_number" => customer.tax_identification_number
-          },
-          "fees" => [
-            {
-              "item_key" => fee_add_on.item_key,
-              "item_id" => fee_add_on.id,
-              "item_code" => "m1",
-              "amount_cents" => 200
+    it_behaves_like "an integration payload", :avalara do
+      def build_expected_payload(mapping_codes)
+        [
+          {
+            "issuing_date" => invoice.issuing_date,
+            "currency" => invoice.currency,
+            "contact" => {
+              "external_id" => integration_customer&.external_customer_id || customer.external_id,
+              "name" => customer.name,
+              "address_line_1" => customer.shipping_address_line1 || customer.address_line1,
+              "city" => customer.shipping_city || customer.city,
+              "zip" => customer.shipping_zipcode || customer.zipcode,
+              "country" => customer.shipping_country || customer.country,
+              "taxable" => customer.tax_identification_number.present?,
+              "tax_number" => customer.tax_identification_number
             },
-            {
-              "item_key" => fee_add_on_two.item_key,
-              "item_id" => fee_add_on_two.id,
-              "item_code" => "1",
-              "amount_cents" => 200
-            }
-          ]
-        }
-      ]
-    end
+            "fees" => match_array([
+              {
+                "item_key" => add_on_fee.item_key,
+                "item_id" => add_on_fee.id,
+                "amount_cents" => 200,
+                "item_code" => mapping_codes.dig(:add_on, :external_id)
+              },
+              {
+                "item_key" => billable_metric_fee.item_key,
+                "item_id" => billable_metric_fee.id,
+                "amount_cents" => 300,
+                "item_code" => mapping_codes.dig(:billable_metric, :external_id)
+              },
+              {
+                "item_key" => minimum_commitment_fee.item_key,
+                "item_id" => minimum_commitment_fee.id,
+                "amount_cents" => 400,
+                "item_code" => mapping_codes.dig(:minimum_commitment, :external_id)
+              },
+              {
+                "item_key" => subscription_fee.item_key,
+                "item_id" => subscription_fee.id,
+                "amount_cents" => 500,
+                "item_code" => mapping_codes.dig(:subscription, :external_id)
+              }
+            ])
+          }
+        ]
+      end
 
-    it "returns the payload body" do
-      expect(call).to eq payload_body
-    end
-
-    context "when invoice.issuing_date is too far in the future" do
-      it "uses issuing date 30 days in the future at most" do
-        invoice.issuing_date = 61.days.from_now.to_date
-        expect(call.sole["issuing_date"]).to eq 30.days.from_now.to_date
+      context "when invoice.issuing_date is too far in the future" do
+        it "uses issuing date 30 days in the future at most" do
+          invoice.issuing_date = 61.days.from_now.to_date
+          expect(payload.sole["issuing_date"]).to eq 30.days.from_now.to_date
+        end
       end
     end
   end
