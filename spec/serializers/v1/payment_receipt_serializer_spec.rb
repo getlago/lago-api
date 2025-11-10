47ff0c1e8e375b3140561dec248ec5643fc5fe28
Mario Diniz <mariohenriquediniz@gmail.com>
2025-11-10 16:59:51 -0300

    feat(e-invoices/payments): Add file and xml url to PaymentReceiptSerializer

--- spec/serializers/v1/payment_receipt_serializer_spec.rb
+++ spec/serializers/v1/payment_receipt_serializer_spec.rb
@@ -15,7 +15,9 @@ RSpec.describe ::V1::PaymentReceiptSerializer do
     expect(result["payment_receipt"]).to include(
       "lago_id" => payment_receipt.id,
       "number" => payment_receipt.number,
-      "created_at" => payment_receipt.created_at.iso8601
+      "created_at" => payment_receipt.created_at.iso8601,
+      "file_url" => payment_receipt.file_url,
+      "xml_url" => payment_receipt.xml_url
     )
 
     expect(result["payment_receipt"]["payment"]).to include(
