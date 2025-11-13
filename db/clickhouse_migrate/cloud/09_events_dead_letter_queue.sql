12ba6b2c3dea3f1ed1de7e44caffaccfad37023c
Vincent Pochet <vincent@getlago.com>
2025-11-13 10:38:53 +0100

    fix(clickhouse): Fix cloud migration for events dead letter

--- db/clickhouse_migrate/cloud/09_events_dead_letter_queue.sql
+++ db/clickhouse_migrate/cloud/09_events_dead_letter_queue.sql
@@ -7,5 +7,5 @@ CREATE TABLE default.events_dead_letter_queue
     `error_message` String
 )
 ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
-ORDER BY (failed_at, event, initial_error_message, error_code, error_message)
+ORDER BY (failed_at, initial_error_message, error_code, error_message)
 SETTINGS index_granularity = 8192
