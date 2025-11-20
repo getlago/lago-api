ca1f3b2bcdba489b90f4e644f3e1c73ea4ef21d0
Ancor Cruz <hello@ancorcruz.com>
2025-11-20 16:15:45 +0000

    feat(fixed-charges): GraphQL: Invoice all fixed charges has fees flag (#4558)
    To allow users to add fees of fixed charges with zero units on draft
    invoices, graphQL Invoice object type should expose a flag to indicate
    if invoice has fixed charges without a fee.

--- spec/graphql/types/invoices/object_spec.rb
+++ spec/graphql/types/invoices/object_spec.rb
@@ -44,6 +44,7 @@ RSpec.describe Types::Invoices::Object do
     expect(subject).to have_field(:payment_overdue).of_type("Boolean!")
     expect(subject).to have_field(:ready_for_payment_processing).of_type("Boolean!")
     expect(subject).to have_field(:all_charges_have_fees).of_type("Boolean!")
+    expect(subject).to have_field(:all_fixed_charges_have_fees).of_type("Boolean!")
 
     expect(subject).to have_field(:created_at).of_type("ISO8601DateTime!")
     expect(subject).to have_field(:updated_at).of_type("ISO8601DateTime!")
