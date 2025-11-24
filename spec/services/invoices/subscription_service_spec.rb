c06ce9a2eb3a33b41c121ba613ba7b2ab78186ea
Julia <j.e.egorova@gmail.com>
2025-11-24 14:01:34 +0300

    Finalize invoice if keep_anchor is selected

--- spec/services/invoices/subscription_service_spec.rb
+++ spec/services/invoices/subscription_service_spec.rb
@@ -308,6 +308,18 @@ RSpec.describe Invoices::SubscriptionService do
 
         expect { invoice_service.call }.to change { wallet.reload.ready_to_be_refreshed }.from(false).to(true)
       end
+
+      context "with keep_anchor as issuing_date adjustment" do
+        before do
+          customer.update!(subscription_invoice_issuing_date_adjustment: "keep_anchor")
+        end
+
+        it "creates a finalized invoice" do
+          result = invoice_service.call
+          expect(result).to be_success
+          expect(result.invoice).to be_finalized
+        end
+      end
     end
 
     context "when invoice already exists" do
