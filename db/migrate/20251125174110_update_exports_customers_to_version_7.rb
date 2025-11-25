7cc96cd658ad76959ede396e4085365844865389
jdenquin <jdenquin@gmail.com>
2025-11-25 18:49:17 +0100

    linter

--- db/migrate/20251125174110_update_exports_customers_to_version_7.rb
+++ db/migrate/20251125174110_update_exports_customers_to_version_7.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UpdateExportsCustomersToVersion7 < ActiveRecord::Migration[8.0]
   def change
     update_view :exports_customers, version: 7, revert_to_version: 6
