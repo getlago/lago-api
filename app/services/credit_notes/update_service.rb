78223c34296603b454a51a7f2e35b681586ced78
Andrew Kozin <andrew.kozin@gmail.com>
2025-11-26 12:54:26 +0000

    Extract metadata logics to Metadata::UpdateItemService

--- app/services/credit_notes/update_service.rb
+++ app/services/credit_notes/update_service.rb
@@ -5,9 +5,8 @@ module CreditNotes
     def initialize(credit_note:, partial_metadata: false, **params)
       @params = params&.with_indifferent_access
       @credit_note = credit_note
-      @refund_status = params[:refund_status]
+      @refund_status = @params[:refund_status]
       @partial_metadata = partial_metadata
-      @metadata_value = params[:metadata]&.then { |m| m.respond_to?(:to_unsafe_h) ? m.to_unsafe_h : m.to_h }
 
       super
     end
@@ -21,61 +20,44 @@ module CreditNotes
           credit_note.refunded_at = Time.current if credit_note.succeeded?
         end
 
-        change_metadata!
-        credit_note.save!
-        delete_metadata!
+        update_metadata!
+        credit_note.save! if credit_note.changed?
       end
 
+      handle_changes
+
       result.credit_note = credit_note
-
-      Utils::SegmentTrack.refund_status_changed(credit_note.refund_status, credit_note.id, credit_note.organization.id)
-
       result
+    rescue ArgumentError
+      result.single_validation_failure!(field: :refund_status, error_code: "value_is_invalid")
     rescue ActiveRecord::RecordInvalid => e
       result.record_validation_failure!(record: e.record)
+    rescue BaseService::FailedResult => e
+      e.result
     end
 
     private
 
-    attr_reader :credit_note, :params, :refund_status, :metadata_value, :partial_metadata
+    attr_reader :credit_note, :params, :refund_status, :metadata_changed, :partial_metadata
 
     def update_refund_status?
       params.key?(:refund_status)
     end
 
-    def create_metadata?
-      credit_note.metadata.blank? && !metadata_value.nil? && (metadata_value.present? || !partial_metadata)
+    # @return [Boolean] if the metadata was changed in any way
+    def update_metadata!
+      value = params[:metadata]&.then { |m| m.respond_to?(:to_unsafe_h) ? m.to_unsafe_h : m.to_h }
+      service = Metadata::UpdateItemService.new(owner: credit_note, value:, partial: partial_metadata.present?)
+      result = service.call!
+      @metadata_changed = result.metadata_changed
     end
 
-    def replace_metadata?
-      credit_note.metadata.present? && !partial_metadata && !metadata_value.nil?
-    end
-
-    def merge_metadata?
-      credit_note.metadata.present? && partial_metadata && metadata_value.present?
-    end
-
-    def delete_metadata?
-      return @delete_metadata if defined?(@delete_metadata)
-      @delete_metadata = credit_note.metadata.present? && !partial_metadata && metadata_value.nil?
-    end
-
-    def change_metadata!
-      if create_metadata?
-        credit_note.create_metadata!(
-          owner: credit_note,
-          organization_id: credit_note.organization_id,
-          value: metadata_value
-        )
-      elsif replace_metadata?
-        credit_note.metadata.update!(value: metadata_value)
-      elsif merge_metadata?
-        credit_note.metadata.update!(value: credit_note.metadata.value.merge(metadata_value))
+    def handle_changes
+      if credit_note.previous_changes.key?(:refund_status)
+        Utils::SegmentTrack.refund_status_changed(credit_note.refund_status, credit_note.id, credit_note.organization.id)
+      elsif metadata_changed
+        # Potential place for future tracking of credit note metadata changes
       end
-    end
-
-    def delete_metadata!
-      credit_note.metadata.destroy! if delete_metadata?
     end
   end
 end
