921e36ce6271ffffebddce341509400966ec4432
Andrew Kozin <andrew.kozin@gmail.com>
2025-11-28 22:11:03 +0000

    Add metadata merge: PATCH /v1/credit_notes/:id/metadata

--- spec/requests/api/v1/credit_notes/metadata_controller_spec.rb
+++ spec/requests/api/v1/credit_notes/metadata_controller_spec.rb
@@ -44,10 +44,7 @@ RSpec.describe Api::V1::CreditNotes::MetadataController do
     end
 
     context "when credit note has existing metadata" do
-      before do
-        metadata = create(:item_metadata, owner: credit_note, organization:, value: {old: "value", foo: "old"})
-        credit_note.update!(metadata_id: metadata.id)
-      end
+      before { create(:item_metadata, owner: credit_note, organization:, value: {old: "value", foo: "old"}) }
 
       it "replaces all metadata" do
         subject
@@ -61,10 +58,7 @@ RSpec.describe Api::V1::CreditNotes::MetadataController do
     context "when params are empty" do
       let(:params) { {} }
 
-      before do
-        metadata = create(:item_metadata, owner: credit_note, organization:, value: {old: "value"})
-        credit_note.update!(metadata_id: metadata.id)
-      end
+      before { create(:item_metadata, owner: credit_note, organization:, value: {old: "value"}) }
 
       it "replaces metadata with empty hash" do
         subject
@@ -96,6 +90,84 @@ RSpec.describe Api::V1::CreditNotes::MetadataController do
         expect(response).to have_http_status(:success)
         expect(json[:metadata]).to eq({})
         expect(credit_note.reload.metadata.value).to eq({})
+      end
+    end
+  end
+
+  describe "PATCH /api/v1/credit_notes/:id/metadata" do
+    subject { patch_with_token(organization, "/api/v1/credit_notes/#{credit_note_id}/metadata", {metadata: params}) }
+
+    let(:credit_note_id) { credit_note.id }
+    let(:params) { {foo: "bar", baz: "qux"} }
+
+    it_behaves_like "requires API permission", "credit_note", "write"
+
+    context "when credit note is not found" do
+      let(:credit_note_id) { SecureRandom.uuid }
+
+      it "returns not found error" do
+        subject
+        expect(response).to be_not_found_error("credit_note")
+      end
+    end
+
+    context "when credit note has no metadata" do
+      it "creates metadata" do
+        subject
+
+        expect(response).to have_http_status(:success)
+        expect(json[:metadata]).to eq(foo: "bar", baz: "qux")
+        expect(credit_note.reload.metadata.value).to eq("foo" => "bar", "baz" => "qux")
+      end
+    end
+
+    context "when credit note has existing metadata" do
+      before { create(:item_metadata, owner: credit_note, organization:, value: {"old" => "value", "foo" => "old"}) }
+
+      it "merges metadata" do
+        subject
+
+        expect(response).to have_http_status(:success)
+        expect(json[:metadata]).to eq(old: "value", foo: "bar", baz: "qux")
+        expect(credit_note.reload.metadata.value).to eq("old" => "value", "foo" => "bar", "baz" => "qux")
+      end
+    end
+
+    context "when params are empty and metadata exists" do
+      let(:params) { {} }
+
+      before { create(:item_metadata, owner: credit_note, organization:, value: {"old" => "value"}) }
+
+      it "keeps existing metadata unchanged" do
+        subject
+
+        expect(response).to have_http_status(:success)
+        expect(json[:metadata]).to eq(old: "value")
+        expect(credit_note.reload.metadata.value).to eq("old" => "value")
+      end
+    end
+
+    context "when params are empty and metadata does not exist" do
+      let(:params) { {} }
+
+      it "does not create metadata" do
+        subject
+
+        expect(response).to have_http_status(:success)
+        expect(json[:metadata]).to be_nil
+        expect(credit_note.reload.metadata).to be_nil
+      end
+    end
+
+    context "when metadata param is not provided" do
+      subject { patch_with_token(organization, "/api/v1/credit_notes/#{credit_note_id}/metadata", {}) }
+
+      it "does not create metadata" do
+        subject
+
+        expect(response).to have_http_status(:success)
+        expect(json[:metadata]).to be_nil
+        expect(credit_note.reload.metadata).to be_nil
       end
     end
   end
