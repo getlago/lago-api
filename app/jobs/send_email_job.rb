8b25e96a14f5a1e4f03238b973fd8a085e58d430
Vincent Pochet <vincent@getlago.com>
2025-12-08 09:54:05 +0100

    fix(email): Retry email delivery job on Net::SMTPServerBusy error

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -6,4 +6,5 @@ class SendEmailJob < ActionMailer::MailDeliveryJob
   retry_on ActiveJob::DeserializationError, wait: :polynomially_longer, attempts: 6
   retry_on LagoHttpClient::HttpError, wait: :polynomially_longer, attempts: 6
   retry_on Net::ReadTimeout, wait: :polynomially_longer, attempts: 6
+  retry_on Net::SMTPServerBusy, wait: :polynomially_longer, attempts: 25
 end
