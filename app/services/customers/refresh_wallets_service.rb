be79dd203c921df1aabb1b4f7d0123900dd5d6e1
Miguel Pinto <darkymiguel@gmail.com>
2025-12-09 17:24:35 +0000

    feat: update specs

--- app/services/customers/refresh_wallets_service.rb
+++ app/services/customers/refresh_wallets_service.rb
@@ -36,7 +36,6 @@ module Customers
         )
       end
 
-      byebug
       result.usage_amount_cents = usage_amount_cents
       result.allocation_rules = allocation_rules
       result.wallets = customer.wallets.active.reload
