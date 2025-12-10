9261ee720ba2d287c0a29f9408b8e9779639ac6f
Miguel Pinto <darkymiguel@gmail.com>
2025-12-10 02:37:03 +0000

    feat: added specs

--- spec/scenarios/wallets/customer_wallets_balance_refresh_spec.rb
+++ spec/scenarios/wallets/customer_wallets_balance_refresh_spec.rb
@@ -135,7 +135,7 @@ describe "Use wallet's credits and recalculate balances", transaction: false do
       before do
         fee
         fee2
-        invoice.update!(invoice_type:, fees_amount_cents: 110, total_amount_cents: 110)
+        invoice.update!(invoice_type:, fees_amount_cents: 210, total_amount_cents: 210)
       end
 
       ##
@@ -151,16 +151,54 @@ describe "Use wallet's credits and recalculate balances", transaction: false do
         expect_wallet(wallet2, ongoing_usage: 400, credits_usage: 4, ongoing: 600, credits: 6)
       end
     end
+
+    context "when there is a draft invoice" do
+      let(:draft_invoice) do
+        create(
+          :invoice,
+          status: :draft,
+          issuing_date: DateTime.now,
+          customer:,
+          organization: customer.organization
+        )
+      end
+
+      let(:fee) do
+        create(
+          :charge_fee,
+          charge: second_charge,
+          subscription:,
+          precise_coupons_amount_cents: 0,
+          invoice: draft_invoice,
+          amount_cents: 100,
+          taxes_amount_cents: 10
+        )
+      end
+
+      before do
+        fee
+        draft_invoice.update!(fees_amount_cents: 110, total_amount_cents: 110)
+      end
+
+      ##
+      # wallet 1
+      # 1000 - 3000(from events) = -2000 #untouched
+      # wallet 2
+      # 1000 - 500(from event) - 110(from draft invoice) = 390
+      # = 400 # progressive billing invoice
+      ##
+      it "updates wallet ongoing balances including progressive billing invoice" do
+        expect_wallet(wallet, ongoing_usage: 3000, credits_usage: 30, ongoing: -2000, credits: -20)
+        expect_wallet(wallet2, ongoing_usage: 610, credits_usage: 6.1, ongoing: 390, credits: 3.9)
+      end
+    end
   end
 
   def expect_wallet(wallet, ongoing_usage:, credits_usage:, ongoing:, credits:)
     w = wallets.find(wallet.id)
-
-    aggregate_failures "wallet #{wallet.id}" do
-      expect(w.ongoing_usage_balance_cents).to eq ongoing_usage
-      expect(w.credits_ongoing_usage_balance).to eq credits_usage
-      expect(w.ongoing_balance_cents).to eq ongoing
-      expect(w.credits_ongoing_balance).to eq credits
-    end
+    expect(w.ongoing_usage_balance_cents).to eq ongoing_usage
+    expect(w.credits_ongoing_usage_balance).to eq credits_usage
+    expect(w.ongoing_balance_cents).to eq ongoing
+    expect(w.credits_ongoing_balance).to eq credits
   end
 end
