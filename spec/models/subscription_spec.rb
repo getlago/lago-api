ffbcccfc67d12cb93fb90ea56eaf808ef11c5eb7
Julien Bourdeau <julien@julienbourdeau.com>
2025-12-10 14:36:35 +0100

    models and DB

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -48,6 +48,7 @@ RSpec.describe Subscription do
       expect(subject).to have_many(:integration_resources)
       expect(subject).to have_many(:fees)
       expect(subject).to have_many(:daily_usages)
+      expect(subject).to have_many(:usage_thresholds)
       expect(subject).to have_many(:fixed_charges).through(:plan)
       expect(subject).to have_many(:fixed_charge_events)
       expect(subject).to have_many(:add_ons).through(:fixed_charges)
