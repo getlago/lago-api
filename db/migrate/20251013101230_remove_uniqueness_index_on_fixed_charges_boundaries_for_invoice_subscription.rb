d50678e8e72bcdb46ea3270e0476e14ab536f991
Anna Velentsevich <annvelents@gmail.com>
2025-12-10 11:32:49 +0100

    do not bill monthly fixed_charges at all

--- db/migrate/20251013101230_remove_uniqueness_index_on_fixed_charges_boundaries_for_invoice_subscription.rb
+++ db/migrate/20251013101230_remove_uniqueness_index_on_fixed_charges_boundaries_for_invoice_subscription.rb
@@ -5,16 +5,12 @@ class RemoveUniquenessIndexOnFixedChargesBoundariesForInvoiceSubscription < Acti
   def up
     safety_assured do
       remove_index :invoice_subscriptions, name: :index_uniq_invoice_subscriptions_on_fixed_charges_boundaries
+      add_index :invoice_subscriptions,
+        [:subscription_id, :fixed_charges_from_datetime, :fixed_charges_to_datetime],
+        name: :index_uniq_invoice_subscriptions_on_fixed_charges_boundaries,
+        where: "recurring IS TRUE AND regenerated_invoice_id IS NULL",
+        algorithm: :concurrently,
+        if_not_exists: true
     end
-  end
-
-  def down
-    add_index :invoice_subscriptions,
-      [:subscription_id, :fixed_charges_from_datetime, :fixed_charges_to_datetime],
-      unique: true,
-      name: :index_uniq_invoice_subscriptions_on_fixed_charges_boundaries,
-      where: "recurring IS TRUE AND regenerated_invoice_id IS NULL",
-      algorithm: :concurrently,
-      if_not_exists: true
   end
 end
