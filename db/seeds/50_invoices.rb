06d3755be32f0f29b00a1033a1b448861d8a4504
Julien Bourdeau <julien@julienbourdeau.com>
2025-12-11 09:48:32 +0100

    fix(seeders): ensure seeders can be run multiple times (#4703)
    If billable metrics exists, use it.
    If there are invoices in the system, no need to create more fake (it
    avoids error of "invoice already exists for this period).

--- db/seeds/50_invoices.rb
+++ db/seeds/50_invoices.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-# NOTE: Generate invoices for the customers
-Subscription.all.find_each do |subscription|
-  invoice_count = (Time.current - subscription.subscription_at).fdiv(1.month).round
+if Invoice.count.zero?
+  Subscription.all.find_each do |subscription|
+    invoice_count = (Time.current - subscription.subscription_at).fdiv(1.month).round
 
-  (1..invoice_count).each do |offset|
-    Invoices::SubscriptionService.call(
-      subscriptions: [subscription],
-      timestamp: subscription.subscription_at + offset.months,
-      invoicing_reason: :subscription_periodic
-    )
+    (1..invoice_count).each do |offset|
+      Invoices::SubscriptionService.call(
+        subscriptions: [subscription],
+        timestamp: subscription.subscription_at + offset.months,
+        invoicing_reason: :subscription_periodic
+      )
+    end
   end
 end
