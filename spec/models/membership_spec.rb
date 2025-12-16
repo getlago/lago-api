343527c8edf9c250d0818f72bff6afb1ea64e82c
Andrew Kozin <andrew.kozin@gmail.com>
2025-12-16 13:42:13 +0000

    [MIGRATION] Bind roles to memberships via member_roles

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -6,8 +6,22 @@ RSpec.describe Membership do
   subject(:membership) { create(:membership) }
 
   it { is_expected.to have_many(:data_exports) }
+  it { is_expected.to have_many(:membership_roles) }
+  it { is_expected.to have_many(:roles).through(:membership_roles) }
 
   it_behaves_like "paper_trail traceable"
+
+  describe "#admin?" do
+    it "returns true when membership role is admin" do
+      membership = create(:membership, role: :admin)
+      expect(membership.admin?).to be true
+    end
+
+    it "returns false when membership role is not admin" do
+      membership = create(:membership, role: :finance)
+      expect(membership.admin?).to be false
+    end
+  end
 
   describe "#mark_as_revoked" do
     it "revokes the membership with a Time" do
