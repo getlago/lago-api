178ec90ddd1e1511d75462f230ca8e89442ceea8
Romain Sempé <romain@getlago.com>
2025-12-16 11:35:23 +0100

    feat(ai-agent): MCP server connection (#4629)
    ## Context
    
    We want the AI agent to assist users in running common actions (e.g.,
    API queries) or accessing product knowledge without requiring
    engineering support.
    
    ## Description
    
    The goal of this PR is to setup the connection between Lago API and our
    MCP server.

--- app/graphql/resolvers/ai_conversations_resolver.rb
+++ app/graphql/resolvers/ai_conversations_resolver.rb
@@ -14,6 +14,9 @@ module Resolvers
     type Types::AiConversations::Object.collection_type, null: true
 
     def resolve(limit: nil)
+      raise unauthorized_error unless License.premium?
+      raise forbidden_error(code: "feature_unavailable") if ENV["MISTRAL_API_KEY"].blank? || ENV["MISTRAL_AGENT_ID"].blank?
+
       membership = current_organization.memberships.find_by(user_id: context[:current_user].id)
       current_organization.ai_conversations.where(membership:).order(created_at: :desc).first(limit)
     end
