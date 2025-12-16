4368492525affe0dc6ae156726f88ec39acee8f4
Murillo Parreira <murparreira@gmail.com>
2025-12-16 16:46:02 -0300

    feat(add-superset-integration): Add superset endpoints for authentication and dashboards (#4626)
    ## Context
    
    The frontend need some way to get authenticated with superset on the
    data side. This PR provides some graphql queries and a service that
    handles this scenarios.
    
    ## Description
    
    Added a graphql query and a service to hit some superset API routes.

--- lib/lago_http_client/lago_http_client.rb
+++ lib/lago_http_client/lago_http_client.rb
@@ -4,6 +4,7 @@ require "net/http"
 require "json"
 
 require "lago_http_client/client"
+require "lago_http_client/session_client"
 require "lago_http_client/http_error"
 
 module LagoHttpClient; end
