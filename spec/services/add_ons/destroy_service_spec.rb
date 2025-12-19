36bc6430cedc4526683d8d5dbe13a628234c8e05
Anna Velentsevich <annvelents@gmail.com>
2025-12-19 10:14:40 +0100

    destroy fixed charges when add on is deleted

--- spec/services/add_ons/destroy_service_spec.rb
+++ spec/services/add_ons/destroy_service_spec.rb
@@ -19,6 +19,32 @@ RSpec.describe AddOns::DestroyService do
       end
     end
 
+    context "when there are fixed charges associated with the add-on" do
+      let(:fixed_charges) { create_list(:fixed_charge, 2, add_on:) }
+
+      before { fixed_charges }
+
+      it "soft deletes the add-on and the fixed charges" do
+        expect { destroy_service.call }.to change(AddOn, :count).by(-1)
+          .and change { add_on.reload.deleted_at }.from(nil)
+          .and change(FixedCharge, :count).by(-2)
+          .and change { fixed_charges.map(&:reload).map(&:deleted_at) }.from([nil, nil]).to(all(be_present))
+      end
+
+      context "when failed to discard fixed charges" do
+        before do
+          allow_any_instance_of(FixedCharge).to receive(:discard!).and_raise(ActiveRecord::RecordInvalid.new(fixed_charges.first))
+        end
+
+        it "does not soft delete the add-on" do
+          result = destroy_service.call
+
+          expect(result).not_to be_success
+          expect(add_on.reload.deleted_at).to be_nil
+        end
+      end
+    end
+
     context "when add-on is not found" do
       let(:add_on) { nil }
 
