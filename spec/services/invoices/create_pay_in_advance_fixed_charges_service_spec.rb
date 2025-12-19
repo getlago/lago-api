fb9467cd4c39a79ce24e1ed96db43e916805b48e
Anna Velentsevich <annvelents@gmail.com>
2025-12-19 09:57:25 +0100

    fix tests

--- spec/services/invoices/create_pay_in_advance_fixed_charges_service_spec.rb
+++ spec/services/invoices/create_pay_in_advance_fixed_charges_service_spec.rb
@@ -398,7 +398,7 @@ RSpec.describe Invoices::CreatePayInAdvanceFixedChargesService do
         before { create(:wallet, customer:, balance_cents: 100) }
 
         it "propagates the error" do
-          allow_any_instance_of(Credits::AppliedPrepaidCreditService) # rubocop:disable RSpec/AnyInstance
+          allow_any_instance_of(Credits::AppliedPrepaidCreditsService) # rubocop:disable RSpec/AnyInstance
             .to receive(:call).and_raise(ActiveRecord::StaleObjectError)
 
           expect { invoice_service.call }.to raise_error(ActiveRecord::StaleObjectError)
