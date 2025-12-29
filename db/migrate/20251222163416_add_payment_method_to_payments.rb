fccafa6f29fc86490ebe851d15e277a5772cbd2f
Lovro Colic <lovro.colic.lc@gmail.com>
2025-12-29 12:56:59 +0100

    remove safety_assured block

--- db/migrate/20251222163416_add_payment_method_to_payments.rb
+++ db/migrate/20251222163416_add_payment_method_to_payments.rb
@@ -4,9 +4,7 @@ class AddPaymentMethodToPayments < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    safety_assured do
-      add_reference :payments, :payment_method, null: true, type: :uuid, index: {algorithm: :concurrently}
-    end
+    add_reference :payments, :payment_method, null: true, type: :uuid, index: {algorithm: :concurrently}
     add_foreign_key :payments, :payment_methods, validate: false
   end
 end
