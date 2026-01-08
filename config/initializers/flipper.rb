1e625367c50756c02b90e338eb8a286d31421157
Julien Bourdeau <julien@julienbourdeau.com>
2026-01-08 15:45:47 +0100

    redis config

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,5 +3,13 @@
 require "flipper/adapters/redis"
 
 Flipper.configure do |config|
-  config.adapter { Flipper::Adapters::Redis.new(Redis.new(db: 8)) }
+  config.adapter do
+    Flipper::Adapters::Redis.new(
+      Redis.new({
+        url: ENV["LAGO_REDIS_FEATURE_FLAGS_URL"] || ENV["REDIS_URL"],
+        password: ENV["LAGO_REDIS_FEATURE_FLAGS_PASSWORD"] || ENV["REDIS_PASSWORD"]
+      }),
+      key_prefix: "flipper:"
+    )
+  end
 end
