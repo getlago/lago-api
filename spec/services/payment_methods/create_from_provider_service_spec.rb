88cdb538219c65e0d7934feaaa3b69881d0745f6
Mario Diniz <mariohenriquediniz@gmail.com>
2026-01-12 16:47:59 -0300

    Multi payments new services (#4825)
    This PR adds the services related to PaymentMethod.

---
+++ spec/services/payment_methods/create_from_provider_service_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PaymentMethods::CreateFromProviderService do
+  subject(:create_service) { described_class.new(customer:, params:, provider_method_id:, payment_provider_id:, payment_provider_customer:) }
+
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+
+  let(:customer) { create(:customer, organization:) }
+  let(:params) {}
+  let(:provider_method_id) { "i_cant_be_nil" }
+  let(:payment_provider_id) { nil }
+  let(:payment_provider_customer) { nil }
+
+  describe "#call" do
+    context "without customer" do
+      let(:customer) { nil }
+
+      it "fails" do
+        result = create_service.call
+
+        expect(result).not_to be_success
+        expect(result.error).to be_a(BaseService::NotFoundFailure)
+        expect(result.error.message).to eq("customer_not_found")
+      end
+    end
+
+    context "with payment_provider_id" do
+      let(:payment_provider_id) { create(:stripe_provider).id }
+
+      it "saves the value" do
+        result = create_service.call
+        payment_method = result.payment_method
+
+        expect(payment_method).not_to be_nil
+        expect(payment_method.payment_provider_id).to eq(payment_provider_id)
+      end
+    end
+
+    context "with payment_provider_customer" do
+      let(:payment_provider_customer) { create(:stripe_customer, customer:, provider_customer_id: "cus_test") }
+
+      it "saves the reference" do
+        result = create_service.call
+        payment_method = result.payment_method
+
+        expect(payment_method).not_to be_nil
+        expect(payment_method.payment_provider_customer).to eq(payment_provider_customer)
+      end
+    end
+
+    describe "provider_method_type" do
+      context "when included in params" do
+        let(:params) do
+          {provider_payment_methods: %w[link card sepa_debit]}
+        end
+
+        it "saves the first param value" do
+          result = create_service.call
+          payment_method = result.payment_method
+
+          expect(payment_method.provider_method_type).to eq("link")
+        end
+      end
+
+      context "when default" do
+        let(:params) do
+          {}
+        end
+
+        it "saves the card value" do
+          result = create_service.call
+          payment_method = result.payment_method
+
+          expect(payment_method.provider_method_type).to eq("card")
+        end
+      end
+    end
+
+    describe "is_default" do
+      context "without payment methods" do
+        it "set as default" do
+          result = create_service.call
+          payment_method = result.payment_method
+
+          expect(payment_method.is_default).to be_truthy
+        end
+      end
+
+      context "with payment methods" do
+        before do
+          create(:payment_method, customer:, is_default:)
+        end
+
+        context "without default" do
+          let(:is_default) { false }
+
+          it "set as default" do
+            result = create_service.call
+            payment_method = result.payment_method
+
+            expect(payment_method.is_default).to be_truthy
+          end
+        end
+
+        context "with default" do
+          let(:is_default) { true }
+
+          it "does not set as default" do
+            result = create_service.call
+            payment_method = result.payment_method
+
+            expect(payment_method.is_default).to be_falsey
+          end
+        end
+      end
+    end
+  end
+end
