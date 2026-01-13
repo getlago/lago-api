818fb647c7c8de4f01678d91f019baa5d011e6f7
Murillo <murparreira@gmail.com>
2026-01-13 10:46:05 -0300

    rename: file

--- db/migrate/20260113102028_create_exports_entitlement_entitlement_values.rb
+++ db/migrate/20260113102028_create_exports_entitlement_entitlement_values.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateExportsEntitlementValues < ActiveRecord::Migration[8.0]
+class CreateExportsEntitlementEntitlementValues < ActiveRecord::Migration[8.0]
   def change
     create_view :exports_entitlement_entitlement_values
   end
