8551ee66731d9b8838af0251777a6bb354376cde
Andrew Kozin <andrew.kozin@gmail.com>
2026-01-23 15:53:09 +0000

    Fix factory and seed

--- spec/graphql/resolvers/activity_log_resolver_spec.rb
+++ spec/graphql/resolvers/activity_log_resolver_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Resolvers::ActivityLogResolver, clickhouse: true do
       query($activityLogId: ID!) {
         activityLog(activityId: $activityLogId) {
           activityId
+          activityObjectChanges
         }
       }
     GQL
@@ -50,8 +51,10 @@ RSpec.describe Resolvers::ActivityLogResolver, clickhouse: true do
     it_behaves_like "blocked feature", "feature_unavailable"
   end
 
-  context "with premium feature", :premium do
-    it "returns a single activity log" do
+  context "with premium feature" do
+    around { |test| lago_premium!(&test) }
+
+    it "returns a single activity log with parsed object changes" do
       result = execute_graphql(
         current_user: membership.user,
         current_organization: organization,
@@ -63,6 +66,7 @@ RSpec.describe Resolvers::ActivityLogResolver, clickhouse: true do
       activity_log_response = result["data"]["activityLog"]
 
       expect(activity_log_response["activityId"]).to eq(clickhouse_activity_log.activity_id)
+      expect(activity_log_response["activityObjectChanges"]).to eq("foo" => {"old" => "bar", "new" => "baz"})
     end
 
     context "when activity log is not found" do
