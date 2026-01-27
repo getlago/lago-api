aef84e1731645bebd9b0ca802b61495d9b1641fd
Julien Bourdeau <julien@julienbourdeau.com>
2026-01-27 11:56:00 +0100

    restore ID

--- app/graphql/types/usage_thresholds/object.rb
+++ app/graphql/types/usage_thresholds/object.rb
@@ -5,6 +5,8 @@ module Types
     class Object < Types::BaseObject
       graphql_name "UsageThreshold"
 
+      field :id, ID, null: false
+
       field :amount_cents, GraphQL::Types::BigInt, null: false
       field :recurring, Boolean, null: false
       field :threshold_display_name, String, null: true
