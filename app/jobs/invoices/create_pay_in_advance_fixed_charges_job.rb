c0ce84609c31e3d0a08f165d11a28ea6e42ba686
D1353L <D1353L@users.noreply.github.com>
2026-01-28 17:11:10 +0100

    feat(wallet): Add advisory lock to CreatePayInAdvanceFixedChargesService

--- app/jobs/invoices/create_pay_in_advance_fixed_charges_job.rb
+++ app/jobs/invoices/create_pay_in_advance_fixed_charges_job.rb
@@ -2,6 +2,10 @@
 
 module Invoices
   class CreatePayInAdvanceFixedChargesJob < ApplicationJob
+    def self.retry_delay
+      rand(0...16)
+    end
+
     queue_as do
       if ActiveModel::Type::Boolean.new.cast(ENV["SIDEKIQ_BILLING"])
         :billing
@@ -9,6 +13,16 @@ module Invoices
         :default
       end
     end
+
+    retry_on Sequenced::SequenceError, wait: :polynomially_longer, attempts: 15, jitter: 0.75
+    retry_on BaseService::ThrottlingError, wait: :polynomially_longer, attempts: 25
+
+    # We acquire a lock on the customer to prevent concurrent pay-in-advance invoice creation.
+    # When it fails, it raises a WithAdvisoryLock::FailedToAcquireLock error.
+    # If the lock succeeds but another job/request updates the wallet concurrenly, it will raise a ActiveRecord::StaleObjectError error.
+    retry_on WithAdvisoryLock::FailedToAcquireLock, ActiveRecord::StaleObjectError, attempts: 25, wait: ->(_) { CreatePayInAdvanceFixedChargesJob.retry_delay }
+
+    unique :until_executed, on_conflict: :log
 
     def perform(subscription, timestamp)
       Invoices::CreatePayInAdvanceFixedChargesService.call!(
