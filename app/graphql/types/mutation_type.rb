c7e015fe2c946662f5ed0ab031fa0df749462326
Romain Sempé <romain@getlago.com>
2026-02-02 15:18:28 +0100

    feat(subscription): Add GraphQL charge update mutation
    Add GraphQL support for updating charges on subscriptions:
    - Add updateSubscriptionCharge mutation to create/update charge overrides
    - Add charges field to Subscription type to list effective charges
    - Rename service to UpdateOrOverrideChargeService to better reflect its behavior

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -54,6 +54,7 @@ module Types
     field :create_subscription, mutation: Mutations::Subscriptions::Create
     field :terminate_subscription, mutation: Mutations::Subscriptions::Terminate
     field :update_subscription, mutation: Mutations::Subscriptions::Update
+    field :update_subscription_charge, mutation: Mutations::Subscriptions::UpdateCharge
 
     field :create_coupon, mutation: Mutations::Coupons::Create
     field :destroy_coupon, mutation: Mutations::Coupons::Destroy
--- spec/graphql/types/subscriptions/object_spec.rb
+++ spec/graphql/types/subscriptions/object_spec.rb
@@ -40,6 +40,7 @@ RSpec.describe Types::Subscriptions::Object do
     expect(subject).to have_field(:next_subscription_at).of_type("ISO8601DateTime")
 
     expect(subject).to have_field(:activity_logs).of_type("[ActivityLog!]")
+    expect(subject).to have_field(:charges).of_type("[Charge!]")
     expect(subject).to have_field(:fees).of_type("[Fee!]")
 
     expect(subject).to have_field(:lifetime_usage).of_type("SubscriptionLifetimeUsage")
