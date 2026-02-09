b3d222cff73df5c40466f11b7dcd3f2d4cf2a4a6
Nikita M. <D1353L@users.noreply.github.com>
2026-02-09 16:16:32 +0100

    feat(wallet): Fix tests

--- spec/jobs/invoices/create_pay_in_advance_fixed_charges_job_spec.rb
+++ spec/jobs/invoices/create_pay_in_advance_fixed_charges_job_spec.rb
@@ -66,7 +66,7 @@ RSpec.describe Invoices::CreatePayInAdvanceFixedChargesJob do
 
     [
       [Sequenced::SequenceError.new("Sequenced::SequenceError"), 15],
-      [WithAdvisoryLock::FailedToAcquireLock.new("customer-1"), 25],
+      [Customers::FailedToAcquireLock.new("customer-1"), 25],
       [ActiveRecord::StaleObjectError.new("Attempted to update a stale object: Wallet."), 25],
       [BaseService::ThrottlingError.new(provider_name: "Stripe"), 25]
     ].each do |error, attempts|
@@ -83,26 +83,6 @@ RSpec.describe Invoices::CreatePayInAdvanceFixedChargesJob do
               described_class.perform_later(subscription, timestamp)
             end.to raise_error(error_class)
           end
-        end
-      end
-    end
-  end
-
-  [
-    [Customers::FailedToAcquireLock.new("customer-1"), 25]
-  ].each do |error, attempts|
-    error_class = error.class
-
-    context "when a #{error_class} error is raised" do
-      before do
-        allow(Invoices::CreatePayInAdvanceFixedChargesService).to receive(:call!).and_raise(error)
-      end
-
-      it "raises a #{error_class.class.name} error and retries" do
-        assert_performed_jobs(attempts, only: [described_class]) do
-          expect do
-            described_class.perform_later(subscription, timestamp)
-          end.to raise_error(error_class)
         end
       end
     end
