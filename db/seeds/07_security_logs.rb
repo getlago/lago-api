5fe209662ad105c2c19ef037240bf38c9546ebf1
Andrew Kozin <andrew.kozin@gmail.com>
2026-02-10 14:53:51 +0000

    feat: add security logging for user registration
    Added security logging of the `user.signed_up` event for successful
    user registration.
    
    The event is sent only when a user and an organization are successfully
    created. It skips the premium check since the organization is being
    created at that moment, and never has a premium subscription yet.

--- db/seeds/07_security_logs.rb
+++ db/seeds/07_security_logs.rb
@@ -10,3 +10,14 @@ existing = Karafka::Admin.cluster_info.topics.map { |t| t[:topic_name] }
 unless existing.include?(topic)
   Karafka::Admin.create_topic(topic, 1, 1)
 end
+
+organization = Organization.find_by!(name: "Hooli")
+user = organization.memberships.first!.user
+
+Utils::SecurityLog.produce(
+  organization:,
+  log_type: "user",
+  log_event: "user.signed_up",
+  user:,
+  skip_organization_check: true
+)
