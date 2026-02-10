1e8e08362ee082aab6594390ca92be01338f07df
Nikita <D1353L@users.noreply.github.com>
2026-02-10 11:12:41 +0100

    feat(wallet): Add customer lock to CreatePayInAdvanceFixedCharges (#4935)
    ## Context
    This is a part of https://github.com/getlago/lago-api/pull/4987
    
    ## Description
    - Added the new Customers::LockService to
    CreatePayInAdvanceFixedChargesService
    - Added handling of `Sequenced::SequenceError,
    BaseService::ThrottlingError, Customers::FailedToAcquireLock,
    ActiveRecord::StaleObjectError` to CreatePayInAdvanceFixedChargesJob so
    it matches CreatePayInAdvanceChargeJob

--- app/jobs/invoices/create_pay_in_advance_fixed_charges_job.rb
+++ app/jobs/invoices/create_pay_in_advance_fixed_charges_job.rb
@@ -2,6 +2,10 @@
 
 module Invoices
   class CreatePayInAdvanceFixedChargesJob < ApplicationJob
+    def self.retry_delay
+      rand(0...16)
+    end
+
     queue_as do
       if ActiveModel::Type::Boolean.new.cast(ENV["SIDEKIQ_BILLING"])
         :billing
@@ -10,7 +14,15 @@ module Invoices
       end
     end
 
-    retry_on Customers::FailedToAcquireLock, attempts: 25, wait: ->(_) { rand(0...16) }
+    retry_on Sequenced::SequenceError, wait: :polynomially_longer, attempts: 15, jitter: 0.75
+    retry_on BaseService::ThrottlingError, wait: :polynomially_longer, attempts: 25
+
+    # We acquire a lock on the customer to prevent concurrent pay-in-advance invoice creation.
+    # When it fails, it raises a WithAdvisoryLock::FailedToAcquireLock error.
+    # If the lock succeeds but another job/request updates the wallet concurrenly, it will raise a ActiveRecord::StaleObjectError error.
+    retry_on Customers::FailedToAcquireLock, ActiveRecord::StaleObjectError, attempts: 25, wait: ->(_) { CreatePayInAdvanceFixedChargesJob.retry_delay }
+
+    unique :until_executed, on_conflict: :log
 
     def perform(subscription, timestamp)
       result = Invoices::CreatePayInAdvanceFixedChargesService.call(
