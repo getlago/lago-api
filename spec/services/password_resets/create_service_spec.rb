040e65246b2a8b8fdfd60f4abbabdc44943ea385
Andrew Kozin <andrew.kozin@gmail.com>
2026-02-12 12:39:10 +0000

    feat: add security logging for password reset request
    Added security logging of the `user.password_reset_requested` event for
    successful password reset request.
    
    The event is sent for all organizations where the user has an active membership.
    Logging occurs only when a password reset request is successfully created.
    Logging does not occur in error cases: user not found.

--- spec/services/password_resets/create_service_spec.rb
+++ spec/services/password_resets/create_service_spec.rb
@@ -5,8 +5,12 @@ require "rails_helper"
 RSpec.describe PasswordResets::CreateService do
   subject(:create_service) { described_class }
 
+  include_context "with mocked security logger"
+
   describe "#call" do
-    let(:user) { create(:user) }
+    let(:organization) { create(:organization) }
+    let(:membership) { create(:membership, organization:) }
+    let(:user) { membership.user }
     let(:create_args) do
       {
         user:
@@ -18,12 +22,54 @@ RSpec.describe PasswordResets::CreateService do
         .to change(PasswordReset, :count).by(1)
     end
 
+    it "produces a security log" do
+      create_service.call(**create_args)
+
+      expect(security_logger).to have_received(:produce).with(
+        organization: organization,
+        log_type: "user",
+        log_event: "user.password_reset_requested",
+        user: user,
+        resources: {email: user.email}
+      )
+    end
+
+    context "with multiple active memberships" do
+      let!(:other_organization) { create(:membership, user:).organization }
+
+      it "produces a security log for each organization" do
+        create_service.call(**create_args)
+
+        expect(security_logger).to have_received(:produce).with(
+          organization: organization,
+          log_type: "user",
+          log_event: "user.password_reset_requested",
+          user: user,
+          resources: {email: user.email}
+        )
+
+        expect(security_logger).to have_received(:produce).with(
+          organization: other_organization,
+          log_type: "user",
+          log_event: "user.password_reset_requested",
+          user: user,
+          resources: {email: user.email}
+        )
+      end
+    end
+
     context "without arguments" do
       it "raises an error" do
         result = create_service.call(user: nil)
 
         expect(result).not_to be_success
         expect(result.error.error_code).to eq("user_not_found")
+      end
+
+      it "does not produce a security log" do
+        create_service.call(user: nil)
+
+        expect(security_logger).not_to have_received(:produce)
       end
     end
 
