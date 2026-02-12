a4ac441175836c664b69553fc39181532302a0b3
Vincent Pochet <vincent@getlago.com>
2026-02-12 15:25:06 +0100

    misc(database): Add pg extension helper (#5028)
    ## Context
    
    This PR is related to
    https://github.com/getlago/lago-api/pull/5025#discussion_r2798059134
    
    ## Description
    
    It adds a new helper for migrations with postgres to check the presence
    on an extension library.
    
    Usage:
    ```ruby
    require Rails.root.join("lib/migrations/extension_helper")
    
    class SomeMigration < ActiveRecord::Migration[8.0]
      include Migrations::ExtensionHelper
    
      def up
        return unless pg_extension_present?("some_pg_extension")
        
        # ... some migration logic
      end
    end
    ```

--- db/migrate/20260109092932_setup_partman.rb
+++ db/migrate/20260109092932_setup_partman.rb
@@ -3,19 +3,15 @@
 class SetupPartman < ActiveRecord::Migration[8.0]
   def up
     safety_assured do
-      # Check if pg_partman is available on the server
-      result = execute <<~SQL
-        SELECT 1 FROM pg_available_extensions WHERE name = 'pg_partman';
-      SQL
-
-      if result.ntuples.zero?
+      unless pg_extension_present?("pg_partman")
         Rails.logger.debug "pg_partman extension is not available on this PostgreSQL server, skipping..."
-      else
-        execute <<~SQL
-          CREATE SCHEMA IF NOT EXISTS partman;
-          CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
-        SQL
+        return
       end
+
+      execute <<~SQL
+        CREATE SCHEMA IF NOT EXISTS partman;
+        CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
+      SQL
     end
   end
 end
