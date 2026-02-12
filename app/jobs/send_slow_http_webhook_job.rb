831e50c562498b7123a4367f0364ad3ddd0b7c0a
Anna Velentsevich <annvelents@gmail.com>
2026-02-12 16:55:03 +0100

    move slow webhooks to slow webhooks queue

--- app/jobs/send_slow_http_webhook_job.rb
+++ app/jobs/send_slow_http_webhook_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SendSlowHttpWebhookJob < ApplicationJob
-  queue_as :low_priority
+  queue_as :webhook_low_priority
 
   retry_on ActiveJob::DeserializationError, wait: :polynomially_longer, attempts: 3 do |job, error|
     Rails.logger.warn("Discarding #{job.class.name} after 3 retries due to: #{error.message}")
