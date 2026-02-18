122f81450ed5684613b6f467462a675de0252654
Nikita M. <D1353L@users.noreply.github.com>
2026-02-18 19:38:59 +0100

    feat(wallet): Add tests

--- spec/jobs/clock/terminate_ended_subscriptions_job_spec.rb
+++ spec/jobs/clock/terminate_ended_subscriptions_job_spec.rb
@@ -5,16 +5,13 @@ require "rails_helper"
 describe Clock::TerminateEndedSubscriptionsJob, job: true do
   subject { described_class }
 
-  describe ".perform" do
-    let(:ending_at) { (Time.current + 2.months).beginning_of_day }
-    let(:subscription1) { create(:subscription, ending_at:) }
-    let(:subscription2) { create(:subscription, ending_at: ending_at + 1.year) }
-    let(:subscription3) { create(:subscription, ending_at: nil) }
+  let(:ending_at) { (Time.current + 2.months).beginning_of_day }
+  let!(:subscription1) { create(:subscription, ending_at:) }
+  let!(:subscription2) { create(:subscription, ending_at: ending_at + 1.year) }
+  let!(:subscription3) { create(:subscription, ending_at: nil) }
 
+  describe ".perform" do
     before do
-      subscription1
-      subscription2
-      subscription3
       allow(Subscriptions::TerminateService).to receive(:call)
     end
 
@@ -50,6 +47,33 @@ describe Clock::TerminateEndedSubscriptionsJob, job: true do
             .not_to have_received(:call).with(subscription: subscription2)
           expect(Subscriptions::TerminateService)
             .not_to have_received(:call).with(subscription: subscription3)
+        end
+      end
+    end
+  end
+
+  describe "retry_on" do
+    [
+      [Customers::FailedToAcquireLock.new("customer-1-prepaid_credit"), 25],
+      [ActiveRecord::StaleObjectError.new("Attempted to update a stale object: Wallet."), 25]
+    ].each do |error, attempts|
+      error_class = error.class
+
+      context "when a #{error_class} error is raised" do
+        before do
+          allow(Subscriptions::TerminateService).to receive(:call).and_raise(error)
+        end
+
+        it "raises a #{error_class.class.name} error and retries" do
+          current_date = Time.current + 2.months
+
+          travel_to(current_date) do
+            assert_performed_jobs(attempts, only: [described_class]) do
+              expect do
+                described_class.perform_later
+              end.to raise_error(error_class)
+            end
+          end
         end
       end
     end
