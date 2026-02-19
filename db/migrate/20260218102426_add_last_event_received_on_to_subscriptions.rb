d29ea8f4b9ccbe803c827f6044d1fff21accfa78
Miguel Pinto <darkymiguel@gmail.com>
2026-02-19 17:07:43 +0000

    feat: update migration

--- db/migrate/20260218102426_add_last_event_received_on_to_subscriptions.rb
+++ db/migrate/20260218102426_add_last_event_received_on_to_subscriptions.rb
@@ -22,18 +22,32 @@ class AddLastEventReceivedOnToSubscriptions < ActiveRecord::Migration[8.0]
 
   def backfill_today
     today = Date.current
+    batch_size = 1000
+    offset = 0
 
-    safety_assured do
-      execute <<~SQL
+    loop do
+      batch = safety_assured do
+        execute(<<~SQL).map { |row| row["subscription_id"] }
+        SELECT DISTINCT subscription_id
+        FROM events
+        WHERE DATE(timestamp) >= '#{today - 1.day}'
+          AND deleted_at IS NULL
+        LIMIT #{batch_size}
+        OFFSET #{offset}
+      SQL
+      end
+
+      break if batch.empty?
+
+      safety_assured do
+        execute <<~SQL
         UPDATE subscriptions
         SET last_received_event_on = '#{today}'
-        WHERE id IN (
-          SELECT DISTINCT subscription_id
-          FROM events
-          WHERE DATE(timestamp) >= '#{today - 1.day}'
-            AND deleted_at IS NULL
-        )
+        WHERE id IN (#{batch.join(",")})
       SQL
+      end
+
+      offset += batch_size
     end
   end
 end
