0e7c89cee00889297fe390d03804de739c79694d
Miguel Pinto <darkymiguel@gmail.com>
2026-02-19 16:41:12 +0000

    feat: update migration

--- db/migrate/20260218102426_add_last_event_received_on_to_subscriptions.rb
+++ db/migrate/20260218102426_add_last_event_received_on_to_subscriptions.rb
@@ -6,16 +6,16 @@ class AddLastEventReceivedOnToSubscriptions < ActiveRecord::Migration[8.0]
   def up
     add_column :subscriptions, :last_received_event_on, :date
     add_index :subscriptions, :last_received_event_on,
-              name: "index_subscriptions_on_last_received_event_on",
-              algorithm: :concurrently,
-              if_not_exists: true
+      name: "index_subscriptions_on_last_received_event_on",
+      algorithm: :concurrently,
+      if_not_exists: true
 
     backfill_today
   end
 
   def down
     remove_index :subscriptions, name: "index_subscriptions_on_last_received_event_on", if_exists: true
-    remove_column :subscriptions, :last_received_event_on
+    remove_column :subscriptions, :last_received_event_on # rubocop:disable Lago/NoDropColumnOrTable
   end
 
   private
@@ -23,14 +23,15 @@ class AddLastEventReceivedOnToSubscriptions < ActiveRecord::Migration[8.0]
   def backfill_today
     today = Time.zone.today
 
-    Subscription
-      .where(
-        id: Event
-              .where("DATE(timestamp) = ?", today)
-              .where(deleted_at: nil)
-              .select(:subscription_id)
-              .distinct
+    execute <<~SQL
+      UPDATE subscriptions
+      SET last_received_event_on = '#{today}'
+      WHERE id IN (
+        SELECT DISTINCT subscription_id
+        FROM events
+        WHERE DATE(timestamp) = '#{today}'
+          AND deleted_at IS NULL
       )
-      .update_all(last_received_event_on: today)
+    SQL
   end
 end
