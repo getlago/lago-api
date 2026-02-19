8ac05c31dae6dbf677e6865e235ad0c8b0da9bc1
Miguel Pinto <darkymiguel@gmail.com>
2026-02-19 16:29:07 +0000

    feat: update migration

--- db/migrate/20260218102426_add_last_event_received_on_to_subscriptions.rb
+++ db/migrate/20260218102426_add_last_event_received_on_to_subscriptions.rb
@@ -3,11 +3,34 @@
 class AddLastEventReceivedOnToSubscriptions < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
-  def change
+  def up
     add_column :subscriptions, :last_received_event_on, :date
     add_index :subscriptions, :last_received_event_on,
-      name: "index_subscriptions_on_last_received_event_on",
-      algorithm: :concurrently,
-      if_not_exists: true
+              name: "index_subscriptions_on_last_received_event_on",
+              algorithm: :concurrently,
+              if_not_exists: true
+
+    backfill_today
+  end
+
+  def down
+    remove_index :subscriptions, name: "index_subscriptions_on_last_received_event_on", if_exists: true
+    remove_column :subscriptions, :last_received_event_on
+  end
+
+  private
+
+  def backfill_today
+    today = Time.zone.today
+
+    Subscription
+      .where(
+        id: Event
+              .where("DATE(timestamp) = ?", today)
+              .where(deleted_at: nil)
+              .select(:subscription_id)
+              .distinct
+      )
+      .update_all(last_received_event_on: today)
   end
 end
