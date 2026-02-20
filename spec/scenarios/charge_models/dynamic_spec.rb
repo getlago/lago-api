b5ce16876433c88eb77f8c0db2cb9bfdebc28518
Yohan Robert <yohan@getlago.com>
2026-02-20 16:02:48 +0100

    perf(cache): Optimize charge cache by excluding zero units/amounts/events fees

--- spec/scenarios/charge_models/dynamic_spec.rb
+++ spec/scenarios/charge_models/dynamic_spec.rb
@@ -27,100 +27,108 @@ describe "Charge Models - Dynamic Pricing Scenarios", transaction: false do
     charge
   end
 
-  it "returns the expected customer usage" do
-    travel_to(DateTime.new(2023, 3, 5)) do
-      create_subscription(
-        {
-          external_customer_id: customer.external_id,
-          external_id: customer.external_id,
-          plan_code: plan.code
-        }
-      )
-    end
-
-    travel_to(DateTime.new(2023, 3, 6)) do
-      create_event(
-        {
-          code: billable_metric.code,
-          transaction_id: SecureRandom.uuid,
-          external_subscription_id: customer.external_id,
-          precise_total_amount_cents: "10.1",
-          properties: {item_id: "10"}
-        }
-      )
-
-      fetch_current_usage(customer:)
-      expect(json[:customer_usage][:total_amount_cents]).to eq(10)
-      expect(json[:customer_usage][:charges_usage][0][:units]).to eq("10.0")
-
-      create_event(
-        {
-          code: billable_metric.code,
-          transaction_id: SecureRandom.uuid,
-          external_subscription_id: customer.external_id,
-          precise_total_amount_cents: "901.9",
-          properties: {item_id: "10"}
-        }
-      )
-
-      fetch_current_usage(customer:)
-      expect(json[:customer_usage][:total_amount_cents]).to eq(912)
-      expect(json[:customer_usage][:charges_usage][0][:units]).to eq("20.0")
-    end
-  end
-
-  context "with grouping" do
-    let(:charge_properties) { {grouped_by: [:group_key]} }
-
-    it "returns the expected customer usage" do
-      travel_to(DateTime.new(2023, 3, 5)) do
-        create_subscription(
-          {
-            external_customer_id: customer.external_id,
-            external_id: customer.external_id,
-            plan_code: plan.code
-          }
-        )
+  [true, false].each do |ff_enabled|
+    context "with non_persistable_charge_cache_optimization #{ff_enabled ? "enabled" : "disabled"}" do
+      before do
+        organization.enable_feature_flag!(:non_persistable_charge_cache_optimization) if ff_enabled
       end
 
-      travel_to(DateTime.new(2023, 3, 6)) do
-        create_event(
-          {
-            code: billable_metric.code,
-            transaction_id: SecureRandom.uuid,
-            external_subscription_id: customer.external_id,
-            precise_total_amount_cents: "10.1",
-            properties: {item_id: "10", group_key: "value 1"}
-          }
-        )
+      it "returns the expected customer usage" do
+        travel_to(DateTime.new(2023, 3, 5)) do
+          create_subscription(
+            {
+              external_customer_id: customer.external_id,
+              external_id: customer.external_id,
+              plan_code: plan.code
+            }
+          )
+        end
 
-        fetch_current_usage(customer:)
+        travel_to(DateTime.new(2023, 3, 6)) do
+          create_event(
+            {
+              code: billable_metric.code,
+              transaction_id: SecureRandom.uuid,
+              external_subscription_id: customer.external_id,
+              precise_total_amount_cents: "10.1",
+              properties: {item_id: "10"}
+            }
+          )
 
-        expect(json[:customer_usage][:total_amount_cents]).to eq(10)
-        expect(json[:customer_usage][:charges_usage][0][:units]).to eq("10.0")
-        expect(json[:customer_usage][:charges_usage][0][:grouped_usage][0][:grouped_by]).to eq({group_key: "value 1"})
+          fetch_current_usage(customer:)
+          expect(json[:customer_usage][:total_amount_cents]).to eq(10)
+          expect(json[:customer_usage][:charges_usage][0][:units]).to eq("10.0")
 
-        create_event(
-          {
-            code: billable_metric.code,
-            transaction_id: SecureRandom.uuid,
-            external_subscription_id: customer.external_id,
-            precise_total_amount_cents: "901.9",
-            properties: {item_id: "10", group_key: "value 2"}
-          }
-        )
+          create_event(
+            {
+              code: billable_metric.code,
+              transaction_id: SecureRandom.uuid,
+              external_subscription_id: customer.external_id,
+              precise_total_amount_cents: "901.9",
+              properties: {item_id: "10"}
+            }
+          )
 
-        fetch_current_usage(customer:)
-        expect(json[:customer_usage][:total_amount_cents]).to eq(912)
-        expect(json[:customer_usage][:charges_usage][0][:units]).to eq("20.0")
-        expect(json[:customer_usage][:charges_usage][0][:grouped_usage].size).to eq(2)
+          fetch_current_usage(customer:)
+          expect(json[:customer_usage][:total_amount_cents]).to eq(912)
+          expect(json[:customer_usage][:charges_usage][0][:units]).to eq("20.0")
+        end
+      end
 
-        expect(json[:customer_usage][:charges_usage][0][:grouped_usage]).to match_array(
-          [
-            {amount_cents: 902, events_count: 1, total_aggregated_units: "10.0", units: "10.0", grouped_by: {group_key: "value 2"}, filters: [], pricing_unit_details: nil},
-            {amount_cents: 10, events_count: 1, total_aggregated_units: "10.0", units: "10.0", grouped_by: {group_key: "value 1"}, filters: [], pricing_unit_details: nil}
-          ]
-        )
+      context "with grouping" do
+        let(:charge_properties) { {grouped_by: [:group_key]} }
+
+        it "returns the expected customer usage" do
+          travel_to(DateTime.new(2023, 3, 5)) do
+            create_subscription(
+              {
+                external_customer_id: customer.external_id,
+                external_id: customer.external_id,
+                plan_code: plan.code
+              }
+            )
+          end
+
+          travel_to(DateTime.new(2023, 3, 6)) do
+            create_event(
+              {
+                code: billable_metric.code,
+                transaction_id: SecureRandom.uuid,
+                external_subscription_id: customer.external_id,
+                precise_total_amount_cents: "10.1",
+                properties: {item_id: "10", group_key: "value 1"}
+              }
+            )
+
+            fetch_current_usage(customer:)
+
+            expect(json[:customer_usage][:total_amount_cents]).to eq(10)
+            expect(json[:customer_usage][:charges_usage][0][:units]).to eq("10.0")
+            expect(json[:customer_usage][:charges_usage][0][:grouped_usage][0][:grouped_by]).to eq({group_key: "value 1"})
+
+            create_event(
+              {
+                code: billable_metric.code,
+                transaction_id: SecureRandom.uuid,
+                external_subscription_id: customer.external_id,
+                precise_total_amount_cents: "901.9",
+                properties: {item_id: "10", group_key: "value 2"}
+              }
+            )
+
+            fetch_current_usage(customer:)
+            expect(json[:customer_usage][:total_amount_cents]).to eq(912)
+            expect(json[:customer_usage][:charges_usage][0][:units]).to eq("20.0")
+            expect(json[:customer_usage][:charges_usage][0][:grouped_usage].size).to eq(2)
+
+            expect(json[:customer_usage][:charges_usage][0][:grouped_usage]).to match_array(
+              [
+                {amount_cents: 902, events_count: 1, total_aggregated_units: "10.0", units: "10.0", grouped_by: {group_key: "value 2"}, filters: [], pricing_unit_details: nil},
+                {amount_cents: 10, events_count: 1, total_aggregated_units: "10.0", units: "10.0", grouped_by: {group_key: "value 1"}, filters: [], pricing_unit_details: nil}
+              ]
+            )
+          end
+        end
       end
     end
   end
