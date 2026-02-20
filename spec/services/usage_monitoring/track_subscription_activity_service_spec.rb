8873be0ba2cf65b5843ebd438f03d3dcabcd07cb
Miguel Pinto <darkymiguel@gmail.com>
2026-02-20 12:27:14 +0000

    revert: daily usage flag

--- spec/services/usage_monitoring/track_subscription_activity_service_spec.rb
+++ spec/services/usage_monitoring/track_subscription_activity_service_spec.rb
@@ -8,18 +8,12 @@ RSpec.describe UsageMonitoring::TrackSubscriptionActivityService, :premium do
   let(:organization) { create(:organization, premium_integrations: %w[lifetime_usage]) }
   let(:customer) { create(:customer, organization:) }
   let(:subscription) { create(:subscription, customer:) }
-  let(:date) { Date.new(2025, 1, 15) }
 
   context "when the plan has usage_thresholds" do
     it "tracks activity" do
       create(:usage_threshold, plan: subscription.plan)
       expect { subject.call }.to change { organization.subscription_activities.count }.by(1)
       expect { subject.call }.not_to change { organization.subscription_activities.count }
-    end
-
-    it "sets last_received_event_on" do
-      create(:usage_threshold, plan: subscription.plan)
-      expect { subject.call }.to change { subscription.reload.last_received_event_on }.from(nil).to(date)
     end
   end
 
@@ -30,18 +24,6 @@ RSpec.describe UsageMonitoring::TrackSubscriptionActivityService, :premium do
       subject.call
       expect(organization.subscription_activities.count).to eq(0)
     end
-
-    it "still sets last_received_event_on" do
-      expect { subject.call }.to change { subscription.reload.last_received_event_on }.from(nil).to(date)
-    end
-  end
-
-  context "when last_received_event_on is already set to the same date" do
-    before { subscription.update(last_received_event_on: date) }
-
-    it "does not update the subscription" do
-      expect { subject.call }.not_to change { subscription.reload.updated_at }
-    end
   end
 
   context "when subscription isn't active" do
@@ -50,19 +32,6 @@ RSpec.describe UsageMonitoring::TrackSubscriptionActivityService, :premium do
     it "does not track activity" do
       subject.call
       expect(organization.subscription_activities.count).to eq(0)
-    end
-
-    it "does not set last_received_event_on" do
-      subject.call
-      expect(subscription.reload.last_received_event_on).to be_nil
-    end
-  end
-
-  context "when license is not premium" do
-    it "does not set last_received_event_on" do
-      allow(License).to receive(:premium?).and_return(false)
-      subject.call
-      expect(subscription.reload.last_received_event_on).to be_nil
     end
   end
 end
