078f2ef3a6ca11625f73546a2e7e12972dc4e81e
Romain Sempé <romain@getlago.com>
2026-02-23 17:26:26 +0100

    fix(charge): Make check constraint migration idempotent

--- db/migrate/20251229153718_add_code_not_null_check_constraint_to_charges.rb
+++ db/migrate/20251229153718_add_code_not_null_check_constraint_to_charges.rb
@@ -2,7 +2,7 @@
 
 class AddCodeNotNullCheckConstraintToCharges < ActiveRecord::Migration[8.0]
   def change
-    add_check_constraint :charges, "code IS NOT NULL", name: "charges_code_not_null", validate: false
-    add_check_constraint :fixed_charges, "code IS NOT NULL", name: "fixed_charges_code_not_null", validate: false
+    add_check_constraint :charges, "code IS NOT NULL", name: "charges_code_not_null", validate: false, if_not_exists: true
+    add_check_constraint :fixed_charges, "code IS NOT NULL", name: "fixed_charges_code_not_null", validate: false, if_not_exists: true
   end
 end
