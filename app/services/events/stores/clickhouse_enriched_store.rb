f779ee4468a8c30e1fac88362cc3ba1258c44545
Vincent Pochet <vincent@getlago.com>
2026-02-24 15:11:12 +0100

    feat(clickhouse): Start implementing EnrichedEventStore (#5094)
    ## Context
    
    Today, event aggregation for billing and usage relies on the `events`
    (PG) or `events_enriched` (CH) tables and filters/groups them by parsing
    a JSON `properties` column at query time.
    
    This is inefficient at scale, especially when many subscriptions are
    being refreshed in parallel.
    
    Two tables — `enriched_events` (PG) and `events_enriched_expanded` (CH),
    have been added recently and pre-enrich the events with `charge_id`,
    `charge_filter_id`, and `grouped_by` columns. Today they are only used
    for pre-filtering which charges to process.
    
    The goal of this feature is to create two new store classes that will
    use these tables for the actual aggregation, replacing JSON property
    scanning and improving the performance.
    
    ## Description
    
    This PR introduce the base structure of the
    `Events::Stores::ClickhouseEnrichedStore` and defines the `count` and
    `max` aggregation
    
    Since the specs are shared between all stores, this feature refactors
    the `an_event_store` shared examples to allow a step by step definition
    of all aggregations by disabling the features that are not yet
    implemented on the store.
    
    Also, since the behavior to filter and groups the events differs from
    the other stores, some refactoring have been made to allow filtering by
    `charge_filter_id` in parallel of the `matching_filters` and
    `excluding_filters` values

---
+++ app/services/events/stores/clickhouse_enriched_store.rb
@@ -0,0 +1,217 @@
+# frozen_string_literal: true
+
+module Events
+  module Stores
+    class ClickhouseEnrichedStore < BaseStore
+      include Events::Stores::Utils::QueryHelpers
+
+      def events
+        raise NotImplementedError
+      end
+
+      def events_cte_queries(**args)
+        return events_cte_queries_with_deduplication(**args) if deduplicate
+
+        events_cte_queries_without_deduplication(**args)
+      end
+
+      def events_cte_queries_without_deduplication(force_from: false, ordered: false, select: arel_table[Arel.star], deduplicated_columns: [])
+        query = arel_table.where(
+          arel_table[:subscription_id].eq(subscription.id)
+            .and(arel_table[:organization_id].eq(subscription.organization_id))
+            .and(arel_table[:charge_id].eq(charge_id))
+        ).then { with_charge_filter_id(it) }
+
+        query = query.order(arel_table[:timestamp].desc, arel_table[:value].asc) if ordered
+
+        query = with_timestamp_boundaries(
+          query,
+          (from_datetime if force_from || use_from_boundary),
+          applicable_to_datetime
+        )
+
+        query = apply_arel_grouped_by_values(query) if grouped_by_values?
+
+        {"events" => query.project(select).to_sql}
+      end
+
+      def events_cte_queries_with_deduplication(force_from: false, ordered: false, select: arel_table[Arel.star], deduplicated_columns: [])
+        # Ensure presence of one of value or decimal_value for the ordering
+        order_column = deduplicated_columns.include?("decimal_value") ? "decimal_value" : "value"
+        deduplicated_columns << order_column if ordered
+
+        base_sql = deduplicated_events_sql(
+          from_datetime: (from_datetime if force_from || use_from_boundary),
+          to_datetime: (applicable_to_datetime if applicable_to_datetime),
+          deduplicated_columns:
+        ).to_sql
+
+        query = Arel::Table.new(:events_enriched_expanded)
+        query = query.order(arel_table[:timestamp].desc, arel_table[order_column.to_sym]) if ordered
+        query = apply_arel_grouped_by_values(query) if grouped_by_values?
+
+        {
+          "events_enriched_expanded" => base_sql, # Override events table name with deduplicated events
+          "events" => query.project(select).to_sql
+        }
+      end
+
+      # Clickhouse cannot garanty that events_enriched_expanded will be deduplicated all the time
+      # To address this problem, we have to implement deduplication at query time.
+      # This is done by grouping events on transaction_id and timestamp (unicity key) and
+      # by using `argMax` function, to keep only the most recent event of each group
+      def deduplicated_events_sql(from_datetime:, to_datetime:, deduplicated_columns: [])
+        query = arel_table.where(
+          arel_table[:subscription_id].eq(subscription.id)
+            .and(arel_table[:organization_id].eq(subscription.organization_id))
+            .and(arel_table[:charge_id].eq(charge_id))
+        ).then { with_charge_filter_id(it) }
+          .then { with_timestamp_boundaries(it, from_datetime, to_datetime) }
+
+        columns = deduplicated_columns.dup
+
+        if grouped_by.present? || grouped_by_values.present?
+          columns << "sorted_grouped_by"
+        end
+
+        arel_columns = columns.uniq.map do
+          Arel::Nodes::NamedFunction.new("argMax", [arel_table[it.to_sym], arel_table[:enriched_at]]).as(it)
+        end
+
+        query = query.group(
+          arel_table[:charge_id],
+          arel_table[:charge_filter_id],
+          arel_table[:subscription_id],
+          arel_table[:organization_id],
+          arel_table[:timestamp],
+          arel_table[:transaction_id]
+        )
+        query.project(
+          [
+            arel_table[:charge_id],
+            arel_table[:charge_filter_id],
+            arel_table[:subscription_id],
+            arel_table[:organization_id],
+            arel_table[:timestamp],
+            arel_table[:transaction_id]
+          ] + arel_columns
+        )
+      end
+
+      def events_values
+        raise NotImplementedError
+      end
+
+      def last_event
+        raise NotImplementedError
+      end
+
+      def prorated_events_values
+        raise NotImplementedError
+      end
+
+      def count
+        Utils::ClickhouseConnection.connection_with_retry do |connection|
+          sql = with_ctes(events_cte_queries(deduplicated_columns: %w[value]), <<-SQL)
+            SELECT count()
+            FROM events
+          SQL
+
+          connection.select_value(sql).to_i
+        end
+      end
+
+      def grouped_count
+        raise NotImplementedError
+      end
+
+      def max
+        Utils::ClickhouseConnection.connection_with_retry do |connection|
+          sql = with_ctes(events_cte_queries(deduplicated_columns: %w[decimal_value]), <<-SQL)
+            SELECT max(events.decimal_value)
+            FROM events
+          SQL
+
+          connection.select_value(sql)
+        end
+      end
+
+      def grouped_max
+        raise NotImplementedError
+      end
+
+      def last
+        raise NotImplementedError
+      end
+
+      def grouped_last
+        raise NotImplementedError
+      end
+
+      def sum
+        raise NotImplementedError
+      end
+
+      def grouped_sum
+        raise NotImplementedError
+      end
+
+      def sum_precise_total_amount_cents
+        raise NotImplementedError
+      end
+
+      def grouped_sum_precise_total_amount_cents
+        raise NotImplementedError
+      end
+
+      def prorated_sum(period_duration:, persisted_duration: nil)
+        raise NotImplementedError
+      end
+
+      def grouped_prorated_sum(period_duration:, persisted_duration: nil)
+        raise NotImplementedError
+      end
+
+      def sum_date_breakdown
+        raise NotImplementedError
+      end
+
+      def weighted_sum(initial_value: 0)
+        raise NotImplementedError
+      end
+
+      def grouped_weighted_sum(initial_values: [])
+        raise NotImplementedError
+      end
+
+      def arel_table
+        @arel_table ||= ::Clickhouse::EventsEnrichedExpanded.arel_table
+      end
+
+      def with_timestamp_boundaries(query, from_datetime, to_datetime)
+        query = query.where(arel_table[:timestamp].gteq(from_datetime)) if from_datetime
+        query = query.where(arel_table[:timestamp].lteq(to_datetime)) if to_datetime
+        query
+      end
+
+      def with_charge_filter_id(query)
+        if charge_filter_id.present?
+          query.where(arel_table[:charge_filter_id].eq(charge_filter_id))
+        else
+          query.where(arel_table[:charge_filter_id].eq(""))
+        end
+      end
+
+      def apply_arel_grouped_by_values(query)
+        # NOTE: grouped_by is populated from a sorted Map(String, String) converted into a String
+        #       to make it comparable, we need to sort the group keys and replace nil values with "<nil>" string
+        groups = grouped_by_values
+          .sort_by { |key, _| key }
+          .flat_map { |k, v| [Arel::Nodes.build_quoted(k), Arel::Nodes.build_quoted(v.presence || "")] }
+
+        map_fn = Arel::Nodes::NamedFunction.new("map", groups)
+        query.where(arel_table[:sorted_grouped_by].eq(map_fn))
+      end
+    end
+  end
+end
---
+++ spec/services/events/stores/clickhouse_enriched_store_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+require_relative "shared_examples/an_event_store"
+
+RSpec.describe Events::Stores::ClickhouseEnrichedStore, clickhouse: {clean_before: true} do
+  def create_event(timestamp:, value:, properties: {}, transaction_id: SecureRandom.uuid, code: billable_metric.code, charge_filter: nil, enriched_at: nil)
+    grouped_values = if events_grouped_by.present?
+      events_grouped_by.index_with({}) { properties[it] || "" }
+    end
+
+    Clickhouse::EventsEnrichedExpanded.create!(
+      transaction_id:,
+      organization_id: organization.id,
+      external_subscription_id: subscription.external_id,
+      subscription_id: subscription.id,
+      plan_id: subscription.plan_id,
+      code:,
+      aggregation_type: billable_metric.aggregation_type,
+      charge_id: charge.id,
+      charge_version: charge.updated_at,
+      charge_filter_id: charge_filter&.id,
+      charge_filter_version: charge_filter&.updated_at,
+      timestamp:,
+      properties:,
+      grouped_by: grouped_values,
+      value:,
+      decimal_value: value&.to_i&.to_d,
+      precise_total_amount_cents: nil,
+      enriched_at:
+    )
+  end
+
+  alias_method :create_enriched_event, :create_event
+
+  context "without deduplication" do
+    it_behaves_like "an event store", with_event_duplication: false,
+      excluding_features: %i[
+        events
+        events_values
+        last_event
+        grouped_last_event
+        prorated_events_values
+        distinct_codes
+        distinct_charges_and_filters
+        active_unique_property?
+        grouped_count
+        grouped_max
+        last
+        grouped_last
+        unique_count
+        grouped_unique_count
+        sum
+        sum_date_breakdown
+        grouped_sum
+        sum_precise_total_amount_cents
+        grouped_sum_precise_total_amount_cents
+        prorated_sum
+        grouped_prorated_sum
+        weighted_sum
+        weighted_sum_breakdown
+        grouped_weighted_sum
+      ]
+  end
+
+  context "with deduplication" do
+    it_behaves_like "an event store", with_event_duplication: true,
+      excluding_features: %i[
+        events
+        events_values
+        last_event
+        grouped_last_event
+        distinct_codes
+        distinct_charges_and_filters
+        prorated_events_values
+        active_unique_property?
+        grouped_count
+        grouped_max
+        last
+        grouped_last
+        unique_count
+        grouped_unique_count
+        sum
+        sum_date_breakdown
+        grouped_sum
+        sum_precise_total_amount_cents
+        grouped_sum_precise_total_amount_cents
+        prorated_sum
+        grouped_prorated_sum
+        weighted_sum
+        weighted_sum_breakdown
+        grouped_weighted_sum
+      ]
+  end
+end
