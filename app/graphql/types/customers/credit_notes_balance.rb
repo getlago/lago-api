6a936f276d4fe4e5ba5608fd4fe1a73765096b29
Lovro Colic <lovro.colic.lc@gmail.com>
2026-03-25 14:49:19 +0100

    fix linter issues

--- app/graphql/types/customers/credit_notes_balance.rb
+++ app/graphql/types/customers/credit_notes_balance.rb
@@ -5,8 +5,8 @@ module Types
     class CreditNotesBalance < Types::BaseObject
       graphql_name "CustomerCreditNotesBalance"
 
-      field :currency, Types::CurrencyEnum, null: false
       field :amount_cents, GraphQL::Types::BigInt, null: false
+      field :currency, Types::CurrencyEnum, null: false
     end
   end
 end
