5ed8455e54bbd74795d7bdd740b5b6ec90d15ac6
Romain Sempé <romain@getlago.com>
2026-03-26 12:14:01 +0100

    fix(products): Increment intervals_billed in update_next_billing_date!

--- spec/models/subscription_rate_schedule_spec.rb
+++ spec/models/subscription_rate_schedule_spec.rb
@@ -31,4 +31,81 @@ RSpec.describe SubscriptionRateSchedule do
         .is_greater_than_or_equal_to(0)
     end
   end
+
+  describe "#update_next_billing_date!" do
+    subject(:srs) do
+      create(:subscription_rate_schedule,
+        rate_schedule:,
+        started_at: Date.new(2026, 1, 1),
+        intervals_billed: 0)
+    end
+
+    let(:rate_schedule) { create(:rate_schedule, billing_interval_unit: "month", billing_interval_count: 1) }
+
+    it "increments intervals_billed and sets next_billing_date" do
+      srs.update_next_billing_date!
+
+      expect(srs.intervals_billed).to eq(1)
+      expect(srs.next_billing_date).to eq(Date.new(2026, 2, 1))
+    end
+
+    it "computes correctly after multiple billings" do
+      3.times { srs.update_next_billing_date! }
+
+      expect(srs.intervals_billed).to eq(3)
+      expect(srs.next_billing_date).to eq(Date.new(2026, 4, 1))
+    end
+
+    context "when billing_interval_unit is week" do
+      let(:rate_schedule) { create(:rate_schedule, billing_interval_unit: "week", billing_interval_count: 1) }
+
+      it "advances by weeks" do
+        srs.update_next_billing_date!
+
+        expect(srs.next_billing_date).to eq(Date.new(2026, 1, 8))
+      end
+    end
+
+    context "when billing_interval_unit is day" do
+      let(:rate_schedule) { create(:rate_schedule, billing_interval_unit: "day", billing_interval_count: 1) }
+
+      it "advances by days" do
+        srs.update_next_billing_date!
+
+        expect(srs.next_billing_date).to eq(Date.new(2026, 1, 2))
+      end
+    end
+
+    context "when billing_interval_unit is year" do
+      let(:rate_schedule) { create(:rate_schedule, billing_interval_unit: "year", billing_interval_count: 1) }
+
+      it "advances by years" do
+        srs.update_next_billing_date!
+
+        expect(srs.next_billing_date).to eq(Date.new(2027, 1, 1))
+      end
+    end
+
+    context "when billing_interval_count is greater than 1" do
+      let(:rate_schedule) { create(:rate_schedule, billing_interval_unit: "month", billing_interval_count: 3) }
+
+      it "advances by multiple intervals" do
+        srs.update_next_billing_date!
+
+        expect(srs.intervals_billed).to eq(1)
+        expect(srs.next_billing_date).to eq(Date.new(2026, 4, 1))
+      end
+    end
+
+    context "when started_at is nil" do
+      subject(:srs) do
+        create(:subscription_rate_schedule, rate_schedule:, started_at: nil, intervals_billed: 0)
+      end
+
+      it "does nothing" do
+        expect { srs.update_next_billing_date! }.not_to change(srs, :next_billing_date)
+        expect { srs.update_next_billing_date! }.not_to change(srs, :intervals_billed)
+      end
+    end
+  end
 end
