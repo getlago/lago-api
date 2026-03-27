6d82ef7f4c459ddf5a6614b5ceb58ee94ba3d44a
Miguel Pinto <darkymiguel@gmail.com>
2026-03-27 14:36:20 +0000

    wip: added customer timezone specs

--- app/services/rate_schedules/organization_billing_service.rb
+++ app/services/rate_schedules/organization_billing_service.rb
@@ -29,12 +29,8 @@ module RateSchedules
       organization.subscription_rate_schedules
         .active
         .joins(subscription: {customer: :billing_entity})
-        .where(
-          "subscription_rate_schedules.next_billing_date <= DATE(:billing_at::timestamptz "\
-          "AT TIME ZONE COALESCE(customers.timezone, billing_entities.timezone, 'UTC'))",
-          billing_at:
-        )
-        .where("subscription_rate_schedules.intervals_to_bill IS NULL "\
+        .where("subscription_rate_schedules.next_billing_date <= DATE((:billing_at)#{at_time_zone})", billing_at:)
+        .where("subscription_rate_schedules.intervals_to_bill IS NULL " \
           "OR subscription_rate_schedules.intervals_billed < subscription_rate_schedules.intervals_to_bill")
         .includes(subscription: :customer)
     end
