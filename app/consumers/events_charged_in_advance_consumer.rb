ca7a7018ba20d5fda0cf5bd100cbfa386a1e96a8
Vincent Pochet <vincent@getlago.com>
2026-03-27 14:45:31 +0100

    misc(clickhouse): Add delay for pay in advance handling

--- app/consumers/events_charged_in_advance_consumer.rb
+++ app/consumers/events_charged_in_advance_consumer.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class EventsChargedInAdvanceConsumer < ApplicationConsumer
+  # Give clickhouse the time to consume and merge the event processed on the events processor side
+  CLICKHOUSE_MERGE_DELAY = 15.seconds
+
   def consume
     messages.each do |message|
-      Events::PayInAdvanceJob.perform_later(message.payload)
+      Events::PayInAdvanceJob.set(wait: CLICKHOUSE_MERGE_DELAY).perform_later(message.payload)
     end
   end
 end
