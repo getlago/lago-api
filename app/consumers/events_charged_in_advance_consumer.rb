591db4eb44b872467369f939ff308ee844bae583
Vincent Pochet <vincent@getlago.com>
2026-03-27 09:05:02 +0100

    Allign payed in advance events with the cache refresh delay

--- app/consumers/events_charged_in_advance_consumer.rb
+++ app/consumers/events_charged_in_advance_consumer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class EventsChargedInAdvanceConsumer < ApplicationConsumer
+  CHARGE_CACHE_EXPIRATION_DELAY = 15.seconds
+
   def consume
     messages.each do |message|
-      Events::PayInAdvanceJob.perform_later(message.payload)
+      Events::PayInAdvanceJob.set(wait: CHARGE_CACHE_EXPIRATION_DELAY).perform_later(message.payload)
     end
   end
 end
