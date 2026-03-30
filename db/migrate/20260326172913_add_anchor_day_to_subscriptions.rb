20e2afca87fffbbe9fdc37d4d793ac15656235b9
Romain Sempé <romain@getlago.com>
2026-03-30 16:14:11 +0200

    refactor(products): Rename anchor_date to billing_anchor_date

--- db/migrate/20260326172913_add_anchor_day_to_subscriptions.rb
+++ db/migrate/20260326172913_add_anchor_day_to_subscriptions.rb
@@ -2,6 +2,6 @@
 
 class AddAnchorDayToSubscriptions < ActiveRecord::Migration[8.0]
   def change
-    add_column :subscriptions, :anchor_date, :date
+    add_column :subscriptions, :billing_anchor_date, :date
   end
 end
