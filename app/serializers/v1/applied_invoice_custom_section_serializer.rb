079983919006a42e193dd7b18f3fabcfed603c1a
Mario Diniz <mariohenriquediniz@gmail.com>
2026-04-08 15:44:42 -0300

    feat(api): Nest invoice custom section in applied section serializer

--- app/serializers/v1/applied_invoice_custom_section_serializer.rb
+++ app/serializers/v1/applied_invoice_custom_section_serializer.rb
@@ -5,9 +5,17 @@ module V1
     def serialize
       {
         lago_id: model.id,
-        invoice_custom_section_id: model.invoice_custom_section_id,
-        created_at: model.created_at.iso8601
+        created_at: model.created_at.iso8601,
+        invoice_custom_section:
       }
+    end
+
+    private
+
+    def invoice_custom_section
+      ::V1::InvoiceCustomSectionSerializer.new(
+        model.invoice_custom_section
+      ).serialize
     end
   end
 end
--- spec/serializers/v1/applied_invoice_custom_section_serializer_spec.rb
+++ spec/serializers/v1/applied_invoice_custom_section_serializer_spec.rb
@@ -11,13 +11,21 @@ RSpec.describe V1::AppliedInvoiceCustomSectionSerializer do
   end
 
   describe "#serialize" do
+    let(:invoice_custom_section) { applied_invoice_custom_section.invoice_custom_section }
+
     it "serializes the applied invoice custom section correctly" do
       serialized_data = serializer.serialize
 
-      expect(serialized_data).to include(
-        lago_id: applied_invoice_custom_section.id,
-        invoice_custom_section_id: applied_invoice_custom_section.invoice_custom_section_id,
-        created_at: applied_invoice_custom_section.created_at.iso8601
+      expect(serialized_data[:lago_id]).to eq(applied_invoice_custom_section.id)
+      expect(serialized_data[:created_at]).to eq(applied_invoice_custom_section.created_at.iso8601)
+      expect(serialized_data[:invoice_custom_section]).to eq(
+        lago_id: invoice_custom_section.id,
+        code: invoice_custom_section.code,
+        name: invoice_custom_section.name,
+        description: invoice_custom_section.description,
+        details: invoice_custom_section.details,
+        display_name: invoice_custom_section.display_name,
+        organization_id: invoice_custom_section.organization_id
       )
     end
   end
