134f1ae8f40c34db562a8707beddb84daa010440
Ancor Cruz <hello@ancorcruz.com>
2026-04-15 12:29:00 +0100

    refactor(subscriptions): use ActivateService in ActivateAllPendingService
    ## Context
    
    ActivateAllPendingService contained inline activation logic that is now
    encapsulated in the extracted ActivateService.
    
    ## Description
    
    Replace the inline activation logic (mark_as_active, emit fixed charge
    events, hubspot sync, webhooks, billing) with a single call to
    ActivateService for each pending subscription.

--- app/services/subscriptions/activate_all_pending_service.rb
+++ app/services/subscriptions/activate_all_pending_service.rb
@@ -21,26 +21,7 @@ module Subscriptions
           Time.zone.at(timestamp)
         )
         .find_each do |subscription|
-          subscription.mark_as_active!(Time.zone.at(timestamp))
-          fixed_charge_timestamp = subscription.started_at + 1.second
-
-          EmitFixedChargeEventsService.call!(
-            subscriptions: [subscription],
-            timestamp: fixed_charge_timestamp
-          )
-
-          if subscription.should_sync_hubspot_subscription?
-            Integrations::Aggregator::Subscriptions::Hubspot::UpdateJob.perform_later(subscription:)
-          end
-
-          SendWebhookJob.perform_later("subscription.started", subscription)
-          Utils::ActivityLog.produce(subscription, "subscription.started")
-
-          if subscription.plan.pay_in_advance? && !subscription.in_trial_period?
-            BillSubscriptionJob.perform_later([subscription], timestamp, invoicing_reason: :subscription_starting)
-          elsif subscription.fixed_charges.pay_in_advance.any?
-            Invoices::CreatePayInAdvanceFixedChargesJob.perform_later(subscription, fixed_charge_timestamp)
-          end
+          ActivateService.call!(subscription:, timestamp: Time.zone.at(timestamp))
         end
 
       result
