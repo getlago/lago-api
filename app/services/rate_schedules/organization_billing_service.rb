67af9c981947359453c401dc5cdfc983056eeadf
Miguel Pinto <darkymiguel@gmail.com>
2026-04-17 15:41:11 +0100

    WIP rewrite the logic to be future prof

--- app/services/rate_schedules/organization_billing_service.rb
+++ app/services/rate_schedules/organization_billing_service.rb
@@ -13,9 +13,9 @@ module RateSchedules
 
     def call
       billable_subscription_rate_schedules
-        .group_by { |srs| srs.subscription.customer_id }
-        .each_value do |customer_srs|
-          BillRateSchedulesJob.perform_later(customer_srs.map(&:id), billing_at.to_i)
+        .group_by { |srs| srs.subscription.customer }
+        .each do |customer, customer_srs|
+          BillRateSchedulesJob.perform_later(customer, customer_srs.map(&:id), billing_at.to_i)
         end
 
       result
@@ -26,17 +26,14 @@ module RateSchedules
     attr_reader :organization, :billing_at
 
     # NOTE: SubscriptionRateSchedules eligible to be billed now:
+    # - next_billing_date is set (nil means nothing more to bill)
     # - next_billing_date has been reached (in the customer/billing entity TZ)
-    # - billing cycle limit not yet exhausted
     def billable_subscription_rate_schedules
       organization.subscription_rate_schedules
         .joins(subscription: {customer: :billing_entity})
+        .where.not(next_billing_date: nil)
         .where(<<~SQL.squish, billing_at:)
-          subscription_rate_schedules.next_billing_date <= DATE((:billing_at)#{at_time_zone})
-        SQL
-        .where(<<~SQL.squish)
-          subscription_rate_schedules.intervals_to_bill IS NULL
-          OR subscription_rate_schedules.intervals_billed < subscription_rate_schedules.intervals_to_bill
+          subscription_rate_schedules.next_billing_date = DATE((:billing_at)#{at_time_zone})
         SQL
         .includes(subscription: :customer)
     end
