167f51219b3e3558e0efff039ecc7ae312efb713
Nikita M. <D1353L@users.noreply.github.com>
2026-04-22 17:29:14 +0200

    Fix linter

--- db/migrate/20260422153456_backfill_invalid_coupon_frequency_duration.rb
+++ db/migrate/20260422153456_backfill_invalid_coupon_frequency_duration.rb
@@ -17,30 +17,30 @@ class BackfillInvalidCouponFrequencyDuration < ActiveRecord::Migration[8.0]
     AppliedCoupon
       .where(frequency: AppliedCoupon.frequencies[:recurring], frequency_duration_remaining: nil)
       .where("frequency_duration > 0")
-      .update_all("frequency_duration_remaining = frequency_duration, updated_at = NOW()")
+      .update_all("frequency_duration_remaining = frequency_duration, updated_at = NOW()") # rubocop:disable Rails/SkipsModelValidations
   end
 
   def backfill_invalid_applied_coupons
     AppliedCoupon
       .where(frequency: AppliedCoupon.frequencies[:recurring])
       .where(frequency_duration: nil)
-      .update_all(frequency_duration: 1, frequency_duration_remaining: 1, updated_at: Time.current)
+      .update_all(frequency_duration: 1, frequency_duration_remaining: 1, updated_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
 
     AppliedCoupon
       .where(frequency: AppliedCoupon.frequencies[:recurring])
       .where("frequency_duration < 0")
-      .update_all(frequency_duration: 0, updated_at: Time.current)
+      .update_all(frequency_duration: 0, updated_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
   end
 
   def backfill_invalid_coupons
     Coupon
       .where(frequency: Coupon.frequencies[:recurring], status: Coupon.statuses[:active])
       .where("frequency_duration IS NULL")
-      .update_all(frequency_duration: 1, updated_at: Time.current)
+      .update_all(frequency_duration: 1, updated_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
 
     Coupon
       .where(frequency: Coupon.frequencies[:recurring], status: Coupon.statuses[:active])
       .where("frequency_duration < 0")
-      .update_all(frequency_duration: 0, updated_at: Time.current)
+      .update_all(frequency_duration: 0, updated_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
   end
 end
