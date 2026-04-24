71dfb8babb0adea7c16f053c7a3afccf2fb4dd52
Lovro Colic <lovro.colic.lc@gmail.com>
2026-04-24 15:29:04 +0200

    add batching

--- db/migrate/20260424131927_backfill_dunning_currency_attempts.rb
+++ db/migrate/20260424131927_backfill_dunning_currency_attempts.rb
@@ -4,13 +4,13 @@ class BackfillDunningCurrencyAttempts < ActiveRecord::Migration[8.0]
   def up
     # Seed JSONB from existing single-currency counter to prevent
     # re-dunning customers who already reached max_attempts
-    safety_assured do
-      execute <<~SQL
-        UPDATE customers
-        SET dunning_currency_attempts = jsonb_build_object(currency, last_dunning_campaign_attempt)
-        WHERE last_dunning_campaign_attempt > 0
-          AND currency IS NOT NULL
-      SQL
+    ::Customer
+      .where("last_dunning_campaign_attempt > 0 AND currency IS NOT NULL")
+      .find_in_batches(batch_size: 1000) do |batch|
+      safety_assured do
+        ::Customer.where(id: batch.map(&:id))
+          .update_all("dunning_currency_attempts = jsonb_build_object(currency, last_dunning_campaign_attempt)") # rubocop:disable Rails/SkipsModelValidations
+      end
     end
   end
 
