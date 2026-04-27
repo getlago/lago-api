1d08763a629dc715ff7692af0e15d35187f98c3e
Nikita M. <D1353L@users.noreply.github.com>
2026-04-27 10:31:26 +0200

    Rebase fix-fees-add-original-fee-id

--- db/migrate/20260416150517_add_original_fee_id_to_fees.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddOriginalFeeIdToFees < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
-
-  def change
-    add_reference :fees, :original_fee, type: :uuid, index: {algorithm: :concurrently}
-  end
-end
