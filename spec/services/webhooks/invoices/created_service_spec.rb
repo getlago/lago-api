6b7f45d872b8f61d117ac4b64d2f218f16008a8a
Tiago Lupepic <tiagolpc@gmail.com>
2026-04-27 15:09:06 +0200

    Update webhooks to also cover presentation_breakdowns

--- spec/services/webhooks/invoices/created_service_spec.rb
+++ spec/services/webhooks/invoices/created_service_spec.rb
@@ -11,11 +11,28 @@ RSpec.describe Webhooks::Invoices::CreatedService do
   let(:invoice) { create(:invoice, customer:, organization:) }
 
   before do
-    create_list(:fee, 4, invoice:)
+    create_list(:fee, 1, invoice:, presentation_breakdowns: [create(:presentation_breakdown)])
+    create_list(:fee, 3, invoice:)
     create_list(:credit, 4, invoice:)
   end
 
   describe ".call" do
     it_behaves_like "creates webhook", "invoice.created", "invoice", {"fees" => Array, "credits" => Array}
+
+    it "includes presentation_breakdowns in fees" do
+      webhook_service.call
+
+      webhook = Webhook.order(created_at: :desc).first
+      fees = webhook.payload["invoice"]["fees"]
+
+      expect(fees).to include(
+        hash_including(
+          "presentation_breakdowns" => [
+            hash_including("presentation_by" => {"department" => "engineering"}, "units" => "60.0")
+          ]
+        ),
+        hash_including("presentation_breakdowns" => [])
+      )
+    end
   end
 end
