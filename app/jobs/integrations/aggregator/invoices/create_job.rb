69177d3b47b29b199634457fa3686fd4640bbdd5
osmarluz <osmar.luz@gmail.com>
2026-05-11 11:07:50 +0200

    Revert "feat(netsuite-integration): temporarily apply spaced retry logic to job"
    This reverts commit ae929a5245993cf3f8e8812cde2128965a04a0d1.

--- app/jobs/integrations/aggregator/invoices/create_job.rb
+++ app/jobs/integrations/aggregator/invoices/create_job.rb
@@ -6,35 +6,15 @@ module Integrations
       class CreateJob < ApplicationJob
         include ConcurrencyThrottlable
 
-        READ_TIMEOUT_WAIT_BY_PROVIDER_KEY = {
-          "netsuite" => 6.minutes
-        }.freeze
-
         queue_as "integrations"
 
         unique :until_executed, on_conflict: :log
 
         retry_on LagoHttpClient::HttpError, wait: :polynomially_longer, attempts: 3
+        retry_on Net::ReadTimeout, wait: :polynomially_longer, attempts: 6
         retry_on RequestLimitError, wait: :polynomially_longer, attempts: 100
         retry_on BaseService::ThrottlingError, wait: :polynomially_longer, attempts: 25
         discard_on BaseService::NonRetryableFailure
-
-        rescue_from(Net::ReadTimeout) do |error|
-          attempts = 6
-          executions_count = executions_for([Net::ReadTimeout])
-
-          if executions_count >= attempts
-            instrument :retry_stopped, error:
-            raise
-          end
-
-          wait_strategy = READ_TIMEOUT_WAIT_BY_PROVIDER_KEY.fetch(integration_provider_key, :polynomially_longer)
-
-          retry_job(
-            wait: determine_delay(seconds_or_duration_or_algorithm: wait_strategy, executions: executions_count),
-            error: error
-          )
-        end
 
         def perform(invoice:)
           if executions > 1
@@ -45,13 +25,6 @@ module Integrations
 
           result = Integrations::Aggregator::Invoices::CreateService.call(invoice:)
           result.raise_if_error!
-        end
-
-        private
-
-        def integration_provider_key
-          invoice = arguments.first[:invoice]
-          invoice&.customer&.integration_customers&.accounting_kind&.first&.integration&.provider_key
         end
       end
     end
