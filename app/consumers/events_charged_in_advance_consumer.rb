73465ac891b522dea6d007aaa8d8041f7d67be58
Vincent Pochet <vincent@getlago.com>
2026-05-11 14:41:44 +0200

    misc(consumer): Temporary ignore organization id

--- app/consumers/events_charged_in_advance_consumer.rb
+++ app/consumers/events_charged_in_advance_consumer.rb
@@ -3,6 +3,8 @@
 class EventsChargedInAdvanceConsumer < ApplicationConsumer
   def consume
     messages.each do |message|
+      next if message.payload["organization_id"] == ENV["LAGO_SKIPPED_ORGANIZATION_ID"]
+
       Events::PayInAdvanceJob.set(wait: Events::Stores::ClickhouseStore::CLICKHOUSE_MERGE_DELAY).perform_later(message.payload)
     end
   end
