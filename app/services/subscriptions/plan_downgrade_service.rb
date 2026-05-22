b819d4f3c2bfe69b9bb64d0d8d27a742e8e436d4
Lovro Colic <lovro.colic.lc@gmail.com>
2026-05-22 11:24:14 +0200

    fix merge conflict

--- app/services/subscriptions/plan_downgrade_service.rb
+++ app/services/subscriptions/plan_downgrade_service.rb
@@ -37,7 +37,8 @@ module Subscriptions
           status: :pending,
           billing_time: current_subscription.billing_time,
           ending_at: params.key?(:ending_at) ? params[:ending_at] : current_subscription.ending_at,
-          progressive_billing_disabled: params[:progressive_billing_disabled] || false
+          progressive_billing_disabled: params[:progressive_billing_disabled] || false,
+          consolidate_invoice: params.key?(:consolidate_invoice) ? params[:consolidate_invoice] : current_subscription.consolidate_invoice
         )
 
         if params.key?(:payment_method)
