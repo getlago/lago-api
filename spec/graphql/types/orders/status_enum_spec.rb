939c1b2aed7ba9cc727936144c59d63bde9fe32c
Thomas Battiston <thomas@getlago.com>
2026-06-15 17:43:10 +0200

    test(orders): assert enum values with .values

--- spec/graphql/types/orders/status_enum_spec.rb
+++ spec/graphql/types/orders/status_enum_spec.rb
@@ -4,6 +4,6 @@ require "rails_helper"
 
 RSpec.describe Types::Orders::StatusEnum do
   it "exposes all enum values" do
-    expect(described_class.values.keys).to match_array(Order::STATUSES.keys.map(&:to_s))
+    expect(described_class.values.keys).to match_array(Order::STATUSES.values)
   end
 end
