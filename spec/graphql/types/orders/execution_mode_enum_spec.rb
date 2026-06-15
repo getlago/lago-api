081621a29371f715ba23d9edcc45e73fb3a3086c
Thomas Battiston <thomas@getlago.com>
2026-06-15 17:41:07 +0200

    test(orders): cover Order GraphQL types

--- spec/graphql/types/orders/execution_mode_enum_spec.rb
+++ spec/graphql/types/orders/execution_mode_enum_spec.rb
@@ -4,6 +4,6 @@ require "rails_helper"
 
 RSpec.describe Types::Orders::ExecutionModeEnum do
   it "exposes all enum values" do
-    expect(described_class.values.keys).to match_array(OrderForms::MarkAsSignedService::EXECUTION_MODES)
+    expect(described_class.values.keys).to match_array(Order::EXECUTION_MODES.keys.map(&:to_s))
   end
 end
