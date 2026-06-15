311d416e10a175537b7e7d98c2835b8853a28900
Ancor Cruz <hello@ancorcruz.com>
2026-06-15 16:08:35 +0100

    fix linter

--- db/migrate/20260611170450_remove_cancelation_reason_from_subscriptions.rb
+++ db/migrate/20260611170450_remove_cancelation_reason_from_subscriptions.rb
@@ -3,7 +3,7 @@
 class RemoveCancelationReasonFromSubscriptions < ActiveRecord::Migration[8.0]
   def change
     safety_assured do
-      remove_column :subscriptions, :cancelation_reason, :subscription_cancelation_reasons
+      remove_column :subscriptions, :cancelation_reason, :subscription_cancelation_reasons # rubocop:disable Lago/NoDropColumnOrTable
       drop_enum :subscription_cancelation_reasons, %w[payment_failed timeout]
     end
   end
