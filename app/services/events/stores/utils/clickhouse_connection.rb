922102a71f467de21d6c66fc0cd1cfb525c1d3dd
Vincent Pochet <vincent@getlago.com>
2026-06-22 11:09:00 +0200

    fix(Clickhouse): Handle mempory issues for pay in advance events

--- app/services/events/stores/utils/clickhouse_connection.rb
+++ app/services/events/stores/utils/clickhouse_connection.rb
@@ -5,6 +5,7 @@ module Events
     module Utils
       class ClickhouseConnection
         MAX_RETRIES = 3
+        MEMORY_ERROR_CODE = "MEMORY_LIMIT_EXCEEDED"
 
         def self.with_retry(&)
           attempts = 0
@@ -13,7 +14,9 @@ module Events
             attempts += 1
 
             yield
-          rescue Errno::ECONNRESET, ActiveRecord::ActiveRecordError, NoMethodError
+          rescue Errno::ECONNRESET, ActiveRecord::ActiveRecordError, NoMethodError => e
+            raise Events::Stores::Clickhouse::MemoryLimitError, e.message if memory_limit_error?(e)
+
             if attempts < MAX_RETRIES
               sleep(0.05)
               retry
@@ -29,13 +32,21 @@ module Events
           begin
             attempts += 1
             ::Clickhouse::BaseRecord.with_connection(&)
-          rescue Errno::ECONNRESET, ActiveRecord::ActiveRecordError, NoMethodError
+          rescue Errno::ECONNRESET, ActiveRecord::ActiveRecordError, NoMethodError => e
+            raise Events::Stores::Clickhouse::MemoryLimitError, e.message if memory_limit_error?(e)
+
             if attempts < MAX_RETRIES
               sleep(0.05)
               retry
             end
             raise
           end
+        end
+
+        def self.memory_limit_error?(error)
+          return false unless error.is_a?(ActiveRecord::ActiveRecordError)
+
+          error.message.include?(MEMORY_ERROR_CODE)
         end
       end
     end
