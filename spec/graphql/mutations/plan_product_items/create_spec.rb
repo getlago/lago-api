15a055971efce17c04a9f5b68bae0b999e2b3a40
Romain Sempé <romain@getlago.com>
2026-06-30 14:04:52 +0200

    feat(products): add plan product item GraphQL
    ## Context
    
    Expose plan product items over GraphQL so the app can assign a product
    item to a plan and list a plan's pricing items.
    
    ## Description
    
    Add the PlanProductItem object type (with product_item, rate_card and a
    rate_phases_count), the CreatePlanProductItem mutation (plans:update),
    and the planProductItems resolver (plans:view), wired into the schema.

---
+++ spec/graphql/mutations/plan_product_items/create_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Mutations::PlanProductItems::Create do
+  subject(:execution) do
+    execute_graphql(
+      current_user: membership.user,
+      current_organization: organization,
+      permissions: required_permission,
+      query: mutation,
+      variables: {input: {planId: plan.id, rateCardCode: rate_card.code, units: 10.0}}
+    )
+  end
+
+  let(:required_permission) { "plans:update" }
+  let(:membership) { create(:membership) }
+  let(:organization) { membership.organization }
+  let(:plan) { create(:plan, organization:) }
+  let(:rate_card) { create(:rate_card, organization:) }
+
+  let(:mutation) do
+    <<~GQL
+      mutation($input: CreatePlanProductItemInput!) {
+        createPlanProductItem(input: $input) {
+          id
+          units
+          ratePhasesCount
+          productItem { id }
+          rateCard { id }
+        }
+      }
+    GQL
+  end
+
+  it_behaves_like "requires current user"
+  it_behaves_like "requires current organization"
+  it_behaves_like "requires permission", "plans:update"
+
+  it "assigns the product item to the plan with a default rate phase" do
+    response = execution["data"]["createPlanProductItem"]
+
+    expect(response["productItem"]["id"]).to eq(rate_card.product_item.id)
+    expect(response["rateCard"]["id"]).to eq(rate_card.id)
+    expect(response["units"]).to eq(10.0)
+    expect(response["ratePhasesCount"]).to eq(1)
+  end
+end
