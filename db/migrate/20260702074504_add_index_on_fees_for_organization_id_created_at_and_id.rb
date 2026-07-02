c6373080814e5decb2e0e1669c31ff14230aa87c
Yohan R. <7434196+groyoh@users.noreply.github.com>
2026-07-02 09:14:39 +0200

    refactor: rename fees index migration for clarity

--- db/migrate/20260702074504_add_index_on_fees_for_organization_id_created_at_and_id.rb
+++ db/migrate/20260702074504_add_index_on_fees_for_organization_id_created_at_and_id.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddIndexOnFeesForOrganizationIdAndCreatedAt < ActiveRecord::Migration[8.0]
+class AddIndexOnFeesForOrganizationIdCreatedAtAndId < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
