9cb8c35bbcabc257fc60b138830e84cd6852e8dc
sudhanshu112233shukla <shukla1243sudhanshu@gmail.com>
2026-07-03 21:53:36 +0530

    Fix migration for existing databases

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -5,8 +5,7 @@ if [ "$RAILS_ENV" == "staging" ]
 then
   bundle exec rake db:prepare
 else
-  bundle exec rake db:create
-  bundle exec rails db:migrate
+  bundle exec rails db:prepare
   bundle exec rails roles:seed_predefined
 
   if [ -v LAGO_CREATE_ORG ] && [ "$LAGO_CREATE_ORG" == "true" ]
