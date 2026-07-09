7075d99f4870b04f178b70e590dcbc0c6d7652cb
Vincent Pochet <vincent@getlago.com>
2026-07-09 16:00:25 +0200

    misc: Add TypedResult concern for legacy services

---
+++ app/services/typed_results.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# TEMPORARY bridge for legacy multi-method services not yet migrated to the single
+# `call` + typed `Result` pattern.
+#
+# Include the concern and declare a `RESULTS` constant mapping each public
+# method to the typed Result it should return:
+#
+#   class Auth::GoogleService < BaseService
+#     include TypedResults
+#
+#     RESULTS = {
+#       authorize_url: BaseResult[:url],
+#       login: BaseResult[:user, :token],
+#       register_user: BaseResult[:user, :organization, :membership, :token],
+#       accept_invite: Invites::AcceptService::Result
+#     }.freeze
+#
+#     def login(code) = ...
+#   end
+#
+# The class-level `call` returns a typed Result instead of the
+# deprecated LegacyResult (OpenStruct):
+#
+#   Auth::GoogleService.call(:login, code)
+#   Auth::GoogleService.call(:register_user, code, organization_name)
+#
+# The service is built without constructor arguments; all arguments are
+# forwarded to the target method.
+module TypedResults
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def call(method_name, *args, **kwargs, &block)
+      result_class = self::RESULTS.fetch(method_name) do
+        raise ArgumentError, "#{name}: #{method_name.inspect} is not declared in RESULTS"
+      end
+
+      instance = new
+      instance.send(:result=, result_class.new)
+      instance.send(:with_middlewares) do
+        instance.public_send(method_name, *args, **kwargs, &block)
+      end
+    end
+
+    def call!(method_name, *args, **kwargs, &block)
+      call(method_name, *args, **kwargs, &block).raise_if_error!
+    end
+  end
+end
