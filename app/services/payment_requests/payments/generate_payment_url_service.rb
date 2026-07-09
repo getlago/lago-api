getlago/lago-api

commit 80c7ff2c00c89944f044fc4f5463977772d00305

[STA-9] misc: Define more service Results (#5873)
## Context

Following https://github.com/getlago/lago-api/pull/5859, this PR is part
of the long standing epic required to enable YJIT on Lago by removing
all remaining usage of `OpenStruct`

## Description

The goal of this changes is to define the `Result` for a lot of new
service class to avoid using the `LegacyResult` (an `OpenStruct`) that
will be definitely removed when all services will be migrated to the new
approach.

app/services/payment_requests/payments/generate_payment_url_service.rb
@@ -3,6 +3,8 @@
 module PaymentRequests
   module Payments
     class GeneratePaymentUrlService < BaseService
+      Result = BaseResult
+
       include Customers::PaymentProviderFinder
 
       PROVIDER_GOCARDLESS = "gocardless"

spec/services/payment_requests/payments/generate_payment_url_service_spec.rb
@@ -113,6 +113,8 @@ RSpec.describe PaymentRequests::Payments::GeneratePaymentUrlService do
     end
 
     before do
+      provider_customer
+
       allow(PaymentRequests::Payments::CashfreeService)
         .to receive(:new)
         .and_return(payment_provider_service)
@@ -143,7 +145,7 @@ RSpec.describe PaymentRequests::Payments::GeneratePaymentUrlService do
   end
 
   context "when provider service return an error" do
-    let(:payment_provider_service) { instance_double(Invoices::Payments::StripeService) }
+    let(:payment_provider_service) { instance_double(PaymentRequests::Payments::StripeService) }
 
     let(:error_result) do
       BaseService::Result.new.tap do |result|
@@ -158,7 +160,9 @@ RSpec.describe PaymentRequests::Payments::GeneratePaymentUrlService do
     end
 
     before do
-      allow(Invoices::Payments::StripeService)
+      provider_customer
+
+      allow(PaymentRequests::Payments::StripeService)
         .to receive(:new)
         .and_return(payment_provider_service)
 

app/services/api_keys/track_usage_service.rb
@@ -2,6 +2,8 @@
 
 module ApiKeys
   class TrackUsageService < BaseService
+    Result = BaseResult
+
     def call
       ApiKey.find_each do |api_key|
         cache_key = "api_key_last_used_#{api_key.id}"

app/services/charges/apply_pay_in_advance_charge_model_service.rb
@@ -2,6 +2,8 @@
 
 module Charges
   class ApplyPayInAdvanceChargeModelService < BaseService
+    Result = BaseResult[:aggregation, :aggregator, :amount, :amount_details, :count, :options, :pay_in_advance_event, :precise_amount, :precise_total_amount_cents, :unit_amount, :units]
+
     def initialize(charge:, aggregation_result:, properties:)
       @charge = charge
       @aggregation_result = aggregation_result

app/services/charges/pay_in_advance/amount_details_calculator.rb
@@ -3,6 +3,8 @@
 module Charges
   module PayInAdvance
     class AmountDetailsCalculator < BaseService
+      Result = BaseResult
+
       AMOUNT_DETAILS_FOR_SINGLE_EVENT_ENABLED = %w[percentage graduated_percentage].freeze
       PERCENTAGE_CHARGE_AMOUNT_DETAILS_KEYS = %i[units free_units paid_units free_events paid_events fixed_fee_total_amount
         min_max_adjustment_total_amount per_unit_total_amount].freeze

app/services/charges/pay_in_advance_aggregation_service.rb
@@ -2,6 +2,8 @@
 
 module Charges
   class PayInAdvanceAggregationService < BaseService
+    Result = BaseResult
+
     def initialize(charge:, boundaries:, properties:, event:, charge_filter: nil)
       @charge = charge
       @boundaries = boundaries

app/services/commitments/calculate_amount_service.rb
@@ -2,6 +2,8 @@
 
 module Commitments
   class CalculateAmountService < BaseService
+    Result = BaseResult[:commitment_amount_cents]
+
     def initialize(commitment:, invoice_subscription:)
       @commitment = commitment
       @invoice_subscription = invoice_subscription

app/services/commitments/calculate_prorated_coefficient_service.rb
@@ -2,6 +2,8 @@
 
 module Commitments
   class CalculateProratedCoefficientService < BaseService
+    Result = BaseResult[:proration_coefficient]
+
     def initialize(commitment:, invoice_subscription:)
       @commitment = commitment
       @invoice_subscription = invoice_subscription

commit 1d11708bce80902c0da28c87a36f61647011503d

fix(payment_request): PaymentRequestMailer empty payment_url (#5893)
## Context

This PR fixes a regression introduced by
https://github.com/getlago/lago-api/pull/5873. When an error happen when
we want to generate a `payment_url` from the `PaymentRequestMailer` the
email must be sent without including the missing url.

The definition of the Result in
`PaymentRequests::Payments::GeneratePaymentUrlService` changed this
behavior as it was not returning a `LegacyResult` anymore in case of
error but a
`PaymentRequests::Payments::GeneratePaymentUrlService::Result` on which
no `payment_url` was defined, leading to this error.

## Description

The fix is simple: 
- Add the `payment_url` attribute to the Result definition so that the
behavior in the mailer remain consistent with what it was
- Avoid returning a result from one of the
`PaymentRequests::Payments::XxxService` in
`PaymentRequests::Payments::GeneratePaymentUrlService`, but the
service's `Result` instance

app/services/payment_requests/payments/generate_payment_url_service.rb
@@ -3,7 +3,7 @@
 module PaymentRequests
   module Payments
     class GeneratePaymentUrlService < BaseService
-      Result = BaseResult
+      Result = BaseResult[:payment_url]
 
       include Customers::PaymentProviderFinder
 
@@ -35,7 +35,8 @@ module PaymentRequests
 
         return result.single_validation_failure!(error_code: "payment_provider_error") if payment_url_result.payment_url.blank?
 
-        payment_url_result
+        result.payment_url = payment_url_result.payment_url
+        result
       rescue BaseService::ThirdPartyFailure => e
         deliver_error_webhook(e)
 

spec/services/payment_requests/payments/generate_payment_url_service_spec.rb
@@ -60,6 +60,7 @@ RSpec.describe PaymentRequests::Payments::GeneratePaymentUrlService do
         expect(result).not_to be_success
         expect(result.error).to be_a(BaseService::ValidationFailure)
         expect(result.error.messages[:base]).to eq(["invalid_payment_status"])
+        expect(result.payment_url).to be_nil
       end
     end
   end