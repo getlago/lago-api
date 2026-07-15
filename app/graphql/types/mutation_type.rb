bd01d6433742430d2313ecff275b79ea6a74c741
Ancor Cruz <hello@ancorcruz.com>
2026-07-15 12:10:28 +0100

    fix linter

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -253,8 +253,8 @@ module Types
     field :create_or_update_subscription_entitlement, mutation: Mutations::Entitlement::CreateOrUpdateSubscriptionEntitlement
     field :remove_subscription_entitlement, mutation: Mutations::Entitlement::RemoveSubscriptionEntitlement
 
-    field :create_ai_conversation, mutation: Mutations::AiConversations::Create
     field :ask_finance_assistant, mutation: Mutations::FinanceAssistant::Ask
+    field :create_ai_conversation, mutation: Mutations::AiConversations::Create
     field :export_finance_assistant_result, mutation: Mutations::FinanceAssistant::Export
   end
 end
