7ba9fde4cb82d64125cd13a0a4eff8e0e0df3a8c
jdenquin <jdenquin@gmail.com>
2026-07-20 11:02:41 +0200

    remove useless comment

--- app/graphql/types/auth/entra_id/authorize.rb
+++ app/graphql/types/auth/entra_id/authorize.rb
@@ -4,8 +4,6 @@ module Types
   module Auth
     module EntraId
       class Authorize < Types::BaseObject
-        # NOTE: Types::Auth::Okta::Authorize already owns the default
-        #       "Authorize" type name, so this one must be explicit.
         graphql_name "EntraIdAuthorize"
 
         field :url, String, null: false
