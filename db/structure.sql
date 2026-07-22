f12d2618530cf535edad29e15ae154a8591e6f6b
Miguel Pinto <darkymiguel@gmail.com>
2026-07-22 18:43:56 +0100

    rebase resolve conflicts

--- db/structure.sql
+++ db/structure.sql
@@ -1474,6 +1474,18 @@ CREATE TYPE public.billing_cycle_status AS ENUM (
 
 
 --
+-- Name: connection_category; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.connection_category AS ENUM (
+    'payment',
+    'tax',
+    'accounting',
+    'crm'
+);
+
+
+--
 -- Name: customer_account_type; Type: TYPE; Schema: public; Owner: -
 --
 
