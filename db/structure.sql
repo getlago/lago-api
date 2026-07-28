1f1bf58ea16c112d5350e2d2d4067986e59bd4a5
Miguel Pinto <darkymiguel@gmail.com>
2026-07-28 12:43:51 +0100

    rebase changes fix

--- db/structure.sql
+++ db/structure.sql
@@ -1439,28 +1439,6 @@ CREATE TYPE public.billable_metric_weighted_interval AS ENUM (
 
 
 --
--- Name: billing_object_connection_behavior; Type: TYPE; Schema: public; Owner: -
---
-
-CREATE TYPE public.billing_object_connection_behavior AS ENUM (
-    'specific',
-    'skip'
-);
-
-
---
--- Name: connection_category; Type: TYPE; Schema: public; Owner: -
---
-
-CREATE TYPE public.connection_category AS ENUM (
-    'payment',
-    'tax',
-    'accounting',
-    'crm'
-);
-
-
---
 -- Name: billing_cycle_status; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -1469,6 +1447,16 @@ CREATE TYPE public.billing_cycle_status AS ENUM (
     'processing',
     'done',
     'failed'
+);
+
+
+--
+-- Name: billing_object_connection_behavior; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.billing_object_connection_behavior AS ENUM (
+    'specific',
+    'skip'
 );
 
 
