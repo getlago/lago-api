084c2a0be15f6489d6aaee30b51d4f5843865ab6
Lovro Colic <lovro.colic.lc@gmail.com>
2026-07-30 15:31:52 +0200

    create payment provider customer in dedicated mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -94,7 +94,9 @@ module Types
     field :generate_checkout_url, mutation: Mutations::PaymentMethods::GenerateCheckoutUrl
     field :set_payment_method_as_default, mutation: Mutations::PaymentMethods::SetAsDefault
 
+    field :create_payment_provider_customer, mutation: Mutations::PaymentProviderCustomers::Create
     field :destroy_payment_provider_customer, mutation: Mutations::PaymentProviderCustomers::Destroy
+
     field :set_integration_customer_as_default, mutation: Mutations::IntegrationCustomers::SetAsDefault
     field :set_payment_provider_customer_as_default, mutation: Mutations::PaymentProviderCustomers::SetAsDefault
     field :update_integration_customer, mutation: Mutations::IntegrationCustomers::Update
