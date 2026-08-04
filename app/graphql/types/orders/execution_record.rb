4b7eca3cad3f1a3ce478c1527c073c4b5b37769e
Thomas Battiston <thomas@getlago.com>
2026-08-04 15:53:36 +0200

    feat(orders): expose the records an execution created

--- app/graphql/types/orders/execution_record.rb
+++ app/graphql/types/orders/execution_record.rb
@@ -10,8 +10,26 @@ module Types
       field :execution_mode, Types::Orders::ExecutionModeEnum, null: true
       field :invoice_id, ID, null: true
 
+      field :applied_coupon_ids, [ID], null: false
+      field :subscription_ids, [ID], null: false
+      field :wallet_ids, [ID], null: false
+
       def errors
         object["errors"] || []
+      end
+
+      # Order types that create none of these, and records written before they existed, have no
+      # such key at all.
+      def applied_coupon_ids
+        object["applied_coupon_ids"] || []
+      end
+
+      def subscription_ids
+        object["subscription_ids"] || []
+      end
+
+      def wallet_ids
+        object["wallet_ids"] || []
       end
     end
   end
