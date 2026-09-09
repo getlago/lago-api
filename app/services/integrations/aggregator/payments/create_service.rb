4efe35e0c2aae8c138081fed4007735993f01631
osmarluz <osmar.luz@gmail.com>
2026-09-09 11:34:57 +0000

    fix(payments-syncs): remove useless advisory lock

--- app/services/integrations/aggregator/payments/create_service.rb
+++ app/services/integrations/aggregator/payments/create_service.rb
@@ -20,35 +20,30 @@ module Integrations
           return result unless integration
           return result unless integration.sync_payments
           return result unless invoice.finalized?
+          return result if payload.integration_payment
 
-          LockService.call(payment:, timeout_seconds: 0, transaction: false) do
-            next if payload.integration_payment
+          throttle!(:netsuite, :xero)
 
-            throttle!(:netsuite, :xero)
+          response = http_client.post_with_response(payload.body, headers)
+          body = JSON.parse(response.body)
 
-            response = http_client.post_with_response(payload.body, headers)
-            body = JSON.parse(response.body)
-
-            if body.is_a?(Hash)
-              process_hash_result(body)
-            else
-              process_string_result(body)
-            end
-
-            next unless result.external_id
-
-            IntegrationResource.create!(
-              organization_id: integration.organization_id,
-              integration:,
-              external_id: result.external_id,
-              syncable_id: payment.id,
-              syncable_type: "Payment",
-              resource_type: :payment
-            )
+          if body.is_a?(Hash)
+            process_hash_result(body)
+          else
+            process_string_result(body)
           end
 
-          result
-        rescue BaseLockService::FailedToAcquireLock
+          return result unless result.external_id
+
+          IntegrationResource.create!(
+            organization_id: integration.organization_id,
+            integration:,
+            external_id: result.external_id,
+            syncable_id: payment.id,
+            syncable_type: "Payment",
+            resource_type: :payment
+          )
+
           result
         rescue LagoHttpClient::HttpError => e
           raise RequestLimitError(e) if request_limit_error?(e)
