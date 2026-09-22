b1d461affd80ce3e02703f935c71e4a473250afd
Jérémy Denquin <jdenquin@users.noreply.github.com>
2026-09-22 11:09:47 +0200

    chore(console): Remove OTEL info at startup (#6457)
    - Remove OTEL spam at console startup

--- config/initializers/open_telemetry.rb
+++ config/initializers/open_telemetry.rb
@@ -3,6 +3,11 @@
 require "opentelemetry/sdk"
 require "opentelemetry/instrumentation/all"
 
+# Set OTEL log level to ERROR in console, unless OTEL_LOG_LEVEL is set
+if defined?(Rails::Console) && ENV["OTEL_LOG_LEVEL"].blank?
+  OpenTelemetry.logger = Logger.new($stdout, level: Logger::ERROR)
+end
+
 OpenTelemetry::SDK.configure(&:use_all) if ENV["OTEL_EXPORTER"].present?
 
 LagoTracer = OpenTelemetry.tracer_provider.tracer("lago")
